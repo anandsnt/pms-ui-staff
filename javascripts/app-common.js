9b071b619e639d7c7ca846752a98097baa29a591
akhilastayntouch <akhila@stayntouch.com>
2013-09-26 16:34:49 +0530

    cico_349 minor changes in search implementaion

--- javascripts/app-common.js
+++ javascripts/app-common.js
@@ -1,28 +1,19 @@
- function load_search_data(url,$query){
-  	$.ajax({
-                type:           "POST",
-                url:            "/dashboard/search_api.json",
-                data:           { $match: $query, fakeDataToAvoidCache: new Date()}, // fakeDataToAvoidCache is iOS Safari fix
-                dataType:       "json",
-                success: function (response) {
-                    $("#search-results").empty().removeClass('hidden');
-                    $('#preloaded-results').addClass('hidden');
-                    $('#no-results').addClass('hidden');
-                    if(response.guests.length>0)
-                    {
-                    	displaySearchResults(response, $query);
-                    }
-                    // No data in JSON file
-                    else
-                    {
-                        $('#search-results').html('<li class="notice">No data</li>');
-                    }
-                },
-                error: function (result) {
-                   console.log(JSON.stringify(result));
-                }
-            });
+jQuery(function($){
+	showGreetings();
+});
 
-  }
-
-
+function showGreetings(){
+	var d = new Date();
+	var time = d.getHours();
+	
+	//Display greetings message
+	if (time < 12){
+		$('#greetings').html('Good Morning');
+	}
+	else if (time > 12 && time < 16){
+		$('#greetings').html('Good Afternoon');
+	}
+	else{
+		$('#greetings').html('Good Evening');
+	}
+}
--- javascripts/app-simplesearch.js
+++ javascripts/app-simplesearch.js
@@ -112,15 +112,44 @@ $(function($){
 
 });
 
-function getFilteredResults($query){
-	displaySearchResults(searchResults, $query);
+function load_search_data(url,$query){
+  	$.ajax({
+                type:           "GET",
+                url:            url + "&query=" + $query,
+                data:           { $match: $query, fakeDataToAvoidCache: new Date()}, // fakeDataToAvoidCache is iOS Safari fix
+                dataType:       "json",
+                success: function (response) {
+                    $("#search-results").empty().removeClass('hidden');
+                    $('#preloaded-results').addClass('hidden');
+                    $('#no-results').addClass('hidden');
+                    if(response.guests.length>0)
+                    {
+                    	searchResults = response;
+                    	displaySearchResults(response, $query);
+                    }
+                    // No data in JSON file
+                    else
+                    {
+                        $('#search-results').html('<li class="notice">No data</li>');
+                    }
+                },
+                error: function (result) {
+                   console.log(JSON.stringify(result));
+                }
+            });
+
 }
 
-function displaySearchResults(response, $query){
+function getFilteredResults($query){
+	$('#search-results').html("");
+	displayFilteredResults(searchResults, $query);
+	
+}
+function displayFilteredResults(searchResults, $query){
     try
     {
         var items=[];
-        $.each(response.guests, function(i,value){
+        $.each(searchResults.guests, function(i,value){
             // Search by name
             if ($query.match(/^([a-zA-Z]+)$/) && (value.firstname.indexOf($query) >= 0 || value.lastname.indexOf($query) >= 0 || value.group.indexOf($query) >= 0))
             {
@@ -152,6 +181,33 @@ function displaySearchResults(response, $query){
             setTimeout(function () {
                 contentScroll.refresh();
             }, 0);
+        });
+    }
+    catch(e)
+    {
+    	console.log(e);
+        $('#search-results').html('<li class="notice">Error occured</li>');
+    }
+
+    // As this search filters JSON content, we need temp custom handling for no results scenario
+    if ($('#search-results').is(':empty'))
+    {
+        $('#search-results').html('<li class="notice">Found nothing</li>');
+    }
+
+}
+
+function displaySearchResults(response, $query){
+    try
+    {
+        var items=[];
+        $.each(response.guests, function(i,value){
+        	
+        items.push($('<li />').html( 
+                    writeSearchResult(value.id,value.firstname,value.lastname,value.image,value.confirmation,value.status,value.room,value.roomstatus,value.roomstatusextra,value.roomstatusexplained,value.location,value.group,value.vip)
+                ));
+
+                $('#search-results').append.apply($('#search-results'),items).highlight($query);
         });
     }
     catch(e)
