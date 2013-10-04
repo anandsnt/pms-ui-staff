b38737f4af602ba60bb2933f646b0ce42488d914
akhilastayntouch <akhila@stayntouch.com>
2013-10-04 16:23:42 +0530

    cico-378 removed unused funtion as per the code review comments

--- javascripts/app-stay-card.js
+++ javascripts/app-stay-card.js
@@ -54,27 +54,6 @@ $(function($){
 	   	}
 	});
 
-	// Show/hide guest card on click
-	$(document).on('click', '#guest-card .ui-resizable-handle', function(){
-		// Show if hidden or open in less than 50% of screen height
-		if ($('#guest-card').height() == '90' || $('#guest-card').height() < $breakpoint)
-		{
-			$('#guest-card').animate({height: ($maxHeight-90)}, 300);
-			$('#guest-card-header .switch-button, #guest-card-content').show();
-		}
-
-		// Hide if open or shown in more than 50% of screen height
-		else
-		{
-			$('#guest-card').animate({height: '90px'}, 300);
-			$('#guest-card-header .switch-button, #guest-card-content').hide();
-		}
-
-		// Refresh scrollers
-	    setTimeout(function(){
-	    	refreshGuestCardScroll();
-	    }, 300);
-    });
 
 	// Reservation card tabs
 	$('.reservation-tabs').each(function(){
