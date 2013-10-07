ab7ae3038a40ae481a0047cfe12253ddc27fd18e
soumya <soumya@stayntouch.com>
2013-10-07 14:34:33 +0530

    cico - 550: HAML view contact information, render values from sample api to contact form, call API only for the first time of click or resize on tab.

---
+++ javascripts/app-guest-card.js
@@ -0,0 +1,94 @@
+$(function($){ 
+
+	// Resizable guest card variables
+	var $maxHeight = $(window).height(),
+		$breakpoint = ($maxHeight/2),
+		$cardHeight = '90px';
+		// Variable used to handle call API only for the first time of click or resize event of guest card window
+		$guestCardClickTime = true;
+	
+	// Show/hide guest card on click
+	$(document).on('click', '#guest-card .ui-resizable-handle', function(){
+	     renderContactInformation();
+		// Show if hidden or open in less than 50% of screen height
+		if ($('#guest-card').height() == '90' || $('#guest-card').height() < $breakpoint)
+		{
+			$('#guest-card').animate({height: ($maxHeight-90)}, 300);
+			$('#guest-card-header .switch-button, #guest-card-content').show();
+		}
+		// Hide if open or shown in more than 50% of screen height
+		else
+		{
+			$('#guest-card').animate({height: '90px'}, 300);
+			$('#guest-card-header .switch-button, #guest-card-content').hide();
+		}
+		// Refresh scrollers
+	    setTimeout(function(){
+	    	refreshGuestCardScroll();
+	    }, 300);
+    });
+
+	// Resize guest card
+	$('#guest-card').resizable({
+		minHeight: 		'90',
+		maxHeight: 		($maxHeight-90), // 90 is height of the guest card visible part
+		handles: 		's',
+		resize: function( event, ui ) {
+			if ($(this).height() > 120) {
+				$('#guest-card-header .switch-button, #guest-card-content').show();
+			}
+			else {
+				$('#guest-card-header .switch-button, #guest-card-content').hide();
+			}
+		},
+	    stop: function(event, ui) {	    	
+	    	$cardHeight = $(this).css('height');
+	    	// Refresh scrollers
+	    	setTimeout(function(){
+		    	callFunctions();
+		    }, 300);
+	   	}
+	});		
+});
+//Functions to be called after resize window complete event
+function callFunctions(){	
+	renderContactInformation();
+	refreshGuestCardScroll();	
+}
+//Function to render the contact information values in the contact form of guest card from API.
+function renderContactInformation(){
+		if($guestCardClickTime){
+			$.ajax({
+				type: "GET",
+	            url: '/dashboard/guestcard.json',
+	            data: {fakeDataToAvoidCache: new Date()}, // fakeDataToAvoidCache is iOS Safari fix
+	            async: false,
+	            success: function(data) {              
+	               $("#guest_firstname").val(data.first_name);
+	               $("#guest_lastname").val(data.last_name);
+	               $("#title").val(data.title);
+	               $("#language").val(data.language);
+	               $("#birthday-month").val(data.birth_month);
+	               $("#birthday-day").val(data.birth_date);
+	               $("#birthday-year").val(data.birth_year);
+	               $("#passport-number").val(data.passport_number);
+	               $("#passport-month").val(data.passport_expiry_month);
+	               $("#passport-year").val(data.passport_expiry_year);
+	               $("#nationality").val(data.nationality);
+	               $("#email").val(data.email_address);
+	               $("#streetname").val(data.address);
+	               $("#city").val(data.city);
+	               $("#postalcode").val(data.postal_code);
+	               $("#state").val(data.state);
+	               $("#country").val(data.country);
+	               $("#phone").val(data.phone);
+	               $("#mobile").val(data.mobile);
+	               $guestCardClickTime = false;
+	            },
+	            error: function(){
+	                console.log("There is an error!!");
+	                $guestCardClickTime = true;
+	            }
+	        });
+	       }
+	}
