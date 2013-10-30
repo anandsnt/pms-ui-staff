32c7d4b853774a4ae36bcabb88c308019959fb20
Dipin <dipin@stayntouch.com>
2013-10-30 12:31:53 +0530

    Changed the reservation details route in staycard.js

--- javascripts/staycard.js
+++ javascripts/staycard.js
@@ -62,7 +62,7 @@ var StayCard = function(viewDom){
     if (!($($href).length > 0)) {
       $.ajax({
         type : 'GET',
-        url : "/dashboard/reservation_details?reservation=" + reservation,
+        url : "staff/dashboard/reservation_details?reservation=" + reservation,
         dataType : 'html',
         timeout : 5000,
         success : function(data) {
