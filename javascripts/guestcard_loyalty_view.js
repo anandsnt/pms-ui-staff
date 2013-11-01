5a7e0d970826301c827772449ec8686a3671e1b6
soumya <soumya@stayntouch.com>
2013-11-01 12:36:49 +0530

    CICO-1071 Chnge event binding in Guest card

--- javascripts/guestcard_loyalty_view.js
+++ javascripts/guestcard_loyalty_view.js
@@ -8,9 +8,38 @@ var GuestcardLoyaltyView = function(domRef){
   }
   this.delegateEvents = function(){
   	console.log("delegateEvents");
-  	this.myDom.find('#loyalty-type-flyer #add-new-ffp').on('click', that.addFFPModal);
-  	this.myDom.find('#loyalty-type-hotel #add-new-hlp').on('click', that.addHLPModal);
-  	this.myDom.find('#loyalty-tab .active-item').on('click', that.deleteLoyaltyModal);
+  	//this.myDom.find('#loyalty-type-flyer #add-new-ffp').on('click', that.addFFPModal);
+  	//this.myDom.find('#loyalty-type-hotel #add-new-hlp').on('click', that.addHLPModal);
+  	//this.myDom.find('#loyalty-tab .active-item').on('click', that.deleteLoyaltyModal);
+  	
+  	that.myDom.find('#loyalty-tab #loyalty-type-flyer a').on('click', that.clickedOnFFP);
+  	that.myDom.find('#loyalty-tab #loyalty-type-hotel a').on('click', that.clickedOnHLP);
+  	
+  }
+  this.clickedOnFFP = function(e){
+  	console.log("clickedOnFFP");
+  	console.log(that.myDom);
+  	
+  	if($(this).hasClass("add-new-button")){
+  		that.addFFPModal();
+  	}
+  	else if($(this).hasClass("active-item")){
+  		that.deleteLoyaltyModal(this);
+  	}
+  }
+  this.clickedOnHLP = function(e){
+  	console.log("clickedOnFFP");
+  	console.log(that.myDom);
+  	console.log(this);
+  	console.log($(this).hasClass("add-new-button"));
+  	console.log($(this).hasClass("active-item"));
+  	
+  	if($(this).hasClass("add-new-button")){
+  		that.addHLPModal();
+  	}
+  	else if($(this).hasClass("active-item")){
+  		that.deleteLoyaltyModal(this);
+  	}
   }
   this.addFFPModal = function(e){
   	console.log("Initialise addFFPModal");
@@ -22,13 +51,14 @@ var GuestcardLoyaltyView = function(domRef){
   	var addHLPModal = new AddHLPModal();
     addHLPModal.initialize();
   }
-  this.deleteLoyaltyModal = function(e){
+  this.deleteLoyaltyModal = function(that){
   	
-  	var loyaltyType = $(this).attr('loyaltytype');
-  	var loyaltyId = $(this).attr('loyaltyid');
+  	var loyaltyType = $(that).attr('loyaltytype');
+  	var loyaltyId = $(that).attr('loyaltyid');
   	
   	var deleteLoyaltyModal = new DeleteLoyaltyModal();
     deleteLoyaltyModal.initialize();
     deleteLoyaltyModal.params = {"loyalty_id":loyaltyId , "loyalty_type":loyaltyType};
+    console.log(deleteLoyaltyModal.params);
   }
 };
