0d970f4311fcee5a2d600cde9f49415175663dc6
Jonathan Herr <jonathan@stayntouch.com>
2013-11-08 09:11:41 +0530

    CICO-1468 Update javascript to use new API.

--- javascripts/app/staycard.js
+++ javascripts/app/staycard.js
@@ -95,30 +95,11 @@ var StayCard = function(viewDom){
     var userId = $("#user_id").val();
     $guestCardJsonObj = {};
     $guestCardJsonObj['guest_id'] = $("#guest_id").val();
-    $guestCardJsonObj['user'] = {};
-    $guestFirstName = $guestCardJsonObj['user']['first_name'] = $("#gc-firstname").val();
-    $guestLastName = $guestCardJsonObj['user']['last_name'] = $("#gc-lastname").val();
-    $guestCardJsonObj['user']['addresses_attributes'] = [];
-    $addresses_attributes = {};
-    $guestCity = $addresses_attributes['city'] = $("#gc-location").val();
-    $addresses_attributes['is_primary'] = true;
-    $addresses_attributes['label'] = "HOME";
-    $guestCardJsonObj['user']['addresses_attributes'].push($addresses_attributes);
-    $guestCardJsonObj['user']['contacts_attributes'] = [];
-    $contact_attributes = {};
-    $contact_attributes['contact_type'] = "PHONE";
-    $contact_attributes['label'] = "HOME";
-    $guestPhone = $contact_attributes['value'] = $("#gc-phone").val();
-    $contact_attributes['is_primary'] = true;
-    $guestCardJsonObj['user']['contacts_attributes'].push($contact_attributes);
-    $contact_attributes = {};
-    $contact_attributes['contact_type'] = "EMAIL";
-    $contact_attributes['label'] = "BUSINESS";
-    $guestEmail = $contact_attributes['value'] = $("#gc-email").val();
-    $contact_attributes['is_primary'] = true;
-    $contact_attributes['id'] = "";
-    $guestCardJsonObj['user']['contacts_attributes'].push($contact_attributes);
-
+    $guestFirstName = $guestCardJsonObj['first_name'] = $("#gc-firstname").val();
+    $guestLastName = $guestCardJsonObj['last_name'] = $("#gc-lastname").val();
+    $guestCity = $guestCardJsonObj['city'] = $("#gc-location").val();
+    $guestPhone = $guestCardJsonObj['phone'] = $("#gc-phone").val();
+    $guestEmail = $guestCardJsonObj['email'] = $("#gc-email").val();
 
     $.ajax({
       type : 'PUT',
