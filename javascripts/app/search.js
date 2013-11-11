94467834ba34ffca8011119e67c20aaad96a1551
Dipin <dipin@stayntouch.com>
2013-11-11 12:58:38 +0530

    CICO-1409 Done UI Changes to display image url from server

--- javascripts/app/search.js
+++ javascripts/app/search.js
@@ -1,16 +1,16 @@
 var Search = function(domRef){
   BaseView.call(this);
   var that = this;
-  this.myDomElement = domRef;  
+  this.myDomElement = domRef;
   searchResults = {};
   searchResults.guests = [];
-  	
+
   this.pageinit = function(){
     // setUpSearch(that.myDomElement);
-    that.myDomElement.find($('#query')).on('focus', that.callCapitalize);    
-    that.myDomElement.find($('#query')).on('keyup', that.loadResults);  
+    that.myDomElement.find($('#query')).on('focus', that.callCapitalize);
+    that.myDomElement.find($('#query')).on('keyup', that.loadResults);
   };
-  
+
   //when user focus on search text
   this.callCapitalize = function(e){
   	$(this).capitalize();
@@ -18,25 +18,25 @@ var Search = function(domRef){
    //when user focus on search text
   this.loadResults = function(event){
 		var $query = $(this).val();
-	
+
 	    // Clear button visibility toggle
 	    if($.trim($('#query').val()) !== '') {
 	        $('#clear-query:not(.visible)').addClass('visible');
 	    } else {
 	        $('#clear-query.visible').removeClass('visible');
 	    }
-	    //Keyboard backspace pressed 
+	    //Keyboard backspace pressed
 	    if(event.keyCode == 8){
 	    	searchResults = {};
 	    	searchResults.guests = [];
 	    }
-	
+
 	    if($query.length >= 3){
 	    	if(searchResults.guests.length > 0){
 	    		that.getFilteredResults($query);
 	    		return false;
 	    	}
-	    	
+
 	    	$search_url = 'search.json?';
 	    	that.load_search_data($search_url,$query);
 	    }
@@ -46,10 +46,10 @@ var Search = function(domRef){
 	    else
 	    {
 	        $('#search-results').empty().addClass('hidden');
-	        that.updateView();            
+	        that.updateView();
 	    }
     };
-    
+
    this.load_search_data = function(url,$query){
 	 	$.ajax({
 	    type:           "GET",
@@ -77,7 +77,7 @@ var Search = function(domRef){
 	        }
 	    });
     };
-    
+
      this.getFilteredResults = function($query){
      	$('#search-results').html("");
 	    that.displayFilteredResults(searchResults, $query);
@@ -90,13 +90,13 @@ var Search = function(domRef){
 	            // Search by name
 	            if ($query.match(/^([a-zA-Z]+)$/) && (value.firstname.indexOf($query) >= 0 || value.lastname.indexOf($query) >= 0 || value.group.indexOf($query) >= 0))
 	            {
-	                items.push($('<li />').html( 
+	                items.push($('<li />').html(
 	                    that.writeSearchResult(value.id,value.firstname,value.lastname,value.image,value.confirmation,value.reservation_status,value.room,value.roomstatus,value.fostatus,value.location,value.group,value.vip)
 	                ));
-	
+
 	                $('#search-results').append.apply($('#search-results'),items).highlight($query);
 	            }
-	            // Search by number 
+	            // Search by number
 	            else if ($query.match(/^([0-9]+)$/) && $query.length <= 5 && (value.room.toString().indexOf($query) >= 0 || value.confirmation.toString().indexOf($query) >= 0))
 	            {
 	                items.push($('<li />').html(
@@ -104,16 +104,16 @@ var Search = function(domRef){
 	                ));
 	                $('#search-results').append.apply($('#search-results'),items).highlight($query);
 	            }
-	            // Search by number 
+	            // Search by number
 	            else if ($query.length > 6 && (value.confirmation.toString().indexOf($query) >= 0))
 	            {
-	                items.push($('<li />').html( 
+	                items.push($('<li />').html(
 	                    that.writeSearchResult(value.id,value.firstname,value.lastname,value.image,value.confirmation,value.reservation_status,value.room,value.roomstatus,value.fostatus,value.location,value.group,value.vip)
 	                ));
 	                $('#search-results').append.apply($('#search-results'),items).highlight($query);
-	                
+
 	            }
-	
+
 	            // Reset scroller
 	            /*setTimeout(function () {
 	                contentScroll.refresh();
@@ -124,7 +124,7 @@ var Search = function(domRef){
 	    {
 	    	$('#search-results').html('<li class="no-content"><span class="icon-no-content icon-search"></span></li>');
 	    }
-	
+
 	    // As this search filters JSON content, we need temp custom handling for no results scenario
 	    if ($('#search-results').is(':empty'))
 	    {
@@ -132,21 +132,21 @@ var Search = function(domRef){
 	    }
 
     };
-    
+
     this.displaySearchResults = function(response, $query){
     	console.log(response);
 	    try
 		    {
 		        var items=[];
 		        $.each(response.guests, function(i,value){
-		        	
-		        items.push($('<li />').html( 
+
+		        items.push($('<li />').html(
 		                    that.writeSearchResult(value.id,value.firstname,value.lastname,value.image,value.confirmation,value.reservation_status,value.room,value.roomstatus,value.fostatus,value.location,value.group,value.vip)
 		                ));
-		
+
 		                $('#search-results').append.apply($('#search-results'),items).highlight($query);
 		        });
-		        
+
 		     	// Reset scroller
 		        /*setTimeout(function () {
 		            contentScroll.refresh();
@@ -156,21 +156,21 @@ var Search = function(domRef){
 		    {
 		    	$('#search-results').html('<li class="no-content"><span class="icon-no-content icon-search"></span></li>');
 		    }
-		
+
 		    // As this search filters JSON content, we need temp custom handling for no results scenario
 		    if ($('#search-results').is(':empty'))
 		    {
 		    	$('#search-results').html('<li class="no-content"><span class="icon-no-content icon-search"></span><strong class="h1">No matches</strong><span class="h2">Check you didn\'t mispell the <strong>Name</strong> or <strong>Group</strong>, or typed in the wrong <strong>Room </strong> or <strong>Confirmation</strong> number</span></li>');
 		    };
     };
-    
+
     this.writeSearchResult = function(id, firstname, lastname, image, confirmation, reservation_status, room, roomstatus, foStatus, location, group, vip){
-    	
+
     	var viewStatus = this.getReservationStatusMapped(reservation_status);
     	var roomStatusMapped = this.getRoomStatusMapped(roomstatus);
     	var roomstatusexplained = "";
     	roomstatusextra = false;
-    	
+
     	if(foStatus =="VACANT" && roomStatusMapped=="not-ready"){
     		roomstatusexplained ="VACANT";
     		roomstatusextra = true;
@@ -179,15 +179,15 @@ var Search = function(domRef){
     		roomstatusexplained ="DUEOUT";
     		roomstatusextra = true;
     	}
-    	
+
     	var $location = (escapeNull(location) != '') ? '<span class="icons icon-location">' + escapeNull(location) + '</span>' : '',
         $group = (escapeNull(group) != '') ? '<em class="icons icon-group">' + escapeNull(group) + '</em>' : '',
         $vip = vip ? '<span class="vip">VIP</span>' : '',
-        $image = (escapeNull(image) != '') ? '<figure class="guest-image"><img src="/assets/' + escapeNull(image) + '" />' + $vip +'</figure>' : '<figure class="guest-image"><img src="/assets/blank-avatar.png" />' + $vip +'</figure>',
+        $image = (escapeNull(image) != '') ? '<figure class="guest-image"><img src="' + escapeNull(image) + '" />' + $vip +'</figure>' : '<figure class="guest-image"><img src="/assets/blank-avatar.png" />' + $vip +'</figure>',
         $roomAdditional = roomstatusextra ? '<span class="room-status">' + roomstatusexplained + '</span>' : '',
         $viewStatus = viewStatus ? '<span class="guest-status ' + escapeNull(viewStatus) + '">' + escapeNull(viewStatus) + '</span>':'',
-        $output = 
-        '<a href="staff/staycards/staycard?confirmation=' + confirmation+'&id='+ escapeNull(id)+ '" class="guest-' + escapeNull(status) + ' link-item float" data-transition="inner-page">' + 
+        $output =
+        '<a href="staff/staycards/staycard?confirmation=' + confirmation+'&id='+ escapeNull(id)+ '" class="guest-' + escapeNull(status) + ' link-item float" data-transition="inner-page">' +
             $image +
             '<div class="data">' +
                 '<h2>' + escapeNull(lastname) + ', ' + escapeNull(firstname) + '</h2>' +
@@ -226,7 +226,7 @@ var Search = function(domRef){
     	}
 		return roomStatus;
     }
-    
+
     this.updateView = function(){
      	// Content update
 	    if ($('#search-results').is(':empty'))
