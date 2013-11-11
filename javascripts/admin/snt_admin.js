f13c03ba504caeabdb32237638406768835aff0e
soumya <soumya@stayntouch.com>
2013-11-11 17:38:28 +0530

    design issue

---
+++ javascripts/admin/snt_admin.js
@@ -0,0 +1,22 @@
+var SntAdminView = function(domRef){
+  BaseView.call(this);
+  var that = this;
+  this.myDom = domRef;
+
+  this.pageinit = function(){
+    setUpAdmin(domRef, this);
+  };
+  this.delegateEvents = function(){
+  	// $('.icon-admin-menu').on('draggable', that.customDrag);
+  	
+  	// this.myDom.find('#notes #delete_note').on('click', that.deleteReservationNotes);		
+  };
+  this.bookMarkAdded = function(bookMarkId){alert()
+  	var delegateBookMark = new DelegateBookMark();
+  	delegateBookMark.addBookMark(bookMarkId);
+  };
+  this.bookMarkRemoved = function(bookMarkId){
+  	var delegateBookMark = new DelegateBookMark();
+  	delegateBookMark.removeBookMark(bookMarkId);
+  };
+};
