b177d6839d89280195df89f46a844ff9f2861a62
krishobh <krishobh@stayntouch.com>
2013-11-12 11:45:25 +0530

    CICO-1401 Completed deteting notes in reservation card

--- javascripts/app/reservation-notes.js
+++ javascripts/app/reservation-notes.js
@@ -2,32 +2,33 @@ var reservationCardNotesView = function(domRef){
   BaseView.call(this);
   var that = this;
   this.myDom = domRef;
-
+  this.delegateEvents = function(){
+  	that.myDom.find('#post_notes #reservation_notes').on('click', that.saveReservationNotes);
+  	that.myDom.find('#notes').on('click', that.deleteReservationNotes);		
+  };
   this.pageinit = function(){
     
   };
-  this.delegateEvents = function(){
-  	this.myDom.find('#post_notes #reservation_notes').on('click', that.saveReservationNotes);
-  	this.myDom.find('#notes #delete_note').on('click', that.deleteReservationNotes);		
-  };
   this.deleteReservationNotes = function(e){
-  	var noteId= $(e.target).attr('note_id');
-	$.ajax({
-		type : "POST",
-		url : '/staff/reservation/delete-reservation-note',	
-		data : {note_id:noteId},
-		dataType : 'json',
-		success : function(data) {
-			console.log("Posted Succesfully. ");			
-			if (data.status == "success") {
-			    $("#notes li#note"+noteId).remove();
-				refreshViewScroll();
+  	var isDelete = $(e.target).hasClass('icon-trash');
+  	if(isDelete){
+	  	var noteId= $(e.target).attr('note_id');
+		$.ajax({
+			type : "DELETE",
+			url : '/reservation_notes/'+noteId,
+			dataType : 'json',
+			success : function(data) {
+				console.log("Delete Note Succesfully");			
+				if (data.status == "success") {
+				    $("#notes li#note"+noteId).remove();
+					refreshViewScroll();
+				}
+			},
+			error : function() {		
+			   console.log("There is an error!!");
 			}
-		},
-		error : function() {		
-		   console.log("There is an error!!");
-		}
-	});
+		});
+	}
   };
   this.saveReservationNotes = function(){
 	  	$notes = $("#post_notes textarea").val();
@@ -55,15 +56,14 @@ var reservationCardNotesView = function(domRef){
 			data : $data,
 			dataType : 'json',
 			success : function(data) {
-		
 				if (data.status == "success") {
 					returnData = data.data;
-					console.log(returnData.user_image);
-					$newNote = '<li><figure class="guest-image">' + 
+					$newNote = '<li id="note'+returnData.note_id+'"><figure class="guest-image">' + 
 						'<img src="' + returnData.user_image + '" alt=""></figure>' + 
 						'<div class="note-title"><h4>' + returnData.username + '</h4>' + 
 						'<time datetime="2013-10-23 06:05:20"><span class="time">'+returnData.posted_time + 
 						'</span><span class="date"> '+returnData.posted_date+'</span>' + '</time><span class="topic">' + returnData.topic +
+						'<a id="delete_note" class="icons icon-trash" note_id="'+returnData.note_id+'">Delete post</a>'+
 						'</span></div><p>' + returnData.text + '</p></li>';				
 					$("#notes").prepend($newNote);
 					refreshViewScroll();
@@ -75,7 +75,6 @@ var reservationCardNotesView = function(domRef){
 			}
 		});
   };
-
 };
 
 
