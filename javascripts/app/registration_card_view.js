4d5d8c23071141fc63a8fcec1d4cb077efcaf70a
krishobh <krishobh@stayntouch.com>
2013-11-14 19:15:12 +0530

    CICO-1457 Add validation for terms and conditions

--- javascripts/app/registration_card_view.js
+++ javascripts/app/registration_card_view.js
@@ -5,16 +5,45 @@ var RegistrationCardView = function(viewDom){
 
   this.pageinit = function(){
     console.log("RegistrationCardView pageinit");
-    
-    
-    
+    if (viewScroll) { destroyViewScroll(); }
+    setTimeout(function(){
+        createViewScroll('#registration-content');
+  	}, 300);
   }
   this.delegateEvents = function(){
   	console.log("RegistrationCardView delegateEvents");
   	that.myDom.find('#checkin-button').on('click', that.completeCheckin);
   }
   this.completeCheckin = function(){
+  	
   	console.log("RegistrationCardView completeCheckin");
+  	var terms_and_conditions = that.myDom.find("#subscribe-via-email").hasClass("checked");
+  	var signature = that.myDom.find(".output").val();
+  	var is_promotions_and_email_set = that.myDom.find("#subscribe-via-email").hasClass("checked");
+  	console.log(terms_and_conditions);
+  	
+  	var data= {
+	    "is_promotions_and_email_set": is_promotions_and_email_set,
+	    "signature": signature
+	};
+	
+	console.log(data);
+	/*
+  	$.ajax({
+				type : "PUT",
+				url : 'staff/guest_cards/' + userId,
+				data : JSON.stringify($contactJsonObj),
+				async : false,
+				dataType : 'json',
+				contentType : 'application/json',
+				success : function() {
+					console.log("success");
+				},
+				error : function() {
+					console.log("There is an error!!");
+				}
+	});
+		*/
   	
   }
 }
