d1a82d5b97bcbe9617dc9c357ae89c46fec7ddda
krishobh <krishobh@stayntouch.com>
2013-11-14 19:54:25 +0530

    CICO-1457 Add validation for terms and conditions

--- javascripts/app/registration_card_view.js
+++ javascripts/app/registration_card_view.js
@@ -17,33 +17,25 @@ var RegistrationCardView = function(viewDom){
   this.completeCheckin = function(){
   	
   	console.log("RegistrationCardView completeCheckin");
+  	
   	var terms_and_conditions = that.myDom.find("#subscribe-via-email").hasClass("checked");
   	var signature = that.myDom.find(".output").val();
   	var is_promotions_and_email_set = that.myDom.find("#subscribe-via-email").hasClass("checked");
+  	
   	console.log(terms_and_conditions);
   	
-  	var data= {
+  	if(!terms_and_conditions){
+  		alert("Please check agree to the Terms &Conditions");
+  	}
+  	else{
+  		
+  		var data= {
 	    "is_promotions_and_email_set": is_promotions_and_email_set,
 	    "signature": signature
-	};
+		};
 	
-	console.log(data);
-	/*
-  	$.ajax({
-				type : "PUT",
-				url : 'staff/guest_cards/' + userId,
-				data : JSON.stringify($contactJsonObj),
-				async : false,
-				dataType : 'json',
-				contentType : 'application/json',
-				success : function() {
-					console.log("success");
-				},
-				error : function() {
-					console.log("There is an error!!");
-				}
-	});
-		*/
-  	
+		console.log(data);
+  		
+  	}
   }
 }
