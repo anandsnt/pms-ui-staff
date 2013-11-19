8b3464206b0dae57addd3e1ff98145666cc3987b
Rashila Noushad <rashila@stayntouch.com>
2013-11-19 13:28:44 +0530

    Fix - Search, Stay Card, Loyalty

--- javascripts/app/guest_contact_view.js
+++ javascripts/app/guest_contact_view.js
@@ -133,8 +133,8 @@ var GuestContactView = function(domRef){
 				sntapp.fetchAndRenderView('staff/preferences/likes', $("#likes"), viewParams);
 
 				sntapp.fetchAndRenderView('staff/payments/payment', $("#cc-payment"), viewParams);
-				var reservation_id = getReservationId();
-				viewParams = {"reservation_id" : reservation_id};
+				//var reservation_id = getReservationId();
+				//viewParams = {"reservation_id" : reservation_id};
 				sntapp.fetchAndRenderView('staff/user_memberships', $("#loyalty"), viewParams);
 				setTimeout(function() {
 					refreshGuestCardScroll();
