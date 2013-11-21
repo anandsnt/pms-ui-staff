10b37abdef59151fd0b5fa3a39c6c20964a18699
soumya <soumya@stayntouch.com>
2013-11-21 18:34:23 +0530

    CICO-1970 Guest email address is not validating in creating/adding keys pop up

--- javascripts/app/AddKeysModal.js
+++ javascripts/app/AddKeysModal.js
@@ -3,10 +3,10 @@ var AddKeysModal = function() {
 	BaseModal.call(this);
 	var that = this;
 	var reservation_id = getReservationId();
-	this.url = "staff/reservations/"+reservation_id+"/get_key_setup";
+	this.url = "staff/reservations/" + reservation_id + "/get_key_setup";
 	this.delegateEvents = function() {
 
-		that.myDom.find('.radio').on('click', function(){
+		that.myDom.find('.radio').on('click', function() {
 			that.myDom.find($("#key_print_new,#key_print_additional")).removeClass("is-disabled");
 			that.myDom.find($("#key_print_new,#key_print_additional")).attr("disabled", false);
 		});
@@ -15,14 +15,14 @@ var AddKeysModal = function() {
 		$('#key_print_additional').on('click', that.printAdditionalKey);
 		var guestEmail = $("#change-name #gc-email").val();
 
-		if(guestEmail =="")
-		{
+		if (guestEmail == "") {
 			$(".print_key_missing").show();
 		} else {
 			$(".print_key_missing").hide();
 			that.myDom.find($("#key-guest-email").val(guestEmail));
 			var keyEmailElement = $("#key-guest-email").length;
-			if(keyEmailElement>0){
+
+			if (keyEmailElement > 0) {
 
 				that.myDom.find($("#print-keys")).removeClass("is-disabled");
 				$("input:radio").attr("disabled", false);
@@ -32,47 +32,55 @@ var AddKeysModal = function() {
 	this.modalInit = function() {
 
 	};
-	this.printNewKey = function(){
+	this.printNewKey = function() {
 		var reservation_id = getReservationId();
 		var keyEmailElement = $("#key-guest-email").length;
-		if(keyEmailElement>0){
-			key_guest_email = that.myDom.find(("#key-guest-email")).val();
-		}else{
-			key_guest_email = $("#change-name #gc-email").val();
+		console.log("keyEmailElement", keyEmailElement);
+		var guest_email = that.myDom.find(("#key-guest-email")).val();
+		if (validateEmail(guest_email)) {
+			if (keyEmailElement > 0) {
+				key_guest_email = that.myDom.find(("#key-guest-email")).val();
+
+			} else {
+				key_guest_email = $("#change-name #gc-email").val();
+			}
+			selected_key = $('input:radio[name="keys"]:checked').val();
+			var data = {
+				"reservation_id" : reservation_id,
+				"email" : key_guest_email,
+				"key" : selected_key,
+				"is_additional" : "false"
+			};
+			console.log("save print new");
+			data = JSON.stringify(data);
+
+			that.saveKey(data);
 		}
-	    selected_key = $('input:radio[name="keys"]:checked').val();
-	    var data = {
-					"reservation_id": reservation_id,
-				    "email": key_guest_email,
-				    "key": selected_key,
-				    "is_additional": "false"
-				  };
-				  console.log("save print new");
-      data = JSON.stringify(data);
-				  
-	  that.saveKey(data);
 
 	};
-	this.printAdditionalKey = function(){
+	this.printAdditionalKey = function() {
 		var reservation_id = getReservationId();
 		var keyEmailElement = $("#key-guest-email").length;
-		if(keyEmailElement>0){
-			key_guest_email = that.myDom.find($("#key-guest-email")).val();
-		}else{
-			key_guest_email = $("#change-name #gc-email").val();
+		var guest_email = that.myDom.find(("#key-guest-email")).val();
+		if (validateEmail(guest_email)) {
+			if (keyEmailElement > 0) {
+				key_guest_email = that.myDom.find($("#key-guest-email")).val();
+			} else {
+				key_guest_email = $("#change-name #gc-email").val();
+			}
+			selected_key = $('input:radio[name="keys"]:checked').val();
+			var data = {
+				"reservation_id" : reservation_id,
+				"email" : key_guest_email,
+				"key" : selected_key,
+				"is_additional" : "true"
+			};
+			data = JSON.stringify(data);
+			that.saveKey(data);
 		}
-	    selected_key = $('input:radio[name="keys"]:checked').val();
-	    var data = {
-					"reservation_id": reservation_id,
-				    "email": key_guest_email,
-				    "key": selected_key,
-				    "is_additional": "true"
-				  };
-        data = JSON.stringify(data);
-		that.saveKey(data);
 	};
-	this.saveKey = function (data){
-		
+	this.saveKey = function(data) {
+
 		$.ajax({
 			type : "POST",
 			url : 'staff/reservation/print_key',
@@ -83,7 +91,7 @@ var AddKeysModal = function() {
 			success : function() {
 				if (data.status == "success") {
 					// Commenting for now. Might be we need this in future
-				    // $("#change-name #gc-email").val(key_guest_email);
+					// $("#change-name #gc-email").val(key_guest_email);
 				}
 			},
 			error : function() {
@@ -93,5 +101,4 @@ var AddKeysModal = function() {
 		that.hide();
 	};
 
-
-};
+}; 
