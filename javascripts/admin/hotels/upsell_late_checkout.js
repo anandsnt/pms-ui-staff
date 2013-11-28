75fc6f09e22490be58089b8ace6d4c1563e7135b
akhilastayntouch <akhila@stayntouch.com>
2013-11-28 20:11:08 +0530

    CICO-1427 API integration post data modifications

--- javascripts/admin/hotels/upsell_late_checkout.js
+++ javascripts/admin/hotels/upsell_late_checkout.js
@@ -9,22 +9,52 @@ var UpsellLateCheckoutView = function(domRef){
 	   that.myDom.find('#save').on('click', that.saveHotelDetails); 
   };
   this.saveHotelDetails =  function(){
-	  var check_out_time_charge = new Array();
+	  var extended_checkout = new Array();
 	  var i = 1;
 	  var time = "", charge = "";
 	  var dict = "";
 	  var is_late_checkout_set = "false", is_exclude_guests = "false";
-	  var allowed_late_checkout = "";
+	  var allowed_late_checkout = "", sent_alert = "", currency_code = "";
 	  $("input[name=checkout-time-extended-to]").each(function(){
 		  time = $("#checkout-time-extended-to-" + i).val();
 		  charge = $("#charge-for-checkout-" + i).val();
 		  dict = {'time': time, 'charge': charge};
-		  check_out_time_charge.push(dict);
+		  extended_checkout.push(dict);
 		  i = i + 1;
 	  });
-	  console.log(check_out_time_charge);
 	  if($("#div-late-checkout-upsell").hasClass("on")) {
 		  is_late_checkout_set = "true";
 	  }
+	  allowed_late_checkout = $("#number-of-late-checkout").val();
+	  if($("#exclude-guest-pre-allocated-room").parent("label:eq(0)").hasClass("checked")) {
+	  	  is_exclude_guests = "true";
+	  }	  
+	  sent_alert = $("#sent-alert-to-all-guests").val();
+	  currency_code = $("#currency-code").val();
+	  var postParams = {};
+	  postParams.is_late_checkout_set = is_late_checkout_set;
+	  postParams.allowed_late_checkout = allowed_late_checkout;
+	  postParams.is_exclude_guests = is_exclude_guests;
+	  postParams.currency_code = currency_code;
+	  postParams.extended_checkout = extended_checkout;
+	  postParams.sent_alert = sent_alert;
+
+	  
+	  $.ajax({
+		type : "POST",
+		url : 'http://localhost:3000/staff/hotel/1/update_late_checkout_setup.json',	
+		data : postParams,
+		dataType : 'json',
+		async:false,
+		success : function(data) {					
+			if (data.status == "success") {
+
+			}
+		},
+		error : function() {		
+		}
+	});
+	  return false;
+
   };
 };
