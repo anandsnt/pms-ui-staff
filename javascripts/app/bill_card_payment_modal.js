6369a9d81776f467db3784cc43a7dad71ee2ea28
krishobh <krishobh@stayntouch.com>
2013-11-29 16:59:11 +0530

    CICO-1890 API integration done

--- javascripts/app/bill_card_payment_modal.js
+++ javascripts/app/bill_card_payment_modal.js
@@ -2,7 +2,7 @@ var BillCardPaymentModal = function(){
   	BaseModal.call(this);
   	var that = this;
   	this.reservation_id = getReservationId();
-  	this.url = "reservation/"+this.reservation_id+"/get_pay_bill_details";
+  	this.url = "staff/reservation/"+this.reservation_id+"/get_pay_bill_details";
   	this.delegateEvents = function(){
 		that.myDom.find("#submit-payment").on("click",that.clickedSubmitPayment);
 	};
