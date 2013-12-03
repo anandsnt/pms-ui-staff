70638501153cad3d2e8abdd6e1731a01aad48ebb
soumya <akhila@stayntouch.com>
2013-12-03 12:05:23 +0530

    CICO-1404 Added Hotel Admin Book mark click, Bug fix in Snt admin bookmark click

--- javascripts/admin/hotel_admin.js
+++ javascripts/admin/hotel_admin.js
@@ -13,9 +13,35 @@ var HotelAdminView = function(domRef){
   		$('#change-hotel').toggleClass('open');
   	});
   	that.myDom.find('li.ui-state-default a.ui-tabs-anchor').on('click', that.clearReplacingDiv);
-  	
+  	that.myDom.find('#admin-header nav').on('click', that.bookMarkClick);
   };
-  
+  this.bookMarkClick = function(event){
+	event.preventDefault();
+	var target = $( event.target );
+	if(target.prop('tagName') != "A")
+		return false;
+
+	var url = target.attr("href");
+	if(url != "#" && url != undefined){
+  		var backDom = null;
+  		that.myDom.find("#dashboard section.tab").each(function(){
+  			if($(this).is(":visible")){
+  				backDom = $(this); 				 				
+  			}
+  		});  		
+  		var div = that.myDom.find("#replacing-div-first");
+  		if(div.html() != ""){  
+  			div.hide();
+  			div = that.myDom.find("#replacing-div-second");
+  			if(backDom == null)
+  				backDom = that.myDom.find("#replacing-div-first");
+  		}
+  		that.myDom.find("#dashboard section.tab").hide(); 
+  		viewParams = {'backDom': backDom};
+  		
+  		sntapp.fetchAndRenderView(url, div, {}, false, viewParams);
+	}	  
+  };
   this.clearReplacingDiv = function() {	  
 	  $("#replacing-div-first").html("");
 	  $("#replacing-div-first").show();
--- javascripts/admin/snt_admin.js
+++ javascripts/admin/snt_admin.js
@@ -10,22 +10,33 @@ var SntAdminView = function(domRef){
   	that.myDom.find('li.ui-state-default a.ui-tabs-anchor').on('click', sntadminapp.clearReplacingDiv);
   	that.myDom.find('ul.dashboard-items li').on('click', sntadminapp.appendNewPage);
   	
-  	that.myDom.find($('#admin-menu #add_new_hotel')).on('click', function(event){
-  		event.preventDefault();
-  		var div = $("#replacing-div-first");
-  		if($("#replacing-div-second").is(":visible")){  			
-  			div = $("#replacing-div-second");
-  		}
+  	that.myDom.find('#admin-menu li a').on('click', that.bookMarkClick);
+  	that.myDom.find('#admin-header nav a').on('click', that.bookMarkClick);
+  };
+  this.bookMarkClick = function(event){
+	event.preventDefault();
+	var url = $(this).attr("href");
+	if(url != "#" && url != undefined){
+
   		var backDom = null;
-  		$("#content section").each(function(){
-  			if($(this).is(":visible"))
-  				backDom = $(this);
+  		that.myDom.find("#content section.tab").each(function(){
+  			if($(this).is(":visible")){
+  				backDom = $(this); 				 				
+  			}
   		});
-  		$("#content section").hide();
+  		
+  		var div = that.myDom.find("#replacing-div-first");
+  		if(div.html() != ""){  
+  			div.hide();
+  			div = that.myDom.find("#replacing-div-second");
+  			if(backDom == null)
+  				backDom = that.myDom.find("#replacing-div-first");
+  		}
+  		that.myDom.find("#content section.tab").hide(); 
   		viewParams = {'backDom': backDom};
-  		var url = $(this).attr("href");
+  		
   		sntapp.fetchAndRenderView(url, div, {}, false, viewParams);
-  	});
+	}	  
   };
   this.bookMarkAdded = function(bookMarkId){
   	var delegateBookMark = new DelegateBookMark();
