be095476b8894756669bb5b42c285cb53ff1137e
akhilastayntouch <akhila@stayntouch.com>
2013-12-03 16:16:43 +0530

    CICO-2415 bug fix

--- javascripts/app/render/reservation-card.js
+++ javascripts/app/render/reservation-card.js
@@ -30,4 +30,43 @@ var setUpStaycard = function(viewDom) {
 			}
 		}).addClass('ui-tabs-vertical ui-helper-clearfix');
 	});
+
+	viewDom.find($('.reservation-listing-tabs')).each(function() {
+		var activeTabIndex = 0;
+		var currentReservation = viewDom.find($('#confirm_no')).val();
+		viewDom.find($("#reservation-listing ul li")).each(function(index){
+			if($(this).attr("data-confirmation-num") == currentReservation )
+				activeTabIndex = index;
+		});
+		var abc = viewDom.find($( "#reservation-listing ul li" )).index( currentReservation ) ;
+		$(this).tabs({
+			active: activeTabIndex,
+			beforeActivate : function(event, ui) {
+				var $prevTab = ui.oldPanel.attr('id'), $nextTab = ui.newPanel.attr('id'), $changeTab = new chainedAnimation(), $delay = 600;
+
+				// Bring in new tab
+				$changeTab.add(function() {
+					viewDom.find($('#' + $nextTab)).removeAttr('style').addClass('loading');
+					viewDom.find($('#' + $prevTab)).removeAttr('style').addClass('set-back');
+				});
+
+				// Show/hide
+				$changeTab.add(function() {
+					viewDom.find($('#' + $nextTab)).show();
+					viewDom.find($('#' + $prevTab)).hide();
+				}, $delay);
+
+				// Clear transition classes
+				$changeTab.add(function() {
+					viewDom.find($('#' + $nextTab)).removeClass('loading');
+					viewDom.find($('#' + $prevTab)).removeClass('set-back');
+				});
+
+				$changeTab.start();
+
+				// Refresh scrollers
+				refreshViewScroll();
+			}
+		}).addClass('ui-tabs-vertical ui-helper-clearfix');
+	});
 };
