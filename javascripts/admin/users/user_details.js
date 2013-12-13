117d34e4576ce2e8c4016ee9aa1dc0dc03678f34
soumya <soumya@stayntouch.com>
2013-12-13 20:04:37 +0530

    CICO-1406 Add new user screen

--- javascripts/admin/users/user_details.js
+++ javascripts/admin/users/user_details.js
@@ -1,4 +1,4 @@
-var UsersListView = function(domRef){
+var UserDetailsView = function(domRef){
   BaseView.call(this);  
   this.myDom = domRef; 
   var that = this;
@@ -7,7 +7,6 @@ var UsersListView = function(domRef){
    
   };
   this.delegateEvents = function(){  	
-  	that.myDom.find($('#users_list_table')).tablesorter();
   	that.myDom.find($('#add_new_user')).on('click', sntadminapp.gotoNextPage);
   };
   this.goBackToPreviousView = function() {
