c3577d6b7312d09091e73dacb66a34fdbde4ce9c
soumya <soumya@stayntouch.com>
2013-12-24 13:19:54 +0530

    CICO-2892 - Edit department

--- javascripts/admin/department/hotel_departments.js
+++ javascripts/admin/department/hotel_departments.js
@@ -1,7 +1,9 @@
 var HotelDepartmentsView = function(domRef){
   BaseInlineView.call(this);  
   this.myDom = domRef; 
-  this.callSaveApi = function(){  	
+  
+  this.callSaveApi = function(){ 
+  	 	
   	return;
   };
 };
