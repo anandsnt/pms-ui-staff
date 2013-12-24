857593c2427de9270d72ba7899d236407292ca91
akhilastayntouch <akhila@stayntouch.com>
2013-12-24 19:15:55 +0530

    CICO-1432 updates

--- javascripts/admin/guestcard_likes/guestcard_likes_view.js
+++ javascripts/admin/guestcard_likes/guestcard_likes_view.js
@@ -3,12 +3,12 @@ var GuestCardLikesView = function(domRef){
   this.myDom = domRef; 
   var that = this;
 
-  this.delegateEvents = function(){
+ /* this.delegateEvents = function(){
   	console.log(that.myDom);
   	 //that.myDom.find('.switch-button').on('click', 'onOffClicked');
   	 that.myDom.find('tr').on('click', that.appendInlineData);
   };
-
+*/
   this.onOffClicked = function(){
   	onOffSwitch();
   };
