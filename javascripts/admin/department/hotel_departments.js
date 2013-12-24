7cc35653525b027f1179a35ad4320d6e9f90dc51
soumya <soumya@stayntouch.com>
2013-12-24 12:13:14 +0530

    CICO-2892 Departments - Generic method

--- javascripts/admin/department/hotel_departments.js
+++ javascripts/admin/department/hotel_departments.js
@@ -1,5 +1,7 @@
 var HotelDepartmentsView = function(domRef){
   BaseInlineView.call(this);  
   this.myDom = domRef; 
-  console.log("-------------------");
+  this.callSaveApi = function(){  	
+  	return;
+  };
 };
