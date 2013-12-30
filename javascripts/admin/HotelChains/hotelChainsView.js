09b061247a4b87334cbe506a465c9667adf7a3bc
soumya <akhila@stayntouch.com>
2013-12-30 20:11:10 +0530

    CICO-1373 Show list of chains, Added view for adding chains, but problem with level showing

--- javascripts/admin/HotelChains/hotelChainsView.js
+++ javascripts/admin/HotelChains/hotelChainsView.js
@@ -8,13 +8,13 @@ var HotelChainsView = function(domRef){
 	};
 	this.updateApi = function(event){
 		var chainName = $.trim(that.myDom.find("#chain-name").val());
-		var chainID = that.myDom.find("#edit-chain-details").attr("data-chain-id");		
+		var chainID = that.myDom.find("#edit-chain-details").attr("data_chain_id");		
 		if(typeof chainName === 'undefined' || chainName === ""){
 			alert('Please enter a valid chain name');
 			return false;
 		}
 		else if(typeof chainID === 'undefined' || chainID === ""){ // rare case
-			sntapp.notificationMessage.showErrorMessage('Some thing went wrong, please refresh the page and try again');
+			sntapp.notification.showErrorMessage('Some thing went wrong, please refresh the page and try again');
 			return false;
 		}
 		else{
@@ -35,20 +35,21 @@ var HotelChainsView = function(domRef){
 	};
 	
 	this.fetchCompletedOfUpdateApi = function(data){
-		sntapp.notificationMessage.showSuccessMessage('Successfully updated');
+		sntapp.notification.showSuccessMessage('Successfully updated');
 		// update the view of listing the chain listing
 		
 	};
 	
 	this.saveNewApi = function(){
 		var chainName = $.trim(that.myDom.find("#chain-name").val());
-		var chainID = that.myDom.find("#edit-chain-details").attr("data-chain-id");		
-		if(typeof chainName === 'undefined' || chainName === ""){
-			alert('Please enter a valid chain name');
+		var hotelCode = $.trim(that.myDom.find("#hotel-code").val());	
+
+		if(typeof hotelCode === 'undefined' || hotelCode === ""){ 
+			alert('Please enter a valid hotel code');
 			return false;
 		}
-		else if(typeof chainID === 'undefined' || chainID === ""){ // rare case
-			sntapp.notificationMessage.showErrorMessage('Some thing went wrong, please refresh the page and try again');
+		else if(typeof chainName === 'undefined' || chainName === ""){
+			alert('Please enter a valid chain name');
 			return false;
 		}
 		else{
@@ -58,7 +59,7 @@ var HotelChainsView = function(domRef){
 			if(typeof url === 'undefined' || url == "#" )
 				return false;
 			
-			var data = {'id': chainID, 'name':chainName  };
+			var data = {'hotel_code': hotelCode, 'name':chainName  };
 		    var options = {
 					   successCallBack: that.fetchCompletedOfSaveNewApi,
 					   requestParameters: data,
@@ -68,7 +69,7 @@ var HotelChainsView = function(domRef){
 		}		
 	};
 	this.fetchCompletedOfSaveNewApi = function(data){
-		sntapp.notificationMessage.showSuccessMessage('Successfully updated');
+		sntapp.notification.showSuccessMessage('Successfully updated');
 		// update the view of listing the chain listing
 		
 	};	
