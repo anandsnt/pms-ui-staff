57028acaf1e3cc273c32895018d4df32ffdce5da
soumya <soumya@stayntouch.com>
2013-12-30 16:35:45 +0530

    CICO-1432 Likes - show text box for different options

--- javascripts/admin/guestcard_likes/guestcard_likes_view.js
+++ javascripts/admin/guestcard_likes/guestcard_likes_view.js
@@ -2,7 +2,7 @@ var GuestCardLikesView = function(domRef){
   BaseInlineView.call(this);  
   this.myDom = domRef; 
   var that = this;
-  var $textOptionStart = 53;
+  var $textOptionStart = 1;
  /* this.delegateEvents = function(){
   	console.log(that.myDom);
   	 //that.myDom.find('.switch-button').on('click', 'onOffClicked');
@@ -18,6 +18,7 @@ var GuestCardLikesView = function(domRef){
   	that.myDom.find('.icon-delete').on('click', that.deleteItem);
   };
   this.changeData = function(){
+  	
   	var target = $(this).attr('data-type');
 	if (target != 'textbox'){
 
@@ -53,22 +54,33 @@ var GuestCardLikesView = function(domRef){
   };
   this.delegateInlineEvents = function(){
 	that.myDom.find('.change-data').on('click', that.changeData);
+	
 	that.myDom.find('.add-new-option').on('click', that.addNewOption);
+	that.myDom.find('.delete-option').on('keyup', that.deleteOption);
+   };
+   this.deleteOption = function(){
+	   	if ($(this).val() == '')
+		{
+			$(this).parent('.entry').remove();
+		}	
    };
    this.addNewOption = function(){
-   	var $type = $(this).attr('data-type');
-
-			$textOptionStart++;
-
-			$(this)
-				.clone() 											// Clone element
-				.val('') 											// Clear value
-				.attr('id', $type + '-option' + $textOptionStart) 	// Increment ID value
-				.insertAfter($(this).parent('.entry'))				// Insert after this one
-				.wrap('<div class="entry" />');						// Wrap to div
-
-			// Set new class
-		    $(this).removeClass('add-new-option').addClass('delete-option');    
+	   	var $type = $(this).attr('data-type');
+	
+		$textOptionStart++;
+	
+		$(this)
+			.clone() 											// Clone element
+			.val('') 											// Clear value
+			.attr('id', $type + '-option' + $textOptionStart) 	// Increment ID value
+			.insertAfter($(this).parent('.entry'))				// Insert after this one
+			.wrap('<div class="entry" />');						// Wrap to div
+	
+		// Set new class
+		$('.add-new-option').unbind();
+	    $(this).removeClass('add-new-option').addClass('delete-option');    
+	    
+	    that.delegateInlineEvents();
    };
 
 
