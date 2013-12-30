6c16035bf79f975fb5fe1e055d0ee4ce89bb58c1
soumya <soumya@stayntouch.com>
2013-12-30 11:40:05 +0530

    CICO-1416 Hotel - Admin - Hotel & Staff - Create new 'Permissions' button

--- javascripts/admin/guestcard_ffp/guestcard_ffp_view.js
+++ javascripts/admin/guestcard_ffp/guestcard_ffp_view.js
@@ -5,10 +5,26 @@ var GuestCardFFPView = function(domRef) {
 
 	this.delegateEvents = function() {
 		that.myDom.find($('#ffp')).tablesorter();
+		that.myDom.find($(".activate-inactivate-button")).on('click', that.activateInactivateLoyalty);
 	};
 
-	this.onOffClicked = function() {
-		onOffSwitch();
-	};
 
+  //activate/inactivate loyallty
+  this.activateInactivateLoyalty = function(){
+  	//TODO set url.
+  	var url = '';
+  	var postData = {};
+  	var selectedId = $(this).attr("ffp");// to get the current toggle ffp id
+    if($("#activate-inactivate-button_"+selectedId+" .switch-button").hasClass("on")) {
+		  postData.activity = "inactivate";
+	} else {
+		postData.activity = "activate";
+	}
+  	postData.id = selectedId;
+	var webservice = new WebServiceInterface();		
+	var options = {
+			   requestParameters: postData
+	};
+	webservice.postJSON(url, options);	
+  };
 }; 
--- javascripts/admin/permissions/permission_role_view.js
+++ javascripts/admin/permissions/permission_role_view.js
@@ -4,8 +4,15 @@ var PermissionView = function(domRef) {
 	var that = this;
 
 	this.delegateEvents = function() {
-		
+		that.myDom.find($('.permission-tabs')).tabs(); 
+		that.myDom.find($("#assigned-roles li")).on('click', that.savePermissionRoles);
 	};
-
-
+	this.savePermissionRoles = function() {
+		var postData = {};
+		postData.user_roles = [];
+		that.myDom.find("#assigned-roles li").each(function(n) {
+    	postData.user_roles.push($(this).attr("id"));
+    	console.log("postData",postData.user_roles);
+	});
+};
 }; 
