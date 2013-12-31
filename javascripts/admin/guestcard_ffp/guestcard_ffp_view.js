b56a93005e8156f80659ed9d111276a8928c8cc4
soumya <soumya@stayntouch.com>
2013-12-31 10:37:08 +0530

    Hotel - Admin - Hotel & Staff - Create new 'Permissions' button

--- javascripts/admin/guestcard_ffp/guestcard_ffp_view.js
+++ javascripts/admin/guestcard_ffp/guestcard_ffp_view.js
@@ -8,23 +8,23 @@ var GuestCardFFPView = function(domRef) {
 		that.myDom.find($(".activate-inactivate-button")).on('click', that.activateInactivateLoyalty);
 	};
 
-
-  //activate/inactivate loyallty
-  this.activateInactivateLoyalty = function(){
-  	//TODO set url.
-  	var url = '';
-  	var postData = {};
-  	var selectedId = $(this).attr("ffp");// to get the current toggle ffp id
-    if($("#activate-inactivate-button_"+selectedId+" .switch-button").hasClass("on")) {
-		  postData.activity = "inactivate";
-	} else {
-		postData.activity = "activate";
-	}
-  	postData.id = selectedId;
-	var webservice = new WebServiceInterface();		
-	var options = {
-			   requestParameters: postData
+	//activate/inactivate loyallty
+	this.activateInactivateLoyalty = function() {
+		//TODO set url.
+		var url = '';
+		var postData = {};
+		var selectedId = $(this).attr("ffp");
+		// to get the current toggle ffp id
+		if ($("#activate-inactivate-button_" + selectedId + " .switch-button").hasClass("on")) {
+			postData.activity = "inactivate";
+		} else {
+			postData.activity = "activate";
+		}
+		postData.id = selectedId;
+		var webservice = new WebServiceInterface();
+		var options = {
+			requestParameters : postData
+		};
+		webservice.postJSON(url, options);
 	};
-	webservice.postJSON(url, options);	
-  };
-}; 
+};
--- javascripts/admin/permissions/permission_role_view.js
+++ javascripts/admin/permissions/permission_role_view.js
@@ -4,15 +4,27 @@ var PermissionView = function(domRef) {
 	var that = this;
 
 	this.delegateEvents = function() {
-		that.myDom.find($('.permission-tabs')).tabs(); 
-		that.myDom.find($("#assigned-roles li")).on('click', that.savePermissionRoles);
+		that.myDom.find($('.permission-tabs')).tabs();
+		that.myDom.find($('#save_permissions')).on('click', that.savePermissionRoles);
 	};
 	this.savePermissionRoles = function() {
 		var postData = {};
-		postData.user_roles = [];
+		postData.assigned_roles = [];
 		that.myDom.find("#assigned-roles li").each(function(n) {
-    	postData.user_roles.push($(this).attr("id"));
-    	console.log("postData",postData.user_roles);
-	});
+			postData.assigned_roles.push($(this).attr("id"));
+			console.log("postData", postData.assigned_roles);
+			});
+/*
+			var url = '';
+			var webservice = new WebServiceInterface();
+
+			var options = {
+				requestParameters : postData,
+				successCallBack : that.fetchCompletedOfSave,
+				loader : "BLOCKER"
+			};
+			webservice.postJSON(url, options);*/
+
+		
+	};
 };
-}; 
