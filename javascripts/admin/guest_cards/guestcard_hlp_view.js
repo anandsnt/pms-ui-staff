a31aaf521e659b9491b1b304981b24d18734b808
akhilastayntouch <akhila@stayntouch.com>
2014-01-02 16:04:27 +0530

    CICO-1435 code optimization

--- javascripts/admin/guest_cards/guestcard_hlp_view.js
+++ javascripts/admin/guest_cards/guestcard_hlp_view.js
@@ -11,6 +11,16 @@ var GuestCardHLPView = function(domRef) {
 		that.myDom.on('keyup', that.viewKeyupEventHandler);
 	};
 
+	//handle view click events
+	this.viewClickEventHandler = function(event) {
+		var element = $(event.target);
+
+		if (element.hasClass('add-new-option'))
+			return that.addNewOption(element, event);
+		return true;
+
+	};
+
 	//to handle keyup events- text box show new textbox on key up
 	this.viewKeyupEventHandler = function(event) {
 		var element = $(event.target);
@@ -24,131 +34,113 @@ var GuestCardHLPView = function(domRef) {
 			element.parent('.entry').remove();
 		}
 	};
-	//handle click events
-	this.viewClickEventHandler = function(event) {
-		var element = $(event.target);
 
-		if (element.hasClass('add-new-option'))
-			return that.addNewOption(element, event);
-		return true;
-
-	};
 
 	this.toggleButtonClicked = function(element) {
 
 		var hlpId = element.closest('tr').attr('hlp_id');
 		setTimeout(function(){
 			var toggleStatus = element.parent().hasClass('on') ? "true" : "false";
-			var postParams = {
+			var postData = {
 				"value" : hlpId,
 				"set_active" : toggleStatus
 			};
+		//console.log(JSON.stringify(postData));// DELETE once API Integration is complete
+
 			var webservice = new WebServiceInterface();
 			var options = {
-				requestParameters : postParams,
+				requestParameters : postData,
 				loader : "NONE"
 			};
-			//var url = '/staff/reservations/upgrade_room';
+			//var url = "";
 			//webservice.postJSON(url, options);
 			return true;
 		}, 100);
 	};
+
 	this.addNewOption = function(element, event) {
 		var type = element.attr('data-type');
 
 		textOptionStart++;
 
 		element.clone()// Clone element
-		.val('')// Clear value
-		.attr('id', type + '-option' + textOptionStart)// Increment ID value
-		.insertAfter(element.parent('.entry'))// Insert after this one
-		.wrap('<div class="entry" />');
-		// Wrap to div
+			.val('')// Clear value
+			.attr('id', type + '-option' + textOptionStart)// Increment ID value
+			.insertAfter(element.parent('.entry'))// Insert after this one
+			.wrap('<div class="entry" />');// Wrap to div
 
 		// Set new class
 		$('.add-new-option').unbind('click');
 		element.removeClass('add-new-option').addClass('delete-option');
 
 	};
+
 	//refreshing view with new data and showing message
 	this.fetchCompletedOfSave = function(data, requestParams) {
 
 		var url = "/admin/hotel_loyalty_program/";
 		viewParams = {};
-		sntapp.fetchAndRenderView(url, that.myDom, {}, 'BLOCKER', viewParams);
+		
 		if (data.status == "success") {
+			sntapp.fetchAndRenderView(url, that.myDom, {}, 'BLOCKER', viewParams);
 			sntapp.notification.showSuccessMessage("Saved Successfully", that.myDom);
 			that.cancelFromAppendedDataInline(requestParams['event']);
 		} else {
 			sntapp.notification.showErrorList(data.errors, that.myDom);
 		}
 	};
+
 	this.updateApi = function(event) {
-		var postData = {};
-		postData.value = that.myDom.find("#edit-loyalty").attr("hlp_id");
-		postData.name = that.myDom.find("#name").val();
-		postData.code = that.myDom.find("#code").val();
-		var levels = new Array();
-		$("input[name=loyalty-levels]").each(function() {
-			value = $(this).attr("data-id");
-			name = $(this).val();
-			dict = {
-				'value' : value,
-				'name' : name
-			};
-			if (name != "") {
-				levels.push(dict);
-			}
-		});
-		postData.levels = levels;
-		//console.log(JSON.stringify(postData));// DELETE once API Integration is complete
 
-		var url = '/admin/hotel_loyalty_program/' + postData.id;
-		var webservice = new WebServiceInterface();
-		var options = {
-			requestParameters : postData,
-			successCallBack : that.fetchCompletedOfSave,
-			successCallBackParameters : {
-				"event" : event
-			},
-			loader : "BLOCKER"
-
-		};
-		webservice.putJSON(url, options);
+		var hlpId = that.myDom.find("form#edit-loyalty").attr("hlp_id");
+		var url = '/admin/hotel_loyalty_program/' + hlpId ;
+		var action = "ACTION_EDIT"
+		that.makeAPICall(url , action, event);
 
 	};
+
 	this.saveNewApi = function(event) {
-		var postData = {};
-		postData.value = that.myDom.find("#edit-like").attr("hlp_id");
-		postData.name = that.myDom.find("#name").val();
-		postData.code = that.myDom.find("#code").val();
-		var levels = new Array();
-		$("input[name=loyalty-levels]").each(function() {
-			value = $(this).attr("data-id");
-			name = $(this).val();
-			dict = {
-				'value' : value,
-				'name' : name
-			};
-			if (name != "") {
-				levels.push(dict);
-			}
-		});
-		postData.levels = levels;
-		//console.log(JSON.stringify(postData));// DELETE once API Integration is complete
 
 		var url = '/admin/hotel_loyalty_program/create';
+		var action = "ACTION_SAVE"
+		that.makeAPICall(url, action, event);
+
+
+	};
+
+	this.makeAPICall = function(url, action, event){
+		var postData = {};
+		if(action == "ACTION_EDIT"){
+			postData.value = that.myDom.find("form#edit-loyalty").attr("hlp_id");
+		}
+		postData.name = that.myDom.find("#name").val();
+		postData.code = that.myDom.find("#code").val();
+		var levels = [];
+		$("input[name=loyalty-levels]").each(function() {
+			
+			var name = $(this).val();
+			if (name != "") {
+				var dict = {};
+				if(action == "ACTION_EDIT"){
+					dict.value = $(this).attr("data-id");
+				}
+				dict.name = name;
+				levels.push(dict);
+			}
+		});
+		postData.levels = levels;
+		//console.log(JSON.stringify(postData));// DELETE once API Integration is complete
+
+
 		var webservice = new WebServiceInterface();
 		var options = {
 			requestParameters : postData,
 			successCallBack : that.fetchCompletedOfSave,
-			successCallBackParameters : {
-				"event" : event
-			},
+			successCallBackParameters : { "event" : event },
 			loader : "BLOCKER"
 
 		};
 		webservice.putJSON(url, options);
 
-	};
+	}
 };
