fe964467011bad1a4c3446e9f0d3b6dfbeb70074
krishobh <krishobh@stayntouch.com>
2014-01-06 15:05:04 +0530

    CICO-1437 Add rooms listing and image preview

--- javascripts/admin/rooms/rooms_view.js
+++ javascripts/admin/rooms/rooms_view.js
@@ -3,7 +3,100 @@ var RoomsView = function(domRef) {
 	this.myDom = domRef;
 	var that = this;
 
-	this.delegateSubviewEvents = function() {
+	// to handle sub view events
+	this.delegateSubviewEvents = function(){
+  		that.myDom.on('change', that.viewChangeEventHandler);
+  		that.myDom.on('click', that.viewClickEventHandler);
 	};
+   
+	this.viewChangeEventHandler = function(event){  
+		console.log(event);
+	   	var element = $(event.target);
+	   	if(element.parent().hasClass('file-upload')) {return that.readURL(event.target);}
+	   	console.log(element);
+	   	console.log(event.target);
+	};
+  
+  //to show preview of the image using file reader
+  this.readURL = function(input) {
+  	console.log("readURL");
+  	   $('#file-preview').attr('changed', "changed");
+       if (input.files && input.files[0]) {
+           var reader = new FileReader();
+           reader.onload = function(e) {
+           	   $('#file-preview').attr('src', e.target.result);
+               that.fileContent = e.target.result;
+           };
+           reader.readAsDataURL(input.files[0]);
+       }
+  };
 
+  //function to add new room type
+  this.saveNewApi = function(event){ 
+  	 	
+  	var postData = {};
+  	postData.room_number = that.myDom.find("#room-number").val(); 
+  	postData.room_type_id = that.myDom.find("#room-type").val();
+  	postData.active_room_features = []; 
+  	postData.active_room_likes = []; 
+  	
+  	// to handle image uploaded or not
+  	if(that.myDom.find("#file-preview").attr("changed") == "changed")
+  		postData.room_image = that.myDom.find("#file-preview").attr("src");
+  	else
+  		postData.room_image = "";
+  		
+  	var url = '';
+	var webservice = new WebServiceInterface();		
+	var options = {
+			   requestParameters: postData,
+			   successCallBack: that.fetchCompletedOfSave,
+			   successCallBackParameters:{ "event": event},
+			   failureCallBack: that.fetchFailedOfSave,
+			   loader:"BLOCKER"
+	};
+	webservice.postJSON(url, options);	
+  };
+  //refreshing view with new data and showing message
+  this.fetchCompletedOfSave = function(data, requestParams){
+  	
+  	var url = "";
+   	viewParams = {};
+  	sntapp.fetchAndRenderView(url, $("#replacing-div-first"), {}, 'BLOCKER', viewParams);
+  	sntapp.notification.showSuccessMessage("Saved Successfully", that.myDom);		
+  	that.cancelFromAppendedDataInline(requestParams['event']);  
+  };
+  
+  //function to update department
+  this.updateApi = function(event){
+  	
+  	var postData = {};
+  	postData.room_id = that.myDom.find("#edit-room").attr('data-room-id'); 
+  	postData.room_number = that.myDom.find("#room-number").val(); 
+  	postData.room_type_id = that.myDom.find("#room-type").val();
+  	postData.active_room_features = []; 
+  	postData.active_room_likes = []; 
+  	
+  	// to handle image uploaded or not
+  	if(that.myDom.find("#file-preview").attr("changed") == "changed")
+  		postData.room_image = that.myDom.find("#file-preview").attr("src");
+  	else
+  		postData.room_image = "";
+  	
+  	var url = '';
+	var webservice = new WebServiceInterface();		
+	var options = {
+			   requestParameters: postData,
+			   successCallBack: that.fetchCompletedOfSave,
+			   successCallBackParameters:{ "event": event},
+			   failureCallBack: that.fetchFailedOfSave,
+			   loader:"BLOCKER"
+	};
+	webservice.putJSON(url, options);	
+  };
+  // To handle failure on save API
+  this.fetchFailedOfSave = function(errorMessage){
+  	sntapp.notification.showErrorMessage(errorMessage, that.myDom);
+  };
+  
 };
