1bd5302d3073d1ffa5fa3fbe8604d22ba7f1dba9
soumya <soumya@stayntouch.com>
2014-01-07 13:02:03 +0530

    CICO-1416 - Set permissons for each roles

--- javascripts/admin/permissions/permission_role_view.js
+++ javascripts/admin/permissions/permission_role_view.js
@@ -2,28 +2,32 @@ var PermissionView = function(domRef) {
 	BaseInlineView.call(this);
 	this.myDom = domRef;
 	var that = this;
-
+    //delegate Events
 	this.delegateEvents = function() {
-		that.myDom.find($('.permission-tabs')).tabs();
+		that.myDom.find($('.permission-tabs')).tabs(); // ui tabs
+		that.myDom.find($('#permissions ul li')).on('click', function(){
+			var currentId = $(this).attr("data-id");
+			$("ul.permission-box").removeClass("current-permission");
+			$("#assigined-roles-"+currentId).addClass("current-permission"); // to get current selected permissions box 
+		});
 		that.myDom.find($('#save_permissions')).on('click', that.savePermissionRoles);
 	};
+	//function to save the permissons
 	this.savePermissionRoles = function() {
 		var postData = {};
-		postData.assigned_roles = [];
-		that.myDom.find("#assigned-roles li").each(function(n) {
-			postData.assigned_roles.push($(this).attr("value"));
-			});
+		postData.permissions = [];
+		postData.value = $(".current-permission").attr("data-id");
+		that.myDom.find(".current-permission li").each(function(n) {
+			postData.permissions.push($(this).attr("value")); // to get the assigned permissons
+		});
+		var url = '#';
+		var webservice = new WebServiceInterface();
 
-			var url = '#';
-			var webservice = new WebServiceInterface();
-
-			var options = {
-				requestParameters : postData,
-				successCallBack : that.fetchCompletedOfSave,
-				loader : "BLOCKER"
-			};
-			webservice.postJSON(url, options);
-
-		
+		var options = {
+			requestParameters : postData,
+			successCallBack : that.fetchCompletedOfSave,
+			loader : "BLOCKER"
+		};
+		webservice.postJSON(url, options);
 	};
 };
