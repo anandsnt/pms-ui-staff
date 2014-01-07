198177ae1c88cd29a3d966f705226c789e0fb200
krishobh <krishobh@stayntouch.com>
2014-01-07 12:10:26 +0530

    CICO-1434 Added error handling

--- javascripts/admin/guest_cards/guestcard_ffp_view.js
+++ javascripts/admin/guest_cards/guestcard_ffp_view.js
@@ -16,6 +16,8 @@ var GuestCardFFPView = function(domRef) {
           	var webservice = new WebServiceInterface(); 
           	var options = {
                	requestParameters: postParams,
+               	successCallBack : that.fetchCompletedOfSave,
+               	failureCallBack : that.fetchFailedOfSave,
                	loader: "NONE"
           	};
           	var url = '/admin/hotel/toggle_ffp_activation/';
@@ -23,4 +25,16 @@ var GuestCardFFPView = function(domRef) {
           	return true;
 	    }, 100);
 	};
+	
+	// To handle success on save API
+	this.fetchCompletedOfSave = function(errorMessage) {
+		sntapp.notification.showSuccessMessage("Saved Successfully", that.myDom);
+	};
+	
+	// To handle failure on save API
+	this.fetchFailedOfSave = function(errorMessage) {
+		sntapp.notification.showErrorMessage(errorMessage, that.myDom);
+	};
+	
+	
 };
