b9de5a0e64ed3cf52e520da709adb4e2abb0022e
krishobh <krishobh@stayntouch.com>
2014-01-09 13:35:14 +0530

    CICO-1437 Change edit view

--- javascripts/admin/rooms/rooms_view.js
+++ javascripts/admin/rooms/rooms_view.js
@@ -9,10 +9,11 @@ var RoomsView = function(domRef) {
 	this.delegateEvents = function() {
 		that.myDom.find('#rooms').tablesorter();
 		that.myDom.find('#add_new_room').on('click', sntadminapp.gotoNextPage);
+		that.myDom.find($('.edit-data-inline')).on('click', sntadminapp.gotoNextPage);
 		that.myDom.find('#go_back,#cancel').on('click', that.goBackToPreviousView);
 		that.myDom.find('#room-picture').on('change', that.readURL);
 		that.myDom.find('#save').on('click', that.addNewRoom);
-
+		that.myDom.find('#update').on('click', that.updateRoom);
 	};
 	// To go back to rooms
 	this.goBackToPreviousView = function() {
@@ -76,6 +77,48 @@ var RoomsView = function(domRef) {
 		};
 		webservice.postJSON(url, options);
 	};
+
+	//update room details
+	this.updateRoom = function() {
+		var postData = {};
+		postData.room_id = that.myDom.find("#edit-room").attr('data-room-id');
+		postData.room_number = that.myDom.find("#room-number").val();
+		postData.room_type_id = that.myDom.find("#room-type").val();
+		postData.active_room_features = [];
+		postData.active_room_likes = [];
+		// to get active features
+		that.myDom.find('#room-features label.checkbox').each(function() {
+			if (that.myDom.find(this).hasClass("checked")) {
+				var value = $(this).find("input").attr('name');
+				postData.active_room_features.push(value);
+				console.log(postData.active_room_features);
+			}
+		});
+		// to get active likes
+		that.myDom.find('#room-likes label.checkbox').each(function() {
+			if (that.myDom.find(this).hasClass("checked")) {
+				var value = $(this).find("input").attr('name');
+				postData.active_room_likes.push(value);
+			}
+		});
+
+		// to handle image uploaded or not
+		if (that.myDom.find("#file-preview").attr("changed") == "changed")
+			postData.room_image = that.myDom.find("#file-preview").attr("src");
+		else
+			postData.room_image = "";
+
+		var url = '/admin/hotel_rooms/'+postData.room_id;
+		var webservice = new WebServiceInterface();
+		var options = {
+			   requestParameters: postData,
+			   successCallBack: that.fetchCompletedOfSave,
+			   loader:"BLOCKER"
+			   
+	};
+		webservice.putJSON(url, options);
+	};
+
 	//refreshing view with new data and showing message
 	this.fetchCompletedOfSave = function(data, requestParams) {
 
