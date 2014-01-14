0184649a1e847f8c222bf70fb67debe09d4d1c12
akhilastayntouch <akhila@stayntouch.com>
2014-01-14 14:44:15 +0530

    CICO-978 calender rendering updates

--- javascripts/app/change_staydates_view.js
+++ javascripts/app/change_staydates_view.js
@@ -15,7 +15,7 @@ var ChangeStayDatesView = function(viewDom){
   this.pageinit = function(){
 
     // Set calendar - timeout set to make sure functions from edit-reservation.js are available
-  setTimeout(function() {
+
     $('#reservation-calendar').fullCalendar({
           header: {
               left        : 'prev',
@@ -24,7 +24,7 @@ var ChangeStayDatesView = function(viewDom){
           },
           year      : 2014,   // Check in year
           month       : 00,     // Check in month (month is zero based)
-          day       : 07,   // Check in day
+          day       : 05,   // Check in day
           editable        : false,
           disableResizing : true,
           contentHeight   : 320,
@@ -39,17 +39,10 @@ var ChangeStayDatesView = function(viewDom){
                       alert('there was an error while fetching events!');
                   }
               }
-          ],
-          // Set how many months are visible on display
-          viewDisplay: function(view) {
-            $.setupCalendarDates(view);
-          },
-          // Stay date has changed
-          eventDrop: function(event, dayDelta, minuteDelta, allDay, revertFunc, jsEvent, ui, view) {
-            //$.datesChanged(event, dayDelta, minuteDelta, allDay, revertFunc, jsEvent, ui, view);
-          }
+          ]
+
       });
-  }, 600);
+
 
   };
 
