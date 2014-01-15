408b8121ed4b7754fa812d5f461a7a722b280183
soumya <soumya@stayntouch.com>
2014-01-15 15:39:39 +0530

    CICO-1594 - change in inteface definition as per db, and calling API's

--- javascripts/admin/hotels/hotel_external_mappings.js
+++ javascripts/admin/hotels/hotel_external_mappings.js
@@ -4,7 +4,7 @@ var HotelExternalMappingsView = function(domRef){
   var that = this;
   this.externalMappings = [];
   
-  
+  // to handle subview events
   this.delegateSubviewEvents = function(){ 
   	that.myDom.find($('#external_mapping_table')).tablesorter();	
   	that.myDom.find('#go_back').on('click', that.goBack); 
@@ -12,6 +12,7 @@ var HotelExternalMappingsView = function(domRef){
   	// to get all external mappings to do internal filtering
   	that.getAllExternalMappings();
   };
+  //to get all external mappings data
   this.getAllExternalMappings =  function(){
   	var webservice = new WebServiceInterface();
 	
@@ -22,20 +23,18 @@ var HotelExternalMappingsView = function(domRef){
 	    webservice.getJSON(url, options);
   	
   };
+  // to go to previous page
   this.goBack = function(){
   	sntadminapp.gotoPreviousPage(that.viewParams, that.myDom);
   };
-  
-  
-
-   this.fetchCompletedOfGetExternalMappings = function(data){
-	   that.externalMappings = data.mapping_type;
-	   console.log(that.externalMappings);
-   };
-   
-     this.filterExternalMappings = function(e){
-     	console.log("--------------")
-  		var selectedMappingType = that.myDom.find("#mapping-type").val();
+  //to fetch external mapping details to do internal filtering
+  this.fetchCompletedOfGetExternalMappings = function(data){
+	  that.externalMappings = data.mapping_type;
+  };
+  // to repopultae snt vlues drop down on selecting external mappings 
+  this.filterExternalMappings = function(e){
+ 	if(e.target.id == "mapping-type"){
+ 		var selectedMappingType = that.myDom.find("#mapping-type").val();
 		mappingTypeValues = '';
 		that.myDom.find("#snt-value").find('option').remove().end();
 		$.each(that.externalMappings, function(key, value) {
@@ -43,18 +42,21 @@ var HotelExternalMappingsView = function(domRef){
 		    	mappingTypeValues = '<option value="" data-image="">Select value</option>';
 		    	$("#snt-value").append(mappingTypeValues);
 		    	$.each(value.sntvalues, function(mappingkey, mappingvalue) {
-		    		mappingTypeValues = '<option value="'+mappingvalue.name+'">'+mappingvalue.name+'</option>';
+		    		mappingTypeValues = '<option value="'+mappingvalue.value+'">'+mappingvalue.name+'</option>';
 		    		$("#snt-value").append(mappingTypeValues);
 		    	});
 		    }		    
 		});
+ 	}
+  		
   };
-  //function to add new department
+  //function to add new external mapping
   this.saveNewApi = function(event){ 
   	var postData = {};
   	postData.mapping_type = that.myDom.find("#mapping-type").val(); 
   	postData.snt_value = that.myDom.find("#snt-value").val(); 
-  	postData.external_value = that.myDom.find("#external-value").val(); 	
+  	postData.external_value = that.myDom.find("#external-value").val(); 
+  	postData.hotel_id = that.myDom.find("#selected_hotel").attr("data-hotel-id"); 	
   	
   	var url = 'urltocreate';
 	var webservice = new WebServiceInterface();		
@@ -67,5 +69,36 @@ var HotelExternalMappingsView = function(domRef){
 	};
 	webservice.postJSON(url, options);	
   };
+  //function to update department
+  this.updateApi = function(event){
+  	
+  	var postData = {};
+  	postData.mapping_type = that.myDom.find("#mapping-type").val(); 
+  	postData.snt_value = that.myDom.find("#snt-value").val(); 
+  	postData.external_value = that.myDom.find("#external-value").val(); 
+  	postData.hotel_id = that.myDom.find("#selected_hotel").attr("data-hotel-id");
+  	postData.value = that.myDom.find("#edit-external-mapping").attr("data-id");
   
+  	var url = 'cvnbvbnvbnvbn/'+postData.value;
+	var webservice = new WebServiceInterface();		
+	var options = {
+			   requestParameters: postData,
+			   successCallBack: that.fetchCompletedOfSave,
+			   successCallBackParameters:{ "event": event},
+			   loader:"BLOCKER"
+			   
+	};
+	webservice.putJSON(url, options);	
+	
+  };
+  //refreshing view with new data and showing message
+  this.fetchCompletedOfSave = function(data, requestParams){
+  	
+  	var url = "/ui/show?haml_file=admin/hotels/external_mappings&json_input=snt_admin/external_mappings.json&is_hash_map=true&is_partial=true";
+   	viewParams = {};
+  	sntapp.fetchAndRenderView(url, that.myDom, {}, 'BLOCKER', viewParams);
+    sntapp.notification.showSuccessMessage("Saved Successfully", that.myDom);		
+    that.cancelFromAppendedDataInline(requestParams['event']);  
+	 
+  };
 };
