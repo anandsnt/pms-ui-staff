fec45e33b74f98c2effa192c788a14994202f7b0
akhilastayntouch <akhila@stayntouch.com>
2014-01-15 15:00:40 +0530

    CICO-979 webservice call added

--- javascripts/app/change_staydates_view.js
+++ javascripts/app/change_staydates_view.js
@@ -9,10 +9,11 @@ var ChangeStayDatesView = function(viewDom){
   };
 
   this.executeLoadingAnimation = function(){
-  	changeView("nested-view", "", "view-nested-first", "view-nested-second", "move-from-right", false); 
+    changeView("nested-view", "", "view-nested-first", "view-nested-second", "move-from-right", false); 
   };
 
   this.pageinit = function(){
+    that.reservationId = that.viewParams.reservation_id;
 
     // Set calendar - timeout set to make sure functions from edit-reservation.js are available
 
@@ -48,41 +49,76 @@ var ChangeStayDatesView = function(viewDom){
           eventDrop: function(event, dayDelta, minuteDelta, allDay, revertFunc, jsEvent, ui, view) {
            // $.datesChanged(event, dayDelta, minuteDelta, allDay, revertFunc, jsEvent, ui, view);
 
-            var checkinOrig = $('.fc-event.check-in').attr('data-date');
-            var checkoutOrig = $('.fc-event.check-out').attr('data-date');
-            var newDateSelected = $.fullCalendar.formatDate(event.start, 'yyyy-MM-dd');
-            var firstAvailableDate = $('.fc-event:first').attr('data-date');
-            var lastAvailableDate = $('.fc-event:last').attr('data-date');
-            var finalCheckin = "";
-            var finalCheckout = "";
-            if(newDateSelected <=firstAvailableDate || newDateSelected >=lastAvailableDate){;
-              revertFunc();
-                
-            }
-           /* var date1 = '2014-01-05';
-            var abc = new Date(date1.split('-')[0], date1.split('-')[1], date1.split('-')[2]);
-            console.log(abc.getDate());*/
-            if(event.id == 'check-in'){
-              if(newDateSelected > checkoutOrig){
-                revertFunc();
-                return false;
-              }
-              finalCheckin = newDateSelected;
-              finalCheckout = checkoutOrig;
-            }else if (event.id == "check-out"){
-              if(newDateSelected < checkinOrig){
-                revertFunc();
-                return false;
-              }
-              finalCheckin = checkinOrig;
-              finalCheckout = newDateSelected;
-            }
+            that.datesChanged(event, revertFunc);
           }
 
       });
 
 
   };
+
+  /*this.datesChanged = function(event, revertFunc){
+    that.handleDragRange(event, revertFunc);
+  };*/
+
+
+  this.datesChanged = function(event, revertFunc){
+
+    var checkinOrig = $('.fc-event.check-in').attr('data-date');
+    var checkoutOrig = $('.fc-event.check-out').attr('data-date');
+    var newDateSelected = $.fullCalendar.formatDate(event.start, 'yyyy-MM-dd');
+    var firstAvailableDate = $('.fc-event:first').attr('data-date');
+    var lastAvailableDate = $('.fc-event:last').attr('data-date');
+    var finalCheckin = "";
+    var finalCheckout = "";
+    if(newDateSelected <=firstAvailableDate || newDateSelected >=lastAvailableDate){;
+      revertFunc();
+        
+    }
+    /*var date1 = '2014-01-05';
+    var abc = new Date(date1);
+    console.log(abc.getDate());*/
+
+    if(event.id == 'check-in'){
+      if(newDateSelected > checkoutOrig){
+        revertFunc();
+        return false;
+      }
+      finalCheckin = newDateSelected;
+      finalCheckout = checkoutOrig;
+    }else if (event.id == "check-out"){
+      if(newDateSelected < checkinOrig){
+        revertFunc();
+        return false;
+      }
+      finalCheckin = checkinOrig;
+      finalCheckout = newDateSelected;
+    }
+
+   // that.showReservationUpdates(finalCheckin, finalCheckout);
+
+  };
+
+  this.showReservationUpdates = function(checkinDate, checkoutDate){
+    var postParams = {"arrival_date": checkinDate, "dep_date": checkoutDate};
+
+    var url = '/staff/change_stay_dates/'+that.reservationId+'/update';
+    var webservice = new WebServiceInterface(); 
+    /*var successCallBackParams = {
+        'reservationId': reservationId,
+        'roomNumberSelected': roomNumberSelected, 
+    }; */ 
+    var options = {
+           requestParameters: postParams,
+           successCallBack: that.upgradeSuccess,
+           //successCallBackParameters: successCallBackParams,
+           loader: "BLOCKER"
+    };
+    webservice.postJSON(url, options);  
+
+  };
+
+
 
   this.backbuttonClicked = function(e){
     e.preventDefault();
