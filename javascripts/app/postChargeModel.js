3201c47707de501dfed942405cb1e76c825abe17
krishobh <krishobh@stayntouch.com>
2014-01-22 11:46:18 +0530

    CICO-1940 Implemented filtering

--- javascripts/app/postChargeModel.js
+++ javascripts/app/postChargeModel.js
@@ -7,9 +7,11 @@ var PostChargeModel = function(callBack) {
 	this.currentQuery = "";
 	
 	this.delegateEvents = function() {
+		
+		// Append bill number on modal while click +ADD from bill card page.
 		this.origin = this.params.origin;
-		if (this.origin == "bill_card") {
-			$("#select-bill-number").hide();
+		if(this.origin == views.BILLCARD){
+			that.myDom.find("#select-bill-number").hide();
 			that.myDom.find(".h2.message").append(this.params.bill_number);
 		}
 
@@ -30,20 +32,17 @@ var PostChargeModel = function(callBack) {
 
 	this.modalInit = function() {
 		this.fetchItemList();
-		
 	};
- //Clear Search Results 
-  this.clearResults = function(e){
-    //if the method is invoked from other views to clear search results, 'this', 'e' are undefined.
-  	if($(this).hasClass('visible')){  	
-  		$(this).removeClass('visible');
-    }
-
-    $('#query').val('');
-    
-  };
+ 	//Clear Search Results 
+  	this.clearResults = function(e){
+	    //if the method is invoked from other views to clear search results, 'this', 'e' are undefined.
+	  	if($(this).hasClass('visible')){  	
+	  		$(this).removeClass('visible');
+	    }
+	    $('#query').val('');
+  	};
   
-  //Search items
+  	//Search items
     this.queryEntered = function(e){
 		that.currentQuery = $(this).val();
         console.log(that.currentQuery);
@@ -51,15 +50,16 @@ var PostChargeModel = function(callBack) {
     	that.showHideClearQueryButton();
     	
     	that.displaySearchItem(that.currentQuery);
-    	
     };
+    
+    // To display search results
     this.displaySearchItem = function(query){
-    	console.log("here---"+query)
+    	that.myDom.find('#search-item-results').html("");
+    	
     	if(query == ""){
-        	//that.displaySearchItem(query);
         	return false;
       	}
-      	$('#search-item-results ul').html("");
+      	
     	try
 	    {
 	        var items=[];
@@ -68,8 +68,6 @@ var PostChargeModel = function(callBack) {
 	        	console.log(i);
 	        	console.log(value);
 	            if ((escapeNull(value.item_name).toUpperCase()).indexOf(query.toUpperCase()) >= 0 ){
-	            	console.log("found"+value.item_name);
-	            	console.log("found"+value.value);
 	            	var $count_html = "";
 	            	var html="";
 	            	var currency_code = getCurrencySymbol(value.currency_code);
@@ -80,12 +78,9 @@ var PostChargeModel = function(callBack) {
 					html = '<li id="items-list"><a href="#" data-type="post-charge" data-price="' + value.unit_price + '" data-item="' + value.item_name + '" data-is-favourite="' + value.is_favourite + '" data-id="' + value.value + '" data-charge-group="' + value.charge_group_value + '" data-cc="' + value.currency_code + '" data-base="unit" class="button white">' + value.item_name + '<span class="price"> '+currency_code+' <span class="value">' + value.unit_price + '</span></span>'+$count_html+'</a></li>';
 	            
 	            	items.push($('#search-item-results').append(html));
-	            
-	            
 	            }
     		});
     		
-    		//$('#search-item-results').html(html);
     		$.each(items, function(i,value){
 	            	$('#search-item-results').append(value).highlight(query);
 	        });
@@ -95,6 +90,8 @@ var PostChargeModel = function(callBack) {
 	    	$('#search-item-results').html('<li class="no-content"><span class="icon-no-content icon-search"></span></li>');
 	    }
     };
+    
+    // To show and hide clear button on search panel.
 	this.showHideClearQueryButton = function(){
 	  	if($('#query').val() !== '') {
 	        $('#clear-query:not(.visible)').addClass('visible');
@@ -102,6 +99,8 @@ var PostChargeModel = function(callBack) {
 	        $('#clear-query.visible').removeClass('visible');
 	    }
 	};
+	
+	// To fetch items data from API , initializing count of each item - store locally.
 	this.fetchItemList = function() {
 		$.ajax({
 			type : "GET",
@@ -127,100 +126,105 @@ var PostChargeModel = function(callBack) {
 
 	// Add item to charges to be posted list
 	this.clickItemList = function(e) {
+		
 		var element = $(e.target);
-
-		e.preventDefault();
-		e.stopImmediatePropagation();
-
-		// Count clicks
-		element.data('count', 1 + (element.data('count') || 0 ));
-
-		var $id = element.attr('data-id'),
-		$item = element.attr('data-item'),
-		$price = element.attr('data-price'),
-		$currency_code = getCurrencySymbol(element.attr('data-cc')),
-		$base = element.attr('data-base'),
-		$output = $item + ' <span class="count" /><span class="base">at ' + $currency_code + $price + ' / ' + $base + '</span><span class="price">$ <span class="value">' + $price + '</span></span>';
-
-		// Update right side panel
-		if (that.myDom.find('#items-added.hidden')) {
-			that.myDom.find('#no-items-added').addClass('hidden');
-			that.myDom.find('#items-added.hidden').removeClass('hidden');
-		}
 		
-		var is_item_selected = 0;
+		if(element.closest('li').prop('id') == "items-list"){
 		
-		$("#items-summary li" ).each(function() {
-			var id = $(this).attr('data-id')
-		  	if(id == $id){
-		  		is_item_selected = 1;
-		  	} 
-		});
-		
-		// To get count of the clicked item
-		var $count ;
-		for(var i=0; i < that.itemCompleteList.length; i++){
-			if($id == that.itemCompleteList[i].value){
-				that.itemCompleteList[i].count ++;
-				$count = that.itemCompleteList[i].count;
+			e.preventDefault();
+			e.stopImmediatePropagation();
+	
+			// Count clicks
+			element.data('count', 1 + (element.data('count') || 0 ));
+	
+			var $id = element.attr('data-id'),
+			$item = element.attr('data-item'),
+			$price = element.attr('data-price'),
+			$currency_code = getCurrencySymbol(element.attr('data-cc')),
+			$base = element.attr('data-base'),
+			$output = $item + ' <span class="count" /><span class="base">at ' + $currency_code + $price + ' / ' + $base + '</span><span class="price">$ <span class="value">' + $price + '</span></span>';
+	
+			// Update right side panel
+			if (that.myDom.find('#items-added.hidden')) {
+				that.myDom.find('#no-items-added').addClass('hidden');
+				that.myDom.find('#items-added.hidden').removeClass('hidden');
 			}
-		}
-
-		// First click - add item to list
-		if (!is_item_selected) {
 			
-			$('<span class="count" />').appendTo(element).text($count || 0);
-
-			// Add item to list
-			var items = [];
-			items.push($('<li data-id="' + $id + '" />').html($output));
-			that.myDom.find('#items-summary ul').append.apply(that.myDom.find('#items-summary ul'), items);
-
-			// Set scrollers & position charge total
-			if (pageScroll) {
-				destroyPageScroll();
-			}
-			createPageScroll('#items-summary');
-
-			if (that.myDom.find('#items-summary li').length > '4') {
-				pageScroll.scrollTo(0, -(that.myDom.find('#items-summary li').length - 4) * 45);
-				that.myDom.find('#total-charge').removeAttr('class');
-			} else {
-				that.myDom.find('#total-charge').removeAttr('class').addClass('offset-' + that.myDom.find('#items-summary li').length)
-			}
-		}
-
-		// Other clicks - increase count and value
-		else {
+			// To set flag whether the list-item already clicked or not.
+			var is_item_selected = 0;
+			$("#items-summary li" ).each(function() {
+				var id = $(this).attr('data-id')
+			  	if(id == $id){
+			  		is_item_selected = 1;
+			  	} 
+			});
 			
-			element.find('.count').text($count || 0);
-			$('#items-summary ul').find('li[data-id="' + $id + '"] .count').text($count || 0);
-
-			// Update count
-			if ($count > '1') {
-				$('#items-summary ul').find('li[data-id="' + $id + '"] .count').text('(' + ($count || 0) + ')');
+			// To get count of the clicked item
+			var current_item_count ;
+			for(var i=0; i < that.itemCompleteList.length; i++){
+				if($id == that.itemCompleteList[i].value){
+					that.itemCompleteList[i].count ++;
+					current_item_count = that.itemCompleteList[i].count;
+				}
 			}
-
-			// Update price
-			var $price = parseFloat($price * $count || 0).toFixed(2);
-			$('#items-summary ul').find('li[data-id="' + $id + '"] .value').text($price);
+	
+			// First click on a list item - add item to item summary 
+			if (!is_item_selected) {
+				
+				$('<span class="count" />').appendTo(element).text(current_item_count || 0);
+	
+				// Add item to list
+				var items = [];
+				items.push($('<li data-id="' + $id + '" />').html($output));
+				that.myDom.find('#items-summary ul').append.apply(that.myDom.find('#items-summary ul'), items);
+	
+				// Set scrollers & position charge total
+				if (pageScroll) {
+					destroyPageScroll();
+				}
+				createPageScroll('#items-summary');
+	
+				if (that.myDom.find('#items-summary li').length > '4') {
+					pageScroll.scrollTo(0, -(that.myDom.find('#items-summary li').length - 4) * 45);
+					that.myDom.find('#total-charge').removeAttr('class');
+				} else {
+					that.myDom.find('#total-charge').removeAttr('class').addClass('offset-' + that.myDom.find('#items-summary li').length)
+				}
+			}
+	
+			// Other clicks - increase count and value
+			else {
+				
+				element.find('.count').text(current_item_count || 0);
+				$('#items-summary ul').find('li[data-id="' + $id + '"] .count').text(current_item_count || 0);
+	
+				// Update count
+				if (current_item_count > '1') {
+					$('#items-summary ul').find('li[data-id="' + $id + '"] .count').text('(' + (current_item_count || 0) + ')');
+				}
+	
+				// Update price
+				var $price = parseFloat($price * current_item_count || 0).toFixed(2);
+				$('#items-summary ul').find('li[data-id="' + $id + '"] .value').text($price);
+			}
+	
+			// Update total charge price
+			var $totalPrice = 0;
+			$('#items-summary li .value').each(function() {
+				$totalPrice += parseFloat(1 * ($(this).text()));
+			});
+			$('#total-charge .value').text($totalPrice.toFixed(2));
+	
+			// Set scrollers
+			var $style = that.myDom.find('#items-listing .wrapper').attr('style').split('transform: translate('), $translate = $style[1].split(')')[0], $current = $translate.split(',')[1], $target = $current.split('px')[0];
+	
+			if (viewScroll) {
+				destroyViewScroll();
+			}
+			createViewScroll('#items-listing');
+			viewScroll.scrollTo(0, parseInt($target));
+			
 		}
-
-		// Update total charge price
-		var $totalPrice = 0;
-		$('#items-summary li .value').each(function() {
-			$totalPrice += parseFloat(1 * ($(this).text()));
-		});
-		$('#total-charge .value').text($totalPrice.toFixed(2));
-
-		// Set scrollers
-		var $style = that.myDom.find('#items-listing .wrapper').attr('style').split('transform: translate('), $translate = $style[1].split(')')[0], $current = $translate.split(',')[1], $target = $current.split('px')[0];
-
-		if (viewScroll) {
-			destroyViewScroll();
-		}
-		createViewScroll('#items-listing');
-		viewScroll.scrollTo(0, parseInt($target));
 
 	};
 
@@ -229,9 +233,9 @@ var PostChargeModel = function(callBack) {
 		var element = $(e.target);
 		var selectedGroupId = $(element).find('option:selected').attr('id');
 		// To apply filter
-		if (selectedGroupId == "show-fav")
+		if (selectedGroupId == "favorites")
 			that.showFavouriteItems();
-		else if (selectedGroupId == "show-all")
+		else if (selectedGroupId == "all-charge-groups")
 			that.showAllItems();
 		else
 			that.showFilteredItems(selectedGroupId);
@@ -240,7 +244,6 @@ var PostChargeModel = function(callBack) {
 	// Filter : To show favourites items
 	this.showFavouriteItems = function() {
 		that.myDom.find("#items-listing ul").html("");
-		
 		var html = '';
 		for (var i = 0; i < that.itemCompleteList.length; i++) {
 			if (that.itemCompleteList[i].is_favourite == "true") {
@@ -272,6 +275,7 @@ var PostChargeModel = function(callBack) {
 
 	// Filter : To show selected charge group items
 	this.showFilteredItems = function(filterId) {
+		that.myDom.find("#items-listing ul").html("");
 		var html = '';
 		for (var i = 0; i < that.itemCompleteList.length; i++) {
 			if (that.itemCompleteList[i].charge_group_value == filterId) {
