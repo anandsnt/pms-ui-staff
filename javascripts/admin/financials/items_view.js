72ec94b7ee94b85ecec61f77d6de3d97c0af0fe1
krishobh <krishobh@stayntouch.com>
2014-01-23 15:25:05 +0530

    CICO-3882 Admin-Hotel-Financials-Create a new button and screen to add 'Items'

--- javascripts/admin/financials/items_view.js
+++ javascripts/admin/financials/items_view.js
@@ -3,6 +3,89 @@ var ItemsView = function(domRef){
 	this.myDom = domRef; 
 	var that = this;
 
-  	
+  	this.delegateSubviewEvents = function(){
+    	that.myDom.find('#items').tablesorter();
+    	that.myDom.on('click', that.viewClickEventHandler);
+   	};
   
+  // To handle active/inactive ffps.
+	this.toggleButtonClicked = function(element){
+	    var itemId = element.closest('tr').attr('item-id');
+	    setTimeout(function(){
+          	var toggleStatus = element.parent().hasClass('on') ? "true" : "false";
+          	var postParams = {"id" : itemId, "set_active" : toggleStatus};
+console.log(postParams);
+          	var webservice = new WebServiceInterface(); 
+          	var options = {
+               	requestParameters: postParams,
+               	successCallBack : that.fetchCompletedOfSave,
+               	failureCallBack : that.fetchFailedOfSave,
+               	loader: "NONE"
+          	};
+          	var url = '';
+          	webservice.postJSON(url, options);
+          	return true;
+	    }, 100);
+	};
+	
+	this.saveNewApi = function(event) {
+
+	var url = '';
+	var action = "ACTION_SAVE"
+	that.makeAPICall(url, action, event);
+
+	};
+	
+	this.updateApi = function(event) {
+
+		var hlpId = that.myDom.find("form#edit-items").attr("item_id");
+		var url = '' ;
+		var action = "ACTION_EDIT"
+		that.makeAPICall(url , action, event);
+
+	};
+
+
+
+	this.makeAPICall = function(url, action, event){
+		var postData = {};
+		if(action == "ACTION_EDIT"){
+			postData.value = that.myDom.find("form#edit-items").attr("item_id");
+		}
+		postData.description = that.myDom.find("#item_desc").val();
+		postData.unit_price = that.myDom.find("#unit_price").val();
+		postData.charge_code = that.myDom.find("#charge_code").val();
+		postData.is_favorite = that.myDom.find("#is_favorite").val();
+console.log(postData);
+		var webservice = new WebServiceInterface();
+		var options = {
+			requestParameters : postData,
+			successCallBack : that.fetchCompletedOfSave,
+			successCallBackParameters : { "event" : event },
+			loader : "BLOCKER"
+
+		};
+		if(action == "ACTION_EDIT"){
+			webservice.putJSON(url, options);
+		}else if(action == "ACTION_SAVE"){
+			webservice.postJSON(url, options);
+		}
+		
+
+	}
+	
+	//refreshing view with new data and showing message
+	this.fetchCompletedOfSave = function(data, requestParams) {
+
+		var url = "";
+		viewParams = {};
+		
+		if (data.status == "success") {
+			sntapp.fetchAndRenderView(url, that.myDom, {}, 'BLOCKER', viewParams);
+			sntapp.notification.showSuccessMessage("Saved Successfully", that.myDom);
+			that.cancelFromAppendedDataInline(requestParams['event']);
+		} else {
+			sntapp.notification.showErrorList(data.errors, that.myDom);
+		}
+	};
 };
