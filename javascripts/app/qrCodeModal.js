cd67fe8b2d104473cff5a677c4436ea6ed2e26af
krishobh <krishobh@stayntouch.com>
2014-01-24 17:17:48 +0530

    CICO-3629 Rover - Display key QR code on Tablet after checkin complete

---
+++ javascripts/app/qrCodeModal.js
@@ -0,0 +1,7 @@
+var QrCodeModel = function(callBack) {
+	BaseModal.call(this);
+	var that = this;
+	this.reservation_id = getReservationId();
+	this.url = "ui/show?haml_file=modals/qrCodeModal.html.haml&json_input=stay_card/key_qr_code.json&is_hash_map=true&is_partial=true";
+
+};
