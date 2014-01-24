cddc40299d5da27a0294b72304a4a3bb4c5f236e
akhilastayntouch <akhila@stayntouch.com>
2014-01-24 15:38:44 +0530

    CICO-3503 Added FTP settings texboxes & changed the labels against phone & e-mail

--- javascripts/admin/HotelChains/hotelChainsView.js
+++ javascripts/admin/HotelChains/hotelChainsView.js
@@ -88,7 +88,13 @@ var HotelChainsView = function(domRef){
 		var emailT_C = $.trim(that.myDom.find("#email").val()); //T_C : terms & conditions
 		var terms_and_condtn = $.trim(that.myDom.find("#terms_and_condtn").val());
 		var import_frequency = $.trim(that.myDom.find("#import-frequency").val());
-		
+		// ftp details
+		var ftp_location = $.trim(that.myDom.find("#ftp-location").val());
+		var ftp_port = $.trim(that.myDom.find("#ftp-port").val());
+		var ftp_user = $.trim(that.myDom.find("#ftp-user").val());
+		var ftp_password = $.trim(that.myDom.find("#ftp-password").val());	
+		var ftp_respath = $.trim(that.myDom.find("#ftp-respath").val());
+			
 		var lovs = new Array();
 		// looping over list of values text boxes
 		that.myDom.find('input[name=lov]').each(function(){ 
@@ -117,6 +123,11 @@ var HotelChainsView = function(domRef){
 		data.terms_cond = terms_and_condtn;
 		data.lov = lovs;
 		data.import_frequency = import_frequency;
+		data.ftp_location = ftp_location;
+		data.ftp_port = ftp_port;
+		data.ftp_user = ftp_user;
+		data.ftp_password = ftp_password;		
+		data.ftp_respath = ftp_respath;	
 	    var options = {
 				   successCallBack: that.fetchCompletedOfSave,
 				   requestParameters: data,
@@ -137,6 +148,13 @@ var HotelChainsView = function(domRef){
 		var emailT_C = $.trim(that.myDom.find("#email").val()); //T_C : terms & conditions
 		var terms_and_condtn = $.trim(that.myDom.find("#terms_and_condtn").val());	
 		var import_frequency = $.trim(that.myDom.find("#import-frequency").val());
+		// ftp details
+		var ftp_location = $.trim(that.myDom.find("#ftp-location").val());
+		var ftp_port = $.trim(that.myDom.find("#ftp-port").val());
+		var ftp_user = $.trim(that.myDom.find("#ftp-user").val());
+		var ftp_password = $.trim(that.myDom.find("#ftp-password").val());	
+		var ftp_respath = $.trim(that.myDom.find("#ftp-respath").val());
+
 		var lovs = []; //list of values
 		// looping over list of values text boxes
 		that.myDom.find('input[name=lov]').each(function(){ 
@@ -165,6 +183,11 @@ var HotelChainsView = function(domRef){
 		data.terms_cond = terms_and_condtn;
 		data.lov = lovs;	
 		data.import_frequency = import_frequency;
+		data.ftp_location = ftp_location;
+		data.ftp_port = ftp_port;
+		data.ftp_user = ftp_user;
+		data.ftp_password = ftp_password;		
+		data.ftp_respath = ftp_respath;			
 	    var options = {
 				   successCallBack: that.fetchCompletedOfSave,
 				   requestParameters: data,
