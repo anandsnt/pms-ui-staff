51e2e6b707e9f8c9abfe621bb10ffa952b757ee7
Vijay Dev <vijay@stayntouch.com>
2014-01-27 16:34:03 +0530

    CICO-3312: Fixed js bug

--- javascripts/app/staycard.js
+++ javascripts/app/staycard.js
@@ -37,15 +37,11 @@ var StayCard = function(viewDom){
   this.postCardSwipData = function(swipedCardData) {
     var swipedCardData = swipedCardData;
 
-    alert( JSON.stringify(swipedCardData) );
-
     var url = 'http://pms-dev.stayntouch.com/staff/payments/tokenize';
 
     var _successCallBack = function(token) {
       // add token to card data
       swipedCardData.token = token.data;
-
-      alert( JSON.stringify(swipedCardData) );
 
       // if addNewPaymentModal instance doen't exist, create it
       if ( !sntapp.getViewInst('addNewPaymentModal') ) {
@@ -72,7 +68,7 @@ var StayCard = function(viewDom){
 
     var options = {
       loader: 'BLOCKER',
-      requestParameters: cardData.getTokenFrom,
+      requestParameters: swipedCardData.getTokenFrom,
       successCallBack: _successCallBack,
       failureCallBack: function(error) {
         sntapp.notification.showErrorMessage('Sorry we could not get a response from server. Please try again.');
