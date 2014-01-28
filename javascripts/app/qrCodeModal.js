95f0353472b76272d2d498993dc7ebf9cfb53beb
krishobh <krishobh@stayntouch.com>
2014-01-28 16:51:43 +0530

    CICO-3629 API integration

--- javascripts/app/qrCodeModal.js
+++
@@ -1,6 +0,0 @@
-var QrCodeModel = function(callBack) {
-	BaseModal.call(this);
-	var that = this;
-	this.reservation_id = getReservationId();
-	this.url = "staff/reservations/"+that.reservation_id+"/get_key_on_tablet";
-};
--- javascripts/app/select_key_modal.js
+++ javascripts/app/select_key_modal.js
@@ -2,10 +2,7 @@ var SelectKeyModel = function(callBackForSuccess,callBackForQrCode) {
 	BaseModal.call(this);
 	var that = this;
 	this.reservation_id = getReservationId();
-	
-	this.url = "ui/show?haml_file=modals/selectKeyModal.html.haml&json_input=stay_card/key_qr_code.json&is_hash_map=true&is_partial=true";
-	//this.url = "staff/reservations/"+that.reservation_id+"/get_key_on_tablet";
-	
+	this.url = "staff/reservations/"+that.reservation_id+"/show_key_delivery";
 	this.delegateEvents = function(){
 		that.myDom.find('#key-cut').on('click', callBackForSuccess);
 		that.myDom.find('#show-qr').on('click', callBackForQrCode);
