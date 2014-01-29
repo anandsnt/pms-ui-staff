0165a384cda21d9eb697d21be84b2d49a89ed9eb
krishobh <krishobh@stayntouch.com>
2014-01-29 16:43:34 +0530

    CICO-3629 Fixed issue on qr code modal close

--- javascripts/app/qr_code_modal.js
+++ javascripts/app/qr_code_modal.js
@@ -6,7 +6,17 @@ var QrCodeModel = function(callBack) {
 	this.url = "staff/reservations/"+that.reservation_id+"/get_key_on_tablet";
 	
 	this.delegateEvents = function(){
+		// Setting style and attributes to the modal.
 		$("#modal").addClass("ui-draggable");
 		$("#modal").attr("data-position","bottom-right");
+		that.myDom.find('#modal-close').on('click', that.closeButtonClicked);
+   };
+   
+   this.closeButtonClicked = function(e){
+   		// To go back to search screen when close button clicked from registraion card.
+	   	if(that.myDom.find('#modal-close').attr('data-reservation-status') == "CHECKING_IN"){
+	   		callBack();
+	   	}
+   	
    };
 };
