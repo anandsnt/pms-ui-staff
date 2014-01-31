6909518c530c3ae515cc397d8463bb3d2eef19a1
akhilastayntouch <akhila@stayntouch.com>
2014-01-31 18:11:17 +0530

    CICO-4109 file upload with base 64 encoding

--- javascripts/admin/hotels/hotel_details.js
+++ javascripts/admin/hotels/hotel_details.js
@@ -3,6 +3,7 @@ var HotelDetailsView = function(domRef){
   this.myDom = domRef; 
   this.currentView = $("body").attr("id") ;
   var that = this;
+  this.fileContent = "";
   
   this.pageinit = function(){
   	
@@ -14,6 +15,9 @@ var HotelDetailsView = function(domRef){
   	that.myDom.find('#cancel, #go_back').on('click', that.goBackToPreviousView); 
   	that.myDom.find("#re-invite").on('click', that.reInvite);
   	that.myDom.find("#external-mappings").on('click', that.renderExternalMappings);
+  	that.myDom.find('#mli-certificate').on('change', function(){
+  		that.readCertificate(this);
+  	});
   
   };
   // function to view external mappings 
@@ -186,7 +190,8 @@ var HotelDetailsView = function(domRef){
 					admin_password:password,
 					admin_password_confirmation:confirmPassword,
 					hotel_time_zone: hotelTimeZone,
-					auto_logout_delay: hotelAutoLogoutTime
+					auto_logout_delay: hotelAutoLogoutTime,
+					mli_certificate : that.fileContent
 					} ;
 	        }
 	        else 
@@ -223,5 +228,18 @@ var HotelDetailsView = function(domRef){
   this.gotoPreviousPage = function() {
 	 	  
 	  sntadminapp.gotoPreviousPage(that.viewParams, that.myDom);
+  };
+
+  this.readCertificate = function(input) {
+  	   //$('#file-preview').attr('changed', "changed");
+       if (input.files && input.files[0]) {
+           var reader = new FileReader();
+           reader.onload = function(e) {
+           		console.log(e.target.result);
+           	   //$('#file-preview').attr('src', e.target.result);
+               that.fileContent = e.target.result;
+           };
+           reader.readAsDataURL(input.files[0]);
+       }
   };
 };
