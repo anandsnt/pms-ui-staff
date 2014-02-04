128b0db4f1fd54cdf8a98cbc16b0308500cef6c0
akhilastayntouch <akhila@stayntouch.com>
2014-02-04 14:58:02 +0530

    CICO-4112 scroll check

--- javascripts/app/room_assignment_view.js
+++ javascripts/app/room_assignment_view.js
@@ -42,14 +42,14 @@ var RoomAssignmentView = function(viewDom){
   };
   //
   this.createViewScroll = function(){
-    console.log("20000 time");
+    console.log("10000 time");
    // if (viewScroll) { destroyViewScroll(); }
       setTimeout(function(){
 
         if (that.myDom.find($('#room-attributes')).length) { createViewScroll('#room-attributes'); }
         //if (that.myDom.find($('#room-upgrades')).length) { createViewScroll('#room-upgrades'); }
         createViewScroll('#room-upgrades');
-      }, 20000);
+      }, 10000);
   };
 
   //Scroll view creation for the the room list
