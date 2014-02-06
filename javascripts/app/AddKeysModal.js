86ccab79ffa63d5d6072c8984ebaa2cf12727455
krishobh <krishobh@stayntouch.com>
2014-02-06 13:32:22 +0530

    CICO-4342 Implemented checkin process with sample JSON

--- javascripts/app/AddKeysModal.js
+++ javascripts/app/AddKeysModal.js
@@ -2,7 +2,8 @@ var AddKeysModal = function(gotoStayCard,gotoSearch) {
 	BaseModal.call(this);
 	var that = this;
 	var reservation_id = getReservationId();
-	this.url = "staff/reservations/" + reservation_id + "/get_key_setup";
+	//this.url = "staff/reservations/" + reservation_id + "/get_key_setup";
+	this.url = "/ui/show?haml_file=modals/addKeys&json_input=stay_card/key_email.json&is_hash_map=true&is_partial=true";
 	this.delegateEvents = function() {
 		
 		that.myDom.find('#goto-staycard').on('click', that.clickedGotoStayCard);
@@ -10,8 +11,13 @@ var AddKeysModal = function(gotoStayCard,gotoSearch) {
 		
 		if(this.params.origin == views.BILLCARD){
 			that.myDom.find('#modal-close').remove();
+			$("#modal-overlay").css("pointer-events","none");
+			$("#modal-overlay").unbind( "click" );
 		}
-		
+		if(this.params.origin == views.STAYCARD){
+			that.myDom.find("#goto-staycard").hide();
+			that.myDom.find("#goto-search").hide();
+		}
 	};
 	this.modalInit = function() {
 
