d17d71efde0ce052722665dacbcee55ece3bc463
krishobh <krishobh@stayntouch.com>
2014-02-10 17:26:04 +0530

    CICO-4562 Validation on chekcout for accepting the charges

--- javascripts/app/registration_card_view.js
+++ javascripts/app/registration_card_view.js
@@ -205,7 +205,7 @@ var RegistrationCardView = function(viewDom) {
 		if (signature == "[]" && required_signature_at == "CHECKIN")
 			errorMessage = "Signature is missing";
 		else if (!terms_and_conditions)
-			errorMessage = "Please check the box to accept the charges";
+			errorMessage = "Please check agree to the Terms & Conditions";
 
 		if (errorMessage != "") {
 			that.showErrorMessage(errorMessage);
@@ -342,8 +342,8 @@ var RegistrationCardView = function(viewDom) {
 			if (signature == "[]" && required_signature_at == "CHECKOUT")
 				errorMessage = "Signature is missing";
 			else if (!terms_and_conditions)
-				errorMessage = "Please check agree to the Terms & Conditions";
-
+				errorMessage = "Please check the box to accept the charges";
+				
 			if (errorMessage != "") {
 				that.showErrorMessage(errorMessage);
 				return;
