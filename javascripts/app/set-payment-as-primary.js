d04f4e6132ee3bbeb0953a9e5bb13b81ec7a2c7c
soumya <soumya@stayntouch.com>
2014-02-10 19:28:35 +0530

    CICO-1070 - Show error message on delete failure

--- javascripts/app/set-payment-as-primary.js
+++ javascripts/app/set-payment-as-primary.js
@@ -10,13 +10,14 @@ var SetPaymentAsPrimaryModal = function(){
 		that.myDom.find('#credit-card-delete').on('click', that.deleteCreditCard);		
 		var creditParam = this.params;
 		this.credit_id = creditParam.credit_id;
+		this.backDom = creditParam.myDom;
 
 	};
 	
 	this.unbindCancelEvent = function(){
 		that.myDom.find('#credit-card-set-as-primary').off('click');
 		that.myDom.find('#credit-card-delete').off('click');
-	}
+	};
 	
 	this.modalInit = function(){
         
@@ -55,6 +56,11 @@ var SetPaymentAsPrimaryModal = function(){
 		//$("#credit_row" + $credit_card_id).remove();
     	$(".credit-card-option-row" + $credit_card_id).remove();
     	sntapp.notification.showSuccessMessage('Payment method successfully deleted!');
+    	var message_element = that.backDom.find("#notification-message-guest");
+		message_element.removeClass('notice success_message error_message');
+		message_element.html("");			
+		that.myDom.find("#notification-message-guest").slideDown(700, function() {});
+		$("#guest-credit").removeClass("error");
     };
     this.deleteCreditCard = function(){
   		var $credit_card_id = that.credit_id;		
@@ -65,8 +71,17 @@ var SetPaymentAsPrimaryModal = function(){
 	    var options = { 
 	    				requestParameters: data,
 	    				successCallBack: that.fetchCompletedOfDeleteCreditCard,
+	    				failureCallBack: that.fetchFailedOfDeleteCreditCard,
 	    				loader: 'blocker'
 	    		};
 	    webservice.postJSON(url, options);	
+    };
+    this.fetchFailedOfDeleteCreditCard = function(errorMessage){
+    	var message_element = that.backDom.find("#notification-message-guest");
+		message_element.removeClass('success_message error_message').addClass("notice error_message");
+		message_element.html("Some error occured:"+ errorMessage);			
+		that.myDom.find("#notification-message-guest").slideDown(700, function() {});
+		$("#guest-credit").addClass("error");
+    	sntapp.notification.showSuccessMessage('Payment method successfully deleted!');
     };
 };
