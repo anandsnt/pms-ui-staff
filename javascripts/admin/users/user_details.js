3fe81f385b89276dabf400349fe03d8f0ebff06a
soumyacyriac <soumya@stayntouch.com>
2014-02-12 15:30:57 +0530

    CICO-4626 - Fixed upload button shows along with WAC

--- javascripts/admin/users/user_details.js
+++ javascripts/admin/users/user_details.js
@@ -45,6 +45,7 @@ var UserDetailsView = function(domRef){
   };
   //go to previous page withount any update in view
   this.gotoPreviousPage = function() {
+  	that.myDom.html("");
   	sntadminapp.gotoPreviousPage(that.viewParams, that.myDom);
   };
 
