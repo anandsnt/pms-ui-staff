cde027ebef2976f9e66235c7ac8a73d991ebee8a
Resheil <resheil@stayntouch.com>
2014-02-18 14:34:25 +0530

    CICO-4124 handling error

--- guestweb/checkoutnow/controllers/checkOutStatusController.js
+++ guestweb/checkoutnow/controllers/checkOutStatusController.js
@@ -1,17 +1,43 @@
 
 (function() {
-	var checkOutStatusController = function($scope, baseWebService) {
+	var checkOutStatusController = function($scope, baseWebService,$q,$http,$rootScope) {
 //to be deleted
 	$scope.finalMessage = "Thank You for staying with us!"
+	$scope.posted = false;
 
-	// baseWebService.post().then(function() {
-	// 		$scope.finalMessage = "Thank You for staying with us!"
-	// 	});
-	// };
-};
+
+		$scope.posted = false;
+		var posting = function() {
+			var deferred = $q.defer();
+			var reservation_id = '';
+			var url = '/guest_web/home/checkout_guest';
+			var data = {'reservation_id':$rootScope.reservationID};
+			$http.post(url, data).success(function(response){
+				deferred.resolve(response);
+			}).error(function(){
+				deferred.reject();
+			});
+				
+			return deferred.promise;
+		}
+		
+		posting().then(function (response) {
+			$scope.posted = true;	
+			// $scope.lateCheckOut = response;
+			$scope.success = response.status ? true : false;
+			console.log($scope);
+
+			alert($scope.success)
+			
+		});
+
+
+
+	 };
+
 	var dependencies = [
 		'$scope',
-		'baseWebService',
+		'baseWebService','$q','$http','$rootScope',
 		checkOutStatusController
 	];
 
