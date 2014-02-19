3fba79da9b7871f6d6096c14bd77f72d80cd6d79
Resheil <resheil@stayntouch.com>
2014-02-19 16:03:19 +0530

    CICO-4124 handling checkoutlater unavailable case in landing page case

--- guestweb/scripts/app.js
+++ guestweb/scripts/app.js
@@ -4,9 +4,11 @@ var snt = angular.module('snt', ['ngRoute']);
 snt.config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/', {
 		templateUrl: '/assets/landing/landing.html',
+		controller: 'checkOutLandingController',
 		resolve: {
 			// load only when urls and user have been loadded
 			load: function(UrlService, UserService) {
+
 				return UrlService.fetch() && UserService.fetch();
 			}
 		}
@@ -108,3 +110,18 @@ snt.config(function ($httpProvider) {
 
 
     
+
+(function() {
+	var checkOutLandingController = function($rootScope,$location) {
+
+	if ($rootScope.isLateCheckoutAvailable === 'false') 
+		 $location.path('/checkOutNow')
+};
+
+	var dependencies = [
+		'$rootScope','$location',
+		checkOutLandingController
+	];
+
+	snt.controller('checkOutLandingController', dependencies);
+})();
