ef163766365f3b15b600a3ef49e99c1e2a3fb546
shijustayntouch <shiju@stayntouch.com>
2014-02-21 18:43:14 +0530

    Guestweb-Fix for webcheckout-reorder js.

--- javascripts/guestweb.js
+++ javascripts/guestweb.js
@@ -1,7 +1,7 @@
-//=require ../guestweb/static/lib/js/angular.min.js
-//=require ../guestweb/static/lib/js/angular-route.js
 //=require ../guestweb/static/lib/js/underscore.min.js
 //=require ../guestweb/static/lib/js/less-1.4.1.min.js
+//=require ../guestweb/static/lib/js/angular.min.js
+//=require ../guestweb/static/lib/js/angular-route.js
 //= require_tree ../guestweb/scripts
 //= require_tree ../guestweb/shared
 //= require_tree ../guestweb/checkoutlater
