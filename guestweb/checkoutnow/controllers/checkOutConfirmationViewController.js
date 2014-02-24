7beeda3f80cf3c6dc27d8bd2b8a1beffe005b01f
Resheil <resheil@stayntouch.com>
2014-02-24 12:20:38 +0530

    CICO-4124 cleaning code and adding comments

--- guestweb/checkoutnow/controllers/checkOutConfirmationViewController.js
+++ guestweb/checkoutnow/controllers/checkOutConfirmationViewController.js
@@ -2,9 +2,11 @@
 (function() {
 	var checkOutConfirmationController = function($scope, confirmationService,$rootScope) {
 		
+		// fecth text details to display
+		
 		confirmationService.fetch().then(function(details) {
 			$scope.details = details;
-		
+			
 		});
 		
 		$scope.footerMessage1 = $rootScope.isLateCheckoutAvailable ? 'Late check-out is not available.' :'' 
@@ -12,9 +14,9 @@
 	};
 
 	var dependencies = [
-		'$scope',
-		'confirmationService','$rootScope',
-		checkOutConfirmationController
+	'$scope',
+	'confirmationService','$rootScope',
+	checkOutConfirmationController
 	];
 
 	snt.controller('checkOutConfirmationController', dependencies);
