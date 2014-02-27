295ca46f4cf442ff253811309c6a5f5deaad6b98
soumya <soumya@stayntouch.com>
2014-02-27 11:19:08 +0530

    Initial Commit angular js

--- rover/js/app.js
+++ rover/js/app.js
@@ -1,3 +1,10 @@
-var sntRover = angular.module('sntRover');
+var sntRover = angular.module('sntRover',[]);
 
+
+
+sntRover.controller('dashboardController',['$scope', function($scope){
+	$scope.goToSearch = function(){
+		alert("jjj")
+	};
+}]);
 
