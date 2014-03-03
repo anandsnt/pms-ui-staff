338737cce0c5b388a744af524b9b5161bb181986
Resheil <resheil@stayntouch.com>
2014-03-03 11:55:46 +0530

    CICO-5002 handling network error in checkout later options page

--- guestweb/checkoutlater/controllers/checkOutLaterController.js
+++ guestweb/checkoutlater/controllers/checkOutLaterController.js
@@ -11,10 +11,22 @@
 		 $location.path('/checkOutNow')
 	
 
-		$scope.showBackButtonImage = true
+		$scope.showBackButtonImage = true;
+		$rootScope.netWorkError = false;
+
+		$rootScope.isFetching = true;
+
+		$rootScope.$watch('netWorkError',function(){
+
+			if($rootScope.netWorkError)
+				$scope.isFetching = false;
+		});
+
 
 		LateCheckOutChargesService.fetch().then(function(charges) {
 			$scope.charges = charges;
+			$rootScope.netWorkError = false;
+			$scope.isFetching = false;
 
 
 			if($scope.charges.length > 0)
