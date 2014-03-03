7cf191856cc19e2e2d26b8a71781cead30750797
Dipin <dipin@stayntouch.com>
2014-03-03 13:50:58 +0530

    CICO-4143 Added the UI changes to have the checkin alert time

--- javascripts/admin/hotels/zest_checkin_configuration.js
+++ javascripts/admin/hotels/zest_checkin_configuration.js
@@ -1,21 +1,21 @@
 var ZestCheckinConfiguration = function(domRef){
-  BaseView.call(this);  
-  this.myDom = domRef; 
+  BaseView.call(this);
+  this.myDom = domRef;
   var that = this;
-  
-  this.delegateEvents = function(){  	
-  	that.myDom.find('#cancel, #go_back').on('click', that.goBackToPreviousView); 
+
+  this.delegateEvents = function(){
+  	that.myDom.find('#cancel, #go_back').on('click', that.goBackToPreviousView);
   	that.myDom.find('#save_guest_checkin').on('click', that.saveGuestCheckin);
   };
-  
+
   this.goBackToPreviousView = function() {
  	sntadminapp.gotoPreviousPage(that.viewParams, that.myDom);
   };
   // To save checkin configuration
   this.saveGuestCheckin = function() {
-  	
+
   	 var is_send_alert = "false";
-  	 
+
   	 if(that.myDom.find("#send-checkin-alert").parent().hasClass("on")){
   	 	 is_send_alert = "true";
   	 }
@@ -24,23 +24,25 @@ var ZestCheckinConfiguration = function(domRef){
 	  	  var is_notify = "true";
 	 }
 	 var guest_checkin_alert = that.myDom.find("#guest-checkin-alert").val();
-	 
+	 var alert_time = that.myDom.find("#checkin-alert-time").val();
+
 	 var data = {
 		    "checkin_alert_message": guest_checkin_alert,
 		    "is_send_alert": is_send_alert,
+		    "checkin_alert_time": alert_time,
 		    "is_notify_on_room_ready": is_notify
 	 };
-	 
+
 	 var url = '/admin/checkin_setups/save_setup';
 	 var webservice = new WebServiceInterface();
-	 var options = { 
+	 var options = {
 				requestParameters: data,
 				successCallBack: that.fetchCompletedOfSaveGuestCheckinConfig,
 				failureCallBack : that.fetchFailedOfSaveGuestCheckInConfig,
 				loader: 'blocker'
 	 };
-	 webservice.postJSON(url, options);	
-	    
+	 webservice.postJSON(url, options);
+
   };
   // To handle success on save API
   this.fetchCompletedOfSaveGuestCheckinConfig = function(data) {
@@ -50,5 +52,5 @@ var ZestCheckinConfiguration = function(domRef){
   this.fetchFailedOfSaveGuestCheckInConfig = function(errorMessage) {
   	sntapp.notification.showErrorMessage(errorMessage, that.myDom);
   };
-  
+
 };
