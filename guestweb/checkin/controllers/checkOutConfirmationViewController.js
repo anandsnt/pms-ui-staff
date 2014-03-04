4911d7fa9e49cd7b81d737eb110d7871e4518f52
Resheil <resheil@stayntouch.com>
2014-03-04 12:39:48 +0530

    CICO-4125 adding checkinconfirmation controller functionalities

---
+++ guestweb/checkin/controllers/checkOutConfirmationViewController.js
@@ -0,0 +1,20 @@
+
+(function() {
+	var checkInConfirmationViewController = function($scope) {
+
+  
+		$scope.subtitle1 = "To provide you with the a ";
+		$scope.subtitle2 = "secure check-in ";
+		$scope.subtitle3 = "process, please confirm the following";
+
+
+
+};
+
+	var dependencies = [
+	'$scope',
+	checkInConfirmationViewController
+	];
+
+	snt.controller('checkInConfirmationViewController', dependencies);
+})();
