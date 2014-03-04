12622a7cbdfc206110e537f2a8e746f1d33dd923
Resheil <resheil@stayntouch.com>
2014-03-04 14:32:26 +0530

    CICO-4125 popup implementation for error

--- guestweb/checkin/controllers/checkOutConfirmationViewController.js
+++ guestweb/checkin/controllers/checkOutConfirmationViewController.js
@@ -1,18 +1,29 @@
 
 (function() {
-	var checkInConfirmationViewController = function($scope) {
+	var checkInConfirmationViewController = function($scope,$modal,$rootScope) {
 
   
 		$scope.subtitle1 = "To provide you with the a ";
 		$scope.subtitle2 = "secure check-in ";
 		$scope.subtitle3 = "process, please confirm the following";
 
+$scope.opts = {
+    backdrop: true,
+    backdropClick: true,
+    templateUrl: '/assets/checkin/partials/errorModal.html',
 
+  };
+
+$scope.openDialog = function() {
+    var d = $modal.open($scope.opts);
+   
+  };
+  $scope.openDialog();
 
 };
 
 	var dependencies = [
-	'$scope',
+	'$scope','$modal','$rootScope',
 	checkInConfirmationViewController
 	];
 
