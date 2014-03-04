aaf286a4ebfdd87886ae28274d08aec30c116600
Resheil <resheil@stayntouch.com>
2014-03-04 18:53:31 +0530

    CICO-4125 adding controller and partial for reservation details

--- guestweb/checkin/controllers/checkInConfirmationViewController.js
+++ guestweb/checkin/controllers/checkInConfirmationViewController.js
@@ -1,21 +1,47 @@
 
 (function() {
-	var checkInConfirmationViewController = function($scope) {
-
+	var checkInConfirmationViewController = function($scope,$modal,$rootScope) {
 
 
 		$scope.subtitle1 = "To provide you with the a ";
 		$scope.subtitle2 = "secure check-in ";
 		$scope.subtitle3 = "process, please confirm the following";
 
+ //setup options for modal
 
+		$scope.opts = {
+			backdrop: true,
+			backdropClick: true,
+			templateUrl: '/assets/checkin/partials/errorModal.html',
+			controller: ModalInstanceCtrl,
 
-     }
+		};
+
+		$scope.openDialog = function() {
+			var d = $modal.open($scope.opts);
+
+		};
+  //to be used when authentication failded
+
+ 		 // $scope.openDialog();
+
+};
 
 		var dependencies = [
-	'$scope',
-	checkInConfirmationViewController
-	];
+		'$scope','$modal','$rootScope',
+		checkInConfirmationViewController
+		];
 
-	snt.controller('checkInConfirmationViewController', dependencies);
-})();
+		snt.controller('checkInConfirmationViewController', dependencies);
+		})();
+
+
+// controller for the modal
+
+		var ModalInstanceCtrl = function ($scope, $modalInstance) {
+
+
+			$scope.closeDialog = function () {
+				$modalInstance.dismiss('cancel');
+			};
+};
