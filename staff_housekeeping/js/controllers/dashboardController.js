1cbc2aea4c0df7de14d6787a6e06269555c2dd8c
akhilastayntouch <akhila@stayntouch.com>
2014-03-06 16:58:53 +0530

    CICO-5057 display dashboard contents from the API

--- staff_housekeeping/js/controllers/dashboardController.js
+++ staff_housekeeping/js/controllers/dashboardController.js
@@ -1,16 +1,9 @@
 hkRover.controller('DashboardController',['$scope', 'DashboardService',  function($scope, DashboardService){
-	/*$scope.data = {
-		"guests_duein": "144",
-		"guests_inhouse": "1",
-		"guests_dueout": "1",
-		"rooms_occupied": "20",
-		"rooms_clean": "2",
-		"rooms_dirty": "23"
-	}*/
 
-	$scope.data = DashboardService.data;
-
-
+	DashboardService.fetch().then(function(messages) {
+		console.log(messages);
+	      $scope.data = messages;
+	});
 
 }]);
 
--- staff_housekeeping/js/services/dashboardService.js
+++ staff_housekeeping/js/services/dashboardService.js
@@ -1,23 +1,17 @@
 hkRover.service('DashboardService',['$http', '$q', function($http, $q){
-	this.data = {
-		"guests_duein": "144",
-		"guests_inhouse": "1",
-		"guests_dueout": "1",
-		"rooms_occupied": "20",
-		"rooms_clean": "2",
-		"rooms_dirty": "23"
-	}	
 
 	this.fetch = function(){
 		var deferred = $q.defer();
-		var url = 'keys/fetch_encode_key.json';
-		$http.get(url).success(function(data, status) {
-		    // Some extra manipulation on data if you want...
-		    deferred.resolve(data);
+		var url = '/house/dashboard.json';
+
+		$http.get(url).success(function(response, status) {
+		    deferred.resolve(response.data);
 		}).error(function(data, status) {
 		    deferred.reject(data);
 		});
 
 		return deferred.promise;
 	}
+
+
 }]);
