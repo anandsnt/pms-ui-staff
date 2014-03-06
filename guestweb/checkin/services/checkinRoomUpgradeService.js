dd17c92701b0622afaf79f5ac69d2f07c4824c0d
Resheil <resheil@stayntouch.com>
2014-03-06 19:46:15 +0530

    CICO-4125 Implementing API for upgrades page

---
+++ guestweb/checkin/services/checkinRoomUpgradeService.js
@@ -0,0 +1,40 @@
+(function() {
+	var checkinRoomUpgradeService = function($q,baseWebService,$rootScope) {
+		
+		var responseData = {};
+
+
+		//fetch texts to be displayed
+
+		var login = function(data) {
+			
+			var deferred = $q.defer();
+
+			var url = '/guest_web/search.json';
+	
+			baseWebService.post(url,data).then(function(response) {
+
+
+
+				this.responseData = response;
+				deferred.resolve(this.responseData);
+			});
+			
+
+			return deferred.promise;
+		};
+
+
+		return {
+			responseData: responseData,
+			login : login
+		}
+	};
+
+	var dependencies = [
+	'$q','baseWebService','$rootScope',
+	checkinRoomUpgradeService
+	];
+
+	snt.factory('checkinRoomUpgradeService', dependencies);
+})();
