c6e15b24f92017bc5ae7d3cd584291fb9052f7b5
Resheil <resheil@stayntouch.com>
2014-03-06 11:24:06 +0530

    CICO-4125 cleaning resrvation details controller

--- guestweb/checkin/controllers/checkInReservationDetails.js
+++ guestweb/checkin/controllers/checkInReservationDetails.js
@@ -1,17 +1,35 @@
 
 (function() {
-	var checkInReservationDetails = function($scope,$rootScope) {
+	var checkInReservationDetails = function($scope,$rootScope,$location) {
 
 
 		$scope.heading = "Your Trip details";
+
 		$scope.ShowupgradedLabel = false;
+
+		$scope.upgradesAvailable = true;
+
+		$scope.checkInButtonClicked = function(){
+
+			if($scope.upgradesAvailable){
+
+				$scope.upgradesAvailable = false;
+				$location.path('/checkinUpgrade');
+			}
+			else
+				$location.path('/checkinKeys');
+
+
+
+
+		}
 
 
 
 };
 
 		var dependencies = [
-		'$scope','$rootScope',
+		'$scope','$rootScope','$location',
 		checkInReservationDetails
 		];
 
