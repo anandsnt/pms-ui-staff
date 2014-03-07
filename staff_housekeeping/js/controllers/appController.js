c33cd4a991545226ce1c69a2dcdb6333b5f41370
akhilastayntouch <akhila@stayntouch.com>
2014-03-07 21:39:11 +0530

    CICO-5057 search listing and filter by room implemented

--- staff_housekeeping/js/controllers/appController.js
+++ staff_housekeeping/js/controllers/appController.js
@@ -1,22 +1,14 @@
 hkRover.controller('appController',['$scope', '$state', function($scope, $state){
 
-    $scope.init = function () {
-            console.log("App Controler init");
-		// preselect the current reservation group
-    };
     $scope.menuOpen = false;
 
     $scope.$on("navToggled", function(){
         $scope.menuOpen = !$scope.menuOpen;
-        console.log("My event triggered");
     });
     
     $scope.getMenuClass = function(){
-        console.log("getMenuClass called");
         return $scope.menuOpen ? "menu-open" : "";
     };
     
-
-    $scope.init();
 }]);
 
