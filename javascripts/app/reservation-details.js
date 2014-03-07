59d60a7714ffee0f4e52b366dd2cc97312dd10b1
krishobh <krishobh@stayntouch.com>
2014-03-07 14:55:28 +0530

    CICO-5246 solved issue regrding breaking reservation list while switching to previous.

--- javascripts/app/reservation-details.js
+++ javascripts/app/reservation-details.js
@@ -5,7 +5,7 @@ var reservationDetailsView = function(domRef) {
 	this.reservation_id = getReservationId();
 	var that = this;
 	this.pageinit = function() {
-		//that.updateTimelineIcon();
+		that.updateTimelineIcon();
 	};
 
 	this.updateTimelineIcon = function(){
@@ -15,15 +15,8 @@ var reservationDetailsView = function(domRef) {
 		var activeTimeline = $('#reservation-card').attr('data-current-timeliine');
 		$("#"+activeTimeline+" .reservations-tabs ul li").each(function(index){
 			if($(this).attr("data-confirmation-num") == currentReservation ){
-				var guestIconHtml = $(this).html();
-		        var guestIconHtmlMod = guestIconHtml.replace("arrival", guestStatusIcon);
-		        var guestIconHtmlMod = guestIconHtmlMod.replace("check-in", guestStatusIcon);
-		        var guestIconHtmlMod = guestIconHtmlMod.replace("inhouse", guestStatusIcon);
-		        var guestIconHtmlMod = guestIconHtmlMod.replace("departed", guestStatusIcon);
-		        var guestIconHtmlMod = guestIconHtmlMod.replace("check-out", guestStatusIcon);
-		        var guestIconHtmlMod = guestIconHtmlMod.replace("cancel", guestStatusIcon);
-		        var guestIconHtmlMod = guestIconHtmlMod.replace("no-show", guestStatusIcon);
-		        $(this).html(guestIconHtmlMod);
+				var guestIconHtml = $(this).find('span.guest-status');
+				guestIconHtml.attr('class','guest-status small-icon '+guestStatusIcon);
 			}
 		});
 	};
