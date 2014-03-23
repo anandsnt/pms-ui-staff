164e712fe6c650329ed51f5faf68dbb92fff160c
akhilastayntouch <akhila@stayntouch.com>
2014-03-23 13:52:43 +0530

    CICO-5387 modified the folder structure

--- javascripts/admin_main.js
+++ javascripts/admin_main.js
@@ -4,10 +4,7 @@
 //=require ../admin/lib/js/angular-ui-router.js
 //=require ../admin/lib/js/ng-iscroll.js
 //=require ../admin/lib/js/iscroll.js
-//=require ../admin/adApp.js
-//=require ../admin/adRouter.js
-//=require_tree ../admin/js/services
-//=require_tree ../admin/js/controllers
+//=require_tree ../admin/js
 
 
 
