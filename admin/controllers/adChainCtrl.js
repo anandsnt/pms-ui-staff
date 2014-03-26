6e3bdf95e8ad566207b221613264d59678b282af
Resheil <resheil@stayntouch.com>
2014-03-26 17:26:45 +0530

    ng-develop deleting unwanted code

--- admin/controllers/adChainCtrl.js
+++ admin/controllers/adChainCtrl.js
@@ -22,7 +22,7 @@ admin.controller('ADChainListCtrl',['$scope', '$rootScope','adChainsSrv', functi
 
     $scope.addNew = function(index, department)	{
 			$scope.showAddChainForm = true;
-			$scope.formTitle = 'Add';
+			
 	};
 
 
@@ -30,21 +30,24 @@ admin.controller('ADChainListCtrl',['$scope', '$rootScope','adChainsSrv', functi
 	$scope.addFormView = false;
 	$scope.editDepartments = function(index, department)	{
 			$scope.currentClickedElement = index;
-			$scope.formTitle ='Edit StayNTouch Demo Chain';
+			
 	};
 
 
 		//Function to get the template for edit url
-	$scope.getTemplateUrl = function(index){
-		if(index!="undefined"){
+	$scope.getTemplateUrl = function(index,name){
+		if(index!="undefined" && name != "undefined"){
+		
 			if($scope.currentClickedElement == index){
+
+				$scope.formTitle ='Edit StayNTouch Demo Chain';
 			 	// $scope.value = department.value;
 			 	// $scope.departmentName = department.name;
 			 	return "/assets/partials/chains/adChainForm.html";
 			 } 
 		}
 		if(index == ""	){
-		
+				$scope.formTitle = 'Add';
 			 	return "/assets/partials/chains/adChainForm.html";
 		}
 		 
