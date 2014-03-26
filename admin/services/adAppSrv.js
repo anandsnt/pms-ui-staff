dfcdab41ce271be7951e79985f85bab3245cf3b4
soumya <soumya@stayntouch.com>
2014-03-26 10:58:36 +0530

    Sample json changed to snt admin

--- admin/services/adAppSrv.js
+++ admin/services/adAppSrv.js
@@ -2,7 +2,7 @@ admin.service('ADAppSrv',['$http', '$q', 'ADBaseWebSrv', function($http, $q, ADB
 
 	this.fetch = function(){
 		var deferred = $q.defer();
-		var url = '/sample_json/ng_admin/adApp.json';	
+		var url = '/sample_json/ng_admin/adSntApp.json';	
 		//var url = '/sample_json/ng_admin/adSntApp.json';
 		ADBaseWebSrv.getJSON(url).then(function(data) {
 		    deferred.resolve(data);
