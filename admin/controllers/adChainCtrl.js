21dc9dd559538879b6e1b0919c88ca51e2e7f4c5
Resheil <resheil@stayntouch.com>
2014-03-26 17:14:45 +0530

    ng-develop adding inline edit for chains

--- admin/controllers/adChainCtrl.js
+++ admin/controllers/adChainCtrl.js
@@ -1,8 +1,9 @@
 
 admin.controller('ADChainListCtrl',['$scope', '$rootScope','adChainsSrv', function($scope, $rootScope,adChainsSrv){
 	
-	$scope.menuOpen = false;
+	// $scope.menuOpen = false;
 	$scope.chainsList = [];
+
 
 	adChainsSrv.fetch().then(function(data) {
 		$scope.chainsList = data;
@@ -14,9 +15,41 @@ admin.controller('ADChainListCtrl',['$scope', '$rootScope','adChainsSrv', functi
 
 	
     
- 	$scope.isMenuOpen = function(){
-        return $scope.menuOpen ? true : false;
-    };
+ 	// $scope.isMenuOpen = function(){
+  //       return $scope.menuOpen ? true : false;
+  //   };
+
+
+    $scope.addNew = function(index, department)	{
+			$scope.showAddChainForm = true;
+			$scope.formTitle = 'Add';
+	};
+
+
+    $scope.currentClickedElement = -1;
+	$scope.addFormView = false;
+	$scope.editDepartments = function(index, department)	{
+			$scope.currentClickedElement = index;
+			$scope.formTitle ='Edit StayNTouch Demo Chain';
+	};
+
+
+		//Function to get the template for edit url
+	$scope.getTemplateUrl = function(index){
+		if(index!="undefined"){
+			if($scope.currentClickedElement == index){
+			 	// $scope.value = department.value;
+			 	// $scope.departmentName = department.name;
+			 	return "/assets/partials/chains/adChainForm.html";
+			 } 
+		}
+		if(index == ""	){
+		
+			 	return "/assets/partials/chains/adChainForm.html";
+		}
+		 
+	};
+
 }]);
 
     
