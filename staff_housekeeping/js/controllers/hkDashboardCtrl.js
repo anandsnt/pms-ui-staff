7992c32fa1e06a0534495a95486a301dd54dec38
Vijay Dev <vijay2stayntouch.com>
2014-03-26 15:41:21 +0530

    CICO-5856: Scrolling is not working in Dashboard and Room status page in landscape view

--- staff_housekeeping/js/controllers/hkDashboardCtrl.js
+++ staff_housekeeping/js/controllers/hkDashboardCtrl.js
@@ -1,5 +1,12 @@
 hkRover.controller('HKDashboardCtrl',['$scope', 'hkDashboardSrv',  function($scope, hkDashboardSrv){
 
+	// setting up few default values
+	$scope.data = {};
+	$scope.data.occupied = 'NA';
+	$scope.data.vacant_clean_room_count = 'NA';
+	$scope.data.vacant_dirty_room_count = 'NA';
+	$scope.data.out_of_order_rooms_count = 'NA';
+	
 	hkDashboardSrv.fetch().then(function(messages) {
         $scope.data = messages;
 	});
@@ -11,10 +18,7 @@ hkRover.controller('HKRootCtrl',['$rootScope', '$scope', function($rootScope, $s
 	
 	$scope.$on("navc", function(event){
 		$scope.menuOpen = $scope.menuOpen ? false : true;
-	
 	})
-
-
 }]);
 
     
