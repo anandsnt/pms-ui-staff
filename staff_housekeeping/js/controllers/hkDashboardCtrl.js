7a11cb3ee684fc54076c9bb7070617738ec8ac9c
Vijay Dev <vijay2stayntouch.com>
2014-03-27 16:22:19 +0530

    CICO-5057: scrolling added for search screen, removed ng-iscroll

--- staff_housekeeping/js/controllers/hkDashboardCtrl.js
+++ staff_housekeeping/js/controllers/hkDashboardCtrl.js
@@ -1,18 +1,5 @@
 hkRover.controller('HKDashboardCtrl',['$scope', 'dashboardData',  function($scope, dashboardData){
-	//TODO: Vijay Please checkthis
-	/*
-	hkRover.controller('HKDashboardCtrl',['$scope', 'hkDashboardSrv',  function($scope, hkDashboardSrv){
 
-		// setting up few default values
-		$scope.data = {};
-		$scope.data.occupied = 'NA';
-		$scope.data.vacant_clean_room_count = 'NA';
-		$scope.data.vacant_dirty_room_count = 'NA';
-		$scope.data.out_of_order_rooms_count = 'NA';
-		
-		hkDashboardSrv.fetch().then(function(messages) {
-	        $scope.data = messages;
-	});*/
 	$scope.data = dashboardData;
 
 	// stop bounce effect only on the login container
