be501bdfaf7bc0f4ae129fdab17da7a3c5c9fb27
krishobh <krishobh@stayntouch.com>
2014-03-28 18:19:37 +0530

    CICO-5436 Creted htmls for add and edit screen

--- admin/controllers/adMappingDetailsCtrl.js
+++ admin/controllers/adMappingDetailsCtrl.js
@@ -1,22 +1,41 @@
-admin.controller('ADMappingDetailsCtrl', ['$scope', '$state', '$stateParams', 'ADMappingSrv',
+admin.controller('ADMappingDetailsCtrl', ['$scope', '$state', '$stateParams', 'ADMappingDetailsSrv',
 function($scope, $state, $stateParams, ADMappingDetailsSrv) {
 	BaseCtrl.call(this, $scope);
 	
 	
-	if($stateParams.action=="edit"){
-		$scope.mappingId = $stateParams.id;
-		console.log("$scope.mappingId"+$scope.mappingId );
-	}	
-	
-	if($stateParams.action=="add"){ 
+	if($stateParams.action == "add"){
+		$scope.isAdd = true;
 		$scope.hotelId = $stateParams.id;
-		console.log("$scope.hotelId"+$scope.hotelId);
+		console.log("$scope.hotelId---"+$scope.hotelId);
+		var fetchSuccess = function(data){
+			$scope.data = data;
+			$scope.$emit('hideLoader');
+		};
 		
+		var fetchFailed = function(){
+			console.log("fetchFailed");
+			$scope.$emit('hideLoader');
+		};
+		
+		$scope.invokeApi(ADMappingDetailsSrv.fetchAddData, {'id':$scope.hotelId }, fetchSuccess, fetchFailed);
 	}
-	
-	
+	if($stateParams.action == "edit"){
+		
+		$scope.mappingId = $stateParams.id;
+		console.log("$scope.mappingId----"+$scope.mappingId );
 
-	
+		
+		var fetchSuccess = function(data){
+			$scope.data = data;
+			$scope.$emit('hideLoader');
+		};
+		
+		var fetchFailed = function(){
+			console.log("fetchFailed");
+			$scope.$emit('hideLoader');
+		};
+		$scope.invokeApi(ADMappingDetailsSrv.fetchEditData, {'id':$scope.mappingId }, fetchSuccess, fetchFailed);
+	}
 
 
 }]);
--- admin/services/adMappingDetailsSrv.js
+++ admin/services/adMappingDetailsSrv.js
@@ -1,29 +1,53 @@
 admin.service('ADMappingDetailsSrv', ['$http', '$q', 'ADBaseWebSrv',
 function($http, $q, ADBaseWebSrv) {
-	
-	
-		this.fetchAddData = function(){
-		var deferred = $q.defer();
-		var url = '/admin/hotels/new.json';	
-		//var url = '/sample_json/ng_admin/adSntApp.json';
-		ADBaseWebSrv.getJSON(url).then(function(data) {
-		    deferred.resolve(data);
-		},function(data){
-		    deferred.reject(data);
-		});	
-		return deferred.promise;
-	};	
-	this.fetchEditData = function(data){
-		var deferred = $q.defer();
-		var url = '/admin/hotels/'+data.id+'/edit.json';	
-		//var url = '/sample_json/ng_admin/adSntApp.json';
-		ADBaseWebSrv.getJSON(url).then(function(data) {
-		    deferred.resolve(data);
-		},function(data){
-		    deferred.reject(data);
-		});	
-		return deferred.promise;
-	};	
-	
 
-}]); 
+	// To fetch data add external mapping
+	this.fetchAddData = function(data) {
+		var deferred = $q.defer();
+		var url = "/admin/external_mappings/" + data.id + "/new_mappings.json";
+		ADBaseWebSrv.getJSON(url).then(function(data) {
+			deferred.resolve(data);
+		}, function(data) {
+			deferred.reject(data);
+		});
+		return deferred.promise;
+	};
+
+	// To fetch data for Edit  external mapping
+	this.fetchEditData = function(data) {
+		var deferred = $q.defer();
+		var url = "/admin/external_mappings/" + data.id + "/edit_mapping.json";
+console.log(url)
+		ADBaseWebSrv.getJSON(url).then(function(data) {
+			deferred.resolve(data);
+		}, function(data) {
+			deferred.reject(data);
+		});
+		return deferred.promise;
+	};
+	// To POST data for Add  new external mapping
+	this.addNewMapping = function(data) {
+		var deferred = $q.defer();
+		var url = '/admin/external_mappings/save_mapping';
+
+		ADBaseWebSrv.postJSON(url, data).then(function(data) {
+			deferred.resolve(data);
+		}, function(data) {
+			deferred.reject(data);
+		});
+		return deferred.promise;
+	};
+	// To UPDATE data for edit external mapping
+	this.updateMapping = function(data) {
+		var deferred = $q.defer();
+		var url = '/admin/external_mappings/save_mapping';
+
+		ADBaseWebSrv.putJSON(url, data).then(function(data) {
+			deferred.resolve(data);
+		}, function(data) {
+			deferred.reject(data);
+		});
+		return deferred.promise;
+	};
+
+}]);
