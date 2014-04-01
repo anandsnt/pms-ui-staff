334cad10607510d1481739753212d930f80ec5fd
soumya <soumya@stayntouch.com>
2014-04-01 19:53:53 +0530

    CICO-5444 Add/edit and delete departments

--- admin/controllers/adAppCtrl.js
+++ admin/controllers/adAppCtrl.js
@@ -8,7 +8,7 @@ admin.controller('ADAppCtrl',['$state', '$scope', '$rootScope','ADAppSrv', funct
 	//when there is an occured while trying to access any menu details, we need to show that errors
 	$scope.errorMessage = '';
 
-	if($rootScope.admin_role == "hotel-admin" ){
+	if($rootScope.adminRole == "hotel-admin" ){
 		$scope.isHotelAdmin =  true;
 	}	
 	else{
@@ -19,7 +19,7 @@ admin.controller('ADAppCtrl',['$state', '$scope', '$rootScope','ADAppSrv', funct
 		$scope.currentIndex = 0;
 		$scope.data = data;
 		$scope.selectedMenu = $scope.data.menus[0];		
-	}
+	};
 
 	$scope.invokeApi(ADAppSrv.fetch, {}, $scope.successCallbackOfMenuLoading);
 
--- admin/services/adDepartmentSrv.js
+++ admin/services/adDepartmentSrv.js
@@ -1,6 +1,45 @@
 admin.service('ADDepartmentSrv',['$http', '$q', 'ADBaseWebSrv', function($http, $q, ADBaseWebSrv){
+   /**
+    * To fetch the list of users
+    * @return {object} users list json
+    */
+	this.fetch = function(){
+		
+		var deferred = $q.defer();
+		var url = '/admin/departments.json';
 
-	this.postDepartmentDetails = function(data){
+		ADBaseWebSrv.getJSON(url).then(function(data) {
+		    deferred.resolve(data);
+		},function(data){
+		    deferred.reject(data);
+		});	
+		return deferred.promise;
+	};
+	this.saveDepartment = function(data){
+		var deferred = $q.defer();
+		var url = '/admin/departments';	
+
+		ADBaseWebSrv.postJSON(url, data).then(function(data) {
+		    deferred.resolve(data);
+		},function(data){
+		    deferred.reject(data);
+		});	
+		return deferred.promise;
+	};
+	this.getDepartmentDetails = function(data){
+		var deferred = $q.defer();
+		var id = data.id;
+		var url = '/admin/departments/'+id+'/edit.json';	
+
+		ADBaseWebSrv.getJSON(url).then(function(data) {
+		    deferred.resolve(data);
+		},function(data){
+		    deferred.reject(data);
+		});	
+		return deferred.promise;
+	};
+	
+	this.updateDepartment = function(data){
 
 		var deferred = $q.defer();
 		var url = '/admin/departments/'+data.value;	
@@ -11,6 +50,18 @@ admin.service('ADDepartmentSrv',['$http', '$q', 'ADBaseWebSrv', function($http, 
 		    deferred.reject(data);
 		});	
 		return deferred.promise;
+	};
+	this.deleteDepartment = function(id){
+		var deferred = $q.defer();
+		var url = '/admin/departments/'+id;	
+
+		ADBaseWebSrv.deleteJSON(url).then(function(data) {
+		    deferred.resolve(data);
+		},function(data){
+		    deferred.reject(data);
+		});	
+		return deferred.promise;
+		
 	};
 
 
