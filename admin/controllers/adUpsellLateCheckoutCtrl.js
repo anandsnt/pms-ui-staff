165c959a962318cdb422eadfd3c0a09f26ec04e0
Resheil <resheil@stayntouch.com>
2014-04-01 16:47:34 +0530

    CICO-5450 adding api and retrieving

--- admin/controllers/adUpsellLateCheckoutCtrl.js
+++ admin/controllers/adUpsellLateCheckoutCtrl.js
@@ -19,5 +19,8 @@ admin.controller('ADUpsellLateCheckoutCtrl',['$scope','adUpsellLatecheckoutServi
 
 	$scope.fetchUpsellDetails();
 
+	$scope.hours = ["01","02","03","04","05","06","07","08","09","10","11","12"];
+	$scope.minutes = ["00","15","30","45"];
+
 
 }]);
