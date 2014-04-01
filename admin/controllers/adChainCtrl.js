b62ae711a02dc25cf0da0c85d9bf505d9842a717
soumya <soumya@stayntouch.com>
2014-04-01 11:55:24 +0530

    CICO-5434 Add new hotel chain - added comments

--- admin/controllers/adChainCtrl.js
+++ admin/controllers/adChainCtrl.js
@@ -1,152 +1,88 @@
-
 admin.controller('ADChainListCtrl',['$scope', '$rootScope','adChainsSrv', function($scope, $rootScope,adChainsSrv){
-	
 
 	BaseCtrl.call(this, $scope);
-
 	$scope.chainsList = [];
 	$scope.editData   = {};
 
 	$scope.isAddmode = false;
 	$scope.isEditmode = false;
-
-	// fetch chain list
-
-
+   /*
+    * To fetch hotel chains list
+    */
 	$scope.fetchHotelChains = function(){
-
-
-
-
 		var fetchChainsSuccessCallback = function(data) {
 			$scope.$emit('hideLoader');
 			$scope.chainsList = data.chain_list;
 		};
-		var fetchChainsFailCallback = function(errorMessage){
-			$scope.$emit('hideLoader');
-			console.log("error controller");
-			$scope.errorMessage = errorMessage;
-			
-			
-		};
-
-		$scope.invokeApi(adChainsSrv.fetch, {},fetchChainsSuccessCallback, fetchChainsFailCallback);
-
+		$scope.invokeApi(adChainsSrv.fetch, {},fetchChainsSuccessCallback);
 	};
-
-	
 	$scope.fetchHotelChains();
-
-
 	$scope.currentClickedElement = -1;
 	$scope.addFormView = false;
-
-	// inline edit
-
+  /*
+   * To render edit screen
+   * @param {int} index index of selected chain
+   * @paran {string} id - chain id
+   */
 	$scope.editSelected = function(index,id)	{
-
-
-		
 		$scope.isAddmode = false;
 		$scope.errorMessage ="";
-
-
 		$scope.currentClickedElement = index;
 		$scope.editId = id;
-
-
-		var editID = { 'editID' : id }
-
-
+		var editID = { 'editID' : id };
 		var editChainSuccessCallback = function(data) {
 			$scope.$emit('hideLoader');
 			$scope.editData   = data;
 			$scope.formTitle = 'Edit'+' '+$scope.editData.name;
-
 			if($scope.editData.lov.length === 0)
 				$scope.editData.lov.push({'value':'','name':''});
 			$scope.isEditmode = true;
-			console.log(data)
-		};
-		var editChainsFailCallback = function(errorMessage){
-			$scope.$emit('hideLoader');
-			console.log("error controller");
-			$scope.errorMessage = errorMessage;
-			
-		};
-
-		$scope.invokeApi(adChainsSrv.edit,editID,editChainSuccessCallback,editChainsFailCallback);
-
-
-
+		};		
+		$scope.invokeApi(adChainsSrv.edit,editID,editChainSuccessCallback);
 	};
-
-	//add button clicked
-
+  /*
+   * To render add screen
+   */
 	$scope.addNew = function(){
-
-
 		$scope.editData   = {};
 		$scope.errorMessage ="";
-
-
 		$scope.editData.lov  = [{'value':'','name':''}];
-
 		$scope.formTitle = 'Add';	
 		$scope.isAddmode = true;
 		$scope.isEditmode = false;
 	};
-
- 	// template for add/edit
-
+   /*
+    * To fetch the template for chains details add/edit screens
+    */
  	$scope.getTemplateUrl = function(){
-
-
-
  		return "/assets/partials/chains/adChainForm.html";
-
  	};
-
- 	// add new chain
-
+   /*
+    * To save new chain
+    */
  	$scope.addNewChain = function (){
-
  		var lovNames = [];
  		angular.forEach($scope.editData.lov,function(item, index) {
  			if (item.name == "") {
  				$scope.editData.lov.splice(index, 1);
  			}
  			else{
- 				lovNames.push(item.name)
+ 				lovNames.push(item.name);
  			}
  		});
  		$scope.editData.lov = lovNames;
- 		console.log(lovNames)
-
  		var addChainSuccessCallback = function(data) {
  			$scope.$emit('hideLoader');
- 			console.log(data)
  			$scope.fetchHotelChains();
  			$scope.isAddmode = false;
-
  		};
- 		var addChainFailCallback = function(errorMessage){
- 			$scope.$emit('hideLoader');
- 			console.log("error controller");
- 			$scope.errorMessage = errorMessage;
-				
- 		};
-
- 		$scope.invokeApi(adChainsSrv.post,$scope.editData, addChainSuccessCallback,addChainFailCallback);
-
-
+ 		$scope.invokeApi(adChainsSrv.post,$scope.editData, addChainSuccessCallback);
  	};
-
- 	// update existing chain
-
+   /*
+    * To update chain details
+    * @param {string} id - chain id
+    */
  	$scope.updateChain = function(id){
-
-
  		angular.forEach($scope.editData.lov,function(item, index) {
  			if (item.name == "") {
  				$scope.editData.lov.splice(index, 1);
@@ -155,88 +91,55 @@ admin.controller('ADChainListCtrl',['$scope', '$rootScope','adChainsSrv', functi
  				 delete item.value;
  			}
  		});
-
- 		var updateData = {'id' : id ,'updateData' :$scope.editData }
-
-
+ 		var updateData = {'id' : id ,'updateData' :$scope.editData };
  		var updateChainSuccessCallback = function(data) {
  			$scope.$emit('hideLoader');
  			$scope.fetchHotelChains();
  			$scope.isEditmode = false;
  		};
- 		var updateChainFailCallback = function(errorMessage){
- 			$scope.$emit('hideLoader');
- 			console.log("error controller");
- 			$scope.errorMessage = errorMessage;
-			
- 		};
-
-
- 		$scope.invokeApi(adChainsSrv.update,updateData,updateChainSuccessCallback,updateChainFailCallback);
-
- 		
-
-
+ 		$scope.invokeApi(adChainsSrv.update,updateData,updateChainSuccessCallback);
  	};
-
-
-
-	// form actions
-
+   /*
+    * To handle cancel click event
+    */
 	$scope.cancelClicked = function (){
-
 		if($scope.isAddmode)
 			$scope.isAddmode = false;
 		else if($scope.isEditmode)
 			$scope.isEditmode = false;
-
 	};
-
-
+   /*
+    * To handle save button click - Add/Update action
+    */
 	$scope.saveClicked = function(){
-
-
-		if($scope.isAddmode)
+		if($scope.isAddmode){
 			$scope.addNewChain();
-		else
+		}			
+		else{
 			$scope.updateChain($scope.editId);
+		}
 	};
-
-
-
-
-// on focus event create new option if focussed field is last
-
+   /*
+    * To handle focus event on lov levels
+    */
 	$scope.onFocus = function(index){
-
-
-
 		if((index === $scope.editData.lov.length-1) || ($scope.editData.lov.length==1)){
-
 			$scope.newOptionAvailable = true;
-
 			// exclude first two fields
-
 			if($scope.editData.lov.length > 2){
-
 				angular.forEach($scope.editData.lov,function(item, index) {
 					if (item.name == "" && index < $scope.editData.lov.length-1 ) {
 						$scope.newOptionAvailable = false;
-
 					}
 				});
-
 			}
-
 			if($scope.newOptionAvailable)
 				$scope.editData.lov.push({'value':'','name':''});
-
-
 		}
-		
 	};
-// if content is deleted fully remove the input field
-
+   /*
+    * To handle text change on lov levels
+    */
 	$scope.textChanged = function(index){
 
 		if($scope.editData.lov.length>1){
@@ -244,32 +147,19 @@ admin.controller('ADChainListCtrl',['$scope', '$rootScope','adChainsSrv', functi
 				$scope.editData.lov.splice(index, 1);
 		}
 	};
-
-// on blur check for blank fields and delete
-
+   /*
+    * To handle blur event on lov levels
+    */
 	$scope.onBlur = function(index){
-
-
-
 		if($scope.editData.lov.length>1){
-
-
 			if($scope.editData.lov[index].name == "")
 				$scope.editData.lov.splice(index, 1);
-
 			angular.forEach($scope.editData.lov,function(item, i) {
 				if (item.name == "" && i != $scope.editData.lov.length-1) {
 					$scope.editData.lov.splice(i, 1);
 				}
 			});
-
-
-
 		}
 	};
-
-
-
-
 }]);
 
