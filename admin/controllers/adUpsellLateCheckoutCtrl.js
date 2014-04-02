230cec2e6e10056486c4853bcd033347969985aa
Resheil <resheil@stayntouch.com>
2014-04-02 09:58:23 +0530

    CICO-5450 handling text change to null for charges

--- admin/controllers/adUpsellLateCheckoutCtrl.js
+++ admin/controllers/adUpsellLateCheckoutCtrl.js
@@ -9,18 +9,19 @@ admin.controller('ADUpsellLateCheckoutCtrl',['$scope','$rootScope','$state','adU
     */ 
     $scope.fetchUpsellDetails = function(){
 
-		var fetchUpsellDetailsSuccessCallback = function(data) {
-			$scope.$emit('hideLoader');
-			$scope.upsellData = data;
+    	var fetchUpsellDetailsSuccessCallback = function(data) {
+    		$scope.$emit('hideLoader');
+    		$scope.upsellData = data;
+    		$scope.startWatching();
 
-		};
-		$scope.invokeApi(adUpsellLatecheckoutService.fetch, {},fetchUpsellDetailsSuccessCallback);
-	};
+    	};
+    	$scope.invokeApi(adUpsellLatecheckoutService.fetch, {},fetchUpsellDetailsSuccessCallback);
+    };
 
-	$scope.fetchUpsellDetails();
+    $scope.fetchUpsellDetails();
 
-	$scope.hours = ["01","02","03","04","05","06","07","08","09","10","11","12"];
-	$scope.minutes = ["00","15","30","45"];
+    $scope.hours = ["01","02","03","04","05","06","07","08","09","10","11","12"];
+    $scope.minutes = ["00","15","30","45"];
 
 
 	/**
@@ -28,60 +29,100 @@ admin.controller('ADUpsellLateCheckoutCtrl',['$scope','$rootScope','$state','adU
     *
     */ 
 
-	$scope.switchClicked = function(){
+    $scope.switchClicked = function(){
 
-	$scope.upsellData.is_late_checkout_set =  ($scope.upsellData.is_late_checkout_set === 'true')?'false':'true';
-		
-	}
+    	$scope.upsellData.is_late_checkout_set =  ($scope.upsellData.is_late_checkout_set === 'true')?'false':'true';
+    	
+    }
 
 	/**
     * To handle checkbox actions
     *
     */ 
 
-	$scope.checkBoxClicked = function(){
-		
-	$scope.upsellData.is_exclude_guests = ($scope.upsellData.is_exclude_guests === 'true')?'false':'true';
+    $scope.checkBoxClicked = function(){
+    	
+    	$scope.upsellData.is_exclude_guests = ($scope.upsellData.is_exclude_guests === 'true')?'false':'true';
 
-	}
+    }
+
+    $scope.startWatching = function(){
+
+	/**
+    * To watch charges
+    *
+    */ 
+
+    $scope.$watch('upsellData.extended_checkout_charge_0.charge', function(newValue, oldValue){
+
+
+
+    	if($scope.upsellData.extended_checkout_charge_0.charge.length ===0){
+
+    		$scope.upsellData.extended_checkout_charge_1.charge = "";
+    		$scope.upsellData.extended_checkout_charge_1.time = "";
+    		$scope.upsellData.extended_checkout_charge_2.charge = "";
+    		$scope.upsellData.extended_checkout_charge_2.time = "";
+
+    	}
+    	
+
+    }, true);
+
+  	/**
+    * To watch charges
+    *
+    */ 
+
+    $scope.$watch('upsellData.extended_checkout_charge_1.charge', function(newValue, oldValue){
+    	
+    	if($scope.upsellData.extended_checkout_charge_1.charge.length ===0){
+    		$scope.upsellData.extended_checkout_charge_2.charge = "";
+    		$scope.upsellData.extended_checkout_charge_2.time = "";
+    	}
+    	
+
+    }, true);
+}
+
 
 	/**
     * To handle cancel button action
     *
     */ 
 
-	$scope.cancelClick = function(){
+    $scope.cancelClick = function(){
 
-		$state.go( 'admin.dashboard', {menu:2});
-		
-	}
+    	$state.go( 'admin.dashboard', {menu:2});
+    	
+    }
 
 	/**
     * To handle save button action
     *
     */ 
 
-	$scope.saveClick = function(){
+    $scope.saveClick = function(){
 
-		var updateData = 
-		{
-		'is_late_checkout_set' :$scope.upsellData.is_late_checkout_set,
-		'allowed_late_checkout':$scope.upsellData.allowed_late_checkout,
-		'is_exclude_guests':$scope.upsellData.is_exclude_guests,
-		'sent_alert':$scope.upsellData.alert_hour+':'+$scope.upsellData.alert_minute,
-		'extended_checkout_charge_0':$scope.upsellData.extended_checkout_charge_0,
-		'extended_checkout_charge_1':$scope.upsellData.extended_checkout_charge_1,
-		'extended_checkout_charge_2':$scope.upsellData.extended_checkout_charge_2,
-		'charge_code':$scope.upsellData.selected_charge_code
+    	var updateData = 
+    	{
+    		'is_late_checkout_set' :$scope.upsellData.is_late_checkout_set,
+    		'allowed_late_checkout':$scope.upsellData.allowed_late_checkout,
+    		'is_exclude_guests':$scope.upsellData.is_exclude_guests,
+    		'sent_alert':$scope.upsellData.alert_hour+':'+$scope.upsellData.alert_minute,
+    		'extended_checkout_charge_0':$scope.upsellData.extended_checkout_charge_0,
+    		'extended_checkout_charge_1':$scope.upsellData.extended_checkout_charge_1,
+    		'extended_checkout_charge_2':$scope.upsellData.extended_checkout_charge_2,
+    		'charge_code':$scope.upsellData.selected_charge_code
 
-		 };
- 		var updateChainSuccessCallback = function(data) {
- 			$scope.$emit('hideLoader');
- 			$state.go( 'admin.dashboard', {menu:2});
- 		};
- 		$scope.invokeApi(adUpsellLatecheckoutService.update,updateData,updateChainSuccessCallback);
-	
-	}
-	
+    	};
+    	var updateChainSuccessCallback = function(data) {
+    		$scope.$emit('hideLoader');
+    		$state.go( 'admin.dashboard', {menu:2});
+    	};
+    	$scope.invokeApi(adUpsellLatecheckoutService.update,updateData,updateChainSuccessCallback);
+    	
+    }
+    
 
 }]);
