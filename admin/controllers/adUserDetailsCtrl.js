dce05aa6b3dcf4977cd047ff5a2084003a72cc24
soumyacyriac <soumya@stayntouch.com>
2014-04-02 20:31:17 +0530

    CICO-5446 Drag & Drop of role assignment added, need to add in web service calling

--- admin/controllers/adUserDetailsCtrl.js
+++ admin/controllers/adUserDetailsCtrl.js
@@ -3,12 +3,70 @@ admin.controller('ADUserDetailsCtrl',['$scope', '$state','$stateParams', 'ADUser
 	BaseCtrl.call(this, $scope);
 	$scope.mod = "";
 	$scope.image = "";
+
+
+
+	/** functions & variables related to drag & drop **/
+	$scope.selectedUnassignedRole = -1;
+	$scope.selectedAssignedRole = -1;
+
+	$scope.clickedOnAssignedRole = function(index){
+		$scope.selectedAssignedRole = index;
+	}
+	$scope.clickedOnUnassignedRole = function(index){
+		$scope.selectedUnassignedRole = index;
+	}	
+
+	$scope.leftToRight = function(){
+		var index = $scope.selectedAssignedRole;
+		if(index == -1){
+			return;
+		}
+		var newElement = $scope.assignedRoles[index];
+		$scope.unAssignedRoles.push(newElement);
+		$scope.assignedRoles.splice(index, 1);
+		$scope.selectedAssignedRole = -1;
+	}
+
+	$scope.rightToleft = function(){
+		var index = $scope.selectedUnassignedRole;
+		if(index == -1){
+			return;
+		}		
+		var newElement = $scope.unAssignedRoles[index];
+		$scope.assignedRoles.push(newElement);
+		$scope.unAssignedRoles.splice(index, 1);
+		$scope.selectedUnassignedRole = -1;
+	};
+
+	$scope.$on("ANGULAR_DRAG_START", function(sendchaneel){
+		console.log(sendchaneel);
+	});
+	
+	/**
+	 * To handle drop success event
+	 *
+	 */
+	$scope.dropSuccessHandler = function($event, index, array) {
+		array.splice(index, 1);
+	};
+	/**
+	 * To handle on drop event
+	 *
+	 */
+
+	$scope.onDrop = function($event, $data, array) {		
+		array.push($data);
+	};
+
+
+
+
 	/**
     *   save user details
     */
 	$scope.saveUserDetails = function(){
 		var params = $scope.data;
-		
 		var unwantedKeys = [];
 		if($scope.image.indexOf("data:")!= -1){
 			unwantedKeys = ["departments", "roles"];
@@ -42,6 +100,7 @@ admin.controller('ADUserDetailsCtrl',['$scope', '$state','$stateParams', 'ADUser
 			$scope.data = data;
 			$scope.image = data.user_photo;
 			$scope.data.confirm_email = $scope.data.email;
+			
 		};
 		$scope.invokeApi(ADUserSrv.getUserDetails, {'id':id} , successCallbackRender);
 	};
@@ -52,9 +111,15 @@ admin.controller('ADUserDetailsCtrl',['$scope', '$state','$stateParams', 'ADUser
 	 	var successCallbackRender = function(data){
 			$scope.$emit('hideLoader');
 			$scope.data = data;
+			$scope.unAssignedRoles = $scope.data.roles;
+			$scope.assignedRoles = [];
 		};	
 	 	$scope.invokeApi(ADUserSrv.getAddNewDetails, '' , successCallbackRender);	
 	};
+
+
+
+
     /**
     * To set mod of operation - add/edit
     */
