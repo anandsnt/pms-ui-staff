25497727bedcbc4d479aab8483f551e8d5cb0d4f
akhilastayntouch <akhila@stayntouch.com>
2014-04-04 11:13:16 +0530

    CICO-3931 Created html for list, add and edit view.

--- admin/controllers/adRateTypeCtrl.js
+++ admin/controllers/adRateTypeCtrl.js
@@ -4,12 +4,11 @@ function($scope, $rootScope, ADRateTypeSrv) {
 	BaseCtrl.call(this, $scope);
 	$scope.rateTypeData = {};
 	$scope.isAddMode = false;
-	$scope.currentClickedElement = -1;
-	var fetchSuccess = function() {
+
+	var fetchSuccess = function(data) {
 		$scope.data = data;
-		$scope.systemDefined = (data.rate_types.system_defined == true) ? "(System Defined)": "";
 		console.log("fetchSuccess");
-		console.log($scope.data);
+		//console.log($scope.data.rate_types);
 		$scope.$emit('hideLoader');
 	};
 
@@ -23,87 +22,108 @@ function($scope, $rootScope, ADRateTypeSrv) {
 		$scope.currentClickedElement = "new";
 		$scope.isAddMode = true;
 	};
+
+	/*
+	 * To render edit rate type screen
+	 * @param {index} index of selected rate type
+	 * @param {id} id of the rate type
+	 */
+	$scope.editRateTypes = function(index, id) {
 	
-	   /*
-    * To render edit department screen
-    * @param {index} index of selected department
-    * @param {id} id of the department
-    */	
-	$scope.editRateTypes = function(index, id)	{
-		$scope.rateTypeData={};
+		$scope.rateTypeData = {};
 		$scope.currentClickedElement = index;
 		$scope.isAddMode = false;
-	 	var successCallbackRender = function(data){	
-	 		$scope.rateTypeData = data;
-	 		$scope.$emit('hideLoader');
-	 	};
-	 	var data = {"id":id };
-	 	//$scope.invokeApi(ADRateTypeSrv.getRateTypesDetails, data , successCallbackRender);    
-	};
-	
-	
-	
-	/*
-    * To get the template of edit screen
-    * @param {int} index of the selected rate type
-    * @param {string} id of the rate type
-    */
-	$scope.getTemplateUrl = function(index, id){
-		if(typeof index === "undefined" || typeof id === "undefined") return "";
-		if($scope.currentClickedElement == index){ 
-			 	return "/assets/partials/rate_types/adRateTypeEdit.html";
-		}
-	};
-		/**
-    *   A post method to update ReservationImport for a hotel
-    *   @param {String} index value for the hotel list item.
-    */
-   
-	$scope.toggleClicked = function(index){
-		
-		// checkedStatus will be true, if it checked
-      	// show confirm if it is going turn on stage
-      	if($scope.data.rate_types[index].activated == 'false'){
-          	console.log("false");
-      	}
-      	var isActivated = $scope.data.rate_types[index].activated  == 'true' ? false : true;
-      	var data = {'value' :  $scope.data.rate_types[index].id,  'status': isActivated };
-      	
-      	var postSuccess = function(){
-      		$scope.data.rate_types[index].activated = ($scope.data.rate_types[index].activated == 'true') ? 'false' : 'true';
+		var successCallbackRender = function(data) {
+			$scope.rateTypeData = data;
 			$scope.$emit('hideLoader');
 		};
 		
-		
+		var data = {
+			"id" : id
+		};
+		$scope.invokeApi(ADRateTypeSrv.getRateTypesDetails, data, successCallbackRender);
+	};
+
+	/*
+	 * To get the template of edit screen
+	 * @param {int} index of the selected rate type
+	 * @param {string} id of the rate type
+	 */
+	$scope.getTemplateUrl = function(index, id) {
+		if ( typeof index === "undefined" || typeof id === "undefined")
+			return "";
+		if ($scope.currentClickedElement == index) {
+			return "/assets/partials/rate_types/adRateTypeEdit.html";
+		}
+	};
+	/**
+	 *   A post method to update ReservationImport for a hotel
+	 *   @param {String} index value for the hotel list item.
+	 */
+
+	$scope.toggleClicked = function(index) {
+		alert($scope.data.rate_types[index].activated)
+		// checkedStatus will be true, if it checked
+		// show confirm if it is going turn on stage
+		if ($scope.data.rate_types[index].activated == 'false') {
+			console.log("false");
+		}
+		var isActivated = $scope.data.rate_types[index].activated == 'true' ? false : true;
+		var data = {
+			'id' : $scope.data.rate_types[index].id,
+			'status' : isActivated
+		};
+
+		var postSuccess = function() {
+			$scope.data.rate_types[index].activated = ($scope.data.rate_types[index].activated == 'true') ? 'false' : 'true';
+			$scope.$emit('hideLoader');
+		};
+
 		$scope.invokeApi(ADRateTypeSrv.postRateTypeToggle, data, postSuccess);
 	};
-  /*
-   * To save/update rate typet details
-   */
-   $scope.saveRateType = function(){
-    	var successCallbackSave = function(data){
-    		$scope.$emit('hideLoader');
-			if($scope.isAddMode){
+	/*
+	 * To save/update rate type details
+	 */
+	$scope.saveRateType = function() {
+		var successCallbackSave = function(data) {
+			$scope.$emit('hideLoader');
+			if ($scope.isAddMode) {
 				// To add new data to scope
-    			$scope.data.rate_types.push(data);
-	    	} else {
-	    		//To update data with new value
-	    		$scope.data.rate_types[parseInt($scope.currentClickedElement)].name = $scope.rateTypeData.name;
-	    		$scope.data.rate_types[parseInt($scope.currentClickedElement)].description = $scope.rateTypeData.description;
+				$scope.data.rate_types.push(data);
+			} else {
+				
+				//To update data with new value
+				$scope.data.rate_types[parseInt($scope.currentClickedElement)].name = $scope.rateTypeData.name;
+				$scope.data.rate_types[parseInt($scope.currentClickedElement)].description = $scope.rateTypeData.description;
 
-	    	}
-    		$scope.currentClickedElement = -1;
-    	};
-    	if($scope.isAddMode){
-    		$scope.invokeApi(ADRateTypeSrv.saveRateType, $scope.rateTypeData , successCallbackSave);
-    	} else {
-    		$scope.invokeApi(ADRateTypeSrv.updateRateType, $scope.rateTypeData , successCallbackSave);
-    	}
-    };
-   /*
-    * To handle click event
-    */	
-	$scope.clickCancel = function(){
+			}
+			$scope.currentClickedElement = -1;
+		};
+		if ($scope.isAddMode) {
+			console.log($scope.rateTypeData)
+			$scope.invokeApi(ADRateTypeSrv.saveRateType, $scope.rateTypeData, successCallbackSave);
+		} else {
+			console.log($scope.rateTypeData)
+			$scope.invokeApi(ADRateTypeSrv.updateRateType, $scope.rateTypeData, successCallbackSave);
+		}
+	};
+	/*
+	 * To handle click event
+	 */
+	$scope.clickCancel = function() {
 		$scope.currentClickedElement = -1;
-	};	
+	};
+
+	/*
+	 * To delete rate types
+	 * @param {int} index of the selected rate types
+	 * @param {string} id of the selected rate types
+	 */
+	$scope.deleteRateType = function(index, id) {
+		var successCallbackDelete = function(data) {
+			$scope.$emit('hideLoader');
+			$scope.data.rate_types.splice(index, 1);
+		};
+		$scope.invokeApi(ADRateTypeSrv.deleteRateType, id, successCallbackDelete);
+	};
 }]);
--- admin/services/adRateTypeSrv.js
+++ admin/services/adRateTypeSrv.js
@@ -3,9 +3,78 @@ function($http, $q, ADBaseWebSrv) {
 
 	this.fetch = function() {
 		var deferred = $q.defer();
-		
-		//var url = "/admin/hotel_rate_types.json";
-		var url = "/api/admin/rate_types";
+
+		var url = " /sample_json/ng_admin/rate_types.json";
+		//var url = "/api/admin/rate_types.json";
+		ADBaseWebSrv.getJSON(url).then(function(data) {
+	
+			deferred.resolve(data);
+		}, function(data) {
+			deferred.reject(data);
+		});
+		return deferred.promise;
+	};
+
+	/**
+	 *   A post method to update Rate type for a hotel
+	 *   @param {Object} data for the Rate type list item details.
+	 */
+	this.postRateTypeToggle = function(data) {
+		var deferred = $q.defer();
+		var url = "/api/admin/rate_types/" + data.id + "/activate";
+
+		ADBaseWebSrv.postJSON(url, data).then(function(data) {
+			deferred.resolve(data);
+		}, function(data) {
+			deferred.reject(data);
+		});
+		return deferred.promise;
+	};
+	/*
+	 * To save new RateType
+	 * @param {array} data of the new RateType
+	 * @return {object} status and new id of new RateType
+	 */
+	this.saveRateType = function(data) {
+		var deferred = $q.defer();
+		var url = '/api/admin/rate_types';
+
+		ADBaseWebSrv.postJSON(url, data).then(function(data) {
+			deferred.resolve(data);
+		}, function(data) {
+			deferred.reject(data);
+		});
+		return deferred.promise;
+	};
+
+	/*
+	 * To update RateType data
+	 * @param {array} data of the modified RateType
+	 * @return {object} status of updated RateType
+	 */
+	this.updateRateType = function(data) {
+
+		var deferred = $q.defer();
+		var url = '/api/admin/rate_types/' + data.id;
+
+		ADBaseWebSrv.putJSON(url, data).then(function(data) {
+			deferred.resolve(data);
+		}, function(data) {
+			deferred.reject(data);
+		});
+		return deferred.promise;
+	};
+	/*
+	 * To get the details of the selected rate type
+	 * @param {array} selected rate type id
+	 * @return {object} selected rate type details
+	 */
+	this.getRateTypesDetails = function(data) {
+		var deferred = $q.defer();
+		var id = data.id;
+		var url = "/sample_json/ng_admin/edit_rate_types.json"
+		//var url = '/api/admin/rate_types/' + id+".json";
+
 		ADBaseWebSrv.getJSON(url).then(function(data) {
 			deferred.resolve(data);
 		}, function(data) {
@@ -13,56 +82,22 @@ function($http, $q, ADBaseWebSrv) {
 		});
 		return deferred.promise;
 	};
-	
-		/**
-    *   A post method to update Rate type for a hotel
-    *   @param {Object} data for the Rate type list item details.
-    */
-	this.postRateTypeToggle = function(data){
+
+	/*
+	 * To delete the seleceted rate type
+	 * @param {int} id of the selected rate type
+	 * @return {object} status of delete
+	 */
+	this.deleteRateType = function(id) {
 		var deferred = $q.defer();
-		var url = "/api/admin/rate_types/"+data.id+"/activate";
-		
-		ADBaseWebSrv.postJSON(url, data).then(function(data) {
-		    deferred.resolve(data);
-		},function(data){
-		    deferred.reject(data);
-		});	
+
+		var url = '/api/admin/rate_types/' + id;
+
+		ADBaseWebSrv.deleteJSON(url).then(function(data) {
+			deferred.resolve(data);
+		}, function(data) {
+			deferred.reject(data);
+		});
 		return deferred.promise;
 	};
-	   /*
-    * To save new RateType
-    * @param {array} data of the new RateType
-    * @return {object} status and new id of new RateType
-    */
-	this.saveRateType = function(data){
-		var deferred = $q.defer();
-		var url = '/api/admin/rate_types';	
-
-		ADBaseWebSrv.postJSON(url, data).then(function(data) {
-		    deferred.resolve(data);
-		},function(data){
-		    deferred.reject(data);
-		});	
-		return deferred.promise;
-	};
-	
-	
-	   /*
-    * To update RateType data
-    * @param {array} data of the modified RateType
-    * @return {object} status of updated RateType
-    */
-	this.updateRateType = function(data){
-
-		var deferred = $q.defer();
-		var url = '/api/admin/rate_types/:id'+data.id;	
-
-		ADBaseWebSrv.putJSON(url, data).then(function(data) {
-		    deferred.resolve(data);
-		},function(data){
-		    deferred.reject(data);
-		});	
-		return deferred.promise;
-	};
-
 }]);
