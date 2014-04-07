0ccba9d62d23c5dbeda0c64b5fcf8281b2832ef1
Vijay Dev <vijay2stayntouch.com>
2014-04-07 15:13:08 +0530

    CICO-5780: Cannot click on certain parts of credit cards

--- javascripts/app/guest_payment.js
+++ javascripts/app/guest_payment.js
@@ -2,31 +2,31 @@ var GuestPaymentView = function(domRef){
   BaseView.call(this);
   var that = this;
   this.myDom = domRef;   
- 	
+  
   this.pageinit = function(){
-    that.myDom.find('#payment_tab').on('click', that.clickedOnPaymentTab);	
+    that.myDom.find('#payment_tab').on('click', that.clickedOnPaymentTab);  
   };  
   this.clickedOnPaymentTab= function(e){
-  	var clickedElement = $(e.target);
-  	if(clickedElement.hasClass("active-item")){	
-  		that.renderSetAsPrimary(clickedElement);
-   	}
-  	else if(clickedElement.parent().hasClass("active-item")){
-   		that.renderSetAsPrimary(clickedElement.parent());
-   	}
-  	else if(clickedElement.hasClass("add-new-button") || clickedElement.parent().hasClass("add-new-button")){
-   		that.renderNewPaymentView();
-   	}
+    var clickedElement = $(e.target);
+    if(clickedElement.hasClass("active-item")){ 
+      that.renderSetAsPrimary(clickedElement);
+    }
+    else if(clickedElement.parent().hasClass("active-item") || clickedElement.parent().parent().hasClass("active-item")){
+      that.renderSetAsPrimary(clickedElement.parent());
+    }
+    else if(clickedElement.hasClass("add-new-button") || clickedElement.parent().hasClass("add-new-button")){
+      that.renderNewPaymentView();
+    }
   };
-  this.renderSetAsPrimary = function(element){	
-	var credit_id = element.attr("credit_id");
-  	var setPaymentAsPrimaryModal = new SetPaymentAsPrimaryModal();
+  this.renderSetAsPrimary = function(element){  
+  var credit_id = element.attr("credit_id");
+    var setPaymentAsPrimaryModal = new SetPaymentAsPrimaryModal();
     setPaymentAsPrimaryModal.initialize();
     setPaymentAsPrimaryModal.params = {"credit_id": credit_id, "myDom": that.myDom};
   };
   
   this.renderNewPaymentView = function(e){
-  	var addNewPaymentModal = new AddNewPaymentModal("guest", that.myDom);
+    var addNewPaymentModal = new AddNewPaymentModal("guest", that.myDom);
     addNewPaymentModal.initialize();  
   };
 };
