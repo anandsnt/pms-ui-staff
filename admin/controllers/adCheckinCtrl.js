148b53ac02b1ce8b635652a1dbe3706e7802e42c
Resheil <resheil@stayntouch.com>
2014-04-08 13:39:56 +0530

    CICO-5448 displaying data from response

--- admin/controllers/adCheckinCtrl.js
+++ admin/controllers/adCheckinCtrl.js
@@ -10,7 +10,16 @@ admin.controller('ADCheckinCtrl',['$scope','adCheckinSrv', function($scope,adChe
 		var fetchCheckinDetailsSuccessCallback = function(data) {
 			$scope.$emit('hideLoader');
 			$scope.checkinData = data;
+
 			$scope.checkinData.is_send_alert = ($scope.checkinData.is_send_alert === 'true') ? true:false;
+			$scope.checkinData.is_send_checkin_staff_alert = ($scope.checkinData.is_send_checkin_staff_alert === 'true') ? true:false;
+			$scope.checkinData.is_notify_on_room_ready = ($scope.checkinData.is_send_alert === 'true') ? true:false;
+			$scope.checkinData.require_cc_for_checkin_email = ($scope.checkinData.require_cc_for_checkin_email=== 'true') ? true:false;
+
+
+			//to be deleted 
+
+			$scope.checkinData.checkin_alert_primetime = "PM";
 
 		};
 		$scope.invokeApi(adCheckinSrv.fetch, {},fetchCheckinDetailsSuccessCallback);
@@ -20,18 +29,13 @@ admin.controller('ADCheckinCtrl',['$scope','adCheckinSrv', function($scope,adChe
 
 	$scope.hours = ["01","02","03","04","05","06","07","08","09","10","11","12"];
     $scope.minutes = ["00","15","30","45"];
+    $scope.primeTimes = ["AM","PM"];
 
 
-    $scope.toggleCheckbox =  function(type){
+    $scope.saveCheckin = function(){
 
+    	console.log($scope.checkinData);
 
-
-    	if(type === 'send_alert')
-    	{
-
-    		// alert("fee")
-    		// $scope.checkinData.is_send_alert = ($scope.checkinData.is_send_alert) ? false:true;
-    	}
     }
 
 
