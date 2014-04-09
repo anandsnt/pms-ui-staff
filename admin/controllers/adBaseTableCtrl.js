ae44fa1ea6d82f2e378e9a4c2751241632a7b791
akhilastayntouch <akhila@stayntouch.com>
2014-04-09 20:51:48 +0530

    CICO-3938 created the basetable class

--- admin/controllers/adBaseTableCtrl.js
+++ admin/controllers/adBaseTableCtrl.js
@@ -1,11 +1,11 @@
 function ADBaseTableCtrl($scope, ngTableParams){	
-	console.log("in base controller");
     BaseCtrl.call(this, $scope);
 
-    $scope.displayCountList = [2, 10, 25, 50, 100];
-    $scope.displyCount = 2;
+    $scope.displayCountList = [10, 25, 50, 100];
+    $scope.displyCount = 10;
     $scope.rateType = "";
     $scope.searchTerm = "";
+    $scope.filterType = {};
 
     $scope.$watch("displyCount", function () {
     	$scope.tableParams.reload();
@@ -19,59 +19,43 @@ function ADBaseTableCtrl($scope, ngTableParams){
     	$scope.tableParams.reload();
     };
 
-    $scope.fetchTableData = function(){
-
+    $scope.filterFetchSuccess = function(data){
+    	$scope.filterList = data;
+    	$scope.$emit('hideLoader');
     };
 
-    $scope.fetchSuccessOfTableData = function(){
-		$scope.$emit('hideLoader');
-		$scope.data = data.results;
-    	params.total(data.total_count);
-        $defer.resolve($scope.data);
-
-    };
-
-    $scope.getTableParams = function(){
+   	$scope.calculateGetParams = function(tableParams){
     	var getParams = {};
 		getParams.per_page = $scope.displyCount;
-		getParams.page = params.page();
-		getParams.rate_type_id = $scope.rateType.id;
+		getParams.page = tableParams.page();
+		if($scope.filterType != null && typeof $scope.filterType != "undefined")
+			getParams.rate_type_id = $scope.filterType.id;
 		getParams.query = $scope.searchTerm;
-		var sortData = params.sorting();
+		var sortData = tableParams.sorting();
 		var sortField = Object.keys(sortData)[0]
 		getParams.sort_field = sortField;
 		getParams.sort_dir = sortData[sortField];
 
 		return getParams;
+
     }
-
-    $scope.getTableData = function($defer, params){
-    	
-    	
-
-
-		/*var fetchSuccessOfItemList = function(data){
-			$scope.$emit('hideLoader');
-			$scope.data = data.results;
-        	params.total(data.total_count);
-            $defer.resolve($scope.data);
-		};*/
-
-		$scope.fetchTableData();
+    $scope.fetchTableData = function(){
 
     };
-    //applying sorting functionality in item list
-    $scope.tableParams = new ngTableParams({
-            page: 1,            // show first page
-            count: $scope.displyCount,    // count per page - Need to change when on pagination implemntation
-            sorting: {
-                rate: 'asc'     // initial sorting
-            },
-        }, {
-            total: 0, // length of data
-            getData: $scope.getTableData
-        }
-    );
 
+    $scope.loadTable = function(){
 
+    	$scope.tableParams = new ngTableParams({
+    	        page: 1,  // show first page
+    	        count: $scope.displyCount, // count per page 
+    	        sorting: {
+    	            rate: 'asc' // initial sorting
+    	        }
+    	    }, {
+    	        total: 0, // length of data
+    	        getData: $scope.fetchTableData
+    	    }
+    	);
+
+    }
 }
--- admin/controllers/adRatesListCtrl.js
+++ admin/controllers/adRatesListCtrl.js
@@ -1,50 +1,20 @@
 admin.controller('ADRatesListCtrl',['$scope', '$state', 'ADRatesSrv', 'ngTableParams','$filter',  function($scope, $state, ADRatesSrv, ngTableParams, $filter){
 	
 	$scope.errorMessage = '';
-	BaseCtrl.call(this, $scope);
-	$scope.roomTypeData = {};
-	$scope.displayCountList = [10, 25, 50, 100];
-	$scope.displyCount = 10;
-	$scope.rateType = "";
-	$scope.searchTerm = "";
+	ADBaseTableCtrl.call(this, $scope, ngTableParams);
+
 	/**
     * To fetch all rate types
     */
-	$scope.fetchRateTypes = function(){
-		var fetchSuccess = function(data) {
-			$scope.rateTypeList = data;
-			$scope.$emit('hideLoader');
-		};
-
-		$scope.invokeApi(ADRatesSrv.fetchRateTypes, {}, fetchSuccess);
+	$scope.fetchFilterTypes = function(){
+		$scope.invokeApi(ADRatesSrv.fetchRateTypes, {}, $scope.filterFetchSuccess);
 	};
 
-	$scope.fetchRateTypes();
+	$scope.fetchFilterTypes();
 
-	$scope.$watch("displyCount", function () {
-		$scope.tableParams.reload();
-	});
-
-	$scope.$watch("rateType", function () {
-		$scope.tableParams.reload();
-	});
-
-	$scope.searchClicked = function() {
-		$scope.tableParams.reload();
-	}
-
-	$scope.getTableData = function($defer, params){
-    	var getParams = {};
-		getParams.per_page = $scope.displyCount;
-		getParams.page = params.page();
-		getParams.rate_type_id = $scope.rateType.id;
-		getParams.query = $scope.searchTerm;
-		var sortData = params.sorting();
-		var sortField = Object.keys(sortData)[0]
-		getParams.sort_field = sortField;
-		getParams.sort_dir = sortData[sortField];
-
-
+	$scope.fetchTableData = function($defer, params){
+ 
+		var getParams = $scope.calculateGetParams(params);
 		var fetchSuccessOfItemList = function(data){
 			$scope.$emit('hideLoader');
 			$scope.data = data.results;
@@ -55,19 +25,26 @@ admin.controller('ADRatesListCtrl',['$scope', '$state', 'ADRatesSrv', 'ngTablePa
 		};
 		$scope.invokeApi(ADRatesSrv.fetchRates, getParams, fetchSuccessOfItemList);	
 	}	
+
+
+	$scope.loadTable = function(){
+
+		$scope.tableParams = new ngTableParams({
+		        page: 1,  // show first page
+		        count: $scope.displyCount, // count per page 
+		        sorting: {
+		            rate: 'asc' // initial sorting
+		        }
+		    }, {
+		        total: 0, // length of data
+		        getData: $scope.fetchTableData
+		    }
+		);
+
+	}
 		
-	//applying sorting functionality in item list
-	$scope.tableParams = new ngTableParams({
-	        page: 1,            // show first page
-	        count: $scope.displyCount,    // count per page - Need to change when on pagination implemntation
-	        sorting: {
-	            rate: 'asc'     // initial sorting
-	        }
-	    }, {
-	        total: 0, // length of data
-	        getData: $scope.getTableData
-	    }
-	);
+	$scope.loadTable();
+
 
 
 }]);
