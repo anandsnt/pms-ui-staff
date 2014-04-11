84da88857eee96e712c61d48b60bbe440b6e85ad
Resheil <resheil@stayntouch.com>
2014-04-11 21:28:22 +0530

    CICO-3940 adding service to call rates

--- admin/controllers/adAddRateDetailCtrl .js
+++ admin/controllers/adAddRateDetailCtrl .js
@@ -1,5 +1,27 @@
-admin.controller('ADaddRatesDetailCtrl',['$scope',  function($scope){
+admin.controller('ADaddRatesDetailCtrl',['$scope','ADRatesAddDetailsSrv',  function($scope,ADRatesAddDetailsSrv){
 
+	$scope.init = function(){
+		$scope.rateTypes = [];
+		$scope.basedOn = [];
+	}
+	$scope.init();
+
+	$scope.fetchData = function(){
+		
+		var fetchRateTypesSuccessCallback = function(data){
+			$scope.rateTypes = data.results;
+			$scope.$emit('hideLoader');
+		};
+		$scope.invokeApi(ADRatesAddDetailsSrv.fetchRateTypes, {},fetchRateTypesSuccessCallback);	
+
+		var fetchBasedOnSuccessCallback = function(data){
+			$scope.basedOn=data.results;
+			$scope.$emit('hideLoader');
+		};
+		$scope.invokeApi(ADRatesAddDetailsSrv.fetchBasedOnTypes, {},fetchBasedOnSuccessCallback);	
+	}	
+
+	$scope.fetchData();
 
 	$scope.saveStep1 = function(){
 		$scope.$emit("updateIndex","1");
---
+++ admin/services/adRatesAddDetailsSrv.js
@@ -0,0 +1,29 @@
+admin.service('ADRatesAddDetailsSrv', ['$q', 'ADBaseWebSrvV2',
+function($q, ADBaseWebSrvV2) {
+
+	this.fetchRateTypes = function() {
+		var deferred = $q.defer();
+		var url = "/api/rate_types.json";
+		ADBaseWebSrvV2.getJSON(url).then(function(data) {
+			deferred.resolve(data);
+		}, function(data) {
+			deferred.reject(data);
+		});
+		return deferred.promise;
+	};
+
+	this.fetchBasedOnTypes = function() {
+		var deferred = $q.defer();
+		var url = "/api/rates.json";
+		ADBaseWebSrvV2.getJSON(url).then(function(data) {
+			deferred.resolve(data);
+		}, function(data) {
+			deferred.reject(data);
+		});
+		return deferred.promise;
+	};
+
+
+
+
+}]);
