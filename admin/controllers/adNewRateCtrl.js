494720ca8c2fc9e9035345ee22194fcdadf4f12d
Resheil <resheil@stayntouch.com>
2014-04-11 18:41:25 +0530

    CICO-3940 adding comments

--- admin/controllers/adNewRateCtrl.js
+++ admin/controllers/adNewRateCtrl.js
@@ -1,56 +1,65 @@
 admin.controller('ADAddnewRate', ['$scope', function($scope) {
 	
 
-	$scope.init = function(){
+$scope.init = function(){
+
 	BaseCtrl.call(this, $scope);
-		$scope.currentRateStepIndex = -1;
-		$scope.currentStepIndexList = ["Details"];
+	$scope.currentRateStepIndex = -1;
+	$scope.currentStepIndexList = ["Details"];
 
-    }
-
+};
+ /*
+	* init function
+	*/
     $scope.init();
 
-    $scope.clickedStep =  function(index){
+ /*
+   * init function
+   */
 
-    	$scope.currentRateStepIndex = index;
+$scope.clickedStep =  function(index){
+	$scope.currentRateStepIndex = index;
+};
+  /*
+   	* to be updated from child classes 
+	*/
 
+$scope.$on("updateIndex", function(e,value){
+
+	if(value == 1){
+	if($scope.currentStepIndexList.length< 2){
+    	
+    	$scope.currentStepIndexList.push("Type");
+		 }
+	}
+	else if(value ==2){
+	if($scope.currentStepIndexList.length< 3){
+    	$scope.currentStepIndexList.push("Range");
+    	
     }
+	}
 
-    $scope.$on("updateIndex", function(e,value){
-   
-    	if(value == 1){
-    	if($scope.currentStepIndexList.length< 2){
-	    	
-	    	$scope.currentStepIndexList.push("Type");
-  		 }
-    	}
-    	else if(value ==2){
-    	if($scope.currentStepIndexList.length< 3){
-	    	$scope.currentStepIndexList.push("Range");
-	    	
-	    }
-    	}
+	$scope.currentRateStepIndex = value;
 
-    	$scope.currentRateStepIndex = value;
+});
 
-    });
+  /*
+   	* to include template
+	*/
 
+$scope.includeTemplate = function(index){
 
-
-
-    $scope.includeTemplate = function(index){
-
-	    switch (index){
-			case 0:
-			  return "/assets/partials/rates/adRatesAddDetails.html";
-			  break;
-			case 1:
-				return "/assets/partials/rates/adRatesAddTypes.html";
-			  break;
-			 case 2:
-			 	return "/assets/partials/rates/adRatesAddRange.html";
-			  break;
-		}
-    }
+    switch (index){
+		case 0:
+		  return "/assets/partials/rates/adRatesAddDetails.html";
+		  break;
+		case 1:
+			return "/assets/partials/rates/adRatesAddTypes.html";
+		  break;
+		 case 2:
+		 	return "/assets/partials/rates/adRatesAddRange.html";
+		  break;
+	};
+};
 
 }]);
