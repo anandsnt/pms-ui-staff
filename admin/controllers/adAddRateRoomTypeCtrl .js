badaa3670e92cf65b652fa25bf9b5c615406f8fe
Resheil <resheil@stayntouch.com>
2014-04-14 10:14:37 +0530

    CICO-3941 adding file for service

--- admin/controllers/adAddRateRoomTypeCtrl .js
+++ admin/controllers/adAddRateRoomTypeCtrl .js
@@ -1,11 +1,26 @@
-admin.controller('ADAddRateRoomTypeCtrl',['$scope',  function($scope){
+admin.controller('ADAddRateRoomTypeCtrl',['$scope','ADRatesAddRoomTypeSrv',  function($scope,ADRatesAddRoomTypeSrv){
 
+	$scope.roomTypes = [];
+	$scope.assignedRoomTypes = [];
 
+	$scope.fetchData = function(){
+		
+		var fetchRoomTypesSuccessCallback = function(data){
+			$scope.roomTypes = data.results;
+			$scope.$emit('hideLoader');
+		};
+		var fetchRoomTypesFailureCallback = function(data){
+			$scope.$emit('hideLoader');
+		};
+		$scope.invokeApi(ADRatesAddRoomTypeSrv.fetchRoomTypes, {},fetchRoomTypesSuccessCallback,fetchRoomTypesFailureCallback);	
+
+	};
+	$scope.fetchData();
 
 	$scope.saveStep2 = function(){
 		$scope.$emit("updateIndex","2");
 		
-	}
+	};
 
 
 }]);
