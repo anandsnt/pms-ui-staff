33e84a0d937912ba7f1056e424ed8b3b9fe1c680
Vijay Dev <vijay2stayntouch.com>
2014-04-16 19:34:16 +0530

    CICO-5322: stable state for reports

--- javascripts/reports.js
+++ javascripts/reports.js
@@ -1,4 +1,6 @@
 //=require ../reports/lib/js/underscore.min.js
 //=require ../reports/lib/js/angular.min.js
+//=require ../reports/lib/js/angular-route.js
+//=require ../reports/lib/js/angular-ui-router.js
 //=require ../reports/lib/js/angular-animate.js
 //= require_tree ../reports/js
--- reports/js/repApp.js
+++ reports/js/repApp.js
@@ -1,1 +1,48 @@
-var reports = angular.module('reports', []);
+var reports = angular.module('reports', ['ngAnimate']);
+
+reports.controller('reporstList', [
+	'$http',
+	'$q',
+	'$scope',
+	function($http, $q, $scope) {
+
+		$scope.list = [];
+		$scope.listCount = 0;
+
+		$scope.fetch = function() {
+			var deferred = $q.defer();
+			var url = '/api/reports.json';
+				
+			$http.get(url)
+				.success(function(response, status) {
+					console.log( response );
+
+					deferred.resolve(response);
+				})
+				.error(function(response, status) {
+					// please note the type of error expecting is array
+					// so form error as array if you modifying it
+					if(status == 406){ // 406- Network error
+						deferred.reject(errors);
+					}
+					else if(status == 500){ // 500- Internal Server Error
+						deferred.reject(['Internal server error occured']);
+					}
+					else if(status == 401){ // 401- Unauthorized
+						console.log('lets redirect');
+						// so lets redirect to login page
+						$window.location.href = '/logout' ;
+					}else{
+						deferred.reject(errors);
+					}
+				});
+
+			return deferred.promise;
+		};
+
+		$scope.fetch().then(function(response) {
+			$scope.list = response.results;
+			$scope.listCount = response.total_count;
+		});
+	}
+]);
