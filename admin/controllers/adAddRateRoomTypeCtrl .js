8f0a50c56d3dc455c04eebac54f936410f5cc424
Resheil <resheil@stayntouch.com>
2014-04-16 12:56:47 +0530

    CICO-3941 handling click actions of movers

--- admin/controllers/adAddRateRoomTypeCtrl .js
+++ admin/controllers/adAddRateRoomTypeCtrl .js
@@ -1,5 +1,7 @@
-admin.controller('ADAddRateRoomTypeCtrl',['$scope','ADRatesAddRoomTypeSrv',  function($scope,ADRatesAddRoomTypeSrv){
+	admin.controller('ADAddRateRoomTypeCtrl',['$scope','ADRatesAddRoomTypeSrv',  function($scope,ADRatesAddRoomTypeSrv){
 
+	$scope.selectedAssignedRoomIndex =-1;
+	$scope.selectedUnAssignedRoomIndex =-1;
 	$scope.nonAssignedroomTypes = [];
 	$scope.assignedRoomTypes = [];
 
@@ -40,6 +42,10 @@ admin.controller('ADAddRateRoomTypeCtrl',['$scope','ADRatesAddRoomTypeSrv',  fun
 		array.push($data);
 	};
 
+   /*
+	 * To check if any room is assigned
+	 *
+	 */
 	$scope.anyRoomSelected = function(){
 
 		if($scope.assignedRoomTypes.length >0)
@@ -47,7 +53,78 @@ admin.controller('ADAddRateRoomTypeCtrl',['$scope','ADRatesAddRoomTypeSrv',  fun
 		else
 			return false;
 	}
+   /*
+	 * To register selected assigned room 
+	 *
+	 */
 
+	$scope.assignedRoomSelected = function(index){
+			$scope.selectedAssignedRoomIndex =index;
+		
+	}
+   /*
+	 * To register selected unassigned room 
+	 *
+	 */
 
-}]);
+	$scope.unAssignedRoomSelected = function(index){
+		$scope.selectedUnAssignedRoomIndex =index;
+
+	}
+   /*
+	 * To handle click action for selected room type 
+	 *
+	 */
+
+	$scope.topMoverightClicked = function(){
+		
+		if($scope.selectedAssignedRoomIndex != -1){
+			var temp = $scope.assignedRoomTypes[$scope.selectedAssignedRoomIndex];
+			$scope.nonAssignedroomTypes.push(temp)
+			$scope.assignedRoomTypes.splice($scope.selectedAssignedRoomIndex,1);
+			$scope.selectedAssignedRoomIndex =-1;
+		 }
+	};
+   /*
+	 * To handle click action for selected room type 
+	 *
+	 */
+	$scope.topMoveleftClicked = function(){
+		
+		if($scope.selectedUnAssignedRoomIndex != -1){
+			var temp = $scope.nonAssignedroomTypes[$scope.selectedUnAssignedRoomIndex];
+			$scope.assignedRoomTypes.push(temp)
+			$scope.nonAssignedroomTypes.splice($scope.selectedUnAssignedRoomIndex,1);
+			$scope.selectedUnAssignedRoomIndex =-1;
+	    }
+	};
+   /*
+	 * To handle click action to move all assigned room types 
+	 *
+	 */
+
+	$scope.bottomMoverightClicked = function(){
+
+		if($scope.assignedRoomTypes.length>0){
+			angular.forEach($scope.assignedRoomTypes, function(item){
+			   $scope.nonAssignedroomTypes.push(item);
+			 });
+			$scope.assignedRoomTypes = [];
+		}
+	};
+   /*
+	 * To handle click action to move all unassigned room types 
+	 *
+	 */
+	$scope.bottomMoveleftClicked = function(){
+
+		if($scope.nonAssignedroomTypes.length>0){
+			angular.forEach($scope.nonAssignedroomTypes, function(item){
+	        $scope.assignedRoomTypes.push(item);
+	 });
+			$scope.nonAssignedroomTypes = [];
+		}
+	};
+
+	}]);
 
