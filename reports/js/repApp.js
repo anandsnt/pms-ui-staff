4bf3e7fc4bab709591d8fe0336b66b2b5f3cd839
Vijay Dev <vijay2stayntouch.com>
2014-04-17 17:02:49 +0530

    CICO-5322: Updateed

--- reports/js/repApp.js
+++ reports/js/repApp.js
@@ -25,6 +25,9 @@ reports.controller('reporstList', [
 	'$scope',
 	function($http, $q, $scope) {
 
+		sntapp.activityIndicator.showActivityIndicator('BLOCKER');
+
+		$scope.showReports = false;
 		$scope.list = [];
 		$scope.listCount = 0;
 
@@ -34,8 +37,6 @@ reports.controller('reporstList', [
 				
 			$http.get(url)
 				.success(function(response, status) {
-					console.log( response );
-
 					deferred.resolve(response);
 				})
 				.error(function(response, status) {
@@ -59,20 +60,99 @@ reports.controller('reporstList', [
 			return deferred.promise;
 		};
 
-		$scope.fetch().then(function(response) {
-			$scope.list = response.results;
-			$scope.listCount = response.total_count;
+		$scope.fetchFilters = function(id) {
+			var deferred = $q.defer();
+			var url = '/api/reports/' + id;
 
-			// include show_filter
-			for (var i = 0, j = $scope.list.length; i < j; i++) {
-				$scope.list[i]['show_filter'] = false;
-			};
-		});
+			$http.get(url)
+				.success(function(response, status) {
+					deferred.resolve(response);
+				})
+				.error(function(response, status) {
+					// please note the type of error expecting is array
+					// so form error as array if you modifying it
+					if(status == 406){ // 406- Network error
+						deferred.reject(errors);
+					}
+					else if(status == 500){ // 500- Internal Server Error
+						deferred.reject(['Internal server error occured']);
+					}
+					else if(status == 401){ // 401- Unauthorized
+						console.log('lets redirect');
+						// so lets redirect to login page
+						$window.location.href = '/logout' ;
+					}else{
+						deferred.reject(errors);
+					}
+				});
+
+			return deferred.promise;
+		};
+
+		$scope.fetchFilters()
+			.then(function(response) {
+
+			});
+
+		$scope.fetch()
+			.then(function(response) {
+				$scope.list = response.results;
+				$scope.listCount = response.total_count;
+
+				for (var i = 0, j = $scope.list.length; i < j; i++) {
+
+					// include show_filter
+					$scope.list[i]['show_filter'] = false;
+
+					// search filter rules
+					$scope.fetchFilters( $scope.list[i]['id'] )
+						.then(function(response) {
+
+							sntapp.activityIndicator.hideActivityIndicator();
+							$scope.showReports = true;
+
+							// var report = _.find($scope.list, function(item) {
+							// 	return item.name === response.name;
+							// });
+
+							// report.filters = response.filters;
+							// report.sort_fields = response.sort_fields;
+						});
+				};
+			});
 
 		// show hide filter
 		$scope.toggleFilter = function() {
 			// DO NOT flip as scuh could endup in infinite $digest loop
 			this.item.show_filter = this.item.show_filter ? false : true; 
+		};
+
+		// show date range
+		$scope.hasDateRange = function(item) {
+			var dateRange = _.find(item.filters, function(filter) {
+				return filter.value = 'DATE_RANGE';
+			});
+
+			return dateRange ? true : false;
+		};
+
+		// show tranction type
+		$scope.hasTransactionType = function(item) {
+			var transactionType = _.find(item.filters, function(filter) {
+				return filter.value = 'CICO';
+			});
+
+			console.log(item);
+
+			return transactionType ? true : false;
+		};
+
+		$scope.hasStaff = function(item) {
+			var staff = _.find(item.filters, function(filter) {
+				return filter.value = 'STAFF';
+			});
+
+			return staff ? true : false;
 		};
 	}
 ]);
