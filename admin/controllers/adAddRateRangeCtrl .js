1bd38600a50a2be25dadbf6c6d1b96a45045578c
Resheil <resheil@stayntouch.com>
2014-04-18 08:34:12 +0530

    CICO-3943 adding calendar

--- admin/controllers/adAddRateRangeCtrl .js
+++ admin/controllers/adAddRateRangeCtrl .js
@@ -1,8 +1,7 @@
-admin.controller('ADAddRateRangeCtrl ',['$scope',  function($scope){
-
+admin.controller('ADAddRateRangeCtrl',['$scope','$filter','dateFilter',function($scope,$filter,dateFilter){
 	$scope.saveStep3 = function(){
-		$scope.save(2);
-	}
+	$scope.save(2);
+ }
 
+ $scope.date = dateFilter(new Date(), 'yyyy-MM-dd');
 }]);
-
