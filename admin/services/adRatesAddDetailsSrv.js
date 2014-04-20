a29d6e05d280d0bd3f19ecc587a0ee559a113a5d
Resheil <resheil@stayntouch.com>
2014-04-20 08:07:31 +0530

    CICO-3940 removing json extension from services

--- admin/services/adRatesAddDetailsSrv.js
+++ admin/services/adRatesAddDetailsSrv.js
@@ -7,7 +7,7 @@ function($q, ADBaseWebSrvV2) {
     */
 	this.fetchRateTypes = function() {
 		var deferred = $q.defer();
-		var url = "/api/rate_types/active.json";
+		var url = "/api/rate_types/active";
 		ADBaseWebSrvV2.getJSON(url).then(function(data) {
 			deferred.resolve(data);
 		}, function(data) {
@@ -21,7 +21,7 @@ function($q, ADBaseWebSrvV2) {
     */
 	this.fetchBasedOnTypes = function(data) {
 		var deferred = $q.defer();
-		var url = "/api/rates.json";
+		var url = "/api/rates";
 		ADBaseWebSrvV2.getJSON(url,data).then(function(data) {
 			deferred.resolve(data);
 		}, function(data) {
