2835eb773e22cf043a7a13770aa88761fac0095d
Resheil <resheil@stayntouch.com>
2014-04-22 20:06:12 +0530

    CICO-3941 fixing issues

--- admin/controllers/adNewRateCtrl.js
+++ admin/controllers/adNewRateCtrl.js
@@ -33,7 +33,7 @@ $scope.$on("updateIndex", function(e,value){
 	}
 	else if(value ==2){
 	if($scope.currentStepIndexList.length< 3){
-    	$scope.currentStepIndexList.push("Configure"); 
+    	$scope.currentStepIndexList.push("Range"); 
     	$scope.clickedStep(parseInt(value));   	
     }
 	}
