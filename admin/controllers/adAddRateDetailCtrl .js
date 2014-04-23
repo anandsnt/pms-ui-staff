5e3f086215d5d80975e1c44d162e1213f9c6438a
Resheil <resheil@stayntouch.com>
2014-04-23 20:14:34 +0530

    CICO-3940 corecting chained service

--- admin/controllers/adAddRateDetailCtrl .js
+++ admin/controllers/adAddRateDetailCtrl .js
@@ -2,9 +2,7 @@ admin.controller('ADaddRatesDetailCtrl',['$scope','ADRatesAddDetailsSrv',  funct
 
 $scope.init = function(){
 	BaseCtrl.call(this, $scope);
-	$scope.rateTypes = [];
-	$scope.basedOn = [];
-	$scope.basedOnRateList = [];
+	$scope.rateTypesDetails = [];
 	$scope.basedOnRateTypeSelected = '';
 	$scope.rateTypeselected ='';
 	$scope.rate_name = '';
@@ -46,41 +44,13 @@ $scope.allFieldsnotFilled = function(){
 $scope.fetchData = function(){
 	
 	var fetchRateTypesSuccessCallback = function(data){
-		$scope.rateTypes = data;
+		$scope.rateTypesDetails = data;
 		$scope.$emit('hideLoader');
 	};
 	var fetchRateTypesFailureCallback = function(data){
 		$scope.$emit('hideLoader');
 	};
 	$scope.invokeApi(ADRatesAddDetailsSrv.fetchRateTypes, {},fetchRateTypesSuccessCallback,fetchRateTypesFailureCallback);	
-
-	var fetchBasedOnSuccessCallback = function(data){
-		$scope.basedOn=data.results;
-		$scope.$emit('hideLoader');
-	};
-	var fetchBasedOnFailureCallback = function(data){
-		$scope.$emit('hideLoader');
-	};
-	var getParams ={
-		'page':'1',
-		'per_page':'1000',
-		 'query':'',
-		 'sort_dir':'asc',
-		 'sort_field':''
-		};
-
-	$scope.invokeApi(ADRatesAddDetailsSrv.fetchBasedOnTypes, getParams,fetchBasedOnSuccessCallback,fetchBasedOnFailureCallback);	
-
-	
-	var fetchHotelSettingsSuccessCallback = function(data){
-		$scope.currenyCode =data.currency.symbol;
-		$scope.$emit('hideLoader');
-	};
-	var fetchHotelSettingsFailureCallback = function(data){
-		$scope.$emit('hideLoader');
-	};
-
-	$scope.invokeApi(ADRatesAddDetailsSrv.fetchHotelSettings,{},fetchHotelSettingsSuccessCallback,fetchHotelSettingsFailureCallback);	
 	
 }	
 
