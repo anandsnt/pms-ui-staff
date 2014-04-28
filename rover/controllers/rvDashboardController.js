a0b14cf55f8eb71ad9e5e1e275742eced37281f3
Resheil <resheil@stayntouch.com>
2014-04-28 18:50:20 +0530

    CICO-5381 API integration

--- rover/controllers/rvDashboardController.js
+++ rover/controllers/rvDashboardController.js
@@ -1,26 +1,27 @@
 sntRover.controller('RVdashboardController',['$scope','RVDashboardSrv', function($scope,RVDashboardSrv){
 	
 	
-	$scope.data = {
-		"firstname" : " Jos",
-		"lastname":"Schaap",
-		"late_checkout": "8",
-		"date":"feb 24, 2014",
-		"due_in_count":null,
-		"due_out_count":"1",
-		"inhouse_count":"1",
-		"guest_review_score":"23",
-		"currency_code" :"$",
-		"upsell_target":"9",
-		"actual_target":"9",
-		"vip_checkin":"2",
-		"rooms_for_upsell":"2",
-		"rooms_upsold":"3"
-	}
+	// $scope.data = {
+	// 	"firstname" : " Jos",
+	// 	"lastname":"Schaap",
+	// 	"late_checkout": "8",
+	// 	"date":"feb 24, 2014",
+	// 	"due_in_count":null,
+	// 	"due_out_count":"1",
+	// 	"inhouse_count":"1",
+	// 	"guest_review_score":"23",
+	// 	"currency_code" :"$",
+	// 	"upsell_target":"9",
+	// 	"actual_target":"9",
+	// 	"vip_checkin":"2",
+	// 	"rooms_for_upsell":"2",
+	// 	"rooms_upsold":"3"
+	// }
 
    $scope.init =  function(){
+
    	BaseCtrl.call(this, $scope);
-   	 	//Display greetings message
+   	//Display greetings message based on current time
    	var d = new Date();
   	var time = d.getHours();
   	$scope.greetingsMessage = "";
@@ -33,6 +34,7 @@ sntRover.controller('RVdashboardController',['$scope','RVDashboardSrv', function
   	else{
   		$scope.greetingsMessage = "Good Evening";
   	}
+
    }
    $scope.init();
 
@@ -42,7 +44,10 @@ sntRover.controller('RVdashboardController',['$scope','RVDashboardSrv', function
 $scope.fetchData = function(){
 	
 	var fetchDashboardDetailsSuccessCallback = function(data){
-		$scope.dashBoarddata = data;
+		$scope.dashboardData = data.dashboardData;
+		$scope.userDetails   = data.userDetails;
+		$scope.lateCheckoutDetails = data.lateCheckoutDetails;
+		$scope.currencySymbol=getCurrencySign(data.userDetails.currency_code);
 		$scope.$emit('hideLoader');
 	};
 	var fetchDashboardDetailsFailureCallback = function(data){
--- rover/services/rvDashboardSrv.js
+++ rover/services/rvDashboardSrv.js
@@ -5,30 +5,52 @@ this.dashboardDetails = {};
 
    /*
     * To fetch dashboard details
-    * @return {object}late dashboard details
+    * @return {object} dashboard details
     */	
 
 
    this.fetchDashboardDetails = function(){
 		var deferred = $q.defer();
 
-		 this.fetchlateCheckoutDetails = function(){
-			var deferred = $q.defer();
-			var url = 'staff/dashboard/late_checkout_count.json';	
-			
+
+   /*
+    * To fetch user details
+    * @return {object} user details
+    */	
+		 this.fetchdashboardDetails = function(){
+			var url = 'staff/header_info.json'
 			RVBaseWebSrv.getJSON(url).then(function(data) {
-				that.dashboardDetails.lateCheckoutDetails = data;
-				deferred.resolve(that.dashboardDetails.lateCheckoutDetails);
+				that.dashboardDetails.userDetails = data;
+				this.fetchlateCheckoutDetails();
 			},function(errorMessage){
 				deferred.reject(errorMessage);
 			});
 			return deferred.promise;
 		};
-		var url = '/staff/dashboard.json';	
+   /*
+    * To fetch late checkout details
+    * @return {object}late checkout details
+    */
+
+		 this.fetchlateCheckoutDetails = function(){
+			var url = 'staff/dashboard/late_checkout_count.json';	
+			
+			RVBaseWebSrv.getJSON(url).then(function(data) {
+				that.dashboardDetails.lateCheckoutDetails = data;
+				deferred.resolve(that.dashboardDetails);
+			},function(errorMessage){
+				deferred.reject(errorMessage);
+			});
+			return deferred.promise;
+		};
+		
+
+		var url = '/staff/dashboard.json';
+			
 		
 		RVBaseWebSrv.getJSON(url).then(function(data) {
 			that.dashboardDetails.dashboardData = data;
-			this.fetchlateCheckoutDetails();
+			this.fetchdashboardDetails();
 		},function(errorMessage){
 			deferred.reject(errorMessage);
 		});
