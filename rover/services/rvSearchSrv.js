c2e504b0ee333977ca43835c6fef32aef4bdb3b1
resheilSNT <resheil@stayntouch.com>
2014-04-29 16:59:05 +0530

    CICO-5386 Added Changed Service name

---
+++ rover/services/rvSearchSrv.js
@@ -0,0 +1,20 @@
+sntRover.service('RVSearchSrv',['$q', 'RVBaseWebSrv', function($q, RVBaseWebSrv){
+	
+	var self = this;
+	
+	this.fetch = function(){
+		var deferred = $q.defer();
+		var data = {fakeDataToAvoidCache: new Date(), status: 'DUEIN'};
+		var url =  'search.json';	
+		
+		RVBaseWebSrv.getJSON(url, data).then(function(data) {
+			deferred.resolve(data);
+		},function(data){
+			deferred.reject(data);
+		});
+		return deferred.promise;		
+		
+	};
+
+
+}]);
