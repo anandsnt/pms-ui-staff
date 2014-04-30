048f4c4923bdc8e0bcc101d1ee171da2b72d54a6
Resheil <resheil@stayntouch.com>
2014-04-30 15:14:42 +0530

    CICO-5932 implementing settings

--- rover/controllers/rvstaffsettingsModalController.js
+++ rover/controllers/rvstaffsettingsModalController.js
@@ -1,7 +1,37 @@
-sntRover.controller('RVStaffsettingsModalController', ['$scope','ngDialog', function($scope,ngDialog){
+sntRover.controller('RVStaffsettingsModalController', ['$scope','ngDialog','RVSettingsSrv', function($scope,ngDialog,RVSettingsSrv){
 
- $scope.cancelClicked = function(){
-     ngDialog.close();
+BaseCtrl.call(this, $scope);
+$scope.new_password = '';
+$scope.confirmPassword ='';
+$scope.fetchData = function(){   
+    var fetchUserInfoSuccessCallback = function(data){
+        $scope.userInfo = data;
+        $scope.$emit('hideLoader');
+    };
+    var fetchUserInfoFailureCallback = function(data){
+        $scope.$emit('hideLoader');
+    };
+    $scope.invokeApi(RVSettingsSrv.fetchUserInfo,{},fetchUserInfoSuccessCallback,fetchUserInfoFailureCallback);  
 
-   };
+}   
+
+$scope.fetchData();
+	
+	$scope.cancelClicked = function(){
+ ngDialog.close();
+
+};
+
+
+$scope.updateSettings = function(){
+
+	var updateUserInfoSuccessCallback = function(data){
+	    $scope.$emit('hideLoader');
+	};
+	var updateUserInfoFailureCallback = function(data){
+	    $scope.$emit('hideLoader');
+	};
+	$scope.invokeApi(RVSettingsSrv.updateUserInfo,{'new_password' :$scope.new_password},updateUserInfoSuccessCallback,updateUserInfoFailureCallback);  
+	};
+
 }]);
