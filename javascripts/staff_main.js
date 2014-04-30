c3b009fc40a48ceab3ec8f36882d31ec801d2e1a
resheilSNT <resheil@stayntouch.com>
2014-04-30 15:14:32 +0530

    repositioned ng-iscroll position in staff_main.js

--- javascripts/staff_main.js
+++ javascripts/staff_main.js
@@ -5,8 +5,8 @@
 //=require ../shared/lib/js/angular-route.js
 //=require ../shared/lib/js/angular-ui-router.js
 
-//=require ../shared/lib/js/ng-iscroll.js
 //=require ../shared/lib/js/iscroll.js
+//=require ../shared/lib/js/ng-iscroll.js
 //=require ../shared/lib/js/ngDialog.min.js
 //=require ../shared/lib/js/Utils.js
 
