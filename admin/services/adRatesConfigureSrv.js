c87b0ef550259e3d46eb7068ac501fcd90d1ba51
Shiju Devarajan <shiju-d@stayntouch.com>
2014-05-06 10:34:15 +0530

    CICO-5498: Formatted rates configurator service JS files

--- admin/services/adRatesConfigureSrv.js
+++ admin/services/adRatesConfigureSrv.js
@@ -1,66 +1,68 @@
-admin.service('ADRatesConfigureSrv',['$http', '$q', 'ADBaseWebSrvV2','$rootScope', function($http, $q, ADBaseWebSrvV2,$rootScope){
-	
-	this.currentSetData = {};
-	this.dateId ='';
-	this.hasBaseRate = false;
-	this.setCurrentSetData = function(data){
-		this.currentSetData =data;
-		 $rootScope.$broadcast('dateRangeUpdated',this.currentSetData);
-	};
-	this.getCurrentSetData = function(data){
-		return this.currentSetData;
-	};
-	
-	this.setDateId = function(id){
-		this.dateId =id;
-	};
-	var that = this;
+admin.service('ADRatesConfigureSrv', ['$http', '$q', 'ADBaseWebSrvV2', '$rootScope',
+    function ($http, $q, ADBaseWebSrvV2, $rootScope) {
 
-	this.fetchSetsInDateRange = function(data) {
-		var deferred = $q.defer();
-		var url = "/api/rate_date_ranges/"+data.id;
-		that.setDateId(data.id);
-		ADBaseWebSrvV2.getJSON(url).then(function(data) {
-			deferred.resolve(data);
-		}, function(data) {
-			deferred.reject(data);
-		});
-		return deferred.promise;
-	};
-	
-	this.saveSet = function(data){
-		console.log(data);
-		var deferred = $q.defer();
-		var url = "/api/rate_sets/"+data.id;
-		ADBaseWebSrvV2.putJSON(url, data).then(function(data) {
-			deferred.resolve(data);
-		}, function(data) {
-			deferred.reject(data);
-		});
-		return deferred.promise;
-	};
-	this.deleteSet = function(id){
-		
-		var deferred = $q.defer();
-		var url = "/api/rate_sets/"+id;
-		ADBaseWebSrvV2.deleteJSON(url).then(function(data) {
-			deferred.resolve(data);
-		}, function(data) {
-			deferred.reject(data);
-		});
-		return deferred.promise;
+        this.currentSetData = {};
+        this.dateId = '';
+        this.hasBaseRate = false;
+        this.setCurrentSetData = function (data) {
+            this.currentSetData = data;
+            $rootScope.$broadcast('dateRangeUpdated', this.currentSetData);
+        };
+        this.getCurrentSetData = function (data) {
+            return this.currentSetData;
+        };
 
-	};
-	this.updateDateRange = function(data){
-		var deferred = $q.defer();
-		var id = that.dateId;
-		var url = "/api/rate_date_ranges/"+id;
-		ADBaseWebSrvV2.putJSON(url, data).then(function(data) {
-			deferred.resolve(data);
-		}, function(data) {
-			deferred.reject(data);
-		});
-		return deferred.promise;
-	};
-  
-}]);
+        this.setDateId = function (id) {
+            this.dateId = id;
+        };
+        var that = this;
+
+        this.fetchSetsInDateRange = function (data) {
+            var deferred = $q.defer();
+            var url = "/api/rate_date_ranges/" + data.id;
+            that.setDateId(data.id);
+            ADBaseWebSrvV2.getJSON(url).then(function (data) {
+                deferred.resolve(data);
+            }, function (data) {
+                deferred.reject(data);
+            });
+            return deferred.promise;
+        };
+
+        this.saveSet = function (data) {
+            console.log(data);
+            var deferred = $q.defer();
+            var url = "/api/rate_sets/" + data.id;
+            ADBaseWebSrvV2.putJSON(url, data).then(function (data) {
+                deferred.resolve(data);
+            }, function (data) {
+                deferred.reject(data);
+            });
+            return deferred.promise;
+        };
+        this.deleteSet = function (id) {
+
+            var deferred = $q.defer();
+            var url = "/api/rate_sets/" + id;
+            ADBaseWebSrvV2.deleteJSON(url).then(function (data) {
+                deferred.resolve(data);
+            }, function (data) {
+                deferred.reject(data);
+            });
+            return deferred.promise;
+
+        };
+        this.updateDateRange = function (data) {
+            var deferred = $q.defer();
+            var id = that.dateId;
+            var url = "/api/rate_date_ranges/" + id;
+            ADBaseWebSrvV2.putJSON(url, data).then(function (data) {
+                deferred.resolve(data);
+            }, function (data) {
+                deferred.reject(data);
+            });
+            return deferred.promise;
+        };
+
+    }
+]);
--- admin/services/adRatesSrv.js
+++ admin/services/adRatesSrv.js
@@ -1,82 +1,83 @@
 admin.service('ADRatesSrv', ['$http', '$q', 'ADBaseWebSrvV2',
-function($http, $q, ADBaseWebSrvV2) {
+    function ($http, $q, ADBaseWebSrvV2) {
 
-	this.fetchRates = function(data) {
-		var deferred = $q.defer();
+        this.fetchRates = function (data) {
+            var deferred = $q.defer();
 
-		var url = "/api/rates.json";
-		ADBaseWebSrvV2.getJSON(url, data).then(function(data) {
-			deferred.resolve(data);
-		}, function(data) {
-			deferred.reject(data);
-		});
-		return deferred.promise;
-	};
+            var url = "/api/rates.json";
+            ADBaseWebSrvV2.getJSON(url, data).then(function (data) {
+                deferred.resolve(data);
+            }, function (data) {
+                deferred.reject(data);
+            });
+            return deferred.promise;
+        };
 
-	this.fetchRateTypes = function() {
-		var deferred = $q.defer();
+        this.fetchRateTypes = function () {
+            var deferred = $q.defer();
 
-		//var url = " /sample_json/ng_admin/rate_types.json";
-		var url = "/api/rate_types.json";
-		ADBaseWebSrvV2.getJSON(url).then(function(data) {
-			deferred.resolve(data.results);
-		}, function(data) {
-			deferred.reject(data);
-		});
-		return deferred.promise;
-	};
+            //var url = " /sample_json/ng_admin/rate_types.json";
+            var url = "/api/rate_types.json";
+            ADBaseWebSrvV2.getJSON(url).then(function (data) {
+                deferred.resolve(data.results);
+            }, function (data) {
+                deferred.reject(data);
+            });
+            return deferred.promise;
+        };
 
-	this.importRates = function() {
-		var deferred = $q.defer();
-		var url = "/api/rates/import";
-		ADBaseWebSrvV2.postJSON(url).then(function(data) {
-			deferred.resolve(data.results);
-		}, function(data) {
-			deferred.reject(data);
-		});
-		return deferred.promise;
-	};
+        this.importRates = function () {
+            var deferred = $q.defer();
+            var url = "/api/rates/import";
+            ADBaseWebSrvV2.postJSON(url).then(function (data) {
+                deferred.resolve(data.results);
+            }, function (data) {
+                deferred.reject(data);
+            });
+            return deferred.promise;
+        };
 
-	this.fetchDateRanges = function(params) {
-		var deferred = $q.defer();
+        this.fetchDateRanges = function (params) {
+            var deferred = $q.defer();
 
-		var url = "/api/rates/" + params.rate_id + "/rate_date_ranges";
-		ADBaseWebSrvV2.getJSON(url).then(function(data) {
-			deferred.resolve(data);
-		}, function(data) {
-			deferred.reject(data);
-		});
-		return deferred.promise;
-	};
-	var that = this;
-	this.rateDetails = {};
+            var url = "/api/rates/" + params.rate_id + "/rate_date_ranges";
+            ADBaseWebSrvV2.getJSON(url).then(function (data) {
+                deferred.resolve(data);
+            }, function (data) {
+                deferred.reject(data);
+            });
+            return deferred.promise;
+        };
+        var that = this;
+        this.rateDetails = {};
 
-	// get rate details
-	this.fetchDetails = function(params) {
-		var deferred = $q.defer();
-		// fetch hotel business date
-		this.fetchHotelInfo = function(){
-			var url = "/api/rover_header_info";
-			ADBaseWebSrvV2.getJSON(url).then(function(data) {
-				data = data.data;
-				that.rateDetails.business_date = data.business_date;
-				deferred.resolve(that.rateDetails);
-			}, function(data) {
-				deferred.reject(data);
-			});
+        // get rate details
+        this.fetchDetails = function (params) {
+            var deferred = $q.defer();
+            // fetch hotel business date
+            this.fetchHotelInfo = function () {
+                var url = "/api/rover_header_info";
+                ADBaseWebSrvV2.getJSON(url).then(function (data) {
+                    data = data.data;
+                    that.rateDetails.business_date = data.business_date;
+                    deferred.resolve(that.rateDetails);
+                }, function (data) {
+                    deferred.reject(data);
+                });
 
-		}
+            }
 
 
 
-		var url = "/api/rates/" + params.rateId;
-		ADBaseWebSrvV2.getJSON(url).then(function(data) {
-			that.rateDetails = data;
-			this.fetchHotelInfo();
-		}, function(data) {
-			deferred.reject(data);
-		});
-		return deferred.promise;
-	}
+            var url = "/api/rates/" + params.rateId;
+            ADBaseWebSrvV2.getJSON(url).then(function (data) {
+                that.rateDetails = data;
+                this.fetchHotelInfo();
+            }, function (data) {
+                deferred.reject(data);
+            });
+            return deferred.promise;
+        }
 
-}]);
+    }
+]);
