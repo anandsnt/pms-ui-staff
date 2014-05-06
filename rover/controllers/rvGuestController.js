42b49c4105eb0b2efc6abd524c806b4211f20a10
Resheil <resheil@stayntouch.com>
2014-05-06 18:12:38 +0530

    CICO-5922 adding files for contact info

--- rover/controllers/rvGuestController.js
+++ rover/controllers/rvGuestController.js
@@ -4,36 +4,41 @@ sntRover.controller('guestCardController', ['$scope', 'Likes', '$window', 'RVRes
 	//To get data from service
     $scope.guestData = RVReservationCardSrv.getGuestData();
 
+
+    // to be changed
+    $scope.guestCardHeight = 90;
+
 	$scope.guestCardTabSwitch = function(div){
 		$scope.current = div;
 	};
 	
 	$scope.guestCardToggle = function(){
 		
+		$scope.guestCardHeight = ($scope.guestCardHeight === 90) ? 340:90;
 		
-		var $isTablet = navigator.userAgent.match(/Android|iPad/i) != null,
-			$maxHeight = $window.innerHeight,
-			$breakpoint = ($maxHeight/10);
+		// var $isTablet = navigator.userAgent.match(/Android|iPad/i) != null,
+		// 	$maxHeight = $window.innerHeight,
+		// 	$breakpoint = ($maxHeight/10);
 	
-		if (!$isTablet) {
-			$(window).resize(function() {
-		    	$maxHeight = $(window).height();
+		// if (!$isTablet) {
+		// 	$(window).resize(function() {
+		//     	$maxHeight = $(window).height();
 	
-		    	// Resize guest card if too big
-		    	if ($('#guest-card').hasClass('open') && $('#guest-card').height() > ($maxHeight-90))
-		    	{
-		    		$('#guest-card').css({'height':$maxHeight-90+'px'});
-		    	}
+		//     	// Resize guest card if too big
+		//     	if ($('#guest-card').hasClass('open') && $('#guest-card').height() > ($maxHeight-90))
+		//     	{
+		//     		$('#guest-card').css({'height':$maxHeight-90+'px'});
+		//     	}
 	
-		    	// Close guest card if too small
-		    	if ($('#guest-card').height() < 90)
-		    	{
-		    		$('#guest-card').removeClass('open').css({'height':90+'px'});
-		    	}
+		//     	// Close guest card if too small
+		//     	if ($('#guest-card').height() < 90)
+		//     	{
+		//     		$('#guest-card').removeClass('open').css({'height':90+'px'});
+		//     	}
 	
-		    	resizableGuestCard($maxHeight);
-			});
-	}
+		//     	resizableGuestCard($maxHeight);
+		// 	});
+	//}
 		
 		
 		
