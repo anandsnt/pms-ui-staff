7ba9944b2cf035ce8695aad64ab593eda6a97450
soumya <soumya@stayntouch.com>
2014-05-06 15:26:14 +0530

    CICO-5920 RESERVATION DETAILS GUEST DATA

---
+++ rover/controllers/rvReservationCardController.js
@@ -0,0 +1,79 @@
+sntRover.controller('reservationCardController',['$scope', 'RVReservationCardSrv', function($scope, RVReservationCardSrv){
+	BaseCtrl.call(this, $scope);
+	$scope.timeline = "current";
+	$scope.reservationList = [];
+	$scope.currentReservationId = "";
+	/*
+	 * to get state params from resrvation details controller
+	 */
+	$scope.$on('passReservationParams',function(event, data){
+		
+		// $scope.fetchReservationData(data.reservationId);
+		// $scope.currentReservationId = data.confirmationNumber;
+
+		$scope.data = data;
+		$scope.countCurrent = data.reservation_list.current_reservations_arr.length;
+		$scope.countUpcoming = data.reservation_list.upcoming_reservations_arr.length;
+		$scope.countHistory = data.reservation_list.history_reservations_arr.length;
+				
+		// $scope.$broadcast("RESERVATIONDETAILS", $scope.currentReservationId);
+		$scope.currentReservationId = data.reservation_details.confirmation_num;
+		$scope.reservationList = data.reservation_list.current_reservations_arr;
+		
+		RVReservationCardSrv.setGuestData($scope.data.guest_details);
+		
+		// $scope.$broadcast('passGuestDetails', $scope.data.guest_details);
+	});
+	/*
+	 * Handles time line click events
+	 * @param {string} time line
+	 */
+	$scope.showTimeLineReservation = function(timeline){
+		if(timeline == "current"){
+			$scope.timeline = "current";
+			$scope.reservationList = $scope.data.reservation_list.current_reservations_arr;
+			
+			if($scope.countCurrent>0){
+				$scope.currentReservationId = $scope.data.reservation_list.current_reservations_arr[0].confirmation_num;
+			 	$scope.$broadcast("RESERVATIONDETAILS", $scope.currentReservationId);
+			} else {
+				$scope.currentReservationId = "";
+			 	$scope.$broadcast("RESERVATIONDETAILS", $scope.currentReservationId);
+			}
+			
+		}
+		if(timeline == "upcoming"){
+			$scope.timeline = "upcoming";
+			$scope.reservationList = $scope.data.reservation_list.upcoming_reservations_arr;
+			if($scope.countUpcoming>0){
+				$scope.currentReservationId = $scope.data.reservation_list.upcoming_reservations_arr[0].confirmation_num;
+			 	$scope.$broadcast("RESERVATIONDETAILS", $scope.currentReservationId);
+			} else {
+				$scope.currentReservationId = "";
+			 	$scope.$broadcast("RESERVATIONDETAILS", $scope.currentReservationId);
+			}
+			
+		}
+		if(timeline == "history"){
+			$scope.timeline = "history";
+			$scope.reservationList = $scope.data.reservation_list.history_reservations_arr;
+			if($scope.countHistory > 0){
+				$scope.currentReservationId = $scope.data.reservation_list.history_reservations_arr[0].confirmation_num;
+			 	$scope.$broadcast("RESERVATIONDETAILS", $scope.currentReservationId);
+			} else {
+				$scope.currentReservationId = "";
+			 	$scope.$broadcast("RESERVATIONDETAILS", $scope.currentReservationId);
+			}
+		}
+		
+	 };
+	 /*
+	  * get reservation details on click each reservation
+	  * @param {string} current clicked confirmation number
+	  */
+	 $scope.getReservationDetails = function(currentConfirmationNumber){
+	 	
+	 	$scope.$broadcast("RESERVATIONDETAILS", currentConfirmationNumber);
+	 	$scope.currentReservationId = currentConfirmationNumber;
+	 };
+}]);
--- rover/controllers/rvStaycardController.js
+++ rover/controllers/rvStaycardController.js
@@ -1,9 +1,4 @@
 sntRover.controller('staycardController',['$scope', function($scope){
-	// $scope.stayCardLoading = false;
-	/*
-	 * Show page only after completing API call
-	 */
-	// $scope.$on('showStaycard',function(){
-		// $scope.stayCardLoading = true;
-	// });
+
+	
 }]);
