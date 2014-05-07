bc4936b307d4f8e730174399e579ed37b8d00f23
Vijay Dev <--global>
2014-05-07 21:19:30 +0530

    CICO-6012: update

--- admin/controllers/adFrequentFlyerProgramCtrl.js
+++ admin/controllers/adFrequentFlyerProgramCtrl.js
@@ -2,34 +2,27 @@ admin.controller('ADFrequentFlyerProgramCtrl', ['$scope', '$rootScope', '$state'
 function($scope, $state, $rootScope, $stateParams, ADFrequentFlyerProgramSrv, ngTableParams, $filter) {
 	BaseCtrl.call(this, $scope);
 
-	// var fetchSuccess = function(data) {
-	// 	$scope.data = data;
-	// 	$scope.$emit('hideLoader');
-		
-	// 	$scope.tableParams = new ngTableParams({
-	//         page: 1,            // show first page
-	//         count: $scope.data.frequent_flyer_program.length,    // count per page - Need to change when on pagination implemntation
-	//         sorting: {
-	//             name: 'asc'     // initial sorting
-	//         }
-	// 	},
-	// 	{
-	//         total: $scope.data.frequent_flyer_program.length, // length of data
-	//         getData: function($defer, params) {
-	//             // use build-in angular filter
-	//             var orderedData = params.sorting() ?
-	//                                 $filter('orderBy')($scope.data.frequent_flyer_program, params.orderBy()) :
-	//                                 $scope.data.frequent_flyer_program;
-	//             $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
-	//         }
-	//     });
-	// };
-	// $scope.invokeApi(ADFrequentFlyerProgramSrv.fetch, {}, fetchSuccess);
-
 	$scope.fetchFFP = function() {
 		var callback = function (data) {
-			$scope.$emit('hideLoader');
 			$scope.ffp = data.frequent_flyer_program;
+
+			$scope.tableParams = new ngTableParams({
+		        page: 1,            // show first page
+		        count: $scope.ffp.length,    // count per page - Need to change when on pagination implemntation
+		        sorting: {
+		            name: 'asc'     // initial sorting
+		        }
+			},
+			{
+		        total: $scope.ffp.length, // length of data
+		        getData: function($defer, params) {
+		            // use build-in angular filter
+		            var orderedData = params.sorting() ? $filter('orderBy')($scope.ffp, params.orderBy()) : $scope.ffp;
+		            $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+		        }
+		    });
+
+		    $scope.$emit('hideLoader');
 		}
 
 		$scope.invokeApi(ADFrequentFlyerProgramSrv.fetch, {}, callback);
