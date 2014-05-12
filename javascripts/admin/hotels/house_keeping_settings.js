8b0c15b071a79cbe980bd5815b88e4a4805b2dc7
shijustayntouch <shiju@stayntouch.com>
2014-05-12 16:06:35 +0530

    CICO-5844 - UI fix for manage admin screen.

--- javascripts/admin/hotels/house_keeping_settings.js
+++ javascripts/admin/hotels/house_keeping_settings.js
@@ -3,41 +3,60 @@ var HouseKeepingSettingsView = function(domRef) {
 	this.myDom = domRef;
 	this.currentView = $("body").attr("id");
 	var that = this;
-    this.fileContent = "";
+	this.fileContent = "";
 
 	this.pageinit = function() {
+		that.use_pickup = "false";
+		that.use_inspected = "false";
+		that.checkin_inspected_only = "false";
 	};
 
 	this.delegateEvents = function() {
 		// To unbind all events that happened - CICO-5474 fix
 		that.myDom.on('load').unbind("click");
 		that.myDom.find('#save').on('click', that.saveHouseKeepingSettings);
+		that.myDom.find('#div-use-inspected').on('change', that.disableCheckbox);
 		that.myDom.find('#cancel, #go_back').on('click', that.goBackToPreviousView);
+
+		// Initialize checkbox disabled by default.
 
 	};
 	this.goBackToPreviousView = function() {
 		sntadminapp.gotoPreviousPage(that.viewParams, that.myDom);
 	};
 
+	this.disableCheckbox = function() {
+
+		if (that.myDom.find("#div-use-inspected").hasClass("on")) {
+			console.log("On Enabled...")
+			that.myDom.find("#div-checkin_inspected_only").removeAttr("disabled");
+		} else {
+			that.myDom.find("#div-checkin_inspected_only").closest("label").find("span:eq(0)").removeClass("checked");
+			that.myDom.find("#div-checkin_inspected_only").closest("label").removeClass("checked");
+			that.myDom.find("#div-checkin_inspected_only").removeAttr("checked").attr("disabled", true);
+
+		}
+
+	};
+
 	this.saveHouseKeepingSettings = function() {
 		var extended_checkout = new Array();
 		var postParams = {};
-		use_pickup = "false";
-		use_inspected = "false";
-		checkin_inspected_only = "false"
+
 		if (that.myDom.find("#div-use-pickup").hasClass("on")) {
-			use_pickup = "true";
+			that.use_pickup = "true";
 		}
 		if (that.myDom.find("#div-use-inspected").hasClass("on")) {
-			use_inspected = "true";
+			that.use_inspected = "true";
+
 		}
 		if (that.myDom.find("#div-checkin_inspected_only").is(":checked")) {
-			checkin_inspected_only = "true";
+			that.checkin_inspected_only = "true";
 		}
-		
-		postParams.use_pickup = use_pickup;
-		postParams.use_inspected = use_inspected;
-		postParams.checkin_inspected_only = checkin_inspected_only;
+
+		postParams.use_pickup = that.use_pickup;
+		postParams.use_inspected = that.use_inspected;
+		postParams.checkin_inspected_only = that.checkin_inspected_only;
 
 		var url = '/admin/house_keeping_settings.json';
 		var webservice = new WebServiceInterface();
@@ -53,9 +72,10 @@ var HouseKeepingSettingsView = function(domRef) {
 	// To handle success on save API
 	this.fetchCompletedOfSave = function() {
 		sntapp.notification.showSuccessMessage("Saved successfully", that.myDom);
+		that.goBackToPreviousView();
 	};
 	// To handle failure on save API
 	this.fetchFailedOfSave = function(errorMessage) {
 		sntapp.notification.showErrorMessage(errorMessage, that.myDom);
 	};
-}; 
+};
