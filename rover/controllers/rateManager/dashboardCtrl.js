063763174a63c889cc4a0def9712c5a46ad20aef
krishobh <krishobh@stayntouch.com>
2014-05-13 15:16:51 +0530

    CICO-3997 Implemneted scope data in parent controller

--- rover/controllers/rateManager/dashboardCtrl.js
+++ rover/controllers/rateManager/dashboardCtrl.js
@@ -2,14 +2,26 @@ sntRover.controller('RMDashboradCtrl', ['$scope', function($scope){
 	
 	BaseCtrl.call(this, $scope);
 	$scope.displayMode = "CALENDAR";
-
-
+	
+	$scope.currentFilterData =	{
+           begin_date : new Date(),
+           end_date : new Date(),
+           zoom_level : [{"value": "3","name": "3 days"},{"value": "4","name": "4 days"},{"value": "5","name": "5 days"},{"value": "6","name": "6 days"},{"value": "7","name": "7 days"}],
+           zoom_level_selected : '',
+           is_checked_all_rates : 'true',
+           rate_types: [],
+           rate_type_selected : '',
+           rates : [],
+           selectedRatesList : [],
+           nameOnCards : []
+   	};
+   	
 	$scope.showCalendarView = function(){
 		$scope.displayMode = "CALENDAR";
-	}
+	};
 
 	$scope.showGraphView = function(){
 		$scope.displayMode = "GRAPH";
-	}
+	};
   
 }]);
