e6401e36bd8ad495c96461dc0d07140f3911ec63
krishobh <krishobh@stayntouch.com>
2014-05-13 20:36:46 +0530

    CICO-6045 Implemented sample json for contracts

--- rover/controllers/rvCompanyCardContractsCtrl.js
+++ rover/controllers/rvCompanyCardContractsCtrl.js
@@ -1,16 +1,16 @@
-sntRover.controller('companyCardContractsCtrl',['$scope', 'companyCardContractsSrv', '$stateParams', function($scope, companyCardContractsSrv, $stateParams){
-	
-	
+sntRover.controller('companyCardContractsCtrl',['$scope', 'RVCompanyCardSrv', '$stateParams', function($scope, RVCompanyCardSrv, $stateParams){
+
 	$scope.fetchData = function(){   
   	    var fetchSuccessCallback = function(data){
   	    	console.log("sucss");
   	    	console.log(data);
+  	    	$scope.contractsData = data;
   	    	
   	    };
   	    var fetchFailureCallback = function(data){
   	        $scope.$emit('hideLoader');
   	    };
-  	    $scope.invokeApi(companyCardContractsSrv.fetch,{},fetchSuccessCallback,fetchFailureCallback);  
+  	    $scope.invokeApi(RVCompanyCardSrv.fetchContracts,{},fetchSuccessCallback,fetchFailureCallback);  
 
     };
 	$scope.fetchData();
