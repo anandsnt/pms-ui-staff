ac285c0532f7e76cdafdc10071efd2f0c6a54554
Resheil <resheil@stayntouch.com>
2014-05-15 18:09:30 +0530

    CICO-5922 tab actions minor change

--- rover/controllers/rvContactInfoController.js
+++ rover/controllers/rvContactInfoController.js
@@ -5,6 +5,7 @@ sntRover.controller('RVContactInfoController',['$scope','RVContactInfoSrv','ngDi
   */
 var presentContactInfo = JSON.parse(JSON.stringify($scope.guestCardData.contactInfo));
 $scope.errorMessage = "";
+
 $scope.saveContactInfo = function(){
     var saveUserInfoSuccessCallback = function(data){
         $scope.$emit('hideLoader');
@@ -38,7 +39,7 @@ $scope.saveContactInfo = function(){
   * watch and update formatted date for display
   */
 $scope.$watch('guestCardData.contactInfo.birthday',function(){
-$scope.birthdayText = JSON.parse(JSON.stringify(dateFilter($scope.guestCardData.contactInfo.birthday, 'MM-dd-yyyy')));
+	$scope.birthdayText = JSON.parse(JSON.stringify(dateFilter($scope.guestCardData.contactInfo.birthday, 'MM-dd-yyyy')));
 });
 /**
   * to handle click actins outside this tab
