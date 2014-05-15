ee34f34343aa3bb3ec69834e26adb0c4b720dc80
akhilastayntouch <akhila@stayntouch.com>
2014-05-15 11:16:58 +0530

    CICO-3997 added rstriction listing in the add restriction dialog

--- rover/controllers/rateManager/updatePriceAndRestrictionsCtrl.js
+++ rover/controllers/rateManager/updatePriceAndRestrictionsCtrl.js
@@ -1,10 +1,24 @@
 sntRover.controller('UpdatePriceAndRestrictionsCtrl', ['$q', '$scope', 'ngDialog',
     function ($q, $scope, ngDialog) {
     	console.log($scope.calendarData);
+    	console.log($scope.currentlySelectedDate);
+    	console.log($scope.currentlySelectedRate);
+
+
 
         $scope.hideUpdatePriceAndRestrictionsDialog = function(){
             console.log('reached::hideUpdatePriceAndRestrictionsDialog');
             ngDialog.close();
-        }
+        };
+
+        $scope.isRestrictionOn = function(rateRestrictions, searchRestrictionKey){
+        	var ret = false;
+        	for(var i=0 in rateRestrictions){
+        		if(rateRestrictions[i].restriction_type_id == searchRestrictionKey){
+        			ret = true;
+        		}
+        	}
+        	return ret;
+        };
     }
 ]);
