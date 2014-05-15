e3f90b67edf39ec0b53859e41fa32baaad5f0ca0
Vijay Dev <--global>
2014-05-15 20:12:51 +0530

    CICO-6021: updates and corrections

--- admin/controllers/adRulesRestrictionCtrl.js
+++ admin/controllers/adRulesRestrictionCtrl.js
@@ -135,7 +135,7 @@ admin.controller('ADRulesRestrictionCtrl', [
                 $scope.showCancelForm = false;
                 $scope.showDepositForm = true;
 
-                $scope.rulesSubtitle = 'Deposit Request Rule';
+                $scope.rulesSubtitle = 'Deposit Request';
 
                 $scope.singleRule = {};
                 $scope.singleRule.policy_type = 'DEPOSIT_REQUEST';
@@ -150,10 +150,22 @@ admin.controller('ADRulesRestrictionCtrl', [
             var callback = function(data) {
                 
                 // clear any previous data
-                $scope.singleRule = {};
+                $scope.singleRule = data;
 
+                console.log( $scope.singleRule.amount );
+                var amtString = $scope.singleRule.amount + '',
+                    num       = amtString.split('.')[0],
+                    dec       = amtString.split('.')[1] * 1;
+
+                if ( dec < 9 ) {
+                    dec += '0';
+                };
+
+                $scope.singleRule.amount = num + '.' + dec;
+
+                console.log( $scope.singleRule.amount );
+;
                 if ( from === 'Cancellation Penalties' ) {
-                    $scope.singleRule = data;
                     $scope.singleRule.policy_type = 'CANCELLATION_POLICY';
 
                     // need to split HH:MM into individual keys
@@ -190,7 +202,6 @@ admin.controller('ADRulesRestrictionCtrl', [
                 }
 
                 if ( from === 'Deposit Requests' ) {
-                    $scope.singleRule = data;
                     $scope.singleRule.policy_type = 'DEPOSIT_REQUEST';
 
                     $scope.showCancelForm = false;
