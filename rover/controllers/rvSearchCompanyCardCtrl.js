86cda22bef48d963adf76dd3b840f7f3eb9f6a8f
krishobh <krishobh@stayntouch.com>
2014-05-16 12:07:15 +0530

    CICO-6045 added scroll to results

--- rover/controllers/rvSearchCompanyCardCtrl.js
+++ rover/controllers/rvSearchCompanyCardCtrl.js
@@ -9,8 +9,24 @@ sntRover.controller('searchCompanyCardController',['$scope', 'RVCompanyCardSearc
 	var successCallBackofInitialFetch = function(data){
 		$scope.$emit("hideLoader");
 		$scope.results = data.accounts;
+		setTimeout(function(){refreshScroller();}, 750);
 	}
+	/**
+  	* function used for refreshing the scroller
+  	*/
+  	var refreshScroller = function(){
 
+	    $scope.$parent.myScroll['comapany_card_scroll'].refresh();
+	    //scroller options
+	    $scope.$parent.myScrollOptions = {
+	        snap: false,
+	        scrollbars: true,
+	        bounce: true,
+	        vScroll: true,
+	        vScrollbar: true,
+	        hideScrollbar: false
+	    };
+  	};
   	//function that converts a null value to a desired string.
 
 	//if no replace value is passed, it returns an empty string
@@ -29,10 +45,12 @@ sntRover.controller('searchCompanyCardController',['$scope', 'RVCompanyCardSearc
   	* function to perform filtering/request data from service in change event of query box
   	*/
 	$scope.queryEntered = function(){
-	    //setting the heading of the screen to "Search"
-	    //$scope.heading = headingListDict['']; 
-		console.log("query enterd");
-	    displayFilteredResults();  
+		if($scope.textInQueryBox === ""){
+			$scope.results = [];
+		}
+		else{
+	    	displayFilteredResults();  
+	   	}
   	};
   
 	$scope.clearResults = function(){
@@ -40,46 +58,46 @@ sntRover.controller('searchCompanyCardController',['$scope', 'RVCompanyCardSearc
 	};
   	
   	/**
-  * function to perform filering on results.
-  * if not fouund in the data, it will request for webservice
-  */
-  var displayFilteredResults = function(){ 
-    //if the entered text's length < 3, we will show everything, means no filtering    
-    if($scope.textInQueryBox.length < 3){
-      //based on 'is_row_visible' parameter we are showing the data in the template      
-      for(var i = 0; i < $scope.results.length; i++){
-          $scope.results[i].is_row_visible = true;
-      }     
-      
-      // we have changed data, so we are refreshing the scrollerbar
-      //refreshScroller();      
-    }
-    else{
-      var value = ""; 
-      var visibleElementsCount = 0;
-      //searching in the data we have, we are using a variable 'visibleElementsCount' to track matching
-      //if it is zero, then we will request for webservice
-      for(var i = 0; i < $scope.results.length; i++){
-        value = $scope.results[i];
-        if (($scope.escapeNull(value.account_first_name).toUpperCase()).indexOf($scope.textInQueryBox.toUpperCase()) >= 0 || 
-            ($scope.escapeNull(value.account_last_name).toUpperCase()).indexOf($scope.textInQueryBox.toUpperCase()) >= 0 ) 
-            {
-               $scope.results[i].is_row_visible = true;
-               visibleElementsCount++;
-            }
-        else {
-          $scope.results[i].is_row_visible = false;
-        }
-              
-      }
-      // last hope, we are looking in webservice.      
-     if(visibleElementsCount == 0){    
-        var dataDict = {'query': $scope.textInQueryBox.trim()};
-        $scope.invokeApi(RVCompanyCardSearchSrv.fetch, dataDict, successCallBackofInitialFetch);
-      }
-      // we have changed data, so we are refreshing the scrollerbar
-      //refreshScroller();                  
-    }
+  	* function to perform filering on results.
+  	* if not fouund in the data, it will request for webservice
+  	*/
+  	var displayFilteredResults = function(){ 
+	    //if the entered text's length < 3, we will show everything, means no filtering    
+	    if($scope.textInQueryBox.length < 3){
+	      //based on 'is_row_visible' parameter we are showing the data in the template      
+	      for(var i = 0; i < $scope.results.length; i++){
+	          $scope.results[i].is_row_visible = true;
+	      }     
+	      
+	      // we have changed data, so we are refreshing the scrollerbar
+	      refreshScroller();      
+	    }
+	    else{
+	      var value = ""; 
+	      var visibleElementsCount = 0;
+	      //searching in the data we have, we are using a variable 'visibleElementsCount' to track matching
+	      //if it is zero, then we will request for webservice
+	      for(var i = 0; i < $scope.results.length; i++){
+	        value = $scope.results[i];
+	        if (($scope.escapeNull(value.account_first_name).toUpperCase()).indexOf($scope.textInQueryBox.toUpperCase()) >= 0 || 
+	            ($scope.escapeNull(value.account_last_name).toUpperCase()).indexOf($scope.textInQueryBox.toUpperCase()) >= 0 ) 
+	            {
+	               $scope.results[i].is_row_visible = true;
+	               visibleElementsCount++;
+	            }
+	        else {
+	          $scope.results[i].is_row_visible = false;
+	        }
+	              
+	      }
+	      // last hope, we are looking in webservice.      
+	     if(visibleElementsCount == 0){    
+	        var dataDict = {'query': $scope.textInQueryBox.trim()};
+	        $scope.invokeApi(RVCompanyCardSearchSrv.fetch, dataDict, successCallBackofInitialFetch);
+	      }
+	      // we have changed data, so we are refreshing the scrollerbar
+	      refreshScroller();                  
+	    }
   };
 
 }]);
