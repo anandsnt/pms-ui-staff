885746008ef331f124eab94c8b753cbe607c17b8
Resheil <resheil@stayntouch.com>
2014-05-16 14:27:05 +0530

    CICO-5922 fixing issues in staycard

--- rover/controllers/rvStaycardController.js
+++ rover/controllers/rvStaycardController.js
@@ -1,13 +1,20 @@
 sntRover.controller('staycardController',['$scope', function($scope){
 
-
+		
 		 $scope.guestCardData ={};
+		 $scope.guestCardData.contactInfo = {};
+		 $scope.countriesList = [];
+		 $scope.guestCardData.userId = '';
+		 $scope.guestCardData.contactInfo.birthday = '';
+
 		 $scope.$on('guestCardUpdateData',function(event, data){
 		 	$scope.guestCardData.contactInfo = data.data;
 		 	$scope.countriesList = data.countries;
 		 	$scope.guestCardData.userId=data.userId;
-		
+	
 		 });
+
+
 
 	 //setting the heading of the screen to "Search"
     $scope.heading = "Stay Card";
