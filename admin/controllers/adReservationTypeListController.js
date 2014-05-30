f7b7938ed2db24ae6992abf927675e15c09a3ad4
Aswanth <aswanth@stayntouch.com>
2014-05-30 14:06:58 +0530

    CICO-6043 added language translation and api integrations

--- admin/controllers/adReservationTypeListController.js
+++ admin/controllers/adReservationTypeListController.js
@@ -1,9 +1,26 @@
-admin.controller('ADRateTypeCtrl', ['$scope', '$rootScope', 'ADRateTypeSrv', 'ADRatesSrv',
-function($scope, $rootScope, ADRateTypeSrv, ADRatesSrv) {
+admin.controller('ADReservationTypeListController', ['$scope', '$rootScope', 'ADReservationTypesSrv',
+function($scope, $rootScope, ADReservationTypesSrv) {
 	$scope.errorMessage = '';
 	BaseCtrl.call(this, $scope);
+	$scope.data = [];
 
-	
-
+	$scope.listReservationTypes = function(){
+		
+		var successCallbackReservationTypes = function(data){
+			$scope.$emit('hideLoader');
+			$scope.data = data.reservation_types;
+		};
+	   $scope.invokeApi(ADReservationTypesSrv.fetch, {} , successCallbackReservationTypes);	
+	};
+	$scope.listReservationTypes(); 
+	$scope.saveReservationType = function(index){
+		var params = {};
+		params.id = $scope.data[index].value;
+		params.is_active = $scope.data[index].is_active;
+		var successCallbackSetReservationType = function(data){
+			$scope.$emit('hideLoader');
+		};
+	   $scope.invokeApi(ADReservationTypesSrv.save, params, successCallbackSetReservationType);	
+	};
 
 }]);
