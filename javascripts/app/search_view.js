062f83b44c40a08e27e989b46f6cd75d62614fe2
shijustayntouch <shiju@stayntouch.com>
2014-06-02 11:51:22 +0530

    CICO-6662 - UI - Place holders added for Queue Reservation Indicators.

--- javascripts/app/search_view.js
+++ javascripts/app/search_view.js
@@ -418,7 +418,7 @@ var Search = function(domRef) {
 			$.each(searchResults, function(i, value) {
 
 				if ((escapeNull(value.firstname).toUpperCase()).indexOf($query.toUpperCase()) >= 0 || (escapeNull(value.lastname).toUpperCase()).indexOf($query.toUpperCase()) >= 0 || (escapeNull(value.group).toUpperCase()).indexOf($query.toUpperCase()) >= 0 || (escapeNull(value.room).toString()).indexOf($query) >= 0 || (escapeNull(value.confirmation).toString()).indexOf($query) >= 0) {
-					items.push($('<li />').html(that.writeSearchResult(value.id, value.firstname, value.lastname, value.image, value.confirmation, value.reservation_status, value.room, value.roomstatus, value.fostatus, value.location, value.group, value.vip, value.late_checkout_time, value.is_opted_late_checkout, value.room_ready_status, value.use_pickup, value.use_inspected, value.checkin_inspected_only)));
+					items.push($('<li />').html(that.writeSearchResult(value.id, value.firstname, value.lastname, value.image, value.confirmation, value.reservation_status, value.room, value.roomstatus, value.fostatus, value.location, value.group, value.vip, value.late_checkout_time, value.is_opted_late_checkout, value.room_ready_status, value.use_pickup, value.use_inspected, value.checkin_inspected_only, value.is_reservation_queued)));
 				}
 
 			});
@@ -446,7 +446,7 @@ var Search = function(domRef) {
 			var items = [];
 			$.each(response, function(i, value) {
 
-				items.push($('<li />').html(that.writeSearchResult(value.id, value.firstname, value.lastname, value.image, value.confirmation, value.reservation_status, value.room, value.roomstatus, value.fostatus, value.location, value.group, value.vip, value.late_checkout_time, value.is_opted_late_checkout, value.room_ready_status, value.use_pickup, value.use_inspected, value.checkin_inspected_only)));
+				items.push($('<li />').html(that.writeSearchResult(value.id, value.firstname, value.lastname, value.image, value.confirmation, value.reservation_status, value.room, value.roomstatus, value.fostatus, value.location, value.group, value.vip, value.late_checkout_time, value.is_opted_late_checkout, value.room_ready_status, value.use_pickup, value.use_inspected, value.checkin_inspected_only, value.is_reservation_queued)));
 
 				$('#search-results').removeAttr('style').append.apply($('#search-results'), items).highlight($query);
 			});
@@ -464,7 +464,7 @@ var Search = function(domRef) {
 		sntapp.activityIndicator.hideActivityIndicator('loader-html-appending');
 	};
 
-	this.writeSearchResult = function(id, firstname, lastname, image, confirmation, reservation_status, room, roomstatus, foStatus, location, group, vip, lateCheckoutTime, isLateCheckoutOn, room_ready_status, use_pickup, use_inspected, checkin_inspected_only) {
+	this.writeSearchResult = function(id, firstname, lastname, image, confirmation, reservation_status, room, roomstatus, foStatus, location, group, vip, lateCheckoutTime, isLateCheckoutOn, room_ready_status, use_pickup, use_inspected, checkin_inspected_only, is_reservation_queued) {
 		var guestStatusIcon = this.getGuestStatusMapped(reservation_status, isLateCheckoutOn);
 		var roomStatusMapped = this.getRoomStatusMapped(roomstatus, foStatus);
 
@@ -490,8 +490,7 @@ var Search = function(domRef) {
 		} else {
 			roomStatus = '<strong class="room-number">' + escapeNull(room) + '</strong>';
 		}
-
-		var $location = (escapeNull(location) != '') ? '<span class="icons icon-location">' + escapeNull(location) + '</span>' : '', $group = (escapeNull(group) != '') ? '<em class="icons icon-group">' + escapeNull(group) + '</em>' : '', $vip = vip ? '<span class="vip">VIP</span>' : '', $image = (escapeNull(image) != '') ? '<figure class="guest-image"><img src="' + escapeNull(image) + '" />' + $vip + '</figure>' : '<figure class="guest-image"><img src="/assets/blank-avatar.png" />' + $vip + '</figure>', $roomAdditional = showRoomStatus ? '<span class="room-status">' + roomstatusexplained + '</span>' : '', $viewStatus = guestStatusIcon ? '<span class="guest-status ' + escapeNull(guestStatusIcon) + '"></span>' : '<span class="guest-status"></span>', $lateCheckoutStatus = (escapeNull(lateCheckoutTime) == "" || "CHECKING_OUT" != reservation_status || isLateCheckoutOn == false) ? "" : '<span class="late-checkout-time">' + escapeNull(lateCheckoutTime) + '</span>', $guestViewIcons = '<div class="status">' + $lateCheckoutStatus + $viewStatus + '</div>';
+		var $location = (escapeNull(location) != '') ? '<span class="icons icon-location">' + escapeNull(location) + '</span>' : '', $group = (escapeNull(group) != '') ? '<em class="icons icon-group">' + escapeNull(group) + '</em>' : '', $vip = vip ? '<span class="vip">VIP</span>' : '', $image = (escapeNull(image) != '') ? '<figure class="guest-image"><img src="' + escapeNull(image) + '" />' + $vip + '</figure>' : '<figure class="guest-image"><img src="/assets/blank-avatar.png" />' + $vip + '</figure>', $roomAdditional = showRoomStatus ? '<span class="room-status">' + roomstatusexplained + '</span>' : '', $viewStatus = guestStatusIcon ? '<span class="guest-status ' + escapeNull(guestStatusIcon) + '"></span><span>'+is_reservation_queued+'</span>' : '<span class="guest-status"></span>', $lateCheckoutStatus = (escapeNull(lateCheckoutTime) == "" || "CHECKING_OUT" != reservation_status || isLateCheckoutOn == false) ? "" : '<span class="late-checkout-time">' + escapeNull(lateCheckoutTime) + '</span>', $guestViewIcons = '<div class="status">' + $lateCheckoutStatus + $viewStatus + '</div>';
 		$output = '<a href="staff/staycards/staycard?confirmation=' + confirmation + '&id=' + escapeNull(id) + '" class="guest-check-in link-item float" data-transition="inner-page">' + $image + '<div class="data">' + '<h2>' + escapeNull(lastname) + ', ' + escapeNull(firstname) + '</h2>' + '<span class="confirmation">' + escapeNull(confirmation) + '</span>' + $location + $group + '</div>' + $guestViewIcons + roomStatus + $roomAdditional + '</a>';
 		return $output;
 	};
