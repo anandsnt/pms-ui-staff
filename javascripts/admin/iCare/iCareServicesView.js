7ecdc2e4df6e6cb17589586d34fe52ead09ea21f
shahulqb <shahulp@qburst.com>
2014-06-03 13:54:24 +0530

    CICO-6362 Admin interface & API interface for icare service completed

--- javascripts/admin/iCare/iCareServicesView.js
+++ javascripts/admin/iCare/iCareServicesView.js
@@ -6,21 +6,32 @@ var ICareServicesView = function(domRef){
 
 	this.delegateEvents = function() {		
 		that.myDom.find('#save_icare_service').on('click', that.saveICareService);
-		
+		that.myDom.find('#cancel').on('click', that.goBackToPreviousView);
 	};
+
+	this.goBackToPreviousView = function() {
+ 	  sntadminapp.gotoPreviousPage(that.viewParams, that.myDom);
+  	};
+
+  	this.successCallbackOfSaveAction = function(data){
+  		that.goBackToPreviousView();
+  	}
 
 	this.saveICareService = function(){
 		var dataToPost = {};
+		dataToPost.icare = {};
+		dataToPost.icare.enabled = that.myDom.find('#icare-service-toggle').is(":checked");
+		dataToPost.icare.debit_charge_code_id = that.myDom.find('#debit-charge-group').val();
+		dataToPost.icare.credit_charge_code_id = that.myDom.find('#credit-charge-group').val();
 		var webservice = new NewWebServiceInterface();
-		var url = '/';
+		var url = '/api/hotel_settings/change_settings';
 	    var options = { 
 			requestParameters: dataToPost,
 			successCallBack: that.successCallbackOfSaveAction,
-			failureCallBack: that.failureCallbackOfSaveAction,
 			loader: 'blocker',
 			async: false
 	    };
 		// we prepared, we shooted!!	    			
-	    webservice.putJSON(url, options);	
+	    webservice.postJSON(url, options);	
 	}
 };
