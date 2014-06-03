aa0057e1f84e4f0633705f582a696d4e219726ae
Vijay Dev <--global>
2014-06-03 18:30:16 +0530

    CICO-5933: DONE - item listing, search by query, search by group

--- rover/controllers/rvPostChargeCtrl.js
+++ rover/controllers/rvPostChargeCtrl.js
@@ -2,17 +2,16 @@ sntRover.controller('RVPostChargeController',
 	[
 		'$rootScope',
 		'$scope',
-		
-		function($rootScope, $scope) {
-			
+		'rvChargeItems',
+
+		function($rootScope, $scope, rvChargeItems) {
+
 			// hook up the basic things
-			BaseCtrl.call(this, $scope);
+			BaseCtrl.call( this, $scope );
 
-
-
-			// get the data 
-			// https://pms-dev.stayntouch.com/staff/items/35980/get_items.json
-			// resv id
+			// its wired in a way that this modal will be
+			// created only when the data is fetched
+			console.log( $scope.fetchedData );
 
 			// post the data
 			// https://pms-dev.stayntouch.com/staff/items/post_items_to_bill
@@ -28,6 +27,43 @@ sntRover.controller('RVPostChargeController',
 			// 	items[1][amount]:7.00
 			// 	items[1][quantity]:1
 			// }
+
+			// filter the items based on the chosen charge group
+			$scope.filterbyChargeGroup = function() {
+				for (var i = 0, j = $scope.fetchedData.items.length; i < j; i++) {
+					var item = $scope.fetchedData.items[i];
+
+					if ( !$scope.chargeGroup || item.charge_group_value === $scope.chargeGroup ) {
+						item.show = true;
+					} else {
+						item.show = false;
+					}
+				}
+			};
+
+			// filter the items based on the search query
+			// will search on all items, discard chosen 'chargeGroup'
+			$scope.filterByQuery = function() {
+				var query = $scope.query.toLowerCase();
+
+				$scope.chargeGroup = '';
+
+				for (var i = 0, j = $scope.fetchedData.items.length; i < j; i++) {
+					var item = $scope.fetchedData.items[i];
+
+					// let show all items
+					item.show = true;
+
+					// find
+					if( item.item_name.toLowerCase().indexOf(query) >= 0 ) {
+						item.show = true;
+					} else {
+						item.show = false;
+					}
+				};
+			};
 		}
 	]
 );
+
+// $scope.reservationData.reservation_card.reservation_id
