791ca9faf5cd7e29fb043b1c211190b85892d803
shahulqb <shahulp@qburst.com>
2014-06-03 19:57:44 +0530

    CICO-5217 Resizable implemented for top & left drawers

--- rover/controllers/reservation/settingsCtrl.js
+++ rover/controllers/reservation/settingsCtrl.js
@@ -1,3 +1,20 @@
 sntRover.controller('ReservationSettingsCtrl', ['$scope', function($scope){
     
+    $scope.ReservationSettingsVisible = false;
+    /**
+	* scroller options
+	*/
+	$scope.resizableOptions = 
+	{	
+		minWidth: '10',
+		maxWidth: '260',
+		handles: 'e',
+		resize: function( event, ui ) {
+			
+		},
+		stop: function(event, ui){
+			preventClicking = true;
+			$scope.eventTimestamp = event.timeStamp;
+		}
+	}
 }]);
