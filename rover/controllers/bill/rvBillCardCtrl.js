e8991b7199d49aa11f64c2ffd2d2eb2c8e3bb629
soumya <soumya@stayntouch.com>
2014-06-05 18:35:40 +0530

    CICO-5930 Move to another bill

--- rover/controllers/bill/rvBillCardCtrl.js
+++ rover/controllers/bill/rvBillCardCtrl.js
@@ -1,16 +1,38 @@
-sntRover.controller('RVbillCardController',['$scope','$state','RVBillCardSrv','reservationBillData', function($scope,$state, RVBillCardSrv, reservationBillData){
+sntRover.controller('RVbillCardController',['$scope','$rootScope','$state','RVBillCardSrv','reservationBillData', function($scope,$rootScope,$state, RVBillCardSrv, reservationBillData){
 	
 	BaseCtrl.call(this, $scope);
-	$scope.reservationBillData = reservationBillData;
-	$scope.routingArrayCount = $scope.reservationBillData.routing_array.length;
-	$scope.incomingRoutingArrayCount = $scope.reservationBillData.routing_array.length;
-	//Variables used to calculate height of the wrapper.To do scroll refresh
-	var countFeesElements = parseInt(reservationBillData.bills[0].total_fees[0].fees_details.length)+parseInt(5);//1 - For heading, 2 for totl fees and balance, 2 for guest balnce and creditcard
+	var countFeesElements = 0;//1 - For heading, 2 for totl fees and balance, 2 for guest balnce and creditcard
 	var roomTypeDescriptionLength = parseInt(150); //Approximate height
-	var billTabHeight=parseInt(35);
-	var calenderDaysHeight=parseInt(35);
-	var totalHeight = parseInt(countFeesElements*64)+calenderDaysHeight+billTabHeight+roomTypeDescriptionLength;
-	$scope.calculatedHeight = totalHeight;
+	var billTabHeight = parseInt(35);
+	var calenderDaysHeight = parseInt(35);
+	var totalHeight = 0;
+	$scope.init = function(reservationBillData){
+		/*
+		 * Adding biilValue and oldBillValue with data. Adding with each bills fees details
+		 * To handle move to bill action
+		 * Added same value to two different key because angular is two way binding
+		 * Check in HTML moveToBillAction
+		 */
+		angular.forEach(reservationBillData.bills, function(value, key) {
+			value.isOpenFeesDetails = false;
+			if(key == 0){
+				value.isOpenFeesDetails = true;
+			}
+	        angular.forEach(value.total_fees[0].fees_details, function(feesValue, feesKey) {
+	        	feesValue.billValue = value.bill_number;//Bill value append with bill details
+	        	feesValue.oldBillValue = value.bill_number;// oldBillValue used to identify the old billnumber
+	     	});
+	     });
+		$scope.reservationBillData = reservationBillData;
+		$scope.routingArrayCount = $scope.reservationBillData.routing_array.length;
+		$scope.incomingRoutingArrayCount = $scope.reservationBillData.routing_array.length;
+		//Variables used to calculate height of the wrapper.To do scroll refresh
+		countFeesElements = parseInt(reservationBillData.bills[0].total_fees[0].fees_details.length)+parseInt(5);//1 - For heading, 2 for totl fees and balance, 2 for guest balnce and creditcard
+		var totalHeight = parseInt(countFeesElements*64)+calenderDaysHeight+billTabHeight+roomTypeDescriptionLength;
+		$scope.calculatedHeight = totalHeight;
+	};
+	$scope.init(reservationBillData);
+	
 	//Scope variable to set active bill
 	$scope.currentActiveBill = 0;
 	//Scope variable used for show/hide rate per day when clicks on each day in calender 
@@ -22,6 +44,8 @@ sntRover.controller('RVbillCardController',['$scope','$state','RVBillCardSrv','r
 	//Scope variable used to show room details
 	$scope.showRoomDetailsIndex = -1;
 	$scope.showActiveBillFeesDetails = 0;
+	$scope.showFeesDetails = true;
+	$scope.moveToBill = 0;
 	/*
 	 * Adding class for active bill
 	 */
@@ -111,19 +135,51 @@ sntRover.controller('RVbillCardController',['$scope','$state','RVBillCardSrv','r
 	 		isShowNotDefined = false;
 	 	}
 	 };
-	
+	 $scope.toggleFeesDetails = function(){
+	 	
+	 };
+	 /*
+	  * Success callback of fetch - After moving fees item from one bill to another
+	  */
+	 $scope.fetchSuccessCallback = function(data){
+	 	$scope.$emit('hideLoader');
+	 	$scope.init(data);
+	 };
+	 /*
+	  * MOve fees item from one bill to another
+	  * @param {int} old Bill Value
+	  * @param {int} fees index
+	  */
+	 $scope.moveToBillAction = function(oldBillValue, feesIndex){
+	 	var parseOldBillValue = parseInt(oldBillValue)-1;
+		var newBillValue = $scope.reservationBillData.bills[parseOldBillValue].total_fees[0].fees_details[feesIndex].billValue;
+		var transactionId = $scope.reservationBillData.bills[parseOldBillValue].total_fees[0].fees_details[feesIndex].transaction_id;
+		var dataToMove = {
+			"reservation_id" : $scope.reservationBillData.reservation_id,
+			"to_bill" : newBillValue,
+			"from_bill" : oldBillValue,
+			"transaction_id" : transactionId
+		};
+		/*
+		 * Success Callback of move action
+		 */
+		var moveToBillSuccessCallback = function(data){
+			$scope.$emit('hideLoader');
+			//Fetch data again to refresh the screen with new data
+			$scope.invokeApi(RVBillCardSrv.fetch, $scope.reservationBillData.reservation_id, $scope.fetchSuccessCallback);
+		};
+		$scope.invokeApi(RVBillCardSrv.movetToAnotherBill, dataToMove, moveToBillSuccessCallback);  
+	 };
 		
-		 $scope.$parent.myScrollOptions = {		
-			    'registration-content': {
-			    	scrollbars: true,
-			        snap: false,
-			        hideScrollbar: false,
-			        preventDefault: false
-			    }
-			};
-	
-	
-	$scope.$on('$viewContentLoaded', function() {
+	 $scope.$parent.myScrollOptions = {		
+		    'registration-content': {
+		    	scrollbars: true,
+		        snap: false,
+		        hideScrollbar: false,
+		        preventDefault: false
+		    }
+		};
+	 $scope.$on('$viewContentLoaded', function() {
 	
 		setTimeout(function(){
 			$scope.$parent.myScroll['registration-content'].refresh();
--- rover/services/bill/rvBillCardSrv.js
+++ rover/services/bill/rvBillCardSrv.js
@@ -3,9 +3,21 @@ sntRover.service('RVBillCardSrv',['$http', '$q', 'BaseWebSrvV2', function($http,
 	
 	this.fetch = function(reservationId){
 		var deferred = $q.defer();
-			console.log("----------KKKKKK=---------")	
 		var url = '/staff/reservation/bill_card.json?reservation_id='+reservationId;
 			BaseWebSrvV2.getJSON(url).then(function(data) {
+				
+			   	 deferred.resolve(data);
+			},function(data){
+			    deferred.reject(data);
+			});	
+
+		return deferred.promise;
+	};
+	
+	this.movetToAnotherBill = function(data){
+		var deferred = $q.defer();
+		var url = '/staff/bills/transfer_transaction';
+			BaseWebSrvV2.postJSON(url, data).then(function(data) {
 				
 			   	 deferred.resolve(data);
 			},function(data){
