b448e3f29286005e56a2d0ff281ca96ae524330e
Jonathan Herr <jonathan@stayntouch.com>
2014-06-06 15:44:15 -0400

    CICO-6040 Rename the accounts search to index.  Add more request parameters and response parameters to it so it works for the reservation creation.

--- rover/services/rmFilterOptionsSrv.js
+++ rover/services/rmFilterOptionsSrv.js
@@ -1,12 +1,12 @@
 sntRover.service('RMFilterOptionsSrv',['$q', 'BaseWebSrvV2', function( $q, RVBaseWebSrv){
-    
+
     /*
     * To fetch filter options
     * @return {object} filter options
     */
     this.fetchRates = function(){
             var deferred = $q.defer();
-            var url =  '/api/rates';    
+            var url =  '/api/rates';
             RVBaseWebSrv.getJSON(url).then(function(data) {
                 deferred.resolve(data);
             },function(data){
@@ -16,7 +16,7 @@ sntRover.service('RMFilterOptionsSrv',['$q', 'BaseWebSrvV2', function( $q, RVBas
     };
     this.fetchRateTypes = function(){
             var deferred = $q.defer();
-            var url =  '/api/rate_types/active';    
+            var url =  '/api/rate_types/active';
             RVBaseWebSrv.getJSON(url).then(function(data) {
                 deferred.resolve(data);
             },function(data){
@@ -26,14 +26,14 @@ sntRover.service('RMFilterOptionsSrv',['$q', 'BaseWebSrvV2', function( $q, RVBas
     };
 
     this.fetchCompanyCard = function(data){
-        var deferred = $q.defer();      
-        var url =  '/api/accounts/search_account';  
-        RVBaseWebSrv.postJSON(url,data).then(function(data) {
+        var deferred = $q.defer();
+        var url =  '/api/accounts';
+        RVBaseWebSrv.getJSON(url,data).then(function(data) {
             deferred.resolve(data);
         },function(data){
             deferred.reject(data);
         });
-        return deferred.promise;        
+        return deferred.promise;
     };
 
 
