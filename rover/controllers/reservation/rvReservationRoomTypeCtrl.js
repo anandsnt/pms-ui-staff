48c5974d2fce3fd39e0c54deecc0afe835d0bf4f
sdilipsam <dilips@stayntouch.com>
2014-06-10 10:45:43 +0530

    CICO-5218 Rate calculation implemented

--- rover/controllers/reservation/rvReservationRoomTypeCtrl.js
+++ rover/controllers/reservation/rvReservationRoomTypeCtrl.js
@@ -5,7 +5,6 @@ sntRover.controller('RVReservationRoomTypeCtrl', ['$rootScope', '$scope', 'roomR
 		$scope.displayData = {};
 		$scope.selectedRoomType = -1;
 		$scope.expandedRoom = -1;
-		$scope.displayData.stay = [];
 		$scope.containerHeight = 300;
 
 		//scroller options
@@ -22,40 +21,33 @@ sntRover.controller('RVReservationRoomTypeCtrl', ['$rootScope', '$scope', 'roomR
 
 		var init = function() {
 
+			console.log($scope.reservationData);
 			//defaults and hardcoded values
 			$scope.tax = roomRates.tax || 20;
 			$scope.rooms = roomRates.rooms;
 			$scope.activeRoom = 0;
 			$scope.activeCriteria = "ROOM_TYPE";
 
-			//TODO : Make adjustments if multiple rooms are selected and the room selection bar is displayed
-			$scope.containerHeight = $(window).height() - 300;
 
-
-			$scope.roomAvailability = $scope.getAvailability(roomRates);
-
-			$scope.displayData.allRooms = $(roomRates.room_types).filter(function() {
-				return $scope.roomAvailability[this.id].availability == true &&
- 					$scope.roomAvailability[this.id].rates.length > 0;
-			});
-
-			$scope.displayData.roomTypes = $scope.displayData.allRooms;
-
+			//Restructure rates for easy selection
 			var rates = [];
-
 			$(roomRates.rates).each(function(i, d) {
 				rates[d.id] = d;
 			});
-
 			$scope.displayData.allRates = rates;
 
-			var dates = $scope.getStayDays();
+			//TODO : Make adjustments if multiple rooms are selected and the room selection bar is displayed
+			$scope.containerHeight = $(window).height() - 300;
 
-			$(dates).each(function(i, d) {
-				$scope.displayData.stay.push({
-					"date": d
-				});
+			$scope.roomAvailability = $scope.getAvailability(roomRates);
+
+			//Filter for rooms which are available and have rate information
+			$scope.displayData.allRooms = $(roomRates.room_types).filter(function() {
+				return $scope.roomAvailability[this.id].availability == true &&
+					$scope.roomAvailability[this.id].rates.length > 0;
 			});
+
+			$scope.displayData.roomTypes = $scope.displayData.allRooms;
 		};
 
 
@@ -87,26 +79,11 @@ sntRover.controller('RVReservationRoomTypeCtrl', ['$rootScope', '$scope', 'roomR
 			}
 		}
 
-
-		$scope.getStayDays = function() {
-			var dates = [];
-			startDate = new Date($scope.reservationData.arrivalDate);
-			stopDate = new Date($scope.reservationData.departureDate);
-
-			var currentDate = startDate;
-
-			while (currentDate <= stopDate) {
-				dates.push(currentDate)
-				currentDate = new Date(Date.parse(currentDate) + 86400000);
-			}
-			return dates;
-		}
-
 		$scope.getAvailability = function(roomRates) {
 			var rooms = [];
-			//step1: check for room availability in the date range
 			$(roomRates.results).each(function(i, d) {
 				var for_date = d.date;
+				//step1: check for room availability in the date range
 				$(d.room_types).each(function(i, d) {
 					if (typeof rooms[d.id] == "undefined") {
 						rooms[d.id] = {
@@ -123,6 +100,7 @@ sntRover.controller('RVReservationRoomTypeCtrl', ['$rootScope', '$scope', 'roomR
 					}
 				});
 
+				//step2: extract rooms with rate information
 				$(d.rates).each(function(i, d) {
 					var rate_id = d.id;
 					$(d.room_rates).each(function(i, d) {
@@ -131,7 +109,7 @@ sntRover.controller('RVReservationRoomTypeCtrl', ['$rootScope', '$scope', 'roomR
 						}
 						rooms[d.room_type_id].ratedetails.push({
 							rate_id: rate_id,
-							rate: 100.00,
+							rate: $scope.calculateRate(d),
 							tax: 0,
 							day: new Date(for_date)
 						});
@@ -141,6 +119,7 @@ sntRover.controller('RVReservationRoomTypeCtrl', ['$rootScope', '$scope', 'roomR
 
 			});
 
+			//step3: total and average calculation
 			for (var id in rooms) {
 				var value = rooms[id];
 				$(value.ratedetails).each(function(i, d) {
@@ -151,17 +130,15 @@ sntRover.controller('RVReservationRoomTypeCtrl', ['$rootScope', '$scope', 'roomR
 						}
 					}
 					value.total[id].total = parseInt(value.total[id].total) + parseInt(d.rate);
+					//ASSUMPTION: use the number of days as the denominator for the average calculation
 					value.total[id].average = parseInt(value.total[id].total / 6);
 				})
-				//TODO: Caluculate the default ID
 
+				//TODO: Caluculate the default ID
 				value.defaultRate = $(value.rates).first().length > 0 ? $(value.rates).first()[0] : -1;
 			}
-
-			console.log(rooms);
 			return rooms;
 		}
-
 
 		$scope.refreshScroll = function() {
 			$timeout(function() {
@@ -169,6 +146,42 @@ sntRover.controller('RVReservationRoomTypeCtrl', ['$rootScope', '$scope', 'roomR
 			}, 300);
 		}
 
+		$scope.calculateRate = function(rateTable) {
+			// Hi Shiju, 
+			// The rate amount calculation works as follows: 
+			// 1.       1 Adult, 0 Children: Single
+			// 2.       2 Adults, 0 Children: Double
+			// 3.       3 Adults, 0 Children: Double + Extra Adult
+			// 4.       4 Adults, 0 Children: Double + 2x Extra Adult
+			// 5.       1 Adult, 1 Child: Single + Child Rate
+			// 6.       1 Adult, 2 Children: Single + 2x Child Rate
+			// 7.       2 Adults, 2 Children: Double + 2x Child Rate
+			// 8.       3 Adults, 3 Children: Double + Extra Adult + 3x Child Rate
+			// And for forth….
+			// Can we set the ADR & Total Stay calculation already now, so we can make sure it works correctly. When we add the tax, we just need to add in the extra amount. For now just add 0.00 for the tax value.
+			// Thanks,
+			// Nicki
+			
+			var roomIndex = $scope.activeRoom;
+			// var adults = $scope.reservationData.rooms[roomIndex].numAdults;
+			// var children = $scope.reservationData.rooms[roomIndex].numChildren;
+			var adults = 1;
+			var children = 3;
+
+			//Assumption
+			var baseRoomRate = adults >= 2 ? rateTable.double : rateTable.single;
+
+			var extraAdults = adults >= 2 ? adults - 2 : 0;
+
+			// console.log({
+			// 	baseRoomRate: baseRoomRate,
+			// 	adults : adults,
+			// 	extraAdults : extraAdults,
+			// 	children : children 
+			// });
+
+			return baseRoomRate + (extraAdults * rateTable.extra_adult) + (children * rateTable.child);
+		}
 
 		init();
 	}
