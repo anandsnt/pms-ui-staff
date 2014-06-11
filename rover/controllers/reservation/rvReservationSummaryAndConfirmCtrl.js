f896f4c7965e50eabafb9ec2acc942a9573fe778
akhilastayntouch <akhila@stayntouch.com>
2014-06-11 17:39:04 +0530

    CICO-6685 fixed issues with reservation save

--- rover/controllers/reservation/rvReservationSummaryAndConfirmCtrl.js
+++ rover/controllers/reservation/rvReservationSummaryAndConfirmCtrl.js
@@ -56,11 +56,11 @@ sntRover.controller('RVReservationSummaryAndConfirmCtrl', ['$scope', '$state', '
 											$scope.reservationData.checkoutTime.mm,
 											$scope.reservationData.checkoutTime.ampm);
 		
-		data.adults_count = $scope.reservationData.rooms[0].numAdults;
-		data.children_count = $scope.reservationData.rooms[0].numChildren;
-		data.infants_count = $scope.reservationData.rooms[0].numInfants;
-		data.rate_id = $scope.reservationData.rooms[0].rateId;
-		data.room_type_id = $scope.reservationData.rooms[0].roomTypeId;
+		data.adults_count = parseInt($scope.reservationData.rooms[0].numAdults);
+		data.children_count = parseInt($scope.reservationData.rooms[0].numChildren);
+		data.infants_count = parseInt($scope.reservationData.rooms[0].numInfants);
+		data.rate_id = parseInt($scope.reservationData.rooms[0].rateId);
+		data.room_type_id = parseInt($scope.reservationData.rooms[0].roomTypeId);
 
 		if($scope.reservationData.guest.id != null) {
 			data.guest_detail_id = $scope.reservationData.guest.id;
@@ -70,7 +70,7 @@ sntRover.controller('RVReservationSummaryAndConfirmCtrl', ['$scope', '$state', '
 			data.guest_detail.last_name = $scope.reservationData.guest.lastName;
 			data.guest_detail.email = $scope.reservationData.guest.email;
 			data.guest_detail.payment_type = {};
-			data.guest_detail.payment_type.type_id = $scope.reservationData.paymentType.type.name;//TODO: verify
+			data.guest_detail.payment_type.type_id = parseInt($scope.reservationData.paymentType.type.name);//TODO: verify
 			data.guest_detail.payment_type.card_number = $scope.reservationData.paymentType.ccDetails.number;
 			data.guest_detail.payment_type.expiry_date = $scope.reservationData.paymentType.ccDetails.expYear + '-' +
 															$scope.reservationData.paymentType.ccDetails.expMonth; //TODO: format
@@ -78,7 +78,7 @@ sntRover.controller('RVReservationSummaryAndConfirmCtrl', ['$scope', '$state', '
 			
 		}
 
-		if($scope.reservationData.company.id != null) {
+		/*if($scope.reservationData.company.id != null) {
 			data.company_id = $scope.reservationData.company.id;
 		} else {
 			data.company = {};
@@ -92,12 +92,12 @@ sntRover.controller('RVReservationSummaryAndConfirmCtrl', ['$scope', '$state', '
 			data.travel_agent = {};
 			data.travel_agent.name = $scope.reservationData.travelAgent.name;
 			data.travel_agent.corporate_id = $scope.reservationData.travelAgent.iataNumber; //TODO: verify iataNum vs corporateid
-		}
+		}*/
 
-		data.reservation_type_id = $scope.reservationData.demographics.reservationType;
-		data.source_id = $scope.reservationData.demographics.source;
-		data.market_segment_id = $scope.reservationData.demographics.market;
-		data.booking_origin_id = $scope.reservationData.demographics.origin;
+		data.reservation_type_id = parseInt($scope.reservationData.demographics.reservationType);
+		data.source_id = parseInt($scope.reservationData.demographics.source);
+		data.market_segment_id = parseInt($scope.reservationData.demographics.market);
+		data.booking_origin_id = parseInt($scope.reservationData.demographics.origin);
 		data.confirmation_email = $scope.reservationData.guest.sendConfirmMailTo;
 
 		console.log(JSON.stringify(data));
