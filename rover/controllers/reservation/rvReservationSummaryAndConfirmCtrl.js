b10e18bbbf5bd9922b73221a69bf8de857312bc2
akhilastayntouch <akhila@stayntouch.com>
2014-06-11 16:46:00 +0530

    CICO-6685 ui updates

--- rover/controllers/reservation/rvReservationSummaryAndConfirmCtrl.js
+++ rover/controllers/reservation/rvReservationSummaryAndConfirmCtrl.js
@@ -56,11 +56,11 @@ sntRover.controller('RVReservationSummaryAndConfirmCtrl', ['$scope', '$state', '
 											$scope.reservationData.checkoutTime.mm,
 											$scope.reservationData.checkoutTime.ampm);
 		
-		data.adults_count = $scope.reservationData.rooms.numAdults;
-		data.children_count = $scope.reservationData.rooms.numChildren;
-		data.infants_count = $scope.reservationData.rooms.numInfants;
-		data.rate_id = $scope.reservationData.rooms.rateId;
-		data.room_type_id = $scope.reservationData.rooms.roomTypeId;
+		data.adults_count = $scope.reservationData.rooms[0].numAdults;
+		data.children_count = $scope.reservationData.rooms[0].numChildren;
+		data.infants_count = $scope.reservationData.rooms[0].numInfants;
+		data.rate_id = $scope.reservationData.rooms[0].rateId;
+		data.room_type_id = $scope.reservationData.rooms[0].roomTypeId;
 
 		if($scope.reservationData.guest.id != null) {
 			data.guest_detail_id = $scope.reservationData.guest.id;
@@ -72,7 +72,7 @@ sntRover.controller('RVReservationSummaryAndConfirmCtrl', ['$scope', '$state', '
 			data.guest_detail.payment_type = {};
 			data.guest_detail.payment_type.type_id = $scope.reservationData.paymentType.type.name;//TODO: verify
 			data.guest_detail.payment_type.card_number = $scope.reservationData.paymentType.ccDetails.number;
-			data.guest_detail.payment_type.expiry_date = $scope.reservationData.paymentType.ccDetails.expMonth +
+			data.guest_detail.payment_type.expiry_date = $scope.reservationData.paymentType.ccDetails.expYear + '-' +
 															$scope.reservationData.paymentType.ccDetails.expMonth; //TODO: format
 			data.guest_detail.payment_type.card_name = $scope.reservationData.paymentType.ccDetails.nameOnCard;
 			
