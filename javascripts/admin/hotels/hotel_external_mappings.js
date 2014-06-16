a70b02497c515b9fe931392a7efda035bc9974f7
Dipin <dipin@stayntouch.com>
2014-06-16 13:58:05 +0530

    CICO-7134 Fixed issue with wrong class given for edit

--- javascripts/admin/hotels/hotel_external_mappings.js
+++ javascripts/admin/hotels/hotel_external_mappings.js
@@ -1,21 +1,21 @@
 var HotelExternalMappingsView = function(domRef){
-  BaseInlineView.call(this);  
-  this.myDom = domRef;  
+  BaseInlineView.call(this);
+  this.myDom = domRef;
   var that = this;
   this.externalMappings = [];
-  
-  
+
+
   // to handle subview events
-  this.delegateSubviewEvents = function(){ 
+  this.delegateSubviewEvents = function(){
   	var ext_mapping_table = that.myDom.find('#external_mapping_table');
 	// we need to apply table sorter if the table has more than one row
 	if(ext_mapping_table.find("tr").length > 1){
-		ext_mapping_table.tablesorter();	
+		ext_mapping_table.tablesorter();
 	}
-  	that.myDom.find('#go_back').on('click', that.goBack); 
-  	that.myDom.on('change', that.filterExternalMappings); 
+  	that.myDom.find('#go_back').on('click', that.goBack);
+  	that.myDom.on('change', that.filterExternalMappings);
   	that.myDom.find('#add-new-external-mapping').on('click', that.addNewExternalMapping);
-  	// that.myDom.find('.edit-data-inline-external-mapping').on('click', that.editExternalMapping);
+    that.myDom.find('.edit-data-inline').on('click', that.editExternalMapping);
   	// to get all external mappings to do internal filtering
   	//that.getAllExternalMappings();
   };
@@ -31,16 +31,16 @@ var HotelExternalMappingsView = function(domRef){
   };
   //to get all external mappings data
   this.getAllExternalMappings =  function(event){
-  	var hotel_id = that.myDom.find("#selected_hotel").attr("data-hotel-id"); 	
+  	var hotel_id = that.myDom.find("#selected_hotel").attr("data-hotel-id");
   	var webservice = new WebServiceInterface();
-	
+
     var url = "/admin/external_mappings/"+hotel_id+"/new_mappings.json";
     var options = {
 			   successCallBack: that.fetchCompletedOfGetExternalMappings
 	};
     webservice.getJSON(url, options);
-   
-  	
+
+
   };
   // to go to previous page
   this.goBack = function(){
@@ -50,10 +50,10 @@ var HotelExternalMappingsView = function(domRef){
   this.fetchCompletedOfGetExternalMappings = function(data){
 	  that.externalMappings = data.data.mapping_type;
   };
-  // to repopultae snt vlues drop down on selecting external mappings 
+  // to repopultae snt vlues drop down on selecting external mappings
   this.filterExternalMappings = function(e){
  	if(e.target.id == "mapping-type"){
- 		
+
  		var selectedMappingType = that.myDom.find("#mapping-type").val();
 		mappingTypeValues = '';
 		that.myDom.find("#snt-value").find('option').remove().end();
@@ -65,57 +65,57 @@ var HotelExternalMappingsView = function(domRef){
 					mappingTypeValues = '<option value="'+mappingvalue.name+'">'+mappingvalue.name+'</option>';
 					$("#snt-value").append(mappingTypeValues);
 				});
-			}		    
+			}
 		});
-		
+
  	}
-  		
+
   };
   //function to add new external mapping
-  this.saveNewApi = function(event){ 
+  this.saveNewApi = function(event){
   	var postData = {};
-  	postData.mapping_type = that.myDom.find("#mapping-type").val(); 
-  	postData.snt_value = that.myDom.find("#snt-value").val(); 
-  	postData.external_value = that.myDom.find("#external-value").val(); 
-  	postData.hotel_id = that.myDom.find("#selected_hotel").attr("data-hotel-id"); 	
-  	
+  	postData.mapping_type = that.myDom.find("#mapping-type").val();
+  	postData.snt_value = that.myDom.find("#snt-value").val();
+  	postData.external_value = that.myDom.find("#external-value").val();
+  	postData.hotel_id = that.myDom.find("#selected_hotel").attr("data-hotel-id");
+
   	var url = '/admin/external_mappings/save_mapping';
-	var webservice = new WebServiceInterface();		
+	var webservice = new WebServiceInterface();
 	var options = {
 			   requestParameters: postData,
 			   successCallBack: that.fetchCompletedOfSave,
 			   successCallBackParameters:{ "event": event},
 			   loader:"BLOCKER"
-			   
+
 	};
-	webservice.postJSON(url, options);	
+	webservice.postJSON(url, options);
   };
   //function to update department
   this.updateApi = function(event){
-  	
+
   	var postData = {};
-  	postData.mapping_type = that.myDom.find("#mapping-type").val(); 
-  	postData.snt_value = that.myDom.find("#snt-value").val(); 
-  	postData.external_value = that.myDom.find("#external-value").val(); 
+  	postData.mapping_type = that.myDom.find("#mapping-type").val();
+  	postData.snt_value = that.myDom.find("#snt-value").val();
+  	postData.external_value = that.myDom.find("#external-value").val();
   	postData.hotel_id = that.myDom.find("#selected_hotel").attr("data-hotel-id");
   	postData.value = that.myDom.find("#edit-external-mapping").attr("data-id");
-  
+
   	var url = '/admin/external_mappings/save_mapping';
-	var webservice = new WebServiceInterface();		
+	var webservice = new WebServiceInterface();
 	var options = {
 			   requestParameters: postData,
 			   successCallBack: that.fetchCompletedOfSave,
 			   failureCallBack: that.fetchFailedOfSave,
 			   successCallBackParameters:{ "event": event},
 			   loader:"BLOCKER"
-			   
+
 	};
-	webservice.postJSON(url, options);	
-	
+	webservice.postJSON(url, options);
+
   };
   //refreshing view with new data and showing message
   this.fetchCompletedOfSave = function(data, requestParams){
-  	currentHotel = that.myDom.find("#selected_hotel").attr("data-hotel-id"); 	
+  	currentHotel = that.myDom.find("#selected_hotel").attr("data-hotel-id");
   	var url = "/admin/external_mappings/"+currentHotel+"/list_mappings";
    	viewParams = that.viewParams;
   	sntapp.fetchAndRenderView(url, that.myDom, {}, 'BLOCKER', viewParams, false);
@@ -124,9 +124,9 @@ var HotelExternalMappingsView = function(domRef){
   //Handling failure
   this.fetchFailedOfSave = function(errorMessage){
 	sntapp.activityIndicator.hideActivityIndicator();
-	sntapp.notification.showErrorMessage("Error: " + errorMessage, that.myDom);  
+	sntapp.notification.showErrorMessage("Error: " + errorMessage, that.myDom);
   };
-  
+
    //function to delete mapping
   this.deleteItem = function(event){
   	event.preventDefault();
@@ -134,7 +134,7 @@ var HotelExternalMappingsView = function(domRef){
   	var selectedId = that.myDom.find(event.target).attr("id");
   	var url = '/admin/external_mappings/'+selectedId+'/delete_mapping';
   	postData.value = selectedId;
-	var webservice = new WebServiceInterface();		
+	var webservice = new WebServiceInterface();
 	var options = {
 			   requestParameters: postData,
 			   successCallBack: that.fetchCompletedOfDelete,
