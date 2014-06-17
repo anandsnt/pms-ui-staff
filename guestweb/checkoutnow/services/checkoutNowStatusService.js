9ea7f6a9f2d2948dbcff5d1f9c819152e6eae45f
Resheil <resheil@stayntouch.com>
2014-06-17 13:20:22 +0530

    CICO-7325 implementing new service

---
+++ guestweb/checkoutnow/services/checkoutNowStatusService.js
@@ -0,0 +1,32 @@
+(function() {
+var checkoutNowService = function($q,$rootScope,$http) {
+	var response = {};
+
+	//fetch bill details
+
+	var completeCheckout = function(url,data) {
+
+	var deferred = $q.defer();
+	$http.post(url, data).success(function(response){
+		deferred.resolve(response);
+	}).error(function(){				
+	$rootScope.netWorkError = true;
+		deferred.reject();			
+	});
+	return deferred.promise;
+	};
+
+	return {
+		response:response,
+		completeCheckout:completeCheckout
+
+	}
+};
+
+var dependencies = [
+'$q','$rootScope','$http',
+checkoutNowService
+];
+
+snt.factory('checkoutNowService', dependencies);
+})();
