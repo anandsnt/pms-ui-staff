e6dabdc8ff6318e7885fe031393b218638775db2
soumya <soumya@stayntouch.com>
2014-06-17 16:15:16 +0530

    CICO-7373 Splitted translation files

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,12 +33,61 @@ module.exports = function(grunt) {
     bower: {
 		  install: {
 		  }
-	}
+	},
+	concat: {
+		
+	   roverEnglishJson: {
+            src: ['rover/rvLocales/en/*.json'],
+            dest: 'rover/rvLocales/EN.json',
+            options: {
+                // Added to the top of the file
+                banner: '{',
+                // Will be added at the end of the file
+                footer: '}',
+                separator: ','
+            }
+       },
+       adminEnglishJson: {
+            src: ['admin/adLocales/en/*.json'],
+            dest: 'admin/adLocales/EN.json',
+            options: {
+                // Added to the top of the file
+                banner: '{',
+                // Will be added at the end of the file
+                footer: '}',
+                separator: ','
+            }
+       },
+        roverGermanJson: {
+            src: ['rover/rvLocales/de/*.json'],
+            dest: 'rover/rvLocales/DE.json',
+            options: {
+                // Added to the top of the file
+                banner: '{',
+                // Will be added at the end of the file
+                footer: '}',
+                separator: ','
+            }
+       },
+       adminGermanJson: {
+            src: ['admin/adLocales/de/*.json'],
+            dest: 'admin/adLocales/DE.json',
+            options: {
+                // Added to the top of the file
+                banner: '{',
+                // Will be added at the end of the file
+                footer: '}',
+                separator: ','
+            }
+        }
+		
+
+    }
   });
 
   grunt.loadNpmTasks('grunt-angular-templates');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-bower-task');
-
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
 };
