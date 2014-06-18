c611e1380e3fbc27d30f16947b43f7afdb259b7c
Dipin <dipin@stayntouch.com>
2014-06-18 13:12:25 +0530

    CICO-7209 Added the modification for no charge button

--- javascripts/app/room_type_charge_modal.js
+++ javascripts/app/room_type_charge_modal.js
@@ -5,31 +5,40 @@ var RoomTypeChargeModal = function(options) {
 
 	this.delegateEvents = function() {
 		that.myDom.find('#ok').on('click',that.okButtonClicked);
+		that.myDom.find('#nocharge').on('click',that.noChargeClicked);
 		that.myDom.find('#room-type-charge').on('keypress',that.keypressed);
 		that.myDom.find('#room-type-charge').on('keyup',that.keyUp);
 	};
-	
+
 	this.okButtonClicked = function(){
-		
+
 		var roomTypeCharge = that.myDom.find("#room-type-charge").val();
 		options.requestParameters.upsell_amount = roomTypeCharge;
-		
+
 		var url = '/staff/reservations/upgrade_room';
 		var webservice = new WebServiceInterface();
 		webservice.postJSON(url, options);
-		
+
 		that.hide();
-		
+
 	};
+	this.noChargeClicked = function(e){
+		var url = '/staff/reservations/upgrade_room';
+		var webservice = new WebServiceInterface();
+		webservice.postJSON(url, options);
+		that.hide();
+	 };
+
+
 	this.keypressed = function(e){
 		if ( e.keyCode == 13 ) {
 			e.preventDefault();
 	   	}
-	 };  	
-	   	
+	 };
+
 	this.keyUp = function(e){
 	   	var roomTypeCharge = that.myDom.find("#room-type-charge").val();
-	   	
+
 	   	if(roomTypeCharge == ""){
 	   		that.myDom.find('#ok').attr("disabled", true);
 	   	}
@@ -37,5 +46,5 @@ var RoomTypeChargeModal = function(options) {
 	   		that.myDom.find('#ok').attr("disabled", false);
 	   	}
 	};
-	
+
 };
