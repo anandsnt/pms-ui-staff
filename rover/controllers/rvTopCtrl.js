54a172878183168d13b2f496f98875fe8be698e6
Vijay Dev <--global>
2014-06-20 13:31:49 +0530

    CICO-6399: css3 animations added and tested in iPad

--- rover/controllers/rvTopCtrl.js
+++ rover/controllers/rvTopCtrl.js
@@ -1,9 +1,3 @@
 sntRover.controller('topController', ['$state', '$scope', function($state, $scope) {
-
-	$scope.$on('$stateChangeSuccess', function (event, toState, dwad, fromState, dwads) {
-	  fromState.name = fromState.name ? fromState.name : 'Null'
-	  console.log( 'From: "' + fromState.name + '" state \nTo: "' + toState.name + '" state' );
-	});
-
 	$state.go('rover.dashboard');
 }]);
