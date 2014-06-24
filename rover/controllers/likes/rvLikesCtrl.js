38b2fc96c88c07bb3ceb6926dcda02761495625a
krishobh <krishobh@stayntouch.com>
2014-06-24 13:59:59 +0530

    CICO-7640 Implemeted controller and service for likes

---
+++ rover/controllers/likes/rvLikesCtrl.js
@@ -0,0 +1,87 @@
+sntRover.controller('RVLikesController',['$scope','RVLikesSrv','dateFilter',function($scope, RVLikesSrv, dateFilter){
+
+
+$scope.errorMessage = "";
+$scope.guestCardData.likes = {};
+var presentLikes = {};
+
+$scope.init = function(){
+	
+	var fetchLikesSuccessCallback = function(data){
+		console.log(data);
+        $scope.$emit('hideLoader');
+        $scope.guestCardData.likes = data;
+        /**
+		  * storing to check if data will be updated
+		  */
+        presentLikes = JSON.parse(JSON.stringify($scope.guestCardData.likes));
+    };
+    var fetchLikesFailureCallback = function(data){
+        $scope.$emit('hideLoader');
+        $scope.errorMessage = data;
+    };
+    var data = { 'userId' : $scope.guestCardData.contactInfo.user_id };
+    console.log(data);
+	$scope.invokeApi(RVLikesSrv.fetchLikes,data,fetchLikesSuccessCallback,fetchLikesFailureCallback);  	
+};
+
+$scope.init();
+
+$scope.saveLikes = function(){
+    var saveUserInfoSuccessCallback = function(data){
+        $scope.$emit('hideLoader');
+    };
+    var saveUserInfoFailureCallback = function(data){
+        $scope.$emit('hideLoader');
+        $scope.errorMessage = data;
+        $scope.$emit('contactInfoError',true);
+    };
+   
+/**
+  * change date format for API call 
+  */
+    var dataToUpdate =  JSON.parse(JSON.stringify($scope.guestCardData.likes));
+    var dataUpdated = false;
+
+    if(angular.equals(dataToUpdate, presentLikes)) {
+			dataUpdated = true;
+	}
+	else{
+		presentContactInfo = dataToUpdate;	
+		//var unwantedKeys = ["avatar","vip"]; // remove unwanted keys for API
+		//dataToUpdate = dclone(dataToUpdate, unwantedKeys); 
+	};	    	
+    
+    var data ={'data':dataToUpdate,
+    			'userId':$scope.guestCardData.contactInfo.user_id
+    		};
+    if(!dataUpdated)
+     $scope.invokeApi(RVLikesSrv.saveLikes,data,saveUserInfoSuccessCallback,saveUserInfoFailureCallback);  	
+};
+
+
+/**
+  * to handle click actins outside this tab
+ */
+$scope.$on('saveLikes',function(){
+$scope.saveLikes();
+});	
+
+
+
+$scope.$parent.myScrollOptions = {		
+    'contact_info': {
+    	scrollbars: true,
+        snap: false,
+        hideScrollbar: false
+    },
+};
+
+$scope.$on('CONTACTINFOLOADED', function(event) {
+	setTimeout(function(){
+		$scope.$parent.myScroll['contact_info'].refresh();
+		}, 
+	1500);
+	
+});
+}]);
---
+++ rover/services/likes/rvLikesSrv.js
@@ -0,0 +1,29 @@
+sntRover.service('RVLikesSrv',['$q', 'RVBaseWebSrv', function($q, RVBaseWebSrv){
+		
+	this.fetchLikes = function(param){
+		var deferred = $q.defer();
+		var userId = param.userId;
+		var url = '/staff/preferences/likes?user_id='+userId
+		RVBaseWebSrv.getJSON(url).then(function(data) {
+			deferred.resolve(data);
+		},function(data){
+			deferred.reject(data);
+		});
+		return deferred.promise;		
+	};
+	
+	this.saveLikes = function(param){
+		var deferred = $q.defer();
+		var dataToSend = param.data;
+		var url = '/staff/guest_cards/'+params.userId+'/update_preferences';
+		RVBaseWebSrv.postJSON(url, dataToSend).then(function(data) {
+			deferred.resolve(data);
+		},function(data){
+			deferred.reject(data);
+		});
+		return deferred.promise;		
+		
+	};
+
+
+}]);
