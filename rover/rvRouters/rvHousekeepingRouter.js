75355d48c2d6b1521e0d62cc10876e5a9d1967a4
Vijay Dev <--global>
2014-06-27 10:47:35 +0530

    CICO-5944 styles and router updated

--- rover/rvRouters/rvHousekeepingRouter.js
+++ rover/rvRouters/rvHousekeepingRouter.js
@@ -4,12 +4,40 @@ angular.module('housekeepingModule', [])
     $stateProvider.state('rover.housekeeping', {
         abstract : true,
         url: '/housekeeping',
-        templateUrl: '/assets/partials/housekeeping/rvHousekeeping.html'
+        templateUrl: '/assets/partials/housekeeping/rvHousekeeping.html',
+        controller: 'RVHkAppCtrl'
     });
 
     $stateProvider.state('rover.housekeeping.dashboard', {
         url: '/dashboard',
         templateUrl: '/assets/partials/housekeeping/rvHkDashboard.html',
-        controller: 'RVHkDashboardCtrl'
+        controller: 'RVHkDashboardCtrl',
+        resolve: {
+            dashboardData: function(RVHkDashboardSrv) {
+                return RVHkDashboardSrv.fetch();
+            }
+        }
+    });
+
+    $stateProvider.state('rover.housekeeping.search', {
+        url: '/search',
+        templateUrl: '/assets/partials/housekeeping/rvHkSearch.html',
+        controller: 'RVHkSearchCtrl',
+        resolve: {
+            fetchedRoomList: function(RVHkSearchSrv) {
+                return RVHkSearchSrv.roomList;
+            }
+        }
+    });
+
+    $stateProvider.state('rover.housekeeping.roomDetails', {
+        url: '/roomDetails/:id',
+        templateUrl: '/assets/partials/housekeeping/rvHkRoomDetails.html',
+        controller: 'RVHkRoomDetailsCtrl',
+        resolve: {
+            roomDetailsData: function(RVHkRoomDetailsSrv, $stateParams) {
+                return RVHkRoomDetailsSrv.fetch( $stateParams.id );
+            }
+        }
     });
 });
