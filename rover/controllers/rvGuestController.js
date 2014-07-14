d12caae4f73f7e3fc55feca45e3cfb28b1fd59bd
sdilipsam <dilips@stayntouch.com>
2014-07-14 15:20:12 +0530

    CICO-7936 QA Comment 4 and 5 checked

--- rover/controllers/rvGuestController.js
+++ rover/controllers/rvGuestController.js
@@ -507,7 +507,7 @@ sntRover.controller('guestCardController', ['$scope', '$window', 'RVCompanyCardS
 				var paramDict = {
 					'name': $scope.searchData.companyCard.companyName,
 					'city': $scope.searchData.companyCard.companyCity,
-					'corporate_id': $scope.searchData.companyCard.companyCorpId
+					'account_number': $scope.searchData.companyCard.companyCorpId
 				};
 				$scope.invokeApi(RVReservationAllCardsSrv.fetchCompaniesOrTravelAgents, paramDict, successCallBackFetchCompanies);
 			} else {
@@ -563,7 +563,7 @@ sntRover.controller('guestCardController', ['$scope', '$window', 'RVCompanyCardS
 				var paramDict = {
 					'name': $scope.searchData.travelAgentCard.travelAgentName,
 					'city': $scope.searchData.travelAgentCard.travelAgentCity,
-					'corporate_id': $scope.searchData.travelAgentCard.travelAgentIATA
+					'account_number': $scope.searchData.travelAgentCard.travelAgentIATA
 				};
 				$scope.invokeApi(RVReservationAllCardsSrv.fetchCompaniesOrTravelAgents, paramDict, successCallBackFetchTravelAgents);
 			} else {
