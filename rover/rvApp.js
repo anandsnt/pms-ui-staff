94e72b4ee8a0b00d57c774a5548c73d56bb4e958
Vijay Dev <--global>
2014-07-17 09:01:07 -0400

    CICO-8005: going forward more changes

--- rover/rvApp.js
+++ rover/rvApp.js
@@ -9,6 +9,75 @@ sntRover.run(['$rootScope', '$state', '$stateParams', function ($rootScope, $sta
 	
 
 	/**
+	*	For certain state transitions
+	*	the transition animation must be reversed
+	*
+	*	This is achived by adding class 'return-back'
+	*	to the imediate parent of 'ui-view'
+	*/
+	$rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+
+		// spiting state names so as to add them to 'revList', if needed
+		console.log(fromState.name + ' ===> ' + toState.name);
+
+		// check this template to see how this class is applied:
+		// app/assets/rover/partials/staycard/rvStaycard.html
+
+		if ( $rootScope.shallRevDir(fromState.name, toState.name) ) {
+			$rootScope.returnBack = true;
+		} else {
+			$rootScope.returnBack = false;
+		}
+
+		// saving the prevState name and params
+		// for quick 'goBack' method
+		$rootScope.prevStateName  = fromState.name;
+		$rootScope.prevStateParam = fromParams;
+	});
+
+	/**
+	*	revList is an array that holds
+	*	state name sets that when transitioning
+	*	the transition animation should be reversed 
+	*	
+	*	@private
+	*/
+	var revList = [{
+		fromState: 'rover.housekeeping.roomDetails',
+		toState  : 'rover.housekeeping.roomStatus'
+	}, {
+		fromState: 'rover.staycard.reservationcard.reservationdetails',
+		toState  : 'rover.search'
+	}, {
+		fromState: 'rover.staycard.billcard',
+		toState  : 'rover.staycard.reservationcard.reservationdetails'
+	}, {
+		fromState: 'rover.staycard.nights',
+		toState  : 'rover.staycard.reservationcard.reservationdetails'
+	}, {
+		fromState: 'rover.companycarddetails',
+		toState  : 'rover.companycardsearch'
+	}];
+
+	/**
+	*	
+	*	
+	*	@param {String} fromState - name of the from state
+	*	@param {Sting} toState - name of the to state
+	*/
+	$rootScope.addToRevList = function(fromState, toState) {
+		var notAdded = true;
+		for (var i = 0; i < revList.length; i++) {
+			if ( revList[i].fromState === fromState && revList[i].toState === toState ) {
+				notAdded = false;
+				break;
+			};
+		};
+
+		notAdded && hasAddedrevList.push({ 'fromState': fromState, 'toState'  : toState });
+	};
+
+	/**
 	*	A method on the $rootScope to determine if the
 	*	slide animation during stateChange should run in reverse or forward
 	*
@@ -18,53 +87,16 @@ sntRover.run(['$rootScope', '$state', '$stateParams', function ($rootScope, $sta
 	*	@return {boolean} - to indicate reverse or not
 	*/
 	$rootScope.shallRevDir = function(fromState, toState) {
-		if (fromState === 'rover.housekeeping.roomDetails' && toState === 'rover.housekeeping.roomStatus') {
-			return true;
-		};
-
-		if (fromState === 'rover.staycard.reservationcard.reservationdetails' && toState === 'rover.search') {
-			return true;
-		};
-
-		if (fromState === 'rover.staycard.billcard' && toState === 'rover.staycard.reservationcard.reservationdetails') {
-			return true;
-		};
-
-		if (fromState === 'rover.staycard.nights' && toState === 'rover.staycard.reservationcard.reservationdetails') {
-			return true;
-		};
-
-		if (fromState === 'rover.companycarddetails' && toState === 'rover.companycardsearch') {
-			return true;
+		for (var i = 0; i < revList.length; i++) {
+			if ( revList[i].fromState === fromState && revList[i].toState === toState ) {
+				return true;
+				break;
+			};
 		};
 
 		return false;
 	};
 
-
-	/**
-	*	this is make sure we add an
-	*	additional class 'return-back' as a
-	*	parent to ui-view, so as to apply a
-	*	reverse slide animation
-	*/
-	$rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
-
-		// to study the current changing states, remove on release
-		console.log(fromState.name + ' ===> ' + toState.name);
-
-		// check this template for the applied class:
-		// app/assets/rover/partials/staycard/rvStaycard.html
-
-		if ( $rootScope.shallRevDir(fromState.name, toState.name) ) {
-			$rootScope.returnBack = true;
-		} else {
-			$rootScope.returnBack = false;
-		}
-
-		$rootScope.prevStateName  = fromState.name;
-		$rootScope.prevStateParam = fromParams;
-	});
 
 	/**
 	*	A very simple methods to
