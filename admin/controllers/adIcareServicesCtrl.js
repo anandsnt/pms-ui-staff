94bd9abf38f74a7af063b2a49f0760d4b4aa0a2a
krishobh <krishobh@stayntouch.com>
2014-07-18 14:37:28 +0530

    CICO-7864 updated icare page with service

--- admin/controllers/adIcareServicesCtrl.js
+++ admin/controllers/adIcareServicesCtrl.js
@@ -1,6 +1,52 @@
 admin.controller('ADIcareServicesCtrl', ['$scope', '$rootScope', '$state', '$stateParams', 'ADICareServicesSrv',
-function($scope, $state, $rootScope, $stateParams, ADICareServicesSrv) {
+function($scope, $rootScope, $state,  $stateParams, ADICareServicesSrv) {
 	BaseCtrl.call(this, $scope);
+	$scope.$emit("changedSelectedMenu", 8);
+	$scope.errorMessage = "";
+	$scope.icare = {};
+	/*
+    * Success callback of render
+    * @param {object} icare service details
+    */    
+    $scope.successCallbackRender = function(data){
+    	console.log(data);
+    	$scope.$emit('hideLoader');
+    	$scope.icare = data;
+    };
+   /**
+    * Render icare service screen
+    */
+	$scope.renderIcareServices = function(){
+		$scope.invokeApi(ADICareServicesSrv.getIcareServices, {} , $scope.successCallbackRender);
+	};
+	//To render screen
+	$scope.renderIcareServices();
+	/**
+    * To handle save button action
+    *
+    */ 
+    $scope.saveClick = function(){
+    	console.log($scope.icare);
+    	var data = { "icare" : $scope.icare };
+    	$scope.invokeApi(ADICareServicesSrv.saveIcareServices, data , $scope.successCallbackSave);
+
+    };
+	/**
+    * To handle cancel/back button action
+    *
+    */ 
+    $scope.cancelClick = function(){
+        
+    	if($rootScope.previousStateParam){
+            $state.go($rootScope.previousState, { menu:$rootScope.previousStateParam });
+        }
+        else if($rootScope.previousState){
+            $state.go($rootScope.previousState);
+        }
+        else{
+            $state.go('admin.dashboard', {menu : 0});
+        }
+    };
 	
 }]);
 
--- admin/services/adIcareServicesSrv.js
+++ admin/services/adIcareServicesSrv.js
@@ -1,4 +1,48 @@
-admin.service('ADICareServicesSrv', ['$http', '$q', 'ADBaseWebSrv',
-function($http, $q, ADBaseWebSrv) {
+admin.service('ADICareServicesSrv', ['$http', '$q', 'ADBaseWebSrvV2',
+function($http, $q, ADBaseWebSrvV2) {
+	/**
+    * To fetch the details of icare services
+    * @return {object} icare service details
+    */
+	this.getIcareServices = function(){
+		
+		var deferred = $q.defer();
+		var url = '/api/hotel_settings/icare.json';
+
+		ADBaseWebSrvV2.getJSON(url).then(function(data) {
+			
+			//console.log(data.charge_codes);
+			
+			var cargeCodes = [];
+			
+			// Initial loading data to SNT VALUES dropdown.
+			angular.forEach(data.charge_codes,function(item, index) {
+	       		var obj = {"value": item.id, "name": item.description };
+	       		cargeCodes.push(obj);
+       		});
+			data.charge_codes = [];
+			data.charge_codes = cargeCodes;
+			//console.log(data);
+		    deferred.resolve(data);
+		},function(data){
+		    deferred.reject(data);
+		});	
+		return deferred.promise;
+	};
 	
+   /*
+    * To save icare services details
+    * @return {object} details of icare services
+    */
+	this.saveIcareServices = function(data){
+		var deferred = $q.defer();
+		var url = '/api/hotel_settings/change_settings';
+
+		ADBaseWebSrvV2.postJSON(url, data).then(function(data) {
+		    deferred.resolve(data);
+		},function(data){
+		    deferred.reject(data);
+		});	
+		return deferred.promise;
+	};
 }]);
