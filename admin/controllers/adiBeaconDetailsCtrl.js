5951df2a2fc105357593ed517643e40037996d71
Resheil <resheil@stayntouch.com>
2014-07-18 17:40:57 +0530

    CICO-6553 enabling/disabling toggle button action

--- admin/controllers/adiBeaconDetailsCtrl.js
+++ admin/controllers/adiBeaconDetailsCtrl.js
@@ -5,6 +5,10 @@ admin.controller('ADiBeaconDetailsCtrl',['$scope','$stateParams','$rootScope','$
   $scope.displayMessage = $scope.addmode ? "Add new iBeacon" :"Edit"+" "+$stateParams.action;
 	$scope.isIpad = navigator.userAgent.match(/iPad/i) != null;
 	$scope.isIpad = true;
+  $scope.message ={};
+  $scope.message.description ="";
+  $scope.message.title ="";
+
 
 	/**
     *   Method to go back to previous state.
@@ -22,6 +26,17 @@ admin.controller('ADiBeaconDetailsCtrl',['$scope','$stateParams','$rootScope','$
       $state.go('admin.dashboard', {menu : 0});
     }
   
+  };
+
+  $scope.toggleStatus = function(){
+
+    if($scope.isActive){
+      $scope.isActive = false;
+    }
+    else if($scope.message.description.length>0 && $scope.message.title.length>0){
+      $scope.isActive = ! $scope.isActive;
+    }
+      
   };
 
 }]);
