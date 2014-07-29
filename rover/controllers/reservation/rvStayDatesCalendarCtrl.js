bfe68644fcf5af1f5a6db2071b9aef612633dfc9
akhilastayntouch <akhila@stayntouch.com>
2014-07-29 15:49:43 +0530

    CICO-5247 scroller

--- rover/controllers/reservation/rvStayDatesCalendarCtrl.js
+++ rover/controllers/reservation/rvStayDatesCalendarCtrl.js
@@ -7,46 +7,16 @@ function($state, $stateParams, $rootScope, $scope, RVRoomRateCalendarSrv, $filte
 	$scope.heading = $filter('translate')('CHANGE_STAY_DATES_TITLE');
 	$scope.setTitle($scope.heading);
 	//scroller options
-	$scope.$parent.myScrollOptions = {
-		'edit_staydate_updatedDetails' : {
-			snap : false,
-			scrollbars : true,
-			vScroll : true,
-			vScrollbar : true,
-			hideScrollbar : false,
-			click : true,
-			tap : true
-		},
-		'edit_staydate_calendar' : {
-			snap : false,
-			scrollbars : true,
-			vScroll : true,
-			vScrollbar : true,
-			hideScrollbar : false,
-			click : true,
-			tap : true
-		}
+	$scope.setScroller('stay-dates-calendar'); 
 
-	};
-
-/*	this.dataAssign = function(data) {
+	this.dataAssign = function(data) {
 		//Data from Resolve method
 		$scope.stayDetails = data;
-		$scope.stayDetails.isOverlay = false;
-		//For future comparison / reset
-		console.log($scope.stayDetails);
 
 		$scope.checkinDateInCalender = $scope.confirmedCheckinDate = getDateObj($scope.stayDetails.arrival_date);
 		$scope.checkoutDateInCalender = $scope.confirmedCheckoutDate = getDateObj($scope.stayDetails.departure_date);
 
-		//Data for rightside Pane.
-		$scope.rightSideReservationUpdates = '';
-		$scope.roomSelected = $scope.stayDetails.room_number;
-		$scope.calendarNightDiff = '';
-		$scope.avgRate = '';
-		$scope.availableRooms = [];
-
-	};*/
+	};
 
 	this.renderFullCalendar = function() {
 		console.log('renderFullCalendar');
@@ -58,7 +28,6 @@ function($state, $stateParams, $rootScope, $scope, RVRoomRateCalendarSrv, $filte
 		$scope.eventSources.length = 0;
 		$scope.eventSources.push($scope.events);
 
-		console.log($scope.eventSources);
 		//calender options used by full calender, related settings are done here
 		$scope.fullCalendarOptions = {
 			height : 450,
@@ -80,61 +49,11 @@ function($state, $stateParams, $rootScope, $scope, RVRoomRateCalendarSrv, $filte
 		};
 	}
 	this.initialise = function() {
-		console.log('initialise');
 		$scope.eventSources = [];
-		/* var date = new Date();
-    var d = date.getDate();
-    var m = date.getMonth();
-    var y = date.getFullYear();
-
-
-    // For DEMO only  This can be created however you please. 
-    // *************
-    $scope.events = [
-        {
-        title: 'All Day Event',
-        start: new Date(y, m, 1)},
-    {
-        title: 'Long Event',
-        start: new Date(y, m, d - 5),
-        end: new Date(y, m, d - 2)},
-    {
-        id: 999,
-        title: 'Repeating Event',
-        start: new Date(y, m, d - 3, 16, 0),
-        allDay: false},
-    {
-        id: 999,
-        title: 'Repeating Event',
-        start: new Date(y, m, d + 4, 16, 0),
-        allDay: false},
-    {
-        title: 'Meeting',
-        start: new Date(y, m, d, 10, 30),
-        allDay: false},
-    {
-        title: 'Lunch',
-        start: new Date(y, m, d, 12, 0),
-        end: new Date(y, m, d, 14, 0),
-        allDay: false},
-    {
-        title: 'Birthday Party',
-        start: new Date(y, m, d + 1, 19, 0),
-        end: new Date(y, m, d + 1, 22, 30),
-        allDay: false},
-    {
-        title: 'Click for Google',
-        start: new Date(y, m, 28),
-        end: new Date(y, m, 29),
-        url: 'http://google.com/'}];
-
-        $scope.eventSources = [$scope.events];*/
-
 
 		var fetchSuccessCallback = function(data) {
-			console.log("fetchSuccessCallback");
 			$scope.$emit('hideLoader');
-			//that.dataAssign(data);
+			that.dataAssign(data);
 			$scope.stayDetails = data;
 			that.renderFullCalendar();
 		};
@@ -143,6 +62,5 @@ function($state, $stateParams, $rootScope, $scope, RVRoomRateCalendarSrv, $filte
 	};
 
 	this.initialise();
-
 
 }]); 
