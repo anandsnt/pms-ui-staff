eb7369afc52a7d16a2a83f830d76fa7773117d81
Resheil <resheil@stayntouch.com>
2014-08-04 14:29:43 +0530

    CICO-6057 implementing admin screen for changing bussiness date

--- admin/services/adSettingsAndParamsSrv.js
+++ admin/services/adSettingsAndParamsSrv.js
@@ -22,7 +22,7 @@ admin.service('settingsAndParamsSrv',['$http', '$q', 'ADBaseWebSrvV2', function(
     */	
 	this.saveSettingsAndParamsSrv = function(data){
 		var deferred = $q.defer();
-		var url = "/api/hotel_settings";		
+		var url = "/api/hotel_settings/change_settings";		
 		ADBaseWebSrvV2.postJSON(url,data).then(function(data) {
 			deferred.resolve(data);
 		},function(data){
