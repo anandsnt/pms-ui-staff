c483a7a3ceb818cc7f25660ba4b3cf2a57297f0d
Vijay Dev <--global>
2014-08-05 08:10:37 -0400

    CICO-8613: return to staycard errors fixed

--- rover/rvApp.js
+++ rover/rvApp.js
@@ -26,20 +26,20 @@ sntRover.run(['$rootScope', '$state', '$stateParams', function ($rootScope, $sta
 		fromState: 'rover.housekeeping.roomDetails',
 		toState  : 'rover.housekeeping.roomStatus'
 	}, {
-		fromState: 'rover.staycard.reservationcard.reservationdetails',
+		fromState: 'rover.reservation.staycard.reservationcard.reservationdetails',
 		toState  : 'rover.search'
 	}, {
 		fromState: 'rover.reservation.staycard.billcard',
 		toState  : 'rover.reservation.staycard.reservationcard.reservationdetails'
 	}, {
 		fromState: 'rover.staycard.nights',
-		toState  : 'rover.staycard.reservationcard.reservationdetails'
+		toState  : 'rover.reservation.staycard.reservationcard.reservationdetails'
 	}, {
 		fromState: 'rover.companycarddetails',
 		toState  : 'rover.companycardsearch'
 	}, {
-		fromState: 'rover.staycard.roomassignment',
-		toState  : 'rover.staycard.reservationcard.reservationdetails'
+		fromState: 'rover.reservation.staycard.roomassignment',
+		toState  : 'rover.reservation.staycard.reservationcard.reservationdetails'
 	}];
 
 
