3acfddf39beed30a547d093fa239e5abbb789407
Aswanth <aswanth@stayntouch.com>
2014-08-06 21:01:27 +0530

    CICO-5937 fixed the reported defects

--- rover/controllers/reservationCard/rvReservationDetailsController.js
+++ rover/controllers/reservationCard/rvReservationDetailsController.js
@@ -213,6 +213,9 @@ sntRover.controller('reservationDetailsController', ['$scope', '$rootScope', 'RV
 		});
 
 		$scope.openPaymentList = function() {
+			//Disable the feature when the reservation is checked out
+            if(!$scope.isNewsPaperPreferenceAvailable())
+                return;
 			$scope.reservationData.currentView = "stayCard";
 			$scope.$emit('SHOWPAYMENTLIST', $scope.reservationData);
 		};
