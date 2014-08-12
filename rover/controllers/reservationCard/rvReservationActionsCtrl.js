64523fde4d32ebc8a90c7363db130ee92f66b230
sdilipsam <dilips@stayntouch.com>
2014-08-12 15:52:03 +0530

    CICO-1403 UI : Cancellation popups placed

--- rover/controllers/reservationCard/rvReservationActionsCtrl.js
+++ rover/controllers/reservationCard/rvReservationActionsCtrl.js
@@ -1,113 +1,111 @@
-sntRover.controller('reservationActionsController', 
-	[
-		'$rootScope',
-		'$scope',
-		'ngDialog',
-		'RVChargeItems',
-		'$state',
-		'RVReservationCardSrv',
-		function($rootScope, $scope, ngDialog, RVChargeItems, $state, RVReservationCardSrv) {
-			BaseCtrl.call(this, $scope);
-			
-			$scope.displayTime = function(status){
-				var display = false;
-				if(status == 'CHECKEDIN' || status == 'CHECKING_OUT'){
-					display = true;
-				}
-				return display;
+sntRover.controller('reservationActionsController', [
+	'$rootScope',
+	'$scope',
+	'ngDialog',
+	'RVChargeItems',
+	'$state',
+	'RVReservationCardSrv',
+	function($rootScope, $scope, ngDialog, RVChargeItems, $state, RVReservationCardSrv) {
+		BaseCtrl.call(this, $scope);
+
+		$scope.displayTime = function(status) {
+			var display = false;
+			if (status == 'CHECKEDIN' || status == 'CHECKING_OUT') {
+				display = true;
+			}
+			return display;
+		};
+		$scope.displayBalance = function(status) {
+			var display = false;
+			if (status == 'CHECKING_IN' || status == 'CHECKEDIN' || status == 'CHECKING_OUT') {
+				display = true;
+			}
+			return display;
+		};
+		$scope.getBalanceAmountColor = function(balance) {
+			var balanceClass = "";
+			if (balance == 0 || balance == 0.00 || balance == 0.0) {
+				balanceClass = "green";
+			} else {
+				balanceClass = "red";
+			}
+			return balanceClass;
+		};
+
+		$scope.displayAddon = function(status) {
+			var display = false;
+			if (status == 'RESERVED' || status == 'CHECKING_IN' || status == 'CHECKEDIN' || status == 'CHECKING_OUT') {
+				display = true;
+			}
+			return display;
+		};
+
+		$scope.displayAddCharge = function(status) {
+			var display = false;
+
+			if (status == 'RESERVED' || status == 'CHECKING_IN' || status == 'CHECKEDIN' || status == 'CHECKING_OUT' || status == 'NOSHOW_CURRENT') {
+				display = true;
+			}
+			return display;
+		};
+
+		$scope.displayArrivalTime = function(status) {
+			var display = false;
+			if (status == 'CHECKING_IN' || status == 'NOSHOW_CURRENT') {
+				display = true;
+			}
+			return display;
+		};
+
+		$scope.getTimeColor = function(time) {
+			var timeColor = "";
+			if (time != null) {
+				timeColor = "time";
+			}
+			return timeColor;
+		};
+
+
+
+		$scope.openPostCharge = function() {
+
+			// pass on the reservation id
+			$scope.reservation_id = $scope.reservationData.reservation_card.reservation_id;
+
+			// translating this logic as such from old Rover
+			// api post param 'fetch_total_balance' must be 'true' when posted from 'staycard'
+			$scope.fetchTotalBal = true;
+
+			var callback = function(data) {
+				$scope.$emit('hideLoader');
+
+				$scope.fetchedData = data;
+
+				ngDialog.open({
+					template: '/assets/partials/postCharge/postCharge.html',
+					controller: 'RVPostChargeController',
+					scope: $scope
+				});
 			};
-			$scope.displayBalance = function(status){
-				var display = false;
-				if(status == 'CHECKING_IN' || status == 'CHECKEDIN' || status == 'CHECKING_OUT'){
-					display = true;
-				}
-				return display;
-			};
-			$scope.getBalanceAmountColor = function(balance){
-				var balanceClass = "";
-				if(balance == 0 || balance == 0.00 || balance == 0.0){
-					balanceClass = "green";
-				} else {
-					balanceClass = "red";
-				}
-				return balanceClass;
-			};
-			
-			$scope.displayAddon = function(status){
-				var display = false;
-				if(status == 'RESERVED' || status == 'CHECKING_IN' || status == 'CHECKEDIN' || status == 'CHECKING_OUT'){
-					display = true;
-				}
-				return display;
-			};
-			
-			$scope.displayAddCharge = function(status){
-				var display = false;
-				
-				if(status == 'RESERVED' || status == 'CHECKING_IN' || status == 'CHECKEDIN' || status == 'CHECKING_OUT' || status == 'NOSHOW_CURRENT'){
-					display = true;
-				}
-				return display;
-			};
-			
-			$scope.displayArrivalTime = function(status){
-				var display = false;
-				if(status == 'CHECKING_IN' || status == 'NOSHOW_CURRENT' ){
-					display = true;
-				}
-				return display;
-			};
-			
-			$scope.getTimeColor = function(time){
-				var timeColor = "";
-				if(time!=null){
-					timeColor = "time";
-				}
-				return timeColor;
-			};
+
+			$scope.invokeApi(RVChargeItems.fetch, $scope.reservation_id, callback);
+		};
 
 
+		// update the price on staycard.
+		var postchargeAdded = $scope.$on('postcharge.added', function(event, netPrice) {
+			var balance = parseFloat($scope.reservationData.reservation_card.balance_amount);
 
-			
-			$scope.openPostCharge = function() {
+			balance += netPrice;
 
-				// pass on the reservation id
-				$scope.reservation_id = $scope.reservationData.reservation_card.reservation_id;
+			$scope.reservationData.reservation_card.balance_amount = balance;
+		});
 
-				// translating this logic as such from old Rover
-				// api post param 'fetch_total_balance' must be 'true' when posted from 'staycard'
-				$scope.fetchTotalBal = true;
+		// the listner must be destroyed when no needed anymore
+		$scope.$on('$destroy', postchargeAdded);
 
-				var callback = function(data) {
-				    $scope.$emit( 'hideLoader' );
 
-				    $scope.fetchedData = data;
-
-		    		ngDialog.open({
-		        		template: '/assets/partials/postCharge/postCharge.html',
-		        		controller: 'RVPostChargeController',
-		        		scope: $scope
-		        	});
-				};
-
-				$scope.invokeApi(RVChargeItems.fetch, $scope.reservation_id, callback);
-			};
-			
-
-			// update the price on staycard.
-			var postchargeAdded = $scope.$on('postcharge.added', function(event, netPrice) {
-				var balance = parseFloat( $scope.reservationData.reservation_card.balance_amount );
-
-				balance += netPrice;
-
-				$scope.reservationData.reservation_card.balance_amount = balance;
-			});
-
-			// the listner must be destroyed when no needed anymore
-			$scope.$on( '$destroy', postchargeAdded );
-
-			
-			$scope.goToCheckin = function() {
+		$scope.goToCheckin = function() {
 			if (typeof $scope.guestCardData.userId != "undefined" && $scope.guestCardData.userId != "" && $scope.guestCardData.userId != null) {
 				if ($scope.guestCardData.contactInfo.email == '' || $scope.guestCardData.contactInfo.phone == '' || $scope.guestCardData.contactInfo.email == null || $scope.guestCardData.contactInfo.phone == null) {
 					$scope.$emit('showLoader');
@@ -150,88 +148,128 @@ sntRover.controller('reservationActionsController',
 				});
 			}
 		};
-		$scope.showPutInQueue = function(isQueueRoomsOn, isReservationQueued, reservationStatus){
+		$scope.showPutInQueue = function(isQueueRoomsOn, isReservationQueued, reservationStatus) {
 			var displayPutInQueue = false;
-			if(reservationStatus == 'CHECKING_IN' || reservationStatus == 'NOSHOW_CURRENT'){
-				if(isQueueRoomsOn == "true" && isReservationQueued == "false"){
+			if (reservationStatus == 'CHECKING_IN' || reservationStatus == 'NOSHOW_CURRENT') {
+				if (isQueueRoomsOn == "true" && isReservationQueued == "false") {
 					displayPutInQueue = true;
 				}
 			}
-			
+
 			return displayPutInQueue;
 		};
-		$scope.showRemoveFromQueue  = function(isQueueRoomsOn, isReservationQueued, reservationStatus){
+		$scope.showRemoveFromQueue = function(isQueueRoomsOn, isReservationQueued, reservationStatus) {
 			var displayPutInQueue = false;
-			if(reservationStatus == 'CHECKING_IN' || reservationStatus == 'NOSHOW_CURRENT'){
-				if(isQueueRoomsOn == "true" && isReservationQueued == "true"){
+			if (reservationStatus == 'CHECKING_IN' || reservationStatus == 'NOSHOW_CURRENT') {
+				if (isQueueRoomsOn == "true" && isReservationQueued == "true") {
 					displayPutInQueue = true;
 				}
 			}
 			return displayPutInQueue;
 		};
-		$scope.successPutInQueueCallBack = function(){
-			  $scope.$emit( 'hideLoader' );
-			  $scope.reservationData.reservation_card.is_reservation_queued = "true";
-			   RVReservationCardSrv.updateResrvationForConfirmationNumber($scope.reservationData.reservation_card.reservation_id, $scope.reservationData);
+		$scope.successPutInQueueCallBack = function() {
+			$scope.$emit('hideLoader');
+			$scope.reservationData.reservation_card.is_reservation_queued = "true";
+			RVReservationCardSrv.updateResrvationForConfirmationNumber($scope.reservationData.reservation_card.reservation_id, $scope.reservationData);
 		};
-		$scope.successRemoveFromQueueCallBack = function(){
-			  $scope.$emit( 'hideLoader' );
-			  $scope.reservationData.reservation_card.is_reservation_queued = "false";
-			  RVReservationCardSrv.updateResrvationForConfirmationNumber($scope.reservationData.reservation_card.reservation_id, $scope.reservationData);
+		$scope.successRemoveFromQueueCallBack = function() {
+			$scope.$emit('hideLoader');
+			$scope.reservationData.reservation_card.is_reservation_queued = "false";
+			RVReservationCardSrv.updateResrvationForConfirmationNumber($scope.reservationData.reservation_card.reservation_id, $scope.reservationData);
 		};
-		$scope.putInQueue = function(reservationId){
+		$scope.putInQueue = function(reservationId) {
 			var data = {
 				"reservationId": reservationId,
 				"status": "true"
 			};
 			$scope.invokeApi(RVReservationCardSrv.modifyRoomQueueStatus, data, $scope.successPutInQueueCallBack);
 		};
-		$scope.removeFromQueue = function(reservationId){
+		$scope.removeFromQueue = function(reservationId) {
 			var data = {
 				"reservationId": reservationId,
 				"status": false
 			};
 			$scope.invokeApi(RVReservationCardSrv.modifyRoomQueueStatus, data, $scope.successRemoveFromQueueCallBack);
 		};
-		
-		
-		$scope.openSmartBands = function() {
-	 		ngDialog.open({
-        		template: '/assets/partials/smartbands/rvSmartBandDialog.html',
-        		controller: 'RVSmartBandsController',
-        		className: 'ngdialog-theme-default1',
-        		closeByDocument: false,
-        		closeByEscape: false,
-        		scope: $scope
-        	});
 
+		$scope.cancelReservation = function(applyPenalty) {
+			console.log('cancelReservation', applyPenalty);
+			// TODO: Make calls to the cancel reservation API here
+		}
+
+		/**
+		 * This method handles cancelling an exisiting reservation or
+		 * reinstating a cancelled reservation CICO-1403 and CICO-6056(Sprint20 >>> to be implemented in the next sprint)
+		 */
+		$scope.toggleCancellation = function() {
+			/**
+			 * The below method checks through the cancellation policied to find if within the
+			 * cancellation period
+			 */
+			var inCancellationPeriod = function() {
+				return true;
+			}
+
+			// Note: current implementation will only handle cancelling reservations
+			// TODO: Upon actioning the cancel request, the system should check for cancellation policies that may have been linked to the rate code.
+			var cancelationPolicies = {};
 			
+			/**
+			 * If the reservation is within cancellation period, no action will take place.
+			 * If the reservation is outside of the cancellation period, a screen will display to show the cancellation rule.
+			 * [Cancellation period is the date and time set up in the cancellation rule]
+			 */
+			if (inCancellationPeriod()) {
+				ngDialog.open({
+					template: '/assets/partials/reservationCard/rvCancelReservation.html',
+					scope: $scope,
+					data: JSON.stringify({
+						state: 'PENALTY'
+					})
+				});
+			}
+		}
 
-			};
-		$scope.showSmartBandsButton = function(reservationStatus, icareEnabled){
+		$scope.openSmartBands = function() {
+			ngDialog.open({
+				template: '/assets/partials/smartbands/rvSmartBandDialog.html',
+				controller: 'RVSmartBandsController',
+				className: 'ngdialog-theme-default1',
+				closeByDocument: false,
+				closeByEscape: false,
+				scope: $scope
+			});
+
+
+
+		};
+		$scope.showSmartBandsButton = function(reservationStatus, icareEnabled) {
 			var showSmartBand = false;
-			if(icareEnabled){
-				if(reservationStatus == 'RESERVED' ||  reservationStatus == 'CHECKING_IN' || reservationStatus == 'CHECKEDIN' || reservationStatus == 'CHECKING_OUT' || reservationStatus == 'NOSHOW_CURRENT' || reservationStatus == 'CHECKEDOUT'){
+			if (icareEnabled) {
+				if (reservationStatus == 'RESERVED' || reservationStatus == 'CHECKING_IN' || reservationStatus == 'CHECKEDIN' || reservationStatus == 'CHECKING_OUT' || reservationStatus == 'NOSHOW_CURRENT' || reservationStatus == 'CHECKEDOUT') {
 					showSmartBand = true;
 				}
 			}
 			return showSmartBand;
 		};
 		//({reservationId:, clickedButton: 'checkoutButton'})
-	//	goToCheckoutButton(reservationData.reservation_card.reservation_id, 'checkoutButton');
-		$scope.goToCheckoutButton = function(reservationId, clickedButton, smartbandHasBalance){
-			if(smartbandHasBalance == "true"){
+		//	goToCheckoutButton(reservationData.reservation_card.reservation_id, 'checkoutButton');
+		$scope.goToCheckoutButton = function(reservationId, clickedButton, smartbandHasBalance) {
+			if (smartbandHasBalance == "true") {
 				$scope.clickedButton = clickedButton;
 				ngDialog.open({
-	        		template: '/assets/partials/smartbands/rvSmartbandListCheckoutscreen.html',
-	        		controller: 'RVSmartBandsCheckoutController',
-	        		className: 'ngdialog-theme-default1',
-	        		scope: $scope
-	        	});
+					template: '/assets/partials/smartbands/rvSmartbandListCheckoutscreen.html',
+					controller: 'RVSmartBandsCheckoutController',
+					className: 'ngdialog-theme-default1',
+					scope: $scope
+				});
 			} else {
-				$state.go("rover.reservation.staycard.billcard", {"reservationId" : reservationId, "clickedButton": clickedButton});
+				$state.go("rover.reservation.staycard.billcard", {
+					"reservationId": reservationId,
+					"clickedButton": clickedButton
+				});
 			}
 		};
 
-	}]
-);
+	}
+]);
