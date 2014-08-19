3d5e43bd744e6e808f43af5c00b8bab76a2278e9
Aswanth <aswanth@stayntouch.com>
2014-08-19 14:08:12 +0530

    CICO-6080 added the controler for the billing information

---
+++ rover/controllers/reservation/rvReservationBillingAddRoutePopupCtrl.js
@@ -0,0 +1,16 @@
+sntRover.controller('rvReservationBillingAddRoutePopupCtrl',['$scope','$rootScope','$filter','RVGuestCardLoyaltySrv', 'ngDialog', function($scope, $rootScope,$filter, RVGuestCardLoyaltySrv, ngDialog){
+	BaseCtrl.call(this, $scope);
+	
+	
+	$scope.closeDialog = function(){
+		ngDialog.close();
+	};
+
+	$scope.dimissLoaderAndDialog = function(){
+			$scope.$emit('hideLoader');
+			$scope.closeDialog();
+		};
+
+	
+	
+}]);
