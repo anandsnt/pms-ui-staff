c903489a4b6432bd93d291ad29083151bcea1516
akhilastayntouch <akhila@stayntouch.com>
2014-08-20 17:37:23 +0530

    CICO-8633 update to the calendar default date

--- rover/controllers/rateManager/SelectDateRangeModalCtrl.js
+++ rover/controllers/rateManager/SelectDateRangeModalCtrl.js
@@ -1,28 +1,28 @@
 sntRover.controller('SelectDateRangeModalCtrl',['$scope','ngDialog','$filter','dateFilter','$rootScope', function($scope,  ngDialog, $filter, dateFilter,$rootScope){
-
+	$s = $scope;
 	$scope.setUpData = function(){
 
-		//Workaround - By default the system date will be highlighted, 
-		//if we don't specify a default date. But we don't want to highlight any date 
-		//So removing the highligting in jQuery way
-		/*window.setTimeout(function(){
-           $('#modal').find('.ui-state-highlight.ui-state-active')
-           .removeClass('ui-state-highlight ui-state-active');     
-       	},0); 
-		*/
 		if($scope.currentFilterData.begin_date){
 			$scope.fromDate = $scope.currentFilterData.begin_date;
+		} else {
+			//From date is set as the current business date if date is not previousely selected
+			$scope.fromDate = $rootScope.businessDate;
 		}
 		if($scope.currentFilterData.end_date){
 			$scope.toDate = $scope.currentFilterData.end_date
+		} else {
+			//To date will be the 1st of next month if it is not previousely set
+			var now = tzIndependentDate($rootScope.businessDate);
+			var current = new Date(now.getFullYear(), now.getMonth() + 1, 1);
+			$scope.toDate = $filter('date')(current, $rootScope.dateFormatForAPI);
 		}
 
 		$scope.fromDateOptions = {
 			firstDay: 1,
 			changeYear: true,
 			changeMonth: true,
-			minDate: tzIndependentDate($rootScope.businessDate),
 			yearRange: "-5:+5", //Show 5 years in past & 5 years in future
+			defaultDate: "2014-09-03",
 			onSelect: function() {
 				if(tzIndependentDate($scope.fromDate) > tzIndependentDate($scope.toDate)){
 					$scope.toDate = $scope.fromDate;
@@ -34,8 +34,8 @@ sntRover.controller('SelectDateRangeModalCtrl',['$scope','ngDialog','$filter','d
 			firstDay: 1,
 			changeYear: true,
 			changeMonth: true,
-			minDate: tzIndependentDate($rootScope.businessDate),
 			yearRange: "-5:+5",
+			defaultDate: "2014-09-04",
 			onSelect: function() {
 
 				if(tzIndependentDate($scope.fromDate) > tzIndependentDate($scope.toDate)){
