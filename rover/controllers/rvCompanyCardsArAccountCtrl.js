87c5007a6ebbe90e7e689573be30c00458e03f34
Dipin <dipin@stayntouch.com>
2014-08-20 16:36:30 +0530

    CICO-6102 Changed account_id to id

--- rover/controllers/rvCompanyCardsArAccountCtrl.js
+++ rover/controllers/rvCompanyCardsArAccountCtrl.js
@@ -1,10 +1,10 @@
 
 sntRover.controller('companyCardArAccountCtrl', ['$scope','RVCompanyCardSrv',
 	function($scope,RVCompanyCardSrv) {
-		
+
 
 		var init = function(){
-			
+
 			BaseCtrl.call(this, $scope);
 
 			$scope.ARData = {};
@@ -20,39 +20,39 @@ sntRover.controller('companyCardArAccountCtrl', ['$scope','RVCompanyCardSrv',
 					refreshScroller();
 				}, 500);
 			});
-			
+
 		};
 		init();
 		var presentArDetails = {};
-		
-		// to set data to be compared from time to time 
+
+		// to set data to be compared from time to time
 		//to check if data has been edited or not
 		$scope.$on('ARDetailsRecieved',function(){
 			presentArDetails = JSON.parse(JSON.stringify($scope.arAccountDetails));
 		});
-		
+
 
 		$scope.saveNote = function(){
-			
+
 			var successCallbackOfsaveARNote = function(){
 				$scope.$emit("hideLoader");
 				$scope.arAccountNotes.ar_notes.push($scope.ARData.note);
 				$scope.ARData.note = "";
 				console.log($scope.arAccountNotes)
 			};
-			
-			var dataToSend = {"account_id":$scope.contactInformation.id,"note":$scope.ARData.note};
+
+			var dataToSend = {"id":$scope.contactInformation.id,"note":$scope.ARData.note};
 			$scope.invokeApi(RVCompanyCardSrv.saveARNote, dataToSend, successCallbackOfsaveARNote);
 
 		}
 		var updateArAccount = function(){
-			
+
 			var successCallbackOfsaveARDetails = function(data){
 				$scope.$emit("hideLoader");
 				console.log(data);
 			};
 			var dataToSend = {"id":$scope.contactInformation.id,"details":$scope.arAccountDetails};
-			
+
 			var presentArDetailsAfterEdit = JSON.parse(JSON.stringify($scope.arAccountDetails));
 		    var dataNotUpdated = false;
 		    //check if data was edited
