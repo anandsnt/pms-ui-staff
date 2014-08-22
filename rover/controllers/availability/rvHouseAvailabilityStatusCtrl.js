742ea5712317927ba29e3584b4472eed0865221a
akhilastayntouch <akhila@stayntouch.com>
2014-08-22 14:03:21 +0530

    CICO-7849 graph plot updates

--- rover/controllers/availability/rvHouseAvailabilityStatusCtrl.js
+++ rover/controllers/availability/rvHouseAvailabilityStatusCtrl.js
@@ -6,7 +6,7 @@ sntRover.controller('RVHouseAvailabilityStatusCtrl', [
 	'$filter',
 	'rvAvailabilitySrv',
 	function($scope, $timeout, ngDialog, $rootScope, $filter, rvAvailabilitySrv){
-		
+		$s = $scope;
 
 		$scope.$on('$includeContentLoaded', function(event){
 			$scope.$emit("hideLoader");
