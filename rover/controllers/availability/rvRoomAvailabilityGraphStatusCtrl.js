71b0a36c49eac6e1e262c2c3935892a8ff0df444
shahul <shahulp@qburst.com>
2014-08-22 18:08:27 +0530

    CICO-7850 Added basic ploting

--- rover/controllers/availability/rvRoomAvailabilityGraphStatusCtrl.js
+++ rover/controllers/availability/rvRoomAvailabilityGraphStatusCtrl.js
@@ -9,15 +9,59 @@ sntRover.controller('rvRoomAvailabilityGraphStatusController', [
   		$scope.hideMeBeforeFetching = true;	
 
   		$scope.availabilityGraphCongif = {
-			chart: 	{
-						type: 'area'
-					},
-			 xAxis: { 
-			 			categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                					'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-                	}
-
-  		};
+	 		chart: {
+	            type: 'areaspline',
+	        },
+	        title: {
+	            text: ''
+	        },
+	        xAxis: {
+	            categories: ['Bookable Rooms', 'Out of Order Rooms', 'Total Reserved', 'Available Rooms', 'Occupancy Actual', 'Occupancy Target'],
+	         	labels: {
+	            	stroke: 'red',
+	            	enabled: false, 
+	    		},
+				tickPosition: 'inside',
+				tickWidth: 0
+	        },
+	        yAxis: {
+	        	categories: [0,25,50,75,100,125,150,175],
+	            title: {
+	                text: ''
+	            },
+	            stackLabels: {
+	                enabled: true,
+	                style: {
+	                    fontWeight: 'bold',
+	                    color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+	                }
+	            },
+	            labels: {
+	                
+	            },
+				tickPosition: 'inside',
+				tickWidth: 0
+	        },
+	        series: [{
+	            name: 'Bookable Rooms',
+	            data: [5, 3, 4, 7, 2]
+	        }, {
+	            name: 'Out of Order Rooms',
+	            data: [2, 2, 3, 2, 1]
+	        }, {
+	            name: 'Total Reserved',
+	            data: [3, 4, 4, 2, 5]
+	        }, {
+	            name: 'Available Rooms',
+	            data: [3, 4, 7, 2, 2]
+	        }, {
+	            name: 'Occupancy Actual',
+	            data: [1, 2, 7, 5, 4]
+	        }, {
+	            name: 'Occupancy Target',
+	            data: [3, 4, 7, 2, 2]
+	        }]
+    	}
 
 
 	}
