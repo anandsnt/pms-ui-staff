66fb5e6c0e6fcd3924ea019deb0400f86ec2d754
sdilipsam <dilips@stayntouch.com>
2014-08-26 16:31:55 +0530

    CICO-6067 Additional details and other markup changes

---
+++ rover/controllers/reservationCard/rvReservationAdditionalController.js
@@ -0,0 +1,28 @@
+sntRover.controller('rvReservationAdditionalController', ['$rootScope', '$scope', 'RVReservationSummarySrv',
+	function($rootScope, $scope, RVReservationSummarySrv) {
+		BaseCtrl.call(this, $scope);
+		$scope.additionalDetails = {
+			hideDetails: true // TODO : make this flag true before sending to CR
+		}
+
+		$scope.updateAdditionalDetails = function() {
+			console.log('updateAdditionalDetails', $scope.reservationParentData.demographics);
+			var updateSuccess = function(data) {
+				$scope.$emit('hideLoader');
+				console.log('Data', data);
+			};
+			var updateFailure = function(data) {
+				$scope.$emit('hideLoader');
+				console.log('Data', data);
+			};
+
+			$scope.invokeApi(RVReservationSummarySrv.updateReservation, {
+				'reservationId': $scope.reservationParentData.reservationId,
+				'reservation_type_id': parseInt($scope.reservationParentData.demographics.reservationType),
+				'source_id': parseInt($scope.reservationParentData.demographics.source),
+				'market_segment_id': parseInt($scope.reservationParentData.demographics.market),
+				'booking_origin_id': parseInt($scope.reservationParentData.demographics.origin)
+			}, updateSuccess, updateFailure);
+		}
+	}
+]);
