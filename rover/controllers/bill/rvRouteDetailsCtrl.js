b7383c4a53bac42c07dc3063186ca153a8ebdd06
Aswanth <aswanth@stayntouch.com>
2014-08-26 20:15:38 +0530

    CICO-6080 updated the route detail screen designs

--- rover/controllers/bill/rvRouteDetailsCtrl.js
+++ rover/controllers/bill/rvRouteDetailsCtrl.js
@@ -1,6 +1,6 @@
 sntRover.controller('rvRouteDetailsCtrl',['$scope','$rootScope','$filter','RVGuestCardLoyaltySrv', 'ngDialog', function($scope, $rootScope,$filter, RVGuestCardLoyaltySrv, ngDialog){
 	BaseCtrl.call(this, $scope);
-	
+	$scope.isBillingGroup = true;
 		
 	
 }]);
