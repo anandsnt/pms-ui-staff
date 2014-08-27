0a6357dfdb6e9b5e2dd28262fdf02bec2a4f0f84
Aswanth <aswanth@stayntouch.com>
2014-08-27 14:40:38 +0530

    CICO-6080 added the payment navigations in route detail screen

--- rover/controllers/bill/rvAddRoutePaymentCtrl.js
+++
@@ -1,6 +0,0 @@
-sntRover.controller('rvAddRoutePaymentCtrl',['$scope','$rootScope','$filter','RVGuestCardLoyaltySrv', 'ngDialog', function($scope, $rootScope,$filter, RVGuestCardLoyaltySrv, ngDialog){
-	BaseCtrl.call(this, $scope);
-	
-		
-	
-}]);
--- rover/controllers/bill/rvRouteDetailsCtrl.js
+++ rover/controllers/bill/rvRouteDetailsCtrl.js
@@ -1,6 +1,7 @@
 sntRover.controller('rvRouteDetailsCtrl',['$scope','$rootScope','$filter','RVGuestCardLoyaltySrv', 'ngDialog', function($scope, $rootScope,$filter, RVGuestCardLoyaltySrv, ngDialog){
 	BaseCtrl.call(this, $scope);
 	$scope.isBillingGroup = true;
+	$scope.isAddPayment = false;
 		
 	
 }]);
