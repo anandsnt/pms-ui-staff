ce00a1882519755ff7bda7511b398348e6c8a044
Resheil <resheil@stayntouch.com>
2014-09-14 02:30:45 +0700

    CICO-6094 adding new bill option in post charges screen

--- rover/controllers/bill/rvBillCardCtrl.js
+++ rover/controllers/bill/rvBillCardCtrl.js
@@ -464,11 +464,8 @@ sntRover.controller('RVbillCardController',['$scope','$rootScope','$state','$sta
 		$scope.reservation_id = $scope.reservationBillData.reservation_id;
 
 		// pass down active bill no
-		/*************************************************/
-		/* commented out as per requirement in CICO-6094 */
-		/*************************************************/
-		//$scope.passActiveBillNo = activeBillNo;
-		/*************************************************/
+		
+		$scope.passActiveBillNo = activeBillNo;
 
 		// translating this logic as such from old Rover
 		// api post param 'fetch_total_balance' must be 'false' when posted from 'staycard'
