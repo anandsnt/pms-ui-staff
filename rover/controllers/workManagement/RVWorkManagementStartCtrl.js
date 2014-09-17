084d5d475ba922b739335d99efa3ebbe7cbd8afd
sdilipsam <dilips@stayntouch.com>
2014-09-17 10:27:37 +0530

    CICO-8605 Stub for workmanagement stats on the landing page

--- rover/controllers/workManagement/RVWorkManagementStartCtrl.js
+++ rover/controllers/workManagement/RVWorkManagementStartCtrl.js
@@ -10,6 +10,34 @@ sntRover.controller('RVWorkManagementStartCtrl', ['$rootScope', '$scope', 'ngDia
             });
         }
 
+        $scope.stateVariables = {
+            searching: false,
+            roomSearch: false, // Search can be either for rooms or an employee
+            viewingDate: {
+                date: "",
+                day: 25,
+                month: September,
+                year: 2014
+            }
+        };
+
+        $scope.workStats = {
+            "departures": {
+                "clean": 30,
+                "total_hours": "03.45",
+                "total_maids_required": 12,
+                "total_rooms_assigned": 2,
+                "total_rooms_completed": 3
+            },
+            "stay_overs": {
+                "clean": 12,
+                "total_hours": "1.45",
+                "total_maids_required": 12,
+                "total_rooms_assigned": 2,
+                "total_rooms_completed": 3
+            }
+        };
+
         $scope.closeDialog = function() {
             ngDialog.close();
         }
