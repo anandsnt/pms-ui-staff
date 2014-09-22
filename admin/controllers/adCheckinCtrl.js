9e0f2fd7dede618592cfad31a9859e1cc3a04c6f
resheilSNT <resheil@stayntouch.com>
2014-09-22 16:48:26 +0545

    CICO-9286 Web PRE Check In in admin zest

--- admin/controllers/adCheckinCtrl.js
+++ admin/controllers/adCheckinCtrl.js
@@ -13,10 +13,14 @@
       $scope.init = function(){
           $scope.checkinData = {};
           $scope.hours = ["01","02","03","04","05","06","07","08","09","10","11","12"];
-          $scope.minutes = ["00","15","30","45"];
+          $scope.minutes = -["00","15","30","45"];
           $scope.primeTimes = ["AM","PM"];
           $scope.isLoading = true;
           $scope.hideAlertOption = false;
+          $scope.prior_minutes = [];
+            for (var i=15; i<=300; i=i+15){
+              $scope.prior_minutes.push(i);
+            }
       };
 
       $scope.init();
@@ -39,10 +43,22 @@
           $scope.checkinData.is_send_checkin_staff_alert_flag = ($scope.checkinData.is_send_checkin_staff_alert === 'true') ? true:false;
           $scope.checkinData.is_notify_on_room_ready_flag = ($scope.checkinData.is_notify_on_room_ready === 'true') ? true:false;
           $scope.checkinData.require_cc_for_checkin_email_flag = ($scope.checkinData.require_cc_for_checkin_email=== 'true') ? true:false;
+          //$scope.checkinData.is_precheckin_only_flag = ($scope.checkinData.is_precheckin_only=== 'true') ? true:false;
+          //$scope.checkinData.is_sent_to_que_flag = ($scope.checkinData.is_sent_to_que=== 'true') ? true:false;
+$scope.checkinData.is_precheckin_only_flag = true;
+$scope.checkinData.is_sent_to_que_flag = true;
 
 
           $scope.$watch('checkinData.is_send_checkin_staff_alert_flag',function(){
              $scope.hideAlertOption = $scope.checkinData.is_send_checkin_staff_alert_flag ? false : true;
+          })
+
+          $scope.$watch('checkinData.is_precheckin_only_flag',function(){
+             $scope.hideAddOption = $scope.checkinData.is_precheckin_only_flag ? false : true;
+          })
+
+          $scope.$watch('checkinData.is_sent_to_que_flag',function(){
+             $scope.hidePriorMinutes = ($scope.checkinData.is_sent_to_que_flag === 'yes') ? false : true;
           })
               //to be confirmed 
               $scope.checkinData.checkin_alert_primetime = (!$scope.checkinData.checkin_alert_primetime)? "AM":$scope.checkinData.checkin_alert_primetime;
