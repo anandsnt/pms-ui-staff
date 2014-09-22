f5c399cff8aecf59188c030a36ff5d99b5e2a263
Graham Stewart <graham@stayntouch.com>
2014-09-22 14:37:08 -0400

    Changes to sync with 8633

--- rover/controllers/rateManager/SelectDateRangeModalCtrl.js
+++ rover/controllers/rateManager/SelectDateRangeModalCtrl.js
@@ -5,11 +5,12 @@ sntRover.controller('SelectDateRangeModalCtrl', ['filterDefaults', '$scope','ngD
 	var filterData = $scope.currentFilterData,
 		businessDate = tzIndependentDate($rootScope.businessDate),
 		from_date = tzIndependentDate($rootScope.businessDate), //new Date(),
-		to_date = new Date((new Date(from_date)).setMonth(from_date.getMonth() + 1));
+		to_date = new Date((new Date(from_date)).setMonth(from_date.getMonth() + 1)),
+		orig = _.extend({}, _.pick(filterData, 'begin_date', 'end_date'));
 
 	$scope.setUpData = function() {		
 		filterData.begin_date = _.isEmpty(filterData.begin_date) ? dateFilter(from_date, filterDefaults.UI_DATE_FORMAT) : tzIndependentDate(filterData.begin_date);
-		filterData.end_date = _.isEmpty(filterData.end_date) ? dateFilter(to_date, filterDefaults.UI_DATE_FORMAT) : tzIndependentDate(filterData.end_date);
+		//filterData.end_date = _.isEmpty(filterData.end_date) ? dateFilter(to_date, filterDefaults.UI_DATE_FORMAT) : tzIndependentDate(filterData.end_date);
 
 		$scope.fromDateOptions = {
 			changeYear: true,
@@ -23,17 +24,13 @@ sntRover.controller('SelectDateRangeModalCtrl', ['filterDefaults', '$scope','ngD
 			}
 		};
 
-		$scope.toDateOptions = {
-			changeYear: true,
-			changeMonth: true,
-			minDate: from_date,
-			yearRange: "0:+10",
+		$scope.toDateOptions = _.extend({
 			onSelect: function() {
 				if(tzIndependentDate(filterData.begin_date) > tzIndependentDate(filterData.end_date)) {
 					filterData.begin_date = filterData.end_date;
 				}
 			}
-		};
+		}, $scope.fromDateOptions);
 
 		$scope.errorMessage = '';
 	};
@@ -48,7 +45,14 @@ sntRover.controller('SelectDateRangeModalCtrl', ['filterDefaults', '$scope','ngD
 		ngDialog.close();
 	};
 
+	$scope.toggleUpdate = function() {
+		return _.isEmpty(filterData.begin_date) || _.isEmpty(filterData.end_date);
+	};
+
 	$scope.cancelClicked = function() {
+		filterData.begin_date = orig.begin_date;
+		filterData.end_date = orig.end_date;
+
 		ngDialog.close();
 	};
 }]);
