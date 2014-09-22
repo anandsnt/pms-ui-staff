9f075c14c6d11e44a2a04cc7ba629a491f980966
Vijay Dev <--global>
2014-09-22 10:14:01 -0400

    CICO-8470: all UI part complete

--- rover/controllers/housekeeping/rvHkRoomDetailsCtrl.js
+++ rover/controllers/housekeeping/rvHkRoomDetailsCtrl.js
@@ -136,8 +136,41 @@ sntRover.controller('RVHkRoomDetailsCtrl', [
 
 
 
-
 		$scope.roomDetails = roomDetailsData;
+
+
+		/** Method for getting the guest status icon class
+		  @return the guest status icon class  
+		*/
+		var getGuestStatusMapped = function(reservationStatus, isLateCheckout){
+		    var viewStatus = "";
+
+		    //If the guest is opted for late checkout
+		    if( isLateCheckout == "true" ){
+		        return "late-check-out";
+		    }
+
+		    //Determine the guest status class based on the reservation status
+		    if( "RESERVED" == reservationStatus) {
+		        viewStatus = "arrival";
+		    }else if( "CHECKING_IN" == reservationStatus ) {
+		        viewStatus = "check-in";
+		    }else if( "CHECKEDIN" == reservationStatus ) {
+		        viewStatus = "inhouse";
+		    }else if( "CHECKEDOUT" == reservationStatus ) {
+		        viewStatus = "departed";
+		    }else if( "CHECKING_OUT" == reservationStatus ) {
+		        viewStatus = "check-out";
+		    }else if( "CANCELED" == reservationStatus ) {
+		        viewStatus = "cancel";
+		    }else if( ("NOSHOW" == reservationStatus) || ("NOSHOW_CURRENT" == reservationStatus) ) {
+		        viewStatus = "no-show";
+		    }
+
+		    return viewStatus;
+		};
+		
+		$scope.guestViewStatus = getGuestStatusMapped( $scope.roomDetails.reservation_status, $scope.roomDetails.is_late_checkout );
 
 		// few getters and setters
 		$scope.updateRoom = function(key, value) {
