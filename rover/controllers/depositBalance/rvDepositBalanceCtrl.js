879f70694e1d024ae178424d8f59e4a993e82e11
soumya <soumya@stayntouch.com>
2014-09-24 19:06:08 +0530

    CICO-9587 Save card manually

--- rover/controllers/depositBalance/rvDepositBalanceCtrl.js
+++ rover/controllers/depositBalance/rvDepositBalanceCtrl.js
@@ -1,24 +1,95 @@
-sntRover.controller('RVDepositBalanceCtrl',['$scope','ngDialog', '$rootScope', 'RVDepositBalanceSrv','$stateParams', 
-	function($scope, ngDialog, $rootScope, RVDepositBalanceSrv, $stateParams){
+sntRover.controller('RVDepositBalanceCtrl',[
+					'$scope',
+					'ngDialog', 
+					'$rootScope', 
+					'RVDepositBalanceSrv',
+					'RVPaymentSrv',
+					'$stateParams', 
+		function($scope, 
+				ngDialog, 
+				$rootScope,
+				RVDepositBalanceSrv, 
+				RVPaymentSrv,
+				$stateParams){
 	BaseCtrl.call(this, $scope);
-
+console.log($scope);
 	//$scope.depositBalanceData = $scope.
+	var MLISessionId = "";
+	
+	try {
+			HostedForm.setMerchant($rootScope.MLImerchantId);
+		}
+	catch(err) {};
 	$scope.depositBalanceNewCardData = {};
+	$scope.makePaymentData = {};
 	$scope.clickedMakePayment = function(){
+		console.log($scope.depositBalanceNewCardData);
+		var swiped = false;
+		if(swiped){
+			
+		} else {
+			$scope.handleMLISessionId();
+		}
 		
 	};
-	// $scope.init = function(){
-// 	
-		// var reservationId = $stateParams.id;
-		// var dataToSrv = {
-			// "reservationId": reservationId
-		// };
-		// $scope.invokeApi(RVDepositBalanceSrv.getDepositBalanceData, dataToSrv, $scope.successCallBackFetchDepositBalance);
-// 	
-	// };
-	// $scope.init();
-	// $scope.successCallBackFetchDepositBalance = function(data){
-		// console.log("reached here")
-		// $scope.depositBalanceData = data;
-	// };
+	$scope.handleMLISessionId = function(){
+		if($scope.depositBalanceNewCardData.cardNumber.length>0){
+			$scope.fetchMLISessionId();
+		}
+		else{
+			// Client side validation added to eliminate a false session being retrieved in case of empty card number
+			$scope.errorMessage = ["There is a problem with your credit card"];
+		}
+	};
+	
+	$scope.fetchMLISessionId = function(){
+		 var sessionDetails = {};
+		 sessionDetails.cardNumber = $scope.depositBalanceNewCardData.cardNumber;
+		 sessionDetails.cardSecurityCode = $scope.depositBalanceNewCardData.ccv;
+		 sessionDetails.cardExpiryMonth = $scope.depositBalanceNewCardData.expiryMonth;
+		 sessionDetails.cardExpiryYear = $scope.depositBalanceNewCardData.expiryYear;
+		
+		 var callback = function(response){
+		 	$scope.$emit("hideLoader");
+		 	
+		 	if(response.status ==="ok"){
+
+		 		MLISessionId = response.session;
+		 		$scope.savePayment('manual');// call save payment details WS		 		
+		 	}
+		 	else{
+		 		$scope.errorMessage = ["There is a problem with your credit card"];
+		 	}			
+		 	$scope.$apply(); 	
+		 };
+
+		try {
+		    HostedForm.updateSession(sessionDetails, callback);	
+		    $scope.$emit("showLoader");
+		}
+		catch(err) {
+		   $scope.errorMessage = ["There was a problem connecting to the payment gateway."];
+		};
+		 		
+	};
+	
+	$scope.savePayment = function(type){
+		if(type === "manual"){
+			console.log("save payhhhhhhhhhhhhhhhhhhhhhhhhhhhhment");
+              var dataToApiToAddNewCard = {
+              	"session_id" : MLISessionId,
+              	"user_id" :$scope.guestCardData.userId
+              };
+              $scope.invokeApi(RVPaymentSrv.saveGuestPaymentDetails, dataToApiToAddNewCard, $scope.successSavePayment);
+		}
+	};
+	$scope.successSavePayment = function(data){
+		var dataToMakePaymentApi = {
+			"payment_id": data.data.id,
+			"reservation_id": $scope.reservationData.reservation_card.reservation_id,
+			"amount": $scope.makePaymentData.amount
+		};
+		 $scope.invokeApi(RVPaymentSrv.makePaymentOnDepositBalance, dataToApiToDoPayment);
+	};
+	
 }]);
--- rover/controllers/payment/rvPaymentCtrl.js
+++ rover/controllers/payment/rvPaymentCtrl.js
@@ -371,6 +371,8 @@ sntRover.controller('RVPaymentMethodCtrl',['$rootScope', '$scope', '$state', 'RV
 			$scope.savePayment();
 		}
 		else{
+			
+			/* in case the payment type is cc first we fetch MLI sesionId using card details and then save*/
 			if(parseInt($scope.saveData.selected_payment_type) ===0){
 				if($scope.saveData.card_number.length>0){
 					$scope.fetchMLISessionId();
