1fc6b415281159d10a65361eaaef1d2fec79b2d2
Resheil <resheil@stayntouch.com>
2014-09-25 20:03:02 +0530

    CICO-9284 adding service file

--- guestweb/preCheckin/controllers/preCheckinStayDetailsController.js
+++ guestweb/preCheckin/controllers/preCheckinStayDetailsController.js
@@ -1,14 +1,24 @@
 (function() {
-	var preCheckinStayDetailsController = function($scope, preCheckinSrv,$rootScope,$state) {
+	var preCheckinStayDetailsController = function($scope, preCheckinSrv,$rootScope,$state,$modal) {
 	
 	var init = function(){
-		$scope.hours = ["01","02","03","04","05","06","07","08","09","10","11","12"];
-        $scope.minutes = ["00","15","30","45"];
-        $scope.stayDetails = {};
-        //set 3:00pm as default
-        $scope.stayDetails.hour = "03";
-        $scope.stayDetails.minute = "00";
-        $scope.stayDetails.primeTime = "PM";
+		
+       $scope.stayDetails = {};   
+       $scope.hours = ["01","02","03","04","05","06","07","08","09","10","11","12"];
+       $scope.minutes = ["00","15","30","45"];    
+
+	   $scope.errorOpts = {
+	      backdrop: true,
+	      backdropClick: true,
+	      templateUrl: '/assets/preCheckin/partials/preCheckinErrorModal.html',
+	      controller: ccVerificationModalCtrl,
+	      resolve: {
+	        errorMessage:function(){
+	          return "Please select an estimated arrival time";
+	        }
+	      }
+	    };
+
 
 	};
 	init();
@@ -16,15 +26,14 @@
 	
 
 	$scope.postStayDetails = function(){
-		// var data = {
-		// 	"year":$scope.yearSelected,
-		// 	"month":$scope.monthSelected,
-		// 	"primeTime":primeTimeSelected,
-		// 	"comment":comment,
-		// 	""
-		// }
+	
+		if(!$scope.stayDetails.hour  || !$scope.stayDetails.minute  ||!$scope.stayDetails.primeTime){
+			$modal.open($scope.errorOpts); // error modal popup
+		}
+		else{
+			//call
 
-		// preCheckinSrv.completePrecheckin().then(function(response) {
+			// preCheckinSrv.completePrecheckin().then(function(response) {
 	// 		$scope.isLoading = false;	
 	// 		var success = (response.status != "failure") ? true : false;
 			// if(success){
@@ -34,10 +43,10 @@
 	// 		$scope.netWorkError = true;
 	// 		$$scope.isLoading = false;
 	// });
-		console.log($scope.stayDetails)
-		//navigate to next page
-		$state.go('preCheckinStatus');
+			$state.go('preCheckinStatus');
 
+		}
+		
 	}
 	
 
@@ -45,7 +54,7 @@
 
 var dependencies = [
 '$scope',
-'preCheckinSrv','$rootScope','$state',
+'preCheckinSrv','$rootScope','$state','$modal',
 preCheckinStayDetailsController
 ];
 
