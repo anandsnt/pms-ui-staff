0342370e2e6346319c2c9adb11ed771c72de2c8e
Vijay Dev <--global>
2014-09-29 09:08:38 -0400

    m

--- rover/rvRouters/rvReportsRouter.js
+++ rover/rvRouters/rvReportsRouter.js
@@ -5,14 +5,14 @@ angular.module('reportsModule', [])
         url: '/reports',
         templateUrl: '/assets/partials/reports/rvReports.html',
         controller: 'RVReportsMainCtrl',
-        // resolve: {
-        //     reportsResponse: function(RVreportsSrv) {
-        //         if ( !!RVreportsSrv ) {
-        //             return RVreportsSrv.fetchReportList();
-        //         } else {
-        //             return {};
-        //         }
-        //     }
-        // }
+        resolve: {
+            reportsResponse: function(RVreportsSrv) {
+                if ( !!RVreportsSrv ) {
+                    return RVreportsSrv.fetchReportList();
+                } else {
+                    return {};
+                }
+            }
+        }
     });
 });
