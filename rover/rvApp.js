807b5d7fcd148db2b40019c59992ae3118aa9fff
sdilipsam <dilips@stayntouch.com>
2014-10-02 16:10:57 +0530

    HK Merges

--- rover/rvApp.js
+++ rover/rvApp.js
@@ -1,4 +1,4 @@
-var sntRover = angular.module('sntRover',['ui.router', 'ui.utils', 'ng-iscroll', 'highcharts-ng', 'ngAnimate','ngDialog', 'ngSanitize', 'pascalprecht.translate','ui.date','ui.calendar', 'dashboardModule', 'companyCardModule', 'stayCardModule', 'housekeepingModule', 'reportsModule', 'cacheVaultModule', 'twoMonthscalendar','acute.select', 'documentTouchMovePrevent', 'divTouchMoveStopPropogate', 'pasvaz.bindonce', 'sharedHttpInterceptor', 'orientationInputBlurModule']);
+var sntRover = angular.module('sntRover',['ui.router', 'ui.utils', 'ng-iscroll', 'highcharts-ng', 'ngAnimate','ngDialog', 'ngSanitize', 'pascalprecht.translate','ui.date','ui.calendar', 'dashboardModule', 'companyCardModule', 'stayCardModule', 'housekeepingModule', 'reportsModule', 'cacheVaultModule', 'twoMonthscalendar','acute.select', 'documentTouchMovePrevent', 'divTouchMoveStopPropogate', 'pasvaz.bindonce', 'sharedHttpInterceptor', 'orientationInputBlurModule',  'multi-select', 'ngDragDrop']);
 
 //adding shared http interceptor, which is handling our webservice errors & in future our authentication if needed
 sntRover.config(function ($httpProvider) {
