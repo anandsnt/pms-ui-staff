d01c41e39ed9ad6f2f24978b556b1608ebe3ac3a
sdilipsam <dilips@stayntouch.com>
2014-10-14 17:14:14 +0530

    CICO-8605 CICO-9120 reset fix for the multisheet

--- rover/controllers/workManagement/RVWorkManagementMultiSheetCtrl.js
+++ rover/controllers/workManagement/RVWorkManagementMultiSheetCtrl.js
@@ -65,7 +65,7 @@ sntRover.controller('RVWorkManagementMultiSheetCtrl', ['$rootScope', '$scope', '
 
 						_.each($scope.multiSheetState.selectedEmployees, function(employee) {
 							var employee = employee.id;
-							if (!$scope.multiSheetState.assignments[employee]) {
+							if (!$scope.multiSheetState.assignments[employee] || reset) {
 								$scope.multiSheetState.assignments[employee] = {};
 								$scope.multiSheetState.assignments[employee].rooms = [];
 								$scope.multiSheetState.assignments[employee].summary = {
