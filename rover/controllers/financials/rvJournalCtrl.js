8566346b59009616135f92e2081aac78c24bf77a
krishobh <krishobh@stayntouch.com>
2014-10-17 19:55:25 +0530

    CICO-6105 Implemented drawer

--- rover/controllers/financials/rvJournalCtrl.js
+++ rover/controllers/financials/rvJournalCtrl.js
@@ -3,8 +3,30 @@ sntRover.controller('RVJournalController', ['$scope','$filter','$stateParams',	f
 	BaseCtrl.call(this, $scope);	
 	// Setting up the screen heading.
 	$scope.$emit('HeaderChanged', $filter('translate')('MENU_JOURNAL'));
-	console.log($stateParams.id);
+	
 	$scope.activeTab = $stateParams.id=='' ? 0 : $stateParams.id;
+	$scope.printBoxClass = '';
+	var resizableMinHeight = 0;
+	var resizableMaxHeight = 90;
+
+	$scope.resizableOptions = {
+			minHeight: resizableMinHeight,
+			maxHeight: resizableMaxHeight,
+			handles: 's',
+			resize: function(event, ui) {
+				$scope.printBoxClass = 'open';
+			},
+			stop: function(event, ui) {
+				preventClicking = true;
+				$scope.eventTimestamp = event.timeStamp;
+			}
+	};
+
+	$scope.closeDrawer = function(event){
+		console.log("clicked closeDrawer");
+		$scope.printBoxHeight = resizableMinHeight;
+		$scope.printBoxClass = '';
+	};
 
 
 }]);
