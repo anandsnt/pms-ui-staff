78a63c990e32b17d1c623fa7ef7c72d4bc5c690c
Aswanth <aswanth@stayntouch.com>
2014-10-17 16:54:43 +0530

    CICO-6105 updated the calendar pop up

--- rover/controllers/financials/rvJournalCtrl.js
+++ rover/controllers/financials/rvJournalCtrl.js
@@ -1,10 +1,17 @@
-sntRover.controller('RVJournalController', ['$scope','$filter','$stateParams',	function($scope,$filter,$stateParams) {
+sntRover.controller('RVJournalController', ['$scope','$filter','$stateParams', 'ngDialog',	function($scope,$filter,$stateParams, ngDialog) {
 		
 	BaseCtrl.call(this, $scope);	
 	// Setting up the screen heading.
 	$scope.$emit('HeaderChanged', $filter('translate')('MENU_JOURNAL'));
 	console.log($stateParams.id);
 	$scope.activeTab = $stateParams.id=='' ? 0 : $stateParams.id;
-
+	$scope.popupCalendar = function() {
+      ngDialog.open({
+        template: '/assets/partials/guestCard/contactInfoCalendarPopup.html',
+        controller: 'RVContactInfoDatePickerController',
+        className: 'single-date-picker',
+        scope: $scope
+      });
+    };
 
 }]);
