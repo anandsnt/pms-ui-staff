78b86f9dc886b8bcd3243d23c62dae9629442dfb
krishobh <krishobh@stayntouch.com>
2014-10-20 14:47:15 +0530

    CICO-6105 Added print box

--- rover/controllers/financials/rvJournalCtrl.js
+++ rover/controllers/financials/rvJournalCtrl.js
@@ -10,16 +10,16 @@ sntRover.controller('RVJournalController', ['$scope','$filter','$stateParams', '
 	var resizableMaxHeight = 90;
 
 	$scope.resizableOptions = {
-			minHeight: resizableMinHeight,
-			maxHeight: resizableMaxHeight,
-			handles: 's',
-			resize: function(event, ui) {
-				$scope.printBoxClass = 'open';
-			},
-			stop: function(event, ui) {
-				preventClicking = true;
-				$scope.eventTimestamp = event.timeStamp;
-			}
+		minHeight: resizableMinHeight,
+		maxHeight: resizableMaxHeight,
+		handles: 's',
+		resize: function(event, ui) {
+			$scope.printBoxClass = 'open';
+		},
+		stop: function(event, ui) {
+			preventClicking = true;
+			$scope.eventTimestamp = event.timeStamp;
+		}
 	};
 
 	$scope.closeDrawer = function(event){
@@ -28,12 +28,12 @@ sntRover.controller('RVJournalController', ['$scope','$filter','$stateParams', '
 		$scope.printBoxClass = '';
 	};
 	$scope.popupCalendar = function() {
-      ngDialog.open({
-        template: '/assets/partials/financials/rvJournalCalendarPopup.html',
-        controller: 'RVJournalDatePickerController',
-        className: 'single-date-picker',
-        scope: $scope
-      });
+      	ngDialog.open({
+	        template: '/assets/partials/financials/rvJournalCalendarPopup.html',
+	        controller: 'RVJournalDatePickerController',
+	        className: 'single-date-picker',
+	        scope: $scope
+      	});
     };
 
     $scope.date = $rootScope.businessDate;
