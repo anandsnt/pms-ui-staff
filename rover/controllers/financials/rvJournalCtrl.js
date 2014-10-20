c1671d39dceb1902e5681ef7dc2f288121e3add9
krishobh <krishobh@stayntouch.com>
2014-10-20 18:20:30 +0530

    CICO-6105 Added drawer with resize effect

--- rover/controllers/financials/rvJournalCtrl.js
+++ rover/controllers/financials/rvJournalCtrl.js
@@ -3,30 +3,61 @@ sntRover.controller('RVJournalController', ['$scope','$filter','$stateParams', '
 	BaseCtrl.call(this, $scope);	
 	// Setting up the screen heading.
 	$scope.$emit('HeaderChanged', $filter('translate')('MENU_JOURNAL'));
-	
 	$scope.activeTab = $stateParams.id=='' ? 0 : $stateParams.id;
-	$scope.printBoxClass = '';
+
+	$scope.isDrawerOpened = false;
 	var resizableMinHeight = 0;
 	var resizableMaxHeight = 90;
-
+	$scope.eventTimestamp ='';
+	$scope.printBoxHeight =	resizableMinHeight;
+	// Drawer resize options.
 	$scope.resizableOptions = {
 		minHeight: resizableMinHeight,
 		maxHeight: resizableMaxHeight,
 		handles: 's',
 		resize: function(event, ui) {
-			$scope.printBoxClass = 'open';
+			var height = $(this).height();
+			$scope.printBoxHeight = height;
+			if (height > 5){
+				$scope.isDrawerOpened = true;
+			}
+			else if(height < 5){
+				$scope.isDrawerOpened = false;
+			}
 		},
 		stop: function(event, ui) {
 			preventClicking = true;
 			$scope.eventTimestamp = event.timeStamp;
 		}
 	};
-
-	$scope.closeDrawer = function(event){
-		console.log("clicked closeDrawer");
-		$scope.printBoxHeight = resizableMinHeight;
-		$scope.printBoxClass = '';
+	// To handle click on drawer handle - open/close.
+	$scope.clickedDrawer = function($event){
+		$event.stopPropagation();
+		$event.stopImmediatePropagation();
+		if(getParentWithSelector($event, document.getElementsByClassName("ui-resizable-handle")[0])){
+			if(parseInt($scope.eventTimestamp)) {
+				if(($event.timeStamp - $scope.eventTimestamp)<2){
+					return;
+				}
+			}
+			if($scope.printBoxHeight == resizableMinHeight || $scope.printBoxHeight == resizableMaxHeight) {
+				if ($scope.isDrawerOpened) {
+					$scope.printBoxHeight = resizableMinHeight;
+					$scope.isDrawerOpened = false;
+				}
+				else if(!$scope.isDrawerOpened) {
+					$scope.printBoxHeight = resizableMaxHeight;
+					$scope.isDrawerOpened = true;
+				}
+			}
+			else{
+				// mid way click : close guest card
+				$scope.printBoxHeight = resizableMinHeight;
+				$scope.isDrawerOpened = false;
+			}
+		}
 	};
+	// Calendar popup.
 	$scope.popupCalendar = function() {
       	ngDialog.open({
 	        template: '/assets/partials/financials/rvJournalCalendarPopup.html',
