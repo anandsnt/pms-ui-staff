45514ac6e3e8327340a90bae8138cca73d4ca629
krishobh <krishobh@stayntouch.com>
2014-10-22 12:16:07 +0530

    CICO-10311 handled notification message

--- admin/services/adExternalPmsConnectivitySrv.js
+++ admin/services/adExternalPmsConnectivitySrv.js
@@ -28,6 +28,7 @@ admin.service('ADExternalPmsConnectivitySrv',['$http', '$q', 'ADBaseWebSrv','ADB
 		ADBaseWebSrvV2.postJSON(url, data).then(function(data) {
 		    deferred.resolve(data);
 		},function(data){
+			if(typeof data === 'string') data = [data];
 		    deferred.reject(data);
 		});	
 		return deferred.promise;
