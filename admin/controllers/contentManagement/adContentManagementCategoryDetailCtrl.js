7a28111a3193465b11fccaa78cc64543b4f4d534
Aswanth <aswanth@stayntouch.com>
2014-10-24 12:52:50 +0530

    CICO-6028 updated the category and item creation

--- admin/controllers/contentManagement/adContentManagementCategoryDetailCtrl.js
+++ admin/controllers/contentManagement/adContentManagementCategoryDetailCtrl.js
@@ -1,9 +1,45 @@
-admin.controller('ADContentManagementCategoryDetailCtrl',['$scope', '$state', 'ADContentManagementSrv', 'ngTableParams','$filter', '$anchorScroll', '$timeout',  '$location', 
- function($scope, $state, ADContentManagementSrv, ngTableParams, $filter, $anchorScroll, $timeout, $location){
+admin.controller('ADContentManagementCategoryDetailCtrl',['$scope', '$state', '$stateParams', 'ADContentManagementSrv', 'ngTableParams','$filter', '$anchorScroll', '$timeout',  '$location', 
+ function($scope, $state, $stateParams, ADContentManagementSrv, ngTableParams, $filter, $anchorScroll, $timeout, $location){
 	
 	$scope.errorMessage = '';
 	BaseCtrl.call(this, $scope);
+	$scope.fileName = "Choose file..."
+	$scope.data = {	            
+	            "type": "SECTION",
+	            "status": "AVAILABLE",
+	            "name": "",
+	            "icon": ''
+            }
 
+    
+
+	$scope.fetchCategory = function(){
+		var fetchCategorySuccessCallback = function(data){
+			$scope.$emit('hideLoader');
+			$scope.data = data;
+		}
+		$scope.invokeApi(ADContentManagementSrv.fetchComponent, $stateParams.id , fetchCategorySuccessCallback);
+	}
+
+	if($stateParams.id != 'new'){
+		$scope.isAddMode = false;
+		$scope.fetchCategory();
+	}
+	else{
+		$scope.isAddMode = true;
+	}	
+
+	$scope.goBack = function(){
+        $state.go('admin.cmscomponentSettings');                  
+	}
+
+	$scope.saveCategory = function(){
+		var saveCategorySuccessCallback = function(data){
+			$scope.$emit('hideLoader');
+			
+		}
+		$scope.invokeApi(ADContentManagementSrv.saveComponent, $scope.data , saveCategorySuccessCallback);
+	}
 		
 
 }]);
--- admin/controllers/contentManagement/adContentManagementItemDetailCtrl.js
+++ admin/controllers/contentManagement/adContentManagementItemDetailCtrl.js
@@ -1,10 +1,46 @@
-admin.controller('ADContentManagementItemDetailCtrl',['$scope', '$state', 'ADContentManagementSrv', 'ngTableParams','$filter', '$anchorScroll', '$timeout',  '$location', 
- function($scope, $state, ADContentManagementSrv, ngTableParams, $filter, $anchorScroll, $timeout, $location){
+admin.controller('ADContentManagementItemDetailCtrl',['$scope', '$state', '$stateParams', 'ADContentManagementSrv', 'ngTableParams','$filter', '$anchorScroll', '$timeout',  '$location', 
+ function($scope, $state, $stateParams, ADContentManagementSrv, ngTableParams, $filter, $anchorScroll, $timeout, $location){
 	
 	$scope.errorMessage = '';
 	BaseCtrl.call(this, $scope);
 	
-	 
+	 $scope.fileName = "Choose file..."
+	$scope.data = {	            
+	            "type": "SECTION",
+	            "status": "AVAILABLE",
+	            "name": "",
+	            "icon": ''
+            }
+
+    
+
+	$scope.fetchItem = function(){
+		var fetchItemSuccessCallback = function(data){
+			$scope.$emit('hideLoader');
+			$scope.data = data;
+		}
+		$scope.invokeApi(ADContentManagementSrv.fetchComponent, $stateParams.id , fetchItemSuccessCallback);
+	}
+
+	if($stateParams.id != 'new'){
+		$scope.isAddMode = false;
+		$scope.fetchItem();
+	}
+	else{
+		$scope.isAddMode = true;
+	}	
+
+	$scope.goBack = function(){
+        $state.go('admin.cmscomponentSettings');                  
+	}
+
+	$scope.saveItem = function(){
+		var saveItemSuccessCallback = function(data){
+			$scope.$emit('hideLoader');
+			
+		}
+		$scope.invokeApi(ADContentManagementSrv.saveComponent, $scope.data , saveItemSuccessCallback);
+	}
 
 	
 
