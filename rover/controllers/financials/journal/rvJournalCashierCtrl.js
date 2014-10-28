a911450ae6dfac0c6c4caa47c4858b17aa0dcea9
Resheil <resheil@stayntouch.com>
2014-10-28 11:52:09 +0530

    CICO-6104 adding webservices

--- rover/controllers/financials/journal/rvJournalCashierCtrl.js
+++ rover/controllers/financials/journal/rvJournalCashierCtrl.js
@@ -1,24 +1,30 @@
 sntRover.controller('RVJournalCashierController', ['$scope','RVJournalSrv',function($scope,RVJournalSrv) {
-	BaseCtrl.call(this, $scope);
-
-	$scope.setScroller('cashier_history', {});
-	setTimeout(function(){$scope.refreshScroller('cashier_history');}, 3000);
-
+	
+    //fetch history details
     var fetchHistoryDetails = function(data){
          var fetchDetailsSuccessCallback = function(data){
             $scope.$emit('hideLoader');
             $scope.details = [];
             $scope.details = data;
+            $scope.setScroller('cashier_shift', {});
+            setTimeout(function(){$scope.refreshScroller('cashier_shift');}, 500);
         }
         var url = '/sample_json/journal/journal_cashier_details_'+data.id+'.json';
         $scope.invokeApi(RVJournalSrv.fetchCashierDetails, url, fetchDetailsSuccessCallback);  
     };
-    fetchHistoryDetails({'id':$scope.data.cashierData.history[0].status});
-   
-	$scope.setScroller('cashier_shift', {});
-	setTimeout(function(){$scope.refreshScroller('cashier_shift');}, 3000);
+
+    //init
+    var init = function(){
+        BaseCtrl.call(this, $scope);
+        $scope.selectedHistory = 0;
+        $scope.setScroller('cashier_history', {});
+        setTimeout(function(){$scope.refreshScroller('cashier_history');}, 500);
+        fetchHistoryDetails({'id':$scope.data.cashierData.history[0].status});
+    }
+
+    init();	
 	
-	$scope.selectedHistory = 0;
+    //click action of individual history
 	$scope.historyClicked = function(index){
 		$scope.selectedHistory = index;
         fetchHistoryDetails({'id':$scope.data.cashierData.history[index].status})
