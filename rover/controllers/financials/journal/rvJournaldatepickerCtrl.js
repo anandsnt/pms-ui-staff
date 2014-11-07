c822c1bfbdaa3525e984b5b29fad8935dce42ef2
krishobh <krishobh@stayntouch.com>
2014-11-07 13:02:55 +0530

    CICO-6105 Fixed issue on date picker

--- rover/controllers/financials/journal/rvJournaldatepickerCtrl.js
+++ rover/controllers/financials/journal/rvJournaldatepickerCtrl.js
@@ -1,32 +1,46 @@
 sntRover.controller('RVJournalDatePickerController',['$scope','$rootScope','ngDialog','dateFilter',function($scope,$rootScope,ngDialog,dateFilter){
 
-$scope.setUpData = function(){
-   $scope.dateOptions = {
-     changeYear: true,
-     changeMonth: true,
-     maxDate: tzIndependentDate($rootScope.businessDate),
-     yearRange: "-100:+0",
-      onSelect: function(dateText, inst) {
-      	if($scope.clickedOn === 'FROM'){
-          $scope.data.fromDate = $scope.date;
-          $rootScope.$emit('fromDateChanged');
-        } 
-        else if($scope.clickedOn === 'TO'){
-          $scope.data.toDate = $scope.date;
-          $rootScope.$emit('toDateChanged');
-        } 
-        else if($scope.clickedOn === 'CASHIER'){
-          $scope.data.cashierDate = $scope.date;
-          $scope.$emit('cashierDateChanged');
-        }
-        else if($scope.clickedOn === 'PAYMENT'){
-          $scope.data.paymentDate = $scope.date;
-          $rootScope.$emit('paymentDateChanged');
-        }
-        ngDialog.close();
-      }
+    if($scope.clickedOn === 'FROM'){
+        $scope.date = $scope.data.fromDate;
+    } 
+    else if($scope.clickedOn === 'TO'){
+        $scope.date = $scope.data.toDate;
+    } 
+    else if($scope.clickedOn === 'CASHIER'){
+        $scope.date = $scope.data.cashierDate;
     }
-};
-$scope.setUpData();
+    else if($scope.clickedOn === 'PAYMENT'){
+        $scope.date = $scope.data.paymentDate;
+    }
+
+    $scope.setUpData = function(){
+        $scope.dateOptions = {
+           changeYear: true,
+           changeMonth: true,
+           maxDate: tzIndependentDate($rootScope.businessDate),
+           yearRange: "-100:+0",
+           onSelect: function(dateText, inst) {
+                if($scope.clickedOn === 'FROM'){
+                  $scope.data.fromDate = $scope.date;
+                  $rootScope.$emit('fromDateChanged');
+                } 
+                else if($scope.clickedOn === 'TO'){
+                  $scope.data.toDate = $scope.date;
+                  $rootScope.$emit('toDateChanged');
+                } 
+                else if($scope.clickedOn === 'CASHIER'){
+                  $scope.data.cashierDate = $scope.date;
+                  $scope.$emit('cashierDateChanged');
+                }
+                else if($scope.clickedOn === 'PAYMENT'){
+                  $scope.data.paymentDate = $scope.date;
+                  $rootScope.$emit('paymentDateChanged');
+                }
+                ngDialog.close();
+            }
+        }
+    };
+
+    $scope.setUpData();
 
 }]);
