1a57ecba993148adb6b24e9bc36ed5a8814958ef
Graham Stewart <graham@stayntouch.com>
2014-11-06 23:43:55 -0500

    future class structure

--- rover/services/diary/rvDiaryModelSrv.js
+++ rover/services/diary/rvDiaryModelSrv.js
@@ -1,38 +1,3 @@
-//sntRover
-//.factory('rvDiaryModelSrv', function() {
-	/*var hops = Object.prototype.hasOwnProperty,
-		slice = Array.prototype.slice;
-
-	var set, get;
-
-	set = function(field, val) {
-
-	};
-
-	get = function(field, val) {
-
-	};
-
-	function Collection() {
-		if(!(this instanceof Collection)) {
-			return new Collection(slice.call(arguments));
-		}
-
-		Array.apply(this, args);
-	}
-
-	Collection.prototype = Object.create(Array.prototype);
-	Collection.prototype.constructor = Collection;
-
-
-	function List() {
-		Collection.apply(this, Array.prototype.slice.call(arguments));
-	}
-
-	List.prototype = Object.create(Collection.prototype);
-	List.prototype.constructor = List;
-*/
-//});
 sntRover
 .factory('rvDiaryModelSrv', ['$q', 
 	function($q) {
