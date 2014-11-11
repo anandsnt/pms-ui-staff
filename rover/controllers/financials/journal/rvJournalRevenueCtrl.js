9d4bbce5556b45c4073f235ad0cd52fc70386afd
krishobh <krishobh@stayntouch.com>
2014-11-11 17:31:59 +0530

    CICO-6105 Fixed issue on expand collpse

--- rover/controllers/financials/journal/rvJournalRevenueCtrl.js
+++ rover/controllers/financials/journal/rvJournalRevenueCtrl.js
@@ -33,44 +33,50 @@ sntRover.controller('RVJournalRevenueController', ['$scope','$rootScope', 'RVJou
 
     /** Handle Expand/Collapse on Level1 **/
     $scope.clickedFirstLevel = function(index1){
-        $scope.data.revenueData.charge_groups[index1].active = !$scope.data.revenueData.charge_groups[index1].active;
-        refreshRevenueScroller(); 
+        var toggleData = $scope.data.revenueData.charge_groups[index1];
+        if($scope.checkHasArrowLevel1(index1)){
+            toggleData.active = !toggleData.active;
+            refreshRevenueScroller(); 
+        }
     };
     
     /** Handle Expand/Collapse on Level2 **/
     $scope.clickedSecondLevel = function(index1, index2){
-        $scope.data.revenueData.charge_groups[index1].charge_codes[index2].active = !$scope.data.revenueData.charge_groups[index1].charge_codes[index2].active;
-        refreshRevenueScroller();
+        var toggleData = $scope.data.revenueData.charge_groups[index1].charge_codes[index2];
+        if($scope.checkHasArrowLevel2(index1, index2)){
+            toggleData.active = !toggleData.active;
+            refreshRevenueScroller();
+        }
     };
 
-    // To show table heading for Level3.
+    // To show/hide table heading for Level3.
     $scope.isShowTableHeading = function(index1, index2){
         var isShowTableHeading = false;
-        var data = $scope.data.revenueData.charge_groups[index1].charge_codes[index2].transactions;
-        if(typeof data !== 'undefined' && data.length>0){
-            angular.forEach(data,function(transactions, index) {
-                if(transactions.show) isShowTableHeading=true;
+        var item = $scope.data.revenueData.charge_groups[index1].charge_codes[index2].transactions;
+        if((typeof item !== 'undefined') && (item.length >0)){
+            angular.forEach( item ,function(transactions, index) {
+                if(transactions.show) isShowTableHeading = true;
             });
         }
         return isShowTableHeading;
     };
 
-    // To add expandable arrow to level1
+    // To show/hide expandable arrow to level1
     $scope.checkHasArrowLevel1 = function(index){
         var hasArrow = false;
-        if(typeof $scope.data.revenueData.charge_groups[index].charge_codes !== 'undefined'){
-            if($scope.data.revenueData.charge_groups[index].charge_codes.length >0)
-                hasArrow = true;
+        var item = $scope.data.revenueData.charge_groups[index].charge_codes;
+        if((typeof item !== 'undefined') && (item.length >0)){
+            hasArrow = true;
         }
         return hasArrow;
     };
 
-    // To add expandable arrow to level2
+    // To show/hide expandable arrow to level2
     $scope.checkHasArrowLevel2 = function(index1, index2){
         var hasArrow = false;
-        if(typeof $scope.data.revenueData.charge_groups[index1].charge_codes[index2].transactions !== 'undefined'){
-            if($scope.data.revenueData.charge_groups[index1].charge_codes[index2].transactions.length >0)
-                hasArrow = true;
+        var item = $scope.data.revenueData.charge_groups[index1].charge_codes[index2].transactions;
+        if((typeof item !== 'undefined') && (item.length >0)){
+            hasArrow = true;
         }
         return hasArrow;
     };
