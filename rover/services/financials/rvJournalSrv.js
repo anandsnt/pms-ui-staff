5b9a1901a1c273e0870511e9d0deb91a53c1d059
krishobh <krishobh@stayntouch.com>
2014-11-18 19:52:27 +0530

    CICO-10697 modified fetch employees deatils url

--- rover/services/financials/rvJournalSrv.js
+++ rover/services/financials/rvJournalSrv.js
@@ -42,7 +42,7 @@ sntRover.service('RVJournalSrv',['$http', '$q', 'BaseWebSrvV2','RVBaseWebSrv','$
         };
 
         // fetch employees deatils
-        var url = "/api/users/active.json";
+        var url = "/api/users/active.json?journal=true";
         BaseWebSrvV2.getJSON(url).then(function (data) {
             that.filterData.employees = data;
             angular.forEach(that.filterData.employees,function(item, index) {
