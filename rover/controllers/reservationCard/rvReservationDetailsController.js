e1a087c9c49a4b9a323a94094d4e135889d97ec2
shahul <shahulp@qburst.com>
2014-11-19 12:56:06 +0530

    CICO-9430 Added fix for not redirecting to diary screen

--- rover/controllers/reservationCard/rvReservationDetailsController.js
+++ rover/controllers/reservationCard/rvReservationDetailsController.js
@@ -430,7 +430,7 @@ sntRover.controller('reservationDetailsController', ['$scope', '$rootScope', 'RV
 		
 		
 		$scope.showDiaryScreen = function(){
-			$state.go('rover.diary.reservations');
+			$state.go('rover.reservation.diary');
 		};
 	}
 
