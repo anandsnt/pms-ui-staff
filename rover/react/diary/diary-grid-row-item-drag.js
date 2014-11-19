9cc83b549ac47a20534e30b3306dc525de58a93c
Graham Stewart <graham@stayntouch.com>
2014-11-18 23:55:23 -0500

    booking selection

--- rover/react/diary/diary-grid-row-item-drag.js
+++ rover/react/diary/diary-grid-row-item-drag.js
@@ -96,13 +96,13 @@ var GridRowItemDrag = React.createClass({
 				mouse_down: false,
 				selected: !state.selected
 			}, function() {
-				var data = (props.edit.passive ? props.currentDragItem : props.data);
+				//var data = (props.edit.passive ? props.currentDragItem : props.data);
 
-				data.selected = !state.selected;
+				props.data.selected = !state.selected;
 
 				props.iscroll.grid.enable();
 
-				props.angular_evt.onSelect(props.row_data, data, data.selected, 'edit');	//TODO Make proxy fn, and move this to diary-content	
+				props.angular_evt.onSelect(props.row_data, data, !state.selected, 'edit');	//TODO Make proxy fn, and move this to diary-content	
 			});
 		}
 	},
