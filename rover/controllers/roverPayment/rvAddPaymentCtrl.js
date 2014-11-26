79e04b6bac5a59bdcf87879a9beb5350ea64decb
soumya <soumya@stayntouch.com>
2014-11-26 12:17:49 +0530

    CICO-10342 iframe expiry date - render to screen

--- rover/controllers/roverPayment/rvAddPaymentCtrl.js
+++ rover/controllers/roverPayment/rvAddPaymentCtrl.js
@@ -65,8 +65,7 @@ sntRover.controller('RVPaymentAddPaymentCtrl',
 
 
 	var retrieveExpiryDate = function(){
-		console.log("======================")
-		console.log($scope.cardData.tokenDetails.expiry.split("2"));
+
 		var expiryDate = $scope.cardData.tokenDetails.isSixPayment?
 					$scope.cardData.tokenDetails.expiry.substring(2, 4)+" / "+$scope.cardData.tokenDetails.expiry.substring(0, 2):
 					$scope.cardData.cardDetails.expiryMonth+" / "+$scope.cardData.cardDetails.expiryYear
