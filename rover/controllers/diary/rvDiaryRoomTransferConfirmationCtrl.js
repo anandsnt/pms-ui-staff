9e49fcc242caeb029748d144a1514c6f16d3aa36
shahul <shahulp@qburst.com>
2014-11-26 20:56:30 +0530

    CICO-9512 Added the naviagation to summary screen

--- rover/controllers/diary/rvDiaryRoomTransferConfirmationCtrl.js
+++ rover/controllers/diary/rvDiaryRoomTransferConfirmationCtrl.js
@@ -1,5 +1,12 @@
-sntRover.controller('RVDiaryRoomTransferConfirmationCtrl', [ '$scope', '$rootScope', '$state', 'rvDiarySrv', 'ngDialog', 'rvDiaryMetadata', 
-	function($scope, $rootScope, $state, rvDiarySrv, ngDialog, meta) {
+sntRover.controller('RVDiaryRoomTransferConfirmationCtrl', [ 
+												'$scope', 
+												'$rootScope', 
+												'$state', 
+												'rvDiarySrv', 
+												'ngDialog', 
+												'rvDiaryMetadata',
+												'$vault',
+	function($scope, $rootScope, $state, rvDiarySrv, ngDialog, meta, $vault) {
 
 		var roomXfer = $scope.roomXfer,
 			current = (roomXfer.current),
@@ -21,13 +28,36 @@ sntRover.controller('RVDiaryRoomTransferConfirmationCtrl', [ '$scope', '$rootSco
 
 
 		next.arrivalTime 		= new Date(next[r.row_children][m.start_date]).toLocaleTimeString();
-		next.departureTime 		= new Date(current[r.row_children][m.end_date]).toLocaleTimeString();
+		next.departureTime 		= new Date(next[r.row_children][m.end_date]).toLocaleTimeString();
 
 		next.arrivalDate 		= newArrivalDateComp.date.day + ' ' + newArrivalDateComp.date.month + ' ' + newArrivalDateComp.date.year;
 		next.departureDate 		= newDepartureDateComp.date.day + ' ' + newDepartureDateComp.date.month + ' ' + newDepartureDateComp.date.year;
 
+		$scope.price = roomXfer.next.room.new_price - roomXfer.current.room.old_price;
+
 		$scope.selectAdditional = function() {
 			ngDialog.close();
+		};
+
+		$scope.reserveRoom = function(nextRoom, reservation){
+
+			var dataToPassConfirmScreen = {};
+			dataToPassConfirmScreen.arrival_date = nextRoom.arrivalDate;
+			dataToPassConfirmScreen.arrival_time = nextRoom.arrivalTime;
+			
+			dataToPassConfirmScreen.departure_date = nextRoom.departureDate;
+			dataToPassConfirmScreen.departure_time = nextRoom.departureTime;			
+			
+			var rooms = {
+				room_id: next.room.id,
+				rateId:  next.room.room_type.id,
+				amount: $scope.price
+			}
+			dataToPassConfirmScreen.rooms = [];
+			dataToPassConfirmScreen.rooms.push(rooms);
+			$vault.set('temporaryReservationDataFromDiaryScreen', JSON.stringify(dataToPassConfirmScreen));
+			$scope.closeDialog();
+			$state.go('rover.reservation.staycard.mainCard.summaryAndConfirm', {reservation: 'HOURLY'})
 		};
 
 		$scope.confirm = function() {
