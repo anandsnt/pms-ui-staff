61eea37e9b3b403e8fc4ab68a499131c175072e8
Shiju Devarajan <shiju-d@stayntouch.com>
2014-12-01 11:34:04 +0530

    Removed duplicate stationary controller JS file

--- admin/controllers/ADStationaryCtrl.js
+++
@@ -1,2 +0,0 @@
-admin.controller('ADStationaryCtrl',['$scope','ADStationarySrv','$state','ngTableParams','$filter','$stateParams',function($scope,ADStationarySrv,$state,ngTableParams,$filter,$stateParams){
- }]);
