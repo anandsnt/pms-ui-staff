be427c475f955d84c1b38783ffb3954a7321d3a1
krishobh <krishobh@stayntouch.com>
2014-12-11 12:31:54 +0530

    CICO-11664 Added basic htmls

---
+++ rover/controllers/cardsOutside/rvCompanyCardArTransactionsCtrl.js
@@ -0,0 +1,22 @@
+sntRover.controller('RVCompanyCardArTransactionsCtrl', ['$scope', 'RVCompanyCardSrv', '$timeout',
+	function($scope, RVCompanyCardSrv, $timeout) {
+
+		BaseCtrl.call(this, $scope);
+
+		// $scope.setScroller('cardAccountsScroller');
+
+		// var refreshScroller = function() {
+		// 	$timeout(function() {
+		// 		if ($scope.myScroll && $scope.myScroll['cardAccountsScroller']) {
+		// 			$scope.myScroll['cardAccountsScroller'].refresh();
+		// 		}
+		// 		$scope.refreshScroller('cardAccountsScroller');
+		// 	}, 500)
+		// };
+
+		// $scope.$on('refreshAccountsScroll', refreshScroller);
+
+		
+
+	}
+]);
