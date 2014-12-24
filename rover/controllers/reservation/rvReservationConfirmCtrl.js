572916b4bfa513e79fd1494d215de763ce9e5cba
Shiju Devarajan <shiju-d@stayntouch.com>
2014-12-24 18:41:08 +0530

    CICO-11951: Changes added to send email on/off checkbox in reservation confirmation screen

--- rover/controllers/reservation/rvReservationConfirmCtrl.js
+++ rover/controllers/reservation/rvReservationConfirmCtrl.js
@@ -113,8 +113,8 @@ sntRover.controller('RVReservationConfirmCtrl', [
 				};
 			}
 
-			//skip sending messages if no mail id is provided and go to the next screen
-			if (!$scope.otherData.additionalEmail && !$scope.reservationData.guest.email) {
+			// skip sending messages if no mail id is provided or none of the emails are checked, go to the next screen
+			if ((!$scope.otherData.additionalEmail && !$scope.reservationData.guest.email) || (!$scope.otherData.isGuestPrimaryEmailChecked && !$scope.otherData.isGuestAdditionalEmailChecked)) {
 				$scope.reservationStatus.confirmed = true;
 				updateBackButton();
 				return false;
@@ -134,10 +134,10 @@ sntRover.controller('RVReservationConfirmCtrl', [
 
 
 			postData.emails = [];
-			if (!!$scope.reservationData.guest.email)
+			if (!!$scope.reservationData.guest.email && $scope.otherData.isGuestPrimaryEmailChecked)
 				postData.emails.push($scope.reservationData.guest.email);
 
-			if (!!$scope.otherData.additionalEmail)
+			if (!!$scope.otherData.additionalEmail && $scope.otherData.isGuestAdditionalEmailChecked)
 				postData.emails.push($scope.otherData.additionalEmail);
 
 			if ($scope.reservationData.isHourly) {
