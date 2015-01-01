e45ed639de76f4bc13dbbdd7306c5a54e32fa0aa
Mubarak Rocky <mubarak@stayntouch.com>
2015-01-01 12:39:26 +0530

    CICO-12020 Fixed issue with add room button missing

--- admin/controllers/adRoomListCtrl.js
+++ admin/controllers/adRoomListCtrl.js
@@ -19,7 +19,7 @@ admin.controller('adRoomListCtrl', ['$scope','ADRoomSrv', 'ngTableParams', '$fil
 			$scope.totalPage = Math.ceil($scope.totalCount/$scope.displyCount);
 			$scope.total_number_of_rooms = data.total_number_of_rooms;
 
-			$scope.data.is_add_available = data.is_add_available;
+			$scope.is_add_available = data.is_add_available;
 			$scope.data = data.rooms;
 			//$scope.data = data.rooms;
 
