2f87cefab67e2a96bdc9b9d0e8b3196d1b265332
Resheil <resheil@stayntouch.com>
2015-01-12 15:20:12 +0530

    CICO-12481 removing/showing eod submenu based on admin settings

--- admin/controllers/adSettingsAndParamsCtrl.js
+++ admin/controllers/adSettingsAndParamsCtrl.js
@@ -18,6 +18,38 @@ admin.controller('settingsAndParamsCtrl',['$scope','settingsAndParamsSrv','setti
         var saveDetailsSuccessCallback = function(){
              $scope.$emit('hideLoader');
              $scope.goBackToPreviousState();
+
+             var eodSubMenu =  {
+                        title: "MENU_END_OF_DAY",
+                        action: "staff#/staff/dashboard/changeBussinessDate"
+                      };
+
+            var frontDeskSubmenu = [];
+            angular.forEach($scope.menu, function(menu, index) {
+                if(menu.title === 'MENU_FRONT_DESK'){
+                    frontDeskSubmenu =  menu.submenu;
+                }
+            });
+
+            if($scope.data.is_auto_change_bussiness_date){
+                 var eodSubmenuPresent = false;
+                 angular.forEach(frontDeskSubmenu, function(submenu, index) {
+                    if(submenu.title === 'MENU_END_OF_DAY'){
+                        eodSubmenuPresent = true;
+                    }
+                });
+                if(!eodSubmenuPresent)
+                {
+                    frontDeskSubmenu.push(eodSubMenu);  
+                }                
+            }
+            else{
+                angular.forEach(frontDeskSubmenu, function(submenu, index) {
+                    if(submenu.title === 'MENU_END_OF_DAY'){
+                       frontDeskSubmenu.splice(index,1)
+                    }
+                });
+            };
         }
         var selectedChargeCode = ( typeof $scope.selected_charge_code == 'undefined' ) ? "" : $scope.selected_charge_code;
         var dataToSend = { "no_show_charge_code_id" : selectedChargeCode , "business_date" : $scope.data };
