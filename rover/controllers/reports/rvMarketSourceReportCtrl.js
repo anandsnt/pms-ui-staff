23267e994ee56fe002eb36f88bfe534ad07dc912
sdilipsam <dilips@stayntouch.com>
2015-01-13 16:02:51 +0530

    CICO-10200 Interim commit

--- rover/controllers/reports/rvMarketSourceReportCtrl.js
+++ rover/controllers/reports/rvMarketSourceReportCtrl.js
@@ -4,6 +4,25 @@ sntRover.controller('rvMarketSourceReportCtrl', [
 	'$filter',
 	'RVreportsSrv',
 	function($scope, $rootScope, $filter, RVreportsSrv) {
+
+		$scope.results = [{
+			"source": [{
+				"Booking.com": 27,
+				"COP Account": 5,
+				"Kiosk": 12,
+				"telephoneHQ": 15,
+				"walk-in": 12,
+				"web": 10,
+				"un_assigned": 5
+			}],
+			"market": [],
+			"total_count": 89
+		}];
+
+		var markets = _.keys($scope.results[0].source[0]);
+		var marketValues = _.values($scope.results[0].source[0]);
+
+
 		$scope.marketActual = {
 			options: {
 				chart: {
@@ -11,10 +30,10 @@ sntRover.controller('rvMarketSourceReportCtrl', [
 				}
 			},
 			xAxis: {
-				categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+				categories: markets
 			},
 			series: [{
-				data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+				data: marketValues
 			}]
 
 		}
