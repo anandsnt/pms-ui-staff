c56ee234e0092d26a140dac4e8f22ee38e47b3e9
Aswanth <aswanth@stayntouch.com>
2015-01-21 17:36:53 +0530

    CICO-10394 updated the api urls

--- admin/services/adUpsellEarlyCheckinService.js
+++ admin/services/adUpsellEarlyCheckinService.js
@@ -7,7 +7,7 @@ admin.service('adUpsellEarlyCheckinService',['$http', '$q', 'ADBaseWebSrvV2', fu
 
    this.fetch = function(){
 		var deferred = $q.defer();
-		var url = '/api/early_checkin_setups/early_checkin_setup.json';	
+		var url = '/api/early_checkin_setups/get_setup.json';	
 		
 		ADBaseWebSrvV2.getJSON(url).then(function(data) {
 			
@@ -26,7 +26,7 @@ admin.service('adUpsellEarlyCheckinService',['$http', '$q', 'ADBaseWebSrvV2', fu
 	this.update = function(data){
 		var updateData = data;
 		var deferred = $q.defer();
-		var url = '/api/early_checkin_setups/save_early_checkin_setup.json';	
+		var url = '/api/early_checkin_setups/save_setup.json';	
 		
 		ADBaseWebSrvV2.postJSON(url,updateData).then(function(data) {
 			deferred.resolve(data);
