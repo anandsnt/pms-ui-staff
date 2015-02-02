690638845ce20994e08f5c3fb282bae546048ff3
sdilipsam <dilips@stayntouch.com>
2015-02-02 18:38:08 +0530

    CICO-10202 Interim commit

--- rover/controllers/reports/rvOccupancyRevenueReportCtrl.js
+++ rover/controllers/reports/rvOccupancyRevenueReportCtrl.js
@@ -1,11 +1,43 @@
 sntRover.controller('rvOccupancyRevenueReportCtrl', [
-'$scope',
-'$rootScope',
-'$filter',
-'RVreportsSrv',
-'$timeout',
-function($scope, $rootScope, $filter, RVreportsSrv, $timeout) {
-	$scope.occupanyRevenueState = {
-		name: "Occupancy & Revenue Summary"
+	'$scope',
+	'$rootScope',
+	'$filter',
+	'RVreportsSrv',
+	'$timeout',
+	function($scope, $rootScope, $filter, RVreportsSrv, $timeout) {
+		$scope.occupanyRevenueState = {
+			name: "Occupancy & Revenue Summary"
+		}
+
+		$scope.setScroller('leftPanelScroll', {
+			preventDefault: false
+		});
+
+
+		$scope.tabularData = {
+			hotelName: "Sample Hotel",
+			markets: [{
+				id: "1",
+				name: "MarketOne"
+			}, {
+				id: "2",
+				name: "MarketTwo"
+			}, {
+				id: "3",
+				name: "MarketThree"
+			}],
+			chargeCodes: [{
+				id: "1",
+				name: "ChargeCodeOne"
+			}, {
+				id: "2",
+				name: "ChargeCodeTwo"
+			}, {
+				id: "3",
+				name: "ChargeCodeThree"
+			}]
+
+
+		}
 	}
-}])
+])
