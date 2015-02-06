fc71cc177d5403d4d56674c8b5063c82415e002c
akhilastayntouch <akhila@stayntouch.com>
2015-02-06 18:28:24 +0530

    CICO-11909 ui updates on add campaign screen

--- admin/controllers/adAddCampaignCtrl.js
+++ admin/controllers/adAddCampaignCtrl.js
@@ -1,33 +1,84 @@
-admin.controller('ADAddCampaignCtrl',['$scope', '$rootScope','adBrandsSrv', function($scope, $rootScope,adBrandsSrv){
+admin.controller('ADAddCampaignCtrl',['$scope', '$rootScope','ADCampaignSrv', function($scope, $rootScope,ADCampaignSrv){
 
 	BaseCtrl.call(this, $scope);
-
-	
-
-
-
 	$scope.campaignData = {};
 	$scope.campaignData.audience_type = "EVERYONE";
 	$scope.campaignData.delivery_primetime = "AM";
 	$scope.campaignData.alert_max_length = 120;
 	$scope.campaignData.messageSubjectMaxLength = 60;
 	$scope.campaignData.messageBodyMaxLength = 320;
-	$scope.campaignData.is_recurring = false;
+	$scope.campaignData.is_recurring = "false";
+	$scope.campaignData.header_file = $scope.fileName;
+
+	var computeCampaignSaveData = function(){
+		var campaign = {};
+		campaign.name = $scope.campaignData.name;
+		campaign.audience_type = $scope.campaignData.audience_type;
+		campaign.subject = $scope.campaignData.subject;
+		//TODO: Header image
+		campaign.header_image = $scope.campaignData.header_image;
+		campaign.body = $scope.campaignData.body;
+		campaign.call_to_action_label = $scope.campaignData.call_to_action_label;
+		campaign.call_to_action_target = $scope.campaignData.call_to_action_target;
+		campaign.is_recurring = $scope.campaignData.is_recurring == "true"? true : false;
+		campaign.day_of_week = $scope.campaignData.day_of_week;
+		//TODO: time_to_send
+		campaign.time_to_send = tConvertToAPIFormat($scope.campaignData.delivery_hour, $scope.campaignData.delivery_min, $scope.campaignData.delivery_primetime);
+		//TODO: recurrence_end_date
+		campaign.recurrence_end_date = '2015-03-15';
+		campaign.alert_ios7 = $scope.campaignData.alert_ios7;
+		campaign.alert_ios8 = $scope.campaignData.alert_ios8;
+
+		return campaign;
+
+
+	};
 
 
 	$scope.startCampaign = function(){
 
-		alert("startCampaign");
-		console.log($scope.campaignData);
-	}
+		//alert("startCampaign");
+		console.log($scope.campaignData.is_recurring);
+	};
 
 	$scope.saveAsDraft = function(){
-		alert("saveAsDraft");
-	}
+		var saveSucess = function(data){
+	alert('saveSucess');
+		}
+		var data = computeCampaignSaveData();
+		console.log(data);
+		$scope.invokeApi(ADCampaignSrv.saveCampaign, data, saveSucess);
+	};
+	$scope.onFromDateChanged = function(datePicked){
+		console.log(datePicked);
+	};
+
+	$scope.showCalendar = function(controller) {
+		alert("show date picker");
+		$scope.focusSearchField = false;
+		$scope.$emit("showSearchResultsArea", true);
+        $timeout(function() {
+            ngDialog.open({
+                template: '/assets/partials/search/rvDatePickerPopup.html',
+                controller: 'RVReservationSearchFromDatepickerCtrl',
+                className: '',
+                scope: $scope
+            });
+        }, 1000);
+	};
 
 	$scope.showDatePicker = function(){
 		alert("show showDatePicker");
-	}
+	};
+
+	$scope.$watch(function(){
+		return $scope.campaignData.header_image;
+	}, function(logo) {
+			if(logo == 'false')
+				$scope.fileName = "Choose File....";
+			$scope.campaignData.header_file = $scope.fileName;
+		}
+	);
 
 
 	
--- admin/services/adCampaignSrv.js
+++ admin/services/adCampaignSrv.js
@@ -4,9 +4,9 @@ admin.service('ADCampaignSrv', ['$http', '$q', 'ADBaseWebSrvV2', 'ADBaseWebSrv',
         this.fetchCampaigns = function (data) {
             var deferred = $q.defer();
 
-            var url = "/sample_json/campaign/campaigns.json";
+            //var url = "/sample_json/campaign/campaigns.json";
+            var url = "/api/campaigns";
             ADBaseWebSrvV2.getJSON(url, data).then(function (data) {
-            	console.log("here");
                 deferred.resolve(data);
             }, function (data) {
                 deferred.reject(data);
@@ -15,9 +15,9 @@ admin.service('ADCampaignSrv', ['$http', '$q', 'ADBaseWebSrvV2', 'ADBaseWebSrv',
         };
 
 
-        this.saveEncoder = function (data) {
+        this.saveCampaign = function (data) {
             var deferred = $q.defer();
-            var url = "/api/key_encoders";
+            var url = "/api/campaigns";
             ADBaseWebSrvV2.postJSON(url, data).then(function (data) {
                 deferred.resolve(data);
             }, function (data) {
@@ -26,40 +26,6 @@ admin.service('ADCampaignSrv', ['$http', '$q', 'ADBaseWebSrvV2', 'ADBaseWebSrv',
             return deferred.promise;
         };
 
-        this.updateEncoder = function (data) {
-            var deferred = $q.defer();
-            var url = "/api/key_encoders/"+ data.id;
-            ADBaseWebSrvV2.putJSON(url, data).then(function (data) {
-                deferred.resolve(data);
-            }, function (data) {
-                deferred.reject(data);
-            });
-            return deferred.promise;
-        };
-
-        this.showEncoderDetails = function (data) {
-            var deferred = $q.defer();
-
-            var url = "/api/key_encoders/"+ data.id;
-            ADBaseWebSrvV2.getJSON(url).then(function (data) {
-                deferred.resolve(data);
-            }, function (data) {
-                deferred.reject(data);
-            });
-            return deferred.promise;
-        };
-
-        this.updateEncoderStatus = function (data) {
-            var deferred = $q.defer();
-            var url = "/api/key_encoders/" + data.id +"/activate";
-            delete data['id'];
-            ADBaseWebSrvV2.putJSON(url, data).then(function (data) {
-                deferred.resolve(data);
-            }, function (data) {
-                deferred.reject(data);
-            });
-            return deferred.promise;
-        };
-
+       
     }
 ]);
