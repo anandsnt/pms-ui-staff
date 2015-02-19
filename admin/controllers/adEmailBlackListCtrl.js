79a45443da3a72b229fc8e60b91a4f6b61f1148b
Aswanth <aswanth@stayntouch.com>
2015-02-19 16:16:04 +0530

    CICO-13557 updated the pagination and search

--- admin/controllers/adEmailBlackListCtrl.js
+++ admin/controllers/adEmailBlackListCtrl.js
@@ -6,8 +6,50 @@ admin.controller('ADEmailBlackListCtrl',['$scope', '$state', 'ADEmailBlackListSr
     $scope.isAddMode = false;
 	$scope.emailData= {};
 	$scope.emailData.email = "";
+	ADBaseTableCtrl.call(this, $scope, ngTableParams);
 	
-	
+
+    $scope.getTableData = function($defer, params) {
+		            // use build-in angular filter
+
+		            var tbParams = $scope.calculateGetParams(params);
+                    
+                    var orderedData = $filter('filter')($scope.emailList, {"email":tbParams.query}, function(actual, expected){
+                          if(actual.indexOf(expected) > -1)
+                          	return true;
+                          else
+                          	return false;
+                    }) ;
+
+		            orderedData = params.sorting() ?
+		                                $filter('orderBy')(orderedData, params.orderBy()) :
+		                                orderedData;
+		            $scope.totalCount = orderedData.length;
+		            params.total(orderedData.length);
+		            $scope.totalPage = Math.ceil(orderedData.length/$scope.displyCount);
+		            var startIndex  = tbParams.per_page * (tbParams.page - 1);
+		            var endIndex  =      (tbParams.per_page * (tbParams.page - 1)) + tbParams.per_page;          
+		            $scope.orderedData =  orderedData.splice(startIndex, tbParams.per_page);
+		            			        
+			        $scope.data = $scope.orderedData;
+			        $scope.currentPage = params.page();
+		                              
+		            $defer.resolve($scope.orderedData);
+    };
+
+	$scope.loadTable = function(){
+		$scope.tableParams = new ngTableParams({
+		        page: 1,  // show first page
+		        count: $scope.displyCount, // count per page
+		        sorting: {
+		            email: 'asc' // initial sorting
+		        }
+		    }, {
+		        total: 0, // length of data
+		        getData: $scope.getTableData
+		    }
+		);
+	};
 
    /*
     * To fetch list of blacklisted emails
@@ -16,26 +58,27 @@ admin.controller('ADEmailBlackListCtrl',['$scope', '$state', 'ADEmailBlackListSr
 		
 		var successCallbackFetch = function(data){
 			$scope.$emit('hideLoader');
-			$scope.emailList = data;
+			$scope.emailList = data;  			          
+			$scope.loadTable();
 
-            $scope.tableParams = new ngTableParams({
-		       page: 1,            // show first page
-		       	count: 100,    // count per page - Need to change when on pagination implemntation
-		        sorting: { email: 'asc'     // initial sorting 
-		        }
-		    }, {
+      //       $scope.tableParams = new ngTableParams({
+		    //    page: 1,            // show first page
+		    //    	count: 100,    // count per page - Need to change when on pagination implemntation
+		    //     sorting: { email: 'asc'     // initial sorting 
+		    //     }
+		    // }, {
 		     
-		        getData: function($defer, params) {
-		            // use build-in angular filter
-		            var orderedData = params.sorting() ?
-		                                $filter('orderBy')($scope.emailList, params.orderBy()) :
-		                                $scope.emailList;
+		    //     getData: function($defer, params) {
+		    //         // use build-in angular filter
+		    //         var orderedData = params.sorting() ?
+		    //                             $filter('orderBy')($scope.emailList, params.orderBy()) :
+		    //                             $scope.emailList;
 		                              
-		            $scope.orderedData =  orderedData;
+		    //         $scope.orderedData =  orderedData;
 		                                 
-		            $defer.resolve(orderedData);
-		        }
-		    });
+		    //         $defer.resolve(orderedData);
+		    //     }
+		    // });
 
 		};
 	   $scope.invokeApi(ADEmailBlackListSrv.fetch, {} , successCallbackFetch);	
