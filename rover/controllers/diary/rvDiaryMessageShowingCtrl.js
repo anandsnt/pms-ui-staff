c40291716af85e76218386a53d6a1378dae19d2a
shahul <shahulp@qburst.com>
2015-02-25 20:14:28 +0530

    CICO-13760 Fixed the issue of hanging on not available

--- rover/controllers/diary/rvDiaryMessageShowingCtrl.js
+++ rover/controllers/diary/rvDiaryMessageShowingCtrl.js
@@ -1,16 +1,19 @@
 sntRover.controller('RVDiaryMessageShowingCtrl', ['$scope',
     'ngDialog',
     '$rootScope',
-    function($scope, ngDialog, $rootScope) {
+    '$timeout',
+    function($scope, ngDialog, $rootScope, $timeout) {
     	$scope.messages = $scope.message;
     	var callBack = $scope.callBackAfterClosingMessagePopUp;
 
     	$scope.closeDialog = function(){
             //to add stjepan's popup showing animation
-            $rootScope.modalOpened = false;    		
+            $rootScope.modalOpened = false;  
+            $timeout(function(){
+                ngDialog.close();
+            }, 300);
     		if(callBack){
-    			setTimeout(function(){
-                    ngDialog.close();
+    			$timeout(function(){                    
     				callBack();
     			}, 1000);
     			
