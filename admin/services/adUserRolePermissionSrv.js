3505e5b421f679a25960892e00f58c071b89931b
krishobh <krishobh@stayntouch.com>
2015-03-02 11:54:12 +0530

    CICO-6437  Added permission screen

---
+++ admin/services/adUserRolePermissionSrv.js
@@ -0,0 +1,46 @@
+admin.service('ADUserRolePermissionSrv',['$q', 'ADBaseWebSrvV2', 
+	function($q, ADBaseWebSrvV2){
+   /*
+	* service class for room related operations
+	*/
+	var that = this;
+
+    this.roomTypesArray = [];
+   /*
+    * getter method to fetch rooms list
+    * @return {object} room list
+    */	
+	this.fetchUserRolePermission = function(params){
+		var deferred = $q.defer();
+		var url = '/admin/roles_permissions';	
+		ADBaseWebSrvV2.getJSON(url, params).then(function(data) {
+			deferred.resolve(data);
+		},function(errorMessage){
+			deferred.reject(errorMessage);
+		});
+		
+		return deferred.promise;
+	};
+	this.addedUserRolePermission = function(params){
+		var deferred = $q.defer();
+		var url = '/admin/roles_permissions/add_permission';	
+		ADBaseWebSrvV2.postJSON(url, params).then(function(data) {
+			deferred.resolve(data);
+		},function(errorMessage){
+			deferred.reject(errorMessage);
+		});
+		
+		return deferred.promise;
+	};
+	this.removeUserRolePermission = function(params){
+		var deferred = $q.defer();
+		var url = '/admin/roles_permissions/remove_permission';	
+		ADBaseWebSrvV2.postJSON(url, params).then(function(data) {
+			deferred.resolve(data);
+		},function(errorMessage){
+			deferred.reject(errorMessage);
+		});
+		
+		return deferred.promise;
+	};
+}]);
