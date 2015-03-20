786cb7a41911560f5463ec500f91cca929bdce1c
sdilipsam <dilips@stayntouch.com>
2015-03-20 14:18:05 +0530

    CICO-12790 Tabs for Group configuration placeholders

--- rover/controllers/groups/rvGroupConfigurationCtrl.js
+++ rover/controllers/groups/rvGroupConfigurationCtrl.js
@@ -1,10 +1,14 @@
-sntRover.controller('rvGroupConfigurationCtrl', ['$scope', '$rootScope', 'rvGroupSrv', '$filter',
-	function($scope, $rootScope, rvGroupSrv, $filter) {
+sntRover.controller('rvGroupConfigurationCtrl', ['$scope', '$rootScope', 'rvGroupSrv', '$filter', '$stateParams',
+	function($scope, $rootScope, rvGroupSrv, $filter, $stateParams) {
 		BaseCtrl.call(this, $scope);
 
 
-		var title = $filter('translate')('GROUPS');
+		var title = $stateParams.id === "NEW_GROUP" ? $filter('translate')('NEW_GROUP') :$filter('translate')('GROUPS');
 		$scope.setHeadingTitle(title);
+
+		$scope.groupConfigState = {
+			activeTab: $stateParams.activeTab // Possible values are SUMMARY, ROOM_BLOCK, ROOMING, ACCOUNT, TRANSACTIONS, ACTIVITY
+		}
 
 	}
 ]);
