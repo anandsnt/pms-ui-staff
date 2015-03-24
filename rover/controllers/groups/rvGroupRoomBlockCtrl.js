832a502575229eaffac78f9ad52a514e5af14521
shahul <shahulp@qburst.com>
2015-03-24 20:32:32 +0530

    CICO-12552 Added scroller, started to add/edit mode distingushing,  Almost completed the hiding/showing logic

--- rover/controllers/groups/rvGroupRoomBlockCtrl.js
+++ rover/controllers/groups/rvGroupRoomBlockCtrl.js
@@ -5,6 +5,15 @@ sntRover.controller('rvGroupRoomBlockCtrl',	[
 	function($scope,
 		$rootScope,
 		$filter) {
+		
+		/**
+		* util function to check whether a string is empty
+		* @param {String/Object}
+		* @return {boolean}
+		*/
+		$scope.isEmpty = function(string){
+			return ($scope.escapeNull(string).trim() === '');
+		};
 
 		/**
 		 * Function to decide whether to hide Hold status selection box
@@ -12,8 +21,10 @@ sntRover.controller('rvGroupRoomBlockCtrl',	[
 		 * we will hide hold status area
 		 * @return {Boolean} 
 		 */
-		$scope.shouldHideHoldStatus = function() {
-			return true;
+		$scope.shouldHideHoldStatus = function() {	
+			var addModeCondition = (!$scope.shouldHideCreateBlockButton() && $scope.isInAddMode());
+			var editModeCondition = (!$scope.isInAddMode());
+			return (addModeCondition || editModeCondition);
 		};
 
 		/**
@@ -22,17 +33,30 @@ sntRover.controller('rvGroupRoomBlockCtrl',	[
 		 * @return {Boolean} 
 		 */
 		$scope.shouldHideRoomsAndPickUpArea = function() {
-			return true;
+			var addModeCondition = (!$scope.shouldHideCreateBlockButton() && $scope.isInAddMode());
+			var editModeCondition = (!$scope.isInAddMode());			
+			return (addModeCondition || editModeCondition);
 		};
 
 		/**
 		 * Function to decide whether to disable Create block button
-		 * if from date & to date is not defined and it is in Add mode will return true 
+		 * if from date & to date is not defined, will return true 
 		 * @return {Boolean} 
 		 */
-		$scope.shouldDisableCreatBlockButton = function() {
-			return true;
+		$scope.shouldDisableCreateBlockButton = function() {
+			var isStartDateIsEmpty = $scope.isEmpty ($scope.startDate);
+			var isEndDateIsEmpty   = $scope.isEmpty ($scope.endDate);
+			return (isEndDateIsEmpty && isEndDateIsEmpty);
 		};
+
+		/**
+		 * Function to decide whether to hide Create block button
+		 * once click the create button, it become hidden 
+		 * @return {Boolean} 
+		 */
+		$scope.shouldHideCreateBlockButton = function() {					
+			return ($scope.createButtonClicked);
+		};		
 
 		/**
 		 * Function to decide whether to hide Update button
@@ -40,7 +64,62 @@ sntRover.controller('rvGroupRoomBlockCtrl',	[
 		 * @return {Boolean} 
 		 */
 		$scope.shouldHideUpdateButton = function() {
-			return true;
+			return !$scope.createButtonClicked;
+		};
+
+		/**
+		 * Function to decide whether to hide 'Add Rooms Button'
+		 * @return {Boolean}
+		 */
+		$scope.shouldHideAddRoomsButton = function() {
+			return $scope.shouldHideUpdateButton();
+		};
+
+		/**
+		 * to run angular digest loop,
+		 * will check if it is not running
+		 * return - None
+		 */
+		var runDigestCycle = function(){
+			if(!$scope.$$phase) {	
+				$scope.$digest();
+			}
+		};
+
+		/**
+		 * when the start Date choosed,
+		 * will assign fromDate to using the value got from date picker
+		 * will change the min Date of end Date
+		 * return - None
+		 */
+		var onStartDatePicked = function(date, datePickerObj){
+			$scope.startDate = date;
+			
+			// we will clear end date if chosen start date is greater than end date
+			if ($scope.startDate > $scope.endDate) {
+				$scope.endDate = '';
+			}
+			//setting the min date for end Date
+			$scope.endDateOptions.minDate = $scope.startDate;
+
+			//we have to show create button 
+			$scope.createButtonClicked = false;
+
+			runDigestCycle();
+		};
+
+		/**
+		* when the end Date choosed,
+		* will assign endDate to using the value got from date picker
+		* return - None
+		*/
+		var onEndDatePicked = function(date, datePickerObj){
+			$scope.endDate = date;
+
+			//we have to show create button 
+			$scope.createButtonClicked = false;
+
+			runDigestCycle();
 		};
 
 		/**
@@ -49,35 +128,73 @@ sntRover.controller('rvGroupRoomBlockCtrl',	[
 		 * @return - None
 		 */
 		var setDatePickers = function () {
+
+			//default start date
+			$scope.startDate = '';
+
+			//default to date
+			$scope.endDate = '';
+
+			//referring data model -> from group summary 
+			var refData = $scope.groupConfigData.summary;
+
+			//if from date is not null from summary screen, we are setting it as busines date
+			if (!$scope.isEmpty(refData.block_from)){
+				$scope.startDate = refData.block_from;
+			}
+
+			//if to date is null from summary screen, we are setting it from date
+			if (!$scope.isEmpty(refData.block_to)){
+				$scope.endDate = refData.block_to;
+			}
+
+
 			//date picker options - Common
 			var commonDateOptions = {
 				dateFormat 		: $rootScope.jqDateFormat,
 				numberOfMonths	: 1,
 			};	
 
-			//referring data model -> from group summary 
-			var refData = $scope.groupConfigData.summary
-			//default start date
-			$scope.startDate = new tzIndependentDate (refData.block_from);
-
-			//default to date
-			$scope.endDate = new tzIndependentDate (refData.block_to);
-
 			//date picker options - Start Date
 			$scope.startDateOptions = _.extend ({
-				minDate: $rootScope.businessDate
+				minDate: new tzIndependentDate ($rootScope.businessDate),
+				onSelect: onStartDatePicked
 			}, commonDateOptions);
 
 			//date picker options - End Date
 			$scope.endDateOptions = _.extend ({
-				minDate: $scope.endDate           
-			}, commonDateOptions);		
+				minDate: $scope.startDate,
+				onSelect: onEndDatePicked          
+			}, commonDateOptions);	
 		};
-		
+
 		/**
-		* utiltiy function for setting scroller and things
-		* return - None		
-		*/
+		 * when create button clicked, we will show the 'Hold Status and more section'
+		 * @return None
+		 */
+		$scope.clickedOnCreateButton = function(){
+			$scope.createButtonClicked = true;
+		};
+
+		/**
+		 * We have a list of variables to identify to initialize depending the mode (Add/Edit)
+		 * @return None
+		 */
+		var initializeAddOrEditModeVariables = function(){
+			//variable used to track Create Button
+			$scope.createButtonClicked = false;
+
+			var isOnEditMode = !$scope.isInAddMode();
+
+			if (isOnEditMode){
+				$scope.createButtonClicked = true;
+			}
+		};
+
+		/**
+		 * utiltiy function for setting scroller and things
+		 * return - None	
+		 */
 		var setScroller = function(){
 			//setting scroller things
 			var scrollerOptions = {
@@ -100,6 +217,9 @@ sntRover.controller('rvGroupRoomBlockCtrl',	[
 
 	    	//setting scrollers
 	    	setScroller();
+
+	    	//we have a list of scope varibales which we wanted to assign when it is in add/edit mode
+	    	initializeAddOrEditModeVariables();
 		}();	
 	}
 ]);
