3ac40ff9d59addecbc729ef696565005522f427d
krishobh <krishobh@stayntouch.com>
2015-04-13 18:37:04 +0530

    CICO-14380 HTML integration

--- rover/controllers/groups/activity/rvGroupActivityCtrl.js
+++ rover/controllers/groups/activity/rvGroupActivityCtrl.js
@@ -4,8 +4,55 @@ sntRover.controller('rvGroupActivityCtrl', [
 	'$filter', 
 	'$stateParams', 
 	function($scope, $rootScope, $filter, $stateParams) {
-		BaseCtrl.call(this, $scope);
-		$scope.count = 10;
+		BaseCtrl.call(this, $scope);		
+
+		/**		
+		 * initialisation and basic configuration
+		 * @return {none}
+		 */
+		$scope.init = function(){
+			//TODO- remove $scope.count = 10 for test purpose
+			$scope.count = 10;			
+			$scope.errorMessage = '';			
+		}
+		
+		/**		 
+		 * load next page		
+		 */
+		$scope.loadNextSet = function(){
+		//implementation
+		console.log(" implement loadNextSet");
+		}
+
+		/**
+		 * load Previous page		
+		 */
+		$scope.loadPrevSet = function(){
+			//implementation
+			console.log(" implement loadPrevSet");
+		}
+		
+		/**		 
+		 * for pagination
+		 * @return {boolean}		
+		 */
+		$scope.isPrevButtonDisabled = function(){
+			//implementation
+		}
+
+		/**		 
+		 * for pagination
+		 * @return {boolean}		
+		 */
+		$scope.isNextButtonDisabled = function(){
+			//implementation
+		}
+
+
+		/**
+		 *Show starts here
+		 */
+		 $scope.init();
 		
 	}
 ]);
