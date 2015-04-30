a118cc0033aab715f0566af6009de4f438582180
Shiju Devarajan <shiju-d@stayntouch.com>
2015-04-30 14:03:47 +0530

    Removed git conflict message tags

--- rover/controllers/reports/rvMarketSourceReportCtrl.js
+++ rover/controllers/reports/rvMarketSourceReportCtrl.js
@@ -163,10 +163,7 @@ sntRover.controller('rvMarketSourceReportCtrl', [
 
 
 		var init = function() {
-<<<<<<< HEAD
-			
-=======
->>>>>>> release
+
 			if ( _.isEmpty($scope.results.source) && _.isEmpty($scope.results.market) ) {
 				$scope.hasNoData = true;
 			} else {
