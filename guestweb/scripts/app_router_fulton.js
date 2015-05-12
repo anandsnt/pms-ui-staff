b50a2e8fa8301747049e92dbbecb1ce0f63bf888
Resheil <resheil@stayntouch.com>
2015-05-12 11:34:19 +0530

    CICO-16120 basic setup

---
+++ guestweb/scripts/app_router_fulton.js
@@ -0,0 +1,105 @@
+
+
+snt.config(['$stateProvider','$urlRouterProvider', function($stateProvider,$urlRouterProvider) {
+	
+    $urlRouterProvider.otherwise("/checkoutRoomVerification");
+
+ //    // checkout now states
+    
+	$stateProvider.state('checkoutBalance', {
+        url: '/checkoutBalance',
+        controller: 'checkOutBalanceController',
+       	templateUrl: '/assets/checkoutnow/partials/Montauk/checkoutBalance.html',
+	    title: 'Balance - Check-out Now'
+    })
+    .state('checkOutStatus', {
+        url: '/checkOutStatus',
+       	controller: 'checkOutStatusController',
+       	templateUrl: '/assets/checkoutnow/partials/Montauk/checkOutStatus.html',
+		title: 'Status - Check-out Now'
+    }).state('checkOutConfirmation', {
+        url: '/checkOutConfirmation',
+       	controller: 'checkOutConfirmationController',
+       	templateUrl: '/assets/checkoutnow/partials/Fulton/checkoutConfirmation.html',
+		title: 'Confirm - Check-out Now'
+    });
+
+    // late checkout states
+
+    $stateProvider.state('checkOutOptions', {
+    	url: '/checkOutOptions',
+	 	templateUrl: '/assets/landing/Fulton/landing.html',
+	 	controller: 'checkOutLandingController',
+	 	title: 'Check-out'
+	 }).state('checkOutLaterOptions', {
+	 	url: '/checkOutLaterOptions',
+		templateUrl: '/assets/checkoutlater/partials/Fulton/checkOutLater.html',
+	 	controller: 'checkOutLaterController',
+		title: 'Check-out Later'
+	}).state('checkOutLaterSuccess', {
+		url: '/checkOutLaterSuccess',
+		templateUrl: '/assets/checkoutlater/partials/Montauk/checkOutLaterSuccess.html',
+		controller: 'checkOutLaterSuccessController',
+		title: 'Status - Check-out Later'
+	 });
+
+	// checkin states
+
+	$stateProvider.state('checkinConfirmation', {
+	 	url: '/checkinConfirmation',
+	 	templateUrl: '/assets/checkin/partials/Montauk/checkInConfirmation.html',
+	 	controller : 'checkInConfirmationViewController',
+	 	title: 'Check-in'
+	 }).state('checkinReservationDetails', {
+	 	url: '/checkinReservationDetails',
+	 	templateUrl: '/assets/checkin/partials/Montauk/checkInReservationDetails.html',
+	 	controller : 'checkInReservationDetails',
+	 	title: 'Details - Check-in'
+	 }).state('checkinUpgrade', {
+	 	url: '/checkinUpgrade',
+	 	templateUrl: '/assets/checkin/partials/Montauk/checkinUpgradeRoom.html',
+	 	controller : 'checkinUpgradeRoomController',
+	    title: 'Upgrade - Check-in'
+	 }).state('checkinKeys', {
+	 	url: '/checkinKeys',
+	 	templateUrl: '/assets/preCheckin/partials/Montauk/noOption.html',
+	 	title: 'Status - Check-in'
+	 }).state('checkinSuccess', {
+	 	url: '/checkinSuccess',
+	 	templateUrl: '/assets/preCheckin/partials/Montauk/noOption.html',
+	 	title: 'Status - Check-in'
+	 }).state('checkinArrival', {
+	 	url: '/checkinArrival',	 	
+	 	controller:'checkinArrivalDetailsController',
+	 	templateUrl: '/assets/checkin/partials/Montauk/arrivalDetails.html',
+	 	title: 'Arrival Details - Check-in'
+	 });
+
+
+	 //room verification
+
+	 $stateProvider.state('checkoutRoomVerification', {
+	 	url: '/checkoutRoomVerification',
+	 	templateUrl: '/assets/checkoutnow/partials/Fulton/checkoutRoomVerification.html',
+	 	controller : 'checkoutRoomVerificationViewController',
+	 	title: 'Room verification'
+	 }).state('ccVerification', {
+	 	url: '/ccVerification/:fee/:message/:isFromCheckoutNow',
+	 	templateUrl: '/assets/checkoutnow/partials/Montauk/ccVerification.html',
+	 	controller : 'ccVerificationViewController',
+	 	title: 'CC verification'
+	 });
+	  // pre checkin states
+
+    $stateProvider.state('preCheckinTripDetails', {
+    	url: '/tripDetails',
+	 	templateUrl: '/assets/preCheckin/partials/noOption.html',
+	 	title: 'Pre Check-in'
+	}).state('preCheckinStatus', {
+		url: '/preCheckinStatus',
+		templateUrl: '/assets/preCheckin/partials/Montauk/preCheckinStatus.html',
+		controller : 'preCheckinStatusController',
+		title: 'Status - Pre Check-In'
+	 });
+
+}]);
