68535922d2045c56c11c99fb5a65d2ecfdb15729
Vijay Dev <vijay@stayntouch.com>
2015-06-03 03:49:47 -0400

    CICO-16990: fixed

--- rover/rvRouters/rvReportsRouter.js
+++ rover/rvRouters/rvReportsRouter.js
@@ -7,11 +7,7 @@ angular.module('reportsModule', [])
         controller: 'RVReportsMainCtrl',
         resolve: {
             reportsResponse: function(RVreportsSrv) {
-                if ( !!RVreportsSrv ) {
-                    return RVreportsSrv.fetchReportList();
-                } else {
-                    return {};
-                }
+                return RVreportsSrv.fetchReportList();
             },
             activeUserList: function(RVreportsSrv) {
                 return RVreportsSrv.fetchActiveUsers();
