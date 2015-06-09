a27814214029ecd3f9e05c16b3e9f6a8df5bf01b
Resheil <resheil@stayntouch.com>
2015-06-09 10:42:27 +0530

    CICO-6100 implementing popup

--- rover/controllers/bill/rvMoveChargeCtrl.js
+++ rover/controllers/bill/rvMoveChargeCtrl.js
@@ -7,6 +7,8 @@ sntRover.controller('RVMoveChargeCtrl', ['$scope',
 			$scope.textQuery = "";
 			$scope.searchResults = [];
 			$scope.targetSelected = false;
+			$scope.selectedTarget = {};
+			$scope.targetBillId = "";
 		};
 		initiate();
 
@@ -15,8 +17,45 @@ sntRover.controller('RVMoveChargeCtrl', ['$scope',
 
 			var fetchSucces = function(){
 				$scope.searchResults =  [
-       
+        {
+            "conf_num": 343645,
+            "room": "334",
+            "type": "RESERVATION",
+            "first_name": "Resheil",
+            "last_name": "mohammed",
+            "number_of_bills": 1,
+            "bills":[{bill_number: 1, id: 1235},{bill_number: 2, id: 1235}]
+        },
+        {
+            "account_id": 4324,
+            "type": "ACCOUNT",
+            "account_num": "1235435346346",
+            "account_name": "testaccount",
+            "bills":[{bill_number: 1, id: 1235}]
+        },
+        {
+            "account_id": 46523784,
+            "type": "GROUP",
+            "account_num": "12354423",
+            "account_name": "testaccount",
+            "bills":[{bill_number: 1, id: 1235}]
+        },
+        {
+            "conf_num": 333645,
+            "room": "123",
+            "type": "RESERVATION",
+            "first_name": "Steve",
+            "last_name": "G",
+            "number_of_bills": 2,
+            "bills":[{bill_number: 1, id: 1235}]
+        }
     ];
+
+
+
+    			_.each($scope.searchResults, function(result,index) {
+    				result.entity_id = index;
+    			});
 			}
 			fetchSucces();
 		};
@@ -30,6 +69,24 @@ sntRover.controller('RVMoveChargeCtrl', ['$scope',
 			} else {
 				fetchFilterdData();
 			}
+		};
+
+		$scope.targetClicked =  function(selectedId){
+			_.each($scope.searchResults, function(result) {
+				if(result.entity_id === selectedId){
+					$scope.selectedTarget = result;
+					$scope.selectedTarget.displayNumber = (result.type ==="ACCOUNT" ||result.type ==="GROUP") ? result.account_num : result.conf_num;
+					$scope.selectedTarget.displaytext = (result.type ==="ACCOUNT" ||result.type ==="GROUP") ? result.account_name : (result.last_name+' ,'+result.first_name);
+					$scope.targetBillId = $scope.selectedTarget.bills[0].id;
+					$scope.targetSelected = true;
+				}
+		    });
+		};
+
+
+		$scope.changeSelection =  function(){
+			$scope.selectedTarget = {};
+			$scope.targetSelected = false;
 		};
 	}
 ]);
