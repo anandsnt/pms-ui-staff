42977834308fc90f4e9ceb612b0b466c79e09758
akhilastayntouch <akhila@stayntouch.com>
2015-06-09 15:14:33 +0530

    CICO-17285 ui updates for the save api call

--- admin/controllers/adDoorLockInterfaceCtrl.js
+++ admin/controllers/adDoorLockInterfaceCtrl.js
@@ -16,6 +16,33 @@ admin.controller('ADDoorLockInterfaceCtrl',['$scope', '$rootScope','ADDoorlockIn
 		$scope.invokeApi(ADDoorlockInterfaceSrv.fetch, {},fetchSuccessCallback);
 	};
 
+	$scope.clickedSave = function(){
+
+		console.log($scope.data);
+		var hotelSupportedCardTypes = [];
+		for(var i in $scope.data.available_card_types){
+			if($scope.data.available_card_types[i].is_selected_for_hotel){
+				hotelSupportedCardTypes.push($scope.data.available_card_types[i].value);
+			}
+		}
+		var unwantedKeys = ["key_systems", "available_card_types"];
+		var saveData = dclone($scope.data, unwantedKeys);
+		saveData.hotel_supported_card_types = hotelSupportedCardTypes;
+		console.log(saveData);
+
+
+		var saveSuccessCallback = function(data) {
+			$scope.$emit('hideLoader');
+			//$scope.data = data;
+			console.log(data);
+			$scope.goBackToPreviousState();	
+		};
+		$scope.invokeApi(ADDoorlockInterfaceSrv.save, saveData, saveSuccessCallback);
+
+
+
+	};
+
 
 
 	init();
--- admin/services/adDoorlockInterfaceSrv.js
+++ admin/services/adDoorlockInterfaceSrv.js
@@ -1,12 +1,25 @@
 admin.service('ADDoorlockInterfaceSrv',['$http', '$q', 'ADBaseWebSrvV2', function($http, $q, ADBaseWebSrvV2){
-   /**
-    * To fetch the list of hold status
-    * @return {object} hold statuses json
-    */
+
 	this.fetch = function(){		
 		var deferred = $q.defer();
-		var url = '/sample_json/door_lock_interface/render.json';
+		//var url = '/sample_json/door_lock_interface/render.json';
+		var url = '/api/door_lock_interfaces.json';
+
 		ADBaseWebSrvV2.getJSON(url).then(function(data) {
+		    deferred.resolve(data);
+		},function(data){
+		    deferred.reject(data);
+		});	
+		return deferred.promise;
+	};
+
+
+	this.save = function(params){		
+		var deferred = $q.defer();
+		//var url = '/sample_json/door_lock_interface/render.json';
+		var url = '/api/door_lock_interfaces';
+
+		ADBaseWebSrvV2.postJSON(url, params).then(function(data) {
 		    deferred.resolve(data);
 		},function(data){
 		    deferred.reject(data);
