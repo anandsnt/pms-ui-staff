c56940ec7024812d6b9fd7e1a37fc21d27b3aad0
Vijay Dev <vijay@stayntouch.com>
2015-06-17 01:50:54 -0400

    CICO-17693: fixed last issue

--- rover/controllers/reservation/rvEditRatesCtrl.js
+++ rover/controllers/reservation/rvEditRatesCtrl.js
@@ -85,7 +85,13 @@ sntRover.controller('RVEditRatesCtrl', ['$scope', '$rootScope',
 		};
 
 		$scope.pastDay = function(date) {
-			return tzIndependentDate($rootScope.businessDate) > new tzIndependentDate(date);
+			// CICO-17693: should be disabled on the Stay Card for Group reservations, until we have the complete functionality working:
+			// Just to clarify: User should be able to enter custom rates at any time for a group reservation
+			if( $scope.reservationData.group_id || $scope.reservationData.reservation_card.group_id ){
+				return tzIndependentDate($rootScope.businessDate) >= new tzIndependentDate(date);
+			} else {
+				return tzIndependentDate($rootScope.businessDate) > new tzIndependentDate(date);
+			}
 		};
 
 	}
