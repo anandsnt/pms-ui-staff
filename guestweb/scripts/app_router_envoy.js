3991508f7206407204ab2bbea5fdfc5d571812cf
Resheil <resheil@stayntouch.com>
2015-06-22 12:06:50 +0530

    CICO-17614 adding files for Envoy templates

---
+++ guestweb/scripts/app_router_envoy.js
@@ -0,0 +1,83 @@
+
+
+snt.config(['$stateProvider','$urlRouterProvider', function($stateProvider,$urlRouterProvider) {
+	
+    $urlRouterProvider.otherwise("/checkoutRoomVerification");
+
+    // checkout now states
+    
+	$stateProvider.state('checkoutBalance', {
+        url: '/checkoutBalance',
+        controller: 'checkOutBalanceController',
+       	templateUrl: '/assets/checkoutnow/partials/Envoy/checkoutBalance.html',
+	    title: 'Balance - Check-out Now'
+    })
+    .state('checkOutStatus', {
+        url: '/checkOutStatus',
+       	controller: 'checkOutStatusController',
+       	templateUrl: '/assets/checkoutnow/partials/Envoy/checkOutStatus.html',
+		title: 'Status - Check-out Now'
+    }).state('checkOutConfirmation', {
+        url: '/checkOutConfirmation',
+       	controller: 'checkOutConfirmationController',
+       	templateUrl: '/assets/checkoutnow/partials/Envoy/checkoutConfirmation.html',
+		title: 'Confirm - Check-out Now'
+    });
+
+    // late checkout states
+
+    $stateProvider.state('checkOutOptions', {
+    	url: '/checkOutOptions',
+	 	templateUrl: '/assets/landing/Envoy/landing.html',
+	 	controller: 'checkOutLandingController',
+	 	title: 'Check-out'
+	 }).state('checkOutLaterOptions', {
+	 	url: '/checkOutLaterOptions',
+		templateUrl: '/assets/checkoutlater/partials/Envoy/checkOutLater.html',
+	 	controller: 'checkOutLaterController',
+		title: 'Check-out Later'
+	}).state('checkOutLaterSuccess', {
+		url: '/checkOutLaterOptions/:id',
+		templateUrl: '/assets/checkoutlater/partials/Envoy/checkOutLaterSuccess.html',
+		controller: 'checkOutLaterSuccessController',
+		title: 'Status - Check-out Later'
+	 });
+
+	// checkin states
+
+	$stateProvider.state('checkinConfirmation', {
+	 	url: '/checkinConfirmation',
+	 	templateUrl: '/assets/preCheckin/partials/noOption.html',
+	 	title: 'Check-in'
+	 }).state('checkinSuccess', {
+	 	url: '/checkinSuccess',
+	 	templateUrl: '/assets/preCheckin/partials/noOption.html',
+	 	title: 'Check-in'
+	 });
+	 
+	 //room verification
+
+	 $stateProvider.state('checkoutRoomVerification', {
+	 	url: '/checkoutRoomVerification',
+	 	templateUrl: '/assets/checkoutnow/partials/Envoy/checkoutRoomVerification.html',
+	 	controller : 'checkoutRoomVerificationViewController',
+	 	title: 'Room verification'
+	 }).state('ccVerification', {
+	 	url: '/ccVerification/:fee/:message/:isFromCheckoutNow',
+	 	templateUrl: '/assets/checkoutnow/partials/Envoy/ccVerification.html',
+	 	controller : 'ccVerificationViewController',
+	 	title: 'CC verification'
+	 });
+
+
+    // pre checkin states
+
+    $stateProvider.state('preCheckinTripDetails', {
+    	url: '/tripDetails',
+	 	templateUrl: '/assets/preCheckin/partials/noOption.html',
+	 	controller : 'preCheckinTripDetailsController',
+	 	title: 'Trip Details'
+	 });
+
+
+}]);
