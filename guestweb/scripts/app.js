7a2bca1fb8ea83102ff0ae45048e8ae8bd6b52fe
Resheil <resheil@stayntouch.com>
2015-07-15 14:44:44 +0530

    CICO-16159 changes for exteranla verification

--- guestweb/scripts/app.js
+++ guestweb/scripts/app.js
@@ -16,8 +16,18 @@ snt.controller('rootController', ['$rootScope','$scope','$attrs', '$location','$
 
 	//store basic details as rootscope variables
 
-	$rootScope.reservationID = $attrs.reservationId;
 	$rootScope.hotelName     = $attrs.hotelName;
+ 	$rootScope.hotelLogo     = $attrs.hotelLogo;
+ 	$rootScope.currencySymbol= $attrs.currencySymbol;
+	$rootScope.hotelPhone    = $attrs.hotelPhone;
+	$rootScope.businessDate  = $attrs.businessDate;
+	$rootScope.mliMerchatId = $attrs.mliMerchatId;
+	$rootScope.dateFormatPlaceholder = $attrs.dateFormatValue;
+ 	$rootScope.dateFormat = getDateFormat($attrs.dateFormatValue);
+ 	$rootScope.roomVerificationInstruction = $attrs.roomVerificationInstruction;
+ 	$rootScope.isSixpayments = ($attrs.paymentGateway  === "sixpayments") ? true:false;
+
+ 	$rootScope.reservationID = $attrs.reservationId;	
 	$rootScope.userName      = $attrs.userName;
 	$rootScope.checkoutDate  = $attrs.checkoutDate;
 	$rootScope.checkoutTime  = $attrs.checkoutTime;
@@ -26,27 +36,18 @@ snt.controller('rootController', ['$rootScope','$scope','$attrs', '$location','$
 	$rootScope.roomNo        = $attrs.roomNo;
 	$rootScope.isLateCheckoutAvailable  = ($attrs.isLateCheckoutAvailable  === 'true') ? true : false;
 	$rootScope.emailAddress  = $attrs.emailAddress;
-	$rootScope.hotelLogo     = $attrs.hotelLogo;
-	$rootScope.currencySymbol= $attrs.currencySymbol;
-	$rootScope.hotelPhone    = $attrs.hotelPhone;
-	$rootScope.businessDate  = $attrs.businessDate;
 	$rootScope.isCheckedout  = ($attrs.isCheckedout === 'true') ? true : false;
 	$rootScope.isCheckin     =   ($attrs.isCheckin ==='true') ? true : false;
 	$rootScope.reservationStatusCheckedIn = ($attrs.reservationStatus ==='CHECKIN')? true :false;
     $rootScope.isActiveToken = ($attrs.isActiveToken ==='true') ? true : false;
  	$rootScope.isCheckedin  =  ($rootScope.reservationStatusCheckedIn  && !$rootScope.isActiveToken);
-
  	$rootScope.isCCOnFile = ($attrs.isCcAttached ==='true')? true:false;
- 	$rootScope.mliMerchatId = $attrs.mliMerchatId;
- 	$rootScope.isRoomVerified =  false;
- 	$rootScope.dateFormatPlaceholder = $attrs.dateFormatValue;
- 	$rootScope.dateFormat = getDateFormat($attrs.dateFormatValue);
- 	$rootScope.isPrecheckinOnly = ($attrs.isPrecheckinOnly ==='true' && $attrs.reservationStatus ==='RESERVED')?true:false;
- 	$rootScope.roomVerificationInstruction = $attrs.roomVerificationInstruction;
- 	$rootScope.isCcAttachedFromGuestWeb = false;
- 	$rootScope.isSixpayments = ($attrs.paymentGateway  === "sixpayments") ? true:false;
- 	$rootScope.isAutoCheckinOn = (($attrs.isAutoCheckin === 'true') && ($attrs.isPrecheckinOnly === 'true')) ? true :false;;
  	$rootScope.isPreCheckedIn   = ($attrs.isPreCheckedIn === 'true') ? true: false;
+ 	$rootScope.isRoomVerified =  false;
+ 	$rootScope.isPrecheckinOnly = ($attrs.isPrecheckinOnly ==='true' && $attrs.reservationStatus ==='RESERVED')?true:false;
+ 	$rootScope.isCcAttachedFromGuestWeb = false;
+ 	$rootScope.isAutoCheckinOn = (($attrs.isAutoCheckin === 'true') && ($attrs.isPrecheckinOnly === 'true')) ? true :false;;
+ 	
 
     //Params for zest mobile and desktop screens
     if($attrs.hasOwnProperty('isPasswordReset')){
