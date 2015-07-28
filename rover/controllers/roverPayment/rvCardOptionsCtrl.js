802263a9711bc8a39361e8f644da38c88d1efec2
Resheil <resheil@stayntoch.com>
2015-07-28 16:59:17 +0530

    CICO-18767 Rover - Hourly: Res Summary: Can no longer change cc selection once entered

--- rover/controllers/roverPayment/rvCardOptionsCtrl.js
+++ rover/controllers/roverPayment/rvCardOptionsCtrl.js
@@ -24,7 +24,8 @@ sntRover.controller('RVCardOptionsCtrl',
 		$scope.refreshIframe = function(){
 			//in case of hotel with MLI iframe will not be present
 			if(!!$("#sixIframe").length){
-				$("#sixIframe").src = $("#sixIframe").src;
+				var iFrame = document.getElementById('sixIframe');
++               iFrame.src = iFrame.src;
 			}			
 		};
 
