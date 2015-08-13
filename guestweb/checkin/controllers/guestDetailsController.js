4f6602ccba0b6a36514a8d8c30f0cefe601d1349
Resheil <resheil@stayntouch.com>
2015-08-13 17:04:30 +0530

    CICO-18787 controllers

--- guestweb/checkin/controllers/guestDetailsController.js
+++ guestweb/checkin/controllers/guestDetailsController.js
@@ -14,10 +14,27 @@
 	}		
 
 	if($scope.pageValid){
+
+		$scope.years = [];
+		$scope.months = [];
+		$scope.days = [];
+		for(year=1900;year<=new Date().getFullYear();year++){
+			$scope.years.push({"id":year,"name":year});
+		};
+		for(month=1;month<=12;month++){
+			$scope.months.push({"id":month,"name":month});
+		};
+		for(day=1;day<=31;day++){
+			$scope.days.push({"id":day,"name":day});
+		};
+
+
+		/*to delete*/
+
 		$scope.countries = [{"id":16,"name":"US"},{"id":"19","name":"INDIA"}]
 		$scope.guestDetails =  {
 
-								    "birthday": "2015-01-01",
+								    "birthday": "2015-11-23",
 								    "address": {
 								        "street1": "NADAKAVU",
 								        "street2": "CLA",
@@ -27,6 +44,12 @@
 								        "country_id": 16
 								    	}
 								};
+		/*to delete*/
+	
+		$scope.guestDetails.day   = ($scope.guestDetails.birthday !== null) ? $scope.guestDetails.birthday.substring(8, 10): "";
+		$scope.guestDetails.month = ($scope.guestDetails.birthday !== null)?  $scope.guestDetails.birthday.substring(5, 7) : "";
+		$scope.guestDetails.year  = ($scope.guestDetails.birthday !== null)?  $scope.guestDetails.birthday.substring(0, 4) : "";
+		console.log($scope.guestDetails);
 	}
 };
 
