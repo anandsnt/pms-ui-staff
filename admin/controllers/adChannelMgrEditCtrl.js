a9dc188199c7a1fd0852b7bc9aa271de92f1b137
Mike <mike@stayntouch.com>
2015-08-18 23:01:53 -0400

    CICO-18742 UI change fixes Update for Channel Mgr Rate > room types

--- admin/controllers/adChannelMgrEditCtrl.js
+++ admin/controllers/adChannelMgrEditCtrl.js
@@ -7,21 +7,84 @@ admin.controller('ADChannelMgrEditCtrl', ['$scope', '$rootScope', '$state', 'ADC
         $scope.is_import_available = true;
         $scope.showMainAddRemove = false;
         $scope.showInlineAddRemove = false;
+        
+        $scope.available_room_types = [];
+        $scope.available_rates = [];
+        $scope.addRemoveMode = 'add';//add | edit
+        
 	var lastDropedTime = '';
         
         $scope.selectInterface = function(interface){
             $scope.selectedInterface = interface;
         };
-        
+        $scope.setRateDropdown = function(rate_id){
+            for (var x in $scope.rateData){
+                if ($scope.rateData[x].id === rate_id){
+                    $scope.selectedRate = $scope.rateData[x];
+                }
+            }
+        };
         $scope.editChannelManagerRate = function(rate){
+            $scope.editingRate = rate;
             for (var x in $scope.data){
                 if ($scope.data[x].id === rate.id){
                     rate.editing = true;
                     $scope.showInlineAddRemove = true;
+                    $scope.showMainAddRemove = false;
+                    
+                    $scope.addRemoveMode = 'edit';//add | edit
                 } else {
                     rate.editing = false;
                 }
             }
+            
+            //go through the rate list and add to included which are in the ids of the rate from channelMgr data object
+            
+            $scope.includedRoomTypes = [];
+            $scope.excludedRoomTypes = [];
+            for (var xo in $scope.available_room_types){
+                $scope.excludedRoomTypes.push($scope.available_room_types[xo]);
+            }
+            $scope.rateData = $scope.available_rates;
+            var item, exItem;
+            for (var i in $scope.data){
+                item = $scope.data[i];
+                if (rate.rate.id === item.rate.id){
+                    //for each room in the rate's room_types, add it to the included list,
+                    //place all others in excluded
+                    for (var ex in $scope.excludedRoomTypes){
+                        exItem = $scope.excludedRoomTypes[ex];
+                        for (var x in item.room_types){
+                            
+                            if (typeof item.room_types[x].id !== typeof 0123 && typeof item.room_types[x].id === typeof 'str'){
+                                item.room_types[x].id = parseInt(item.room_types[x].id);
+                            }
+                            if (typeof exItem.id !== typeof 0123 && typeof exItem.id === typeof 'str'){
+                                exItem.id = parseInt(exItem.id);
+                            }
+                            
+                            if (item.room_types[x].id === exItem.id){
+                                $scope.includedRoomTypes.push(exItem);
+                            }
+                        }
+                    }
+                    
+                    //remove the ones added to included
+                    for (var inc in $scope.includedRoomTypes){
+                        
+                        for (var ex in $scope.excludedRoomTypes){
+                            exItem = $scope.excludedRoomTypes[ex];
+                            if ($scope.includedRoomTypes[inc].id === exItem.id){
+                                delete $scope.excludedRoomTypes[ex];
+                            }
+                        }
+                        
+                    };
+                    $scope.setRateDropdown(rate.rate.id);
+                    return;
+            }
+        }
+            
         };
 
         $scope.loadTable = function () {
@@ -48,14 +111,20 @@ admin.controller('ADChannelMgrEditCtrl', ['$scope', '$rootScope', '$state', 'ADC
             $scope.invokeApi(ADChannelMgrSrv.deleteRateOnChannel, data, fetchSuccess, fetchFailure);
                 
         };
+        $scope.selectedOverExcluded = function(){
+            console.log('selectedOverExcluded');
+            console.log($scope.selectedIncludedRoomType)
+        }
         
+        $scope.selectedOverIncluded = function(){
+            console.log('selectedOverIncluded');
+            console.log($scope.selectedExcludedRoomType)
+        }
         
         
         $scope.reloadTable = function(){
             var fetchSuccess = function (data) {
                 $scope.$emit('hideLoader');
-                $scope.totalCount = data.data.channel_manager_rates.length;
-                $scope.totalPage = Math.ceil(data.data.channel_manager_rates.length / $scope.displyCount);
                 $scope.data = data.data.channel_manager_rates;
                 $scope.$emit('hideLoader');
             };
@@ -93,7 +162,8 @@ admin.controller('ADChannelMgrEditCtrl', ['$scope', '$rootScope', '$state', 'ADC
                 interface_id: interface_id,
                 channel_rate_id: rate.id,
                 active: !active,
-                room_type_ids: includedRoomIds
+                room_type_ids: includedRoomIds,
+                rate_id: rate.rate.id
             };
             var toggleSuccess = function () {
                 $scope.$emit('hideLoader');
@@ -108,7 +178,35 @@ admin.controller('ADChannelMgrEditCtrl', ['$scope', '$rootScope', '$state', 'ADC
                 $scope.$emit('hideLoader');
                 $scope.errorMessage = data;
             };
-            $scope.invokeApi(ADChannelMgrSrv.toggleRateActivate, params, toggleSuccess, toggleFailure);
+            $scope.invokeApi(ADChannelMgrSrv.updateRate, params, toggleSuccess, toggleFailure);
+        };
+        $scope.updateRate = function () {
+            var rate = $scope.editingRate;
+            var interface_id = $scope.selectedInterface.id, active = rate.active;
+            var included = $scope.includedRoomTypes;
+            //collect rate id's for included/excluded
+            var includedRoomIds = $scope.getIds(included);
+            
+            var params = {
+                interface_id: interface_id,
+                channel_rate_id: rate.id,
+                active: active,
+                room_type_ids: includedRoomIds,
+                rate_id: rate.rate.id
+            };
+            var toggleSuccess = function () {
+                //close edit window
+                $scope.showInlineAddRemove = false;
+                $scope.resetShowAddRmove();
+                $scope.reloadTable();
+                $scope.$emit('hideLoader');
+                
+            };
+            var toggleFailure = function (data) {
+                $scope.$emit('hideLoader');
+                $scope.errorMessage = data;
+            };
+            $scope.invokeApi(ADChannelMgrSrv.updateRate, params, toggleSuccess, toggleFailure);
         };
         
         $scope.includedRoomTypes = [];
@@ -158,7 +256,9 @@ admin.controller('ADChannelMgrEditCtrl', ['$scope', '$rootScope', '$state', 'ADC
             }
           //take the selected room type from selectedIncludedRoomType  
             var excludedRoomTypeSelected = $scope.excludedRoomTypeSelected;
+            console.log(excludedRoomTypeSelected)
             for (var i in $scope.excludedRoomTypes){
+                console.log($scope.excludedRoomTypes[i])
                 if (excludedRoomTypeSelected.id === $scope.excludedRoomTypes[i].id){
                     delete $scope.excludedRoomTypes[i];
                 }
@@ -166,6 +266,9 @@ admin.controller('ADChannelMgrEditCtrl', ['$scope', '$rootScope', '$state', 'ADC
             $scope.includedRoomTypes.push(excludedRoomTypeSelected);
             $scope.selectedExcludedRoomTypeIndex = -1;
         };
+        $scope.droppedToIncluded = function(){
+            console.log(arguments);
+        }
         
         $scope.refreshLists = function(){
             $scope.includedRoomTypes = [];
@@ -237,6 +340,7 @@ admin.controller('ADChannelMgrEditCtrl', ['$scope', '$rootScope', '$state', 'ADC
 	};
         
         $scope.addRateToChannel = function(selectedInterface){
+            $scope.addRemoveMode = 'add';//add | edit
             $scope.showInlineAddRemove = false;
             $scope.showMainAddRemove = true;
             var fetchSuccess = function(data){
@@ -253,43 +357,76 @@ admin.controller('ADChannelMgrEditCtrl', ['$scope', '$rootScope', '$state', 'ADC
             };
             var fetchFailure = function(data){
                 $scope.$emit('hideLoader');
-                
             };
             
             $scope.invokeApi(ADChannelMgrSrv.fetchRates, {}, fetchSuccess, fetchFailure);
+        };
+        $scope.populateAvailableRates = function(){
+              var fetchSuccess = function(data){
+                $scope.available_rates = [];
+                for (var i in data.results){
+                    $scope.available_rates.push(data.results[i]);
+                }
+                $scope.sortList($scope.available_rates, 'name');
+                $scope.available_rates.unshift({id: 0, placeholder: true, name: 'Select Rate'});
+                $scope.selectedRate = $scope.available_rates[0];
+                $scope.refreshLists();
+                $scope.$emit('hideLoader');
+            };
+            var fetchFailure = function(data){
+                $scope.$emit('hideLoader');
+            };
+            
+            $scope.invokeApi(ADChannelMgrSrv.fetchRates, {}, fetchSuccess, fetchFailure);
+        };
+        $scope.populateAvailableRoomTypes = function(){
+            var fetchSuccess = function(data){
+                    $scope.available_room_types = data.data.room_types;
+                    $scope.resortLists();
+                    $scope.$emit('hideLoader');
+                };
+                var fetchFailure = function(data){
+                    $scope.$emit('hideLoader');
+                };
+
+                $scope.invokeApi(ADChannelMgrSrv.fetchRoomTypes, {}, fetchSuccess, fetchFailure);
         };
         
         $scope.cancelMainAddRemove = function(){
             $scope.showMainAddRemove = false;
             $scope.resetShowAddRmove();
         };
-        $scope.addRateToChannelMgr = function(rate){
-            var included = $scope.includedRoomTypes, excluded = $scope.excludedRoomTypes;
-            var rate = $scope.selectedRate, interface = $scope.selectedInterface;
-            
-            if (rate.placeholder){
-                return;
+        $scope.addRateToChannelMgr = function(){
+            if ($scope.addRemoveMode === 'edit'){
+                $scope.updateRate($scope.selectedRate);
+            } else {
+                var included = $scope.includedRoomTypes, excluded = $scope.excludedRoomTypes;
+                var rate = $scope.selectedRate, interface = $scope.selectedInterface;
+
+                if (rate.placeholder){
+                    return;
+                }
+                //collect rate id's for included/excluded
+                var includedRoomIds = $scope.getIds(included);
+                var excludedRoomIds = $scope.getIds(excluded);
+
+                var params = {
+                    channel_id: interface.id,
+                    rate_id: rate.id,
+                    room_type_ids: includedRoomIds
+                };
+                var postSuccess = function(){
+                    $scope.cancelMainAddRemove();
+                    $scope.reloadTable();
+                    $scope.$emit('hideLoader');
+                };
+                var postFailure = function(){
+
+                    $scope.$emit('hideLoader');
+                };
+
+                $scope.invokeApi(ADChannelMgrSrv.postRateOnChannel, params, postSuccess, postFailure);
             }
-            //collect rate id's for included/excluded
-            var includedRoomIds = $scope.getIds(included);
-            var excludedRoomIds = $scope.getIds(excluded);
-            
-            var params = {
-                channel_id: interface.id,
-                rate_id: rate.id,
-                room_type_ids: includedRoomIds
-            };
-            var postSuccess = function(){
-                $scope.cancelMainAddRemove();
-                $scope.reloadTable();
-                $scope.$emit('hideLoader');
-            };
-            var postFailure = function(){
-                
-                $scope.$emit('hideLoader');
-            };
-            
-            $scope.invokeApi(ADChannelMgrSrv.postRateOnChannel, params, postSuccess, postFailure);
         };
         
         
@@ -305,6 +442,9 @@ admin.controller('ADChannelMgrEditCtrl', ['$scope', '$rootScope', '$state', 'ADC
         $scope.showLoader = function () {
             $scope.$emit('showLoader');
         };
+        
+        $scope.populateAvailableRates();
+        $scope.populateAvailableRoomTypes();
 
     }]);
 
