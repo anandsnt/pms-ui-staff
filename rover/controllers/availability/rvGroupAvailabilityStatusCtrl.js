b71081529cd448c11f02cfce3f4cec98f536f05b
Resheil <resheil@stayntouch.com>
2015-09-04 19:07:52 +0530

    CICO-16136 :Intemediate commit - Code cleaned.

--- rover/controllers/availability/rvGroupAvailabilityStatusCtrl.js
+++ rover/controllers/availability/rvGroupAvailabilityStatusCtrl.js
@@ -11,34 +11,39 @@ sntRover.controller('rvGroupAvailabilityStatusController', [
 			}else if(eventSource === "groupRoomPicked"){
 				$scope.hideHoldStatusOf["groupRoomPicked"] = !$scope.hideHoldStatusOf["groupRoomPicked"];
 			};
-
 			$scope.refreshScroller('groupscroller');
 		};
 		/**
 		* when data changed from super controller, it will broadcast an event 'changedRoomAvailableData'
 		*/
 		$scope.$on("changedRoomAvailableData", function(event){
+			$scope.hideBeforeDataFetch = false;
 			$scope.data = rvAvailabilitySrv.getGridDataForGroupAvailability();
 			$scope.refreshScroller('groupscroller');
 			$scope.$emit("hideLoader");
 		});
 
+		var setScroller = function(){
+			//we need horizontal scroller so adding option 'scrollX', also need to get the click event on toggling button on available room
+			var scrollerOptions = {scrollX: true, preventDefault: false};
+  			$scope.setScroller ('groupscroller', scrollerOptions);
+		};
 
 		var init = function(){
 			$scope.hideHoldStatusOf = {};
+			$scope.hideBeforeDataFetch =true;
 			$scope.hideHoldStatusOf["groupRoomTotal"] = true;
-			$scope.hideHoldStatusOf["groupRoomPicked"] = true;
-			//we need horizonat scroller so adding option 'scrollX', also need to get the click event on toggling button on available room
-			var scrollerOptions = {scrollX: true, preventDefault: false};
-  			$scope.setScroller ('groupscroller', scrollerOptions);
+			$scope.hideHoldStatusOf["groupRoomPicked"] = true;			
 			$scope.data = rvAvailabilitySrv.getGridDataForGroupAvailability();
+			setScroller();	
 			//if already fetched we will show without calling the API
 			if(!isEmptyObject($scope.data)){
+				$scope.hideBeforeDataFetch = false;
 				$scope.refreshScroller('groupscroller');
 				$scope.$emit("hideLoader");
-			}
-		}
-		init();
+			};
+		};
 
+		init();
 	}
 ]);
