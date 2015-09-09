b5e44d54c80cd4283ce8fa39a4835d871d3d6285
Vijay Dev <vijay@stayntouch.com>
2015-09-09 07:51:04 -0400

    saving

--- rover/controllers/reports/rvAddonForecastReportCtrl.js
+++ rover/controllers/reports/rvAddonForecastReportCtrl.js
@@ -15,11 +15,11 @@ sntRover.controller('RVAddonForecastReportCtrl', [
 		    preventDefault: false
 		});
 
-		var addonGroups  = $scope.$parent.$parent.addonGroups,
+
+		var results      = $scope.$parent.$parent.results,
+			addonGroups  = $scope.$parent.$parent.addonGroups,
 			addons       = $scope.$parent.$parent.addons,
 			allAddonHash = {};
-
-		console.log(addons);
 
 		_.each(addonGroups, function(item) {
 			allAddonHash[item.id] = item.description;
@@ -29,22 +29,100 @@ sntRover.controller('RVAddonForecastReportCtrl', [
 			_.each(item['list_of_addons'], function(entry) {
 				allAddonHash[entry.addon_id] = entry.addon_name;
 			});
-		});
+		});		
 
-		console.log(allAddonHash);
-
-		
-
-		var getKey = function(item) {
-			return _.keys(item)[0]
+		$scope.getKey = function(item) {
+			return _.keys(item)[0];
 		};
 
 		$scope.getKeyValues = function(item) {
-			return item[getKey(item)];
+			return item[$scope.getKey(item)];
 		};
 
 		$scope.getKeyName = function(item) {
-			return allAddonHash[getKey(item)] || id;
+			return allAddonHash[$scope.getKey(item)] || id;
 		};
+
+		$scope.toggleSub = function(item) {
+			if ( ! item.hasOwnProperty('hidden') ) {
+				item.hidden = true;
+			} else {
+				item.hidden = !item.hidden;
+			};
+
+			$scope.refreshScroller( 'addon-forecast-report-scroll' );
+		};
+
+		var resClassNames = {
+			'RESERVED' : 'arrival',
+			'CHECKEDIN' : 'check-in',
+			'CHECKEDOUT': 'check-out',
+			'CANCELED': 'cancel',
+			'NOSHOW': 'no-show'
+		}
+		$scope.getStatusClass = function(status) {
+			return resClassNames[status] || '';
+		};
+
+
+		var calPagination = function(addon) {
+			var perPage = 25,
+				pageNo,
+				netTotalCount,
+				uiTotalCount,
+				disablePrevBtn,
+				disableNextBtn,
+				resultFrom,
+				resultUpto
+
+
+			// clear old results and update total counts
+			$scope.netTotalCount = $scope.$parent.totalCount;
+
+			if ( typeof $scope.$parent.results === 'array' ) {
+				$scope.uiTotalCount = $scope.$parent.results.length;
+			} else if ( typeof $scope.$parent.results === 'object' ) {
+				$scope.uiTotalCount = 0;
+				_.each($scope.$parent.results, function(item) {
+					if ( typeof item === 'array' ) {
+						$scope.uiTotalCount += item.length;
+					};
+				});
+			};
+
+			if ( $scope.netTotalCount === 0 && $scope.uiTotalCount === 0 ) {
+				$scope.disablePrevBtn = true;
+				$scope.disableNextBtn = true;
+			} else if ( $_pageNo === 1 ) {
+				$scope.resultFrom = 1;
+				$scope.resultUpto = $scope.netTotalCount < $_resultsPerPage ? $scope.netTotalCount : $_resultsPerPage;
+				$scope.disablePrevBtn = true;
+				$scope.disableNextBtn = $scope.netTotalCount > $_resultsPerPage ? false : true;
+			} else {
+				$scope.resultFrom = $_resultsPerPage * ($_pageNo - 1) + 1;
+				$scope.resultUpto = ($scope.resultFrom + $_resultsPerPage - 1) < $scope.netTotalCount ? ($scope.resultFrom + $_resultsPerPage - 1) : $scope.netTotalCount;
+				$scope.disablePrevBtn = false;
+				$scope.disableNextBtn = $scope.resultUpto === $scope.netTotalCount ? true : false;
+			}
+ 		};
+
+
+ 		_.each(results, function(eachResult, resultKey) {
+ 			_.each(eachResult.addon_groups, function(eachAddonGroup) {
+ 				_.each( $scope.getKeyValues(eachAddonGroup), function(eachAddon) {
+ 					var addonData = $scope.getKeyValues(eachAddon);
+
+ 					_.extend(addonData, {
+ 						'sortKey': '',
+ 						"sortDir": ''
+
+ 						'date': resultKey,
+ 						'addonGroupId': $scope.getKeyValues(eachAddonGroup)
+ 					});
+
+ 					console.log(addonData);
+ 				});
+ 			});
+ 		});
 	}
 ]);
