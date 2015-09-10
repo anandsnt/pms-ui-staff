7a2f8b273c21c784b4f45af2cda6a228717e9b4e
Mike <mike@stayntouch.com>
2015-09-10 16:43:34 -0400

    CICO-17909 UI adds language file support, fixes ipad/android tablet touch responsiveness

--- rover/directives/clickTouch.js
+++ rover/directives/clickTouch.js
@@ -3,10 +3,25 @@ sntRover.directive('clickTouch', function($timeout, $parse) {
     restrict: 'AE',
     link: function(scope, element, attrs, opt) {
          element.bind('touchstart click', function(event) {
+             try {
+                if (element){
+                    if (arguments[0].target.nodeName === 'INPUT'){
+                        element.focus(); 
+                    }
+                }
+             } catch(err){
+                 
+             }
              //bind any touch start event to the element
-            event.preventDefault();
-            event.stopPropagation();
-
+             if (typeof event === typeof {}){
+                if (event.preventDefault){
+                    event.preventDefault();
+                }
+                if (event.stopPropagation){
+                    event.stopPropagation();
+                }
+                
+            }
             scope.$apply(attrs['clickTouch']);
       });
 
