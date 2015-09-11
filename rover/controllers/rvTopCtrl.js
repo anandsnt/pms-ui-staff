7d657d7b26bc9722cb60bf3a0987029274a1c4eb
Mike <mike@stayntouch.com>
2015-09-11 11:32:01 -0400

    CICO-17909 debugging code to init kiosk mode

--- rover/controllers/rvTopCtrl.js
+++ rover/controllers/rvTopCtrl.js
@@ -1,5 +1,6 @@
 sntRover.controller('topController', ['$state', '$scope', function($state, $scope) {
         if (localStorage['isKiosk'] === true){
+            alert('kiosk user');
             $state.go('kiosk');
         } else {
             $state.go('rover.dashboard');
