c2dbc4813d14ae721f1fccd8cfe33f2ff2371340
Shahul <mails2shahul@gmail.com>
2015-10-16 17:17:01 -0400

    CICO-20590
    Added reservation adding

--- rover/services/allotments/reservations/rvAllotmentReservationsListSrv.js
+++ rover/services/allotments/reservations/rvAllotmentReservationsListSrv.js
@@ -50,6 +50,38 @@ sntRover.service('rvAllotmentReservationsListSrv', ['$q', 'rvBaseWebSrvV2', 'rvU
 			);
 
 			return deferred.promise;
-		};		
+		};
+
+		/**
+		 * to add number of reservations agianst a room type & allotment
+		 * @return {Promise} [will get the reservation list]
+		 */
+		this.addReservations = function(params) {
+			var deferred = $q.defer(),
+				allotment_id = params.id,
+				url = '/api/allotments/'+allotment_id+'/reservations';
+
+			var params = {
+				reservations_data: {
+					room_type_id: params.room_type_id,
+					from_date: params.from_date,
+					to_date: params.to_date,
+					occupancy: params.occupancy,
+					no_of_reservations: params.no_of_reservations
+				}
+			};
+
+
+			rvBaseWebSrvV2.postJSON(url, params).then(
+				function(data) {
+					deferred.resolve(data);
+				},
+				function(errorMessage) {
+					deferred.reject(errorMessage);
+				}
+			);
+
+			return deferred.promise;
+		};				
 	}
 ]);
