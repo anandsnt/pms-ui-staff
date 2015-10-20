713210591fa7e3b3ac16233e745ca45595085431
sstajduhar <stjepan@stayntouch.com>
2015-10-20 11:30:30 +0200

    CICO-5247 - Calendar icon and responsiveness style update

--- shared/lib/js/fullcalender/fullcalendar.js
+++ shared/lib/js/fullcalender/fullcalendar.js
@@ -5419,7 +5419,7 @@ function DayEventRenderer() {
 			    }
 			}
 			
-			html += "<span class='fc-event-title'>";
+			html += "<span class='fc-event-title'><span class='icon'></span>";
 		if (parseInt(event.title) > 0 && event.currencySymbol !== 'undefined'){
 			html += htmlEscape(event.currencySymbol);
 		}
