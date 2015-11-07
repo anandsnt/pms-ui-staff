b697dea99f6b24d27b1d2ba4f4cb32c40d5eb0db
Shahul <mails2shahul@gmail.com>
2015-11-07 18:04:37 -0500

    CICO-21231
    Implemented auto completion for charge code fetching and completed API
    integration. Styles for autocomplete textbox were not loading. Its a
    issue with ruby  asset loader. When same asset file name found in two
    folder, it is taking the first one. So renamed one stylesheet file

--- admin/controllers/britePabx/adBritePabXSetupCtrl.js
+++ admin/controllers/britePabx/adBritePabXSetupCtrl.js
@@ -1,4 +1,5 @@
-admin.controller('adBritePabXSetupCtrl', ['$scope', 'britePabXSetupValues', 'adBritePabXSetupSrv', function($scope, britePabXSetupValues, adBritePabXSetupSrv){
+admin.controller('adBritePabXSetupCtrl', ['$scope', 'britePabXSetupValues', 'adBritePabXSetupSrv', 'ADChargeCodesSrv', '$timeout',
+	function($scope, britePabXSetupValues, adBritePabXSetupSrv, ADChargeCodesSrv, $timeout) {
 	
 	BaseCtrl.call (this, $scope);
 	
@@ -23,12 +24,21 @@ admin.controller('adBritePabXSetupCtrl', ['$scope', 'britePabXSetupValues', 'adB
 	 */
 	$scope.savePabXSetup = function() {
 		var params 	= {
-			brite: _.omit( dclone($scope.brite), 'charge_name')
+			brite: _.omit( dclone($scope.brite), 'charge_code_name')
 		};
 
 		if (!$scope.brite.enabled) {
-			params 	= _.omit(params, 'charge_code_id', 'account_number');
+			params.brite = _.omit(params.brite, 'charge_code_id', 'account_number');
 		}
+
+		if (params.brite.charge_code_id === '') {
+			$timeout(function() {
+				$scope.errorMessage = ['Please search a charge code, pick from the list and proceed'];
+				clearConfigValues();
+			}, 20);
+			return;
+		}
+
         var options = {
             params 			: params,
             successCallBack : successCallBackOfSaveBritePabXSetup
@@ -36,11 +46,89 @@ admin.controller('adBritePabXSetupCtrl', ['$scope', 'britePabXSetupValues', 'adB
         $scope.callAPI(adBritePabXSetupSrv.saveBritePabXConfiguration, options);
 	};
 
+	var successCallBackOfFetchChargeCodes = function(data, successCallBackParameters) {
+		if (data.results.length === 0) {
+			$scope.errorMessage = ["Unable find charge code against '" + $scope.brite.charge_code_name + "'"];
+			$scope.brite.charge_code_id	= '';
+			return;
+		}
+		successCallBackParameters.callBackToAutoComplete (data.results);
+	};
+
+	/**
+	 * [fetchChargeCodes description]
+	 * @return {[type]} [description]
+	 */
+	var fetchChargeCodes = function(callBackToAutoComplete) {
+		var params = {
+            query: $scope.brite.charge_code_name
+        };
+        var options = {
+            params 			: params,
+            successCallBack : successCallBackOfFetchChargeCodes,
+            successCallBackParameters: {
+				callBackToAutoComplete: callBackToAutoComplete
+			}
+        };
+        $scope.callAPI(ADChargeCodesSrv.searchChargeCode, options);
+	};
+
+	var clearConfigValues = function() {
+        $scope.brite.charge_code_id 	= '';
+        $scope.brite.charge_code_name 	= '';
+	};
+
+    // jquery autocomplete Souce handler
+    // get two arguments - request object and response callback function
+    var autoCompleteSourceHandler = function(request, callBackToAutoComplete) {
+        if (request.term.length === 0) {
+        	clearConfigValues();
+        } 
+        else if (request.term.length > 1) {
+            fetchChargeCodes(callBackToAutoComplete);
+        }
+    };
+
+    /**
+     * to run angular digest loop,
+     * will check if it is not running
+     * return - None
+     */
+    var runDigestCycle = function() {
+        if (!$scope.$$phase) {
+            $scope.$digest();
+        }
+    };
+
+    /**
+     * [autoCompleteSelectHandler description]
+     * @param  {[type]} event [description]
+     * @param  {[type]} ui    [description]
+     * @return {[type]}       [description]
+     */
+    var autoCompleteSelectHandler = function(event, ui) {
+        $scope.brite.charge_code_id 	= ui.item.id;
+        $scope.brite.charge_code_name 	= ui.item.name;
+        runDigestCycle();
+        return false;    
+    };
+
 	/**
 	 * Initialization stuffs
 	 * @return {undefiend}
 	 */
 	var initializeMe = function() {
 		$scope.brite = britePabXSetupValues;
+		$scope.chargeCodeAutocompleteOptions = {
+            delay		: 600,
+            minLength	: 0,
+	        position	: {
+	            my 			: "right top",
+	            at 			: "right bottom",
+	            collision	: 'flip'
+	        },
+            source 		: autoCompleteSourceHandler,
+            select 		: autoCompleteSelectHandler
+		};
 	}();
 }])
