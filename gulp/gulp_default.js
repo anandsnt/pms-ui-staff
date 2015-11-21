07fa84234c6d187a01accbf78d3753ffee9a5ded
Shahul <mails2shahul@gmail.com>
2015-11-20 19:19:53 -0500

    Asset precompile
    Added default task

--- gulp/gulp_default.js
+++ gulp/gulp_default.js
@@ -1,17 +1,24 @@
 module.exports = function(gulp, $, options) {
 	
 	var del = require('del'),
-		DEST_ROOT_PATH = '../../public/assets/';
+		DEST_ROOT_PATH = '../../public/assets/',
+		allPaths = ['**/*.*', '!node_modules/**/*.*', '!package.json'];
 	
 	gulp.task('clean', function () {
 	    del([DEST_ROOT_PATH], {force: true });
 	});
 
 	gulp.task('copy-all-dev', ['clean'], function() {
-	    return gulp.src(['**/*.*', '!node_modules/**/*.*', '!package.json'])
+	    return gulp.src(allPaths)
 	        .pipe(gulp.dest(DEST_ROOT_PATH));
 	});
 
 	gulp.task('build-dev', ['build-login-dev']);
 	gulp.task('asset-precompile', ['login-asset-precompile']);
+
+	gulp.task('watch', function(){
+		gulp.watch(allPaths, ['build-dev'])
+	});
+
+	gulp.task('default', ['build-dev', 'watch'])
 }
