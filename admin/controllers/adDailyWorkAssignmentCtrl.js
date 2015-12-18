eefb8febfda05ab8054dd5474671d91c0b96f4c4
soumyastayntouch <soumya@stayntouch.com>
2015-12-18 16:59:46 +0530

    CICO-12563 Fixed the error with is_active flag

--- admin/controllers/adDailyWorkAssignmentCtrl.js
+++ admin/controllers/adDailyWorkAssignmentCtrl.js
@@ -553,8 +553,8 @@ admin.controller('ADDailyWorkAssignmentCtrl', [
 				is_vacant                    : $scope.eachTaskList.front_office_status_ids.indexOf(1) > -1,
 				completion_time              : $rootScope.businessDate + ' ' + $scope.eachTaskList.hours + ':' + $scope.eachTaskList.mins + ':00',
 				task_completion_hk_status_id : $scope.eachTaskList.task_completion_hk_status_id,
-				rooms_task_completion        : getRoomTaskTimes(),
-				is_active				     : true
+				rooms_task_completion        : getRoomTaskTimes(), 
+				is_active   			     : true
 			};
 			var frequencyParams = {};
 			frequencyParams.monday = false;
@@ -614,7 +614,7 @@ admin.controller('ADDailyWorkAssignmentCtrl', [
 				task_completion_hk_status_id : $scope.eachTaskList.task_completion_hk_status_id,
 				id                           : $scope.eachTaskList.id,
 				rooms_task_completion        : getRoomTaskTimes(),
-				is_active
+				is_active					 : $scope.eachTaskList.is_active
 			};
 			var frequencyParams = {};
 			frequencyParams.monday = false;
