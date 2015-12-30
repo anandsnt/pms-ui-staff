4eed23edefe14b08a847e112b8e46e1ccbb90148
resheil <resheil@stayntouch.com>
2015-12-30 09:59:55 -0500

    CICO-22459 minor changes

--- admin/controllers/zestSetup/ADZestCheckinDirectUrlEmailCtrl.js
+++ admin/controllers/zestSetup/ADZestCheckinDirectUrlEmailCtrl.js
@@ -20,28 +20,28 @@ admin.controller('ADZestCheckinDirectUrlEmailCtrl',
       $scope.urls               = [{'name':'direct','path':'direct','is_active':true},
                                     {'name':'text','path':'text','is_active':false}];
     };
-
-
+    //hide if is addmode or editmode
+    $scope.isAddOrEditMode = function(){
+        return $scope.isAddMode || $scope.editMode;
+    }
+    //hide the row content if its clicked
     $scope.hideRow = function($index){
         return ($scope.currentClickedUrl === $index && $scope.editMode);
     };
-
-    $scope.showEditRow = function($index){
-        return ($scope.currentClickedUrl === $index && $scope.editMode);
-    };
-
+   
+    // display detail template
     $scope.getTemplateUrl = function() {
         return "/assets/partials/zestSetup/adZestUrlEdit.html";
     };
-
+    //delete the selected url
     $scope.deleteUrl = function($index){
         $scope.urls.splice($index,1);
     };
-
+    //toggle activation status
     $scope.switchActivation = function($index){
         $scope.urls[$index].is_active = !$scope.urls[$index].is_active;
     };
-
+    //click on invidual row
     $scope.editSingle = function(index){
         $scope.currentClickedUrl = index;
         $scope.editMode = true;
@@ -49,19 +49,19 @@ admin.controller('ADZestCheckinDirectUrlEmailCtrl',
         $scope.url.name = $scope.urls[$scope.currentClickedUrl].name;
         $scope.url.path = $scope.urls[$scope.currentClickedUrl].path;
     };
-
+    // add new button press
     $scope.addNew =  function(){
         resetEditScreen();
         $scope.currentClickedUrl = -1;
         $scope.editMode           = false;
         $scope.isAddMode          = true;
     };
-
+    //canceling add/edit mode
     $scope.cancelAddEdit = function(){
         resetEditScreen();
         $scope.currentClickedUrl = -1;
     };
-
+    // save/update  success
     $scope.saveAddEdit = function(){
         var newItem = angular.copy($scope.url);
         if($scope.isAddMode){
