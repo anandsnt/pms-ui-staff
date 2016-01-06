21d65db85aa136b192b390e19a7dcbed0174c473
resheil <resheil@stayntouch.com>
2016-01-06 13:46:25 -0500

    CICO-22462 email try integrations

--- guestweb/preCheckin/controllers/preCheckinStatusController.js
+++ guestweb/preCheckin/controllers/preCheckinStatusController.js
@@ -26,6 +26,10 @@
 		$scope.changeEmail = function(){
 			$state.go('emailAddition');
 		};
+		$scope.isValidEmail = function() {
+	   		 var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	    	return re.test($rootScope.userEmail);
+	 	 };
 	}
 };
 
