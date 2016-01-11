35112cefec33776d23b8e7b8be9cfb53b5639257
Shahul <mails2shahul@gmail.com>
2016-01-11 13:19:51 -0500

    CICO-22556 Implemented the accounts screen js asset list

--- asset_list/stateJsMapping/rover/roverStateJsMappings.js
+++ asset_list/stateJsMapping/rover/roverStateJsMappings.js
@@ -2,26 +2,27 @@ module.exports = {
 	getStateMappingList : function () {
 		var roverJsListRoot = '../../asset_list/js/rover/';
 		return {
-			'rover.diary': roverJsListRoot + 'diaryJsAssetList.js',
-			'rover.dashboard': roverJsListRoot + 'dashboardJsAssetList.js',
-			'rover.reservation': roverJsListRoot + 'stayCardJsAssetList.js',
+			'rover.diary' 		: roverJsListRoot + 'diaryJsAssetList.js',
+			'rover.dashboard' 	: roverJsListRoot + 'dashboardJsAssetList.js',
+			'rover.reservation'	: roverJsListRoot + 'stayCardJsAssetList.js',
 			'rover.availability': roverJsListRoot + 'availabilityJsAssetList.js',
-			'rover.reports': roverJsListRoot + 'reportsJsAssetList.js',
+			'rover.reports'		: roverJsListRoot + 'reportsJsAssetList.js',
 			'rover.availability': roverJsListRoot + 'availabilityJsAssetList.js',
-			'rover.ratemanager': roverJsListRoot + 'rateManagerJsAssetList.js',
+			'rover.ratemanager'	: roverJsListRoot + 'rateManagerJsAssetList.js',
 			'rover.housekeeping': roverJsListRoot + 'houseKeepingJsAssetList.js',
-			'rover.companycardsearch': roverJsListRoot + 'companyCardSearchJsAssetList.js',
-			'rover.companycarddetails': roverJsListRoot + 'companyCardDetailsJsAssetList.js',
-			'rover.groups': roverJsListRoot + 'groupJsAssetList.js',
-			'rover.allotments': roverJsListRoot + 'allotmentJsAssetList.js',
-			'rover.financials': roverJsListRoot + 'financialsJsAssetList.js',
-			'postcharge': roverJsListRoot + 'postChargeJsAssetList.js',
-			'endofday': roverJsListRoot + 'endOfDayJsAssetList.js',
-			'rover.workManagement': roverJsListRoot + 'workManagementJsAssetList.js',
-			'staffpasswordchange': roverJsListRoot + 'staffPasswordChangeJsAssetList.js',
-			'rover.reservation.staycard.billcard': roverJsListRoot + 'billScreenJsAssetList.js',
-			'rover.reservation.staycard.roomassignment': roverJsListRoot + 'roomAssignmentJsAssetList.js',
-			'changestaydates': roverJsListRoot + 'changeDatesJsAssetList.js'
+			'rover.groups' 		: roverJsListRoot + 'groupJsAssetList.js',
+			'rover.allotments' 	: roverJsListRoot + 'allotmentJsAssetList.js',
+			'rover.financials' 	: roverJsListRoot + 'financialsJsAssetList.js',
+			'postcharge'		: roverJsListRoot + 'postChargeJsAssetList.js',
+			'endofday'			: roverJsListRoot + 'endOfDayJsAssetList.js',
+			'changestaydates'	: roverJsListRoot + 'changeDatesJsAssetList.js',
+			'rover.accounts'	: roverJsListRoot + 'accountsJsAssetList.js',
+			'rover.workManagement'	: roverJsListRoot + 'workManagementJsAssetList.js',
+			'staffpasswordchange'	: roverJsListRoot + 'staffPasswordChangeJsAssetList.js',
+			'rover.companycardsearch'	: roverJsListRoot + 'companyCardSearchJsAssetList.js',
+			'rover.companycarddetails'	: roverJsListRoot + 'companyCardDetailsJsAssetList.js',
+			'rover.reservation.staycard.billcard'		: roverJsListRoot + 'billScreenJsAssetList.js',
+			'rover.reservation.staycard.roomassignment'	: roverJsListRoot + 'roomAssignmentJsAssetList.js'
 		}
 	}
 }
