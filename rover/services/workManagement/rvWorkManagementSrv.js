d7516163003a5e74d2c4e905330cec0c15a65936
Vijay Dev <vijay@stayntouch.com>
2016-01-12 15:02:44 +0530

    CICO-22591: testing

--- rover/services/workManagement/rvWorkManagementSrv.js
+++ rover/services/workManagement/rvWorkManagementSrv.js
@@ -368,8 +368,6 @@ sntRover.service('RVWorkManagementSrv', ['$q', 'rvBaseWebSrvV2',
 			var deferred = $q.defer(),
 				url = 'api/work_assignments/assign';
 
-			console.log( options );
-
 			var params = compileAssignedRoomsParams( options.assignedRoomTasks, options.date );
 
 			RVBaseWebSrvV2.postJSON(url, params)
@@ -642,8 +640,6 @@ sntRover.service('RVWorkManagementSrv', ['$q', 'rvBaseWebSrvV2',
 
 				// flatten and remove duplicates
 				copyEmployee.touched_work_types = _.uniq( _.flatten(copyEmployee.touched_work_types) );
-
-				compiled.push(copyEmployee);
 			};
 
 			return compiled;
@@ -712,13 +708,13 @@ sntRover.service('RVWorkManagementSrv', ['$q', 'rvBaseWebSrvV2',
 						return workTypeId == id;
 					});
 
-					if ( !! hasThisWorkType ) {
-						newAssignment = $.extend(
-								{},
-								{ 'employee_id': art.id },
-								{ 'tasks': [] }
-							);
+					newAssignment = $.extend(
+							{},
+							{ 'employee_id': art.id },
+							{ 'tasks': [] }
+						);
 
+					if ( !! hasThisWorkType ) {
 						allTaskInThisWorkType = _.where(art.only_tasks, { 'work_type_id': workTypeId });
 
 						_.each(allTaskInThisWorkType, function(eachTask) {
@@ -732,14 +728,16 @@ sntRover.service('RVWorkManagementSrv', ['$q', 'rvBaseWebSrvV2',
 								.tasks
 								.push( newTask );
 						});
-
-						complied
-							.work_types[index]
-							.assignments
-							.push( newAssignment );
 					};
+
+					complied
+						.work_types[index]
+						.assignments
+						.push( newAssignment );
 				});
 			});
+
+			console.log( complied );
 
 			return complied;
 		};
