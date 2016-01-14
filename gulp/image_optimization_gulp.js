0f229e88191984d329f14721093eb735ebd82944
Shahul <mails2shahul@gmail.com>
2016-01-14 12:14:25 -0500

    CICO-22556 Increased the trail level of image compression

--- gulp/image_optimization_gulp.js
+++ gulp/image_optimization_gulp.js
@@ -7,7 +7,7 @@ module.exports = function(gulp, $, options) {
 	gulp.task('compress-images-loselessly', function(){
 		return gulp.src(['images/*', 'cssimg/*'], {base: '.'})
 			.pipe(imagemin({
-				optimizationLevel: 2,
+				optimizationLevel: 7,
 				progressive: true,
 				svgoPlugins: [{removeViewBox: false}],
 				use: [pngquant()]
