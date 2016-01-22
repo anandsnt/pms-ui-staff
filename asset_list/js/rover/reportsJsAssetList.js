b3dd6d971f47116729332f31b8ff9e84357c1a86
Soumya Cyriac <soumya@stayntouch.com>
2016-01-22 15:57:54 +0530

    CICO-22556 Added missed js files

--- asset_list/js/rover/reportsJsAssetList.js
+++ asset_list/js/rover/reportsJsAssetList.js
@@ -1,16 +1,18 @@
-module.exports = {	
+module.exports = {
 	getList : function() {
 		var jsLibRoot 	= 'shared/lib/js/',
 		roverRoot 		= 'rover/',
 		reportReactViewRoot = roverRoot + 'react/reports/',
 		reportJsAssets 	= {
 			minifiedFiles: [
+				jsLibRoot + "date.js",
 				jsLibRoot + "react/react.min.js",
 				jsLibRoot + "react/JSXTransformer.min.js",
 				jsLibRoot + "react/react-with-addons.min.js",
 				roverRoot + "directives/ngReact/ngReact.js"
 			],
 			nonMinifiedFiles: [
+				jsLibRoot + "Utils.js",
 				roverRoot + "services/reports/**/*.js",
 				roverRoot + "factories/reports/**/*.js",
 				roverRoot + "constants/reports/**/*.js",
--- asset_list/js/rover/workManagementJsAssetList.js
+++ asset_list/js/rover/workManagementJsAssetList.js
@@ -1,11 +1,14 @@
-module.exports = {	
+module.exports = {
 	getList : function() {
-		var controllerRoot 	= 'rover/controllers/',
+		var sharedRoot 	= 'shared/',
+			jsLibRoot 		= sharedRoot + 'lib/js/',
+			controllerRoot 	= 'rover/controllers/',
 			servicesRoot 	= 'rover/services/',
 			assetsForScreen = {
 				minifiedFiles: [
+					jsLibRoot + 'angular-multi-select.js'
 				],
-				nonMinifiedFiles: [						
+				nonMinifiedFiles: [
 					controllerRoot + 'workManagement/**/*.js',
 					servicesRoot + 'workManagement/**/*.js',
 					'rover/filters/rangeFilter.js'
