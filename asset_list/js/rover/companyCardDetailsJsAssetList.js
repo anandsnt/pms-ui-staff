18e001e34d4249274629fbfdb9b5a374cc27af18
Shahul <mails2shahul@gmail.com>
2016-01-24 23:49:45 -0500

    CICO-22556 CICO-24332 changed the js asset list for add billing info popup

--- asset_list/js/rover/companyCardDetailsJsAssetList.js
+++ asset_list/js/rover/companyCardDetailsJsAssetList.js
@@ -5,8 +5,18 @@ module.exports = {
 			minifiedFiles: [],
 			nonMinifiedFiles: [			
 				roverRoot + "controllers/companycard/details/**/*.js",
+				roverRoot + "controllers/bill/rvBillingInformationPopupCtrl.js",
+				roverRoot + "controllers/bill/rvRouteDetailsCtrl.js",
 				roverRoot + "controllers/cardsOutside/rvCompanyCardArTransactionsCtrl.js",
 				roverRoot + "services/rvCompanyCardSrv.js",
+				roverRoot + "services/bill/rvBillingInfoSrv.js",
+				'rover/services/bill/rvBillCardSrv.js',
+				'rover/controllers/bill/rvRoutesAddPaymentCtrl.js',
+				'rover/controllers/roverPayment/rvCardOptionsCtrl.js',
+				'rover/services/rvReservationSrv.js',
+				'rover/services/payment/rvPaymentSrv.js',
+				'rover/services/payment/rvGuestPaymentSrv.js',
+
  				'rover/directives/delayTextbox/rvDelayTextBox.js',
 				'rover/directives/Autogrowing text field/autoGrowFieldDirective.js',
 
