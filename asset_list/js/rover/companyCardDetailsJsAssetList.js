ce0483063154fcff93b91b9a0d93a1432fbeceda
Shahul <mails2shahul@gmail.com>
2016-01-26 11:48:28 -0500

    CICO-24363 Added pay button related js asset to company card

--- asset_list/js/rover/companyCardDetailsJsAssetList.js
+++ asset_list/js/rover/companyCardDetailsJsAssetList.js
@@ -7,8 +7,11 @@ module.exports = {
 				roverRoot + "controllers/companycard/details/**/*.js",
 				roverRoot + "controllers/cardsOutside/rvCompanyCardArTransactionsCtrl.js",
 				roverRoot + "services/rvCompanyCardSrv.js",
+				'rover/services/payment/rvPaymentSrv.js',
+				'rover/services/accounts/rvAccountsTransactionSrv.js',
 				'rover/controllers/contractStartCalendarCtrl.js',
 				'rover/controllers/contractEndCalendarCtrl.js',
+				'rover/controllers/cardsOutside/rvArTransactionsPayCreditsController.js',
 
  				'rover/directives/delayTextbox/rvDelayTextBox.js',
 				'rover/directives/Autogrowing text field/autoGrowFieldDirective.js',
