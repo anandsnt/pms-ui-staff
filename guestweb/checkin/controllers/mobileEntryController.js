f8965f964a0c7a2fcf0f01d1dc087205dbbbb736
resheil <resheil@stayntouch.com>
2016-02-04 17:16:31 -0500

    CICO-23803 saving country code

--- guestweb/checkin/controllers/mobileEntryController.js
+++ guestweb/checkin/controllers/mobileEntryController.js
@@ -61,7 +61,7 @@
     		$modal.open(errorOpts);
     	}
     	else{
-        guestDetailsService.postGuestDetails({"mobile":$scope.guestDetails.mobile}).then(function(response) {
+        guestDetailsService.postGuestDetails({"mobile":$scope.dial+"-"+$scope.guestDetails.mobile}).then(function(response) {
           $scope.isLoading = false;
           $scope.mobileUpdated = true;
           $rootScope.userMobile = $scope.guestDetails.mobile;
