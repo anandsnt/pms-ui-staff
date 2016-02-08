59824d3a3b5491221021ca37b92a238e7f91de09
resheil <resheil@stayntouch.com>
2016-02-08 12:00:14 -0500

    CICO-24453 adding provision to add checkout time

--- guestweb_v2/controllers/checkout/gwCheckoutNowInitialCtrl.js
+++ guestweb_v2/controllers/checkout/gwCheckoutNowInitialCtrl.js
@@ -3,11 +3,15 @@
 */
 sntGuestWeb.controller('gwCheckoutNowInitialCtrl', ['$scope','$state','gwWebSrv','$timeout',
  function($scope,$state,gwWebSrv,$timeout) {
+
+ 		var checkout_time = gwWebSrv.reservationAndhotelData.checkoutTime =  "10:20 PM";
+
 	    //TODO : remove unwanted injections like $timeout
 	 	BaseCtrl.call(this, $scope);
 	 	var init = function(){
 			var screenIdentifier = "CHECKOUT_NOW_LANDING";
 			$scope.screenCMSDetails =  gwWebSrv.extractScreenDetails(screenIdentifier);
+			$scope.screenCMSDetails.description = $scope.screenCMSDetails.description.replace("@checkout-time",checkout_time);
 		}();
 
 }]);
--- guestweb_v2/controllers/checkout/gwCheckoutOptionsCtrl.js
+++ guestweb_v2/controllers/checkout/gwCheckoutOptionsCtrl.js
@@ -4,11 +4,14 @@
 
 sntGuestWeb.controller('checkOutOptionsController', ['$scope','$state','gwWebSrv','$timeout',
  function($scope,$state,gwWebSrv,$timeout) {
+
+ 		var checkout_time = gwWebSrv.reservationAndhotelData.checkoutTime =  "10:20 PM";
 	    //TODO : remove unwanted injections like $timeout
 	 	BaseCtrl.call(this, $scope);
 	 	var init = function(){
 			var screenIdentifier = "CHECKOUT_LANDING";
 			$scope.screenCMSDetails =  gwWebSrv.extractScreenDetails(screenIdentifier);
+			$scope.screenCMSDetails.description = $scope.screenCMSDetails.description.replace("@checkout-time",checkout_time);
 		}();
 
 }]);
