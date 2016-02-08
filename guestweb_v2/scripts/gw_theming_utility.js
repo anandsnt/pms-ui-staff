c59c5d84de5a4053bf8ebcff83a52c35117d5250
resheil <resheil@stayntouch.com>
2016-02-08 09:54:00 -0500

    CICO-24453 minor change

--- guestweb_v2/scripts/gw_theming_utility.js
+++ guestweb_v2/scripts/gw_theming_utility.js
@@ -6,10 +6,16 @@ function isBlank(pString){
 }
 
 function addStyleString(str) {
-    var node = document.createElement('style');
+	var node = document.createElement('style');
     node.innerHTML = str;
-    document.body.appendChild(node);
+    document.head.appendChild(node);   
 }
+
+var styleString = "";
+
+function appendStyleString(str){
+	styleString = styleString.length > 0 ? (styleString + str)  : str;
+};
 
 /*
 * This is to override the existing demo theme styling with the CMS contents if
@@ -18,34 +24,40 @@ function addStyleString(str) {
 var overrideStylesWithCMSdata = function (styles){
 	//set the background color
 	if(!isBlank(styles.body_background)){
-		addStyleString('body { background:'+ styles.body_background+' !important}');
+		appendStyleString('body { background:'+ styles.body_background+' !important}');
 	}
 	// set nav bar background color
 	if(!isBlank(styles.header_background)){
-		addStyleString('.header-bar { background:'+ styles.header_background+' !important}');
+		appendStyleString('.header-bar { background:'+ styles.header_background+' !important}');
 	}
 	// set sub main text color
 	if(!isBlank(styles.template_sub_text_color)){
-		addStyleString('.sub-main-text { color:'+ styles.template_sub_text_color+' !important}');
+		appendStyleString('.sub-main-text { color:'+ styles.template_sub_text_color+' !important}');
 	}
 	// set  main text color
 	if(!isBlank(styles.template_text_color)){
-		addStyleString('.template-text,.main-text { color:'+ styles.template_text_color+'!important}');
+		appendStyleString('.template-text,.main-text { color:'+ styles.template_text_color+'!important}');
 	}
 	// set template button bg color
 	if(!isBlank(styles.template_button_bg)){
-		addStyleString('.btn,.btn:hover { background:'+ styles.template_button_bg+'!important}');
+		appendStyleString('.btn,.btn:hover { background:'+ styles.template_button_bg+'!important}');
 	}
 	// set template button text color
 	if(!isBlank(styles.template_button_color)){
-		addStyleString('.btn,.btn:hover  { color:'+ styles.template_button_color+'!important}');
+		appendStyleString('.btn,.btn:hover  { color:'+ styles.template_button_color+'!important}');
 	}
 	// set light button bg color
 	if(!isBlank(styles.light_button_bg)){
-		addStyleString('.light-button,.light-button:hover { background:'+ styles.light_button_bg+'!important}');
+		appendStyleString('.light-button,.light-button:hover { background:'+ styles.light_button_bg+'!important}');
 	}
 	// set dark button bg color
 	if(!isBlank(styles.dark_button_bg)){
-		addStyleString('.dark-button,.dark-button:hover { background:'+ styles.dark_button_bg+'!important}');
+		appendStyleString('.dark-button,.dark-button:hover { background:'+ styles.dark_button_bg+'!important}');
 	}
+	if(styleString.length > 0){
+		addStyleString(styleString+';');
+	}
+	else{
+		return;
+	};
 };
