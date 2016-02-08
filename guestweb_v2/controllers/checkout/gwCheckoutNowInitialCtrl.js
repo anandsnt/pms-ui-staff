973274af11fb5f410b9c65437583e38ae0992b3e
resheil <resheil@stayntouch.com>
2016-02-08 16:08:14 -0500

    CICO-24453 date picker styles

--- guestweb_v2/controllers/checkout/gwCheckoutNowInitialCtrl.js
+++ guestweb_v2/controllers/checkout/gwCheckoutNowInitialCtrl.js
@@ -4,14 +4,14 @@
 sntGuestWeb.controller('gwCheckoutNowInitialCtrl', ['$scope','$state','gwWebSrv','$timeout',
  function($scope,$state,gwWebSrv,$timeout) {
 
- 		var checkout_time = gwWebSrv.reservationAndhotelData.checkoutTime =  "10:20 PM";
+ 		$scope.checkout_time = gwWebSrv.reservationAndhotelData.checkoutTime =  "10:20 PM";
 
 	    //TODO : remove unwanted injections like $timeout
 	 	BaseCtrl.call(this, $scope);
 	 	var init = function(){
 			var screenIdentifier = "CHECKOUT_NOW_LANDING";
 			$scope.screenCMSDetails =  gwWebSrv.extractScreenDetails(screenIdentifier);
-			$scope.screenCMSDetails.description = $scope.screenCMSDetails.description.replace("@checkout-time",checkout_time);
+			$scope.screenCMSDetails.description = $scope.screenCMSDetails.description.replace("@checkout-time",$scope.checkout_time);
 		}();
 
 }]);
--- guestweb_v2/controllers/checkout/gwCheckoutOptionsCtrl.js
+++ guestweb_v2/controllers/checkout/gwCheckoutOptionsCtrl.js
@@ -5,13 +5,13 @@
 sntGuestWeb.controller('checkOutOptionsController', ['$scope','$state','gwWebSrv','$timeout',
  function($scope,$state,gwWebSrv,$timeout) {
 
- 		var checkout_time = gwWebSrv.reservationAndhotelData.checkoutTime =  "10:20 PM";
+ 		$scope.checkout_time = gwWebSrv.reservationAndhotelData.checkoutTime =  "10:20 PM";
 	    //TODO : remove unwanted injections like $timeout
 	 	BaseCtrl.call(this, $scope);
 	 	var init = function(){
 			var screenIdentifier = "CHECKOUT_LANDING";
 			$scope.screenCMSDetails =  gwWebSrv.extractScreenDetails(screenIdentifier);
-			$scope.screenCMSDetails.description = $scope.screenCMSDetails.description.replace("@checkout-time",checkout_time);
+			$scope.screenCMSDetails.description = $scope.screenCMSDetails.description.replace("@checkout-time",$scope.checkout_time);
 		}();
 
 }]);
