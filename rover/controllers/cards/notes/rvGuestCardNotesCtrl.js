b34b4930048eb442a9e45187660c23c49613176d
Shahul <mails2shahul@gmail.com>
2016-02-08 11:11:27 -0500

    CICO-11838 UI - implemented the changes required for editing Note as well as JSCS Formatter applying

--- rover/controllers/cards/notes/rvGuestCardNotesCtrl.js
+++ rover/controllers/cards/notes/rvGuestCardNotesCtrl.js
@@ -1,119 +1,181 @@
-angular.module('sntRover').controller('rvGuestCardNotesCtrl', 
-	['$scope', 
-	'rvGuestCardNotesSrv', 'RVDashboardSrv', 
-	function($scope, rvGuestCardNotesSrv, RVDashboardSrv){
+angular.module('sntRover').controller('rvGuestCardNotesCtrl',
+	['$scope',
+	'rvGuestCardNotesSrv', 'RVDashboardSrv',
+	function($scope, rvGuestCardNotesSrv, RVDashboardSrv) {
 
-		BaseCtrl.call(this, $scope);
-		
-		var guestID = null;
+  BaseCtrl.call(this, $scope);
 
-		/**
-		 * success call back for notes list fetch
-		 * @param  {Array} notes [description]
-		 * @return {[type]}       [description]
-		 */
-		var successCallBackOfFetchNotesForThisGuest = function(notes){
-			$scope.notes = notes;
-			$scope.refreshScroller('guestcard_notes_scroller');
-		};
+  var guestID = null;
 
-		/**
-		 * to fetch against the 
-		 * @return {[type]} [description]
-		 */
-		var fetchNotesForThisGuest = function(){
-			var params  = {
-				guestID : guestID
-			};
-	        var options = {
-	            params 			: params,
-	            successCallBack : successCallBackOfFetchNotesForThisGuest
-	        };
-	        $scope.callAPI(rvGuestCardNotesSrv.fetchNotesForGuest, options);
-		};
+  /**
+   * success call back for notes list fetch
+   * @param  {Array} notes [description]
+   * @return {undefined}
+   */
+  var successCallBackOfFetchNotesForThisGuest = function(notes) {
+    $scope.notes = notes;
+    $scope.refreshScroller('guestcard_notes_scroller');
+  };
 
-		/**
-		 * @param  {Object} data [response from backend]
-		 * @param  {Object} successCallBackParameters
-		 * @return {undefined}
-		 */
-		var successCallBackOfFetchDeleteNoteFromGuestCard = function(data, successCallBackParameters) {
-			//we are going to stripe the note from the list
-			var indexToDelete = successCallBackParameters.index;
-			$scope.notes.splice(indexToDelete, 1);
-			$scope.refreshScroller('guestcard_notes_scroller');
-		};
+  /**
+   * to fetch against the
+   * @return {undefined}
+   */
+  var fetchNotesForThisGuest = function() {
+    var params  = {
+      guestID : guestID
+    };
 
-		/**
-		 * to delete a note from the list
-		 * @param  {number} noteID
-		 * @return {undefined}
-		 */
-		$scope.deleteGuestcardNote = function(noteID, deletingIndex){
-			var params  = {
-				noteID 	: noteID,
-				guestID : guestID
-			};
-	        var options = {
-	            params 			: params,
-	            successCallBack : successCallBackOfFetchDeleteNoteFromGuestCard,
-	            successCallBackParameters : {
-     				index: deletingIndex
-     			}
-	        };
-	        $scope.callAPI(rvGuestCardNotesSrv.deleteNoteFromGuestCard, options);			
-		};
+    var options = {
+      params 			: params,
+      successCallBack 	: successCallBackOfFetchNotesForThisGuest
+    };
+    $scope.callAPI(rvGuestCardNotesSrv.fetchNotesForGuest, options);
+  };
 
-		/**
-		 * @param  {Object} data [response from backend with new note id, time, user details]
-		 * @return {undefined}
-		 */
-		var successCallBackOfFetchCreateNoteFromGuestCard = function(data) {
-			//we are adding to the list with the response
-			var userDetails = RVDashboardSrv.getUserDetails();
-			var noteToAdd = {
-				'posted_user_first_name': userDetails.first_name,
-				'posted_user_last_name' : userDetails.last_name,
-				'posted_user_image_url' : userDetails.user_image_url,
-				'text'					: $scope.note_text,
-				'time' 					: data.time,
-				'date' 					: data.date,
-				'id' 					: data.id
-			};
-			$scope.notes.unshift(0);
-			$scope.notes[0] = noteToAdd;
+  /**
+   * @param  {Object} data [response from backend]
+   * @param  {Object} successCallBackParameters
+   * @return {undefined}
+   */
+  var successCallBackOfFetchDeleteNoteFromGuestCard = function(data, successCallBackParameters) {
+    //we are going to stripe the note from the list
+    var indexToDelete = successCallBackParameters.index;
+    $scope.notes.splice(indexToDelete, 1);
+    $scope.refreshScroller('guestcard_notes_scroller');
+  };
 
-			//clearing the textbox
-			$scope.note_text = '';
+  /**
+   * to delete a note from the list
+   * @param  {number} noteID
+   * @return {undefined}
+   */
+  $scope.deleteGuestcardNote = function(event, noteID, deletingIndex) {
+  	event.stopPropagation();
+  	
+  	$scope.cancelEditMode();
 
-			$scope.refreshScroller('guestcard_notes_scroller');
-		};
+    var params  = {
+      noteID 	: noteID,
+      guestID 	: guestID
+    };
 
-		/**
-		 * to delete a note from the list
-		 * @param  {number} noteID
-		 * @return {undefined}
-		 */
-		$scope.createGuestcardNote = function(){
-			var params  = {
-				guestID : guestID,
-				text 	: $scope.note_text
-			};
-	        var options = {
-	            params 			: params,
-	            successCallBack : successCallBackOfFetchCreateNoteFromGuestCard
-	        };
-	        $scope.callAPI(rvGuestCardNotesSrv.createNoteFromGuestCard, options);			
-		};
+    var options = {
+      params 			: params,
+      successCallBack 	: successCallBackOfFetchDeleteNoteFromGuestCard,
+      successCallBackParameters : {
+        index: deletingIndex
+      }
+    };
+    $scope.callAPI(rvGuestCardNotesSrv.deleteNoteFromGuestCard, options);
+  };
 
-		/**
-		 * initialization Stuffs
-		 * @return {undefined}
-		 */
-		var initializeMe = function(){
-			guestID 		= $scope.guestCardData.userId;
-			$scope.notes 	= [];
-			$scope.setScroller('guestcard_notes_scroller', {});
-			fetchNotesForThisGuest();
-		}();
+  /**
+   * @param  {Object} data [response from backend with new note id, time, user details]
+   * @return {undefined}
+   */
+  var successCallBackOfFetchCreateNoteFromGuestCard = function(data) {
+    //we are adding to the list with the response
+    var userDetails = RVDashboardSrv.getUserDetails();
+    var noteToAdd = {
+      'posted_user_first_name'	: userDetails.first_name,
+      'posted_user_last_name' 	: userDetails.last_name,
+      'posted_user_image_url' 	: userDetails.user_image_url,
+      'text'					: $scope.noteText,
+      'time' 					: data.time,
+      'date' 					: data.date,
+      'id' 						: data.id
+    };
+    $scope.notes.unshift(0);
+    $scope.notes[0] = noteToAdd;
+
+    //clearing the textbox
+    $scope.noteText = '';
+
+    $scope.refreshScroller('guestcard_notes_scroller');
+  };
+
+  /**
+   * to delete a note from the list
+   * @param  {number} noteID
+   * @return {undefined}
+   */
+  $scope.createGuestcardNote = function() {
+    var params  = {
+      guestID 	: guestID,
+      text 		: $scope.noteText
+    };
+
+    var options = {
+      params 			: params,
+      successCallBack 	: successCallBackOfFetchCreateNoteFromGuestCard
+    };
+    $scope.callAPI(rvGuestCardNotesSrv.createNoteFromGuestCard, options);
+  };
+
+  /**
+   * [successCallBackOfFetchUpdateActiveNote description]
+   * @param  {Object} [API response]
+   * @return {undefined}
+   */
+  var successCallBackOfFetchUpdateActiveNote = function(data){
+  	$scope.cancelEditMode();
+  	fetchNotesForThisGuest();
+  };
+
+  /**
+   * to update the current the choosed note
+   * @return {undefined}
+   */
+  $scope.updateActiveNote = function() {
+  	if($scope.editingNote === null) {
+  		$scope.errorMessage = ['Something went wrong, please switch tab and comeback'];
+  		return;
+  	}
+
+    var params  = {
+      noteID 	: $scope.editingNote.id,
+      guestID 	: guestID,
+      text 		: $scope.noteText
+    };
+
+    var options = {
+      params 			: params,
+      successCallBack 	: successCallBackOfFetchUpdateActiveNote
+    };
+    $scope.callAPI(rvGuestCardNotesSrv.updateNoteFromGuestCard, options);  	
+  };
+
+  /**
+   * whenever we clicked on note, we will switch to editing mode
+   * @return {undefined}
+   */
+  $scope.clickedOnNote = function(note) {
+    $scope.editingNote 	= note;
+    $scope.noteText 	= note.text;
+  };
+
+  /**
+   * to cancel edit mode
+   * @return {undefined}
+   */
+  $scope.cancelEditMode = function(){
+    $scope.editingNote 	= null;
+    $scope.noteText 	= '';
+  };
+
+  /**
+   * initialization Stuffs
+   * @return {undefined}
+   */
+  var initializeMe = function() {
+    guestID 			= $scope.guestCardData.userId;
+    $scope.editingNote 	= null;
+    $scope.notes 		= [];
+    $scope.noteText 	= '';
+
+    $scope.setScroller('guestcard_notes_scroller', {});
+
+    fetchNotesForThisGuest();
+  }();
 }]);
--- rover/services/guestcard/notes/rvGuestCardNotesSrv.js
+++ rover/services/guestcard/notes/rvGuestCardNotesSrv.js
@@ -1,50 +1,62 @@
 angular.module('sntRover').service('rvGuestCardNotesSrv', ['$q', 'rvBaseWebSrvV2',
 	function($q, rvBaseWebSrvV2) {
 
-		this.fetchNotesForGuest = function(params) {
-			var url 		= '/api/guest_details/' + params.guestID + '/notes',
-				deferred 	= $q.defer();
-			
-			rvBaseWebSrvV2.getJSON(url).then(
-				function(data) {
-					deferred.resolve(data.notes);
-				},
-				function(errorMessage) {
-					deferred.reject(errorMessage);
-				}
-			);
-			return deferred.promise;			
-		};
+  this.fetchNotesForGuest = function(params) {
+    var url 		= '/api/guest_details/' + params.guestID + '/notes',
+    	deferred 	= $q.defer();
 
-		this.deleteNoteFromGuestCard = function(params) {
-			var url 		= '/api/guest_details/' + params.guestID + '/notes/' + params.noteID,
-				deferred 	= $q.defer();
-			
-			rvBaseWebSrvV2.deleteJSON(url).then(
-				function(data) {
-					deferred.resolve(data.notes);
-				},
-				function(errorMessage) {
-					deferred.reject(errorMessage);
-				}
-			);
-			return deferred.promise;			
-		};
+    rvBaseWebSrvV2.getJSON(url)
+    .then(function(data) {
+      	deferred.resolve(data.notes);
+    },
+	function(errorMessage) {
+  	  	deferred.reject(errorMessage);
+	});
+    return deferred.promise;
+  };
 
-		this.createNoteFromGuestCard = function(params) {
-			var data 		= { 'text': params.text },
-				//url 		= 'ui/show?json_input=cards/new_note.json&format=json',
-				url 		= '/api/guest_details/' + params.guestID + '/notes',
-				deferred 	= $q.defer();
-			
-			rvBaseWebSrvV2.postJSON(url, data).then(
-				function(data) {
-					deferred.resolve(data);
-				},
-				function(errorMessage) {
-					deferred.reject(errorMessage);
-				}
-			);
-			return deferred.promise;			
-		};		
-}]);		
+  this.updateNoteFromGuestCard = function(params) {
+    var url 		= '/api/guest_details/' + params.guestID + '/notes/' + params.noteID,
+    	deferred 	= $q.defer(),
+    	data 		= {'text': params.text};
+
+    rvBaseWebSrvV2.putJSON(url, data)
+    .then(function(data) {
+  		deferred.resolve(data);
+	},
+	function(errorMessage) {
+  		deferred.reject(errorMessage);
+	});
+    return deferred.promise;
+  };
+
+  this.deleteNoteFromGuestCard = function(params) {
+    var url 		= '/api/guest_details/' + params.guestID + '/notes/' + params.noteID,
+    	deferred 	= $q.defer();
+
+    rvBaseWebSrvV2.deleteJSON(url)
+    .then(function(data) {
+  		deferred.resolve(data);
+	},function(errorMessage) {
+  		deferred.reject(errorMessage);
+	});
+    return deferred.promise;
+  };
+
+  this.createNoteFromGuestCard = function(params) {
+    var data 		= {'text': params.text},
+	    //url 		= 'ui/show?json_input=cards/new_note.json&format=json',
+	    url 		= '/api/guest_details/' + params.guestID + '/notes',
+	    deferred 	= $q.defer();
+
+    rvBaseWebSrvV2.postJSON(url, data)
+    .then(function(data) {
+  		deferred.resolve(data);
+	},
+	function(errorMessage) {
+  		deferred.reject(errorMessage);
+	});
+    return deferred.promise;
+  };
+}]);
+		
