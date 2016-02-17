a9f4a91c94db00864994c2aaede161f69adaf186
soumyastayntouch <soumya@stayntouch.com>
2016-02-17 20:02:46 +0530

    CICO-25067 Borrow button click - Setting params

--- rover/controllers/roomAssignment/rvBorrowRoomTypeCtrl.js
+++ rover/controllers/roomAssignment/rvBorrowRoomTypeCtrl.js
@@ -5,8 +5,22 @@ sntRover.controller('rvBorrowRoomTypeCtrl',[
     // BaseCtrl.call(this, $scope);
 
     console.log(">>>>>>>>>>");
+    console.log($scope.reservationData)
+    console.log("=======")
     console.log($scope.passingParams)
-$scope.check = "test "
+    $scope.clickedBorrowButton = function(){
+        var params = {};
+
+
+        //CICO-17082
+        params.forcefully_assign_room   = true;
+
+        params.reservation_id   = $scope.reservationData.reservation_card.reservation_id;
+        params.upsell_amount = $scope.passingParams.upsell_amount;
+        params.room_no          = $scope.assignedRoom.room_number;
+        console.log(params)
+
+    };
 
 
 
