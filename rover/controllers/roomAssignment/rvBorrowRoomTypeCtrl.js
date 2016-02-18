512c56f7a9c105b36ee30afe01c580e37dba40b9
soumyastayntouch <soumya@stayntouch.com>
2016-02-18 16:27:03 +0530

    CICO-25067 Fixed issue with borrow button

--- rover/controllers/roomAssignment/rvBorrowRoomTypeCtrl.js
+++ rover/controllers/roomAssignment/rvBorrowRoomTypeCtrl.js
@@ -1,23 +1,25 @@
 sntRover.controller('rvBorrowRoomTypeCtrl',[
     '$scope',
-    function($scope) {
+    'RVUpgradesSrv',
+    'ngDialog',
+    function($scope, RVUpgradesSrv, ngDialog) {
 
     // BaseCtrl.call(this, $scope);
-
-    console.log(">>>>>>>>>>");
-    console.log($scope)
-    console.log("=======")
-    console.log($scope.passingParams)
     $scope.selectedUpgrade = {};
 
     _.extend($scope.selectedUpgrade,
         {
             room_id         : $scope.assignedRoom.room_id,
             room_no         : $scope.assignedRoom.room_number,
-         //   room_type_name  : $scope.assignedRoom.upgrade_room_type_name,
+            room_type_name  : $scope.assignedRoom.room_type_name,
             room_type_code  : $scope.assignedRoom.room_type_code,
           //  room_type_level : parseInt(selectedListItem.room_type_level)
         });
+    var successCallbackselectUpgrade = function(data) {
+        $scope.closeDialog();
+      //  $scope.selectedUpgrade.is_upsell_available = data.is_upsell_available;
+        $scope.$emit('upgradeSelected', $scope.selectedUpgrade);
+    };
     $scope.clickedBorrowButton = function(){
         var params = {};
 
@@ -29,6 +31,16 @@ sntRover.controller('rvBorrowRoomTypeCtrl',[
         params.upsell_amount = $scope.passingParams.upsell_amount;
         params.room_no          = $scope.assignedRoom.room_number;
         console.log(params)
+
+
+
+        //yes. ALL set. Go!
+        var options = {
+            params          : params,
+            successCallBack : successCallbackselectUpgrade
+
+        };
+        $scope.callAPI(RVUpgradesSrv.selectUpgrade, options);
 
     };
 
