7079ee6fa8f118dfa75293e4d8769abfe3779b2f
soumyastayntouch <soumya@stayntouch.com>
2016-02-18 17:08:52 +0530

    CICO-25067 Removed unused codes

--- rover/controllers/roomAssignment/rvBorrowRoomTypeCtrl.js
+++ rover/controllers/roomAssignment/rvBorrowRoomTypeCtrl.js
@@ -17,24 +17,19 @@ sntRover.controller('rvBorrowRoomTypeCtrl',[
         });
     var successCallbackselectUpgrade = function(data) {
         $scope.closeDialog();
-      //  $scope.selectedUpgrade.is_upsell_available = data.is_upsell_available;
         $scope.$emit('upgradeSelected', $scope.selectedUpgrade);
     };
     $scope.clickedBorrowButton = function(){
         var params = {};
 
 
-        //CICO-17082
+        //CICO-25067
         params.forcefully_assign_room   = true;
 
         params.reservation_id   = $scope.reservationData.reservation_card.reservation_id;
         params.upsell_amount = $scope.passingParams.upsell_amount;
         params.room_no          = $scope.assignedRoom.room_number;
-        console.log(params)
 
-
-
-        //yes. ALL set. Go!
         var options = {
             params          : params,
             successCallBack : successCallbackselectUpgrade
