1b492058011828a1c8329a29d54976d94362cae7
krishobh <krishobh@stayntouch.com>
2016-02-21 15:12:59 +0530

    CICO-25249 error handling

--- rover/controllers/cards/stayCardMainCtrl.js
+++ rover/controllers/cards/stayCardMainCtrl.js
@@ -709,8 +709,18 @@ angular.module('sntRover').controller('stayCardMainCtrl', ['$rootScope', '$scope
 					$scope.$emit('hideLoader');
 					$scope.newCardData = cardData;
 					that.attachCompanyTACardRoutings(card, cardData);
+					ngDialog.close();
 				},
 				onReplaceFailure = function(error) {
+					console.log(error);
+					console.log(error.httpStatus);
+					//$scope.$broadcast("SHOWERRORMESSAGE",error);
+					ngDialog.open({
+						template: '/assets/partials/cards/alerts/contractedRateChangeFailure.html',
+						scope: $scope,
+						closeByDocument: false,
+						closeByEscape: false
+					});
 					$scope.cardRemoved();
 					//480 is reserved for cases where trial to use the card fails fails
 					if (error.httpStatus === 480) {
