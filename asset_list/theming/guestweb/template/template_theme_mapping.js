dfb82d7036eae71edcb074d24bee2ccfa0ed1328
NeenuBal <neenu@stayntouch.com>
2016-02-29 15:09:21 +0530

    CICO-25735 added web templates files and css files

--- asset_list/theming/guestweb/template/template_theme_mapping.js
+++ asset_list/theming/guestweb/template/template_theme_mapping.js
@@ -201,7 +201,14 @@ module.exports = {
 										'guestweb/**/common_templates/partials/gwNoOption.html',
 										checkoutNowPartials+'*.html',
 										checkinPartials+"*.html",
+										precheckinPartials+'*.html'],
+			'guestweb_naples_grande'		:[ 'guestweb/**/common_templates/partials/checkin/**.html',
+										'guestweb/**/common_templates/partials/checkout/**.html',
+										'guestweb/**/common_templates/partials/gwNoOption.html',
+										checkoutNowPartials+'*.html',
+										checkinPartials+"*.html",
 										precheckinPartials+'*.html']
+
 		}
 	}
 }
