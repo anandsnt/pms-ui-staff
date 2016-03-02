935c2e87b2d3685dfad34f6ca41c296767f912cc
rahulkookal <rahul@stayntouch.com>
2016-03-02 14:49:59 +0530

    CICO-24944 - Added template for check in messages with required translation.

--- admin/controllers/adZestStationCheckInCtrl.js
+++ admin/controllers/adZestStationCheckInCtrl.js
@@ -26,7 +26,8 @@ admin.controller('ADZestStationCheckInCtrl',['$scope','$rootScope', '$state','$s
                                         {
                                             "registration_card":$scope.zestSettings.registration_card,
                                             "reg_card_text":$scope.zestSettings.reg_card_text,
-                                            "enforce_deposit":$scope.zestSettings.enforce_deposit
+                                            "enforce_deposit":$scope.zestSettings.enforce_deposit,
+                                            "check_in_message_texts" : $scope.zestSettings.check_in_message_texts
                                         }
 
                              };
