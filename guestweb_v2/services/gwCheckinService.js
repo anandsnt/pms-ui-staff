adf2424cabc96f3076ff8d0cf710ba92873be26b
resheil <resheil@stayntouch.com>
2016-03-08 14:52:17 -0500

    CICO-24453 integrating external checkin controller

---
+++ guestweb_v2/services/gwCheckinService.js
@@ -0,0 +1,43 @@
+sntGuestWeb.service('GwCheckinSrv',['$q', 'GWBaseWebSrv','GWBaseWebSrv2','GwWebSrv', function($q, GWBaseWebSrv,GWBaseWebSrv2,GwWebSrv){
+
+	this.checkinData = {};
+	this.setcheckinData = function (responseData){
+		this.checkinData = responseData;
+	};
+	this.getcheckinData = function (){
+		return this.checkinData;
+	};
+	/**
+	 * to verify checkin user
+	 * @return {undefined}
+	 */
+	this.findUser = function(params) {
+		var deferred = $q.defer();
+		params.application = (typeof GwWebSrv.zestwebData.application !=="undefined") ? GwWebSrv.zestwebData.application : "";
+		var url = '/guest_web/checkin_reservation_search.json';
+		GWBaseWebSrv2.getJSON(url, params).then(function(data) {
+			deferred.resolve(data);
+		},function(data){
+			deferred.reject(data);
+		});
+		return deferred.promise;
+	};
+
+	/**
+	 * to generate auth token
+	 * @return {undefined}
+	 */
+	this.generateAuthToken = function(params) {
+		var deferred = $q.defer();
+		params.application = (typeof GwWebSrv.zestwebData.application !=="undefined") ? GwWebSrv.zestwebData.application : "";
+		var url = '/guest_web/authenticate_checkin_guest';
+		GWBaseWebSrv2.postJSON(url, params).then(function(data) {
+			deferred.resolve(data);
+		},function(data){
+			deferred.reject(data);
+		});
+		return deferred.promise;
+	};
+
+	
+}]);
