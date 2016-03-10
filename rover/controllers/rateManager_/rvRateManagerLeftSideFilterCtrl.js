d91f49916889c66668d020209d974e6bcb9aa39f
Dilip Sahayadhas <dilips@stayntouch.com>
2016-03-10 16:57:55 -0500

    CICO-25336 Stubs for service calls to load the grid

--- rover/controllers/rateManager_/rvRateManagerLeftSideFilterCtrl.js
+++ rover/controllers/rateManager_/rvRateManagerLeftSideFilterCtrl.js
@@ -41,7 +41,21 @@ angular.module('sntRover').controller('rvRateManagerLeftSideFilterCtrl', [
          * This method handles on-click of the SHOW RATES BUTTON
          */
         $scope.onClickShowRates = function(){
-
+            // Find out the view selected by the user
+            /**
+             * NOTE The possible views are
+             *  1. Multiple Rates
+             *  2. All Room Types
+             *  3. Single Rate
+             */
+            let filter = $scope.rateManagerDataModel.filterOptions;
+            if(filter.viewTypeSelection.showAllRoomTypes){
+            //    All Room Types
+            }else if(filter.viewTypeSelection.selectedRates.length === 1){
+            //    Single Rate
+            }else{
+            //    Multiple Rates View
+            }
         };
 
     }]);
--- rover/services/rateManager_/rvRateManagerCoreSrv.js
+++ rover/services/rateManager_/rvRateManagerCoreSrv.js
@@ -9,17 +9,57 @@ angular.module('sntRover').service('rvRateManagerCoreSrv', ['$q', 'BaseWebSrvV2'
          *          2. rate_ids
          *          3. name_card_ids
          *          4. group_by
-         *          5. order_by
+         *          5. order_id
          *          6. from_date
          *          7. to_date
          * B. ALL ROOM TYPES
-         *      //api/daily_rates/room_restrictions
+         *      /api/daily_rates/room_restrictions
          *      Request Params
          *          1. from_date
          *          2. to_date
+         *          3. order_id
          * C. INDIVIDUAL RATE
-         *
+         *      /api/daily_rates/:id
+         *      Request Params
+         *          1. from_date
+         *          2. to_date
+         *          3. order_id
          */
+
+        var service = this;
+
+        service.fetchMultipleRateInfo = function (params) {
+            var url = '/api/daily_rates/';
+            var deferred = $q.defer();
+            BaseWebSrvV2.getJSON(url, params).then(function (data) {
+                deferred.resolve(data);
+            }, function (data) {
+                deferred.reject(data);
+            });
+            return deferred.promise;
+        };
+
+        service.fetchAllRoomTypesInfo = function (params) {
+            var url = '/api/daily_rates/room_restrictions';
+            var deferred = $q.defer();
+            BaseWebSrvV2.getJSON(url, params).then(function (data) {
+                deferred.resolve(data);
+            }, function (data) {
+                deferred.reject(data);
+            });
+            return deferred.promise;
+        };
+
+        service.fetchSingleRateInfo = function (params) {
+            var url = '/api/daily_rates/' + params.rate_id;
+            var deferred = $q.defer();
+            BaseWebSrvV2.getJSON(url, params).then(function (data) {
+                deferred.resolve(data);
+            }, function (data) {
+                deferred.reject(data);
+            });
+            return deferred.promise;
+        };
 
 
     }
