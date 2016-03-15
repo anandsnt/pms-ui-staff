e7bb4750b584d088f650bfb0e18c4876cd703678
rahulkookal <rahul@stayntouch.com>
2016-03-15 21:00:35 +0530

    CICO-25078 :Code clean - Removed log

--- admin/controllers/adZestStationCheckInCtrl.js
+++ admin/controllers/adZestStationCheckInCtrl.js
@@ -7,7 +7,6 @@ admin.controller('ADZestStationCheckInCtrl',['$scope','$rootScope', '$state','$s
         $scope.fetchSettings = function(){
             var fetchSuccess = function(data){
                 $scope.zestSettings = data;
-                console.log($scope.zestSettings);
                 $scope.$emit('hideLoader');
             };
             $scope.invokeApi(ADZestStationSrv.fetch, {}, fetchSuccess);
