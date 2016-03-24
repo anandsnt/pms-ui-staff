394bf16e3bd4d07fb5cf698c023a3012bdd9f8cb
Mike <mike@stayntouch.com>
2016-03-24 11:19:48 -0400

    CICO-23152 updates QR Code Scanner SVG and call

--- zest_station/zsChromeAppActions.js
+++ zest_station/zsChromeAppActions.js
@@ -3,6 +3,7 @@ this.chromeApp = function(onMessageCallback, chromeAppId, fetchQRCode) {
     if (typeof chrome !== "undefined" && typeof chrome.runtime !=="undefined"){
         //only init these if using chrome, this is for the chromeapp virtual keyboard
         that.onChromeAppMsgResponse = function(response){
+            console.log(response);
             onMessageCallback(response);
             if (response){
                 if (response.isChromeApp){
@@ -10,15 +11,59 @@ this.chromeApp = function(onMessageCallback, chromeAppId, fetchQRCode) {
                 }
             }
         };
+        
         that.setupChromeAppListener = function(){
             // The ID of the extension we want to talk to.
             // Make a simple request:
             chrome.runtime.sendMessage(chromeAppId, {fromZestStation: true},this.onChromeAppMsgResponse);
+        };
+        
+        
+        that.listenerForQRCodeResponse = function(response){
+            var msg = {
+                listening: true,
+                attempt: (this.qrAttempt+1)
+            };
+            console.log('msg from ChromeApp:', response);
             
+            if (!response.qr_code){
+                setTimeout(function(){
+                    console.log('sending listening response obj for QR code...');
+                    chrome.runtime.sendMessage(chromeAppId, msg, this.listenerForQRCodeResponse);
+                },2000);
+            } else {
+                console.log('GOT QR CODE BACK FROM CHROMEAPP !!! : ',response.reservation_id);
+            }
+            
+            
+            
+            //this.onChromeAppMsgResponse();
         };
-        that.fetchQRCode = function(){
-          chrome.runtime.sendMessage(chromeAppId, "initQRCodeScan",this.onChromeAppMsgResponse);  
+        
+        
+        that.fetchQRCode = function(){ 
+            var msg = 'initQRCodeScan';
+            this.qrAttempt = 0;
+            console.log('listening for scan...');
+            chrome.runtime.sendMessage(chromeAppId, msg, this.listenerForQRCodeResponse);
+            console.log('SENDING message: ',msg);
+            
+            
+            /*
+            var port = chrome.runtime.connect(chromeAppId,{name: "qrcode"});
+            port.onMessage.addListener(function(msg) {
+                console.log('chrome app sent a message back for Knock knock! :p ');
+                if (msg.answer){
+                    port.postMessage({joke: "the_end"});
+                    onMessageCallback(onMessageCallback(msg.answer));
+                }
+            });
+            port.postMessage({joke: "whats up?"});
+            */
         };
+        
+        
+        
         if (fetchQRCode){
             that.fetchQRCode();
         } else {
