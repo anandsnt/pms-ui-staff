04a41454db4e5e29c2b2a78975eb42997a7092ac
rahulkookal <rahul@stayntouch.com>
2016-03-29 15:23:11 +0530

    CICO-26553 : Fix for QA comments.

--- zest_station/controllers/zestStation/zsReservationDetailsCtrl.js
+++ zest_station/controllers/zestStation/zsReservationDetailsCtrl.js
@@ -285,6 +285,7 @@ sntZestStation.controller('zsReservationDetailsCtrl', [
                     var nites, avgDailyRate, packageRate, taxes, subtotal, deposits, balanceDue;
                     nites = parseInt(info.total_nights);
                     $scope.selectedReservation.total_nights = nites;
+                    $scope.selectedReservation.total_hours = info.no_of_hours;
                     avgDailyRate = parseFloat(info.avg_daily_rate).toFixed(2);
                     
                     deposits = parseFloat(info.deposit_attributes.deposit_paid).toFixed(2);
