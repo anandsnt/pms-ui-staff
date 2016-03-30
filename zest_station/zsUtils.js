2a0f429f9531894f9f1eaf7cc5c67dd113415d9a
resheil <resheil@stayntouch.com>
2016-03-30 17:45:21 -0400

    CICO-26536 hotel print fix

--- zest_station/zsUtils.js
+++ zest_station/zsUtils.js
@@ -10,6 +10,13 @@ var applyStarTacStyles = function() {
     createStyleNodeWithString(starTacStyles);
     console.info("applied startac styles");
 };
+
+var applyStylesForYotelStarTac = function(){
+	applyStarTacStyles();
+	var starTacStyles = '.page-print{margin-left:10px;}#registration-card .logo{width:120px !important} #registration-card .content{text-align:left;position:fixed;top:80px;width:330px;padding-left:0;padding-right:0;}.page-print > div.content{position:fixed;top:30px;text-align:left !important;padding-left:2%;padding-right:2%}.page-print > div.content > strong.footer{text-align:center !important}.page-print > div.content > strong.large_txt,.page-print > div.content span.large_txt{font-size:2em}.page-print > div.content span > strong.large_txt{font-size:2em}.page-print > div.content span{font-size:1.2em}.page-print > div.content span.reg_text{margin-top:-60px;white-space:pre-wrap}';
+	createStyleNodeWithString(starTacStyles);
+	 
+};
 //for non startTac
 var applyPrintMargin = function(){
 	var starTacStyles = '@page { margin: 1cm 3cm; }';
