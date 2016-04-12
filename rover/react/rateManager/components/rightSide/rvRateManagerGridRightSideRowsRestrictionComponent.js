d8c2194db58ac35dfc27659745f208c87667d517
Igor Benic <igor@stayntouch.com>
2016-04-12 10:44:55 +0200

    Rate Refactor - added container for 'from price' or 'no rate' text. See pms-html for implementation. CICO-25394

--- rover/react/rateManager/components/rightSide/rvRateManagerGridRightSideRowsRestrictionComponent.js
+++ rover/react/rateManager/components/rightSide/rvRateManagerGridRightSideRowsRestrictionComponent.js
@@ -8,12 +8,17 @@ const RateManagerGridRightSideRowsRestrictionComponent = ({ onTdClick, mode, res
 					 	<td onClick={(e) => onTdClick(e, rowIndex, colIndex)} key={'key-' + colIndex} className='cell'>
 							<div className={'cell-container ' + (dateList[colIndex].isWeekEnd ? 'weekend_day': '')}>
 								<div className={'cell-content ' + (dateList[colIndex].isPastDate ? 'isHistory-cell-content': '')}>
+									<div className={'rate-single'}>
+									{/* Look at pms-html for implementation*/}
+									</div>
+									<div className='restriction_holder'>
 									{eachDayRestrictions.map((restriction, restrictionIndex) => 
 										<RateManagerRestrictionIconComponent
 											key={'key-' + restrictionIndex}
 											className={'' + restriction.className}
 											text={restriction.days}/>
 									)}
+									</div>
 								</div>
 							</div>
 						</td>
