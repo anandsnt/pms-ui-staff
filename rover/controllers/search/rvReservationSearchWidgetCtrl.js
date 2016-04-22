9db98bf797363878fe96cf59a5af85c5e7148c70
ragesh <ragesh@stayntouch.com>
2016-04-22 14:52:19 +0530

    CICO-26059 Fixed the issue related to showing the room status

--- rover/controllers/search/rvReservationSearchWidgetCtrl.js
+++ rover/controllers/search/rvReservationSearchWidgetCtrl.js
@@ -518,13 +518,13 @@ sntRover.controller('rvReservationSearchWidgetController', ['$scope', '$rootScop
 		//Map the room status to the view expected format
 		$scope.getRoomStatusMapped = function(roomstatus, fostatus, roomNo) {
 			var mappedStatus = "";
-			if (roomstatus === "READY" && fostatus === "VACANT") {
+			if(roomNo == '' || roomNo == null) {
+				mappedStatus = 'no-number';
+			} else if (roomstatus === "READY" && fostatus === "VACANT") {
 				mappedStatus = 'ready';
 			} else if(roomstatus === 'NOT READY') {
 				mappedStatus = "not-ready";
-			} else if(roomNo == '' || roomNo == null) {
-				mappedStatus = 'no-number';
-			}
+			} 
 			return mappedStatus;
 		};
 
@@ -745,13 +745,17 @@ sntRover.controller('rvReservationSearchWidgetController', ['$scope', '$rootScop
 		};
 
 
-		$scope.getMappedClassWithResStatusAndRoomStatus = function(reservation_status, roomstatus, fostatus, roomReadyStatus, checkinInspectedOnly, serviceStatus) {
+		$scope.getMappedClassWithResStatusAndRoomStatus = function(reservation_status, roomstatus, fostatus, roomReadyStatus, checkinInspectedOnly, serviceStatus, room) {
 			var mappedStatus = "room-number";
 
 			if (serviceStatus) {
 				if (serviceStatus === 'OUT_OF_SERVICE' || serviceStatus === 'OUT_OF_ORDER') {
 					return "room-grey";
 				}
+			}
+
+			if(!room) {
+				return "no-number";
 			}
 
 			if (reservation_status === 'CHECKING_IN') {
