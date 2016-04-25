63bfcd1335e173b8af53c96c0841ce32cc6d28c3
NeenuBal <neenu@stayntouch.com>
2016-04-25 18:51:57 +0530

    CICO-28154 changed routing in same file

--- asset_list/theming/guestweb/js/js_theme_mapping.js
+++ asset_list/theming/guestweb/js/js_theme_mapping.js
@@ -33,7 +33,7 @@ module.exports = {
 			'guestweb_claridge' :[ themeJsRoot + 'app_router_common.js'],
 			'guestweb_demo' 		: [ themeJsRoot + 'app_router_common.js'],
 			'guestweb_balboa':[ themeJsRoot + 'app_router_common.js'],
-			'guestweb_excalibur':[themeJsRoot + 'app_router_mgm_chain.js', themeJsRoot + 'app_router_excalibur.js'],
+			'guestweb_excalibur':[themeJsRoot + 'app_router_excalibur.js'],
 			'guestweb_luxor':[themeJsRoot + 'app_router_luxor.js'],
 			'guestweb_mgm_grand':[ themeJsRoot + 'app_router_mgm_chain.js', themeJsRoot + 'app_router_mgm_grand.js'],
 			'guestweb_signature_at_mgm':[ themeJsRoot + 'app_router_mgm_chain.js', themeJsRoot + 'app_router_signature_at_mgm.js'],
