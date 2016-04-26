70fa80881b7e5e20601aba2dd11dae7bd618932a
NeenuBal <neenu@stayntouch.com>
2016-04-26 18:01:52 +0530

    CICO-27163 CICO-27164 CICO-27165 gulp tasks

--- asset_list/theming/guestweb/css/css_theme_mapping.js
+++ asset_list/theming/guestweb/css/css_theme_mapping.js
@@ -41,7 +41,8 @@ module.exports = {
 			'guestweb_time_hotel':[ themeCSSRoot + 'guestweb_time_hotel.css'],
 			'guestweb_porto_vista':[ themeCSSRoot + 'guestweb_porto_vista.css'],
 			'guestweb_chalet_view':[ themeCSSRoot + 'guestweb_chalet_view.css'],
-			'guestweb_crawford':[ themeCSSRoot + 'guestweb_crawford.css']
+			'guestweb_crawford':[ themeCSSRoot + 'guestweb_crawford.css'],
+			'guestweb_kingsley':[ themeCSSRoot + 'guestweb_crawford.css']
 		}
 	}
 }
