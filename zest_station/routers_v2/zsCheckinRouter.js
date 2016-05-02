8eabf46ec5eab43caa33748b190973b5280aa54c
Mike <mike@stayntouch.com>
2016-05-01 20:55:29 -0400

    CICO-28178 - adds Checkin [ Reservation Details ] view + state in router + controller placeholder

--- zest_station/routers_v2/zsCheckinRouter.js
+++ zest_station/routers_v2/zsCheckinRouter.js
@@ -6,6 +6,13 @@ sntZestStation.config(['$stateProvider',
 			templateUrl: '/assets/partials_v2/checkin/zscheckInReservationSearch.html',
 			controller: 'zscheckInReservationSearchCtrl'
 		});
+                
+        //checkin reservation details 
+        $stateProvider.state('zest_station.checkInReservationDetails', {
+			url: '/checkInReservationDetails',
+			templateUrl: '/assets/partials_v2/checkin/zsCheckInReservationDetails.html',
+			controller: 'zsCheckInReservationDetailsCtrl'
+		});
 
 		//checkin key dispense
 		$stateProvider.state('zest_station.checkInKeyDispense', {
