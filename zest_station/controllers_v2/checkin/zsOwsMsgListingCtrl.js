954c0829d2d9713f232653bfa657b6de55466af6
resheil <resheil@stayntouch.com>
2016-05-04 16:13:51 -0400

    CICO-28178 ows msg implementation

---
+++ zest_station/controllers_v2/checkin/zsOwsMsgListingCtrl.js
@@ -0,0 +1,127 @@
+sntZestStation.controller('zsOwsMsgListingCtrl', [
+	'$scope',
+	'$state',
+	'zsEventConstants',
+	'$stateParams', 'zsCheckinSrv', '$rootScope', '$window',
+	function($scope, $state, zsEventConstants, $stateParams, zsCheckinSrv, $rootScope, $window) {
+
+		BaseCtrl.call(this, $scope);
+
+		var printActions = function() {
+			//emit this to paretnt ctrl to show in print
+			$rootScope.currentOwsMessage = $scope.currentOwsMessage;
+
+			setBeforePrintSetup();
+			// add the orientation
+			addPrintOrientation();
+
+			var onPrintSuccess = function(){
+				//do nothing for now
+			};
+			var onPrintError = function(){
+				//do nothing for now
+			};
+
+			/*
+			 *	======[ READY TO PRINT ]======
+			 */
+			setTimeout(function() {
+				if (sntapp.cordovaLoaded) {
+					var printer = (sntZestStation.selectedPrinter);
+					cordova.exec(
+						onPrintSuccess(), //print complete, should go to final screen
+						onPrintError(), //if print error, inform guest there was an error
+						'RVCardPlugin', 'printWebView', ['filep', '1', printer]);
+				} else {
+					$window.print();
+				}
+			}, 100);
+
+			/*
+			 *	======[ PRINTING COMPLETE. JS EXECUTION WILL UNPAUSE ]======
+			 */
+			setTimeout(function() {
+				removePrintOrientation();
+				$('.popup').show();//show popup again
+				$scope.runDigestCycle();
+			}, 100);
+		};
+
+		var init = function() {
+
+			var showEmailButton = function() {
+				//check if reservation had email id
+				$scope.showEmailButton = ($scope.selectedReservation.guest_details[0].email.length >0) ? true : false;
+			};
+
+			var onOwsMsgFetchSuccess = function(response) {
+
+				$scope.owsMessages = response.messages;
+				if ($scope.owsMessages.length > 0) {
+					//popup in zeststation was implemented in other way, not using ngdialog
+					//open popup only if there are any OWS messages
+					$scope.owsMsgOpenPoup =  true;
+					//select first message
+					$scope.currentOwsMessage = $scope.owsMessages[0].message;
+					var selectedOwsMessageIndex = 0;
+					//on reaching last message, we need to show exit button
+					$scope.isLastOwsMsg = $scope.owsMessages.length === 1 ? true : false;
+					//set page number
+					var setPageNumber = function() {
+						$scope.currentpageNumber = selectedOwsMessageIndex + 1;
+					};
+					var checkifItsLastOwsMsg = function() {
+						$scope.isLastOwsMsg = (selectedOwsMessageIndex + 1 === $scope.owsMessages.length) ? true : false;
+					};
+
+					setPageNumber();
+					showEmailButton();
+
+					//load next ows message
+					$scope.loadNextOwsMsg = function() {
+						selectedOwsMessageIndex++;
+						setPageNumber();
+						$scope.currentOwsMessage = $scope.owsMessages[selectedOwsMessageIndex].message;
+						checkifItsLastOwsMsg();
+					};
+					//print action
+					$scope.printOwsMsg = function() {
+						printActions();
+					};
+					//email the message to the guest
+					$scope.emailOwsMsg = function() {
+						var options = {
+						params: {
+							"message_id" : $scope.owsMessages[selectedOwsMessageIndex].id,
+							"reservation_id": $scope.selectedReservation.id 
+						}
+					};
+					$scope.callAPI(zsCheckinSrv.sendOWSMsgAsMail, options);
+					};
+
+					$scope.closePopup = function() {
+						$scope.owsMsgOpenPoup = false;
+
+					};
+				} else {
+					return;
+				};
+
+			};
+
+			var fetchOwsMessages = function() {
+				$scope.owsMsgOpenPoup =  false;
+				var options = {
+					params: {
+						"reservation_id": $scope.selectedReservation.id 
+					},
+					successCallBack: onOwsMsgFetchSuccess
+				};
+				$scope.callAPI(zsCheckinSrv.fetchOwsMessage, options);
+
+			}();
+
+		}();
+
+	}
+]);
--- zest_station/services_v2/checkin/zsCheckinSrv.js
+++ zest_station/services_v2/checkin/zsCheckinSrv.js
@@ -75,5 +75,31 @@ sntZestStation.service('zsCheckinSrv', ['$http', '$q', 'zsBaseWebSrv', 'zsBaseWe
             return deferred.promise;
         };
 
+
+         this.fetchOwsMessage = function(params){
+
+            var deferred = $q.defer();
+            var url = '/api/reservation_guest_messages/' + params.reservation_id+'.json';
+            //var url = '/sample_json/zest_station/ows_msgs.json';
+            zsBaseWebSrv.getJSON(url, params).then(function(data) {
+                deferred.resolve(data);
+            }, function(data) {
+                deferred.reject(data);
+            });
+            return deferred.promise;
+        };
+
+       this.sendOWSMsgAsMail = function(params) {
+            var deferred = $q.defer();
+            url = 'api/reservation_guest_messages/email_message.json';
+        
+            zsBaseWebSrv.postJSON(url, params).then(function(data) {
+                deferred.resolve(data);
+            }, function(data) {
+                deferred.reject(data);
+            });
+            return deferred.promise;
+        };
+
     }
 ]);
