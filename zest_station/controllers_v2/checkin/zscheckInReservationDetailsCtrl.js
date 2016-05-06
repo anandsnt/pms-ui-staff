16aec88e4d63bf1058e0c08d04faae3dd78d844a
resheil <resheil@stayntouch.com>
2016-05-06 16:46:11 -0400

    CICO-28178 formating code

--- zest_station/controllers_v2/checkin/zscheckInReservationDetailsCtrl.js
+++ zest_station/controllers_v2/checkin/zscheckInReservationDetailsCtrl.js
@@ -11,13 +11,13 @@ sntZestStation.controller('zsCheckInReservationDetailsCtrl', [
 		//This controller is used for viewing reservation details 
 		//add / removing additional guests and transitioning to 
 		//early checkin upsell or terms and conditions
-		
+
 
 		/**********************************************************************************************
-        **      Expected state params -----> none    
-        **      Exit function -> goToSignaturePage                              
-        **                                                                       
-        ***********************************************************************************************/
+		 **      Expected state params -----> none    
+		 **      Exit function -> goToSignaturePage                              
+		 **                                                                       
+		 ***********************************************************************************************/
 
 		/** MODES in the screen
 		 *   1.RESERVATION_DETAILS --> view details 
@@ -63,8 +63,8 @@ sntZestStation.controller('zsCheckInReservationDetailsCtrl', [
 				return true;
 			}
 		};
-                
-                
+
+
 		var init = function() {
 			//hide back button
 			$scope.$emit(zsEventConstants.HIDE_BACK_BUTTON);
@@ -89,105 +89,105 @@ sntZestStation.controller('zsCheckInReservationDetailsCtrl', [
 		};
 
 		//will need to check for ECI & Terms bypass, happy path for now
-                
-                
-                var goToSignaturePage = function() {
-                        var stateParams = {
-                                'email': $scope.selectedReservation.guest_details[0].email,
-                                'reservation_id': $scope.selectedReservation.reservation_details.reservation_id,
-                                'room_no': $scope.selectedReservation.reservation_details.room_no,
-                                'first_name': $scope.selectedReservation.guest_details[0].first_name
-                        }
-                        $state.go('zest_station.checkInSignature', stateParams);
-                };
-                var initTermsPage = function(){
-                    console.log($scope.zestStationData);
-                    var bypassTerms = !$scope.zestStationData.kiosk_display_terms_and_condition;
-                    if (bypassTerms){//add early check-in check here
-                        /*
-                         * to be done:
-                         *  -check for skipe CC 
-                         *  -force deposit
-                         */
-                        goToSignaturePage();
-                        
-                    } else {
-			$state.go('zest_station.checkInTerms', {
-				'guest_id': $scope.selectedReservation.guest_details[0].id,
+
+
+		var goToSignaturePage = function() {
+			var stateParams = {
+				'email': $scope.selectedReservation.guest_details[0].email,
 				'reservation_id': $scope.selectedReservation.reservation_details.reservation_id,
-				'deposit_amount': $scope.selectedReservation.reservation_details.deposit_amount,
 				'room_no': $scope.selectedReservation.reservation_details.room_no,
-				'room_status': $scope.selectedReservation.reservation_details.room_status,
-				'payment_type_id': $scope.selectedReservation.reservation_details.payment_type,
-				'guest_email': $scope.selectedReservation.guest_details[0].email,
-				'guest_email_blacklisted': $scope.selectedReservation.guest_details[0].is_email_blacklisted,
 				'first_name': $scope.selectedReservation.guest_details[0].first_name
-			});
-                    }
-                };
-                
-                var initRoomError = function(){
-                    $state.go('zest_station.checkinRoomError');  
-                };
+			}
+			$state.go('zest_station.checkInSignature', stateParams);
+		};
+		var initTermsPage = function() {
+			console.log($scope.zestStationData);
+			var bypassTerms = !$scope.zestStationData.kiosk_display_terms_and_condition;
+			if (bypassTerms) { //add early check-in check here
+				/*
+				 * to be done:
+				 *  -check for skipe CC 
+				 *  -force deposit
+				 */
+				goToSignaturePage();
+
+			} else {
+				$state.go('zest_station.checkInTerms', {
+					'guest_id': $scope.selectedReservation.guest_details[0].id,
+					'reservation_id': $scope.selectedReservation.reservation_details.reservation_id,
+					'deposit_amount': $scope.selectedReservation.reservation_details.deposit_amount,
+					'room_no': $scope.selectedReservation.reservation_details.room_no,
+					'room_status': $scope.selectedReservation.reservation_details.room_status,
+					'payment_type_id': $scope.selectedReservation.reservation_details.payment_type,
+					'guest_email': $scope.selectedReservation.guest_details[0].email,
+					'guest_email_blacklisted': $scope.selectedReservation.guest_details[0].is_email_blacklisted,
+					'first_name': $scope.selectedReservation.guest_details[0].first_name
+				});
+			}
+		};
+
+		var initRoomError = function() {
+			$state.go('zest_station.checkinRoomError');
+		};
 
 
-                var assignRoomToReseravtion = function(){
-                    var reservation_id = $scope.selectedReservation.id;
-                    console.info('::assigning room to reservation::',reservation_id);
-                    
-                           $scope.invokeApi(zsCheckinSrv.assignGuestRoom, {
-                            'reservation_id':reservation_id
-                        }, roomAssignCallback, roomAssignCallback); 
-                };
-                var roomAssignCallback = function(response){
-                    $scope.$emit('hideLoader');
-                    if (response.status && response.status === 'success'){
-                        $scope.selectedReservation.room = response.data.room_number;
-                        console.info('::room has been assigned: ',$scope.selectedReservation.room);
-                        initTermsPage();
+		var assignRoomToReseravtion = function() {
+			var reservation_id = $scope.selectedReservation.id;
+			console.info('::assigning room to reservation::', reservation_id);
 
-                    } else {
-                        initRoomError();
-                    }
-                };
+			$scope.invokeApi(zsCheckinSrv.assignGuestRoom, {
+				'reservation_id': reservation_id
+			}, roomAssignCallback, roomAssignCallback);
+		};
+		var roomAssignCallback = function(response) {
+			$scope.$emit('hideLoader');
+			if (response.status && response.status === 'success') {
+				$scope.selectedReservation.room = response.data.room_number;
+				console.info('::room has been assigned: ', $scope.selectedReservation.room);
+				initTermsPage();
 
-                
-                var roomIsAssigned = function(){
-                    console.log('::reservation current room :: [ ',$scope.selectedReservation.room,' ]');
-                  if ($scope.selectedReservation.room && (parseInt($scope.selectedReservation.room) === 0 || parseInt($scope.selectedReservation.room) > 0)){
-                      return true;
-                  }
-                  return false;
-                };
+			} else {
+				initRoomError();
+			}
+		};
 
-                var roomIsReady = function(){
-                    if ($scope.selectedReservation.reservation_details){
-                        if ($scope.selectedReservation.reservation_details.room_status === "READY"){
-                            return true;
-                        } else return false;
-                    } else return false;
-                };
 
-                
+		var roomIsAssigned = function() {
+			console.log('::reservation current room :: [ ', $scope.selectedReservation.room, ' ]');
+			if ($scope.selectedReservation.room && (parseInt($scope.selectedReservation.room) === 0 || parseInt($scope.selectedReservation.room) > 0)) {
+				return true;
+			}
+			return false;
+		};
+
+		var roomIsReady = function() {
+			if ($scope.selectedReservation.reservation_details) {
+				if ($scope.selectedReservation.reservation_details.room_status === "READY") {
+					return true;
+				} else return false;
+			} else return false;
+		};
+
+
 		$scope.goToTerms = function() {
 			console.log('$scope.selectedReservation: ', $scope.selectedReservation)
 			console.log($scope.selectedReservation.reservation_details.reservation_id);
-                        
-                        var roomAssigned = roomIsAssigned(),
-                                roomReady = roomIsReady();
-                        console.info('roomAssigned: ',roomAssigned,', roomReady: ',roomReady);
-                        
-                    if (!roomIsAssigned()){
-                        assignRoomToReseravtion();//assigns room, if success- goes to terms
 
-                    } else if (roomIsAssigned() && roomIsReady()){
-                          initTermsPage();
+			var roomAssigned = roomIsAssigned(),
+				roomReady = roomIsReady();
+			console.info('roomAssigned: ', roomAssigned, ', roomReady: ', roomReady);
 
-                    } else if (roomIsAssigned() && !roomIsReady()){
-                        initRoomError();
+			if (!roomIsAssigned()) {
+				assignRoomToReseravtion(); //assigns room, if success- goes to terms
 
-                    }
-                        
+			} else if (roomIsAssigned() && roomIsReady()) {
+				initTermsPage();
+
+			} else if (roomIsAssigned() && !roomIsReady()) {
+				initRoomError();
+
+			}
+
 		};
 	}
 ]);
