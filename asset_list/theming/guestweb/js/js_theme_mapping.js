47a6bd941037c687be309dc2ff77c3574028ec23
NeenuBal <neenu@stayntouch.com>
2016-05-09 17:31:52 +0530

    CICO-28234 Added theming

--- asset_list/theming/guestweb/js/js_theme_mapping.js
+++ asset_list/theming/guestweb/js/js_theme_mapping.js
@@ -47,7 +47,7 @@ module.exports = {
 			'guestweb_beacon_hill':[ themeJsRoot + 'app_router_common.js'],
 			'guestweb_boston_park':[ themeJsRoot + 'app_router_common.js'],
 			'guestweb_hotel_ivrine':[ themeJsRoot + 'app_router_common.js'],
-			'guestweb_newyork':[ themeJsRoot + 'app_router_common.js']
+			'guestweb_newyork':[ themeJsRoot + 'app_router_mgm_chain.js']
 		}
 	}
 }
