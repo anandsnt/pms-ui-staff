ea14fbef044553b5ffcf55916ea43b52bd7332ae
NeenuBal <neenu@stayntouch.com>
2016-05-18 18:28:48 +0530

    CICO-29118 and CICO-29119 done web styling

--- asset_list/theming/guestweb/template/template_theme_mapping.js
+++ asset_list/theming/guestweb/template/template_theme_mapping.js
@@ -299,12 +299,12 @@ module.exports = {
 										checkinPartials+"*.html",
 										precheckinPartials+'*.html'],
 
-			'guestweb_ponchartrain'		 : [ 'guestweb/**/common_templates/partials/checkin/**.html',
+			'guestweb_ponchartrain'	: [ 'guestweb/**/common_templates/partials/checkin/**.html',
 										'guestweb/**/common_templates/partials/checkout/**.html',
 										'guestweb/**/common_templates/partials/gwNoOption.html',
 										checkoutNowPartials+'*.html',
 										checkinPartials+"*.html",
 										precheckinPartials+'*.html']
-		}
+		
 		}
 	}
