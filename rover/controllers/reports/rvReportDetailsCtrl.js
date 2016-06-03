beb696b28e24946f866a78d6596c901cf4aad4c3
shijustayntouch <shiju@stayntouch.com>
2016-06-03 14:29:38 +0530

    CICO-29154 - Add upsell columns in JS, Add the label in en.yml

--- rover/controllers/reports/rvReportDetailsCtrl.js
+++ rover/controllers/reports/rvReportDetailsCtrl.js
@@ -406,7 +406,11 @@ sntRover.controller('RVReportDetailsCtrl', [
 				case reportNames['UPSELL']:
 					$scope.$parent.summaryCounts = {
 						'rooms_upsold'   : totals[0]['value'],
-						'upsell_revenue' : totals[1]['value']
+						'upsell_revenue' : totals[1]['value'],
+						'rover_revenue' : totals[2]['value'],
+						'zest_revenue' : totals[3]['value'],
+						'zest_station_revenue' : totals[4]['value'],
+						'zest_web_revenue' : totals[5]['value']
 					};
 					break;
 
