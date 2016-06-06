c232ed8054a87ba33a47d6739aee5c02b6330fd1
resheil <resheil@stayntouch.com>
2016-06-06 15:50:53 -0400

    CICO-30120 handling special HTTP status for handling time out

--- rover/services/payment/rvPaymentSrv.js
+++ rover/services/payment/rvPaymentSrv.js
@@ -1,4 +1,4 @@
-angular.module('sntRover').service('RVPaymentSrv',['$http', '$q', 'RVBaseWebSrv','rvBaseWebSrvV2', function($http, $q, RVBaseWebSrv,RVBaseWebSrvV2){
+angular.module('sntRover').service('RVPaymentSrv',['$http', '$q', 'RVBaseWebSrv','rvBaseWebSrvV2','$rootScope', function($http, $q, RVBaseWebSrv,RVBaseWebSrvV2,$rootScope){
 
 
 	var that =this;
@@ -97,14 +97,63 @@ angular.module('sntRover').service('RVPaymentSrv',['$http', '$q', 'RVBaseWebSrv'
 			});
 		return deferred.promise;
 	};
-	this.submitPaymentOnBill = function(dataToSrv){
+
+	this.submitPaymentOnBill = function(dataToSrv) {
+
 		var deferred = $q.defer();
-		var url = 'api/reservations/'+dataToSrv.reservation_id+'/submit_payment';
-		RVBaseWebSrvV2.postJSON(url, dataToSrv.postData).then(function(data) {
-			    deferred.resolve(data);
-			},function(data){
-			    deferred.reject(data);
-			});
+		var url = 'api/reservations/' + dataToSrv.reservation_id + '/submit_payment';
+
+		var pollToTerminal = function(async_callback_url) {
+			//we will continously communicate with the terminal till 
+			//the timeout set for the hotel
+			if(timeStampInSeconds >= $rootScope.emvTimeout){
+				var errors = ["Request timed out. Unable to process the transaction"];
+				deferred.reject(errors);
+			}
+			else{
+				RVBaseWebSrvV2.getJSONWithSpecialStatusHandling(async_callback_url).then(function(data) {
+					//if the request is still not proccesed
+					if ((!!data.status && data.status === 'processing_not_completed') || data === "null") {
+						//is this same URL ?
+						pollToTerminal(async_callback_url);
+					} else {
+						deferred.resolve(data);
+					}
+				}, function(data) {
+					if(typeof data === 'undefined'){
+						pollToTerminal(async_callback_url);
+					}
+					else{
+						deferred.reject(data);
+					}
+				});
+			};
+		};
+		if (dataToSrv.postData.is_emv_request) {
+			//for emv actions we need a timer
+			var timeStampInSeconds = 0;
+			var incrementTimer = function(){
+				timeStampInSeconds++;
+			};
+			setInterval(incrementTimer, 1000);
+		};
+
+		RVBaseWebSrvV2.postJSONWithSpecialStatusHandling(url, dataToSrv.postData).then(function(data) {
+			//if connect to emv terminal is neeeded
+			// need to poll oftently to avoid
+			// timeout issues
+			if (dataToSrv.postData.is_emv_request) {
+				if (!!data.status && data.status === 'processing_not_completed') {
+					pollToTerminal(data.location_header);
+				} else {
+					deferred.resolve(data);
+				}
+			} else {
+				deferred.resolve(data);
+			}
+		}, function(data) {
+			deferred.reject(data);
+		});
 		return deferred.promise;
 	};
 	/*
--- rover/services/rvBaseWebSrvV2.js
+++ rover/services/rvBaseWebSrvV2.js
@@ -1,95 +1,144 @@
 //To fix the issue with csrf token in ajax requests
 sntRover.config(function($httpProvider) {
-  $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-  var m = document.getElementsByTagName('meta');
-  for (var i in m) {
-    if (m[i].name === 'csrf-token') {
-	  $httpProvider.defaults.headers.common['X-CSRF-Token'] = m[i].content;
-	  break;
+	$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+	var m = document.getElementsByTagName('meta');
+	for (var i in m) {
+		if (m[i].name === 'csrf-token') {
+			$httpProvider.defaults.headers.common['X-CSRF-Token'] = m[i].content;
+			break;
+		}
 	}
-  }
 });
 
 
-angular.module('sntRover').service('rvBaseWebSrvV2',['$http', '$q', '$window', function($http, $q, $window){
+angular.module('sntRover').service('rvBaseWebSrvV2', ['$http', '$q', '$window', function($http, $q, $window) {
 
-    /**
-    *   A http requester method for calling webservice
-    *   @param {function} function of the method to call like $http.get, $http.put..
-    *   @param {string} webservice url
-    *   @param {Object} data for webservice
-    *   @return {promise}
-    */
-	this.callWebService = function(httpMethod, url, params, data){
+	var webserviceErrorActions = function(url,deferred,errors, status) {
+		var urlStart = url.split('?')[0];
+		// please note the type of error expecting is array
+		// so form error as array if you modifying it
+		if (status === 406) { // 406- Network error
+			deferred.reject(errors);
+		} else if (status === 422) { // 422
+			deferred.reject(errors);
+		} else if (status === 500) { // 500- Internal Server Error
+			deferred.reject(['Internal server error occured']);
+		} else if (status === 501 || status === 502 || status === 503) { // 500- Internal Server Error
+			$window.location.href = '/500';
+		} else if (status === 401) { // 401- Unauthorized
+			// so lets redirect to login page
+			$window.location.href = '/logout';
+		}
+
+		//set of custom error emssage range http status
+		else if (status >= 470 && status <= 490) {
+			errors.httpStatus = status;
+			errors.errorMessage = errors;
+			deferred.reject(errors);
+		}
+		// CICO-26779 : Handling 404 - Not found.
+		else if (status === 404) {
+			console.warn("Found 404 Error : " + url);
+		} else {
+			deferred.reject(errors);
+		}
+	};
+	/**
+	 *   A http requester method for calling webservice
+	 *   @param {function} function of the method to call like $http.get, $http.put..
+	 *   @param {string} webservice url
+	 *   @param {Object} data for webservice
+	 *   @return {promise}
+	 */
+	this.callWebService = function(httpMethod, url, params, data) {
 		var deferred = $q.defer();
-		if(typeof params === "undefined"){
+		if (typeof params === "undefined") {
 			params = "";
 		}
 
 		//Sample params {params:{fname: "fname", lname: "lname"}}
 		var httpDict = {};
- 		httpDict.url = url;
- 		httpDict.method = httpMethod;
- 		if(httpMethod === 'GET' || httpMethod === 'DELETE'){
- 			httpDict.params = params;
- 		}
- 		else if(httpMethod === 'POST' || httpMethod === 'PUT'){
- 			httpDict.data = params;
-  		};
+		httpDict.url = url;
+		httpDict.method = httpMethod;
+		if (httpMethod === 'GET' || httpMethod === 'DELETE') {
+			httpDict.params = params;
+		} else if (httpMethod === 'POST' || httpMethod === 'PUT') {
+			httpDict.data = params;
+		};
 
 		$http(httpDict).success(function(response, status) {
-	    	deferred.resolve(response);
+			deferred.resolve(response);
 		}).error(function(errors, status) {
-			var urlStart = url.split('?')[0];
-			// please note the type of error expecting is array
-			// so form error as array if you modifying it
-			if(status === 406){ // 406- Network error
-				deferred.reject(errors);
-			} else if(status === 422){ // 422
-				deferred.reject(errors);
-			}
-			else if(status === 500){ // 500- Internal Server Error
-				deferred.reject(['Internal server error occured']);
-			}else if(status === 501 || status === 502 || status === 503){ // 500- Internal Server Error
-				$window.location.href = '/500' ;
-			}
-			else if(status === 401){ // 401- Unauthorized
-				// so lets redirect to login page
-				$window.location.href = '/logout' ;
-			}
-
-			//set of custom error emssage range http status
-			else if(status >= 470 && status <= 490) {
-				errors.httpStatus = status;
-				errors.errorMessage = errors;
-				deferred.reject (errors);
-			}
-			// CICO-26779 : Handling 404 - Not found.
-			else if(status === 404){
-				console.warn("Found 404 Error : " + url );
-			}
-			else{
-				deferred.reject(errors);
-			}
-
+			webserviceErrorActions(url,deferred, errors, status);
 		});
 		return deferred.promise;
 	};
 
-   	this.getJSON = function(url, params) {
-    	return this.callWebService("GET", url, params);
-   	};
+	this.getJSON = function(url, params) {
+		return this.callWebService("GET", url, params);
+	};
 
-   	this.putJSON = function(url, params) {
-   		return this.callWebService("PUT", url, params);
-   	};
+	this.putJSON = function(url, params) {
+		return this.callWebService("PUT", url, params);
+	};
 
-   	this.postJSON = function(url, data) {
-   		return this.callWebService("POST", url, data);
-   	};
+	this.postJSON = function(url, data) {
+		return this.callWebService("POST", url, data);
+	};
 
-   	this.deleteJSON = function(url, params) {
-   		return this.callWebService("DELETE", url, params);
-   	};
+	this.deleteJSON = function(url, params) {
+		return this.callWebService("DELETE", url, params);
+	};
+
+
+	
+	/**************************************************************************************/
+
+	/**
+	 *   A http requester method for calling webservice
+	 *   @param {function} function of the method to call like $http.get, $http.put..
+	 *   @param {string} webservice url
+	 *   @param {Object} data for webservice
+	 *   @return {promise}
+	 */
+	this.callWebServiceWithSpecialStatusHandling = function(httpMethod, url, params, data) {
+		var deferred = $q.defer();
+		if (typeof params === "undefined") {
+			params = "";
+		}
+
+		//Sample params {params:{fname: "fname", lname: "lname"}}
+		var httpDict = {};
+		httpDict.url = url;
+		httpDict.method = httpMethod;
+		if (httpMethod === 'GET' || httpMethod === 'DELETE') {
+			httpDict.params = params;
+		} else if (httpMethod === 'POST' || httpMethod === 'PUT') {
+			httpDict.data = params;
+		};
+
+		$http(httpDict).success(function(response, status, headers) {
+				if (status === 202 || status === 102) {
+					var response = {
+						'status': 'processing_not_completed',
+						'location_header': headers('Location')
+					};
+					deferred.resolve(response);
+				} else {
+					deferred.resolve(response);
+				}
+		}).error(function(errors, status) {
+			webserviceErrorActions(url,deferred, errors, status);
+		});
+		return deferred.promise;
+	};
+
+	this.postJSONWithSpecialStatusHandling = function(url, data) {
+		return this.callWebServiceWithSpecialStatusHandling("POST", url, data);
+	};
+
+	this.getJSONWithSpecialStatusHandling = function(url, data) {
+		return this.callWebServiceWithSpecialStatusHandling("GET", url, data);
+	};
 
 }]);
