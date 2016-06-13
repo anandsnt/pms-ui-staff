64ffff68906d079423aa83c5a73606ad6f5a2aaf
NeenuBal <neenu@stayntouch.com>
2016-06-13 12:33:56 +0530

    CICO-30395 Added theming for file

--- asset_list/theming/guestweb/template/template_theme_mapping.js
+++ asset_list/theming/guestweb/template/template_theme_mapping.js
@@ -301,7 +301,7 @@ module.exports = {
 			'guestweb_ponchartrain'	: [ 'guestweb/**/common_templates/partials/checkin/**.html',
 										'guestweb/**/common_templates/partials/checkout/**.html',
 										'guestweb/**/common_templates/partials/gwNoOption.html',
-										'guestweb/**/common_templates/partials/ponchartrain/gwCheckoutfinal.html',
+										'guestweb/**/common_templates/partials/ponchartrain/*.html',
 										checkoutNowPartials+'*.html',
 										checkinPartials+"*.html",
 										precheckinPartials+'*.html']
