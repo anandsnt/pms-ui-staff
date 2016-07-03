0e0d394bf55627026dcce7108e0c34bfdb18c987
resheil <resheil@stayntouch.com>
2016-07-02 20:59:36 -0400

    CICO-31069 adding changes in service layer

--- guestweb/checkin/services/checkinKeysService.js
+++ guestweb/checkin/services/checkinKeysService.js
@@ -21,7 +21,7 @@
 		var fetchNoOfKeysData =  function(data){
 				var deferred = $q.defer();
 				//var url = '/guest_web/reservations/'+data.reservation_id+'.json';
-				var url = '/sample_json/keys/no_of_keys_data.json';
+				var url = '/guest_web/zest_web_keys/'+data.reservation_id+'.json';
 				$http.get(url,{params: data}).success(function(response) {
 					deferred.resolve(response);
 				}.bind(this))
@@ -32,11 +32,11 @@
 		};
 
 		var saveNoKeys = function(data) {
-			var url = '/sample_json/keys/no_of_keys_data.json';
+			var url = '/guest_web/zest_web_keys/'+data.reservation_id+'.json';
 			var deferred = $q.defer();
 			data.application = (typeof $rootScope.application !== "undefined") ? $rootScope.application : "WEB";
 			data.url_suffix = (typeof $rootScope.urlSuffix !== "undefined") ? $rootScope.urlSuffix : "WEB";
-			$http.post(url, data).success(function(response) {
+			$http.put(url, data).success(function(response) {
 					this.responseData = response;
 					deferred.resolve(this.responseData);
 				}.bind(this))
