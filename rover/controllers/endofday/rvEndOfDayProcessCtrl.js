6f5e666259b7b7ee7fc3cc8a28b977cde8aab7f7
rahulkookal <rahul@stayntouch.com>
2016-07-07 11:39:51 +0530

    CICO-10675 : Added EOD process.

--- rover/controllers/endofday/rvEndOfDayProcessCtrl.js
+++ rover/controllers/endofday/rvEndOfDayProcessCtrl.js
@@ -3,15 +3,17 @@ sntRover.controller('RVEndOfDayProcessController', ['$scope','ngDialog','$rootSc
     BaseCtrl.call(this, $scope);
     var init =function(){
         $scope.eodLogDetails = {};
+        $scope.dateFormat = $rootScope.dateFormat;
         $scope.businessDate = $filter('date')($rootScope.businessDate, $rootScope.dateFormat);
         $scope.selectedDate = $scope.businessDate;
         $scope.nextBusinessDate = tzIndependentDate($rootScope.businessDate);
         $scope.nextBusinessDate.setDate($scope.nextBusinessDate.getDate()+1);
         $scope.nextBusinessDate = $filter('date')($scope.nextBusinessDate, $rootScope.dateFormat);
+        setUpDatepData();
         fetchEodLogOfSelectedDate();
     };
     $scope.clickedDate = function(){
-        console.log("Clicked Date Button");
+        popupCalendar();
     };
     $scope.setSelectedDateToBussinessDate = function(){
         $scope.selectedDate = $scope.businessDate;
@@ -19,13 +21,45 @@ sntRover.controller('RVEndOfDayProcessController', ['$scope','ngDialog','$rootSc
     $scope.showSetToTodayButton = function(){
         return ($scope.selectedDate === $scope.businessDate)?true:false;
     };
+    var setUpDatepData = function(){
+        $scope.dateOptions = {
+            changeYear: true,
+            changeMonth: true,
+            dateFormat: $rootScope.jqDateFormat,
+            maxDate: tzIndependentDate($scope.businessDate),
+            yearRange: "-100:+0",
+            onSelect: function(date, inst) {
+                $scope.selectedDate = date;
+                fetchEodLogOfSelectedDate();
+                ngDialog.close();
+            }
+        };
+    };
+    var popupCalendar = function(clickedOn) {
+        ngDialog.open({
+            template: '/assets/partials/endOfDay/rvEodDatepicker.html',
+            className: 'single-date-picker',
+            scope: $scope
+        });
+    };
+
+
+    $scope.openEndOfDayPopup = function() {
+        console.log("Start EOD");
+        ngDialog.open({
+            template: '/assets/partials/endOfDay/rvEndOfDayModal.html',
+            controller: 'RVEndOfDayModalController',
+            className: 'end-of-day-popup ngdialog-theme-plain'
+        });
+    };
+
     var fetchEodLogOfSelectedDate = function(){
         var data = {
             date:$scope.selectedDate
         };
         var fetchEodLogSuccess = function(data){
+            $scope.eodLogDetails = data.eod_processes;
             $rootScope.$broadcast('hideLoader');
-            console.log(data);
         };
         var fetchEodLogFailure = function(){
             $rootScope.$broadcast('hideLoader');
