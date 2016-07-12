d85029438838d40a9b27841a6cd7809993446433
resheil <resheil@stayntouch.com>
2016-07-12 09:23:38 -0400

    CICO-31069 email addition

--- guestweb/scripts/app_router_great_wolf.js
+++ guestweb/scripts/app_router_great_wolf.js
@@ -166,6 +166,25 @@ sntGuestWeb.config(['$stateProvider','$urlRouterProvider', function($stateProvid
 		title: 'Check-in'
 	});
 
+
+	$stateProvider.state('emailAddition', {
+		url: '/emailAddition/:isFrom',
+		templateUrl: '/assets/common_templates/partials/checkin/gwEmailEntry.html',
+		controller: 'emailEntryController',
+		title: 'E-mail entry'
+	}).state('mobileNumberAddition', {
+		url: '/mobileNumberAddition',
+		templateUrl: '/assets/common_templates/partials/checkin/gwPhoneNumberUpdate.html',
+		controller: 'mobileEntryController',
+		title: 'Phone number entry'
+	}).state('mobileNumberOptions', {
+		url: '/mobileNumberOptions',
+		templateUrl: '/assets/common_templates/partials/checkin/gwMobielNUmberOptions.html',
+		controller: 'mobileOptionsController',
+		title: 'Phone number entry'
+	});
+
+
 	$stateProvider.state('selectNoOfkeys',{
 		url:'/selectNoOfkeys',
 		templateUrl: '/assets/common_templates/partials/greatWolf/gwNoOfKeysEntry.html',
