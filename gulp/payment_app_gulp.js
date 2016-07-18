f84af40db02b1864c77e59096197929cf5238068
Shahul <mails2shahul@gmail.com>
2016-07-18 15:56:35 -0400

    CICO-31066 Started to implement the production task for payment app

--- gulp/payment_app_gulp.js
+++ gulp/payment_app_gulp.js
@@ -1,5 +1,6 @@
 module.exports = function (gulp, $, options) {
 
+
     require('./payment/payment_js_gulp')(gulp, $, options);
     require('./payment/payment_template_gulp')(gulp, $, options);
 
@@ -8,7 +9,7 @@ module.exports = function (gulp, $, options) {
 
     gulp.task('watch-payment-files', ['payment-watch-partials', 'payment-watch-js-files']);
 
-    gulp.task('build-payment-dev', ['build-payment-template-cache-dev', 'build-payment-js-dev', 'payment-generate-mapping-list-dev']);
+    gulp.task('build-payment-dev', ['build-payment-template-cache-dev', 'payment-generate-mapping-list-dev']);
 
     // -- for STAGING & PROD ENV
     gulp.task('payment-asset-prod-precompile', ['payment-build-js-prod', 'payment-template-cache-production']);
