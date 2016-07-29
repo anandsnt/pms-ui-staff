248db9518c111255cb0506d54924ed7affb74b40
ragesh <ragesh@stayntouch.com>
2016-07-29 17:40:53 +0530

    CICO-32129 Fixed the issue of showing all the rates in recommened tab while navigating back from enhance stay screen

--- rover/rvRouters/rvStaycardRouter.js
+++ rover/rvRouters/rvStaycardRouter.js
@@ -119,8 +119,8 @@ angular.module('stayCardModule', [])
                         is_active: true
                     });
                 },
-                rates: function(RVRoomRatesSrv, $stateParams, staycardJsAssets) {
-                    var params = {};
+                rates: function(RVRoomRatesSrv, $stateParams, staycardJsAssets, RVReservationBaseSearchSrv) {
+                    var params = {};RVReservationBaseSearchSrv
                         params.from_date = $stateParams.from_date;
                         params.to_date   = $stateParams.to_date;
                         params.override_restrictions =  $stateParams.override_restrictions;
@@ -140,6 +140,7 @@ angular.module('stayCardModule', [])
                         params.room_type_id = $stateParams.room_type_id;
                     if($stateParams.is_member == "true")
                         params.is_member = $stateParams.is_member;
+                    RVRoomRatesSrv.setRoomAndRateActiveTab(RVReservationBaseSearchSrv.getRoomRatesDefaultView());
                     return RVRoomRatesSrv.fetchRatesInitial(params)
                 },
                 ratesMeta: function(RVReservationBaseSearchSrv, staycardJsAssets) {
