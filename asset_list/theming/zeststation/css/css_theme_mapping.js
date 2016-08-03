d6fb5991551a70845d02cc84da517d4231710dc3
Mike <mike@stayntouch.com>
2016-08-03 15:43:45 -0400

    CICO-31905 adds Conscious theme to zest station, need to verify working after API integrate zest web SNT admin item in template config

--- asset_list/theming/zeststation/css/css_theme_mapping.js
+++ asset_list/theming/zeststation/css/css_theme_mapping.js
@@ -1,13 +1,14 @@
 //ZestStation CSS theme mapping file
 module.exports = {
-	getThemeMappingList : function () {
+	getThemeMappingList: function() {
 		var themeCSSRoot = 'zest_station/css/';
 		return {
-			'zoku' 			: [ themeCSSRoot + 'zoku.less'],
-			'fontainebleau' : [ themeCSSRoot + 'fontainebleau.less'],
-			'yotel' : [ themeCSSRoot + 'yotel.less'],
-			'snt' : [ themeCSSRoot + 'snt.less']
-                        
+			'concious': [themeCSSRoot + 'concious.less'],
+			'zoku': [themeCSSRoot + 'zoku.less'],
+			'fontainebleau': [themeCSSRoot + 'fontainebleau.less'],
+			'yotel': [themeCSSRoot + 'yotel.less'],
+			'snt': [themeCSSRoot + 'snt.less']
+
 		}
 	}
 }
