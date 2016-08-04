502079e05f438afeb09840c7a4531f0c57f56839
rahulkookal <rahul@stayntouch.com>
2016-08-04 14:01:32 +0530

    CICO-30150 : Add functionality for edit user.

--- admin/controllers/serviceProviders/adServiceProviderUserDetailsCtrl.js
+++ admin/controllers/serviceProviders/adServiceProviderUserDetailsCtrl.js
@@ -2,25 +2,44 @@ admin.controller('ADServiceProviderUserDetailsCtrl',['$scope','$rootScope', '$q'
 	BaseCtrl.call(this, $scope);	
 
 	var init = function(){
-		$scope.usedDetails = {};
-		$scope.usedDetails.service_provider_id = $stateParams.serviceProviderId;
+		$scope.userDetails = {};
+		$scope.userDetails.service_provider_id = $stateParams.serviceProviderId;
+
+		if(!!$stateParams.userId){
+			$scope.userDetails.user_id = $stateParams.userId;
+			fetchUserDetails();
+		};
 		$scope.serviceProviderName = $stateParams.name;
+	};
+
+	var fetchUserDetails =  function(){
+		var param = {
+			id :$scope.userDetails.user_id
+		};
+		var successCallbackFetch = function(data){			
+			$scope.userDetails.user_id = data;
+			$scope.$emit('hideLoader');			
+		};
+		var failedCallbackFetch = function(data){			
+			$scope.$emit('hideloader');
+		};
+		$scope.invokeApi(ADServiceProviderSrv.getServiceProviderUserDetails, param, successCallbackFetch, failedCallbackFetch);
 	};
 	
 	$scope.save = function(){		
 		var successCallbackFetch = function(data){
 			if(!!data.user_id){
-				$scope.usedDetails.user_id = data.user_id;
+				$scope.userDetails.user_id = data.user_id;
 				};
 			$scope.$emit('hideLoader');			
 		};
 		var failedCallbackFetch = function(data){			
 			$scope.$emit('hideloader');
 		};
-		if(!$scope.usedDetails.user_id){
-			$scope.invokeApi(ADServiceProviderSrv.addServiceProviderUser, $scope.usedDetails, successCallbackFetch, failedCallbackFetch);
+		if(!$scope.userDetails.user_id){
+			$scope.invokeApi(ADServiceProviderSrv.addServiceProviderUser, $scope.userDetails, successCallbackFetch, failedCallbackFetch);
 		}else{
-			$scope.invokeApi(ADServiceProviderSrv.editServiceProviderUser, $scope.usedDetails, successCallbackFetch, failedCallbackFetch);
+			$scope.invokeApi(ADServiceProviderSrv.updateServiceProviderUser, $scope.userDetails, successCallbackFetch, failedCallbackFetch);
 		}
 	};
 
