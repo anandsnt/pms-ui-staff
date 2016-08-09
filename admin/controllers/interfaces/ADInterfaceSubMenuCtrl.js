3a87692d29796f9c05fbe82f5f50210cbc7514ce
Dilip Sahayadhas <dilips@stayntouch.com>
2016-08-09 16:45:33 -0400

    CICO-32062 Error handling for timeout on exact online A general solution for the admin app.

--- admin/controllers/interfaces/ADInterfaceSubMenuCtrl.js
+++ admin/controllers/interfaces/ADInterfaceSubMenuCtrl.js
@@ -1,5 +1,9 @@
 admin.controller('ADInterfaceSubMenuCtrl', ['$scope', '$state',
-    function ($scope, $state) {
+    function($scope, $state) {
         $scope.subComponents = _.find($scope.selectedMenu.components, {state: $state.current.name}).sub_components;
+
+        $scope.$on("STATE_CHANGE_FAILURE", function(event, errorMessage) {
+            $scope.errorMessage = errorMessage;
+        });
     }
 ]);
