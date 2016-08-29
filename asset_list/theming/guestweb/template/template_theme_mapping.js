8b3b2a20b2b453d7185d141d1f920d3c55e45db0
NeenuBal <neenu@stayntouch.com>
2016-08-29 16:07:56 +0530

    CICO-31894 CICO-31895 guestweb pages styles are done, gulp task done

--- asset_list/theming/guestweb/template/template_theme_mapping.js
+++ asset_list/theming/guestweb/template/template_theme_mapping.js
@@ -382,11 +382,13 @@ module.exports = {
 										'guestweb/**/common_templates/partials/gwNoOption.html',
 										checkoutNowPartials+'*.html',
 										checkinPartials+"*.html",
+										precheckinPartials+'*.html'],
+			'guestweb_playa_largo' : [ 'guestweb/**/common_templates/partials/checkin/**.html',
+										'guestweb/**/common_templates/partials/checkout/**.html',
+										'guestweb/**/common_templates/partials/gwNoOption.html',
+										checkoutNowPartials+'*.html',
+										checkinPartials+"*.html",
 										precheckinPartials+'*.html']
-										
-
-			
-
 		}
 	}
 }
