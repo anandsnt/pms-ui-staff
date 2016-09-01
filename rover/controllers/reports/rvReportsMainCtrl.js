efab8410c17b0cc91fa1fdb832a99d7a398f4ba1
Vijay Dev <vijay@stayntouch.com>
2016-09-01 11:08:17 +0530

    CICO-33246: floor number is required in place of id

--- rover/controllers/reports/rvReportsMainCtrl.js
+++ rover/controllers/reports/rvReportsMainCtrl.js
@@ -1770,7 +1770,7 @@ angular.module('sntRover').controller('RVReportsMainCtrl', [
 					params[key] = [];
 					/**/
 					_.each(selected, function(source) {
-						params[key].push( source.id );
+						params[key].push( source.floor_number );
 						/**/
 						if ( changeAppliedFilter ) {
 							$scope.appliedFilter.floorList.push( source.floor_number );
