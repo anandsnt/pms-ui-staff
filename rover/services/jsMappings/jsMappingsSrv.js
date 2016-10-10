b6068e08d523c6196ac983205d09947a2c934585
Vijay Dev <vijay@stayntouch.com>
2016-10-10 17:48:11 +0530

    payment module defer after call stack is empty

--- rover/services/jsMappings/jsMappingsSrv.js
+++ rover/services/jsMappings/jsMappingsSrv.js
@@ -70,7 +70,7 @@ angular.module('sntRover').service('jsMappings', ['$q', 'rvBaseWebSrvV2', '$ocLa
 
                     return $q.all(promises).then(function () {
                         $ocLazyLoad.inject(['sntPayConfig', 'sntPay','sntPayTemplates']);
-                        deferred.resolve();
+                        _.defer( deferred.resolve() );
                     });
 
                 } else {
