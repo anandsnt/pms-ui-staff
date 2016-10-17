b81926f6231e66817457d3c8843f48edf0db1afe
rahulkookal <rahul@stayntouch.com>
2016-10-17 17:59:20 +0530

    CICO-33706 : Intermediate commit - toggle button handled.

--- rover/react/rateManager/components/leftSide/rvRateManagerGridLeftSideHeadButtonComponent.js
+++ rover/react/rateManager/components/leftSide/rvRateManagerGridLeftSideHeadButtonComponent.js
@@ -9,24 +9,19 @@ const RateManagerGridLeftSideHeadButtonComponent = ({
 	showCloseAll
 	}) => (
 	<div className='pinnedLeft-actions'> 
-         {
-         	showOpenAll ? 
-          
-            (<button disabled={!openAllEnabled} onClick = {(e) => onOpenAllClick(e)} 
-            	className={'button rm-buttonOpenClose ' + openAllClass}> 
-                Open All
-            </button>)
-            :
-            ''
-         }
-         {
-         	showOpenAll ? 
-            (<button disabled={!closeAllEnabled} onClick = {(e) => onCloseAllClick(e)} 
-            	className={'button rm-buttonOpenClose ' + closeAllClass}> 
-                Close All
-            </button>)
-            :
-            ''
-         }
+         <div className="switch-button">
+            <input id="rate-toggle" value="" type="checkbox" checked="checked"></input>
+            { true?(<label className="data-off">
+                <span className="value">Rate</span>
+                <span className="switch-icon">Contract Details</span>
+            </label>):''
+            }
+            {                
+            false?(<label className="data-on">
+                <span className="switch-icon">Rate</span>
+                <span className="value">Contract Details</span>
+            </label>):''
+            }            
+        </div>
 	</div>
 )
