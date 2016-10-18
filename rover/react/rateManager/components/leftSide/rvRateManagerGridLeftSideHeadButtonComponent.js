5f6a45d6d3ffb067cefb9bcc74579b8081b525bb
rahulkookal <rahul@stayntouch.com>
2016-10-18 17:40:33 +0530

    CICO-33706 : Intermediate commit - toggle button enabled.

--- rover/react/rateManager/components/leftSide/rvRateManagerGridLeftSideHeadButtonComponent.js
+++ rover/react/rateManager/components/leftSide/rvRateManagerGridLeftSideHeadButtonComponent.js
@@ -6,22 +6,25 @@ const RateManagerGridLeftSideHeadButtonComponent = ({
 	onOpenAllClick,
 	onCloseAllClick,
 	showOpenAll,
-	showCloseAll
+	showCloseAll,
+    shouldShowToggle,
+    toggleClicked,
+    flags,
+    toggleClass
 	}) => (
 	<div className='pinnedLeft-actions'> 
-         <div className="switch-button">
-            <input id="rate-toggle" value="" type="checkbox" checked="checked"></input>
-            { true?(<label className="data-off">
+         {shouldShowToggle?(<div className={toggleClass}>
+            <input id="rate-toggle" value="" type="checkbox" checked="checked" onClick = {(e) => toggleClicked(e)} ></input>
+            { flags.showRateDetail?(<label className="data-off">
                 <span className="value">Rate</span>
                 <span className="switch-icon">Contract Details</span>
             </label>):''
             }
-            {                
-            false?(<label className="data-on">
+            {!flags.showRateDetail?(<label className="data-on">
                 <span className="switch-icon">Rate</span>
                 <span className="value">Contract Details</span>
             </label>):''
             }            
-        </div>
+        </div>):''}
 	</div>
 )
--- rover/react/rateManager/containers/leftSide/rvRateManagerLeftSideHeadButtonContainer.js
+++ rover/react/rateManager/containers/leftSide/rvRateManagerLeftSideHeadButtonContainer.js
@@ -8,35 +8,46 @@ const mapStateToRateManagerGridLeftSideHeadButtonContainerProps = (state) => {
         closeAllEnabled = true,
         showOpenAll = false,
         showCloseAll = false;
+        let flags ={};
 
     if(closedRestriction) {
         showOpenAll = true;
         showCloseAll = true;
     }
-
+    if(!!state.flags){
+        flags = state.flags; 
+    }else{
+        flags.showRateDetail = false;
+    }
     var propsToReturn = {
         openAllClass: openAllEnabled ? 'green': '',
         showOpenAll,
         closeAllClass: closeAllEnabled ? 'red': '',
+        toggleClass: !flags.showRateDetail ? 'switch-button on':'switch-button',
         showCloseAll,
         openAllEnabled,
         closeAllEnabled,
         mode: state.mode,
         fromDate: state.dates[0],
         toDate: state.dates[state.dates.length - 1],
-        closedRestriction
+        closedRestriction,
+        flags: flags
+
     };
 
     if(state.mode === RM_RX_CONST.SINGLE_RATE_EXPANDABLE_VIEW_MODE) {
+        propsToReturn.shouldShowToggle = false;
         propsToReturn.openAllCallbackForSingleRateView = state.callBacksFromAngular.openAllCallbackForSingleRateView;
         propsToReturn.closeAllCallbackForSingleRateView = state.callBacksFromAngular.openAllCallbackForSingleRateView;
     }
     else if(state.mode === RM_RX_CONST.RATE_VIEW_MODE) {
+        propsToReturn.shouldShowToggle = true;
         propsToReturn.openAllCallbackForRateView = state.callBacksFromAngular.openAllRestrictionsForRateView;
         propsToReturn.closeAllCallbackForRateView = state.callBacksFromAngular.closeAllRestrictionsForRateView;
         propsToReturn.rate_ids = _.pluck(state.list.slice(0), 'id'); //first row will be having any id, just for all restrictions
     }
     else if(state.mode ===  RM_RX_CONST.ROOM_TYPE_VIEW_MODE) {
+        propsToReturn.shouldShowToggle = false;
         propsToReturn.openAllCallbackForRoomTypeView = state.callBacksFromAngular.openAllRestrictionsForRoomTypeView;
         propsToReturn.closeAllCallbackForRoomTypeView = state.callBacksFromAngular.closeAllRestrictionsForRoomTypeView;
     }
@@ -44,7 +55,24 @@ const mapStateToRateManagerGridLeftSideHeadButtonContainerProps = (state) => {
     return propsToReturn;
 };
 
+const mapActionToRateManagerGridLeftSideHeadButtonContainerProps =(stateProps, dispatchProps, ownProps)=>{
+    return {
+    toggleClicked : (e)=>{
+        e.preventDefault();
+        let { dispatch } = dispatchProps;       
+        dispatch({
+                    type: RM_RX_CONST.RATE_VIEW_WITH_ADDRESS,
+                    flags: {
+                        showRateDetail: !stateProps.flags.showRateDetail
+                    }
+                });
+        },
+        ...stateProps
+    }
+};
+
 const RateManagerGridLeftSideHeadButtonContainer = connect(
   mapStateToRateManagerGridLeftSideHeadButtonContainerProps,
-  null
+  null,
+  mapActionToRateManagerGridLeftSideHeadButtonContainerProps
 )(RateManagerGridLeftSideHeadButtonComponent);
