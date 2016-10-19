c1e3fd631e66a545225668955689e2cd9afd4980
resheil <resheil@stayntouch.com>
2016-10-19 12:41:56 -0400

    CICO-34143 adding initial files for fontainbleau

--- asset_list/theming/guestweb/template/template_theme_mapping.js
+++ asset_list/theming/guestweb/template/template_theme_mapping.js
@@ -448,6 +448,12 @@ module.exports = {
 										'guestweb/**/common_templates/partials/gwNoOption.html',
 										checkoutNowPartials+'*.html',
 										checkinPartials+"*.html",
+										precheckinPartials+'*.html'],
+			'guestweb_fontainebleau_v2' : [ 'guestweb/**/common_templates/partials/fontainebleau/checkin/**.html',
+									'guestweb/**/common_templates/partials/fontainebleau/checkout/**.html',
+										'guestweb/**/common_templates/partials/gwNoOption.html',
+										checkoutNowPartials+'*.html',
+										checkinPartials+"*.html",
 										precheckinPartials+'*.html']
 
 		}
