eef97bd10f445f03f425ee3a83fe08b7badf47c1
Vijay Dev <vijay@stayntouch.com>
2016-10-19 15:32:15 +0530

    CICO-34352: cherry pick and re-apply

--- payment/payApp.js
+++ payment/payApp.js
@@ -1,11 +1,12 @@
-var sntPay = angular.module('sntPay', [
+angular.module('sntPayTemplates', []);
+
+angular.module('sntPay', [
     'pascalprecht.translate',
     'oc.lazyLoad',
     'sntPayTemplates',
     'ng-iscroll',
     'ngDialog'
-]);
-
-sntPay.run(['$rootScope', 'PAYMENT_CONFIG', function($rootScope, PAYMENT_CONFIG) {
+])
+.run(['$rootScope', 'PAYMENT_CONFIG', function($rootScope, PAYMENT_CONFIG) {
 
 }]);
