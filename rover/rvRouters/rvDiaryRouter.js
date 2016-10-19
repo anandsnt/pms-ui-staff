4ebcc735eeb559704d71b8b9d0d5149b0f933e47
krishobh <krishobh@stayntouch.com>
2016-10-19 20:25:22 +0530

    CICO-34413 Updated nightlyDiary router to load assets

--- rover/rvRouters/rvDiaryRouter.js
+++ rover/rvRouters/rvDiaryRouter.js
@@ -29,21 +29,15 @@ angular
         templateUrl: '/assets/partials/nightlyDiary/rvNightlyDiary.html',
         controller: 'rvNightlyDiaryController',
         resolve: {
+            reactAssets: function(jsMappings, mappingList) {
+                return jsMappings.fetchAssets(['react.files', 'directives'], ['react']);
+            },
+            reduxAssets: function(jsMappings, reactAssets) {
+                return jsMappings.fetchAssets(['redux.files']);
+            },
             diaryAssets: function(jsMappings, mappingList) {
-                return jsMappings.fetchAssets(['react.files', 'rover.nightlyDiary', 'directives'], ['react']);
+                return jsMappings.fetchAssets(['rover.nightlyDiary']);
             }
-            // ,
-
-            // baseSearchData: function(RVReservationBaseSearchSrv, diaryAssets) {
-            //     return RVReservationBaseSearchSrv.fetchBaseSearchData();
-            // },
-            // payload: function($rootScope, rvDiarySrv, $stateParams, $vault, baseSearchData, propertyTime, diaryAssets) {
-            //     var start_date = propertyTime.hotel_time.date;
-            //     if($stateParams.checkin_date){
-            //         start_date = $stateParams.checkin_date;
-            //     }
-            //     return rvDiarySrv.load(rvDiarySrv.properDateTimeCreation(start_date), rvDiarySrv.ArrivalFromCreateReservation());
-            // }
         }
     });
 });
