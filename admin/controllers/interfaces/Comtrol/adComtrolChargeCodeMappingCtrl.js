864f31ffc9bd231f4ebd4a8bdd2d694b70ce2022
Dilip Sahayadhas <dilips@stayntouch.com>
2016-10-20 17:13:45 -0400

    CICO-29468 UI Charge Code Mapping API Integration

--- admin/controllers/interfaces/Comtrol/adComtrolChargeCodeMappingCtrl.js
+++ admin/controllers/interfaces/Comtrol/adComtrolChargeCodeMappingCtrl.js
@@ -1,18 +1,32 @@
-admin.controller('adComtrolChargeCodeMappingCtrl', ['$scope', 'mappedChargeCodes', 'adComtrolChargeCodeMappingSrv',
-    function($scope, mappedChargeCodes, adComtrolChargeCodeMappingSrv) {
+admin.controller('adComtrolChargeCodeMappingCtrl', ['$scope', 'mappedChargeCodes', 'adComtrolChargeCodeMappingSrv', 'ngTableParams',
+    function($scope, mappedChargeCodes, adComtrolChargeCodeMappingSrv, ngTableParams) {
 
         //private methods and variables
         var resetNew = function() {
                 $scope.state.new = {
-                    name: "",
-                    code: ""
+                    revenue_center_code: "",
+                    category_name: "",
+                    charge_code_id: "",
+                    charge_code: ""
                 }
             },
             revertEdit = function() {
                 if ($scope.state.editRef) {
-                    $scope.revCenters[$scope.state.selected] = angular.copy($scope.state.editRef);
+                    $scope.mappings[$scope.state.selected] = angular.copy($scope.state.editRef);
                     $scope.state.editRef = null;
                 }
+            },
+            loadMetaList = function(cb) {
+                $scope.callAPI(adComtrolChargeCodeMappingSrv.fetchMeta, {
+                    successCallBack: function(response) {
+                        $scope.state.revCenters = response.revCenters;
+                        $scope.state.chargeCodes = response.chargeCodes;
+                        cb();
+                    }
+                })
+            },
+            getChargeCode = function(id) {
+                return _.find($scope.state.chargeCodes, {id: Number(id)}).name;
             };
 
         //scope method and variables
@@ -21,9 +35,13 @@ admin.controller('adComtrolChargeCodeMappingCtrl', ['$scope', 'mappedChargeCodes
          * Method to open the add form
          */
         $scope.onClickAdd = function() {
-            $scope.state.mode = "ADD";
-            $scope.state.selected = null;
-            resetNew();
+            if ($scope.state.revCenters) {
+                $scope.state.mode = "ADD";
+                $scope.state.selected = null;
+                resetNew();
+            } else {
+                loadMetaList($scope.onClickAdd);
+            }
         };
 
         /**
@@ -38,19 +56,23 @@ admin.controller('adComtrolChargeCodeMappingCtrl', ['$scope', 'mappedChargeCodes
          * NOTE: Mandatory check is done on the templates
          */
         $scope.onSave = function() {
-            var name = $scope.state.new.name,
-                code = $scope.state.new.code;
+            var revenue_center_code = $scope.state.new.revenue_center_code,
+                category_name = $scope.state.new.category_name,
+                charge_code_id = $scope.state.new.charge_code_id;
 
             $scope.callAPI(adComtrolChargeCodeMappingSrv.create, {
                 params: {
-                    name: name,
-                    code: code
+                    revenue_center_code: revenue_center_code,
+                    category_name: category_name,
+                    charge_code_id: charge_code_id
                 },
                 successCallBack: function(response) {
-                    $scope.revCenters.push({
+                    $scope.mappings.push({
                         id: response.id,
-                        name: name,
-                        code: code
+                        revenue_center_code: revenue_center_code,
+                        category_name: category_name,
+                        charge_code_id: charge_code_id,
+                        charge_code: getChargeCode(charge_code_id)
                     });
                     $scope.state.mode = "";
                 }
@@ -61,9 +83,16 @@ admin.controller('adComtrolChargeCodeMappingCtrl', ['$scope', 'mappedChargeCodes
          * Method to show the edit form
          * @param idx
          */
-        $scope.onSelect = function(idx, revCenter) {
-            $scope.state.editRef = angular.copy(revCenter);
-            $scope.state.selected = idx;
+        $scope.onSelect = function(idx, mapping) {
+            if ($scope.state.revCenters) {
+                $scope.state.editRef = angular.copy(mapping);
+                $scope.state.selected = idx;
+            } else {
+                loadMetaList(function() {
+                    $scope.state.editRef = angular.copy(mapping);
+                    $scope.state.selected = idx;
+                });
+            }
         };
 
         /**
@@ -80,10 +109,11 @@ admin.controller('adComtrolChargeCodeMappingCtrl', ['$scope', 'mappedChargeCodes
          * NOTE: Mandatory check is done on the templates
          * @param revCenter
          */
-        $scope.onUpdate = function(revCenter) {
+        $scope.onUpdate = function(mapping) {
             $scope.callAPI(adComtrolChargeCodeMappingSrv.update, {
-                params: revCenter,
+                params: mapping,
                 successCallBack: function() {
+                    mapping.charge_code = getChargeCode(mapping.charge_code_id);
                     $scope.state.mode = "";
                     $scope.state.selected = null;
                 }
@@ -95,11 +125,11 @@ admin.controller('adComtrolChargeCodeMappingCtrl', ['$scope', 'mappedChargeCodes
          * Deleted ones are  hidden in UI with help of isDeleted flag
          * @param revCenter
          */
-        $scope.onClickDelete = function(revCenter) {
+        $scope.onClickDelete = function(mapping) {
             $scope.callAPI(adComtrolChargeCodeMappingSrv.delete, {
-                params: revCenter.id,
+                params: mapping.id,
                 successCallBack: function() {
-                    revCenter.isDeleted = true;
+                    mapping.isDeleted = true;
                     $scope.state.deletedCount++;
                 }
             });
@@ -110,17 +140,21 @@ admin.controller('adComtrolChargeCodeMappingCtrl', ['$scope', 'mappedChargeCodes
          */
         (function() {
             $scope.state = {
+                revCenters: null,
+                chargeCodes: null,
                 deletedCount: 0,
                 selected: null,
                 mode: "",
                 editRef: null,
                 new: {
-                    name: "",
-                    code: ""
+                    revenue_center_code: "",
+                    category_name: "",
+                    charge_code_id: "",
+                    charge_code: ""
                 }
             };
 
-            $scope.revCenters = mappedChargeCodes;
+            $scope.mappings = mappedChargeCodes;
         })();
     }
 ]);
--- admin/services/interfaces/Comtrol/adComtrolChargeCodeMappingSrv.js
+++ admin/services/interfaces/Comtrol/adComtrolChargeCodeMappingSrv.js
@@ -1,44 +1,83 @@
-admin.service('adComtrolChargeCodeMappingSrv', ['$http', '$q', 'ADBaseWebSrvV2', function($http, $q, ADBaseWebSrvV2) {
+admin.service('adComtrolChargeCodeMappingSrv', ['$http', '$q', 'ADBaseWebSrvV2', 'adComtrolRevenueCenterSrv',
+    function($http, $q, ADBaseWebSrvV2, adComtrolRevenueCenterSrv) {
 
-    var service = this,
-        baseUrl = "/api/hotel_settings/comtrol_mappings/revenue_centers";
+        var service = this,
+            baseUrl = "/api/hotel_settings/comtrol_mappings/charge_code_mappings";
 
-    /**
-     *
-     * @returns {deferred.promise|{then, catch, finally}}
-     */
-    service.fetch = function() {
-        return ADBaseWebSrvV2.getJSON(baseUrl);
-    };
+        var fetchNonPaymentChargeCodes = function() {
+            var deferred = $q.defer();
+            var url = '/api/charge_codes?exclude_payments=true&per_page=1000';
 
-    /**
-     *
-     * @param newRevCenter
-     * @returns {deferred.promise|{then, catch, finally}}
-     */
-    service.create = function(newRevCenter) {
-        return ADBaseWebSrvV2.postJSON(baseUrl, newRevCenter);
-    };
+            ADBaseWebSrvV2.getJSON(url).then(function(data) {
+                deferred.resolve(data.results);
+            }, function(data) {
+                deferred.reject(data);
+            });
+            return deferred.promise;
+        };
 
-    /**
-     *
-     * @param id
-     * @returns {deferred.promise|{then, catch, finally}}
-     */
-    service.delete = function(id) {
-        return ADBaseWebSrvV2.deleteJSON(baseUrl + "/" + id);
-    };
+        /**
+         *
+         */
+        service.fetchMeta = function() {
+            var deferred = $q.defer(),
+                promises = [],
+                results = {};
 
-    /**
-     *
-     * @param revCenter
-     * @returns {deferred.promise|{then, catch, finally}}
-     */
-    service.update = function(revCenter) {
-        return ADBaseWebSrvV2.putJSON(baseUrl + "/" + revCenter.id, {
-            name: revCenter.name,
-            code: revCenter.code
-        });
-    };
+            promises.push(fetchNonPaymentChargeCodes().then(function(chargeCodes) {
+                results["chargeCodes"] = chargeCodes;
+            }));
 
-}]);
+            promises.push(adComtrolRevenueCenterSrv.fetch().then(function(revCenters) {
+                results["revCenters"] = revCenters;
+            }));
+
+            $q.all(promises).then(function() {
+                deferred.resolve(results);
+            }, function() {
+                deferred.resolve("Unable to resolve meta data");
+            });
+
+            return deferred.promise;
+        };
+
+        /**
+         *
+         * @returns {deferred.promise|{then, catch, finally}}
+         */
+        service.fetch = function() {
+            return ADBaseWebSrvV2.getJSON(baseUrl);
+        };
+
+        /**
+         *
+         * @param newMapping
+         * @returns {deferred.promise|{then, catch, finally}}
+         */
+        service.create = function(newMapping) {
+            return ADBaseWebSrvV2.postJSON(baseUrl, newMapping);
+        };
+
+        /**
+         *
+         * @param id
+         * @returns {deferred.promise|{then, catch, finally}}
+         */
+        service.delete = function(id) {
+            return ADBaseWebSrvV2.deleteJSON(baseUrl + "/" + id);
+        };
+
+        /**
+         *
+         * @param mapping
+         * @returns {deferred.promise|{then, catch, finally}}
+         */
+        service.update = function(mapping) {
+            return ADBaseWebSrvV2.putJSON(baseUrl + "/" + mapping.id, {
+                revenue_center_code: mapping.revenue_center_code,
+                category_name: mapping.category_name,
+                charge_code_id: mapping.charge_code_id
+            });
+        };
+
+    }]);
