641f17893a79a558a7569049ae4c835c6ecfe10e
Vijay Dev <vijay@stayntouch.com>
2016-10-25 08:44:32 +0200

    CICO-33550: fixed design issue

--- rover/react/reports/dailyProductionDemographics/daily-prod-demogrphcs-table-header.react.js
+++ rover/react/reports/dailyProductionDemographics/daily-prod-demogrphcs-table-header.react.js
@@ -1,49 +1,59 @@
 var DailyProductionByDemographicsTableHeader = React.createClass({
-  componentWillReceiveProps: function(nextProps) {
-    this.setState(nextProps);
-  },
-  render: function() {
-    var header 			= this.props.header,
-			dateHeaderProp 	= {
-  				'data-cell' : 'colspan',
-  				colSpan 	: header.colspan
-			},
+    componentWillReceiveProps: function(nextProps) {
+        this.setState(nextProps);
+    },
+	
+    render: function() {
+        var header = this.props.header,
+            dateHeaderProp 	= {
+                dataCell: 'colspan',
+                colSpan: header.colspan
+            },
+            indvdlColumns = [
+                React.DOM.th(
+                    {
+                        className: header.showAvailable ? '' : 'hidden',
+                        style: { whiteSpace: 'nowrap' }
+                    },
+                    'Rooms Occ'
+                ),
 
-			indvdlColumns 	= [
-				React.DOM.th(
-				{
-  					className	: (header.showAvailable ? '' : 'hidden')
-				}, 'Rooms Occ'),
+                React.DOM.th(
+                    {
+                        className: (header.showAvailable ? '' : 'hidden') + (!header.showRevenue ? ' day-end' : '')
+                    },
+                    'Rooms Available'
+                ),
 
-				React.DOM.th(
-				{
-  					className	: (header.showAvailable ? '' : 'hidden') + (!header.showRevenue ? ' day-end' : '')
-				}, 'Rooms Available'),
+                React.DOM.th(
+                    {
+                        className: header.showRevenue ? '' : 'hidden'
+                    },
+                    'Forecast Room Revenue'
+                ),
 
-				React.DOM.th(
-				{
-  					className	: header.showRevenue ? '' : 'hidden'
-				}, 'Forecast Room Revenue'),
+                React.DOM.th(
+                    {
+                        className: header.showRevenue ? '' : 'hidden'
+                    },
+                    'ADR'
+                ),
 
-				React.DOM.th(
-				{
-  					className	: header.showRevenue ? '' : 'hidden'
-				}, 'ADR'),
+                React.DOM.th(
+                    {
+                        className: header.showRevenue ? 'day-end' : 'hidden'
+                    },
+                    'Actual Room Revenue'
+                )
+            ],
+            dates = [],
+            indvdlColumnsWithinDateList = [];
 
-				React.DOM.th(
-				{
-  					className	: header.showRevenue ? 'day-end' : 'hidden'
-				}, 'Actual Room Revenue')
-			],
+        _.map(this.props.data.dates, function(row, index) {
+            dates.push(React.DOM.th(dateHeaderProp, tzIndependentDate(row).toComponents().date.toShortDateString()));
+            indvdlColumnsWithinDateList = indvdlColumnsWithinDateList.concat(indvdlColumns);
+        });
 
-			dates  = [],
-			indvdlColumnsWithinDateList = [];
-
-    _.map(this.props.data.dates, function(row, index) {
-      dates.push(React.DOM.th(dateHeaderProp, tzIndependentDate(row).toComponents().date.toShortDateString()));
-      indvdlColumnsWithinDateList = indvdlColumnsWithinDateList.concat(indvdlColumns);
-    });
-
-    return React.DOM.thead({}, React.DOM.tr({}, dates), React.DOM.tr({className: 'bottom-row'}, indvdlColumnsWithinDateList));
-  }
+        return React.DOM.thead({}, React.DOM.tr({}, dates), React.DOM.tr({className: 'bottom-row'}, indvdlColumnsWithinDateList));
+    }
 });
