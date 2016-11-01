9d6170c1a60ef6c2af1f57526a068fffef920bdb
soumyastayntouch <soumya@stayntouch.com>
2016-11-01 19:30:16 +0530

    CICO-24241: Implemented suite rooms logic

--- rover/react/nightlyDiary/components/rvNightlyDiaryRootComponent.js
+++ rover/react/nightlyDiary/components/rvNightlyDiaryRootComponent.js
@@ -26,7 +26,7 @@ const NightlyDiaryRootComponent = () => (
 
                 {/*<!-- Rooms -->*/}
 
-                    <NightlyDiaryRoomsListContainer/>
+                <NightlyDiaryRoomsListContainer/>
 
 
                 {/*<!-- Grid -->*/}
