06a9c480b4f79a049c8d5e36f1d50073e95b6bc5
Dilip Sahayadhas <dilips@stayntouch.com>
2016-11-02 11:19:01 -0400

    CICO-29346 Polling for Shiji Payment

--- payment/controllers/payShijiCtrl.js
+++ payment/controllers/payShijiCtrl.js
@@ -2,17 +2,43 @@ angular.module('sntPay').controller('payShijiCtrl',
     ['$scope', 'sntShijiGatewaySrv', 'ngDialog',
         function($scope, sntShijiGatewaySrv, ngDialog) {
 
+            $scope.shijiPaymentState = {
+                isSuccess: false,
+                isFailure: false,
+                notificationText: '',
+                modal: null
+            };
+
             /**
+             *
+             * @param {object} response Success response from the API call to get QR code
              * @return {undefined}
              */
             function showQRCode(response) {
-                ngDialog.open({
+                $scope.shijiPaymentState.modal = ngDialog.open({
                     template: '/assets/partials/payShijiQRPopup.html',
                     className: '',
                     scope: $scope,
-                    data: JSON.stringify(response.data)
+                    data: JSON.stringify(response.data),
+                    preCloseCallback: function() {
+                        console.log('TODO:', 'Need to stop listening to the async callback');
+                        return true;
+                    }
                 });
             }
+
+            /**
+             * @return {undefined}
+             */
+            $scope.cancelShijiPayment = function() {
+                ngDialog.close($scope.shijiPaymentState.modal.id);
+            };
+
+            /*
+             notification-text:
+             - 'Payment successfull' - for success
+             - 'There was an error while processing payment' - for error
+             */
 
             /**
              * @return {undefined}
@@ -21,12 +47,17 @@ angular.module('sntPay').controller('payShijiCtrl',
                 $scope.$emit('showLoader');
                 sntShijiGatewaySrv.initiatePayment($scope.reservationId, {
                     payment_type: $scope.selectedPaymentType,
+                    bill_number: $scope.billNumber,
                     amount: $scope.payment.amount
                 }).then(response => {
+                    sntShijiGatewaySrv.pollPaymentStatus(
+                        response.data.async_callback_id,
+                        $scope.hotelConfig.emvTimeout
+                    );
                     showQRCode(response);
                     $scope.$emit('hideLoader');
                 }, errorMessage => {
-                    $scope.$emit("SHIJI_PAYMENT_FAILED", errorMessage.data);
+                    $scope.$emit('SHIJI_PAYMENT_FAILED', errorMessage.data);
                     $scope.$emit('hideLoader');
                 });
             }
--- payment/services/sntShijiGatewaySrv.js
+++ payment/services/sntShijiGatewaySrv.js
@@ -1,9 +1,54 @@
-angular.module('sntPay').service('sntShijiGatewaySrv', ['$q', '$http',
-    function($q, $http) {
+angular.module('sntPay').service('sntShijiGatewaySrv', ['$q', '$http', '$timeout',
+    function($q, $http, $timeout) {
         var service = this;
 
         service.initiatePayment = function(reservationId, payLoad) {
             return $http.post('api/reservations/' + reservationId + '/submit_payment/', payLoad);
+        };
+
+
+        service.pollPaymentStatus = function(id, timeout) {
+            var deferred = $q.defer(),
+                ms = 1000,
+                defaultTimeout = 60,
+                baseUrl = '/api/async_callbacks/',
+                timeStampInSeconds = 0,
+                pollingInterval = 5000,
+                refreshIntervalId = setInterval(()=> {
+                    timeStampInSeconds++;
+                }, ms),
+                poller = function() {
+                    if (timeStampInSeconds >= timeout) {
+                        clearInterval(refreshIntervalId);
+                        deferred.reject(['Request timed out. Unable to process the transaction']);
+                    } else {
+                        $http.get(baseUrl + id)
+                            .success((data, status) => {
+                                // if the request is still not processed
+                                if (status === 202 || status === 102 || status === 250) {
+                                    $timeout(function() {
+                                        console.info('polling: [shiji] waiting for response');
+                                        poller();
+                                    }, pollingInterval);
+                                } else {
+                                    clearInterval(refreshIntervalId);
+                                    deferred.resolve(data);
+                                }
+                            })
+                            .error(data => {
+                                if (!data) {
+                                    poller();
+                                } else {
+                                    clearInterval(refreshIntervalId);
+                                    deferred.reject(data);
+                                }
+                            });
+                    }
+                };
+
+            timeout = timeout || defaultTimeout;
+            poller();
+            return deferred.promise;
         };
 
     }
