d9ded48def92e211360c31105d2f0cb2eaef79ac
soumyastayntouch <soumya@stayntouch.com>
2016-11-02 16:40:38 +0530

    CICO-24241: Added filters HTML

--- rover/react/nightlyDiary/components/rvNightlyDiaryRootComponent.js
+++ rover/react/nightlyDiary/components/rvNightlyDiaryRootComponent.js
@@ -1,4 +1,5 @@
 const NightlyDiaryRootComponent = () => (
+    <section id="diary-nightly" className="content diary-days-7" role="main">
     <div className="diary-nightly-main">
         {/*This will be moved to another component*/}
         <div className="grid-timeline">
@@ -37,4 +38,56 @@ const NightlyDiaryRootComponent = () => (
         </div>
 
     </div>
+     <div className="diary-nightly-sidebar">
+
+        <div className="sidebar-options">
+            <button type="button" className="button white active">
+
+                <span className="count">5</span>
+                Unassigned
+            </button>
+            <button type="button" className="button white">
+
+                <span className="count">6</span>
+                Filters
+            </button>
+        </div>
+        <div className="sidebar-content diary-nightly-unassigned visible">
+
+            <div className="sidebar-header">
+                <h2>Unassigned</h2>
+                <p>Drag & Drop to assign a room</p>
+            </div>
+            <div className="unassigned-labels">
+                <div className="data">Name / Room Type</div>
+                <div className="arrival">Arrival</div>
+                <div className="nights">Nights</div>
+            </div>
+            <div className="unassigned-list scrollable">
+                <div className="wrapper">
+                    <div className="guest check-in">
+                        <div className="data">
+                            <strong className="name">SOUMYA CYRIAC</strong>
+                             {/*<!-- If VIP, show: <span className="vip">VIP</span> --> */}
+                            <span className="room">Deluxe Twin</span>
+                        </div>
+                        <div className="arrival">2017-05-12</div>
+                        <div className="nights">3</div>
+                    </div>
+                    <div className="guest check-in">
+                        <div className="data">
+                            <strong className="name">SOUMYA CYRIAC</strong>
+                             {/*<!-- If VIP, show: <span className="vip">VIP</span> --> */}
+                            <span className="room">Deluxe Twin</span>
+                        </div>
+                        <div className="arrival">2017-05-18</div>
+                        <div className="nights">2</div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        </div>
+    </section>
 );
