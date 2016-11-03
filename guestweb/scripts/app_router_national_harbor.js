03778e27671c9bf426400a8de647520694e65bab
NeenuBal <neenu@stayntouch.com>
2016-11-03 16:38:46 +0530

    CICO-35093 eslint issue

--- guestweb/scripts/app_router_national_harbor.js
+++ guestweb/scripts/app_router_national_harbor.js
@@ -1,11 +1,10 @@
-sntGuestWeb.config(['$stateProvider','$urlRouterProvider', function($stateProvider,$urlRouterProvider) {
-	
-	$stateProvider.state('checkOutStatus', {
-        url: '/checkOutStatus',
-       	controller: 'checkOutStatusController',
-       	templateUrl: '/assets/common_templates/partials/MGM/NationalHarbor/gwCheckoutfinal.html',
-		title: 'Status - Check-out Now'
-   	 });
+sntGuestWeb.config(['$stateProvider', function($stateProvider) {
 
-	
+    $stateProvider.state('checkOutStatus', {
+        url: '/checkOutStatus',
+        controller: 'checkOutStatusController',
+        templateUrl: '/assets/common_templates/partials/MGM/NationalHarbor/gwCheckoutfinal.html',
+        title: 'Status - Check-out Now'
+    });
+
 }]);
