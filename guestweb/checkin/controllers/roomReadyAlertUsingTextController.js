18e39b88a0c87ea94340b517ffeaf77f0dcbe5e2
resheil <resheil@stayntouch.com>
2016-11-10 16:06:53 -0500

    CICO-35075 fixing code climate issues

--- guestweb/checkin/controllers/roomReadyAlertUsingTextController.js
+++ guestweb/checkin/controllers/roomReadyAlertUsingTextController.js
@@ -2,22 +2,13 @@
   email entry Ctrl where the email is added
 */
 
-(function() {
-  var roomReadyAlertUsingTextController = function($scope, $state, $rootScope) {
-
-    $scope.skip = function() {
-      $rootScope.userMobileSkipped = true;
-      $state.go('preCheckinStatus');
-    };
-    $scope.changeNumber = function() {
-      $state.go('mobileNumberAddition');
-    };
+sntGuestWeb.controller('roomReadyAlertUsingTextController', ['$scope', '$state', '$rootScope',
+function($scope, $state, $rootScope) {
+  $scope.skip = function() {
+    $rootScope.userMobileSkipped = true;
+    $state.go('preCheckinStatus');
   };
-
-  var dependencies = [
-    '$scope', '$state', '$rootScope',
-    roomReadyAlertUsingTextController
-  ];
-
-  sntGuestWeb.controller('roomReadyAlertUsingTextController', dependencies);
-})();
+  $scope.changeNumber = function() {
+    $state.go('mobileNumberAddition');
+  };
+}]);
