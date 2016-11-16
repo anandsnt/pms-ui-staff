6a161577eea98ef7c79047319183ea21606a8ff4
krishobh <krishobh@stayntouch.com>
2016-11-16 17:28:45 +0530

    Fixed code climate issues

--- rover/controllers/nightlyDiary/rvNightlyDiaryFiltersCtrl.js
+++ rover/controllers/nightlyDiary/rvNightlyDiaryFiltersCtrl.js
@@ -13,7 +13,7 @@ angular.module('sntRover')
             $stateParams,
             $filter,
             ngDialog
-        ){
+        ) {
 
         BaseCtrl.call(this, $scope);
         /*
@@ -23,12 +23,13 @@ angular.module('sntRover')
          * @param {Boolean} - isRightShift : whether shift to future date or previous date.
          * @return {String} - calculated date.
          */
-        var getDateShift = function( startDate, shiftCount,  isRightShift ){
+        var getDateShift = function( startDate, shiftCount,  isRightShift ) {
             var date = tzIndependentDate(startDate);
-            if(isRightShift){
+
+            if (isRightShift) {
                 date.setDate(date.getDate() + shiftCount );
             }
-            else{
+            else {
                 date.setDate(date.getDate() - shiftCount );
             }
             date = $filter('date')(date, 'yyyy-MM-dd');
@@ -36,26 +37,29 @@ angular.module('sntRover')
         };
 
         // Utility method to check for a given date range spans over two months.
-        var checkDateRangeHaveMultipleMonths = function(){
+        var checkDateRangeHaveMultipleMonths = function() {
             var startDate = tzIndependentDate($scope.diaryData.fromDate),
                 endDate = tzIndependentDate($scope.diaryData.toDate),
                 hasMultipleMonth = false;
+
             // if the date range having multiple months.
-            if(startDate.getMonth() !== endDate.getMonth()){
+            if (startDate.getMonth() !== endDate.getMonth()) {
                 hasMultipleMonth = true;
-                if($scope.diaryData.numberOfDays === 21){
+                if ($scope.diaryData.numberOfDays === 21) {
                     var isReachedSecondMonth = false;
+
                     $scope.diaryData.firstMonthDateList = [];
                     $scope.diaryData.secondMonthDateList = [];
-                    angular.forEach($scope.diaryData.datesGridData,function(item) {
+                    angular.forEach($scope.diaryData.datesGridData, function(item) {
                         var dateObj = tzIndependentDate(item.date);
-                        if(dateObj.getDate() === 1){
+
+                        if (dateObj.getDate() === 1) {
                             isReachedSecondMonth = true;
                         }
-                        if(isReachedSecondMonth){
+                        if (isReachedSecondMonth) {
                             $scope.diaryData.secondMonthDateList.push(item);
                         }
-                        else{
+                        else {
                             $scope.diaryData.firstMonthDateList.push(item);
                         }
                     });
@@ -65,7 +69,7 @@ angular.module('sntRover')
             return hasMultipleMonth;
         };
 
-        var init = function(){
+        var init = function() {
             $scope.diaryData.numberOfDays = 7;
             $scope.diaryData.fromDate = $filter('date')(tzIndependentDate($rootScope.businessDate), 'yyyy-MM-dd');
             $scope.diaryData.toDate   = getDateShift( $rootScope.businessDate, 7, true);
@@ -75,7 +79,7 @@ angular.module('sntRover')
         };
 
         // Show calendar popup.
-        $scope.clickedDatePicker = function(){
+        $scope.clickedDatePicker = function() {
             ngDialog.open({
                 template: '/assets/partials/nightlyDiary/rvNightlyDiaryDatePicker.html',
                 controller: 'RVNightlyDiaryDatePickerController',
@@ -84,34 +88,35 @@ angular.module('sntRover')
             });
         };
         // Catching event from date picker controller while date is changed.
-        $scope.$on('DATE_CHANGED',function () {
+        $scope.$on('DATE_CHANGED', function () {
             var isRightShift = true;
-            if($scope.diaryData.numberOfDays === 7){
+
+            if ($scope.diaryData.numberOfDays === 7) {
                 $scope.diaryData.toDate = getDateShift($scope.diaryData.fromDate, 7, isRightShift);
             }
-            else{
+            else {
                 $scope.diaryData.toDate = getDateShift($scope.diaryData.fromDate, 21, isRightShift);
             }
             $scope.$emit('REFRESH_DIARY_TIMELINE');
             $scope.$emit('REFRESH_DIARY_ROOMS_AND_RESERVATIONS');
         });
         // Catching event from main controller, when API is completed.
-        $scope.$on('FETCH_COMPLETED_DATE_LIST_DATA',function () {
+        $scope.$on('FETCH_COMPLETED_DATE_LIST_DATA', function() {
             $scope.diaryData.hasMultipleMonth = checkDateRangeHaveMultipleMonths();
         });
 
         // To toggle 7/21 button.
-        $scope.toggleSwitchMode = function(){
+        $scope.toggleSwitchMode = function() {
             var isRightShift = true;
-            //$scope.diaryData.isSevenSelected = !$scope.diaryData.isSevenSelected;
-            if($scope.diaryData.numberOfDays === 21){
+
+            if ($scope.diaryData.numberOfDays === 21) {
                 $scope.diaryData.toDate = getDateShift($scope.diaryData.fromDate, 7, isRightShift);
                 $scope.diaryData.numberOfDays = 7;
             }
-            else{
+            else {
                 $scope.diaryData.toDate = getDateShift($scope.diaryData.fromDate, 21, isRightShift);
                 $scope.diaryData.numberOfDays = 21;
-                if($scope.diaryData.datesGridData.length !== 21){
+                if ($scope.diaryData.datesGridData.length !== 21) {
                     $scope.$emit('REFRESH_DIARY_TIMELINE');
                 }
             }
@@ -123,41 +128,44 @@ angular.module('sntRover')
          * @param {Boolean} - isRightShift : whether shift to future date or previous date.
          * @return {String} - calculated date.
          */
-        var calculateFromDateAndToDate = function( isRightShift ){
+        var calculateFromDateAndToDate = function( isRightShift ) {
             var fromDate = angular.copy(tzIndependentDate($scope.diaryData.fromDate));
             var nextShift = isRightShift;
-            if(!isRightShift) {
+
+            if (!isRightShift) {
                 // If the fromDate is a shift to the left, then the toDate is a shift right from the new fromDate.
                 nextShift = true;
             }
-            if($scope.diaryData.numberOfDays === 7){
+            if ($scope.diaryData.numberOfDays === 7) {
                 $scope.diaryData.fromDate = getDateShift(fromDate, 7, isRightShift);
                 $scope.diaryData.toDate = getDateShift($scope.diaryData.fromDate, 7, nextShift);
             }
-            else{
+            else {
                 $scope.diaryData.fromDate = getDateShift(fromDate, 21, isRightShift);
                 $scope.diaryData.toDate = getDateShift($scope.diaryData.fromDate, 21, nextShift);
             }
         };
 
         // To handle click on left date shift.
-        $scope.clickedDateLeftShift = function(){
+        $scope.clickedDateLeftShift = function() {
             var isRightShift = false;
+
             calculateFromDateAndToDate(isRightShift);
             $scope.$emit('REFRESH_DIARY_TIMELINE');
             $scope.$emit('REFRESH_DIARY_ROOMS_AND_RESERVATIONS');
         };
 
         // To handle click on right date shift.
-        $scope.clickedDateRightShift = function(){
+        $scope.clickedDateRightShift = function() {
             var isRightShift = true;
+
             calculateFromDateAndToDate(isRightShift);
             $scope.$emit('REFRESH_DIARY_TIMELINE');
             $scope.$emit('REFRESH_DIARY_ROOMS_AND_RESERVATIONS');
         };
 
         // To handle click on reset button.
-        $scope.clickedResetButton = function(){
+        $scope.clickedResetButton = function() {
             init();
             $scope.$emit('REFRESH_DIARY_TIMELINE');
             $scope.$emit('REFRESH_DIARY_ROOMS_AND_RESERVATIONS');
