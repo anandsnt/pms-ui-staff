215fd79bfc6734a732cf5683b6acab85c5330320
rahulkookal <rahul@stayntouch.com>
2016-11-16 18:49:39 +0530

    CICO-35356 : Code climate issue fixes

--- rover/react/nightlyDiary/components/rvGoToNextPageButtonComponent.js
+++ rover/react/nightlyDiary/components/rvGoToNextPageButtonComponent.js
@@ -2,13 +2,14 @@ const GoToNextPageButtonComponent = ({goToNextButtonClicked, perPage }) =>
      (
         <div className="grid-pagination bottom">
             {
-                <button type="button" className="button blue" onClick={(e) => goToNextButtonClicked(e)}>{"Next "+perPage+" Rooms"}</button>
+                <button type = "button" className = "button blue" onClick = {(e) => goToNextButtonClicked(e)}>{"Next "+perPage+" Rooms"}</button>
             }
         </div>
     );
 
 const { PropTypes } = React;
+
 GoToNextPageButtonComponent.propTypes = {
-  goToNextButtonClicked : PropTypes.func,
-  perPage				: PropTypes.number
-}
+  goToNextButtonClicked: PropTypes.func,
+  perPage: PropTypes.number
+};
