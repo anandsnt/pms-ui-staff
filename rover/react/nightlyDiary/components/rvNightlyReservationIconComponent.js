fc201821a22456a098c9774bf725a6d45031c358
soumyastayntouch <soumya@stayntouch.com>
2016-11-16 16:14:21 +0530

    CICO-35356: Removed unused file

--- rover/react/nightlyDiary/components/rvNightlyReservationIconComponent.js
+++
@@ -1,9 +0,0 @@
-const ReservationIconComponent = ({reservation}) => (
-     <div className="reservation-icons">
-        <span className="icons icon-diary-lock"></span>
-        <span className="icons icon-group-large"></span>
-        <span className="icons icon-allotment-large"></span>
-
-    </div>
-);
-
