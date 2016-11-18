dc4b01ade362b1d05c629ca3643ab311d3b00794
krishobh <krishobh@stayntouch.com>
2016-11-18 17:47:00 +0530

    CICO-34407

--- rover/controllers/nightlyDiary/rvNightlyDiaryRoomNumberSearchCtrl.js
+++ rover/controllers/nightlyDiary/rvNightlyDiaryRoomNumberSearchCtrl.js
@@ -13,12 +13,30 @@ angular.module('sntRover')
 			$stateParams,
 			$filter,
 			RVNightlyDiaryRoomNumberSearchSrv
-		){
+		) {
 	
 	BaseCtrl.call(this, $scope);
-	$scope.diaryData.textInQueryBox = "";
-	$scope.diaryData.showSearchResultsArea = false;
-	$scope.diaryData.roomNumberSearchResults = [];
+	
+	var init = function() {
+		$scope.diaryData.textInQueryBox = "";
+		$scope.diaryData.showSearchResultsArea = false;
+		$scope.diaryData.roomNumberSearchResults = [];
+	}
+	init();
+	
+	// Scroller options for search-results view.
+	var scrollerOptions = {
+		tap: true,
+		preventDefault: false,
+		deceleration: 0.0001,
+		shrinkScrollbars: 'clip'
+	};
+
+	$scope.setScroller('result_showing_area', scrollerOptions);
+
+	var refreshScroller = function() {
+		$scope.refreshScroller('result_showing_area');
+	};
 	
 	// clear query box on clicking close button
 	$scope.clearResults = function() {
@@ -30,10 +48,11 @@ angular.module('sntRover')
 	var successCallbackFunction = function(data) {
 		$scope.$emit('hideLoader');
 		$scope.diaryData.roomNumberSearchResults = data.rooms;
+		refreshScroller();
 	};
 	// failure callback of fetching search results
 	var failureCallbackFunction = function(error) {
-		console.log(error);
+		$scope.errorMessage = errorMessage;
 	};
 
 	/**
