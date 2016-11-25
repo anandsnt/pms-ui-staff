56d62198a642566fe188d97bd2a87e9610d4474f
rahulkookal <rahul@stayntouch.com>
2016-11-25 11:28:11 +0530

    CICO-34407 : Intermediate commit - iscroll position update

--- rover/controllers/nightlyDiary/rvNightlyDiaryRoomNumberSearchCtrl.js
+++ rover/controllers/nightlyDiary/rvNightlyDiaryRoomNumberSearchCtrl.js
@@ -1,80 +1,64 @@
 angular.module('sntRover')
 .controller('rvNightlyDiaryRoomNumberSearchController', 
-	[	'$scope',
-		'$rootScope',
-		'$state',
-		'$stateParams',
-		'$filter',
-		'RVNightlyDiaryRoomNumberSearchSrv',
-		function(
-			$scope,
-			$rootScope,
-			$state,
-			$stateParams,
-			$filter,
-			RVNightlyDiaryRoomNumberSearchSrv
-		) {
-	
-	BaseCtrl.call(this, $scope);
-	
-	var init = function() {
-		$scope.diaryData.textInQueryBox = "";
-		$scope.diaryData.showSearchResultsArea = false;
-		$scope.diaryData.roomNumberSearchResults = [];
-	}
-	init();
-	
-	// Scroller options for search-results view.
-	var scrollerOptions = {
-		tap: true,
-		preventDefault: false,
-		deceleration: 0.0001,
-		shrinkScrollbars: 'clip'
-	};
+    [   '$scope',
+        '$filter',
+        'RVNightlyDiaryRoomNumberSearchSrv',
+        function(
+            $scope,
+            $filter,
+            RVNightlyDiaryRoomNumberSearchSrv
+        ) {
+            var init = function() {
+                BaseCtrl.call(this, $scope);
+                $scope.diaryData.textInQueryBox = '';
+                $scope.diaryData.showSearchResultsArea = false;
+                $scope.diaryData.roomNumberSearchResults = [];
+            };
+            // Scroller options for search-results view.
+            var scrollerOptions = {
+                tap: true,
+                preventDefault: false,
+                deceleration: 0.0001,
+                shrinkScrollbars: 'clip'
+            };
+            // success callback of fetching search results
+            var successCallbackFunction = function(data) {
+                $scope.$emit('hideLoader');
+                $scope.diaryData.roomNumberSearchResults = data.rooms;
+                refreshScroller();
+            };
+            var refreshScroller = function() {
+                $scope.refreshScroller('result_showing_area');
+            };
+            // failure callback of fetching search results
+            var failureCallbackFunction = function(error) {
+                $scope.errorMessage = error;
+            };
+            // function to perform filtering on search.     
+            var displayFilteredResults = function() {
+                var params = {};
 
-	$scope.setScroller('result_showing_area', scrollerOptions);
+                params.query = $scope.diaryData.textInQueryBox.trim();
+                $scope.invokeApi(RVNightlyDiaryRoomNumberSearchSrv.fetchRoomSearchResults, params, successCallbackFunction, failureCallbackFunction);
+            };
 
-	var refreshScroller = function() {
-		$scope.refreshScroller('result_showing_area');
-	};
-	
-	// clear query box on clicking close button
-	$scope.clearResults = function() {
-		$scope.diaryData.textInQueryBox = "";
-		$scope.diaryData.showSearchResultsArea = false;
-	};
+            init();
+            $scope.setScroller('result_showing_area', scrollerOptions);
+            // clear query box on clicking close button
+            $scope.clearResults = function() {
+                $scope.diaryData.textInQueryBox = '';
+                $scope.diaryData.showSearchResultsArea = false;
+            };
 
-	// success callback of fetching search results
-	var successCallbackFunction = function(data) {
-		$scope.$emit('hideLoader');
-		$scope.diaryData.roomNumberSearchResults = data.rooms;
-		refreshScroller();
-	};
-	// failure callback of fetching search results
-	var failureCallbackFunction = function(error) {
-		$scope.errorMessage = errorMessage;
-	};
-
-	/**
-	 * function to perform filtering on search.
-	 */
-	var displayFilteredResults = function() {
-		var params = {};
-		params.query = $scope.diaryData.textInQueryBox.trim();
-		$scope.invokeApi(RVNightlyDiaryRoomNumberSearchSrv.fetchRoomSearchResults, params, successCallbackFunction, failureCallbackFunction);
-	};
-
-	/**
-	 * function to perform filtering data, on change-event of query box
-	 */
-	$scope.queryEntered = function() {
-		if ($scope.diaryData.textInQueryBox === "" || $scope.diaryData.textInQueryBox.length < 2) {
-			$scope.diaryData.showSearchResultsArea = false;
-			$scope.diaryData.roomNumberSearchResults = [];
-		}
-		else {
-			$scope.diaryData.showSearchResultsArea = true;
-			displayFilteredResults();
-		}
-	};
-}]);
+            // function to perform filtering data, on change-event of query box
+            $scope.queryEntered = function() {
+                if ($scope.diaryData.textInQueryBox === '' || $scope.diaryData.textInQueryBox.length < 2) {
+                    $scope.diaryData.showSearchResultsArea = false;
+                    $scope.diaryData.roomNumberSearchResults = [];
+                }
+                else {
+                    $scope.diaryData.showSearchResultsArea = true;
+                    displayFilteredResults();
+                }
+            };
+        }]);
