0d40e5bed7f6c552ad11c7017d344a1d22a7cd9a
Mike <mike@stayntouch.com>
2016-11-30 14:52:30 -0500

    CICO-35659 ui only, need to test if working, IFC not running local to try

--- admin/controllers/interfaces/Revinate/adRevinateSetupCtrl.js
+++ admin/controllers/interfaces/Revinate/adRevinateSetupCtrl.js
@@ -1,7 +1,53 @@
-admin.controller('adRevinateSetupCtrl', ['$scope', 'config', 'adInterfacesCommonConfigSrv',
-    function($scope, config, adInterfacesCommonConfigSrv) {
+admin.controller('adRevinateSetupCtrl', ['$scope', '$rootScope, ','config', 'adInterfacesCommonConfigSrv','dateFilter', '$stateParams',
+    function($scope, $rootScope, config, adInterfacesCommonConfigSrv, dateFilter, $stateParams) {
 
-        var interfaceIdentifier = 'revinate';
+        var interfaceIdentifier = 'revinate',
+            MAX_REFRESH_SPAN_DAYS = 40,
+            commonDatePickerOptions = {
+                dateFormat: $rootScope.jqDateFormat,
+                numberOfMonths: 1,
+                changeYear: true,
+                changeMonth: true,
+                beforeShow: function() {
+                    $('<div id="ui-datepicker-overlay">').insertAfter('#ui-datepicker-div');
+                },
+                onClose: function() {
+                    $('#ui-datepicker-overlay').remove();
+                }
+            },
+            getFutureDate = function(fromDate, addDays) {
+                var maxDate = new tzIndependentDate(fromDate);
+
+                return tzIndependentDate(maxDate.setDate(maxDate.getDate() + addDays));
+            },
+            toDateSelected = function() {
+                if (!$scope.sync.start_date ||
+                    new tzIndependentDate($scope.sync.end_date) < new tzIndependentDate($scope.sync.start_date)) {
+                    $scope.sync.start_date = $scope.sync.end_date;
+                }
+                // NOTE: This function call is intentional.
+                $scope.endDatePickerOptions.maxDate = getFutureDate($scope.sync.start_date, MAX_REFRESH_SPAN_DAYS);
+            }, fromDateSelected = function() {
+                if (!$scope.sync.end_date ||
+                    new tzIndependentDate($scope.sync.start_date) > new tzIndependentDate($scope.sync.end_date)) {
+                    $scope.sync.end_date = $scope.sync.start_date;
+                }
+                $scope.endDatePickerOptions.maxDate = getFutureDate($scope.sync.start_date, MAX_REFRESH_SPAN_DAYS);
+            }, getSyncItems = function(full_sync_items) {
+                var arr = [];
+
+                _.each(full_sync_items, function(item) {
+                    arr.push({id: item});
+                });
+                return arr;
+            };
+
+        $scope.sync = {
+            start_date: null,
+            end_date: null,
+            items: getSyncItems(config.full_sync_items)
+        };
+
 
         $scope.toggleEnabled = function() {
             config.enabled = !config.enabled;
@@ -18,6 +64,41 @@ admin.controller('adRevinateSetupCtrl', ['$scope', 'config', 'adInterfacesCommon
                 }
             });
         };
+
+        // sync
+        
+        $scope.startSync = function() {
+            var items = _.pluck(_.filter($scope.sync.items, {isSelected: true}), 'id'),
+                payLoad;
+
+            if (!items.length) {
+                $scope.successMessage = '';
+                $scope.errorMessage = ['ERROR: Please select at least one Item to Synchronize!'];
+                return;
+            }
+
+            payLoad = {
+                start_date: dateFilter($scope.sync.start_date, $rootScope.dateFormatForAPI),
+                end_date: dateFilter($scope.sync.end_date, $rootScope.dateFormatForAPI),
+                items: items
+            };
+
+            $scope.callAPI(adInterfacesCommonConfigSrv.initSync, {
+                params: {
+                    payLoad: payLoad,
+                    interfaceIdentifier: interfaceIdentifier
+                },
+                onSuccess: function() {
+                    $scope.errorMessage = '';
+                    $scope.successMessage = 'SUCCESS: Synchronization Initiated!';
+                }
+            });
+        };
+
+
+
+
+
 
         (function() {
             //    init
