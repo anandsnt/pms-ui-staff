ca5b1f69d12cf19594a9d114076ca5e823777170
Jongmyung Ha <jongmyung@stayntouch.com>
2016-12-07 11:54:07 -0500

    CICO-35124: Fix Rubocop and ESLint offences

--- admin/controllers/interfaces/ideas/adIdeasImportSetupCtrl.js
+++ admin/controllers/interfaces/ideas/adIdeasImportSetupCtrl.js
@@ -1,9 +1,10 @@
-admin.controller('adIdeasImportSetupCtrl', ['$scope', '$rootScope', 'adIdeasSetupSrv', 'dateFilter',
-    function($scope, $rootScope, adIdeasSetupSrv, dateFilter) {
+admin.controller('adIdeasImportSetupCtrl', ['$scope', '$rootScope', 'adIdeasSetupSrv',
+    function($scope, $rootScope, adIdeasSetupSrv) {
         BaseCtrl.call(this, $scope);
 
         $scope.saveSetup = function() {
             var params = _.pick($scope.ideaSetup, 'import_enabled', 'hotel_code');
+
             $scope.errorMessage = '';
             $scope.callAPI(adIdeasSetupSrv.postIdeasSetup, {
                 params: params,
