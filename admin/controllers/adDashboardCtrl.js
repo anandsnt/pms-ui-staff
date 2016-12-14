4675ffe6997858cb00dc749f6862b67d6e209e6c
Dilip Sahayadhas <dilips@stayntouch.com>
2016-12-14 14:42:46 -0500

    CICO-36466 Interfaces menu items and further sub menu items listed in alphabetical order

--- admin/controllers/adDashboardCtrl.js
+++ admin/controllers/adDashboardCtrl.js
@@ -2,6 +2,15 @@ admin.controller('ADDashboardCtrl', ['$scope', '$state', '$stateParams', '$rootS
 	$scope.$emit("changedSelectedMenu", $stateParams.menu);
 	if (typeof $scope.data !== 'undefined') {
 		$scope.selectedMenu = $scope.data.menus[$stateParams.menu];
+
+        // CICO-36466 Admin Interfaces Menu to be sorted by alphabetical
+		// NOTE: Currently only the Interfaces Menu items are sorted!
+		if($scope.selectedMenu.menu_name === "Interfaces"){
+            $scope.selectedMenu.components = _.sortBy($scope.data.menus[$stateParams.menu].components, function(menu) {
+                return menu.name.toLowerCase();
+            });
+		}
+
 		// As part of CICO-27990, Notification menu is hiding for SP, ie shows only for sntAdmin
 		if ($rootScope.isServiceProvider) {
 			 $scope.selectedMenu.components = _.filter($scope.selectedMenu.components, function(item) {
