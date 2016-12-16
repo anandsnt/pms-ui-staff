2f1156180bea40ba6e558497ec9887b97375fdae
Aswanth <aswanth@stayntouch.com>
2016-12-16 19:28:53 +0530

    CICO-35422 fixed the select box issues

--- admin/controllers/interfaces/snapShot/adSnapshotSetupCtrl.js
+++ admin/controllers/interfaces/snapShot/adSnapshotSetupCtrl.js
@@ -2,12 +2,13 @@ admin.controller('adsnapshotSetupCtrl', ['$scope',
 	'$state',
 	'snapshotSetupData',
 	'adSnapShotSetupSrv',
-	function($scope, $state, snapshotSetupData, adSnapShotSetupSrv) {
+	'$timeout',
+	function($scope, $state, snapshotSetupData, adSnapShotSetupSrv, $timeout) {
 		BaseCtrl.call(this, $scope);
 
 		$scope.hours = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
     	$scope.minutes = ["00", "15", "30", "45"];
-    	$scope.primeTimes = ["AM", "PM"];
+    	$scope.primeTimes = [ "AM", "PM"];
     	$scope.retry_count_options = ["1", "2", "3", "4", "5"];
 
 		$scope.publishFullExport = function() {
@@ -34,10 +35,20 @@ admin.controller('adsnapshotSetupCtrl', ['$scope',
 			$scope.callAPI(adSnapShotSetupSrv.saveSettings, options);
 		};
 
+		$scope.fetchSettings = function() {
+			var onFetchSettingsSucces = function(data) {
+					$scope.snapshotData = data;
+				},
+				options = {					
+					successCallBack: onFetchSettingsSucces
+				};
+			$scope.callAPI(adSnapShotSetupSrv.getSettings, options);
+		};
+
 		(function init() {
 			$scope.errorMessage = '';
 			$scope.successMessage = '';
-			$scope.snapshotData = snapshotSetupData;
+			$scope.fetchSettings();
 		}());
 
 	}
