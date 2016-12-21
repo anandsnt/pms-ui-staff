af0be7f94a8a1db4b7a3290adf1f6e2842c766cf
resheil <resheilmohammed@resheils-MBP.fios-router.home>
2016-12-21 16:10:34 -0500

    CICO-31494 setting OOS me3ssage

--- zest_station/controllers_v2/checkin/zsCheckinKeyDispenseCtrl.js
+++ zest_station/controllers_v2/checkin/zsCheckinKeyDispenseCtrl.js
@@ -63,7 +63,7 @@ sntZestStation.controller('zsCheckinKeyDispenseCtrl', [
             $scope.mode = 'DISPENSE_KEY_FAILURE_MODE';
             $scope.zestStationData.consecutiveKeyFailure++;
             if($scope.zestStationData.consecutiveKeyFailure >= $scope.zestStationData.kioskOutOfOrderTreshold){
-               $scope.zestStationData.workstationOooReason = $filter('translate')('KEY_FAILURE');
+               $scope.zestStationData.workstationOooReason = $filter('translate')('KEY_CREATION_FAILED');
                $scope.zestStationData.workstationStatus = 'out-of-order';// go out of order when (printing or key encoding fails)
             }
             $scope.runDigestCycle();
