4fa83cbb4f9a66e5cb648aa8266b74bf0d913265
resheil <resheilmohammed@resheils-MBP.fios-router.home>
2016-12-22 15:31:38 -0500

    CICO-31494 refactoring the key integrations

--- zest_station/controllers_v2/pickupKey/zsPickupKeyDispenseCtrl.js
+++ zest_station/controllers_v2/pickupKey/zsPickupKeyDispenseCtrl.js
@@ -9,29 +9,29 @@ sntZestStation.controller('zsPickupKeyDispenseCtrl', [
     '$filter',
     function($scope, $stateParams, $state, zsEventConstants, $controller, zsGeneralSrv, $timeout, $filter) {
 
-		/** ********************************************************************************************
-		 **		Expected state params -----> reservation_id, room_no and first_name'			  
-		 **		Exit function -> clickedOnCloseButton- root ctrl function						
-		 **																		 
-		 ***********************************************************************************************/
+        /** ********************************************************************************************
+         **     Expected state params -----> reservation_id, room_no and first_name'              
+         **     Exit function -> clickedOnCloseButton- root ctrl function                       
+         **                                                                      
+         ***********************************************************************************************/
 
-		/**
-		 *    MODES inside the page
-		 *    
-		 * 1. DISPENSE_KEY_MODE -> select No of keys
-		 * 2. DISPENSE_KEY_FAILURE_MODE -> failure mode
-		 * 3. SOLO_KEY_CREATION_IN_PROGRESS_MODE -> one key selected case
-		 * 4. KEY_ONE_CREATION_IN_PROGRESS_MODE -> 2 key selected, 1st in progress
-		 * 5. KEY_ONE_CREATION_SUCCESS_MODE -> 2 key selected, 1st completed
-		 * 6. KEY_CREATION_SUCCESS_MODE -> all requested keys were created
-		 */
+        /**
+         *    MODES inside the page
+         *    
+         * 1. DISPENSE_KEY_MODE -> select No of keys
+         * 2. DISPENSE_KEY_FAILURE_MODE -> failure mode
+         * 3. SOLO_KEY_CREATION_IN_PROGRESS_MODE -> one key selected case
+         * 4. KEY_ONE_CREATION_IN_PROGRESS_MODE -> 2 key selected, 1st in progress
+         * 5. KEY_ONE_CREATION_SUCCESS_MODE -> 2 key selected, 1st completed
+         * 6. KEY_CREATION_SUCCESS_MODE -> all requested keys were created
+         */
 
-		/**
-		 * [initializeMe description]
-		 */
+        /**
+         * [initializeMe description]
+         */
         var initializeMe = (function() {
-			// All the common actions for dispensing keys are to be included in
-			// zsKeyDispenseCtrl
+            // All the common actions for dispensing keys are to be included in
+            // zsKeyDispenseCtrl
             $controller('zsKeyDispenseCtrl', {
                 $scope: $scope
             });
@@ -49,308 +49,28 @@ sntZestStation.controller('zsPickupKeyDispenseCtrl', [
             }
         });
 
-		// handling style in ctrl, so as not to mess up style sheet
-		// this is a small style addition
+        // handling style in ctrl, so as not to mess up style sheet
+        // this is a small style addition
         var marginTop = $scope.zestStationData.show_room_number ? '40px' : '0px';
 
         $scope.doneButtonStyle = {
             'margin-top': marginTop
         };
-        var noOfKeysCreated = 0;
+        $scope.noOfKeysCreated = 0;
 
-        var onGeneralFailureCase = function() {
-            $scope.mode = 'DISPENSE_KEY_FAILURE_MODE';
-            $scope.zestStationData.consecutiveKeyFailure++;
-            if ($scope.zestStationData.consecutiveKeyFailure >= $scope.zestStationData.kioskOutOfOrderTreshold) {
-                $scope.zestStationData.workstationOooReason = $filter('translate')('KEY_CREATION_FAILED');
-                $scope.zestStationData.workstationStatus = 'out-of-order'; // go out of order when (printing or key encoding fails)
-            }
-            var keyNo = (noOfKeysCreated === 0) ? 1 : 2;
 
-            $scope.updateLogForKeyActions(keyNo, "failed");
-            $scope.runDigestCycle();
-        };
-		/**
-		 * [dispenseKey description]
-		 *  if webscoket ready state is not ready
-		 */
-        var dispenseKey = function() {
-            if ($scope.inDemoMode()) {
-                setTimeout(function() {
-                    saveUIDToReservationSuccsess();
-                    $scope.runDigestCycle();
-                }, 3500);
-
-            } else {
-				// check if socket is open
-                if (!_.isUndefined($scope.socketOperator.returnWebSocketObject()) && $scope.socketOperator.returnWebSocketObject().readyState === 1) {
-                    $scope.socketOperator.DispenseKey($scope.dispenseKeyData);
-                } else {
-                    $scope.$emit('CONNECT_WEBSOCKET'); // connect socket
-                }
-            }
-        };
-        
-        $scope.reEncodeKey = function() {
-
-            var executeKeyOperations = function() {
-                if ($scope.zestStationData.keyWriter === 'websocket') {
-                    // provide some timeout for user to grab keys
-                    $timeout(dispenseKey, 2000);
-                } else {
-                    $timeout(function() {
-                        $scope.readyForUserToPressMakeKey = true;
-                    }, 1000);
-                }
-            };
-
-            // check if one of the selected no of keys was created or not
-            if (noOfKeysCreated === 1 && $scope.noOfKeysSelected === 2) {
-                $scope.mode = 'KEY_ONE_SUCCESS_KEY_TWO_FAILED';
-                executeKeyOperations();
-            } else {
-                $scope.mode = $scope.noOfKeysSelected === 1 ? 'SOLO_KEY_CREATION_IN_PROGRESS_MODE' : 'KEY_ONE_CREATION_IN_PROGRESS_MODE';
-                executeKeyOperations();
-            }
-        };
-		/**
-		 * [setFailureReason description]
-		 * we need to set the oos reason message in admin
-		 */
-        var setFailureReason = function(response) {
-            $scope.zestStationData.workstationOooReason = $filter('translate')('PICKUP_KEY_FAIL');
-            onGeneralFailureCase();
-        };
-
-		/**
-		 * [resetFailureReason description]
-		 * we need to set the oos reason message in admin
-		 */
-        var revertFailureReason = function(response) {
-            $scope.zestStationData.workstationOooReason = '';
-            $scope.zestStationData.workstationStatus = 'in-order';
-            $scope.zestStationData.consecutiveKeyFailure = 0;
-        };
-
-		/**
-		 * [saveUIDToReservation description]
-		 * @param  {[type]} uid [description]
-		 * @return {[type]}     [description]
-		 */
-        var saveUIDToReservationSuccsess = function() {
-            // reset timer so as to avoid unwanted timeouts
-            $scope.resetTime();
-            noOfKeysCreated++;
-
-            if ($scope.noOfKeysSelected === noOfKeysCreated) {
-				// all keys are made
-                $scope.mode = 'KEY_CREATION_SUCCESS_MODE';
-                revertFailureReason();
-            } else if ($scope.noOfKeysSelected > noOfKeysCreated) {
-				// if more key is needed
-                $scope.mode = 'KEY_ONE_CREATION_SUCCESS_MODE';
-                revertFailureReason();
-				// provide some timeout for user to grab keys
-                $timeout(dispenseKey, 6000);
-            }
-            $scope.updateLogForKeyActions(noOfKeysCreated, "success");
-        };
-        var saveUIDToReservation = function(uid) {
-            $scope.callAPI(zsGeneralSrv.saveUIDtoRes, {
-                params: {
-                    reservation_id: $scope.selectedReservation.reservationId,
-                    uid: uid
-                },
-                'successCallBack': saveUIDToReservationSuccsess,
-                'failureCallBack': onGeneralFailureCase
-            });
-        };
-
-        $scope.$on('DISPENSE_SUCCESS', function(event, data) {
-            $scope.zestStationData.workstationStatus = 'in-order';
-            saveUIDToReservation(data.msg);
-        });
-        $scope.$on('DISPENSE_FAILED', function(event, data) {
-            onGeneralFailureCase();
-        });
-        $scope.$on('SOCKET_FAILED', function() {
-            $scope.zestStationData.workstationOooReason = $filter('translate')('SOCKET_FAILED');
-            onGeneralFailureCase();
-        });
-        $scope.$on('DISPENSE_CARD_EMPTY', function() {
-            $scope.zestStationData.workstationOooReason = $filter('translate')('PICKUP_KEY_FAIL_EMPTY');
-            onGeneralFailureCase();
-        });
-        $scope.$on('SOCKET_CONNECTED', function() {
-            dispenseKey();
+        $scope.$on('ON_GENERAL_ERROR', function() {
+            $scope.onGeneralFailureCase();
         });
 
-		/**
-		 * [localEncodingSuccsess description]
-		 * @param  {[type]} response [description]
-		 * @return {[type]}          [description]
-		 */
-        var localEncodingSuccsess = function(response) {
-            // reset timer so as to avoid unwanted timeouts
-            $scope.resetTime();
-            if ($scope.inDemoMode()) {
-                setTimeout(function() {
-                    $scope.mode = $scope.noOfKeysSelected === 1 ? 'SOLO_KEY_CREATION_IN_PROGRESS_MODE' : 'KEY_ONE_CREATION_IN_PROGRESS_MODE';
-                    dispenseKey();
-
-                    $scope.runDigestCycle();
-                }, 2000);
-
-            } else {
-                if (response !== null && response.key_info && response.key_info[0]) {
-                    if (response.key_info[0].base64) {
-                        $scope.dispenseKeyData = response.key_info[0].base64;
-                        $scope.mode = $scope.noOfKeysSelected === 1 ? 'SOLO_KEY_CREATION_IN_PROGRESS_MODE' : 'KEY_ONE_CREATION_IN_PROGRESS_MODE';
-                        dispenseKey();
-
-                        $scope.runDigestCycle();
-                    } else {
-                        setFailureReason();
-                    }
-                } else {
-                    setFailureReason();
-                }
-            }
-        };
-		/**
-		 * [initMakeKey description]
-		 * @return {[type]} [description]
-		 */
-
-        $scope.$on('printLocalKeyCordovaFailed', function(evt, response) {
-            console.warn('error: ', response);
-            onGeneralFailureCase();
-        });
-
-        $scope.$on('continueFromCordovaKeyWrite', function() {
-            remoteEncodingSuccsess();
-        });
-
-        var startMakingKey = function(keyNo) {
-            var onResponseSuccess;
-            var params = {
-                'is_additional': false,
-                'is_kiosk': true,
-                'key': 1,
-                'reservation_id': $scope.selectedReservation.reservationId
-            };
-
-            if (keyNo) {
-                params.key = keyNo;
-                if (keyNo === 2) {
-                    params.is_additional = true;
-                }
-            }
-
-            if (!$scope.remoteEncoding) {
-                params.uid = null;
-                onResponseSuccess = localEncodingSuccsess;
-            } else {
-                params.key_encoder_id = $scope.zestStationData.key_encoder_id;
-                onResponseSuccess = remoteEncodingSuccsess;
-            }
-
-
-            if ($scope.inDemoMode()) {
-                setTimeout(function() {
-                    onResponseSuccess({
-                        'status': 'success'
-                    });
-                }, 1200);
-            } else {
-                if ($scope.writeLocally()) {
-                    console.log('write locally');
-					// encode / dispense key from infinea || ingenico
-					// local encoding + infinea
-                    if ($scope.inDemoMode()) {
-                        setTimeout(function() {
-                            onSuccessWriteKeyDataLocal();
-                        }, 2800); // add some delay for demo purposes
-                    } else {
-                        $scope.$emit('printLocalKeyCordova', $scope.selectedReservation.reservationId, $scope.noOfKeysSelected);
-                        return;
-                    }
-                } else {
-                    $scope.callAPI(zsGeneralSrv.encodeKey, {
-                        params: params,
-                        'loader': 'none', // to hide loader
-                        'successCallBack': onResponseSuccess,
-                        'failureCallBack': setFailureReason
-                    });
-                }
-
-            }
-        };
-
-
-        var initMakeKey = function() {
-            if ($scope.zestStationData.keyWriter === 'websocket') {
-                $scope.remoteEncoding = false;
-                console.info('starting key create with Sankyo...');
-            } else {
-                console.info('waiting on user to press make key, which will start key create here...');
-            }
-            if ($scope.noOfKeysSelected === 1) {
-                $scope.mode = 'SOLO_KEY_CREATION_IN_PROGRESS_MODE';
-            } else if (noOfKeysCreated === 0) {
-				// one key has been made out of total 2
-                $scope.mode = 'KEY_ONE_CREATION_IN_PROGRESS_MODE';
-            } else {
-				// do nothing
-            }
-            if ($scope.remoteEncoding || $scope.zestStationData.keyWriter === 'local') {
-                $scope.readyForUserToPressMakeKey = true;
-                if ($scope.zestStationData.keyWriter === 'local') {
-                    console.warn('local encoder');
-                    $scope.localWriter = true; // icmp (ingenico) or infinea device
-                }
-            } else {
-                startMakingKey();
-            }
-        };
-
-
-        $scope.onReadyToPrintKey = function(keyNo) {
-            if ($scope.readyForUserToPressMakeKey) {
-                $scope.readyForUserToPressMakeKey = false;
-                startMakingKey(keyNo);
-            }
-        };
-
-
-        function remoteEncodingSuccsess(response) {
-            // reset timer so as to avoid unwanted timeouts
-            $scope.resetTime();
-            noOfKeysCreated++;
-            if ($scope.noOfKeysSelected === noOfKeysCreated) {
-				// all keys are made
-                $scope.mode = 'KEY_CREATION_SUCCESS_MODE';
-                revertFailureReason();
-            } else if ($scope.noOfKeysSelected > noOfKeysCreated) {
-				// one key has been made out of total 2
-                $scope.mode = 'KEY_ONE_CREATION_SUCCESS_MODE';
-                revertFailureReason();
-				// provide some timeout for user to grab keys
-                $timeout(initMakeKey, 3000);
-            }
-            $scope.updateLogForKeyActions(noOfKeysCreated, "success");
-            $scope.runDigestCycle();
-        }
-
-
-		/**
-		 * [makeKeys description]
-		 * @param  {[type]} no_of_keys [description]
-		 * @return {[type]}            [description]
-		 */
+        /**
+         * [makeKeys description]
+         * @param  {[type]} no_of_keys [description]
+         * @return {[type]}            [description]
+         */
         $scope.makeKeys = function(no_of_keys) {
             $scope.noOfKeysSelected = no_of_keys;
-            initMakeKey();
+            $scope.initMakeKey();
         };
 
     }
