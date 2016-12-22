ac0876315144f1ea98514dbc39b1d5f117c742dc
Mike <mike@stayntouch.com>
2016-12-22 15:01:32 -0500

    code cleanup

--- zest_station/controllers_v2/checkin/zsCheckinFinalCtrl.js
+++ zest_station/controllers_v2/checkin/zsCheckinFinalCtrl.js
@@ -1,9 +1,9 @@
 sntZestStation.controller('zsCheckinFinalCtrl', [
-	'$scope',
-	'$stateParams',
-	'$state',
-	'zsEventConstants',
-	function($scope, $stateParams, $state, zsEventConstants) {
+    '$scope',
+    '$stateParams',
+    '$state',
+    'zsEventConstants',
+    function($scope, $stateParams, $state, zsEventConstants) {
 
 
 		/** ********************************************************************************************
@@ -19,45 +19,45 @@ sntZestStation.controller('zsCheckinFinalCtrl', [
 		/**
 		 * [initializeMe description]
 		 */
-		var initializeMe = (function() {
+        var initializeMe = (function() {
 			// hide back button
-			$scope.$emit(zsEventConstants.HIDE_BACK_BUTTON);
+            $scope.$emit(zsEventConstants.HIDE_BACK_BUTTON);
 			// hide close button
-			$scope.$emit(zsEventConstants.HIDE_CLOSE_BUTTON);
+            $scope.$emit(zsEventConstants.HIDE_CLOSE_BUTTON);
 			// show subtexts based upon actions selected
-			var printOpted = $stateParams.print_opted === 'true';
-			var emailOpted = $stateParams.email_opted === 'true';
-			var printSuccess = $stateParams.print_status === "success";
-			var emailSuccess = $stateParams.email_status === "success";
-			var keySucess = $stateParams.key_success === "true";
+            var printOpted = $stateParams.print_opted === 'true';
+            var emailOpted = $stateParams.email_opted === 'true';
+            var printSuccess = $stateParams.print_status === 'success';
+            var emailSuccess = $stateParams.email_status === 'success';
+            var keySucess = $stateParams.key_success === 'true';
 
-			if (printOpted) {
-				if (printSuccess && keySucess) {
-					$scope.subtext = 'PRINT_SUCCESS_AND_KEY_SUCCESS';
-				} else if (!printSuccess && keySucess) {
-					$scope.subtext = 'PRINT_FAILED_AND_KEY_SUCCESS';
-				} else if (printSuccess && !keySucess) {
-					$scope.subtext = 'PRINT_SUCCESS_AND_KEY_FAILED';
-				} else if (!printSuccess && !keySucess) {
-					$scope.subtext = 'PRINT_FAILED_AND_KEY_FAILED';
-				} else {
-					$scope.subtext = "";
-				}
-			} else {
-				if (emailSuccess && keySucess) {
-					$scope.subtext = 'EMAIL_SUCCESS_AND_KEY_SUCCESS';
-				} else if (!emailSuccess && keySucess) {
-					$scope.subtext = 'EMAIL_FAILED_AND_KEY_SUCCESS';
-				} else if (emailSuccess && !keySucess) {
-					$scope.subtext = 'EMAIL_SUCCESS_AND_KEY_FAILED';
-				} else if (!emailSuccess && !keySucess) {
-					$scope.subtext = 'EMAIL_FAILED_AND_KEY_FAILED';
-				} else {
-					$scope.subtext = "";
-				}
-			}
-		}());
+            if (printOpted) {
+                if (printSuccess && keySucess) {
+                    $scope.subtext = 'PRINT_SUCCESS_AND_KEY_SUCCESS';
+                } else if (!printSuccess && keySucess) {
+                    $scope.subtext = 'PRINT_FAILED_AND_KEY_SUCCESS';
+                } else if (printSuccess && !keySucess) {
+                    $scope.subtext = 'PRINT_SUCCESS_AND_KEY_FAILED';
+                } else if (!printSuccess && !keySucess) {
+                    $scope.subtext = 'PRINT_FAILED_AND_KEY_FAILED';
+                } else {
+                    $scope.subtext = '';
+                }
+            } else {
+                if (emailSuccess && keySucess) {
+                    $scope.subtext = 'EMAIL_SUCCESS_AND_KEY_SUCCESS';
+                } else if (!emailSuccess && keySucess) {
+                    $scope.subtext = 'EMAIL_FAILED_AND_KEY_SUCCESS';
+                } else if (emailSuccess && !keySucess) {
+                    $scope.subtext = 'EMAIL_SUCCESS_AND_KEY_FAILED';
+                } else if (!emailSuccess && !keySucess) {
+                    $scope.subtext = 'EMAIL_FAILED_AND_KEY_FAILED';
+                } else {
+                    $scope.subtext = '';
+                }
+            }
+        }());
 
 
-	}
+    }
 ]);
