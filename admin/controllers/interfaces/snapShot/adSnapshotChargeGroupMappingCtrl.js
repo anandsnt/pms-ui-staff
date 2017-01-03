71927b7f7bbb76bad37bc448c53a44b4ff9e5610
Aswanth <aswanth@stayntouch.com>
2017-01-03 21:09:39 +0530

    CICO-36556 added the api changes

---
+++ admin/controllers/interfaces/snapShot/adSnapshotChargeGroupMappingCtrl.js
@@ -0,0 +1,50 @@
+admin.controller('adSnapshotChargeGroupMappingCtrl', ['$scope',
+	'$state',
+	'adSnapShotSetupSrv',
+	'ADChargeGroupsSrv',
+	'$timeout',
+	function($scope, $state, adSnapShotSetupSrv, ADChargeGroupsSrv, $timeout) {
+		BaseCtrl.call(this, $scope);	
+
+		
+		$scope.saveSettings = function() {
+			var onSaveSettingsSucces = function() {
+					$scope.successMessage = 'Success, Your settings has been saved.';
+				},
+				options = {
+					params: $scope.snapshotData,
+					successCallBack: onSaveSettingsSucces
+				};
+
+			$scope.callAPI(adSnapShotSetupSrv.saveChargeGroupMapping, options);
+		};
+
+		$scope.fetchSettings = function() {
+			var onFetchSettingsSucces = function(data) {
+					$scope.snapshotData = data;
+				},
+				options = {					
+					successCallBack: onFetchSettingsSucces
+				};
+			$scope.callAPI(adSnapShotSetupSrv.fetchChargeGroupMapping, options);
+		};
+
+		/*
+	    * To fetch charge groups list
+	    */
+		var fetchChargeGroupsSuccessCallback = function(data) {
+			$scope.chargeGroups = data.charge_groups;
+			$scope.fetchSettings();
+		};
+
+	
+
+		(function init() {
+			$scope.errorMessage = '';
+			$scope.successMessage = '';
+			
+			$scope.invokeApi(ADChargeGroupsSrv.fetch, {}, fetchChargeGroupsSuccessCallback);
+		}());
+
+	}
+]);
