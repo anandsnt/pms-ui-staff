a7d073f5976ac7c162dda9d1eeb0d099984ef895
resheil <resheilmohammed@resheils-MBP.fios-router.home>
2017-01-09 17:10:01 -0500

    CICO-36764 adding toggle button

--- admin/services/zestSetup/adZestWebRoomReadyEmailSetupSrv.js
+++ admin/services/zestSetup/adZestWebRoomReadyEmailSetupSrv.js
@@ -1,13 +1,13 @@
 admin.service('ADZestWebRoomReadyEmailSetupSrv', function(ADBaseWebSrvV2) {
 
 	this.saveRoomReayEmailSettings = function(data) {
-		var url = '/admin/mobile_app_marketing';
+		var url = '/api/zest_web_room_ready_email_settings';
 
 		return ADBaseWebSrvV2.putJSON(url, data);
 	};
 
 	this.getRoomReayEmailSettings = function() {
-		var url = '/admin/mobile_app_marketing';
+		var url = '/api/zest_web_room_ready_email_settings';
 
 		return ADBaseWebSrvV2.getJSON(url);
 	};
