9e95ef8188f6ed82bc4fcce7abd1145f695eba8e
soumyastayntouch <soumya@stayntouch.com>
2017-01-10 14:55:41 +0530

    CICO-24242: Mousemove event

--- rover/react/nightlyDiary/components/rvNightlyDiaryStayRangeComponent.js
+++ rover/react/nightlyDiary/components/rvNightlyDiaryStayRangeComponent.js
@@ -1,21 +1,34 @@
-const NightlyDiaryStayRangeComponent = ({ currentSelectedReservation }) => {
+const NightlyDiaryStayRangeComponent = createClass ({
 
-    return (
-            <div className="grid-stay-range">
-                <a style={currentSelectedReservation.style} className="handle arrival left"  >
+    componentDidMount() {
+
+    },
+    mouseMove: function(e) {
+        console.log("jphme");
+        console.log(e.clientX);
+        console.log(this.props.currentSelectedReservation.arrivalPosition);
+
+    },
+    render() {
+        return (
+            <div className={this.props.currentSelectedReservation.class}>
+                <a style={this.props.currentSelectedReservation.style} className="handle arrival left" onMouseMove={(e) => this.mouseMove(e)} >
                     <span className="title">
                         Arrival
-                        <span className="date">{currentSelectedReservation.arrival_date}</span>
+                        <span className="date">{this.props.currentSelectedReservation.arrival_date}</span>
                     </span>
                     <span className="line"></span>
                 </a>
             </div>
-    );
-};
+        );
+    }
+
+
+});
 
 
 const { PropTypes } = React;
 
 NightlyDiaryStayRangeComponent.propTypes = {
-  currentSelectedReservationArrivalStyle: PropTypes.currentSelectedReservationArrivalStyle
+  currentSelectedReservation: PropTypes.currentSelectedReservation
 };
--- rover/react/nightlyDiary/containers/rvNightlyDiaryStayRangeContainer.js
+++ rover/react/nightlyDiary/containers/rvNightlyDiaryStayRangeContainer.js
@@ -2,9 +2,15 @@ const {connect} = ReactRedux;
 
 let convertCurrentSelectedReservation = (currentSelectedReservation, selectedReservationId) => {
     currentSelectedReservation.style = {};
+    let stayRangeArrivalClass = "grid-stay-range";
+
     if (selectedReservationId) {
         currentSelectedReservation.style.transform = 'translateX(' + currentSelectedReservation.arrivalPosition + ')';
+
+    } else {
+        stayRangeArrivalClass = stayRangeArrivalClass + " hidden";
     }
+    currentSelectedReservation.class = stayRangeArrivalClass;
     // TO DO: Add momentjs and do date formatting using momentjs
     // We have to show date in hotel's date format
     currentSelectedReservation.arrival_date = currentSelectedReservation.arrival_date;
