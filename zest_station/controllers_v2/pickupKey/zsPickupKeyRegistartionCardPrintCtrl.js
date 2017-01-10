f2cc568ed3b157ecaff11bb7b178804dd37f5ab5
resheil <resheilmohammed@resheils-MBP.fios-router.home>
2017-01-10 11:01:25 -0500

    CICO-36923 adding print flow to pickup key

---
+++ zest_station/controllers_v2/pickupKey/zsPickupKeyRegistartionCardPrintCtrl.js
@@ -0,0 +1,205 @@
+sntZestStation.controller('zsPickupKeyRegistartionCardPrintCtrl', [
+    '$scope',
+    '$state',
+    'zsEventConstants',
+    '$stateParams',
+    'zsCheckinSrv',
+    '$filter',
+    '$timeout',
+    '$window',
+    function($scope, $state, zsEventConstants, $stateParams, zsCheckinSrv, $filter, $timeout, $window) {
+
+		/** ********************************************************************************************
+		 **		Expected state params -----> reservation_id						
+		 **																		 
+		 ***********************************************************************************************/
+
+        BaseCtrl.call(this, $scope);
+
+
+    
+		/**
+		 * [clickedPrint description]
+		 * @return {[type]} [description]
+		 */
+        var startPrint = function() {
+            var getTermsPrintable = function(terms) {
+                sntZestStation.filter('unsafe', function($sce) {
+                    return function(terms) {
+                        return $sce.trustAsHtml(terms);
+                    };
+                });
+            };
+
+            var setMessage = function(printSuccess){
+                var keySucess = $stateParams.key_created === 'true';
+                if (printSuccess && keySucess) {
+                    $scope.mode = 'PRINT_SUCCESS_AND_KEY_SUCCESS';
+                } else if (!printSuccess && keySucess) {
+                    $scope.mode = 'PRINT_FAILED_AND_KEY_SUCCESS';
+                } else if (printSuccess && !keySucess) {
+                    $scope.mode = 'PRINT_SUCCESS_AND_KEY_FAILED';
+                } else if (!printSuccess && !keySucess) {
+                    $scope.mode = 'PRINT_FAILED_AND_KEY_FAILED';
+                } else {
+                    $scope.subtext = '';
+                }
+            };
+
+            var printFailedActions = function(errorMessage) {
+                $scope.$emit('hideLoader');
+                var printSuccess = false;
+                setMessage(printSuccess);
+                errorMessage = _.isUndefined(errorMessage) ? 'DISPENSE_KEY_PRINT_FAIL' : errorMessage;
+                $scope.zestStationData.workstationOooReason = $filter('translate')(errorMessage);
+                $scope.zestStationData.workstationStatus = 'out-of-order';
+                $scope.runDigestCycle();
+
+            };
+            var printSuccessActions = function() {
+
+                $scope.$emit('hideLoader');
+                var printSuccess = true;
+                setMessage(printSuccess);
+                $scope.runDigestCycle();
+
+            };
+
+            var handleStarTacPrinterActions = function() {
+                var printData = '';
+
+				/** ** Socket actions starts here *****/
+                $scope.$on('SOCKET_FAILED', function() {
+                    printFailedActions();
+                });
+                $scope.$on('WS_PRINT_SUCCESS', function() {
+                    printSuccessActions();
+                });
+                $scope.$on('WS_PRINT_FAILED', function(event, data) {
+                    printFailedActions(data.error_message);
+                });
+                $scope.$on('SOCKET_CONNECTED', function() {
+                    $scope.socketOperator.startPrint(printData);
+                    $scope.$emit('showLoader');
+                });
+				/** ** Socket actions ends here *****/
+
+                var fetchSatrTacDataSuccess = function(response) {
+                    printData = response.bill_details;
+					// check if socket is open
+                    if ($scope.socketOperator.returnWebSocketObject().readyState === 1) {
+                        $scope.socketOperator.startPrint(printData);
+                        $scope.$emit('showLoader');
+                    } else {
+                        $scope.$emit('CONNECT_WEBSOCKET'); // connect socket
+                    }
+                };
+
+                var data = {
+                    'reservation_id': $stateParams.reservation_id
+                };
+
+                var startTacDataFailedActions = function() {
+                    printFailedActions();
+                };
+
+                var options = {
+                    params: data,
+                    successCallBack: fetchSatrTacDataSuccess,
+                    failureCallBack: startTacDataFailedActions
+                };
+
+                $scope.callAPI(zsCheckinSrv.fetchStarTacPrinterData, options);
+            };
+
+            var handleBillPrint = function() {
+				// add the orientation
+                addPrintOrientation();
+                setBeforePrintSetup();
+                try {
+				// this will show the popup with full bill
+                    $timeout(function() {
+					/*
+					 * ======[ PRINTING!! JS EXECUTION IS PAUSED ]======
+					 */
+
+                        if (sntapp.cordovaLoaded) {
+                            var printer = sntZestStation.selectedPrinter;
+
+                            cordova.exec(function() {
+                                printSuccessActions();
+                            }, function() {
+                                printFailedActions();
+                            }, 'RVCardPlugin', 'printWebView', ['filep', '1', printer]);
+                        } else {
+                            if ($scope.zestStationData.zest_printer_option === 'STAR_TAC' && $scope.zestStationData.kiosk_use_socket_print) {
+							// we will call websocket services to print
+                                handleStarTacPrinterActions();
+                            } else {
+
+                                $window.print();
+                                setTimeout(function() {
+                                    printSuccessActions();
+                                }, 100);
+                            }
+                        }
+					// provide a delay for preview to appear 
+
+                    }, 100);
+
+                } catch (e) {
+                    console.info('something went wrong while attempting to print--->' + e);
+                    printFailedActions();
+
+                }
+
+                setTimeout(function() {
+					// CICO-9569 to solve the hotel logo issue
+                    $('header .logo').removeClass('logo-hide');
+                    $('header .h2').addClass('text-hide');
+
+					// remove the orientation after similar delay
+                    removePrintOrientation();
+
+                }, 100);
+            };
+
+            var fetchPrintViewCompleted = function(data) {
+                var d = new Date();
+
+                $scope.currentDateTime = d.getTime();
+                $scope.$emit('hideLoader');
+				// print section - if its from device call cordova.
+                $scope.printRegCardData = data;
+                $scope.departDate = $scope.printRegCardData.dep_date;
+                $scope.departDate = $scope.returnDateObjBasedOnDateFormat($scope.printRegCardData.dep_date);
+                $scope.printRegCardData.terms_conditions_html = getTermsPrintable($scope.printRegCardData.terms_conditions);
+                handleBillPrint();
+            };
+
+            var options = {
+                params: {
+                    'id': $stateParams.reservation_id,
+                    'application': 'ZEST_STATION'
+                },
+                successCallBack: fetchPrintViewCompleted,
+                failureCallBack: printFailedActions
+            };
+
+            $scope.callAPI(zsCheckinSrv.fetchRegistrationCardPrintData, options);
+        };
+        startPrint();
+	
+
+		/**
+		 * [initializeMe description]
+		 */
+        (function() {// initializeMe
+			// show back button
+            $scope.$emit(zsEventConstants.HIDE_BACK_BUTTON); // hide back buttons in 2 options page
+			// show close button
+            $scope.$emit(zsEventConstants.SHOW_CLOSE_BUTTON);
+        }());
+
+    }
+]);
