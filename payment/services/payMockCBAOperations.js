c9260e918e9af1b9a9c74a2cb0df2713396b3fe9
Dilip Sahayadhas <dilips@stayntouch.com>
2017-01-17 11:41:21 -0500

    CICO-36378 Added mock CBA operations for getLastTransaction Fixed ESLint errors and auto formatted on rvDashboardController.js file

--- payment/services/payMockCBAOperations.js
+++ payment/services/payMockCBAOperations.js
@@ -3,70 +3,111 @@ var CBAMockOperation = function() {
         paymentActionSuccessResponse = {
             account_type: 2,
             amount: 1,
-            auth_code: "",
-            binary_format: "A",
-            card_name: "VISA ISMP",
-            card_sequence: "01",
-            card_type: "",
-            currency_code: "",
-            custom_data: "",
-            cvv: "",
-            data_field: "",
-            date_settlement: "200416",
-            expiry_date: "0418",
-            pan: "494052******5694",
-            processed_offline: "0",
-            rrn: "",
-            tid: "13600822",
-            track1: "",
-            track2: "494052******5694=1804????????????????",
-            transaction_date: "131016",
-            transaction_time: "001850",
-            txn_id: "1"
+            auth_code: '',
+            binary_format: 'A',
+            card_name: 'VISA ISMP',
+            card_sequence: '01',
+            card_type: '',
+            currency_code: '',
+            custom_data: '',
+            cvv: '',
+            data_field: '',
+            date_settlement: '200416',
+            expiry_date: '0418',
+            pan: '494052******5694',
+            processed_offline: '0',
+            rrn: '',
+            tid: '13600822',
+            track1: '',
+            track2: '494052******5694=1804????????????????',
+            transaction_date: '131016',
+            transaction_time: '001850',
+            txn_id: '1'
         },
         paymentActionFailureResponse = {
             RVErrorCode: 114,
-            RVErrorDesc: "The Argument 'transaction_id' not valid."
+            RVErrorDesc: 'The Argument \'transaction_id\' not valid.'
         },
         addCardSuccessResponse = {
-            "RVCardReadCardIIN": "494052",
-            "RVCardReadCardName": "VISA ISMP",
-            "RVCardReadCardType": "VI",
-            "RVCardReadETB": "",
-            "RVCardReadETBKSN": "",
-            "RVCardReadExpDate": "1804",
-            "RVCardReadIsEncrypted": "0",
-            "RVCardReadMaskedPAN": "494052******5694",
-            "RVCardReadTrack1": "B494052******5694^??????????????????????????^1804???????????????????????????",
-            "RVCardReadTrack1KSN": "",
-            "RVCardReadTrack2": "494052******5694=1804????????????????",
-            "RVCardReadTrack2KSN": "",
-            "RVCardReadTrack3": "",
-            "RVCardReadTrack3KSN": ""
-        };
+            'RVCardReadCardIIN': '494052',
+            'RVCardReadCardName': 'VISA ISMP',
+            'RVCardReadCardType': 'VI',
+            'RVCardReadETB': '',
+            'RVCardReadETBKSN': '',
+            'RVCardReadExpDate': '1804',
+            'RVCardReadIsEncrypted': '0',
+            'RVCardReadMaskedPAN': '494052******5694',
+            'RVCardReadTrack1': 'B494052******5694^??????????????????????????^1804???????????????????????????',
+            'RVCardReadTrack1KSN': '',
+            'RVCardReadTrack2': '494052******5694=1804????????????????',
+            'RVCardReadTrack2KSN': '',
+            'RVCardReadTrack3': '',
+            'RVCardReadTrack3KSN': ''
+        },
+        getLastTransactionSuccessResponse = {
+            'txn_id': '1',
+            'last_txn_success': () => Math.random() > 0.5 ? 0 : 1,
+            'amount': '1.00',
+            'tid': '12356',
+            'transaction_date': '',
+            'transaction_time': '',
+            'date_settlement': '',
+            'card_name': '',
+            'card_type': '',
+            'card_sequence': '',
+            'track1': '',
+            'track2': '',
+            'pan': '',
+            'expiry_date': '',
+            'cvv': '',
+            'currency_code': '',
+            'binary_format': '',
+            'data_field': '',
+            'custom_data': '',
+            'state_vars': ''
+        },
+        getLastTransactionFailureResponse = [{
+            RVErrorCode: 104,
+            RVErrorDesc: 'Device Not Connected.'
+        }, {
+            RVErrorCode: 148,
+            RVErrorDesc: 'Transaction was not processed by PIN pad.'
+        }, {
+            RVErrorCode: 147,
+            RVErrorDesc: 'No transaction pending.'
+        }, {
+            RVErrorCode: 146,
+            RVErrorDesc: 'Failed to get transaction details.'
+        }];
 
     /**
-     *
-     * @param options
+     * wrapper Method to call all services
+     * @param {Object} options - payLoad for cordova APIs
+     * @return {Object} simulated response
      */
     self.callCordovaService = function(options) {
         switch (options.action) {
-            case "doPayment":
-                window.setTimeout(()=> {
+            case 'doPayment':
+                window.setTimeout(() => {
                     Math.random() > 0.5 ? options.successCallBack(paymentActionSuccessResponse)
                         : options.failureCallBack(paymentActionFailureResponse);
                 }, 2000);
                 break;
-
-            case "doRefund":
-                window.setTimeout(()=> {
+            case 'doRefund':
+                window.setTimeout(() => {
                     Math.random() > 0.5 ? options.successCallBack(paymentActionSuccessResponse)
                         : options.failureCallBack(paymentActionFailureResponse);
                 }, 2000);
                 break;
-            case "addCard":
-                window.setTimeout(()=> {
+            case 'addCard':
+                window.setTimeout(() => {
                     options.successCallBack(addCardSuccessResponse);
+                }, 2000);
+                break;
+            case 'getLastTransaction':
+                window.setTimeout(() => {
+                    Math.random() > 0.5 ? options.successCallBack(getLastTransactionSuccessResponse)
+                        : options.failureCallBack(getLastTransactionFailureResponse[parseInt(Math.random() * 100 % 4, 10)]);
                 }, 2000);
                 break;
             default:
