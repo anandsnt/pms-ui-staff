377b21422d0685f09be5d18da3ee5371d7bd43f1
resheil <resheilmohammed@resheils-MBP.fios-router.home>
2017-01-18 14:28:35 -0500

    CICO-36985 room upsell integration

--- admin/controllers/zestStation/adZestStationRoomUpsellCtrl.js
+++ admin/controllers/zestStation/adZestStationRoomUpsellCtrl.js
@@ -1,13 +1,7 @@
-admin.controller('ADZestStationRoomUpsellCtrl', ['$scope', '$rootScope', '$state', '$stateParams', 'ADZestStationSrv', '$filter',
-    function($scope, $state, $rootScope, $stateParams, ADZestStationSrv, $filter) {
-        BaseCtrl.call(this, $scope);
-        $scope.data = {};
-        $scope.data.hotel_upsell_is_on = true;
+admin.controller('ADZestStationRoomUpsellCtrl', ['$scope', 'ADZestStationSrv', 'roomUpsellData',
+    function($scope, ADZestStationSrv, roomUpsellData) {
 
-        var maxUpsellPresent = 5;
 
-        $scope.maxUpsellOptions = _.range(1, maxUpsellPresent + 1);
-        console.log($scope.maxUpsellOptions);
 
         $scope.percentageChanged = function(type) {
             var isTier2Changed = type === 'tier2';
@@ -15,11 +9,46 @@ admin.controller('ADZestStationRoomUpsellCtrl', ['$scope', '$rootScope', '$state
             var tier3Value = parseInt($scope.data.upsell_compositions_in_percerntage.tier_3);
             if (isTier2Changed && !_.isNaN(tier2Value) && tier2Value < 100) {
                 $scope.data.upsell_compositions_in_percerntage.tier_3 = 100 - tier2Value;
+                $scope.data.upsell_compositions_in_percerntage.tier_2 = tier2Value;
             } else if (!isTier2Changed && !_.isNaN(tier3Value) && tier3Value < 100) {
                 $scope.data.upsell_compositions_in_percerntage.tier_2 = 100 - tier3Value;
+                $scope.data.upsell_compositions_in_percerntage.tier_3 = tier3Value;
             } else {
                 return;
             }
         };
+
+        /**
+         * function to save the details
+         */
+        $scope.saveUpsellData = function() {
+            var options = {
+                params: $scope.data,
+                successCallBack: $scope.goBackToPreviousState
+            };
+
+            $scope.callAPI(ADZestStationSrv.save, options);
+        };
+        (function() {
+
+            BaseCtrl.call(this, $scope);
+            var upsellData = {};
+            // extract the required values
+            upsellData.offer_kiosk_room_upsell = roomUpsellData.offer_kiosk_room_upsell;
+            upsellData.room_upsell_options_order = roomUpsellData.room_upsell_options_order;
+            upsellData.enforce_max_upsell_offered = roomUpsellData.enforce_max_upsell_offered;
+            upsellData.maximum_room_upsell_offers = roomUpsellData.maximum_room_upsell_offers;
+            upsellData.upsell_compositions_in_percerntage = roomUpsellData.upsell_compositions_in_percerntage;
+            upsellData.room_upsell_style = roomUpsellData.room_upsell_style;
+
+            $scope.data = upsellData;
+
+            $scope.data.hotel_upsell_is_on = true;
+
+            var maxUpsellPresent = 5;
+
+            $scope.maxUpsellOptions = _.range(1, maxUpsellPresent + 1);
+            console.log($scope.maxUpsellOptions);
+        })();
     }
 ]);
