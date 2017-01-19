d3377e5777526a1409acdeefa9476ebe54f5fb36
rahulkookal <rahul@stayntouch.com>
2017-01-19 15:07:36 +0530

    CICO-24242 : Intermediate commit - implemented arrival flag draging

--- rover/react/nightlyDiary/components/rvNightlyDiaryStayRangeComponent.js
+++ rover/react/nightlyDiary/components/rvNightlyDiaryStayRangeComponent.js
@@ -5,7 +5,10 @@ const NightlyDiaryStayRangeComponent = createClass ({
             mouseClikedX: 0,
             mouseLastPositionX: 0,
             arrivalStyle: this.props.currentSelectedReservation.arrivalStyle,
-            arrivalPosition: this.props.currentSelectedReservation.arrivalPosition123
+            // this.props.currentSelectedReservation.arrivalPosition / departurePosition is in the form of xxPX
+            arrivalPosition: parseInt(this.props.currentSelectedReservation.arrivalPosition),
+            departurePosition: parseInt(this.props.currentSelectedReservation.departurePosition),
+            daysMode: 7
         };
     },
 
@@ -14,34 +17,41 @@ const NightlyDiaryStayRangeComponent = createClass ({
         this.mouseStartingEvent = this.isTouchEnabled ? 'touchstart' : 'mousedown';
         this.mouseMovingEvent   = this.isTouchEnabled ? 'touchmove' : 'mousemove';
         this.mouseLeavingEvent  = this.isTouchEnabled ? 'touchend'  : 'mouseup';
-        this.arrivalflag.addEventListener (this.mouseStartingEvent, e => this.arrivalFlagMouseDown (e));
+        this.arrivalFlag.addEventListener (this.mouseStartingEvent, e => this.arrivalFlagMouseDown (e));
     },
 
     arrivalFlagMouseDown(e) {
-        console.log(e);
-        let state = this.state;
+        let state = this.state,
+            flagarea = this.flagarea;
+
         e.preventDefault ();
         e.stopPropagation ();
         e = this.isTouchEnabled ? e.changedTouches[0] : e;
         state.isMouseDragging = true;
         state.mouseClikedX = e.clientX;
         state.mouseLastPositionX = e.clientX;
-        this.arrivalflag.addEventListener (this.mouseMovingEvent, e => this.arrivalFlagMouseMove (e));
-        this.arrivalflag.addEventListener (this.mouseLeavingEvent, e => this.arrivalFlagMouseLeave (e));
-        this.arrivalflag.addEventListener ('mouseleave', e => this.arrivalFlagMouseLeave (e));
+        flagarea.addEventListener (this.mouseMovingEvent, e => this.arrivalFlagMouseMove (e));
+        flagarea.addEventListener (this.mouseLeavingEvent, e => this.arrivalFlagMouseLeave (e));
+        flagarea.addEventListener ('mouseleave', e => this.arrivalFlagMouseLeave (e));
     },
 
     arrivalFlagMouseLeave(e) {
+        let state = this.state,
+            flagarea = this.flagarea;
+
         e.preventDefault ();
         e.stopPropagation ();
-        this.state.isMouseDragging = false;
-        this.arrivalflag.removeEventListener(this.mouseMovingEvent, () =>{ console.log("key up"); });
-        this.arrivalflag.removeEventListener(this.mouseLeavingEvent, () =>{ console.log("key up"); });
+        // TODO - remove this check and remove 'mouseleave' listner
+        if (state.isMouseDragging) {
+            state.isMouseDragging = false;
+            this.calculateArrivalDate();
+        }
+        
+        flagarea.removeEventListener(this.mouseMovingEvent, () =>{});
+        flagarea.removeEventListener(this.mouseLeavingEvent, () =>{});
     },
 
     arrivalFlagMouseMove(e) {
-        console.log("Move");
-        console.log(e);
         e.preventDefault ();
         e.stopPropagation ();
         let diff = e.clientX - this.state.mouseLastPositionX;
@@ -51,27 +61,31 @@ const NightlyDiaryStayRangeComponent = createClass ({
     },
 
     moveArrivalFlag(diff) {
-        let newPosition = this.state.arrivalPosition + diff;        
-        if (this.state.isMouseDragging) {
+        let state = this.state,
+            curentPosition = state.arrivalPosition + diff;
+
+        if (state.isMouseDragging) {
             this.setState({
                 arrivalStyle: {
-                    transform: 'translateX(' + newPosition + 'px)'
+                    transform: 'translateX(' + curentPosition + 'px)'
                 },
-                arrivalPosition: newPosition
+                arrivalPosition: curentPosition
             });
         }
     },
 
+    calculateArrivalDate() {
+        console.log("calculateArrivalDate");
+    },
+
     render() {
-        console.log("Render");
-        console.log(this.state);
         let arrivalStyle = this.state.arrivalStyle,
             reservationClass = this.props.currentSelectedReservation.class;
 
         return (
-            <div className={reservationClass} ref={node => this.arrivalflag = node}>
+            <div className={reservationClass} ref={node => this.flagarea = node}>
                 <a style={arrivalStyle} className="handle arrival left" >
-                    <span className="title" >
+                    <span className="title" ref={node => this.arrivalFlag = node}>
                         Arrival
                         <span className="date">{this.props.currentSelectedReservation.arrivalDate}</span>
                     </span>
