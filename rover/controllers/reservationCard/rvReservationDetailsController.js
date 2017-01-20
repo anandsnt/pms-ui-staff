5939feec23f4b243be1119cdd37a03893a90bc1f
ragesh <ragesh@stayntouch.com>
2017-01-20 15:53:19 +0530

    CICO-36733 Fixed the issue with the house availability while showing overbooking alert

--- rover/controllers/reservationCard/rvReservationDetailsController.js
+++ rover/controllers/reservationCard/rvReservationDetailsController.js
@@ -942,7 +942,7 @@ sntRover.controller('reservationDetailsController',
 
                         //CICO-36733
                         $scope.showOverBookingAlert = !response.data.is_room_type_available && rvPermissionSrv.getPermissionValue('OVERBOOK_ROOM_TYPE');
-                        $scope.showChangeDatesPopup = !rvPermissionSrv.getPermissionValue('OVERBOOK_ROOM_TYPE') || response.data.is_room_type_available;
+                        $scope.showChangeDatesPopup = !rvPermissionSrv.getPermissionValue('OVERBOOK_ROOM_TYPE') || response.data.is_room_type_available || !response.data.is_house_available;
 
 						ngDialog.open({
 							template: '/assets/partials/reservation/alerts/editDatesInStayCard.html',
