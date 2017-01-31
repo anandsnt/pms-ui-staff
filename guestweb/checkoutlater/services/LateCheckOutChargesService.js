84cd98498233749f328a5277557cea541c1d5c47
resheil <resheilmohammed@resheils-MBP.fios-router.home>
2017-01-31 16:39:42 -0500

    CICO-37238 fixing issue with late checkout

--- guestweb/checkoutlater/services/LateCheckOutChargesService.js
+++ guestweb/checkoutlater/services/LateCheckOutChargesService.js
@@ -1,48 +1,39 @@
-(function() {
-	var LateCheckOutChargesService = function($http, $q, $rootScope) {
-	var charges = {};
+sntGuestWeb.service('LateCheckOutChargesService', [
+	'$http',
+	'$q',
+	'$rootScope',
+	function($http, $q, $rootScope) {
+		var service = this;
 
-    var fetchLateCheckoutOptions = function() {
-		var deferred = $q.defer();
-		var url = '/guest_web/get_late_checkout_charges.json',
-		parameters = {'reservation_id': $rootScope.reservationID};
+		service.charges = {};
 
-		$http.get(url, {
-			params: parameters
-		}).then(function(response) {
-			this.charges = response.data;
-			deferred.resolve(this.charges);
-		}, function() {
-			deferred.reject();
-		});
-		return deferred.promise;
-	};
+		service.fetchLateCheckoutOptions = function() {
+			var deferred = $q.defer();
+			var url = '/guest_web/get_late_checkout_charges.json',
+				parameters = {
+					'reservation_id': $rootScope.reservationID
+				};
 
-	var postNewCheckoutOption = function(url, data) {
-		var deferred = $q.defer();
+			$http.get(url, {
+				params: parameters
+			}).then(function(response) {
+				service.charges = response.data;
+				deferred.resolve(service.charges);
+			}, function() {
+				deferred.reject();
+			});
+			return deferred.promise;
+		};
 
-		$http.post(url, data).then(function(response) {
-			deferred.resolve(response.data);
-		}, function() {
-			deferred.reject();
-		});
-		return deferred.promise;
-	};
+		service.postNewCheckoutOption = function(url, data) {
+			var deferred = $q.defer();
 
-
-return {
-	charges: charges,
-	fetchLateCheckoutOptions: fetchLateCheckoutOptions,
-	postNewCheckoutOption: postNewCheckoutOption
-};
-};
-
-var dependencies = [
-'$http',
-'$q',
-'$rootScope',
-LateCheckOutChargesService
-];
-
-sntGuestWeb.factory('LateCheckOutChargesService', dependencies);
-})();
+			$http.post(url, data).then(function(response) {
+				deferred.resolve(response.data);
+			}, function() {
+				deferred.reject();
+			});
+			return deferred.promise;
+		};
+	}
+]);
