cb542107d8a09ebde3caec83951ebe40e8e4bc48
Dilip Sahayadhas <dilips@stayntouch.com>
2017-02-15 09:25:15 -0500

    CICO-38260 Fix for external Mappings dropdown issue

--- shared/directives/convertToNumber/convertToNumberDir.js
+++ shared/directives/convertToNumber/convertToNumberDir.js
@@ -3,7 +3,7 @@ angular.module('convertToNumber', []).directive('convertToNumber', function() {
         require: 'ngModel',
         link: function(scope, element, attrs, ngModel) {
             ngModel.$parsers.push(function(val) {
-                return parseInt(val, 10);
+                return isFinite(parseInt(val, 10)) ? parseInt(val, 10) : val;
             });
             ngModel.$formatters.push(function(val) {
                 return '' + val;
