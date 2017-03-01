aca0c68268d59d61e08c1766c49b6f619d4ec37f
Dilip Sahayadhas <dilips@stayntouch.com>
2017-03-01 10:01:15 -0500

    CICO-38756 Dropdown issues on charge codes of type TAX fixed

--- shared/directives/convertToNumber/convertToNumberDir.js
+++ shared/directives/convertToNumber/convertToNumberDir.js
@@ -6,6 +6,9 @@ angular.module('convertToNumber', []).directive('convertToNumber', function() {
                 return isFinite(parseInt(val, 10)) ? parseInt(val, 10) : val;
             });
             ngModel.$formatters.push(function(val) {
+                if (angular.isUndefined(val)) {
+                    return '';
+                }
                 return '' + val;
             });
         }
