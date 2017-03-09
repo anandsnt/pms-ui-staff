793a0c5b040e7947025b2ace50f721dc727b876d
resheil <resheilmohammed@resheils-MBP.fios-router.home>
2017-03-09 15:40:55 -0500

    CICO-33347 fixing image reuploading issue

--- admin/controllers/upsellAdons/adUpsellAddonSettingsCtrl.js
+++ admin/controllers/upsellAdons/adUpsellAddonSettingsCtrl.js
@@ -1,19 +1,28 @@
 admin.controller('adUpsellAddonSettingsCtrl', function($scope, ADUpsellAddonSrv, data) {
 
 	BaseCtrl.call(this, $scope);
+	var addonDefaultImage;
+
 	/**
 	 * function to save the details
 	 */
 	$scope.saveDetails = function() {
 		var options = {
-			params: $scope.data,
+			params: angular.copy($scope.data),
 			successCallBack: $scope.goBackToPreviousState
 		};
+
+		if (addonDefaultImage === $scope.data.addon_default_image) {
+			// set image as empty string, if image is not changed
+			options.params.addon_default_image = '';
+		}
 
 		$scope.callAPI(ADUpsellAddonSrv.saveDetails, options);
 	};
 
 	(function() {
 		$scope.data = data;
+		// store refernce of the image
+		addonDefaultImage = angular.copy(data.addon_default_image);
 	})();
 });
