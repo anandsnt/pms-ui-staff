868dafeb75ba74d58d161f922e0a56c8c4c8285d
resheil <resheilmohammed@resheils-MBP.fios-router.home>
2017-03-09 09:32:05 -0500

    CICO-33347 service layer changes

--- admin/services/upsellAddons/adUpsellAddonSrv.js
+++ admin/services/upsellAddons/adUpsellAddonSrv.js
@@ -1,15 +1,12 @@
 admin.service('ADUpsellAddonSrv', function(ADBaseWebSrvV2) {
 
 	this.saveDetails = function(data) {
-		//var url = '/api/zest_web_room_ready_email_settings';
-		var url = '/api/upsell_addons_setup/save_upsell_addons_setup';
-		return ADBaseWebSrvV2.putJSON(url, data);
+		var url = '/api/upsell_addons_setups/save_upsell_addons_setup';
+		return ADBaseWebSrvV2.postJSON(url, data);
 	};
 
 	this.getSettings = function() {
-		//var url = '/api/zest_web_room_ready_email_settings';
-		//var url = '/sample_json/upsell_addons/upsell_settings.json';
-		var url = '/api/upsell_addons_setup';
+		var url = '/api/upsell_addons_setups';
 		return ADBaseWebSrvV2.getJSON(url);
 	};
 });
