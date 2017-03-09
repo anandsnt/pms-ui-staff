b0c9a212b95d4efcaf381c57e09076fb048db4b3
resheil <resheilmohammed@resheils-MacBook-Pro.local>
2017-03-09 07:39:38 -0500

    CiCO-33347 service url changes

--- admin/services/upsellAddons/adUpsellAddonSrv.js
+++ admin/services/upsellAddons/adUpsellAddonSrv.js
@@ -1,13 +1,15 @@
 admin.service('ADUpsellAddonSrv', function(ADBaseWebSrvV2) {
 
 	this.saveDetails = function(data) {
-		var url = '/api/zest_web_room_ready_email_settings';
+		//var url = '/api/zest_web_room_ready_email_settings';
+		var url = '/api/upsell_addons_setup/save_upsell_addons_setup';
 		return ADBaseWebSrvV2.putJSON(url, data);
 	};
 
 	this.getSettings = function() {
 		//var url = '/api/zest_web_room_ready_email_settings';
-		var url = '/sample_json/upsell_addons/upsell_settings.json';
+		//var url = '/sample_json/upsell_addons/upsell_settings.json';
+		var url = '/api/upsell_addons_setup';
 		return ADBaseWebSrvV2.getJSON(url);
 	};
 });
