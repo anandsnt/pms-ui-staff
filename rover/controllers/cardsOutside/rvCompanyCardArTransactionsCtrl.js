6dd13dfca50bf403ecd49babee36fb3f2c862d53
anandp <anandp@stayntouch.com>
2017-03-13 15:46:06 +0530

    CICO-35751: Bug Fix.

--- rover/controllers/cardsOutside/rvCompanyCardArTransactionsCtrl.js
+++ rover/controllers/cardsOutside/rvCompanyCardArTransactionsCtrl.js
@@ -706,7 +706,7 @@ sntRover.controller('RVCompanyCardArTransactionsCtrl', ['$scope', '$rootScope', 
 		/*
 	     *Function which fetches and returns the charge details of a grouped charge.
 	     */
-		$scope.expandGroupedCharge = function(item) {
+		$scope.expandGroupedCharge = function(index, item) {
 			// Success callback for the charge detail fetch for grouped charges.
 			var fetchChargeDataSuccessCallback = function(data) {
 				item.light_speed_data = data;
