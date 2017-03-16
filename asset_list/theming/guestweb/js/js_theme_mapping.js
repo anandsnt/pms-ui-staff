f45c6363b3f9dfb76a2184eecd75aa4295e377b4
Aswanth <aswanth@stayntouch.com>
2017-03-16 16:55:58 +0530

    CICO-38692 updated the checkin confirmation flow to add moble marketing

--- asset_list/theming/guestweb/js/js_theme_mapping.js
+++ asset_list/theming/guestweb/js/js_theme_mapping.js
@@ -86,7 +86,7 @@ module.exports = {
             'guestweb_EdenRoc': [ themeJsRoot + 'app_router_common.js'],
             'guestweb_national_harbor': [themeJsRoot + 'app_router_mgm_chain.js',
                 themeJsRoot + 'app_router_national_harbor.js'],
-            'guestweb_freehand': [ themeJsRoot + 'app_router_common.js'],
+            'guestweb_freehand': [ themeJsRoot + 'app_router_freehand.js'],
             'guestweb_dewberry': [ themeJsRoot + 'app_router_common.js'],
             'guestweb_park_valkenburg': [ themeJsRoot + 'app_router_common.js'],
             'guestweb_sohotel': [ themeJsRoot + 'app_router_common.js'],
