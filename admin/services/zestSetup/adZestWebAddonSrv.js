89b366c07c43c823bead008a0f2b15eb6288635b
resheil <resheilmohammed@resheils-MBP.fios-router.home>
2017-03-21 09:12:06 -0400

    CICO-33348 code climate fix

--- admin/services/zestSetup/adZestWebAddonSrv.js
+++ admin/services/zestSetup/adZestWebAddonSrv.js
@@ -1,10 +1,10 @@
-admin.service('ADZestWebAddonSrv', function(ADBaseWebSrvV2,$q) {
+admin.service('ADZestWebAddonSrv', function(ADBaseWebSrvV2, $q) {
 
 	this.fetchAddonSettings = function(params) {
 		var deferred = $q.defer();
 		var url = '/api/upsell_addons';
 
-		ADBaseWebSrvV2.getJSON(url,params).then(function(data) {
+		ADBaseWebSrvV2.getJSON(url, params).then(function(data) {
 			deferred.resolve(data);
 		}, function(data) {
 			deferred.reject(data);
@@ -16,7 +16,7 @@ admin.service('ADZestWebAddonSrv', function(ADBaseWebSrvV2,$q) {
 		var deferred = $q.defer();
 		var url = '/api/upsell_addons/save';
 
-		ADBaseWebSrvV2.postJSON(url,params).then(function(data) {
+		ADBaseWebSrvV2.postJSON(url, params).then(function(data) {
 			deferred.resolve(data);
 		}, function(data) {
 			deferred.reject(data);
