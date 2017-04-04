ba7ed3c87cf8b3145687424a305231515194ec88
Dilip Sahayadhas <dilips@stayntouch.com>
2017-04-04 10:08:25 -0400

    CICO-38340 CICO-39834 ZDirect configuration in admin moved to new endpoint

---
+++ admin/controllers/interfaces/ZDirect/adZDirectSetupCtrl.js
@@ -0,0 +1,27 @@
+admin.controller('adZDirectSetupCtrl', ['$scope', 'config', 'adInterfacesCommonConfigSrv',
+    function($scope, config, adInterfacesCommonConfigSrv) {
+
+        var interfaceIdentifier = 'zdirect';
+
+        $scope.toggleEnabled = function() {
+            config.enabled = !config.enabled;
+        };
+
+        $scope.saveInterfaceConfig = function() {
+            $scope.callAPI(adInterfacesCommonConfigSrv.saveConfiguration, {
+                params: {
+                    config: $scope.config,
+                    interfaceIdentifier: interfaceIdentifier
+                },
+                onSuccess: function() {
+                    $scope.goBackToPreviousState();
+                }
+            });
+        };
+
+        (function() {
+            $scope.config = config;
+            $scope.interface = interfaceIdentifier.toUpperCase();
+        })();
+    }
+])
