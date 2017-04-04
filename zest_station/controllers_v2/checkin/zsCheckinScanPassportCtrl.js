05864415bf1e1d8e9722da70690542f52f9958ce
Mike <mike@stayntouch.com>
2017-04-04 14:49:48 -0400

    CICO-33247 initial UI prototype for passport scanning zest station

--- zest_station/controllers_v2/checkin/zsCheckinScanPassportCtrl.js
+++ zest_station/controllers_v2/checkin/zsCheckinScanPassportCtrl.js
@@ -22,7 +22,7 @@ sntZestStation.controller('zsCheckinScanPassportCtrl', [
 
         /**
          * MODES
-         * 1.SCAN_PRIMARY - Start scan for primary guest 
+         * 1.SCAN_PASSPORT - Start scan for primary guest 
          * 2.SCANNING_IN_PROGRESS - In progress indicator (scanning activated by user)
          * 3.SCAN_RESULTS - List of Users with Scan status (stated/success/failed)
          *                - Show Done if all passports 'success'
@@ -32,38 +32,101 @@ sntZestStation.controller('zsCheckinScanPassportCtrl', [
         var collectPassportEnabled = $scope.zestStationData.check_in_collect_passport;
 
         var onPassportScanSuccess = function() {
+
+            var results = $scope.selectedReservation.guest_details,
+                readyToContinue = true;
+
+            for (var i in $scope.selectedReservation.guest_details) {
+
+                if ($scope.selectedPassport) {
+                    if ($scope.selectedPassportInfo.id === $scope.selectedReservation.guest_details[i].id) {
+                        $scope.selectedReservation.guest_details[i].passport_scan_status = 'SUCCESS';
+                    }
+                }
+
+                if ($scope.selectedReservation.guest_details[i].passport_scan_status !== 'SUCCESS') {
+                    readyToContinue = false;
+                }
+            }
+            $scope.allPassportReady = readyToContinue;
             $scope.mode = 'SCAN_RESULTS';
+            console.log('mode: ',$scope.mode);
+
+
         };
         var onPassportScanfailure = function() {
             $scope.mode = 'SCAN_FAILURE';
+            console.log('mode: ',$scope.mode);
         };
+
+        $scope.addAGuest = function() {
+            $scope.AddGuestMode = true;
+            console.log('mode: ',$scope.mode,' - add guest mode: ',$scope.AddGuestMode);
+        };
+
+
+        $scope.scanPassport = function(guestInfo) {
+            $scope.selectedPassport = true;
+            $scope.selectedPassportInfo = guestInfo;
+            console.log('guest',guestInfo);
+            $scope.reScanPassport();
+        };
+
 
         $scope.scan = function() {
             $scope.mode = 'SCANNING_IN_PROGRESS';
+
+            console.log('mode: ',$scope.mode);
             $scope.resetTime();
 
 
             // debugging
             $timeout(function() {
-                $scope.$emit('PASSPORT_SCAN_FAILURE');
+             //   if (!gotoSuccess) {
+                    $scope.$emit('PASSPORT_SCAN_SUCCESS');
+            //    } else {
+            //        $scope.$emit('PASSPORT_SCAN_FAILURE');    
+             //   }
+                
+
             }, 4000);
             
         };
 
         $scope.reScanPassport = function() {
-            
+
+            $scope.mode = 'SCAN_PASSPORT';
+
+            // debugging
+            gotoSuccess = true;
         };
+        $scope.selectedReservation = {};
+        $scope.selectedReservation.guest_details = [
+            {
+                'last_name': 'fuller',
+                'first_name': 'mike',
+                'id':1232,
+                'passport_scan_status': 'SUCCESS'
+            },{
+                'last_name': 'guy',
+                'id':1231,
+                'first_name': 'that',
+                'passport_scan_status': 'NOT STARTED'
+            }
+        ];
+        $scope.AddGuestMode = false;
+        $scope.showRemoveButton = false;
 
         /**
          * [initializeMe description]
          */
-
+        var gotoSuccess = false; // debugging, remove when done
         var initializeMe = (function() {
             // show back button
             $scope.$emit(zsEventConstants.HIDE_BACK_BUTTON);
             // show close button
             $scope.$emit(zsEventConstants.SHOW_CLOSE_BUTTON);
-            $scope.mode = 'SCAN_PRIMARY';
+            $scope.mode = 'SCAN_PASSPORT';
             console.log('collectPassportEnabled: ', collectPassportEnabled);
             console.log('show mode: ', $scope.mode);
 
