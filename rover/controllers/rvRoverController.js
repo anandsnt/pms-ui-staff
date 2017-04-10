ee59c90415887bffe7c5325c9d14aed880de6b9f
soumyastayntouch <soumya@stayntouch.com>
2017-04-10 18:34:21 +0530

    CICO-39760: Enable extend/shorten in pms-dev

--- rover/controllers/rvRoverController.js
+++ rover/controllers/rvRoverController.js
@@ -125,8 +125,6 @@ sntRover.controller('roverController', [
     $rootScope.isQueuedRoomsTurnedOn = hotelDetails.housekeeping.is_queue_rooms_on;
     $rootScope.advanced_queue_flow_enabled = hotelDetails.advanced_queue_flow_enabled;
     $rootScope.isPmsProductionEnv = hotelDetails.is_pms_prod;
-    // Remove below code after  QA acceptance in dev - CICO-39760
-    $rootScope.isPmsProductionEnv = true;
 
     $rootScope.isManualCCEntryEnabled = hotelDetails.is_allow_manual_cc_entry;
       /**
