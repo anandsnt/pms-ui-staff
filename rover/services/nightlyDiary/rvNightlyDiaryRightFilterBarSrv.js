eed7ac4f3ddac607339fb46bd1acdbb1334521aa
soumyastayntouch <soumya@stayntouch.com>
2017-04-19 17:16:24 +0530

    Revert "CICO-39329: Filter Section is not present after navigating from room …"

--- rover/services/nightlyDiary/rvNightlyDiaryRightFilterBarSrv.js
+++ rover/services/nightlyDiary/rvNightlyDiaryRightFilterBarSrv.js
@@ -1,59 +1,40 @@
 angular.module('sntRover').service('RVNightlyDiaryRightFilterBarSrv',
-    ['$q',
-    'BaseWebSrvV2',
-    function($q, BaseWebSrvV2) {
-        var service = this;
-
-        /*
+	['$q',
+	'BaseWebSrvV2',
+	function($q, BaseWebSrvV2) {
+		var that = this;
+		
+		/*
          * Fetch room type list
          * @param {data} object
          * return object
          */
-        service.fetchRoomType = function(params) {
-            var deferred = $q.defer(),
-                url = '/api/room_types.json?exclude_pseudo=true&exclude_suite=true';
+		that.fetchRoomType = function(params) {
+			var deferred = $q.defer(), 
+				url = '/api/room_types.json?exclude_pseudo=true&exclude_suite=true';
 
-            BaseWebSrvV2.getJSON(url, params).then(function(response) {
-                deferred.resolve(response);
-            }, function(error) {
-                deferred.reject(error);
-            });
-            return deferred.promise;
-        };
-
-        /*
+			BaseWebSrvV2.getJSON(url, params).then(function(response) {
+				deferred.resolve(response);
+			}, function(error) {
+				deferred.reject(error);
+			});
+			return deferred.promise;
+		};
+		
+		/*
          * Fetch Floor list
          * @param {data} object
          * return object
          */
-        service.fetchFloorList = function(params) {
-            var deferred = $q.defer(),
-                url = '/api/floors.json';
-
-            BaseWebSrvV2.getJSON(url, params).then(function(response) {
-                deferred.resolve(response);
-            }, function(error) {
-                deferred.reject(error);
-            });
-            return deferred.promise;
-        };
-
-        service.fetchRoomTypeAndFloorList = function(params) {
-            var deferred = $q.defer(),
-                promises = [],
-                response = {};
-
-            promises.push(service.fetchFloorList(params).then((data) => {
-                response.floors = data.floors;
-            }));
-
-            promises.push(service.fetchRoomType(params).then((data) => {
-                response.rooms = data.results;
-            }));
-
-            $q.all(promises).then(() => {
-                deferred.resolve(response);
-            });
-            return deferred.promise;
-        };
+		that.fetchFloorList = function(params) {
+			var deferred = $q.defer(), 
+				url = '/api/floors.json';
+				
+			BaseWebSrvV2.getJSON(url, params).then(function(response) {
+				deferred.resolve(response);
+			}, function(error) {
+				deferred.reject(error);
+			});
+			return deferred.promise;
+		};
 }]);
