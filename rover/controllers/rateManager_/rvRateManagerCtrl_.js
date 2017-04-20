f5138780671b08debf5632829c1a4474e6ef4206
soumyastayntouch <soumya@stayntouch.com>
2017-04-20 18:59:24 +0530

    CICO-40131: Design issue after setting restriction

--- rover/controllers/rateManager_/rvRateManagerCtrl_.js
+++ rover/controllers/rateManager_/rvRateManagerCtrl_.js
@@ -734,6 +734,9 @@ angular.module('sntRover').controller('rvRateManagerCtrl_', [
 
         // dispatching to redux
         store.dispatch(reduxActionForAllRateTypesView);
+        $timeout(function() {
+            $("#rate-manager").scrollTop(0);
+        }, 2000);
     };
 
     /*
