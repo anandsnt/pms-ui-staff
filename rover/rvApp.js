cc7cadac264c9954a5c7e82ac5283259e65e2771
Dilip Sahayadhas <dilips@stayntouch.com>
2017-05-04 15:17:56 -0400

    CICO-40874 Removed repeated observeForSwipe calls Initiate a new observeForSwipe call when the app comes to the foreground.

--- rover/rvApp.js
+++ rover/rvApp.js
@@ -393,5 +393,10 @@ sntRover.run([
 
 			$rootScope.diaryState.update(toState.name, fromState.name, fromParams);
 		});
+
+        document.addEventListener('cordovaLoaded', function() {
+            $rootScope.$broadcast('RESUME_OBSERVE_FOR_SWIPE_RESETS');
+        });
+
 	}
 ]);
