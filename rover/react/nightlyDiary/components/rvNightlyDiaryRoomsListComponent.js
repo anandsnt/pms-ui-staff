fb4cc1b571365fc1caa27d44010ae960742a4744
Shidil Eringa <shidil@stayntouch.com>
2017-05-16 15:16:45 +0530

    CICO-40764 Made tooltip mutually exclusive for each room

--- rover/react/nightlyDiary/components/rvNightlyDiaryRoomsListComponent.js
+++ rover/react/nightlyDiary/components/rvNightlyDiaryRoomsListComponent.js
@@ -1,10 +1,10 @@
 const { PropTypes } = React;
 
 /**
- * Component to display room number in room list of left sidebar.
- * This is a component with state.
+ * Component to display room list at left side of diary grid
+ * Component with local state since it is presentational only.
  */
-class NightlyDiaryRoomsListItemComponent extends React.Component {
+class NightlyDiaryRoomsListComponent extends React.Component {
     /**
      * [constructor description]
      * @param  {[type]} props [description]
@@ -13,19 +13,45 @@ class NightlyDiaryRoomsListItemComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            showSuiteList: false
+            selectedRoom: null
         };
-        this.showSuitRoomsList = this.showSuitRoomsList.bind(this);
     }
 
     /**
      * [showSuitRoomsList description]
-     * @return {[type]} [description]
+     * @param {Number} index [description]
+     * @return {Undefined} [description]
      */
-    showSuitRoomsList() {
+    setSelectedRoom(index) {
         this.setState({
-            showSuiteList: !this.state.showSuiteList
+            selectedRoom: (index === this.state.selectedRoom) ? null : index
         });
+    }
+
+    /**
+     * [renderRoom description]
+     * @param {Object} item roomData
+     * @param {Number} index item index on list
+     * @return {Object} React element
+     */
+    renderRoom(item, index) {
+        const isSelected = this.state.selectedRoom === index;
+        const couchIcon = item.isSuitesAvailable ?
+            <span className="suite-room" onClick={this.setSelectedRoom.bind(this, index)}>
+                <span className="icons icon-suite-white"></span>
+            </span> : null;
+
+        return (
+            <div className={item.main_room_class} key={index}>
+                <span className={item.room_class}>{item.room_no}</span>
+                <span className="room-type">{item.room_type_name}</span>
+                <div className="suites">
+                    {/* Clickable suite icon, clicking will open a popup with list of suit components */}
+                    { couchIcon }
+                    { isSelected ? <NightlyDiaryRoomsSuitTooltipComponent suites={item.suite_room_details} /> : null}
+                </div>
+            </div>
+        );
     }
 
     /**
@@ -33,57 +59,13 @@ class NightlyDiaryRoomsListItemComponent extends React.Component {
      * @return {[type]} [description]
      */
     render() {
-        const item = this.props.roomData;
-        const isSelected = this.state.showSuiteList;
-        const couchIcon = 
-            <span className="suite-room" onClick={this.showSuitRoomsList}>
-                <span className="icons icon-suite-white"></span>
-            </span>;
-
-        // const tooltipClassList = `suites-rooms ${condition ? 'top' : ''`;
-        const suitRoomsTooltip = 
-            <div className="suites-rooms top">
-                <strong>Suite Rooms</strong>
-                <div className="rooms">
-                    {
-                        item.suite_room_details.map((suiteItem) =>
-                        <span className="suite-room">
-                            <span className="icons icon-suite-white"></span>
-                            {suiteItem.room_no}
-                        </span>)
-                    }
-                </div>
-            </div>;
-
         return (
-            <div className={item.main_room_class}>
-                <span className={item.room_class}>{item.room_no}</span>
-                <span className="room-type">{item.room_type_name}</span>
-                <div className="suites">
-                    {/* Clickable suite icon, clicking will open a popup with list of suit components */}
-                    { item.isSuitesAvailable ? couchIcon : null}
-                    { isSelected ? suitRoomsTooltip : null}
-                </div>
+             <div className="grid-rooms">
+                { this.props.roomListToComponent.map(this.renderRoom.bind(this)) }
             </div>
         );
     }
 }
-
-NightlyDiaryRoomsListItemComponent.propTypes = {
-    roomData: PropTypes.object.isRequired
-};
-
-const NightlyDiaryRoomsListComponent = ({ roomListToComponent }) =>
-    <div className="grid-rooms">
-    {
-        roomListToComponent.map((item, i) =>
-            <NightlyDiaryRoomsListItemComponent roomData={item} key={i}/>
-        )
-    }
-    </div>;
-
-
-// const { PropTypes } = React;
 
 NightlyDiaryRoomsListComponent.propTypes = {
     roomListToComponent: PropTypes.array.isRequired
