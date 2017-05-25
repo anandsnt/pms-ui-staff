22dc7d33c5111d11d87cc24610ff8ff73876d414
Dilip Sahayadhas <dilips@stayntouch.com>
2017-05-25 09:09:23 -0400

    CICO-39531 Fixed eslint errors

--- rover/services/rvOWSTestSrv.js
+++ rover/services/rvOWSTestSrv.js
@@ -1,26 +1,26 @@
 angular.module('sntRover').service('RVOWSTestSrv', ['$http', '$q', '$window', function($http, $q, $window) {
 
-	this.checkOWSConnection = function(id) {
-		var deferred = $q.defer();
-		var url = '/admin/test_pms_connection';
+    this.checkOWSConnection = function() {
+        var deferred = $q.defer(),
+            url = '/admin/test_pms_connection';
 
-		$http.post(url).then(function(res) {
-			var response = res.data;
+        $http.post(url).then(function(res) {
+            var response = res.data;
 
-		    deferred.resolve(response.data);
+            deferred.resolve(response.data);
+        }, function(res) {
+            var response = res.data,
+                status = res.status;
 
-		}, function(res) {
-			var response = res.data,
-				status= res.status;
-
-			if (status === 401) { // 401- Unauthorized
-				// so lets redirect to login page
-				$window.location.href = '/house/logout' ;
-			} else {
-				deferred.reject(response);
-			}
-		});
-		return deferred.promise;
-	};
+            if (status === 401) {
+                // 401- Unauthorized
+                // so lets redirect to login page
+                $window.location.href = '/house/logout';
+            } else {
+                deferred.reject(response);
+            }
+        });
+        return deferred.promise;
+    };
 
 }]);
