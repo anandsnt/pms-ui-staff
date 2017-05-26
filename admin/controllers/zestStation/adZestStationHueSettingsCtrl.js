9b48d7ef2abed3725b893b30934e8a56a56038b8
resheilSNT <resheil@stayntouch.com>
2017-05-26 15:03:45 -0400

    CICO-41717 JS hue integration

--- admin/controllers/zestStation/adZestStationHueSettingsCtrl.js
+++ admin/controllers/zestStation/adZestStationHueSettingsCtrl.js
@@ -1,45 +1,162 @@
 admin.controller('adZestStationHueSettingsCtrl', ['$scope', '$rootScope', '$state', '$stateParams', 'ADZestStationSrv', 'kioskSettings',
 	function($scope, $state, $rootScope, $stateParams, ADZestStationSrv, kioskSettings) {
-		
+
 		BaseCtrl.call(this, $scope);
 		$scope.$emit('changedSelectedMenu', 10);
+		$scope.errorMessage = '';
+		$scope.successMessage = '';
+		$scope.hueSettings = kioskSettings;
 		$scope.availableBridges = []; // Have to manulay discover bridges
 		$scope.availableLights = []; // Have to manulay discover lights
 
-		$scope.hueSettings = kioskSettings.hueSettings;
+		var hue = jsHue(),
+			bridge,
+			user;
+
+		var runDigestCycle = function() {
+			if (!$scope.$$phase) {
+				$scope.$digest();
+			}
+		};
+		var scrollTop = function() {
+			$(".content-scroll").scrollTop(0);
+			$scope.$emit('hideLoader');
+			runDigestCycle();
+		};
+
 		$scope.saveSettings = function() {
-			var saveSuccess = function() {
+			$scope.invokeApi(ADZestStationSrv.save, {
+				'kiosk': $scope.hueSettings
+			}, function() {
 				$scope.successMessage = 'Success';
 				$scope.$emit('hideLoader');
+				scrollTop();
+			});
+		};
+
+		$scope.discoverBridges = function() {
+			hue.discover().then(function(bridges) {
+				if (bridges.length === 0) {
+					$scope.errorMessage = ['No bridges found.'];
+					scrollTop();
+				} else {
+					$scope.availableBridges = bridges;
+				};
+				runDigestCycle();
+			}).catch(function(e) {
+				$scope.errorMessage = ['Error finding bridges'];
+				scrollTop();
+				return;
+			});
+		};
+
+		var createBridge = function() {
+			try {
+				bridge = hue.bridge($scope.hueSettings.hue_bridge_ip)
+			} catch (e) {
+				$scope.errorMessage = ['Error finding bridges'];
+				scrollTop();
+				return;
 			};
-
-			var dataToSend = {
-				'kiosk': $scope.data
+		};
+		var createNewUser = function() {
+			try {
+				user = bridge.user($scope.hueSettings.hue_user_name)
+			} catch (e) {
+				$scope.errorMessage = ['Error finding bridges'];
+				scrollTop();
+				return;
 			};
-
-			$scope.invokeApi(ADZestStationSrv.save, dataToSend, saveSuccess, saveFailed);
 		};
 
+		var checkIfBridgeAndUserArePresent = function() {
 
-		$scope.discoverBridges =  function(){
-			var data = [{"id":"001788fffe100491","internalipaddress":"192.168.2.23","macaddress":"00:17:88:10:04:91","name":"Philips Hue"},{"id":"001788fffe09a168","internalipaddress":"192.168.88.252"},{"id":"001788fffe16c18f","internalipaddress":"192.168.2.20","macaddress":"00:17:88:16:c1:8f","name":"Philips Hue"}];
-			$scope.availableBridges =  data;
-		};
-
-		$scope.createNewUser  = function(){
-			$scope.hueSettings = {};
-			$scope.newUsername = "VFYBEUIN(@NNINVFYBEUIN(@NNINVFYBEUIN(@NNINVFYBEUIN(@NNIN";
-		};
-
-		$scope.getLightsList = function(){
-			var lightsData = {"1":{"state":{"on":true,"bri":144,"hue":13088,"sat":212,"xy":[0.5128,0.4147],"ct":467,"alert":"none","effect":"none","colormode":"xy","reachable":true},"type":"Extended color light","name":"Hue Lamp 1","modelid":"LCT001","swversion":"66009461","pointsymbol":{"1":"none","2":"none","3":"none","4":"none","5":"none","6":"none","7":"none","8":"none"}},"2":{"state":{"on":false,"bri":0,"hue":0,"sat":0,"xy":[0,0],"ct":0,"alert":"none","effect":"none","colormode":"hs","reachable":true},"type":"Extended color light","name":"Hue Lamp 2","modelid":"LCT001","swversion":"66009461","pointsymbol":{"1":"none","2":"none","3":"none","4":"none","5":"none","6":"none","7":"none","8":"none"}}}
-			$scope.availableLights = [];
-			for (var key in lightsData) {
-			  if (lightsData.hasOwnProperty(key)) {
-			    console.log(key + " -> " + lightsData[key].name +" "+ lightsData[key].type);
-			    $scope.availableLights.push({id:key,name:lightsData[key].name,type: lightsData[key].type});
-			  }
+			if (!bridge) {
+				createBridge();
 			}
+			if (bridge && !user) {
+				createNewUser();
+			}
+		};
+
+		$scope.createNewUser = function() {
+			if (!bridge) {
+				createBridge();
+			}
+			// create user account (requires link button to be pressed)
+			bridge.createUser('snt#app').then(function(data) {
+				// extract bridge-generated username from returned data
+				var username = data[0].success.username;
+				$scope.newUsername = username;
+				// instantiate user object with username
+				user = bridge.user(username);
+				runDigestCycle();
+			}).catch(function(e) {
+				$scope.errorMessage = ['Sorry, someting went wrong when creating new user'];
+				scrollTop();
+				return;
+			});
+		};
+
+
+
+		$scope.getLightsList = function() {
+			checkIfBridgeAndUserArePresent();
+			$scope.availableLights = [];
+			user.getLights().then(function(lightsData) {
+				for (var key in lightsData) {
+					if (lightsData.hasOwnProperty(key)) {
+						console.log(key + " -> " + lightsData[key].name + " " + lightsData[key].type);
+						$scope.availableLights.push({
+							id: key,
+							name: lightsData[key].name,
+							type: lightsData[key].type,
+							reachable: lightsData[key].state.reachable
+						});
+					}
+				}
+				runDigestCycle();
+			}).catch(function(e) {
+				$scope.errorMessage = ['Sorry, someting went wrong. Please check the lights connections'];
+				scrollTop();
+				return;
+			});
+		};
+
+		$scope.turnONLight = function() {
+			checkIfBridgeAndUserArePresent();
+			user.setLightState($scope.hueSettings.hue_test_light_id, {
+				on: true
+			}).then(function(data) {
+				if (data[0].error) {
+					$scope.errorMessage = ['Some thing went wrong while trying to turn ON Light with ID - ' + $scope.hueSettings.hue_test_light_id + '. Make sure this light is correctly connected and is reachable'];
+				} else {
+					$scope.successMessage = 'Light with ID - ' + $scope.hueSettings.hue_test_light_id + ' is turned ON';
+				}
+				scrollTop();
+			}).catch(function(e) {
+				$scope.errorMessage = ['Some thing went wrong while trying to turn ON Light with ID - ' + $scope.hueSettings.hue_test_light_id + '. Make sure this light is correctly connected and is reachable'];
+				scrollTop();
+				return;
+			});
+
+		};
+		$scope.turnOFFLight = function() {
+			checkIfBridgeAndUserArePresent();
+			user.setLightState($scope.hueSettings.hue_test_light_id, {
+				on: false
+			}).then(function(data) {
+				if (data[0].error) {
+					$scope.errorMessage = ['Some thing went wrong while trying to turn OFF Light with ID - ' + $scope.hueSettings.hue_test_light_id + '. Make sure this light is correctly connected and is reachable'];
+				} else {
+					$scope.successMessage = 'Light with ID - ' + $scope.hueSettings.hue_test_light_id + ' is turned OFF';
+				}
+				scrollTop();
+			}).catch(function(e) {
+				$scope.errorMessage = ['Some thing went wrong while trying to turn OFF Light with ID - ' + $scope.hueSettings.hue_test_light_id + '. Make sure this light is correctly connected and is reachable'];
+				scrollTop();
+				return;
+			});
 		};
 
 
