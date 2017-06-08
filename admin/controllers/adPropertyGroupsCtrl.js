6f96e1835f44bb5d26d50992f85785ebe8e0708c
soumyastayntouch <soumya@stayntouch.com>
2017-06-08 15:21:24 +0530

    CICO-40570: Controller, service and routing

---
+++ admin/controllers/adPropertyGroupsCtrl.js
@@ -0,0 +1,166 @@
+admin.controller('ADPropertyGroupsCtrl', ['$scope', '$stateParams', 'ADPropertyGroupsSrv', 'ngTableParams', '$filter', '$timeout', '$state', '$rootScope', '$location', '$anchorScroll',
+	function($scope, $stateParams, ADPropertyGroupsSrv, ngTableParams, $filter, $timeout, $state, $rootScope, $location, $anchorScroll) {
+
+		ADBaseTableCtrl.call(this, $scope, ngTableParams);
+		$scope.$emit("changedSelectedMenu", 11); //$stateParams.menu
+		$scope.currentClickedElement = -1;
+		$scope.isAdd = false;
+		$scope.isEdit = false;
+		$scope.prefetchData = {};
+		$scope.successMessage = "";
+
+		$scope.fetchTableData = function($defer, params) {
+			var getParams = $scope.calculateGetParams(params);
+			var fetchSuccessOfItemList = function(data) {
+				$scope.$emit('hideLoader');
+				
+				$scope.currentClickedElement = -1;
+				$scope.totalCount = data.total_count;
+				$scope.totalPage = Math.ceil(data.total_count / $scope.displyCount);
+				$scope.data = data.property_groups;
+				$scope.is_connected_to_pms = data.is_connected_to_pms;
+				$scope.currentPage = params.page();
+	        	params.total(data.total_count);
+	            $defer.resolve($scope.data);
+			};
+
+			$scope.invokeApi(ADPropertyGroupsSrv.fetch, getParams, fetchSuccessOfItemList);
+		};
+
+
+		$scope.loadTable = function() {
+			$scope.tableParams = new ngTableParams({
+			        page: 1,  // show first page
+			        count: $scope.displyCount, // count per page
+			        sorting: {
+			            property_group: 'asc' // initial sorting
+			        }
+			    }, {
+			        total: 0, // length of data
+			        getData: $scope.fetchTableData
+			    }
+			);
+		};
+
+		$scope.loadTable();
+
+		/*
+		 * To fetch the required details for add screen.
+		 */
+		$scope.addNewClicked = function() {
+
+
+			$scope.currentClickedElement = -1;
+			
+			$timeout(function() {
+	            $location.hash('new-form-holder');
+	            $anchorScroll();
+        	});
+			var fetchNewDetailsSuccessCallback = function(data) {
+				$scope.$emit('hideLoader');
+				$scope.isAdd = true;
+				$scope.prefetchData = {};
+				
+				$scope.prefetchData = data;
+			};
+
+			$scope.invokeApi(ADPropertyGroupsSrv.fetchAddData, {}, fetchNewDetailsSuccessCallback);
+		};
+
+		/*
+		 * To fetch the required details for edit screen.
+		 */
+		$scope.editSelected = function(index, value) {
+			$scope.isAdd = false;
+			$scope.editId = value;
+			var data = {
+				'editId': value
+			};
+
+			var editSuccessCallback = function(data) {
+				$scope.$emit('hideLoader');
+				$scope.currentClickedElement = index;
+				$scope.prefetchData = {};
+				$scope.prefetchData = data;
+				
+				$scope.isEdit = true;
+				$scope.isAdd = false;
+			};
+
+			$scope.invokeApi(ADPropertyGroupsSrv.fetchEditData, data, editSuccessCallback);
+		};
+
+		/*
+		 * To fetch the template for property group details add/edit screens
+		 */
+		$scope.getTemplateUrl = function() {
+
+			return "/assets/partials/propertygroups/adPostingGroupDetailsForm.html";
+		};
+
+		/*
+		 * To handle delete button click.
+		 */
+		$scope.deleteItem = function(value) {
+			var deleteSuccessCallback = function(data) {
+				$scope.$emit('hideLoader');
+				angular.forEach($scope.data.property_groups, function(item, index) {
+					if (item.value === value) {
+						$scope.data.property_groups.splice(index, 1);
+					}
+				});
+				$scope.tableParams.reload();
+			};
+			var data = {
+				'value': value
+			};
+
+			$scope.invokeApi(ADPropertyGroupsSrv.deleteItem, data, deleteSuccessCallback);
+		};
+
+		/*
+		 * To handle save button click.
+		 */
+		$scope.clickedSave = function() {
+			var saveSuccessCallback = function(data) {
+				$scope.$emit('hideLoader');
+				if ($scope.isEdit) {
+                                    var p = parseInt($scope.currentClickedElement);
+
+                                    if ($scope.orderedData) {
+                                    if ($scope.orderedData[p]) {
+					$scope.orderedData[parseInt($scope.currentClickedElement)].property_group = data.property_group;
+                                    }
+                                    }
+
+				} else {
+					$scope.data.push(data);
+					$scope.tableParams.reload();
+				}
+
+				$scope.currentClickedElement = -1;
+				if ($scope.isAdd) {
+					$scope.isAdd = false;
+				}
+				if ($scope.isEdit) {
+					$scope.isEdit = false;
+				}
+				$scope.successMessage = 'Success!';
+			};
+            
+			var postData = $scope.prefetchData;
+			$scope.invokeApi(ADPropertyGroupsSrv.save, postData, saveSuccessCallback);
+		};
+
+		/*
+		 * To handle cancel button click.
+		 */
+		$scope.clickedCancel = function() {
+			if ($scope.isAdd) {
+				$scope.isAdd = false;
+			}
+			if ($scope.isEdit) {
+				$scope.isEdit = false;
+			}
+		};
+	}]);
---
+++ admin/services/adPropertyGroupsSrv.js
@@ -0,0 +1,74 @@
+admin.service('ADPropertyGroupsSrv', ['$http', '$q', 'ADBaseWebSrv', function($http, $q, ADBaseWebSrv) {
+	
+	/**
+    *   A getter method to return the property groups list
+    */
+	this.fetch = function(params) {
+		var deferred = $q.defer();
+		var url = '/admin/property_groups/list.json';
+
+		ADBaseWebSrv.getJSON(url, params).then(function(data) {
+		    deferred.resolve(data);
+		}, function(data) {
+		    deferred.reject(data);
+		});
+		return deferred.promise;
+	};
+
+	/**
+    *   A delete method to delete the property group.
+    */
+	this.deleteItem = function(data) {
+		var deferred = $q.defer();
+		var url = '/admin/property_groups/' + data.value + '/delete';
+
+		ADBaseWebSrv.getJSON(url, data).then(function(data) {
+		    deferred.resolve(data);
+		}, function(data) {
+		    deferred.reject(data);
+		});
+		return deferred.promise;
+	};
+	/**
+    *   A getter method to return add new the property group data.
+    */
+	this.fetchAddData = function() {
+		var deferred = $q.defer();
+		var url = '/admin/property_groups/new';
+
+		ADBaseWebSrv.getJSON(url).then(function(data) {
+		    deferred.resolve(data);
+		}, function(data) {
+		    deferred.reject(data);
+		});
+		return deferred.promise;
+	};
+	/**
+    *   A getter method to return add new the property group's data.
+    */
+	this.fetchEditData = function(data) {
+		var deferred = $q.defer();
+		var url = '/admin/property_groups/' + data.editId + '/edit.json';
+
+		ADBaseWebSrv.getJSON(url).then(function(data) {
+		    deferred.resolve(data);
+		}, function(data) {
+		    deferred.reject(data);
+		});
+		return deferred.promise;
+	};
+	/**
+    *   A post method to add/edit the property groups data.
+    */
+	this.save = function(data) {
+		var deferred = $q.defer();
+		var url = '/admin/property_groups/save';
+
+		ADBaseWebSrv.postJSON(url, data).then(function(data) {
+		    deferred.resolve(data);
+		}, function(data) {
+		    deferred.reject(data);
+		});
+		return deferred.promise;
+	};
+}]);
