4c79586fc779ac033e4a1c35222736826c0cd0dd
MubarakRocky <mubarak@stayntouch.com>
2017-06-27 15:32:04 -0400

    CICO-23943 Fixing issues with rubocop

--- admin/controllers/emvTerminal/adEmvTerminalDetailsCtrl.js
+++ admin/controllers/emvTerminal/adEmvTerminalDetailsCtrl.js
@@ -13,7 +13,7 @@ admin.controller('ADEmvTerminalDetailsCtrl', ['$scope', '$rootScope', 'ADEmvTerm
 	$scope.itemDetails.name = '';
 	$scope.itemDetails.terminal_identifier = '';
 	$scope.itemDetails.terminal_access_code = '';
-	$scope.mli_emv_enabled = $rootScope.mli_emv_enabled;
+	$scope.mliEmvEnabled = $rootScope.mliEmvEnabled;
 
 	var itemId = $stateParams.itemid;
 	// if itemid is null, means it is for add item form
