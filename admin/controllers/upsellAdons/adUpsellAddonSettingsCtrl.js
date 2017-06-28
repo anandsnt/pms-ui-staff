5dbeb972d691d6735c339da40f19845c108553d7
resheilSNT <resheil@stayntouch.com>
2017-06-28 10:26:08 -0400

    CICO-41309 UI changes

--- admin/controllers/upsellAdons/adUpsellAddonSettingsCtrl.js
+++ admin/controllers/upsellAdons/adUpsellAddonSettingsCtrl.js
@@ -6,31 +6,8 @@ admin.controller('adUpsellAddonSettingsCtrl', function($scope, ADUpsellAddonSrv,
 		blankPostTypes = [],
 		selectedLanguageTranslationLanguageId = '';
 
-	(function() {
-		var selectedLanguage = _.find(availableLanguages.languages, function(language) {
-			return language.code === 'en';
-		});
-		$scope.selectedLanguageTranslations = _.find(upsellData.translations, function(translation) {
-			return translation.language_id === selectedLanguage.id;
-		});
-		selectedLanguageTranslationLanguageId = selectedLanguage.id;
-
-		// check if translations were already added.
-		if (!_.isUndefined($scope.selectedLanguageTranslations)) {
-			$scope.selectedLanguageTranslationId = $scope.selectedLanguageTranslations.id;
-			$scope.translated_amount_types = $scope.selectedLanguageTranslations.amount_types;
-			$scope.translated_post_types = $scope.selectedLanguageTranslations.post_types;
-		} else {
-			// if no previously added translations, set as blank data initialy
-			$scope.translated_amount_types = angular.copy(blankAmountTypes);
-			$scope.translated_post_types = angular.copy(blankPostTypes);
-		}
-	})();
-
 
 	$scope.onLanguageChange = function() {
-		// if ($scope.selectedLanguage.code !== 'en') {
-		// if language is not english
 		var selectedLanguage = _.find($scope.availableLanguagesSet.locales, function(language) {
 			return language.value === $scope.selectedLanguage.code;
 		});
@@ -49,23 +26,14 @@ admin.controller('adUpsellAddonSettingsCtrl', function($scope, ADUpsellAddonSrv,
 			$scope.translated_amount_types = angular.copy(blankAmountTypes);
 			$scope.translated_post_types = angular.copy(blankPostTypes);
 		}
-		// } else {
-		// 	return;
-		// }
 	};
 
 
 	var returnDataForSaving = function() {
-		var data;
-
-		// if ($scope.selectedLanguage.code === 'en') {
-		// 	data = angular.copy($scope.data);
-		// 	delete data.translations;
-		// } else {
-		data = angular.copy($scope.data);
+		var data = angular.copy($scope.data);
 		delete data.translations;
-		delete data.amount_types;
 		delete data.post_types;
+		delete data.amount_types;
 		data.translation = [{
 			language_id: selectedLanguageTranslationLanguageId,
 			amount_types: $scope.translated_amount_types,
@@ -81,7 +49,6 @@ admin.controller('adUpsellAddonSettingsCtrl', function($scope, ADUpsellAddonSrv,
 		if (!_.isUndefined($scope.selectedLanguageTranslations)) {
 			data.translation[0].id = $scope.selectedLanguageTranslationId;
 		}
-		// }
 		return data;
 	};
 
@@ -138,26 +105,25 @@ admin.controller('adUpsellAddonSettingsCtrl', function($scope, ADUpsellAddonSrv,
 			return translation.language_id === selectedLanguage.id;
 		});
 
+		// set up blank labels for future usage in case of newly added language
+		_.each(angular.copy(upsellData.amount_types), function(amountType) {
+			blankAmountTypes.push({
+				'description': amountType.description,
+				'id': amountType.id,
+				'label': amountType.description
+			});
+		});
+		_.each(angular.copy(upsellData.post_types), function(postType) {
+			blankPostTypes.push({
+				'description': postType.description,
+				'id': postType.id,
+				'label': postType.description
+			});
+		});
 
 		// check if translations were already added.
 		if (!_.isUndefined($scope.selectedLanguageTranslations)) {
 			selectedLanguageTranslationLanguageId = selectedLanguage.id;
-
-			// set up blank labels for future usage in case of newly added language
-			_.each(angular.copy($scope.selectedLanguageTranslations.amount_types), function(amountType) {
-				blankAmountTypes.push({
-					'description': amountType.description,
-					'id': amountType.id,
-					'label': ''
-				});
-			});
-			_.each(angular.copy($scope.selectedLanguageTranslations.post_types), function(postType) {
-				blankPostTypes.push({
-					'description': postType.description,
-					'id': postType.id,
-					'label': ''
-				});
-			});
 			$scope.selectedLanguageTranslationId = $scope.selectedLanguageTranslations.id;
 			$scope.translated_amount_types = $scope.selectedLanguageTranslations.amount_types;
 			$scope.translated_post_types = $scope.selectedLanguageTranslations.post_types;
