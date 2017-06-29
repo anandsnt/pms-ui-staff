d0a8fd6a8cdaa68c1a5aba64f4f1063ac52a362d
Aswanth <aswanth@stayntouch.com>
2017-06-29 18:50:36 +0530

    CICO-41808 updated the zest station mappings for cachet bottique

--- zest_station/services_v2/zsGeneralSrv.js
+++ zest_station/services_v2/zsGeneralSrv.js
@@ -35,7 +35,8 @@ sntZestStation.service('zsGeneralSrv', ['$http', '$q', 'zsBaseWebSrv', 'zsBaseWe
             'de-jonker': 'Hotel de jonker',
             'chalet-view': 'Chalet view',
             'row-nyc': 'row',
-            'circle-inn-fairfield': 'Circle fairfield'
+            'circle-inn-fairfield': 'Circle fairfield',
+            'cachet-boutique': 'Cachet Boutique'
         };
 
         this.isThemeConfigured = function(theme) {
