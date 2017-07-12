5102734155a71778c6a8fbd8697d839f5e37d7a2
Mike <mike@stayntouch.com>
2017-07-12 12:57:14 -0400

    code climate update

--- zest_station/controllers_v2/zsBaseCtrl.js
+++ zest_station/controllers_v2/zsBaseCtrl.js
@@ -349,7 +349,7 @@ function BaseCtrl($scope) {
         // console.log('calling track event: ', 'trackAnalyticEvent', arguments);
         var zs = {};
 
-        if ($scope && $scope.$parent || $scope.zestStationData) {
+        if (($scope && $scope.$parent) || ($scope.zestStationData)) {
             zs = $scope.$parent.zestStationData ? $scope.$parent.zestStationData : $scope.zestStationData;
             if (zs) {
                 if (event_type === 'status_update') {
