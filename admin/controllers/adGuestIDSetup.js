4562fa9f06a3db171e8ae08dd5178c9e43803bb3
resheilSNT <resheil@stayntouch.com>
2017-07-18 15:01:17 -0400

    CICO-40165 updating with develop

--- admin/controllers/adGuestIDSetup.js
+++ admin/controllers/adGuestIDSetup.js
@@ -1,33 +1,35 @@
 admin.controller('ADGuestIDSetup', ['$scope', '$state', 'ADGuestIDSetupSrv', function($scope, $state, ADGuestIDSetupSrv) {
 
-	/*
-	* controller class for guest review setup
-	*/
+    /*
+    * controller class for guest review setup
+    */
 
     BaseCtrl.call(this, $scope);
     $scope.errorMessage = '';
 
-	/*
-	* success call back of details web service call
-	*/
-    var fetchCompletedOfSettingsDetails = function(data) {
-        $scope.$emit('hideLoader');
-        $scope.data = data.guest_id_scan;
-    };
-
-	// fetching the settings details
-    $scope.invokeApi(ADGuestIDSetupSrv.fetchGuestIDSetupDetails, {}, fetchCompletedOfSettingsDetails);
+    /*
+    * success call back of details web service call
+    */
     var fetchCompletedOfIDTypes = function(data) {
+        $scope.$emit('hideLoader');
         $scope.data.id_types = data.id_types;
         if (!data.id_types) {
             $scope.data.id_types = [];// passport ID type
         }
     };
+    
+    var fetchCompletedOfSettingsDetails = function(data) {
+        $scope.data = data.guest_id_scan;
 
-    $scope.invokeApi(ADGuestIDSetupSrv.fetchGuestIDTypeDetails, {}, fetchCompletedOfIDTypes);
-	/*
-	* success call back of details web service call
-	*/
+        $scope.invokeApi(ADGuestIDSetupSrv.fetchGuestIDTypeDetails, {}, fetchCompletedOfIDTypes);
+    };
+
+    // fetching the settings details
+    $scope.invokeApi(ADGuestIDSetupSrv.fetchGuestIDSetupDetails, {}, fetchCompletedOfSettingsDetails);
+
+    /*
+    * success call back of details web service call
+    */
     var successCallbackOfSaveDetails = function() {
         $scope.$emit('hideLoader');
         $scope.errorMessage = '';
@@ -51,7 +53,7 @@ admin.controller('ADGuestIDSetup', ['$scope', '$state', 'ADGuestIDSetupSrv', fun
 
         postingData.guest_id_scan.scan_guest_id_active = $scope.data.scan_guest_id_active;
 
-		// calling the save api
+        // calling the save api
         $scope.invokeApi(ADGuestIDSetupSrv.saveGuestIDSetup, postingData, successCallbackOfSaveDetails);
 
     };
