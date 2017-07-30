846442c1046bc9f41e9bd5eec4fedb69e4967b8d
resheilSNT <resheil@stayntouch.com>
2017-07-30 10:41:52 -0400

    Implementing preview

--- admin/controllers/zestEmailTemplates/adZestEmailGeneralSettingsCtrl.js
+++ admin/controllers/zestEmailTemplates/adZestEmailGeneralSettingsCtrl.js
@@ -1,6 +1,22 @@
-admin.controller('ADZestEmailGeneralSettings', ['$scope', '$state','generalSettings',
-    function($scope, $state,generalSettings) {
+admin.controller('ADZestEmailGeneralSettings', ['$scope', '$state','generalSettings','ngDialog',
+    function($scope, $state,generalSettings,ngDialog) {
         console.log(generalSettings);
         $scope.generalSettings = generalSettings;
+        $scope.currentYear = new Date().getFullYear();
+        $scope.email_text_1 = "This is a sample text for testing";
+        $scope.email_text_2 = "This is a sample text for testing";
+        $scope.button_text = "Button sample text";
+
+        $scope.previewClicked = function(){
+            ngDialog.open({
+                // controller: $controller('adZestWebPreviewCtrl', {
+                //     $scope: $scope
+                // }),
+                closeByDocument: true,
+                template: '/assets/partials/zestEmailTemplates/adzestMailPreview.html',
+                className: 'ngdialog-theme-default ipad-preview',
+                scope:$scope
+            });
+        };
     }
 ]);
