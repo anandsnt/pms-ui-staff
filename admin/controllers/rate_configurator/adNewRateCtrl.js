5d238a7034662c9defd341d12d410f051c499300
krishobh <krishobh@stayntouch.com>
2017-07-31 12:32:18 +0530

    CICO-41778 reverted cc

--- admin/controllers/rate_configurator/adNewRateCtrl.js
+++ admin/controllers/rate_configurator/adNewRateCtrl.js
@@ -432,14 +432,5 @@ admin.controller('ADAddnewRate', ['$scope', 'ADRatesRangeSrv', 'ADRatesSrv', '$s
          * init call
          */
         $scope.init();
-        var a=10;
-        if(a){
-            console.log(a);
-        }
-        var x = function(){
-
-        };
-
-
     }
 ]);
