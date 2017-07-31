70595de69a742ec3c18b9cc0f8f65fb1181702b1
resheilSNT <resheil@stayntouch.com>
2017-07-30 20:10:17 -0400

    style fixes

--- admin/controllers/zestEmailTemplates/adZestEmailGeneralSettingsCtrl.js
+++ admin/controllers/zestEmailTemplates/adZestEmailGeneralSettingsCtrl.js
@@ -3,8 +3,8 @@ admin.controller('ADZestEmailGeneralSettings', ['$scope', '$state','generalSetti
         console.log(generalSettings);
         $scope.generalSettings = generalSettings;
         $scope.currentYear = new Date().getFullYear();
-        $scope.email_text_1 = "This is a sample text for testing";
-        $scope.email_text_2 = "This is a sample text for testing";
+        $scope.email_text_1 = "This is a sample text for testing. You can enter the required texts in the individual settings for each process.";
+        $scope.email_text_2 = "This is a sample text for testing. You can enter the required texts in the individual settings for each process.";
         $scope.button_text = "Button sample text";
 
         $scope.previewClicked = function(){
