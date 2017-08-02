1c919cd78e78355a50f578036b1561accd6b2c43
resheilSNT <resheil@stayntouch.com>
2017-08-02 14:22:52 -0400

    minor change

--- admin/controllers/zestEmailTemplates/adZestEmailGeneralSettingsCtrl.js
+++ admin/controllers/zestEmailTemplates/adZestEmailGeneralSettingsCtrl.js
@@ -2,12 +2,18 @@ admin.controller('ADZestEmailGeneralSettingsCtrl', ['$scope', '$state','generalS
     function($scope, $state,generalSettings,ngDialog) {
         console.log(generalSettings);
         $scope.generalSettings = generalSettings;
+        var main_bg_image = angular.copy(generalSettings.main_bg_image);
         $scope.currentYear = new Date().getFullYear();
         $scope.email_text_1 = "This is a sample text for testing. You can enter the required texts in the individual settings for each process.";
         $scope.email_text_2 = "This is a sample text for testing. You can enter the required texts in the individual settings for each process.";
         $scope.button_text = "Button sample text";
 
         $scope.previewClicked = function(){
+            if ($scope.generalSettings.main_bg_image.length > 0) {
+                $scope.main_bg_style = "background-image:url(" + $scope.generalSettings.main_bg_image + ");";
+            } else {
+                $scope.main_bg_style = 'background:' + $scope.generalSettings.main_bg + ';';
+            }
             ngDialog.open({
                 // controller: $controller('adZestWebPreviewCtrl', {
                 //     $scope: $scope
