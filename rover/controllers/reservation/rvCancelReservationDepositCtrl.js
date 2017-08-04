3a3f079fcebe1a58d6e040765b7e267b195ea375
soumyastayntouch <soumya@stayntouch.com>
2017-08-04 15:23:05 +0530

    CICO-39609: Fixed issue with cancelled reservation in diary

--- rover/controllers/reservation/rvCancelReservationDepositCtrl.js
+++ rover/controllers/reservation/rvCancelReservationDepositCtrl.js
@@ -1,5 +1,5 @@
-sntRover.controller('RVCancelReservationDepositController', ['$rootScope', '$scope', 'ngDialog', '$stateParams', '$state', 'RVReservationCardSrv', 'RVPaymentSrv', '$timeout', '$filter',
-	function($rootScope, $scope, ngDialog, $stateParams, $state, RVReservationCardSrv, RVPaymentSrv, $timeout, $filter) {
+sntRover.controller('RVCancelReservationDepositController', ['$rootScope', '$scope', 'ngDialog', '$stateParams', '$state', 'RVReservationCardSrv', 'RVPaymentSrv', '$timeout', '$filter', 'RVNightlyDiarySrv',
+	function($rootScope, $scope, ngDialog, $stateParams, $state, RVReservationCardSrv, RVPaymentSrv, $timeout, $filter, RVNightlyDiarySrv) {
 
 		BaseCtrl.call(this, $scope);
 		$scope.errorMessage = "";
@@ -29,6 +29,17 @@ sntRover.controller('RVCancelReservationDepositController', ['$rootScope', '$sco
 				// Since RVCancelReservation and RVCancelReservationDepositController do the same above,
 				// its functions are written in parent controller.Ie reservationActionsController
 				$scope.DailogeState.isCancelled = true ;
+
+				var params = RVNightlyDiarySrv.getCache();
+				if(typeof params !== 'undefined'){
+					params.currentSelectedReservationId = "";
+	                params.currentSelectedRoomId = "";
+	                params.currentSelectedReservation = "";
+				}
+                
+
+                RVNightlyDiarySrv.updateCache(params);
+
 				$scope.$emit('hideLoader');
 			};
 
