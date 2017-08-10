58a5d22c75f61c1cdc43579d85e5f10b226cef72
Sudeep G <sudeep@stayntouch.com>
2017-08-10 15:38:31 +0530

    Revert "Zest Web Templates: CICO-35113"

--- asset_list/theming/guestweb/css/css_theme_mapping.js
+++ asset_list/theming/guestweb/css/css_theme_mapping.js
@@ -129,8 +129,7 @@ module.exports = {
             'guestweb_one_washington_circle': [ themeCSSRoot + 'guestweb_one_washington_circle.css'],
             'guestweb_first_hotel_breiseth': [ themeCSSRoot + 'guestweb_first_hotel_breiseth.css'],
             'guestweb_hotel_blackhawk': [ themeCSSRoot + 'guestweb_hotel_blackhawk.css'],
-            'guestweb_viceroy_chicago': [ themeCSSRoot + 'guestweb_viceroy_chicago.css'],
-            'guestweb_hotel_alessandra': [ themeCSSRoot + 'guestweb_hotel_alessandra.css']
+            'guestweb_viceroy_chicago': [ themeCSSRoot + 'guestweb_viceroy_chicago.css']
         };
     }
 };
