34e45a9c0273cf084a03fc1007099c51f27a40f5
Mike <mike@stayntouch.com>
2017-08-14 15:10:00 -0400

    code climate

--- shared/directives/tooltip/qtip2.js
+++ shared/directives/tooltip/qtip2.js
@@ -8,7 +8,7 @@ angular.module('qtip2', [])
           qtipClass = attrs.class || 'qtip-tipsy',
           content,
           htmlString,
-          category,// variable to handle dynamic content tooltip( for eg: dateRange, rateType) - this should be passed as element attr
+          category, // variable to handle dynamic content tooltip( for eg: dateRange, rateType) - this should be passed as element attr
           fetchURL; 
 
         if (attrs.title) {
