7890284a2687ac881cc20cd041748343cbd98f65
krishobh <krishobh@stayntouch.com>
2017-08-21 21:31:34 +0530

    CICO-44249 Basic api call setup added

--- rover/controllers/arTransactions/rvCardArTransactionsMainCtrl.js
+++ rover/controllers/arTransactions/rvCardArTransactionsMainCtrl.js
@@ -4,9 +4,46 @@ sntRover.controller('RVCompanyCardArTransactionsMainCtrl', ['$scope', '$rootScop
 
 		BaseCtrl.call(this, $scope);
 		$scope.errorMessage = '';
-		$scope.arFlags = {};
-		$scope.arFlags.currentSelectedArTab = 'balance';
-		$scope.arFlags.isAddBalanceScreenVisible = false;
+
+		$scope.arFlags = {
+			'currentSelectedArTab': 'balance',
+			'isAddBalanceScreenVisible': false
+		};
+		
+		$scope.arDataObj = {
+			'balanceList': [],
+			'paidList': [],
+			'unallocatedList': [],
+			'allocatedList': [],
+
+			'unpaidAmount': '',
+			'paidAmount': '',
+			'allocatedCredit': '',
+			'unallocatedCredit': ''
+		};
+
+		var successCallbackOfAPIcall = function( data ) {
+
+			$scope.arDataObj.unpaidAmount = data.unpaid_amount;
+			$scope.arDataObj.paidAmount = data.unpaid_amount;
+			$scope.arDataObj.allocatedCredit = data.allocated_credit;
+			$scope.arDataObj.unallocatedCredit = data.unallocated_credit;
+
+			switch($scope.arFlags.currentSelectedArTab) {
+			    case 'balance':
+			        $scope.arDataObj.balanceList = data.ar_transactions;
+			        break;
+			    case 'paid-bills':
+			        $scope.arDataObj.paidList = data.ar_transactions;
+			        break;
+			    case 'unallocated':
+			        $scope.arDataObj.unallocatedList = data.ar_transactions;
+			        break;
+			    case 'allocated':
+			        $scope.arDataObj.allocatedList = data.ar_transactions;
+			        break;
+			}
+		};
 
 		$rootScope.$on("arTransactionTabActive", function(event) {
 			console.log("----------------")
