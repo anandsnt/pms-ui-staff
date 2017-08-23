8496f34d164daeb48281d3cbdcc80673b2485f25
faseela <faseela@stayntouch.com>
2017-08-23 21:30:29 +0530

    Added changes in related areas

---
+++ rover/controllers/arTransactions/rvArBalanceCtrl.js
@@ -0,0 +1,9 @@
+
+sntRover.controller('RvArBalanceController', ['$scope', '$rootScope', 'RVCompanyCardSrv', '$timeout', '$stateParams', 'ngDialog', '$state', '$vault', '$window', 'RVReservationCardSrv', '$filter',
+	function($scope, $rootScope, RVCompanyCardSrv, $timeout, $stateParams, ngDialog, $state, $vault, $window, RVReservationCardSrv, $filter) {
+
+		BaseCtrl.call(this, $scope);
+
+		
+		
+}]);
---
+++ rover/controllers/arTransactions/rvCardArTransactionsMainCtrl.js
@@ -0,0 +1,33 @@
+
+sntRover.controller('RVCompanyCardArTransactionsMainCtrl', ['$scope', '$rootScope', 'RVCompanyCardSrv', '$timeout', '$stateParams', 'ngDialog', '$state', '$vault', '$window', 'RVReservationCardSrv', '$filter',
+	function($scope, $rootScope, RVCompanyCardSrv, $timeout, $stateParams, ngDialog, $state, $vault, $window, RVReservationCardSrv, $filter) {
+
+		BaseCtrl.call(this, $scope);
+		$scope.errorMessage = '';
+		$scope.arFlags = {};
+		$scope.arFlags.currentSelectedArTab = 'balance';
+		$scope.arFlags.isAddBalanceScreenVisible = false;
+
+		$rootScope.$on("arTransactionTabActive", function(event) {
+			console.log("----------------")
+			// refreshArTabScroller();
+		});
+
+		$scope.switchArTransactionTab = function(tab) {
+			$scope.arFlags.currentSelectedArTab = tab;
+			if (tab !== 'balance') {
+				$scope.arFlags.isAddBalanceScreenVisible = false;
+			}
+		};	
+		
+		$scope.showAddBalanceScreen = function () {
+			$scope.arFlags.isAddBalanceScreenVisible = true;
+		};	
+
+		var init = function() {
+			console.log("--init")
+		}
+
+		init();
+		
+}]);
