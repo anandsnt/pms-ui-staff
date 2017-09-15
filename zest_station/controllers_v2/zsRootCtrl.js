77c506f719ec588a45f802403aafe2aad87427fa
resheilSNT <resheil@stayntouch.com>
2017-09-15 12:04:04 -0400

    handling undefined full names

--- zest_station/controllers_v2/zsRootCtrl.js
+++ zest_station/controllers_v2/zsRootCtrl.js
@@ -36,6 +36,10 @@ sntZestStation.controller('zsRootCtrl', [
         // in order to prevent url change or fresh url entering with states
         BaseCtrl.call(this, $scope);
 
+        $scope.isUndefined = function(thing) {
+            return (typeof thing === "undefined");
+        }
+
         $scope.cssMappings = cssMappings;
         $scope.inElectron = false;
 
