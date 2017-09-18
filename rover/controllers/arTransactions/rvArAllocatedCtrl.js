e822c775ce1cd1495537efe24ef6087e54ce1487
krishobh <krishobh@stayntouch.com>
2017-09-18 19:45:29 +0530

    CICO-44250 Remove arrival icon and conf number label from Manual Balance entry

--- rover/controllers/arTransactions/rvArAllocatedCtrl.js
+++ rover/controllers/arTransactions/rvArAllocatedCtrl.js
@@ -27,6 +27,7 @@ sntRover.controller('RvArAllocatedController',
             var successCallbackOfExpansionAPI = function( data ) {
                 sntActivity.stop('EXPAND_ALLOCATED');
                 item.transactions = data.allocated_transactions;
+                item.isExpandedByManualBalance = item.is_manual_balance;
                 item.active = true;
                 refreshScroll();
             },
--- rover/controllers/arTransactions/rvArUnallocatedCtrl.js
+++ rover/controllers/arTransactions/rvArUnallocatedCtrl.js
@@ -27,6 +27,7 @@ sntRover.controller('RvArUnallocatedController',
             var successCallbackOfExpansionAPI = function( data ) {
                 sntActivity.stop('EXPAND_UNALLOCATED');
                 item.transactions = data.allocated_transactions;
+                item.isExpandedByManualBalance = item.is_manual_balance;
                 item.active = true;
                 refreshScroll();
             },
