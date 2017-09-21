f1e3d278630383586b6f56b65203946685c780c0
krishobh <krishobh@stayntouch.com>
2017-09-21 20:19:24 +0530

    CICO-45436 Handled disabling Ar transaction based on new flag

--- rover/controllers/arTransactions/rvCardArTransactionsMainCtrl.js
+++ rover/controllers/arTransactions/rvCardArTransactionsMainCtrl.js
@@ -9,7 +9,8 @@ sntRover.controller('RVCompanyCardArTransactionsMainCtrl',
 	'RVReservationCardSrv',
 	'$window',
     '$filter',
-	function($scope, $rootScope, $stateParams, ngDialog, $timeout, rvAccountsArTransactionsSrv, RVReservationCardSrv, $window, $filter) {
+    'rvPermissionSrv',
+	function($scope, $rootScope, $stateParams, ngDialog, $timeout, rvAccountsArTransactionsSrv, RVReservationCardSrv, $window, $filter, rvPermissionSrv) {
 		BaseCtrl.call(this, $scope);
 		$scope.errorMessage = '';
 
@@ -21,7 +22,8 @@ sntRover.controller('RVCompanyCardArTransactionsMainCtrl',
 			'viewFromOutside': (typeof $stateParams.type !== 'undefined') ? true : false,
 			'shouldShowPayAllButton': false,
 			'shouldShowFooter': false,
-			'insufficientAmount': false
+			'insufficientAmount': false,
+			'isArSynced': false
 		};
 
 		$scope.filterData = {
@@ -127,6 +129,15 @@ sntRover.controller('RVCompanyCardArTransactionsMainCtrl',
 			$scope.arDataObj.allocatedCredit = data.allocated_credit;
 			$scope.arDataObj.unallocatedCredit = data.unallocated_credit;
 			$scope.arDataObj.company_or_ta_bill_id = data.company_or_ta_bill_id;
+			$scope.arFlags.isArSynced = data.is_ar_synced && false;
+			
+	        // CICO-45436 : To be removed 
+	        if ( !$scope.arFlags.isArSynced ) {
+	        	$scope.errorMessage = ['You are not able to do further activity in AR tab. Please sync existing AR transactions for this hotel chain.'];
+	        }
+	        else {
+	        	$scope.errorMessage = '';
+	        }
 
 			switch ($scope.arFlags.currentSelectedArTab) {
 			    case 'balance':
@@ -754,4 +765,14 @@ sntRover.controller('RVCompanyCardArTransactionsMainCtrl',
 
             $scope.callAPI(rvAccountsArTransactionsSrv.emailArStatement, options);
         };
+
+        /**
+		* function to check whether the user has permission
+		* to create/edit AR Account.
+		* @return {Boolean}
+		*/
+		$scope.hasPermissionToCreateArAccount = function() {
+			return ( rvPermissionSrv.getPermissionValue ('CREATE_AR_ACCOUNT') && $scope.arFlags.isArSynced );
+		};
+
 }]);
