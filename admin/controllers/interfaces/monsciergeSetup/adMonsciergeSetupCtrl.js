fc4754b2a99233ce71957de2182678c33655a017
Aswanth <aswanth@stayntouch.com>
2017-09-21 18:41:50 +0530

    CICO-45438 added UI changes

---
+++ admin/controllers/interfaces/monsciergeSetup/adMonsciergeSetupCtrl.js
@@ -0,0 +1,35 @@
+admin.controller('adMonsciergeSetupCtrl', ['$scope', 'adMonsciergeSetupSrv', 'ngTableParams', '$filter',
+	function($scope, adMonsciergeSetupSrv, ngTableParams, $filter) {
+		BaseCtrl.call(this, $scope);
+
+		$scope.saveSettings = function() {
+			var onSaveSettingsSucces = function() {
+					$scope.successMessage = 'Success, Your settings has been saved.';
+				},
+				options = {
+					params: { 'monscierge':$scope.data },
+					successCallBack: onSaveSettingsSucces
+				};
+
+			$scope.callAPI(adMonsciergeSetupSrv.saveSettings, options);
+		};
+
+		$scope.fetchSettings = function() {
+			var onFetchSettingsSucces = function(data) {
+					$scope.data = data.monscierge;
+				},
+				options = {
+					successCallBack: onFetchSettingsSucces
+				};
+
+			$scope.callAPI(adMonsciergeSetupSrv.getSettings, options);
+		};
+
+		(function init() {
+			$scope.errorMessage = '';
+			$scope.successMessage = '';
+			$scope.fetchSettings();
+		}());
+
+	}
+]);
