96648c3c42002992ba8b59ab6292d9bf54da827c
rahulkookal <rahul@stayntouch.com>
2017-11-16 11:36:20 +0530

    CICO-46678: Intermediate commit - update on directive.

--- rover/directives/fullscreen/rvFullScreenDir.js
+++ rover/directives/fullscreen/rvFullScreenDir.js
@@ -2,23 +2,22 @@
  * Created by rahulkookal on 15/11/17.
  */
 sntRover.directive('rvFullscreen', [
-    '$document',
-    function($document) {
-
+    '$rootScope',
+    function($rootScope) {
         return {
             restrict: 'A',
             scope: {
-                test: '=pageOptions'
+                scroll: '@scroll'
             },
             link: function(scope, element, attrs) {
-                console.log(scope);
-                console.log(scope.$parent.getScroller());
                 element.on('click', function(e) {
                     scope.$apply(function() {
-                        var myEl = angular.element(document.querySelector('body'));
-                        myEl.addClass('is-fullscreen fullscreen-card');
+                        var myEl = angular.element(document.querySelector('body'))[0];
+                        myEl.classList.toggle("is-fullscreen");
+                        myEl.classList.toggle("fullscreen-card");
                     });
                     e.stopPropagation(); //stop event from bubbling up to document object
+                    $rootScope.$broadcast(scope.scroll);
                 });
             }
         };
