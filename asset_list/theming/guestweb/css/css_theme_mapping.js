53b58b6a0e2d2ddd3b0e15ca85bb00f1d5319dab
rahulkookal <rahul@stayntouch.com>
2017-12-14 17:47:58 +0530

    CICO-47916: Zest web for AutoCamp

--- asset_list/theming/guestweb/css/css_theme_mapping.js
+++ asset_list/theming/guestweb/css/css_theme_mapping.js
@@ -4,6 +4,7 @@ module.exports = {
         var themeCSSRoot = 'stylesheets/guestweb/';
 
         return {
+            'guestweb_autocamp': [ themeCSSRoot + 'guestweb_autocamp.css'],
             'guestweb_the_grove_portland': [ themeCSSRoot + 'guestweb_the_grove_portland.css'],
             'guestweb_room_2_hammersmith': [ themeCSSRoot + 'guestweb_room_2_hammersmith.css'],
             'guestweb_anvil_hotel': [ themeCSSRoot + 'guestweb_anvil_hotel.css'],
