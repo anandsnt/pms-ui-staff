6e3b946ab8b0aa9707b22ff5bf0c37a87bdabb37
krishobh <krishobh@stayntouch.com>
2017-12-21 17:53:38 +0530

    Fixed cc issues

--- rover/controllers/arTransactions/rvCardArTransactionsMainCtrl.js
+++ rover/controllers/arTransactions/rvCardArTransactionsMainCtrl.js
@@ -836,8 +836,8 @@ sntRover.controller('RVCompanyCardArTransactionsMainCtrl',
 		});
 
 		// CICO-47819: Handling action after navigation back from Staycard
-		$scope.$on('BACK_FROM_STAY_CARD', function(){
-			if(typeof $scope.arDataObj.accountId === 'undefined') {
+		$scope.$on('BACK_FROM_STAY_CARD', function() {
+			if (typeof $scope.arDataObj.accountId === 'undefined') {
 				$timeout(function() {
 					init();
 				}, 2000);
