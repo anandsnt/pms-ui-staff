830ee0c391374d4fc575ef379f3e47b0b40317cb
rahulkookal <rahul@stayntouch.com>
2018-01-05 14:50:48 +0530

    CICO-48566: Zest web for Eben House.

--- asset_list/theming/guestweb/css/css_theme_mapping.js
+++ asset_list/theming/guestweb/css/css_theme_mapping.js
@@ -4,6 +4,7 @@ module.exports = {
         var themeCSSRoot = 'stylesheets/guestweb/';
 
         return {
+            'guestweb_eben_house': [ themeCSSRoot + 'guestweb_eben_house.css'],
             'guestweb_queen_anne': [ themeCSSRoot + 'guestweb_queen_anne.css'],
             'guestweb_autocamp': [ themeCSSRoot + 'guestweb_autocamp.css'],
             'guestweb_the_grove_portland': [ themeCSSRoot + 'guestweb_the_grove_portland.css'],
