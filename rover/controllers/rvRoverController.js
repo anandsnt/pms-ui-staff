3bbbe9cdc143f9487aa4a1871ff606cdfc883c63
Dilip Sahayadhas <dilips@stayntouch.com>
2018-01-16 22:20:18 +0530

    CICO-48759 Fixed workstation identification issue in sixpayments configured hotels

--- rover/controllers/rvRoverController.js
+++ rover/controllers/rvRoverController.js
@@ -778,23 +778,24 @@ sntRover.controller('roverController', [
         }
 
 
-        /*
-         * Start Card reader now!.
-         */
-        if ($rootScope.paymentGateway !== 'sixpayments') {
-            /* Enabling desktop Swipe if we access the app from desktop ( not from devices) and
+
+        /* Enabling desktop Swipe if we access the app from desktop ( not from devices) and
          * desktopSwipeEnabled flag is true
         */
-            if ($rootScope.desktopSwipeEnabled && !rvUtilSrv.checkDevice.any()) {
+        if (!rvUtilSrv.checkDevice.any()) {
+            sntapp.desktopCardReader.isDesktopSwipeEnabled = $rootScope.desktopSwipeEnabled;
+
+            if ($rootScope.desktopSwipeEnabled) {
+                // Start Card reader now!
                 $rootScope.isDesktopUUIDServiceInvoked = true;
                 initiateDesktopCardReader();
+            } else {
+                sntapp.desktopCardReader.setDesktopUUIDServiceStatus(true);
+                sntapp.desktopCardReader.startDesktopReader(
+                    $rootScope.ccSwipeListeningPort,
+                    options,
+                    $rootScope.ccSwipeListeningUrl);
             }
-
-        }
-
-        // If desktopSwipe is not enabled, we have to invoke the desktopUUID service like below
-        if (!$rootScope.isDesktopUUIDServiceInvoked && !rvUtilSrv.checkDevice.any()) {
-            sntapp.desktopUUIDService.startDesktopUUIDService($rootScope.ccSwipeListeningPort, options);
         }
 
         /*
