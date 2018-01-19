29cc283d73f14c03fffbfc11676b0d1d87ba7978
resheilSNT <resheil@stayntouch.com>
2018-01-19 15:40:30 -0500

    changing language fetch API in stationary

--- admin/controllers/adStationaryCtrl.js
+++ admin/controllers/adStationaryCtrl.js
@@ -55,6 +55,18 @@ admin.controller('ADStationaryCtrl',
 	};
 
 	$scope.init = function() {
+		// Rename keys to avoid more changes in multiple places
+		// and to make our select box data set consisntent to use
+		// value and description
+		angular.forEach(availableGuestLanguages.languages, function(availableLanguage) {
+			availableLanguage.value = availableLanguage.code;
+			delete availableLanguage.code;
+			availableLanguage.description = availableLanguage.language;
+			delete availableLanguage.language;
+		});
+		availableGuestLanguages.locales = availableGuestLanguages.languages;
+		delete availableGuestLanguages.languages;
+
 		$scope.languages = availableGuestLanguages;
 		$scope.holdStatusList = availableHoldStatus.data.hold_status;
 		$scope.locale = $scope.languages.default_locale;
