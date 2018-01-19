1579147e4cb97d320b34ad5a7647ad402d5ff6fc
Dilip Sahayadhas <dilips@stayntouch.com>
2018-01-19 21:32:28 +0530

    CICO-48362 Handle 504 Gateway timeout error

--- rover/services/rvBaseWebSrvV2.js
+++ rover/services/rvBaseWebSrvV2.js
@@ -28,6 +28,9 @@ angular.module('sntRover').service('rvBaseWebSrvV2', ['$http', '$q', '$window', 
             deferred.reject(['Internal server error occured']);
         } else if (status === 501 || status === 502 || status === 503) { // 500- Internal Server Error
             $window.location.href = '/500';
+        } else if (status === 504) {
+            $rootScope.showTimeoutError && $rootScope.showTimeoutError();
+            return;
         } else if (status === 401) { // 401- Unauthorized
             // so lets redirect to login page
             $window.location.href = '/logout';
