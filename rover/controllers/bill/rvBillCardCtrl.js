4cbd306d3785137a1910ee8c3b477df7b95dcfe4
soumyastayntouch <soumya@stayntouch.com>
2018-01-19 18:16:26 +0530

    CICO-36709: Pass reference_text and show reference on guest invoice in popup

--- rover/controllers/bill/rvBillCardCtrl.js
+++ rover/controllers/bill/rvBillCardCtrl.js
@@ -2284,7 +2284,7 @@ sntRover.controller('RVbillCardController',
    /*
 	 * open popup for edit/split/remove transaction
 	 */
-	$scope.openActionsPopup = function(id, desc, amount, type, credits, reference_text) {
+	$scope.openActionsPopup = function(id, desc, amount, type, credits, reference_text, show_ref_on_invoice) {
 
 		$scope.errorMessage = "";
 		// hide edit and remove options in case type is  payment
@@ -2293,6 +2293,7 @@ sntRover.controller('RVbillCardController',
 		$scope.selectedTransaction.id = id;
 		$scope.selectedTransaction.desc = desc;
 		$scope.reference_text = reference_text;
+		$scope.show_ref_on_invoice = true//show_ref_on_invoice;
 
 		if (amount) {
 			$scope.selectedTransaction.amount = amount;
