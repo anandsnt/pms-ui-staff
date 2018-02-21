cdbb5cd3c56bf3e0b29732b550da7fcf8dd8f02f
Dilip Sahayadhas <dilips@stayntouch.com>
2018-02-21 15:39:52 +0530

    CICO-49223 Initial karma configuration and sample test for login App

---
+++ login/tests/loginSrv.spec.js
@@ -0,0 +1,13 @@
+describe('Login Services', function () {
+    var loginSrv;
+
+    beforeEach(module('login'));
+
+    beforeEach(inject(function (_loginSrv_) {
+        loginSrv = _loginSrv_;
+    }));
+    
+    it('service should exist', function () {
+        expect(loginSrv).toBeDefined();
+    });
+});
