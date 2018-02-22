a38361fd1e9a9c27be50203348d71347188a6ba3
ragesh <ragesh@stayntouch.com>
2018-02-22 16:24:32 +0530

    CICO-50125 Fixed the issue of not passing payment info while updating the time from staycard

--- rover/controllers/reservationCard/rvReservationDetailsController.js
+++ rover/controllers/reservationCard/rvReservationDetailsController.js
@@ -345,6 +345,7 @@ sntRover.controller('reservationDetailsController',
 		};
 
 		$scope.reservationData.paymentTypes = paymentTypes;
+		$scope.reservationData.paymentMethods = paymentTypes;
 		$scope.reservationData.reseravationDepositData = reseravationDepositData;
 
 		$scope.reservationData.justCreatedRes = (typeof $stateParams.justCreatedRes !== "undefined" && $stateParams.justCreatedRes !== "" && $stateParams.justCreatedRes !== null && $stateParams.justCreatedRes === "true") ? true : false;
