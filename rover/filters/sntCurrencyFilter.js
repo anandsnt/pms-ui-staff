f33fd280527c3665d4d83d0239a0fb421c21c349
krishobh <krishobh@stayntouch.com>
2018-02-23 18:32:31 +0530

    CICO-35453 Updated algorithm

--- rover/filters/sntCurrencyFilter.js
+++ rover/filters/sntCurrencyFilter.js
@@ -2,14 +2,46 @@ angular.module('sntRover').filter('sntCurrency', function() {
 	return function(input, scope, place) {
 
 		if (input && scope) {
-			var currencyFormat = scope.currencyFormat,
-				currencySymbol = scope.currencySymbol
 
-			return processSntCurrency(input, currencySymbol, currencyFormat, place);
+			var paramObj = {
+				input: input,
+				symbol: scope.currencySymbol,
+				place: place
+			};
+
+			switch(scope.currencyFormat) {
+				case 'FORMAT-1': 
+					// EG : 1234567.89 => 1,234,567,89
+					paramObj.integerSeperatorType  = 'COMMA';
+					paramObj.fractionSeperatorType = 'COMMA';
+					break;
+				case 'FORMAT-2': 
+					// EG : 1234567.89 => 1,234,567.89
+					paramObj.integerSeperatorType  = 'COMMA';
+					paramObj.fractionSeperatorType = 'DOT';
+					break;
+				case 'FORMAT-3':
+					// EG : 1234567.89 => 1.234.567 
+					paramObj.integerSeperatorType  = 'DOT';
+					paramObj.fractionSeperatorType = null;
+					break;
+				case 'FORMAT-4': 
+					// EG : 1234567.89 => 1,234,567
+					paramObj.integerSeperatorType  = 'COMMA';
+					paramObj.fractionSeperatorType = null;
+					break;
+				default:
+					// EG : 1234567.89 => 1,234,567.89
+					paramObj.integerSeperatorType  = 'COMMA';
+					paramObj.fractionSeperatorType = 'DOT';
+			}
+
+			return processSntCurrency(paramObj);
 		}
 	};
 });
 
+// Utility method to reverse a given string value.
 function reverseString(str) {
     return str.split("").reverse().join("");
 }
@@ -24,13 +56,22 @@ var getSeperatorType = function(seperator) {
     return CurrencyFormatSeperatorMappings[seperator][0];
 };
 
-function processSntCurrency(input, symbol, format, place) {
-	var input = '123456789123456789.12';
-	var output = '', inputArray = [], integerPart = null, fractionPart = null, i = 0, s ='', j=0,
-		integerSeperatorType  = 'DOT',
-		fractionSeperatorType = 'COMMA';
+function processSntCurrency(paramObj) {
 
-	inputArray = input.split('.');
+	paramObj.input = '123456789123456789.12';
+
+	var inputArray = [],
+		integerPart = null, fractionPart = null,
+		i = 0, j=0,
+		processData = '', sntCurrency = '',
+		CONST_PRECISION = 2;
+
+	/* 
+	 *	STEP-1 : Splits a given input value (type {string}) into two pieces - Integer part & Fractional part,
+	 * 	Then reversing the integer part for parsing.
+	 * 	Eg : 1234567.89 =>  [ '1234567', '89' ] => [ '7654321', '89' ]
+	 */
+	inputArray = paramObj.input.split('.');
 
 	if (inputArray.length > 1) {
 		integerPart    = inputArray[0];
@@ -40,44 +81,45 @@ function processSntCurrency(input, symbol, format, place) {
 		integerPart  = inputArray[0];
 	}
 
+	// Reversing the Interger part.
 	integerPart = reverseString(integerPart);
 
-	// STEP-1 : Process Interger part and add appropriate seperator type.
+	// STEP-2 : Process Interger part and add appropriate seperator type.
 
-	for ( i=integerPart.length-1, j=0 ; i>=0; i--, j++) {
+	for ( i = integerPart.length-1, j = 0 ; i >= 0; i--, j++ ) {
 		if (j%3 === 0 && j > 2) {
-			s = s + getSeperatorType(integerSeperatorType) + integerPart[i];
+			processData = processData + getSeperatorType(paramObj.integerSeperatorType) + integerPart[i];
 		}
 		else {
-			s = s + integerPart[i];
+			processData = processData + integerPart[i];
 		}
 	}
 
-	console.log(s);
+	console.log(processData);
 
-	// STEP-2 : Add central seperator.
+	// STEP-3 : Appending central seperator.
 
-	s = s + getSeperatorType(fractionSeperatorType);
+	processData = processData + getSeperatorType(paramObj.fractionSeperatorType);
 
-	// STEP-3 : Add fractional part.
+	// STEP-4 : Add fractional part.
 
 	if ( fractionPart !== null ) {
 		// 2 digit precision
-		var fraction = fractionPart.slice(0, 2);
+		var fraction = fractionPart.slice(0, CONST_PRECISION);
 
-		s = s + fraction;
+		processData = processData + fraction;
 	}
 
-	// STEP-4 : Append/prepend currency symbol.
+	// STEP-5 : Append/prepend currency symbol based on place value.
 
-	if (typeof place !== 'undefined' && !place) {
-		output = s + ' ' + symbol;
+	if (typeof paramObj.place !== 'undefined' && !paramObj.place) {
+		sntCurrency = processData + ' ' + paramObj.symbol;
 	}
 	else {
-		output = symbol + ' ' + s;
+		sntCurrency = paramObj.symbol + ' ' + processData;
 	}
 
-	console.log(output);
+	console.log(sntCurrency);
 
-	return output;
+	return sntCurrency;
 };
