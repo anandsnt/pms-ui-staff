5dff13340469a5100652e801a52eaa5496393b82
Akshai Raj <akshai@stayntouch.com>
2018-03-07 17:36:11 +0530

    CICO-49152 Create new service for the guest details remove functionality

--- rover/services/rvContactInfoSrv.js
+++ rover/services/rvContactInfoSrv.js
@@ -107,7 +107,7 @@ angular.module('sntRover').service('RVContactInfoSrv', [
          */
         service.removeGuestDetails = function (guestId) {
             var deferred = $q.defer(),
-                url = '/api/guest_details/' + guestId + '/destroy_guest_detail';
+                url = '/api/guest_details/' + guestId + '/remove_guest_detail';
 
             rvBaseWebSrvV2.putJSON(url).then(function(data) {
                 deferred.resolve(data);
