33f00d9c26892ac796f38979c9f92f910fa43613
rahulkookal <rahul@stayntouch.com>
2018-03-07 17:29:33 +0530

    CICO-50680: Fix for QA issues

--- guestweb/scripts/app_router_aria_sky_suites.js
+++ guestweb/scripts/app_router_aria_sky_suites.js
@@ -10,231 +10,231 @@ sntGuestWeb.config(['$stateProvider', '$urlRouterProvider', function($stateProvi
         templateUrl: '/assets/common_templates/partials/MGM/gwExternal.html',
         controller: 'externalVerificationViewController',
         title: 'External verification'
-    });
-
-
-    // room and cc verification
-
-    $stateProvider.state('checkoutRoomVerification', {
+    })
+    .state('checkoutRoomVerification', {
         url: '/checkoutRoomVerification',
         templateUrl: '/assets/common_templates/partials/MGM/gwRoomVerification.html',
         controller: 'checkoutRoomVerificationViewController',
         title: 'Room verification'
-    }).state('ccVerification', {
+    })
+    .state('ccVerification', {
         url: '/ccVerification/:fee/:message/:isFromCheckoutNow',
         templateUrl: '/assets/common_templates/partials/MGM/gwCcEntry.html',
         controller: 'ccVerificationViewController',
         title: 'CC verification'
-    });
-
-    // checkout now states
-
-    $stateProvider.state('checkoutBalance', {
+    })
+    .state('checkoutBalance', {
         url: '/checkoutBalance',
         controller: 'checkOutBalanceController',
         templateUrl: '/assets/common_templates/partials/MGM/gwBill.html',
         title: 'Balance - Check-out Now'
-    }).state('checkOutConfirmation', {
+    })
+    .state('checkOutConfirmation', {
         url: '/checkOutConfirmation',
         controller: 'checkOutConfirmationController',
         templateUrl: '/assets/common_templates/partials/MGM/gwCheckout.html',
         title: 'Confirm - Check-out Now'
-    });
-
-    // late checkout states
-
-    $stateProvider.state('checkOutOptions', {
+    })
+    .state('checkOutOptions', {
         url: '/checkOutOptions',
         templateUrl: '/assets/common_templates/partials/MGM/gwCheckoutoptions.html',
         controller: 'checkOutLandingController',
         title: 'Check-out'
-    }).state('checkOutLaterOptions', {
+    })
+    .state('checkOutLaterOptions', {
         url: '/checkOutLaterOptions',
         templateUrl: '/assets/common_templates/partials/MGM/gwLatecheckoutoptions.html',
         controller: 'checkOutLaterController',
         title: 'Check-out Later'
-    }).state('checkOutLaterSuccess', {
+    })
+    .state('checkOutLaterSuccess', {
         url: '/checkOutLaterOptions/:id',
         templateUrl: '/assets/common_templates/partials/MGM/gwLateCheckoutfinal.html',
         controller: 'checkOutLaterSuccessController',
         title: 'Status - Check-out Later'
-    });
-
-    // checkin states
-
-    $stateProvider.state('checkinConfirmation', {
+    })
+    .state('checkinConfirmation', {
         url: '/checkinConfirmation',
         templateUrl: '/assets/common_templates/partials/MGM/gwCheckin.html',
         controller: 'checkInConfirmationViewController',
         title: 'Check-in'
-    }).state('checkinReservationDetails', {
+    })
+    .state('checkinReservationDetails', {
         url: '/checkinReservationDetails',
         templateUrl: '/assets/common_templates/partials/MGM/gwCheckinDetails.html',
         controller: 'checkInReservationDetails',
         title: 'Details - Check-in'
-    }).state('checkinUpgrade', {
+    })
+    .state('checkinUpgrade', {
         url: '/checkinUpgrade',
         templateUrl: '/assets/common_templates/partials/MGM/gwRoomUpgrades.html',
         controller: 'checkinUpgradeRoomController',
         title: 'Upgrade - Check-in'
-    }).state('checkinKeys', {
+    })
+    .state('checkinKeys', {
         url: '/checkinKeys',
         templateUrl: '/assets/common_templates/partials/MGM/gwCheckinFinal.html',
         controller: 'checkInKeysController',
         title: 'Keys - Check-in'
-    }).state('checkinSuccess', {
+    })
+    .state('checkinSuccess', {
         url: '/checkinSuccess',
         templateUrl: '/assets/common_templates/partials/MGM/gwAlreadyCheckedIn.html',
         title: 'Status - Check-in'
-    }).state('checkinArrival', {
+    })
+    .state('checkinArrival', {
         url: '/checkinArrival',
         controller: 'checkinArrivalDetailsController',
         templateUrl: '/assets/common_templates/partials/MGM/AriaSkySuites/gwArrivalTime.html',
         title: 'Arrival Details - Check-in'
-    }).state('guestDetails', {
+    })
+    .state('guestDetails', {
         url: '/guestDetails',
         templateUrl: '/assets/common_templates/partials/MGM/gwGuestDetail.html',
         controller: 'guestDetailsController',
         title: 'Guest Details'
-    });
-    // pre checkin states
-
-    $stateProvider.state('preCheckinStatus', {
+    })
+    .state('preCheckinStatus', {
         url: '/preCheckinStatus',
         templateUrl: '/assets/common_templates/partials/MGM/gwPreCheckinFinal.html',
         controller: 'preCheckinStatusController',
         title: 'Status - Pre Check-In'
-    });
-
-    $stateProvider.state('earlyCheckinOptions', {
+    })
+    .state('earlyCheckinOptions', {
         url: '/earlyCheckinOptions/:time/:charge/:id/:isFromCheckinNow/:roomAssignedFromZestWeb',
         templateUrl: '/assets/common_templates/partials/MGM/gwEarlyCheckinOptions.html',
         controller: 'earlyCheckinOptionsController',
         title: 'Early Check-in'
-    }).state('earlyCheckinFinal', {
+    })
+    .state('earlyCheckinFinal', {
         url: '/earlyCheckinFinal/:time/:charge/:id/:isFromCheckinNow',
         templateUrl: '/assets/common_templates/partials/MGM/gwEarlyCheckinFinal.html',
         controller: 'earlyCheckinFinalController',
         title: 'Early Check-in'
-    }).state('laterArrival', {
+    })
+    .state('laterArrival', {
         url: '/laterArrival/:time/:isearlycheckin',
         templateUrl: '/assets/common_templates/partials/MGM/gwLateArrivalTime.html',
         controller: 'checkinArrivalDetailsController',
         title: 'Early Check-in'
-    });
-
-    $stateProvider.state('noOptionAvailable', {
+    })
+    .state('noOptionAvailable', {
         url: '/noOptionAvailable',
         templateUrl: '/assets/common_templates/partials/gwNoOption.html',
         title: 'Feature not available'
-    });
-
-    $stateProvider.state('externalCheckinVerification', {
+    })
+    .state('externalCheckinVerification', {
         url: '/externalCheckinVerification',
         templateUrl: '/assets/common_templates/partials/MGM/gwExternalCheckin.html',
         controller: 'externalCheckinVerificationViewController',
         title: 'External Check in verification'
-    }).state('guestCheckinTurnedOff', {
+    })
+    .state('guestCheckinTurnedOff', {
         url: '/guestCheckinTurnedOff',
         templateUrl: '/assets/common_templates/partials/MGM/gwExternalCheckInTurnedOff.html',
         title: 'Check-in'
-    }).state('guestCheckinEarly', {
+    })
+    .state('guestCheckinEarly', {
         url: '/guestCheckinEarly/:date',
         templateUrl: '/assets/common_templates/partials/MGM/gwEarlyToCheckin.html',
         controller: 'earlyToCheckinCtrl',
         title: 'Check-in'
-    }).state('guestCheckinLate', {
+    })
+    .state('guestCheckinLate', {
         url: '/guestCheckinLate',
         templateUrl: '/assets/common_templates/partials/MGM/gwLateToCheckin.html',
         title: 'Check-in'
-    }).state('birthDateDetails', {
+    })
+    .state('birthDateDetails', {
         url: '/birthDateDetails',
         templateUrl: '/assets/common_templates/partials/MGM/gwBirthDataDetails.html',
         controller: 'birthDateDetailsController',
         title: 'Birthdate'
-    }).state('promptGuestDetails', {
+    })
+    .state('promptGuestDetails', {
         url: '/promptGuestDetails',
         templateUrl: '/assets/common_templates/partials/MGM/gwPromptGuestDetails.html',
         controller: 'guestDetailsController',
         title: 'Guest Details'
-    }).state('guestNotEligible', {
+    })
+    .state('guestNotEligible', {
         url: '/guestNotEligible',
         templateUrl: '/assets/common_templates/partials/MGM/gwGuestNotEligible.html',
         title: 'Guest Details'
-    });
-
-
-    $stateProvider.state('checkinCcVerification', {
+    })
+    .state('checkinCcVerification', {
         url: '/checkinCcVerification',
         templateUrl: '/assets/common_templates/partials/MGM/gwCheckinCCAddition.html',
         controller: 'checkinCcVerificationController',
         title: 'CC verification'
-    }).state('emailAddition', {
+    })
+    .state('emailAddition', {
         url: '/emailAddition/:isFrom',
         templateUrl: '/assets/common_templates/partials/MGM/gwEmailEntry.html',
         controller: 'emailEntryController',
         title: 'E-mail entry'
-    }).state('mobileNumberAddition', {
+    })
+    .state('mobileNumberAddition', {
         url: '/mobileNumberAddition',
         templateUrl: '/assets/common_templates/partials/MGM/gwPhoneNumberUpdate.html',
         controller: 'mobileEntryController',
         title: 'Phone number entry'
-    }).state('mobileNumberOptions', {
+    })
+    .state('mobileNumberOptions', {
         url: '/mobileNumberOptions',
         templateUrl: '/assets/common_templates/partials/MGM/gwMobielNUmberOptions.html',
         controller: 'mobileOptionsController',
         title: 'Phone number entry'
-    });
-
-
-    // checkin now states
-    $stateProvider.state('guestCheckinOptions', {
+    })
+    .state('guestCheckinOptions', {
         url: '/guestCheckinOptions',
         templateUrl: '/assets/common_templates/partials/MGM/gwCheckinOptions.html',
         controller: 'checkinOptionsController',
         title: 'Checkin options'
-    }).state('earlyCheckinReady', {
+    })
+    .state('earlyCheckinReady', {
         url: '/earlyCheckinReady',
         controller: 'earlyCheckinReadyController',
         templateUrl: '/assets/common_templates/partials/MGM/gwEarlyCheckinReady.html',
         title: 'Early Check in ready'
-    }).state('roomNotReady', {
+    })
+    .state('roomNotReady', {
         url: '/roomNotReady',
         templateUrl: '/assets/common_templates/partials/MGM/gwCheckinNowCommon.html',
         controller: 'roomNotReadyController',
         title: 'Room unavailable'
-    }).state('roomAssignFailed', {
+    })
+    .state('roomAssignFailed', {
         url: '/roomAssignFailed',
         templateUrl: '/assets/common_templates/partials/MGM/gwCheckinNowCommon.html',
         controller: 'roomAssignFailedController',
         title: 'Room Assign Failed'
-    }).state('eciOffroomAssignFailed', {
+    })
+    .state('eciOffroomAssignFailed', {
         url: '/eciOffroomAssignFailed',
         templateUrl: '/assets/common_templates/partials/MGM/gwCheckinNowCommon.html',
         controller: 'eciOffRoomAssignmentFailedController',
         title: 'Room Assign Failed'
-    }).state('eciOffRoomNotReady', {
+    })
+    .state('eciOffRoomNotReady', {
         url: '/eciOffRoomNotReady',
         templateUrl: '/assets/common_templates/partials/MGM/gwCheckinNowCommon.html',
         controller: 'eciOffRoomNotReadyController',
         title: 'Room unavailable'
-    });
-
-    // comment out later
-
-    $stateProvider.state('offerAddonOptions', {
+    })
+    .state('offerAddonOptions', {
         url: '/addonOptions/:isFrom',
         templateUrl: '/assets/common_templates/partials/MGM/gwOfferAddonOptions.html',
         controller: 'offerAddonOptionsController',
         title: 'Addons'
-    });
-
-    $stateProvider.state('checkOutStatus', {
+    })
+    .state('checkOutStatus', {
         url: '/checkOutStatus',
         controller: 'checkOutStatusController',
         templateUrl: '/assets/common_templates/partials/MGM/Aria/gwCheckoutfinal.html',
         title: 'Status - Check-out Now'
-    }).state('roomNotToSell', {
+    })
+    .state('roomNotToSell', {
         url: '/roomNotToSell',
         templateUrl: '/assets/checkin/partials/MGM/roomNotToSell.html',
         controller: 'roomNotToSellController',
