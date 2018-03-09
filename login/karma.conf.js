377a09040871bf4f3eac2dc89d5b2cf317a86732
Dilip Sahayadhas <dilips@stayntouch.com>
2018-03-09 15:25:08 -0500

    ui-test controller level examples

--- login/karma.conf.js
+++ login/karma.conf.js
@@ -15,6 +15,10 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+        '../rover/rvSntApp.js',
+        '../rover/rvDesktopCardOperations.js',
+        '../rover/rvMLIOperations.js',
+        '../shared/baseCtrl.js',
         '../shared/lib/js/angular.min.js',
         '../shared/lib/js/angular-ui-router.min.js',
         '../shared/lib/js/angular-mocks.js',
@@ -24,9 +28,11 @@ module.exports = function(config) {
         '../shared/lib/js/iscroll.js',
         '../shared/lib/js/ng-iscroll.js',
         '../shared/lib/js/ngDialog.min.js',
+        '../shared/lib/js/SyntaxHighlighter/shCore.js',
+        '../shared/lib/js/Utils.js',
         './loginApp.js',
-        './services/loginSrv.js',
-        './specs/loginSrv.spec.js'
+        './services/**/*.js',
+        './controllers/**/*.js'
     ],
 
 
