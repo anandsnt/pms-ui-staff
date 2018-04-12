a693aa8e6a6c7110a41f5fa3e2dcc97b1f279165
soumyastayntouch <soumya@stayntouch.com>
2018-04-12 19:33:13 +0530

    CICO-52227: Initial configurations for rover app unit tests

--- rover/karma.conf.js
+++ rover/karma.conf.js
@@ -15,9 +15,46 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+        '../shared/lib/js/jquery.min.js',
+        '../shared/lib/js/jquery-ui.min.js',
+        '../shared/lib/js/jquery.ui.touch-punch.min.js',
         '../shared/lib/js/angular.min.js',
+        '../shared/lib/js/angular-route.min.js',
         '../shared/lib/js/angular-ui-router.min.js',
-        '../shared/lib/js/angular-mocks.js'
+        '../shared/lib/js/angular-animate.min.js',
+        '../shared/lib/js/angular-dragdrop.min.js',
+        '../shared/lib/js/angular-mocks.js',        
+        '../shared/lib/js/angular-sanitize.min.js',
+        '../shared/lib/js/angular-translate.min.js',
+        '../shared/lib/js/angular-translate-loader-static-files.min.js',
+        '../shared/lib/js/oclazyload/ocLazyLoad.min.js',
+        '../shared/lib/js/ui-utils.min.js',
+        '../shared/lib/js/underscore.min.js',
+        '../shared/lib/js/iscroll.js',
+        '../shared/lib/js/ng-iscroll.js',
+        '../shared/lib/js/ngDialog.min.js',
+        '../shared/lib/js/fastclick/fastclick.min.js',
+        '../shared/lib/js/moment.min.js',
+        '../shared/lib/js/date.js',
+        '../shared/directives/documentTouchMovePrevent/*.js',
+        '../shared/directives/clickTouch/*.js',
+        '../shared/directives/divTouchMoveStopPropogate/*.js',   
+        '../shared/interceptors/**/*.js',
+        '../shared/directives/**/*.js',
+        './rvApp.js',
+        './rvSntApp.js',
+        './rvCacheVaultModule.js',
+        './rvCardOperations.js',
+        './rvDesktopCardOperations.js',
+        './rvMLIOperations.js',
+        './rvRouter.js',
+        './rvSwipeOperations.js',               
+        './rvRouters/*.js',
+        './controllers/**/*.js',
+        './services/**/*.js',
+        // './directives/**/*.js',
+        './factories/**/*.js',
+        './filters/*.js'        
     ],
 
 
@@ -29,7 +66,11 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+        '../payment/**/*.js': ['babel'],
+        './controllers/**/*.js': ['babel'],
+        './services/rateManager_/rvRateManagerCoreSrv.js': ['babel']
     },
+
 
 
     // test results reporter to use
