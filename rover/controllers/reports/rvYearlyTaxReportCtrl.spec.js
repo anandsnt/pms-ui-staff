511dbad56e835497fab3e201cc30425a18ea2a90
soumyastayntouch <soumya@stayntouch.com>
2018-04-18 14:52:45 +0530

    CICO-51364: Removed commented codes

--- rover/controllers/reports/rvYearlyTaxReportCtrl.spec.js
+++ rover/controllers/reports/rvYearlyTaxReportCtrl.spec.js
@@ -6,104 +6,55 @@ describe('RVYearlyTaxReportDetailsController', function () {
         $q,
         $rootScope,
         RVreportsSubSrv,
-    //     results = {
-    //     "with_vat_id": {
-    //         "accounts": [{
-    //             "account_type": "COMPANY",
-    //             "account_type_id": 1,
-    //             "total_revenue": "0.00",
-    //             "total_vat": "0.00",
-    //             "accounts_list": [],
-    //             "revenueData": [{"vat_id":null,
-    //                 "ar_number":"67676767",
-    //                 "name":"Stayntouch DO NOT UPDATE",
-    //                 "revenue":271.0,"vat":21.0}]
-    //         }, {
-    //             "account_type": "TRAVELAGENT",
-    //             "account_type_id": 2,
-    //             "total_revenue": "0.00",
-    //             "total_vat": "0.00",
-    //             "accounts_list": [],
-    //             "revenueData": [{"vat_id":null,
-    //                 "ar_number":"67676767",
-    //                 "name":"Stayntouch DO NOT UPDATE",
-    //                 "revenue":271.0,"vat":21.0}]
-    //         }],
-    //         "total_revenue": "0.00",
-    //         "total_vat": "0.00"
-    //     },
-    //     "without_vat_id": {
-    //         "accounts": [{
-    //             "account_type": "COMPANY",
-    //             "account_type_id": 1,
-    //             "total_revenue": "16913.82",
-    //             "total_vat": "1717.84",
-    //             "accounts_list": []
-    //         }, {
-    //             "account_type": "TRAVELAGENT",
-    //             "account_type_id": 2,
-    //             "total_revenue": "2177.52",
-    //             "total_vat": "338.31",
-    //             "accounts_list": []
-    //         }],
-    //         "total_revenue": "19091.34",
-    //         "total_vat": "2056.15"
-    //     }
-    // };
-    revenueData = {
+
+        revenueData = {
                 "data": [{"vat_id":null,
                      "ar_number":"67676767",
                     "name":"Stayntouch DO NOT UPDATE",
                     "revenue":271.0,"vat":21.0}]
                 },
-results = {
-        "with_vat_id": {
-            "accounts": [{
-                "account_type": "COMPANY",
-                "account_type_id": 1,
+        results = {
+            "with_vat_id": {
+                "accounts": [{
+                    "account_type": "COMPANY",
+                    "account_type_id": 1,
+                    "total_revenue": "0.00",
+                    "total_vat": "0.00",
+                    "accounts_list": []
+                }, {
+                    "account_type": "TRAVELAGENT",
+                    "account_type_id": 2,
+                    "total_revenue": "0.00",
+                    "total_vat": "0.00",
+                    "accounts_list": []
+                }],
                 "total_revenue": "0.00",
-                "total_vat": "0.00",
-                "accounts_list": []
-            }, {
-                "account_type": "TRAVELAGENT",
-                "account_type_id": 2,
-                "total_revenue": "0.00",
-                "total_vat": "0.00",
-                "accounts_list": []
-            }],
-            "total_revenue": "0.00",
-            "total_vat": "0.00"
-        },
-        "without_vat_id": {
-            "accounts": [{
-                "account_type": "COMPANY",
-                "account_type_id": 1,
-                "total_revenue": "16913.82",
-                "total_vat": "1717.84",
-                "accounts_list": []
-            }, {
-                "account_type": "TRAVELAGENT",
-                "account_type_id": 2,
-                "total_revenue": "2177.52",
-                "total_vat": "338.31",
-                "accounts_list": []
-            }],
-            "total_revenue": "19091.34",
-            "total_vat": "2056.15"
-        }
-    };
+                "total_vat": "0.00"
+            },
+            "without_vat_id": {
+                "accounts": [{
+                    "account_type": "COMPANY",
+                    "account_type_id": 1,
+                    "total_revenue": "16913.82",
+                    "total_vat": "1717.84",
+                    "accounts_list": []
+                }, {
+                    "account_type": "TRAVELAGENT",
+                    "account_type_id": 2,
+                    "total_revenue": "2177.52",
+                    "total_vat": "338.31",
+                    "accounts_list": []
+                }],
+                "total_revenue": "19091.34",
+                "total_vat": "2056.15"
+            }
+        };
 
         describe('variable initalizations', function () {
             var taxReportObj = {};
 
             beforeEach(function () {
                 module('sntRover');
-
-                // inject(function (_$controller_, _$rootScope_) {
-                //     $controller = _$controller_;
-
-                //     $scope = _$rootScope_.$new();
-                // });
 
                 inject(function (_$controller_, _RVreportsSubSrv_, _$q_, _$rootScope_) {
                     $controller = _$controller_;
