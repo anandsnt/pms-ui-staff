91c8a89ec2192488cb3375bd1a65ab896e894b3f
resheilSNT <resheil@stayntouch.com>
2018-04-25 10:52:28 -0400

    greatwolf zestweb initial setup

--- guestweb/scripts/util.js
+++ guestweb/scripts/util.js
@@ -272,7 +272,7 @@ var customizeStylesBasedOnUrlTyppe = function() {
   // customize style
   // logo is not in a common HTML
   var node = document.createElement('style');
-  node.innerHTML = ".logo-image {display: none;} .row.header-bar {margin-bottom: 30px !important;}";
+  node.innerHTML = ".logo-image {display: none;} .row.header-bar {margin-bottom: 50px !important;}";
   document.head.appendChild(node);
 
   // $("#zest-footer").show() is called to show footer after loading, 
