aabb3e88daa82b391ca1f9054d9f3b03c49b3159
krishobh <krishobh@stayntouch.com>
2018-05-10 15:21:38 +0530

    CICO-52949 added rvAddOverBookingDatePickerCtrl.spec.js

--- rover/controllers/overBooking/rvAddOverBookingDatePickerCtrl.js
+++ rover/controllers/overBooking/rvAddOverBookingDatePickerCtrl.js
@@ -1,28 +1,34 @@
 angular.module('sntRover').controller('rvAddOverBookingDatePickerCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
+	var that = this;
 
-	var minDateSelected = moment(tzIndependentDate($rootScope.businessDate))
-				.format($rootScope.momentFormatForAPI),
-		type = $scope.addOverBookingObj.type;
+	// Init values
+	$scope.init = function() {
+		that.minDateSelected = moment(tzIndependentDate($rootScope.businessDate))
+					.format($rootScope.momentFormatForAPI),
+		that.type = $scope.addOverBookingObj.type;
 
-	// Setup default selected date.
-    if ( type === 'FROM') {
-		$scope.date = $scope.addOverBookingObj.fromDate;
-    }
-    else if (type === 'TO') {
-		$scope.date = $scope.addOverBookingObj.toDate;
-    }
+		// Setup default selected date.
+	    if (that.type === 'FROM') {
+			$scope.date = $scope.addOverBookingObj.fromDate;
+	    }
+	    else if (that.type === 'TO') {
+			$scope.date = $scope.addOverBookingObj.toDate;
+	    }
+	};
+
     // Setup options.
 	$scope.setUpData = function() {
 		$scope.dateOptions = {
 			changeYear: true,
 			changeMonth: true,
-			minDate: tzIndependentDate(minDateSelected),
+			minDate: tzIndependentDate(that.minDateSelected),
 			yearRange: "1:+5",
 			onSelect: function() {
-				$scope.$emit('DATE_CHANGED', type, $scope.date);
+				$scope.$emit('DATE_CHANGED', that.type, $scope.date);
 			}
 		};
 	};
 
+	$scope.init();
 	$scope.setUpData();
 }]);
