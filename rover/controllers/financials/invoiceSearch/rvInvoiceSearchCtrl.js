2e6544341650bcbcbbb8f28ace4042a07d1bf24e
soumyastayntouch <soumya@stayntouch.com>
2018-05-11 17:22:54 +0530

    CICO-52092: CICO-51602: Added red color for 0 amount, hide invoice number if informational invoice checked

--- rover/controllers/financials/invoiceSearch/rvInvoiceSearchCtrl.js
+++ rover/controllers/financials/invoiceSearch/rvInvoiceSearchCtrl.js
@@ -84,7 +84,13 @@ sntRover.controller('RVInvoiceSearchController',
 				$scope.invoiceSearchFlags.showFindInvoice = true;
 			}
 		};
-
+		/*
+		 * Update informational invoice flag
+		 * Based on checkbox in popup
+		 */
+	    $scope.$on("UPDATE_INFORMATIONAL_INVOICE", function(event, isInformationalInvoice) {
+	    	$scope.isInformationalInvoice = isInformationalInvoice;
+	    });
 		/*
 		 * Opens the popup which have the option to choose the bill layout while print/email
 		 * @param billNo boolean bill no
