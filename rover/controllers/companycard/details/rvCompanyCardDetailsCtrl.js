2b436aec3cfb27b48b87139f918f1ba53cc3a994
krishobh <krishobh@stayntouch.com>
2018-05-21 20:45:43 +0530

    Revert "CICO-51545: Made fields disabled based on the permission"

--- rover/controllers/companycard/details/rvCompanyCardDetailsCtrl.js
+++ rover/controllers/companycard/details/rvCompanyCardDetailsCtrl.js
@@ -328,7 +328,6 @@ angular.module('sntRover').controller('companyCardDetailsController', ['$scope',
 			return ($scope.account_type === 'TRAVELAGENT');
 		};
 		$scope.isUpdateEnabled = function() {
-
 			if ($scope.contactInformation.is_global_enabled === undefined) {
 				return;
 			}
@@ -663,12 +662,9 @@ angular.module('sntRover').controller('companyCardDetailsController', ['$scope',
 			 * Due to the special requirement, we need to do DOM access here.
 			 * Since we are explicitily triggering click event, this should be outside of angular digest loop.
 			 */
-			 if ($scope.isUpdateEnabled()) {
-			 	$timeout(function() {
-					angular.element('#uplaodCompanyLogo').trigger('click');
-				}, 0, false);
-			 }
-			
+			$timeout(function() {
+				angular.element('#uplaodCompanyLogo').trigger('click');
+			}, 0, false);
 		};
 
 		$scope.isEmptyObject = isEmptyObject;
