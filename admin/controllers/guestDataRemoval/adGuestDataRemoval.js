4885bc950bd75265bfbeeb1fb149aa28c4c14685
rahulkookal <rahul@stayntouch.com>
2018-05-28 15:15:16 +0530

    CICO-50973:Admin settings for GDPR.

---
+++ admin/controllers/guestDataRemoval/adGuestDataRemoval.js
@@ -0,0 +1,54 @@
+admin.controller('ADGuestDataRemovalCtrl', [
+    '$scope',
+    'ngDialog',
+    'adGuestDataRemovalSrv',
+    function ($scope, ngDialog, adGuestDataRemovalSrv) {
+        BaseCtrl.call(this, $scope);
+
+        $scope.confirmSave = function() {
+            if ($scope.GDRSettings.hotels_of_chain > 1) {
+                ngDialog.open({
+                    template: '/assets/partials/guestDataRemoval/adConfirmSaveDailog.html',
+                    className: 'ngdialog-theme-default',
+                    scope: $scope
+                });
+            } else {
+                $scope.updateGuestDataRemovalSettings();
+            }
+        };
+        $scope.closeDialog = function() {
+            ngDialog.close();
+        };
+        $scope.updateGuestDataRemovalSettings = function() {
+            $scope.callAPI(adGuestDataRemovalSrv.updateGDRSettings, {
+                params: {
+                    automatic_guest_data_removal: $scope.GDRSettings.automatic_guest_data_removal,
+                    no_of_days_after_last_checkout: $scope.GDRSettings.no_of_days_after_last_checkout
+                },
+                onFailure: function(err) {
+                    $scope.errorMessage = err;
+                },
+                onSuccess: function (response) {
+                    $scope.GuestDataRemovalSettings = response.data;
+                    $scope.successMessage = 'Guest data settings updated';
+                }
+            });
+        };
+        // Initialize the controller
+        var init = function () {
+                fetchGuestDataRemovalSettings();
+            },
+            fetchGuestDataRemovalSettings = function() {
+                $scope.callAPI(adGuestDataRemovalSrv.initSync, {
+                    params: {},
+                    onSuccess: function (response) {
+                        $scope.GDRSettings = response.data;
+                    },
+                    onFailure: function(err) {
+                        $scope.errorMessage = err;
+                    }
+                });
+            };
+
+        init();
+    }]);
