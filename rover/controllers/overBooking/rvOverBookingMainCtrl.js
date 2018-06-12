56f66051f1b75e33c2bbbbc6b2662341640d9e36
krishobh <krishobh@stayntouch.com>
2018-06-12 18:04:43 +0530

    CICO-53143 check box 'Show Rooms left to sell' selected default

--- rover/controllers/overBooking/rvOverBookingMainCtrl.js
+++ rover/controllers/overBooking/rvOverBookingMainCtrl.js
@@ -106,7 +106,7 @@ angular.module('sntRover').controller('RvOverBookingMainCtrl', [
 			startDate: moment(tzIndependentDate($rootScope.businessDate)).format($rootScope.momentFormatForAPI),
 			endDate: moment(tzIndependentDate($rootScope.businessDate)).add(DATE_SHIFT_LIMIT, 'd')
 					.format($rootScope.momentFormatForAPI),
-			isShowRoomsLeftToSell: false,
+			isShowRoomsLeftToSell: true,
 			isShowRoomTypeFilter: false,
 			editData: {}
 		};
