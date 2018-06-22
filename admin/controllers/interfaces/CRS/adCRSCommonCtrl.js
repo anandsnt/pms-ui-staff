3aa3905fd10630deeaf777776094dddeb69bb9a2
soumyastayntouch <soumya@stayntouch.com>
2018-06-22 18:26:17 +0530

    CICO-53877: set default enabled value as false

--- admin/controllers/interfaces/CRS/adCRSCommonCtrl.js
+++ admin/controllers/interfaces/CRS/adCRSCommonCtrl.js
@@ -22,10 +22,6 @@ angular.module('admin').controller('adCRSCommonCtrl',
 
         $scope.saveInterfaceConfig = function() {
 
-            // var unwantedKeys = ["availableTaxChargeCodes", "selectedTaxChargeCodes"];
-                
-            // $scope.config = dclone($scope.config, unwantedKeys);
-
             $scope.callAPI(adInterfacesCommonConfigSrv.saveConfiguration, {
                 params: {
                     config: $scope.config,
@@ -49,21 +45,12 @@ angular.module('admin').controller('adCRSCommonCtrl',
             $scope.callAPI(adInterfacesCommonConfigSrv.fetchOptionsList, {
                 onSuccess: onFetchMetaSuccess
             });
-
+            config.enabled = (config.enabled !== null) ? config.enabled : false;
             $scope.config = config;
             $scope.availableSettings = _.keys(config);
             $scope.interface = interfaceIdentifier.toUpperCase();
             $scope.chargeGroups = chargeGroups.data.charge_groups;
             $scope.availableTaxChargeCodesForTaxExemptOne = $scope.availableTaxChargeCodesForTaxExemptTwo = $scope.availableTaxChargeCodesForTaxExemptThree = taxChargeCodes.data.charge_codes;
-
-            // $scope.config.selectedTaxChargeCodes = [];
-            // config.selected_tax_charge_ids = (config.selected_tax_charge_ids !== null) ? config.selected_tax_charge_ids : [];
-            // angular.forEach($scope.config.availableTaxChargeCodes, function(item, index) {
-            //     if (_.indexOf(config.selected_tax_charge_ids, item.id) !== -1) {
-            //         $scope.config.selectedTaxChargeCodes.push(item);
-            //         $scope.config.availableTaxChargeCodes.splice(item);
-            //     }
-            // });
         })();
     }
 ]);
