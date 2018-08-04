6937264fa62387bc8d08bbe3e1dfbae03655d325
resheilSNT <resheil@stayntouch.com>
2018-08-04 10:42:56 -0400

    CICO-48480 reseting values

--- zest_station/controllers_v2/checkin/zsCheckinGuestAddressCtrl.js
+++ zest_station/controllers_v2/checkin/zsCheckinGuestAddressCtrl.js
@@ -8,6 +8,16 @@ sntZestStation.controller('zsCheckinGuestAddressCtrl', [
 	function($scope, $state, zsEventConstants, countryList, zsCheckinSrv, guestAddress) {
 
 		var selectedReservation = zsCheckinSrv.getSelectedCheckInReservation();
+		var resetValues = function() {
+			$scope.guestDetails = {
+				'postal_code': '',
+				'state': '',
+				'city': '',
+				'street': '',
+				'street2': '',
+				'country_id': ''
+			};
+		};
 
 		$scope.usePresentAddress = function() {
 			$state.go('zest_station.checkInReservationDetails', {
@@ -16,6 +26,7 @@ sntZestStation.controller('zsCheckinGuestAddressCtrl', [
 		};
 
 		$scope.useNewAddress = function() {
+			resetValues();
 			$scope.mode = "NEW_ADDRESS";
 			$scope.focusInputField('address-1');
 		};
@@ -72,14 +83,7 @@ sntZestStation.controller('zsCheckinGuestAddressCtrl', [
 				}
 			});
 			$scope.countryList = countryList;
-			$scope.guestDetails = {
-				'postal_code': '',
-				'state': '',
-				'city': '',
-				'street': '',
-				'street2': '',
-				'country_id': ''
-			};
+			resetValues();
 			$scope.showErrorMessage = false;
 
 			if (isValidAddress(guestAddress)) {
