4a5e60736e5826e0375ccff388adce8627d8c13b
krishobh <krishobh@stayntouch.com>
2018-08-28 19:31:40 +0530

    CICO-35453 Updated unit test

--- shared/sntCurrency/sntCurrencyFilter.spec.js
+++ shared/sntCurrency/sntCurrencyFilter.spec.js
@@ -1,67 +1,175 @@
 describe('Filter: sntCurrency', function() {
 	var sntCurrency,
-		$filter,
-		that = {
-			currencySymbol: '$',
-			currencyFormat: '1,222,00'
-		};
+		$filter;
 
 	beforeEach(function () {
         module('sntCurrencyFilter');
-        inject(function (_$filter_, _sntCurrency_ ) {
+        inject(function (_$filter_) {
 			$filter = _$filter_;
-			sntCurrency = _sntCurrency_;
-        });
+		});
     });
 
-	it('Test currencyFormat = 1,222,00 ', function() {
+	describe('Test currencyFormat = 1,222,00 ', function() {
 
-		var input = 1234567890.12345,
-			result = '';
+		var input = '', result = '',
+			that = {
+				currencySymbol: '$',
+				currencyFormat: '1,222,00'
+			};
+		
+		it('with Interger part and Fraction part', function(){
+			input = 1234567890.12345;
+			result = $filter('sntCurrency')(input, that);
+			expect(result).toBe('<span class="currency">$</span> 1,234,567,890,12');
+		});
 
-		result = $filter('sntCurrency')(input, that);
+		it('with Interger part only', function(){
+			input = 1234567890;
+			result = $filter('sntCurrency')(input, that);
+			expect(result).toBe('<span class="currency">$</span> 1,234,567,890');
+		});
 
-		expect(result).toBe('<span class="currency">$</span> 1,234,567,890,12');
+		it('with Fraction part only', function(){
+			input = .12345;
+			result = $filter('sntCurrency')(input, that);
+			expect(result).toBe('<span class="currency">$</span> 0,12');
+		});
+
+		it('with Invalid Input', function(){
+			input = '123.45.6';
+			result = $filter('sntCurrency')(input, that);
+			expect(result).toBe(undefined);
+		});
 	});
 
-	it('Test currencyFormat = 1,222.00 ', function() {
+	describe('Test currencyFormat = 1,222.00 ', function() {
 
-		var input = 1234567890.12345,
-			result = '';
+		var input = '', result = '',
+			that = {
+				currencySymbol: '$',
+				currencyFormat: '1,222.00'
+			};
+		
+		it('with Interger part and Fraction part', function(){
+			input = 1234567890.12345;
+			result = $filter('sntCurrency')(input, that);
+			expect(result).toBe('<span class="currency">$</span> 1,234,567,890.12');
+		});
 
-		result = $filter('sntCurrency')(input, that);
+		it('with Interger part only', function(){
+			input = 1234567890;
+			result = $filter('sntCurrency')(input, that);
+			expect(result).toBe('<span class="currency">$</span> 1,234,567,890');
+		});
 
-		expect(result).toBe('<span class="currency">$</span> 1,234,567,890.12');
+		it('with Fraction part only', function(){
+			input = .12345;
+			result = $filter('sntCurrency')(input, that);
+			expect(result).toBe('<span class="currency">$</span> 0.12');
+		});
+
+		it('with Invalid Input', function(){
+			input = 'abcd';
+			result = $filter('sntCurrency')(input, that);
+			expect(result).toBe(undefined);
+		});
 	});
 
-	it('Test currencyFormat = 1.222 ', function() {
+	describe('Test currencyFormat = 1.222 ', function() {
 
-		var input = 1234567890.12345,
-			result = '';
+		var input = '', result = '',
+			that = {
+				currencySymbol: '$',
+				currencyFormat: '1.222'
+			};
+		
+		it('with Interger part and Fraction part', function(){
+			input = 1234567890.12345;
+			result = $filter('sntCurrency')(input, that);
+			expect(result).toBe('<span class="currency">$</span> 1.234.567.890');
+		});
 
-		result = $filter('sntCurrency')(input, that);
+		it('with Interger part only', function(){
+			input = 1234567890;
+			result = $filter('sntCurrency')(input, that);
+			expect(result).toBe('<span class="currency">$</span> 1.234.567.890');
+		});
 
-		expect(result).toBe('<span class="currency">$</span> 1.234.567.890');
+		it('with Fraction part only', function(){
+			input = .12345;
+			result = $filter('sntCurrency')(input, that);
+			expect(result).toBe('<span class="currency">$</span> 0');
+		});
+
+		it('with Invalid Input', function(){
+			input = '123+45.6';
+			result = $filter('sntCurrency')(input, that);
+			expect(result).toBe(undefined);
+		});
 	});
-	
-	it('Test currencyFormat = 1,222 ', function() {
 
-		var input = 1234567890.12345,
-			result = '';
+	describe('Test currencyFormat = 1,222 ', function() {
 
-		result = $filter('sntCurrency')(input, that);
+		var input = '', result = '',
+			that = {
+				currencySymbol: '$',
+				currencyFormat: '1,222'
+			};
+		
+		it('with Interger part and Fraction part', function(){
+			input = 1234567890.12345;
+			result = $filter('sntCurrency')(input, that);
+			expect(result).toBe('<span class="currency">$</span> 1,234,567,890');
+		});
 
-		expect(result).toBe('<span class="currency">$</span> 1,234,567,890');
+		it('with Interger part only', function(){
+			input = 1234567890;
+			result = $filter('sntCurrency')(input, that);
+			expect(result).toBe('<span class="currency">$</span> 1,234,567,890');
+		});
+
+		it('with Fraction part only', function(){
+			input = .12345;
+			result = $filter('sntCurrency')(input, that);
+			expect(result).toBe('<span class="currency">$</span> 0');
+		});
+
+		it('with Invalid data', function(){
+			input = '12,345.60';
+			result = $filter('sntCurrency')(input, that);
+			expect(result).toBe(undefined);
+		});
 	});
 
 	it('Test isWithoutSymbol = true and currencyFormat = 1,222,00 ', function() {
 
 		var input = 1234567890.12345,
 			isWithoutSymbol = true,
-			result = '';
+			result = '',
+			that = {
+				currencySymbol: '$',
+				currencyFormat: '1,222,00'
+			};
 
 		result = $filter('sntCurrency')(input, that, isWithoutSymbol);
 
 		expect(result).toBe('1,234,567,890,12');
 	});
+
+	it('Test isWithoutSymbol = true and currencyFormat = 1,222,00 and precision = 3 ', function() {
+
+		var input = 1234567890.12345,
+			isWithoutSymbol = true,
+			precision = 3,
+			result = '',
+			that = {
+				currencySymbol: '$',
+				currencyFormat: '1,222,00'
+			};
+
+		result = $filter('sntCurrency')(input, that, isWithoutSymbol, precision);
+
+		expect(result).toBe('1,234,567,890,123');
+	});
 });
+
