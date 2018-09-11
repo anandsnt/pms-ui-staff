89f113dcc4089082dc41146cf869e1a16de30dc8
Aswanth <aswanth@stayntouch.cm>
2018-09-11 22:35:04 +0530

    CICO-56525 fixes the conflicts in ipad/android scroll configs

--- shared/directives/documentTouchMovePrevent/documentTouchMovePrevent.js
+++ shared/directives/documentTouchMovePrevent/documentTouchMovePrevent.js
@@ -2,13 +2,16 @@ angular.module('documentTouchMovePrevent', []).directive('documentTouchMovePreve
     return {
 
         link: function(scope, element) {
-            var isIpad = navigator.userAgent.match(/iPad/i) !== null
+            var isIpad = navigator.userAgent.match(/iPad/i) !== null,
+            url = window.location.href;
 
             // CICO-36654 fix for touch events not getting detected iPad.
             window.touchmovepreventdefault = function(event) {
                 event.preventDefault();
             };
             if (isIpad) {
+                document.addEventListener('touchmove', touchmovepreventdefault);
+            } else if (url.indexOf("/admin/h/") == -1) {
                 document.addEventListener('touchmove', touchmovepreventdefault, {passive: false});
             }
         }
