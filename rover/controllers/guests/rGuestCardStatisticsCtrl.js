9ce427898c02e2a2846947f9251ffe0aad260140
ragesh <ragesh@stayntouch.com>
2018-09-17 12:07:09 +0530

    CICO-57419 Fixed CC issues

--- rover/controllers/guests/rGuestCardStatisticsCtrl.js
+++ rover/controllers/guests/rGuestCardStatisticsCtrl.js
@@ -1,102 +1,100 @@
 angular.module('sntRover').controller("RVGuestCardStatisticsController", [
-	'$scope',
-	'RVGuestCardsSrv',
-	function ($scope, RVGuestCardsSrv) {
-		BaseCtrl.call(this, $scope);
+    '$scope',
+    'RVGuestCardsSrv',
+    function ($scope, RVGuestCardsSrv) {
+        BaseCtrl.call(this, $scope);
 
-		const SIDEBAR_SCROLLER = 'sidebarScroller';
-		const MONTHLY_DATA_SCROLLER = 'monthlyDataScroller';
+        const SIDEBAR_SCROLLER = 'sidebarScroller';
+        const MONTHLY_DATA_SCROLLER = 'monthlyDataScroller';
 
-		var listeners = [];
+        var listeners = [];
 
-		// Load the guest card statistics summary
-		var loadStatisticsSummary = function() {
-				var onStatisticsFetchSuccess = function(data) {
-						$scope.statistics.summary = data;
-						$scope.activeView = 'summary';
-					},
-					onStatistcsFetchFailure = function(error) {
-						$scope.statistics.summary = {};
-					},
-					requestConfig = {
-						params: {},
-						onSuccess : onStatisticsFetchSuccess,
-						onFailure: onStatistcsFetchFailure
-					};
+        // Load the guest card statistics summary
+        var loadStatisticsSummary = function() {
+                var onStatisticsFetchSuccess = function(data) {
+                        $scope.statistics.summary = data;
+                        $scope.activeView = 'summary';
+                    },
+                    onStatistcsFetchFailure = function() {
+                        $scope.statistics.summary = {};
+                    },
+                    requestConfig = {
+                        params: {},
+                        onSuccess: onStatisticsFetchSuccess,
+                        onFailure: onStatistcsFetchFailure
+                    };
 
-				$scope.callAPI(RVGuestCardsSrv.fetchGuestCardStatisticsSummary, requestConfig);
-			},
-			// Load the statistic details for the given guest
-			loadStatisticsDetails = function() {
-				var onStatisticsDetailsFetchSuccess = function(data) {
-						$scope.statistics.details = data;
-						$scope.activeView = 'details';
-						refreshScroller();
-					},
-					onStatistcsDetailsFetchFailure = function(error) {
-						$scope.statistics.details = [];
-					},
-					requestConfig = {
-						params: {},
-						onSuccess : onStatisticsDetailsFetchSuccess,
-						onFailure: onStatistcsDetailsFetchFailure
-					};
+                $scope.callAPI(RVGuestCardsSrv.fetchGuestCardStatisticsSummary, requestConfig);
+            },
+            // Load the statistic details for the given guest
+            loadStatisticsDetails = function() {
+                var onStatisticsDetailsFetchSuccess = function(data) {
+                        $scope.statistics.details = data;
+                        $scope.activeView = 'details';
+                        refreshScroller();
+                    },
+                    onStatistcsDetailsFetchFailure = function(error) {
+                        $scope.statistics.details = [];
+                    },
+                    requestConfig = {
+                        params: {},
+                        onSuccess : onStatisticsDetailsFetchSuccess,
+                        onFailure: onStatistcsDetailsFetchFailure
+                    };
 
-				$scope.callAPI(RVGuestCardsSrv.fetchGuestCardStatisticsDetails, requestConfig);
-			},
-			// Set the listeners for the controller
-			setListeners = function() {
-				var statisticsTabActivateListener = $scope.$on('LOAD_GUEST_STATISTICS', function() {
-														loadStatisticsSummary();
-													});
+                $scope.callAPI(RVGuestCardsSrv.fetchGuestCardStatisticsDetails, requestConfig);
+            },
+            // Set the listeners for the controller
+            setListeners = function() {
+                var statisticsTabActivateListener = $scope.$on('LOAD_GUEST_STATISTICS', function() {
+                                                        loadStatisticsSummary();
+                                                    });
 
-				listeners.push(statisticsTabActivateListener);
-			},
-			// Destroy the listeners
-			destroyListeners = function() {
-				listeners.forEach( function ( listener ) {
-					$scope.$on('$destroy', listener);
-				});
-			};
+                listeners.push(statisticsTabActivateListener);
+            },
+            // Destroy the listeners
+            destroyListeners = function() {
+                listeners.forEach( function ( listener ) {
+                    $scope.$on('$destroy', listener);
+                });
+            };
 
-		
+        // Get icon class based on the variance value
+        $scope.getStatusIconClass = function( value ) {
+            var iconClass = 'neutral';
 
-		// Get icon class based on the variance value
-		$scope.getStatusIconClass = function( value ) {
-			var iconClass = 'neutral';
+            if ( value < 0 ) {
+                iconClass = 'icons time check-out rotate-right';
+            } else if ( value > 0 ) {
+                iconClass = 'icons time check-in rotate-right';
+            }
 
-			if ( value < 0 ) {
-				iconClass = 'icons time check-out rotate-right';
-			} else if ( value > 0 ) {
-				iconClass = 'icons time check-in rotate-right';
-			}
+            return iconClass;
+        };
 
-			return iconClass;
-		};
+        // Get style class based on the variance value
+        $scope.getStatusClass = function( value ) {
+            var styleClass = '';
 
-		// Get style class based on the variance value
-		$scope.getStatusClass = function( value ) {
-			var styleClass = '';
+            if ( value > 0 ) {
+                styleClass = 'green';
+            } else if ( value < 0 ) {
+                styleClass = 'red';
+            }
 
-			if ( value > 0 ) {
-				styleClass = 'green';
-			} else if ( value < 0 ) {
-				styleClass = 'red';
-			}
+            return styleClass;
+        };
 
-			return styleClass;
-		};
+        // Set statistics tab active view - summary | details
+        $scope.setActiveView = function( view ) {
+            if ( view === 'details') {
+                loadStatisticsDetails();
+            } else {
+                loadStatisticsSummary();
+            }
+        };
 
-		// Set statistics tab active view - summary | details
-		$scope.setActiveView = function( view ) {
-			if ( view === 'details') {
-				loadStatisticsDetails();
-			} else {
-				loadStatisticsSummary();
-			}
-		};
-
-		$scope.getReservationClass = function(reservationStatus) {
+        $scope.getReservationClass = function(reservationStatus) {
             var class_ = '';
 
             switch (reservationStatus.toUpperCase()) {
@@ -136,53 +134,53 @@ angular.module('sntRover').controller("RVGuestCardStatisticsController", [
             return class_;
         };
 
-		// Toggle the reservation list view displayed for a month
-		$scope.showMonthlyReservations = function( monthlyData ) {
-			monthlyData.isOpen = !monthlyData.isOpen;
-		};
+        // Toggle the reservation list view displayed for a month
+        $scope.showMonthlyReservations = function( monthlyData ) {
+            monthlyData.isOpen = !monthlyData.isOpen;
+        };
 
-		// Get style for statistics details expanded view
-		$scope.getStyleForExpandedView = function( monthlyData ) {
-			var styleClass = {};					
+        // Get style for statistics details expanded view
+        $scope.getStyleForExpandedView = function( monthlyData ) {
+            var styleClass = {};                    
 
-			if (monthlyData.isOpen) {
-				var margin = monthlyData.reservations.length * 70 + 30;
+            if (monthlyData.isOpen) {
+                var margin = monthlyData.reservations.length * 70 + 30;
 
-				styleClass['margin-bottom'] = margin + 'px';
-			}
+                styleClass['margin-bottom'] = margin + 'px';
+            }
 
-			return styleClass;
-		};
+            return styleClass;
+        };
 
-		// Set the two scrollers in the screen
-		var setScroller = function() {            
-	            var scrollerOptions = {
-	                tap: true,
-	                preventDefault: false                
-	            };
+        // Set the two scrollers in the screen
+        var setScroller = function() {            
+                var scrollerOptions = {
+                    tap: true,
+                    preventDefault: false                
+                };
 
-	            $scope.setScroller(SIDEBAR_SCROLLER, scrollerOptions);
-	            $scope.setScroller(MONTHLY_DATA_SCROLLER, scrollerOptions);
-        	},
-        	// Refreshes the two scrollers in the screen
-        	refreshScroller = function() {
-        		$scope.refreshScroller(SIDEBAR_SCROLLER);
-        		$scope.refreshScroller(MONTHLY_DATA_SCROLLER);
-        	};
+                $scope.setScroller(SIDEBAR_SCROLLER, scrollerOptions);
+                $scope.setScroller(MONTHLY_DATA_SCROLLER, scrollerOptions);
+            },
+            // Refreshes the two scrollers in the screen
+            refreshScroller = function() {
+                $scope.refreshScroller(SIDEBAR_SCROLLER);
+                $scope.refreshScroller(MONTHLY_DATA_SCROLLER);
+            };
 
         // Initialize the controller
-		var init = function() {
-			$scope.activeView = "summary";
-			$scope.statistics = {
-				summary: {},
-				details: []
-			};
-			setScroller();
-			setListeners();
-			destroyListeners();
+        var init = function() {
+            $scope.activeView = "summary";
+            $scope.statistics = {
+                summary: {},
+                details: []
+            };
+            setScroller();
+            setListeners();
+            destroyListeners();
 
-		};
+        };
 
-		init();
+        init();
 
-	}]);
+    }]);
