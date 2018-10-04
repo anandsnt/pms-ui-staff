b16050a0bef1e81e59273cccfbc0a5223a3dfa4b
rahulkookal <rahul@stayntouch.com>
2018-10-04 14:30:57 +0530

    CICO-58264: Zest Web - Hu. Hotel

--- asset_list/theming/guestweb/css/css_theme_mapping.js
+++ asset_list/theming/guestweb/css/css_theme_mapping.js
@@ -5,6 +5,7 @@ module.exports = {
         var themeCSSRoot = 'stylesheets/guestweb/';
 
         return {
+            'guestweb_hu_hotel': [ themeCSSRoot + 'guestweb_hu_hotel.css'],
             'guestweb_cortona_inn_suites': [ themeCSSRoot + 'guestweb_cortona_inn_suites.css'],
             'guestweb_crossroads_hotel': [ themeCSSRoot + 'guestweb_crossroads_hotel.css'],
             'guestweb_sandyport_beach_resort': [ themeCSSRoot + 'guestweb_sandyport_beach_resort.css'],
