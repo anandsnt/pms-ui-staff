3495822dcae873a11e74c663a74adeba15c7d031
ragesh <ragesh@stayntouch.com>
2018-11-08 16:27:24 +0530

    CICO-6051 Fixed the issue with showing the AR transactions tab while comming back from staycard

--- rover/controllers/companycard/details/rvCompanyCardDetailsCtrl.js
+++ rover/controllers/companycard/details/rvCompanyCardDetailsCtrl.js
@@ -223,7 +223,7 @@ angular.module('sntRover').controller('companyCardDetailsController', ['$scope',
 		// CICO-11664
 		// To default the AR transactions tab while navigating back from staycard
 		if ($stateParams.isBackFromStaycard) {
-			$scope.isArTabAvailable = !$stateParams.isBackToTACommission;
+			$scope.isArTabAvailable = !$stateParams.isBackToTACommissionisBackToTACommission && !$stateParams.isBackToStatistics;
 			/*
 			*	CICO-45240 - Replace prevState data to that which we stored before going to Staycard.
 			*/
