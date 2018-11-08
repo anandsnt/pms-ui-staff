cb3f6e7da7e0644c67224d99fadeb5c90565da87
resheilSNT <resheil@stayntouch.com>
2018-11-08 16:01:09 -0500

    CICO-55071 minor changes

--- zest_station/controllers_v2/zsSntIDScanCtrl.js
+++ zest_station/controllers_v2/zsSntIDScanCtrl.js
@@ -50,7 +50,7 @@
 
 			$scope.areAllGuestsScanned = function() {
 				var allGuestsScaned = _.all($scope.selectedReservation.guest_details, function(guestDetail) {
-					return guestDetail.idScanStatus === 'SCANNED';
+					return guestDetail.idScanStatus === $filter('translate')('GID_STAFF_REVIEW_ACCEPTED');
 				});
 
 				return allGuestsScaned;
@@ -85,7 +85,7 @@
 			};
 
 			$scope.saveGuestIdDetails = function(action, imageType) {
-				
+
 			};
 
 
@@ -137,6 +137,75 @@
 				}
 				refreshConfrimImagesScroller();
 			});
+
+			var checkIfEmailIsBlackListedOrValid = function() {
+				var email = !stateParams.guest_email ? '' : stateParams.guest_email;
+
+				return email.length > 0 && !(stateParams.guest_email_blacklisted === 'true') && zsUtilitySrv.isValidEmail(email);
+			};
+			var afterGuestCheckinCallback = function() {
+				$scope.checkinInProgress = false;
+				// if email is valid and is not blacklisted
+				var haveValidGuestEmail = checkIfEmailIsBlackListedOrValid();
+				var nextStateParams = {
+					'guest_id': stateParams.guest_id,
+					'reservation_id': stateParams.reservation_id,
+					'room_no': stateParams.room_no,
+					'first_name': stateParams.first_name,
+					'email': stateParams.guest_email
+				};
+
+				if ($scope.zestStationData.is_kiosk_ows_messages_active) {
+					$scope.setScreenIcon('checkin');
+					$state.go('zest_station.checkinSuccess', nextStateParams);
+				}
+				// if collectiing nationality after email, but email is already valid
+				else if ($scope.zestStationData.check_in_collect_nationality && haveValidGuestEmail) {
+					$scope.$emit('showLoader');
+					$state.go('zest_station.collectNationality', nextStateParams);
+				} else if (haveValidGuestEmail) {
+					$state.go('zest_station.checkinKeyDispense', nextStateParams);
+				} else {
+					$state.go('zest_station.checkInEmailCollection', nextStateParams);
+				}
+			};
+
+			var checkinGuest = function() {
+				var stateParams = JSON.parse($stateParams.params);
+				var checkinParams = {
+					'reservation_id': stateParams.reservation_id,
+					'workstation_id': $scope.zestStationData.set_workstation_id,
+					'authorize_credit_card': false,
+					'do_not_cc_auth': false,
+					'is_promotions_and_email_set': false,
+					'is_kiosk': true,
+					'signature': stateParams.signature
+				};
+				var options = {
+					params: checkinParams,
+					successCallBack: afterGuestCheckinCallback,
+					failureCallBack: function() {
+						$state.go('zest_station.speakToStaff', {
+							'message': 'Checkin Failed.'
+						});
+						$scope.checkinInProgress = false;
+					}
+				};
+
+				if ($scope.inDemoMode()) {
+					afterGuestCheckinCallback();
+				} else {
+					$scope.callAPI(zsCheckinSrv.checkInGuest, options);
+				}
+			};
+
+			$scope.doneButtonClicked = function() {
+				if (stateParams.mode === 'PICKUP_KEY' || stateParams.from_pickup_key) {
+					$scope.zestStationData.continuePickupFlow();
+				} else {
+					checkinGuest();
+				}
+			};
 
 			var initializeMe = (function() {
 				$scope.pageData = zsGeneralSrv.retrievePaginationStartingData();
