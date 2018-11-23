48c26a10e93de8d97f7de4d2fb48bc03f08a5b01
rageshsnt <ragesh@stayntouch.com>
2018-11-23 10:29:45 +0530

    CICO-59843 Fixed the issue with the shared check-in where user is navigated to the room assignment screen even when the other reservation is already checked-in

--- rover/controllers/reservationCard/rvReservationActionsCtrl.js
+++ rover/controllers/reservationCard/rvReservationActionsCtrl.js
@@ -282,7 +282,7 @@ sntRover.controller('reservationActionsController', [
         $scope.roomAssignmentNeeded = function() {
             if ($scope.reservationData.reservation_card.room_number === '' ||
                     $scope.reservationData.reservation_card.room_status === 'NOTREADY' ||
-                    $scope.reservationData.reservation_card.fo_status === 'OCCUPIED') {
+                    ($scope.reservationData.reservation_card.fo_status === 'OCCUPIED' && !$scope.hasAnySharerCheckedin() )) {
                 if ($scope.reservationData.reservation_card.room_number === '' && $scope.putInQueueClicked) {
                     return true;
                 }
