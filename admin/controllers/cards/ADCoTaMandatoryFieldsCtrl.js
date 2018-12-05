07738749e12a90dde0fb6bfe38eaef8729844fa7
soumyastayntouch <soumya@stayntouch.com>
2018-12-05 18:09:12 +0530

    CICO-59972: Fixed issue of showing AR tab, show fields only if checkbox turned on - admin

--- admin/controllers/cards/ADCoTaMandatoryFieldsCtrl.js
+++ admin/controllers/cards/ADCoTaMandatoryFieldsCtrl.js
@@ -11,6 +11,21 @@ function($scope, $state, ADCoTaMandatorySrv) {
 
 		$scope.callAPI(ADCoTaMandatorySrv.saveCoTaMandatoryFields, options);
 	};
+	$scope.clickedMandatoryCheck = function() {
+		if (!$scope.coTaMandatoryFields.mandatory_on_ar_account) {
+			$scope.coTaMandatoryFields = {
+				tax_id_mandatory: false,
+				pay_days_mandatory: false,
+				contact_name_mandatory: false,
+				address_line1_mandatory: false,
+				city_mandatory: false,
+				postal_code_mandatory: false,
+				country_mandatory: false,
+				contact_phone_mandatory: false,
+				contact_email_address_mandatory: false
+			};
+		}
+	};
 	/*
 	 * To fetch mandatory fields
 	 */
