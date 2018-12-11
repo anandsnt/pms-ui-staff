3f1fb0213e0866cbe599899c5798747c8258cb03
Dilip Sahayadhas <dilips@stayntouch.com>
2018-12-11 14:01:41 -0500

    CICO-60534 Unit tests for LoginSrv checkSession methods

--- login/services/loginSrv.spec.js
+++ login/services/loginSrv.spec.js
@@ -47,6 +47,54 @@ describe('LoginSrv', function() {
             expect(response).toEqual('success');
         });
 
+        it('validate token', function() {
+            var response;
+
+            this.$httpBackend.when('GET', '/login/validate').
+                respond(200, {
+                    is_sp_admin: false,
+                    redirect_url: '/staff'
+                });
+
+            this.loginSrv.checkSession(null,
+                function() {
+                    response = 'success';
+                }, function() {
+                    response = 'failure';
+                }).
+                then(function(data) {
+                    response = data;
+                });
+
+            this.$httpBackend.flush(1);
+
+            expect(response).
+                toEqual('success');
+        });
+
+        it('validate token - expired or missing token', function() {
+            var response,
+                unauthorizedStatusCode = 401;
+
+            this.$httpBackend.when('GET', '/login/validate').
+                respond(unauthorizedStatusCode);
+
+            this.loginSrv.checkSession(null,
+                function() {
+                    response = 'success';
+                }, function() {
+                    response = 'failure';
+                }).
+                then(function(data) {
+                    response = data;
+                });
+
+            this.$httpBackend.flush(1);
+
+            expect(response).
+                toEqual('failure');
+        });
+
         /**
          * CICO-
          */
