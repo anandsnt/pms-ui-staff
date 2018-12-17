81039fbb5b98b2c866ad404ab9ad9c9aa6a387a2
soumyastayntouch <soumya@stayntouch.com>
2018-12-17 17:15:46 +0530

    CICO-60628: Fixed issue of not locking bill

--- rover/controllers/bill/rvMoveChargeCtrl.js
+++ rover/controllers/bill/rvMoveChargeCtrl.js
@@ -207,10 +207,11 @@ sntRover.controller('RVMoveChargeCtrl',
 			};
 			var chargesMovedSuccess = function(response) {
 				$scope.$emit("hideLoader");
+				response.data.toBill = $scope.targetBillId;
 				$scope.$emit('moveChargeSuccsess', response.data);
 				$scope.closeDialog();
 			};
-			var failureCallback = function(data) {
+			var failureCallback = function(data) {moveChargeSuccsessmoveChargeSuccsess
 				console.log("@failureCallback");
 				console.log(data);
                 $scope.errorMessage = data;
