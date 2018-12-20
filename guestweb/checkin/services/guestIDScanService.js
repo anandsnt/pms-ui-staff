9f29b6d8537289fbaf25214d7778562aa959045f
Resheil mohammed <>
2018-12-20 12:15:20 -0500

    CICO-60175 adding activity log

--- guestweb/checkin/services/guestIDScanService.js
+++ guestweb/checkin/services/guestIDScanService.js
@@ -1,24 +1,39 @@
 (function() {
 	var guestIDScanService = function($q, $http) {
 
-	var savePassport = function(params) {
-            var deferred = $q.defer();
-            var url = '/api/guest_identity';
+		var savePassport = function(params) {
+			var deferred = $q.defer();
+			var url = '/api/guest_identity';
 
-            params.application = 'WEB';
+			params.application = 'WEB';
 
-            $http.post(url, params).success(function(response) {
-				this.responseData = response;
-				deferred.resolve(this.responseData);
-			}.bind(this))
-			.error(function() {
-				deferred.reject();
-			});
+			$http.post(url, params).success(function(response) {
+					this.responseData = response;
+					deferred.resolve(this.responseData);
+				}.bind(this))
+				.error(function() {
+					deferred.reject();
+				});
 			return deferred.promise;
-        };	
+		};
+
+		var recordReservationActions = function(params) {
+
+			var deferred = $q.defer(),
+				url = '/api/reservation_actions';
+
+			$http.post(url, params).success(function(response) {
+					deferred.resolve(response);
+				}.bind(this))
+				.error(function() {
+					deferred.reject();
+				});
+			return deferred.promise;
+		};
 
 	return {
-	savePassport: savePassport
+	savePassport: savePassport,
+	recordReservationActions: recordReservationActions
 	};
 };
 
