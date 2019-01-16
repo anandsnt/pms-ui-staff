5ae93a33ccd23b68326f3fa7b107b9fb6a12ecf6
soumyastayntouch <soumya@stayntouch.com>
2019-01-16 19:24:52 +0530

    CICO-61085: Fixed issue of not showing new bill in dropdown

--- rover/controllers/bill/rvRouteDetailsCtrl.js
+++ rover/controllers/bill/rvRouteDetailsCtrl.js
@@ -435,14 +435,13 @@ sntRover.controller('rvRouteDetailsCtrl', ['$scope', '$rootScope', '$filter', 'R
             } else {
                 data.splice(0, 1);
                 $scope.bills = $scope.excludeExistingBills(data);
-                if ($scope.newBillNumber <= 10) {
-                    var newBill = {};
 
-                    newBill.id = 'new';
-                    newBill.is_active = true;
-                    newBill.bill_number = '' + $scope.newBillNumber + '(new)';
-                    $scope.bills.push(newBill);
-                }
+                var newBill = {};
+
+                newBill.id = 'new';
+                newBill.is_active = true;
+                newBill.bill_number = '' + $scope.newBillNumber + '(new)';
+                $scope.bills.push(newBill);
                 $scope.$parent.bills = $scope.bills;
             }
             $scope.selectedEntity.to_bill = $scope.selectedEntity.is_new ? $scope.bills[0].id : $scope.selectedEntity.to_bill;
