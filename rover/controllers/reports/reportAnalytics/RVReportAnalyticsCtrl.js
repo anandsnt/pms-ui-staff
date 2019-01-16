b1daee654cd83c8fd15bf57c9682cc71fcb485d9
resheilSNT <>
2019-01-16 15:46:16 -0500

    loading indicator display

--- rover/controllers/reports/reportAnalytics/RVReportAnalyticsCtrl.js
+++ rover/controllers/reports/reportAnalytics/RVReportAnalyticsCtrl.js
@@ -4,20 +4,27 @@ sntRover.controller('RVReportAnalyticsCtrl', [
 	'$state',
 	'sntAuthorizationSrv',
 	'RVDashboardSrv',
+	'sntActivity',
 	'$window',
 	'$timeout',
-	function($scope, $rootScope, $state, sntAuthorizationSrv, RVDashboardSrv, $window, $timeout) {
+	function($scope, $rootScope, $state, sntAuthorizationSrv, RVDashboardSrv, sntActivity, $window, $timeout) {
 
 		BaseCtrl.call(this, $scope);
 
 		var loadIframe = function() {
+			sntActivity.start('ANALYTICS_IFRAME_LOADING');
 			var hotelUUID = sntAuthorizationSrv.getProperty(),
 				jwt = $window.localStorage.getItem('jwt'),
 				iFrameUrl = '/sisense/analytics/iframe?hotel_uuid=' + hotelUUID + '&auth_token=' + jwt;
-			
-			///iFrameUrl = '/sisense/sessions/login';
 
 			document.getElementById("report-iframe").src = iFrameUrl;
+
+			document.getElementById('report-iframe').onload = function() {
+				sntActivity.stop('ANALYTICS_IFRAME_LOADING');
+			};
+			document.getElementById('report-iframe').onerror = function() {
+				sntActivity.stop('ANALYTICS_IFRAME_LOADING');
+			};
 		};
 
 		var listenToChildEvents = function() {
