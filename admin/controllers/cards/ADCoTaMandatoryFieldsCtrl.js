214ff8274806cf242194a8ce9835ab711aa2998a
soumyastayntouch <soumya@stayntouch.com>
2019-01-29 17:06:53 +0530

    CICO-61687: Toggle actions - visible, mandatory on ar account creation

--- admin/controllers/cards/ADCoTaMandatoryFieldsCtrl.js
+++ admin/controllers/cards/ADCoTaMandatoryFieldsCtrl.js
@@ -2,6 +2,8 @@ admin.controller('ADCoTaMandatoryFieldsCtrl', ['$scope', '$state', 'ADCoTaMandat
 function($scope, $state, ADCoTaMandatorySrv) {
 	BaseCtrl.call(this, $scope);
 	$scope.fieldsAlwaysVisible = ["address_line1_mandatory", "city_mandatory", "postal_code_mandatory", "country_mandatory", "contact_phone_mandatory", "contact_email_address_mandatory", "contact_name_mandatory", "tax_id_mandatory"];
+
+
 	/*
 	 * Save Mandatory fields
 	 */
@@ -31,9 +33,67 @@ function($scope, $state, ADCoTaMandatorySrv) {
 		}
 	};
 
-	$scope.clickedStatus = function(field) {
+	$scope.clickedStatus = function(fieldName) {
+		if (!(_.contains($scope.fieldsAlwaysVisible, fieldName))) {
+			switch (fieldName) {
+				case "e_invoice_mandatory":
+					$scope.coTaMandatoryFields.e_invoice_mandatory.is_visible = !$scope.coTaMandatoryFields.e_invoice_mandatory.is_visible;
+					break;
+				case "regd_tax_office_mandatory":
+					$scope.coTaMandatoryFields.regd_tax_office_mandatory.is_visible = !$scope.coTaMandatoryFields.regd_tax_office_mandatory.is_visible;
+					break;
+				case "organization_id_mandatory":
+					$scope.coTaMandatoryFields.organization_id_mandatory.is_visible = !$scope.coTaMandatoryFields.organization_id_mandatory.is_visible;
+					break;
+			}
+		}
+	};
+	$scope.clickedMandatoryOnArAccountCreation = function (isFieldVisible, fieldName) {
+		if (isFieldVisible) {
 
-
+			switch (fieldName) {
+				case "address_line1_mandatory":
+					$scope.coTaMandatoryFields.address_line1_mandatory.is_visible = !$scope.coTaMandatoryFields.address_line1_mandatory.is_visible;
+					break;
+				case "city_mandatory":
+					$scope.coTaMandatoryFields.city_mandatory.is_visible = !$scope.coTaMandatoryFields.city_mandatory.is_visible;
+					break;
+				case "postal_code_mandatory":
+					$scope.coTaMandatoryFields.postal_code_mandatory.is_visible = !$scope.coTaMandatoryFields.postal_code_mandatory.is_visible;
+					break;
+				case "country_mandatory":
+					$scope.coTaMandatoryFields.country_mandatory.is_visible = !$scope.coTaMandatoryFields.country_mandatory.is_visible;
+					break;
+				case "contact_phone_mandatory":
+					$scope.coTaMandatoryFields.contact_phone_mandatory.is_visible = !$scope.coTaMandatoryFields.contact_phone_mandatory.is_visible;
+					break;
+				case "contact_email_address_mandatory":
+					$scope.coTaMandatoryFields.contact_email_address_mandatory.is_visible = !$scope.coTaMandatoryFields.contact_email_address_mandatory.is_visible;
+					break;
+				case "contact_name_mandatory":
+					$scope.coTaMandatoryFields.contact_name_mandatory.is_visible = !$scope.coTaMandatoryFields.contact_name_mandatory.is_visible;
+					break;
+				case "tax_id_mandatory":
+					$scope.coTaMandatoryFields.tax_id_mandatory.is_visible = !$scope.coTaMandatoryFields.tax_id_mandatory.is_visible;
+					break;
+				case "e_invoice_mandatory":
+					$scope.coTaMandatoryFields.e_invoice_mandatory.is_visible = !$scope.coTaMandatoryFields.e_invoice_mandatory.is_visible;
+					break;
+				case "regd_tax_office_mandatory":
+					$scope.coTaMandatoryFields.regd_tax_office_mandatory.is_visible = !$scope.coTaMandatoryFields.regd_tax_office_mandatory.is_visible;
+					break;
+				case "organization_id_mandatory":
+					$scope.coTaMandatoryFields.organization_id_mandatory.is_visible = !$scope.coTaMandatoryFields.organization_id_mandatory.is_visible;
+					break;
+			}
+		}
+	};
+	$scope.clickedMandatoryOnAccountCreation = function (isFieldVisible, isMandatoryOnAccountCreation) {
+		if (isFieldVisible) {
+			isMandatoryOnAccountCreation = !isMandatoryOnAccountCreation;
+		} else {
+			isMandatoryOnAccountCreation = false;
+		}
 	};
 	/*
 	 * To fetch mandatory fields
