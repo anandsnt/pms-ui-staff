3ec9bdff58b4ac6377b1bb61989d9981039a5ac1
krishobh <krishobh@stayntouch.com>
2019-02-20 15:38:04 +0530

    Updated AssignRoomContainer

--- rover/react/nightlyDiary/containers/rvNightlyDiaryAssignRoomContainer.js
+++ rover/react/nightlyDiary/containers/rvNightlyDiaryAssignRoomContainer.js
@@ -3,40 +3,40 @@ const { connect } = ReactRedux;
 let calculateAssignRoomPositionAndDuration = (diaryInitialDayOfDateGrid, uarData, numberOfDays) => {
     let nightDuration = NIGHTLY_DIARY_CONST.RESERVATION_ROW_WIDTH / numberOfDays;
     let diaryInitialDate = tzIndependentDate(diaryInitialDayOfDateGrid);
-    let unassignedRoomStartDate = tzIndependentDate(uarData.fromDate);
+    let assignRoomStartDate = tzIndependentDate(uarData.fromDate);
 
     let oneDay = 24 * 60 * 60 * 1000;
-    let diffBtwInitialAndStartDate = unassignedRoomStartDate.getTime() - diaryInitialDate.getTime();
+    let diffBtwInitialAndStartDate = assignRoomStartDate.getTime() - diaryInitialDate.getTime();
     let noOfDaysBtwInitialAndArrivalDate = Math.abs((diffBtwInitialAndStartDate) / (oneDay));
 
-    let unAssignedRoomPosition = 0;
+    let assignRoomPosition = 0;
 
     if (noOfDaysBtwInitialAndArrivalDate > 0) {
-        unAssignedRoomPosition = noOfDaysBtwInitialAndArrivalDate * nightDuration;
+        assignRoomPosition = noOfDaysBtwInitialAndArrivalDate * nightDuration;
     }
     if (noOfDaysBtwInitialAndArrivalDate >= 0) {
         if (numberOfDays === NIGHTLY_DIARY_CONST.DAYS_7) {
-            unAssignedRoomPosition = unAssignedRoomPosition + NIGHTLY_DIARY_CONST.DAYS_POSITION_ADD_7;
+            assignRoomPosition = assignRoomPosition + NIGHTLY_DIARY_CONST.DAYS_POSITION_ADD_7;
         } else if (numberOfDays === NIGHTLY_DIARY_CONST.DAYS_21) {
-            unAssignedRoomPosition = unAssignedRoomPosition + NIGHTLY_DIARY_CONST.DAYS_POSITION_ADD_21;
+            assignRoomPosition = assignRoomPosition + NIGHTLY_DIARY_CONST.DAYS_POSITION_ADD_21;
         }
     }
 
-    let durationOfUnAssignedRoom = 0;
+    let durationOfAssignRoom = 0;
     let numberOfNightsVisibleInGrid = (uarData.nights <= 1) ? 1 : uarData.nights;
     
-    durationOfUnAssignedRoom = numberOfNightsVisibleInGrid * nightDuration;
+    durationOfAssignRoom = numberOfNightsVisibleInGrid * nightDuration;
     if (numberOfDays === NIGHTLY_DIARY_CONST.DAYS_7) {
-        durationOfUnAssignedRoom = durationOfUnAssignedRoom - 2 * NIGHTLY_DIARY_CONST.DAYS_POSITION_ADD_7;
+        durationOfAssignRoom = durationOfAssignRoom - 2 * NIGHTLY_DIARY_CONST.DAYS_POSITION_ADD_7;
     } else if (numberOfDays === NIGHTLY_DIARY_CONST.DAYS_21) {
-        durationOfUnAssignedRoom = durationOfUnAssignedRoom - 2 * NIGHTLY_DIARY_CONST.DAYS_POSITION_ADD_21;
+        durationOfAssignRoom = durationOfAssignRoom - 2 * NIGHTLY_DIARY_CONST.DAYS_POSITION_ADD_21;
     }
     var returnData = {};
 
     returnData.numberOfNightsVisibleInGrid = numberOfNightsVisibleInGrid;
     returnData.style = {};
-    returnData.style.width = durationOfUnAssignedRoom + "px";
-    returnData.style.transform = "translateX(" + unAssignedRoomPosition + "px)";
+    returnData.style.width = durationOfAssignRoom + "px";
+    returnData.style.transform = "translateX(" + assignRoomPosition + "px)";
 
     return returnData;
 };
