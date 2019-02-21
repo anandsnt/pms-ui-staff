f5fd2131c35659de62818286b84da94c1924b050
Jason Lombardozzi <jason@stayntouch.com>
2019-02-21 11:51:06 -0500

    CICO-62751 Migrate all legacy mappings from PMS to IFC - AccountView
    * Added generic mapping module that will require further improvements for supporting method mapping types
    * New rake task to sync PMS external mappings to IFC for an integration
    * Moved AccountView mappings page to tab within setup page
    * Also added an extension to IFC gem. The goal is to deprecate the gem soon. The extension offers a simple RPC caller and hooking system used for proxy calls to IFC
    * Removed a duplicated memcached flush rake task

---
+++ admin/services/interfaces/adInterfaceMappingSrv.js
@@ -0,0 +1,34 @@
+admin.service('adInterfaceMappingSrv', [
+    '$http', '$q', 'ADBaseWebSrvV2', function($http, $q, ADBaseWebSrvV2) {
+        var service = this;
+
+        service.getAllChargeCodes = function() {
+            return ADBaseWebSrvV2.getJSON('/admin/charge_codes/list.json?per_page=1000');
+        };
+
+        service.createMapping = function(params) {
+            var interface = params.interface.toLowerCase();
+            delete params.interface;
+
+            return ADBaseWebSrvV2.postJSON('/ifc/' + interface + '/mappings', params);
+        };
+
+        service.fetchMappings = function(params) {
+            var interface = params.interface.toLowerCase();
+            delete params.interface;
+
+            return ADBaseWebSrvV2.getJSON('/ifc/' + interface + '/mappings', params);
+        };
+
+        service.deleteMapping = function(params) {
+            return ADBaseWebSrvV2.deleteJSON('/ifc/' + params.interface.toLowerCase() + '/mappings/' + params.id);
+        };
+
+        service.updateMapping = function(params) {
+            var interface = params.interface.toLowerCase();
+            delete params.interface;
+
+            return ADBaseWebSrvV2.putJSON('/ifc/' + interface + '/mappings/' + params.id, params);
+        };
+    }])
+;
