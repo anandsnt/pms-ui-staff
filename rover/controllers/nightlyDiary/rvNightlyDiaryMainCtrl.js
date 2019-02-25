7cabc0b2f23e8a64ed28fcd343e2f57d2a339916
krishobh <krishobh@stayntouch.com>
2019-02-25 19:14:33 +0530

    CICO-36015 Hide move button

--- rover/controllers/nightlyDiary/rvNightlyDiaryMainCtrl.js
+++ rover/controllers/nightlyDiary/rvNightlyDiaryMainCtrl.js
@@ -84,6 +84,7 @@ angular.module('sntRover')
                         unassignedReservationList: unassignedReservationList,
                         hasOverlay: false,
                         isEditReservationMode: false,
+                        hideMoveButton: false,
                         showUnassignedPanel: false,
                         showUnassignedReservations: false,
                         innerWidth: screen.width,
@@ -236,7 +237,7 @@ angular.module('sntRover')
                     var srvParams = {};
 
                     $scope.diaryData.showSaveChangeButtonAfterShortenOrExtent.show = false;
-
+                    $scope.diaryData.hideMoveButton = reservation.do_not_move;
                     if (!$scope.diaryData.isEditReservationMode) {
                         $scope.diaryData.isEditReservationMode = true;
                         $scope.currentSelectedReservation = reservation;
