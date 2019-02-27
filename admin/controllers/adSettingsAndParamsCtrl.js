e511f4c33d81d783d2acd5512d8b34b901e641c9
rahulkookal <rahul@stayntouch.com>
2019-02-27 13:06:33 +0530

    CICO-63285: Completed admin setting for multicurruncy

--- admin/controllers/adSettingsAndParamsCtrl.js
+++ admin/controllers/adSettingsAndParamsCtrl.js
@@ -1,23 +1,24 @@
 admin.controller('settingsAndParamsCtrl', ['$scope', 'settingsAndParamsSrv', 'settingsAndParamsData', 'chargeCodes', function($scope, settingsAndParamsSrv, settingsAndParamsData, chargeCodes) {
 
-	BaseCtrl.call(this, $scope);
-	$scope.ccBatchProcessingOptions = [{
-            "value": "PAYMENT_GATEWAY",
-            "name": "Payment Gateway"
-        },
-        {
-            "value": "HOTEL",
-            "name": "Hotel"
-        }];
+    BaseCtrl.call(this, $scope);
+    $scope.ccBatchProcessingOptions = [{
+        'value': 'PAYMENT_GATEWAY',
+        'name': 'Payment Gateway'
+    },
+    {
+        'value': 'HOTEL',
+        'name': 'Hotel'
+    }];
 
-    $scope.cc_batch_processing = settingsAndParamsData.cc_batch_processing === null ? "" : settingsAndParamsData.cc_batch_processing;
+    $scope.cc_batch_processing = settingsAndParamsData.cc_batch_processing === null ? '' : settingsAndParamsData.cc_batch_processing;
     $scope.cc_auto_settlement_by_eod = settingsAndParamsData.cc_auto_settlement_by_eod;
 
-	$scope.hours = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
-	$scope.minutes = ["00", "15", "30", "45"];
+    $scope.hours = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+    $scope.minutes = ['00', '15', '30', '45'];
 
 
     $scope.data = settingsAndParamsData.business_date;
+    $scope.currency_list = settingsAndParamsData.currency_list;
     $scope.chargeCodes = chargeCodes;
     $scope.selected_charge_code = settingsAndParamsData.no_show_charge_code_id;
     $scope.selected_group_charge_code = settingsAndParamsData.group_charge_code_id;
@@ -25,7 +26,7 @@ admin.controller('settingsAndParamsCtrl', ['$scope', 'settingsAndParamsSrv', 'se
     $scope.check_guest_auth_for_interface_postings = settingsAndParamsData.check_guest_auth_for_interface_postings;
     $scope.auto_charge_deposit = settingsAndParamsData.auto_charge_deposit;
     $scope.is_multi_currency = settingsAndParamsData.is_multi_currency;
-    $scope.local_currency = settingsAndParamsData.local_currency; 
+    $scope.selected_local_currency_id = settingsAndParamsData.local_currency_id;
 
     /**
     * To handle save button action
@@ -34,21 +35,24 @@ admin.controller('settingsAndParamsCtrl', ['$scope', 'settingsAndParamsSrv', 'se
     $scope.saveClick = function() {
 
         var saveDetailsSuccessCallback = function() {
-             $scope.$emit('hideLoader');
-             $scope.goBackToPreviousState();
+            $scope.$emit('hideLoader');
+            $scope.goBackToPreviousState();
             
         };
-        var selectedChargeCode = ( typeof $scope.selected_charge_code === 'undefined' ) ? "" : $scope.selected_charge_code;
-        var groupChargeCode = ( typeof $scope.selected_group_charge_code === 'undefined' ) ? "" : $scope.selected_group_charge_code;
-        var dataToSend = {	"no_show_charge_code_id": selectedChargeCode,
-        					"business_date": $scope.data,
-        					"group_charge_code_id": groupChargeCode,
-        					"cc_batch_processing": $scope.cc_batch_processing,
-        					"cc_auto_settlement_by_eod": $scope.cc_auto_settlement_by_eod,
-                            "email_recipients_for_eod_reports": $scope.emailRecipientsForEodReports,
-                            "check_guest_auth_for_interface_postings": $scope.check_guest_auth_for_interface_postings,
-                            "auto_charge_deposit": $scope.auto_charge_deposit
-        				};
+        var selectedChargeCode = typeof $scope.selected_charge_code === 'undefined' ? '' : $scope.selected_charge_code;
+        var groupChargeCode = typeof $scope.selected_group_charge_code === 'undefined' ? '' : $scope.selected_group_charge_code;
+        var dataToSend = {
+            'no_show_charge_code_id': selectedChargeCode,
+            'business_date': $scope.data,
+            'group_charge_code_id': groupChargeCode,
+            'cc_batch_processing': $scope.cc_batch_processing,
+            'cc_auto_settlement_by_eod': $scope.cc_auto_settlement_by_eod,
+            'email_recipients_for_eod_reports': $scope.emailRecipientsForEodReports,
+            'check_guest_auth_for_interface_postings': $scope.check_guest_auth_for_interface_postings,
+            'auto_charge_deposit': $scope.auto_charge_deposit,
+            'local_currency': $scope.local_currency,
+            'local_currency_id': $scope.selected_local_currency_id
+        };
 
         $scope.invokeApi(settingsAndParamsSrv.saveSettingsAndParamsSrv, dataToSend, saveDetailsSuccessCallback);
     };
--- rover/controllers/financials/currencyExchange/RVCurrencyExchangeModalCtrl.js
+++ rover/controllers/financials/currencyExchange/RVCurrencyExchangeModalCtrl.js
@@ -1,18 +1,28 @@
-sntRover.controller('RVCurrencyExchangeModalController', 
+sntRover.controller('RVCurrencyExchangeModalController',
     ['$scope', 
-    '$rootScope', 
-    '$filter', function($scope, $rootScope, $filter) {
+        '$rootScope', 
+        '$filter', function($scope, $rootScope, $filter) {
 
-    BaseCtrl.call(this, $scope);
+            BaseCtrl.call(this, $scope);
     
-    $scope.exchangeRates = {};
-    $scope.exchangeRates.data = [{ "date": '20-02-2019', "exchange_rate": 12.33 },{ "date": '21-02-2019', "exchange_rate": 12.33 },{ "date": '22-02-2019', "exchange_rate": 12.33 },{ "date": '23-02-2019', "exchange_rate": 12.33 },{ "date": '24-02-2019', "exchange_rate": 12.33 }, { "date":'25-02-2019',  "exchange_rate": 13.33 }];
+            $scope.exchangeRates = {};
+            $scope.exchangeRates.data = [{ 'date': '20-02-2019',
+                'exchange_rate': 12.33 }, { 'date': '21-02-2019',
+                    'exchange_rate': 12.33 }, { 'date': '22-02-2019',
+                        'exchange_rate': 12.33 }, { 'date': '23-02-2019',
+                            'exchange_rate': 12.33 }, { 'date': '24-02-2019',
+                                'exchange_rate': 12.33 }, { 'date': '25-02-2019',
+                                    'exchange_rate': 13.33 }];
 
 
-    angular.forEach($scope.exchangeRates.data, function(item, index) {
-		item.day  = moment(item.date, "DD-MM-YYYY").format('dddd');
-	});
+            angular.forEach($scope.exchangeRates.data, function(item, index) {
+                item.day = moment(item.date, 'DD-MM-YYYY').format('dddd');
+            });
 
-    init();
+            var init = function() {
 
-}]);
+            };
+
+            init();
+
+        }]);
