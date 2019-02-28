e7419fd89b5afcdd79b61525f407b528d929d5a5
rahulkookal <rahul@stayntouch.com>
2019-02-28 17:12:10 +0530

    CICO-62813: Fixed CC issues, Removed unwanted files

--- admin/services/adAdminUser/adAdminUserSrv.js
+++ admin/services/adAdminUser/adAdminUserSrv.js
@@ -1,13 +1,9 @@
-admin.service('ADAdminUserSrv', ['$http', '$q', 'ADBaseWebSrv', 'ADBaseWebSrvV2', 'ADHotelListSrv',
-    function( $http, $q, ADBaseWebSrv, ADBaseWebSrvV2, ADHotelListSrv) {
-
-
-        var that = this;
-
-       /**
-        * To fetch the list of users
-        * @return {object} users list json
-        */
+admin.service('ADAdminUserSrv', ['$http', '$q', 'ADBaseWebSrv', 'ADBaseWebSrvV2',
+    function( $http, $q, ADBaseWebSrv, ADBaseWebSrvV2) {
+        /**
+         * To fetch the list of users
+         * @return {object} users list json
+         */
 
         this.fetch = function(params) {
 
@@ -21,11 +17,11 @@ admin.service('ADAdminUserSrv', ['$http', '$q', 'ADBaseWebSrv', 'ADBaseWebSrvV2'
             });
             return deferred.promise;
         };
-	/**
-    * To fetch the details of users
-    * @param {object} id of the clicked user
-    * @return {object} users details json
-    */
+        /*
+         * To fetch the details of users
+         * @param {object} id of the clicked user
+         * @return {object} users details json
+         */
         this.getUserDetails = function(data) {
             var id = data.id;
             var deferred = $q.defer();
@@ -39,30 +35,18 @@ admin.service('ADAdminUserSrv', ['$http', '$q', 'ADBaseWebSrv', 'ADBaseWebSrvV2'
             return deferred.promise;
         };
 
-	/**
-    * To add new user details
-    * @param {object} data - data to be added
-    * @return {object}
-    */
-        this.saveUserDetails = function(data) {
-            var newDataToArray = {
-                'full_name': data.first_name + ' ' + data.last_name,
-                'email': data.email,
-                'department': that.getDepartmentName(data.user_department),
-                'last_login': '',
-                'is_active': 'false',
-                'can_delete': 'true'
-            };
+        /*
+         * To update user details
+         * @param {object} data - data to be updated
+         * @return {object}
+         */
+        this.updateUserDetails = function(data) {
+
             var deferred = $q.defer();
-            var url = '/admin/users';
+            var url = '/admin/users/' + data.user_id;
 
-            if (data.isSNTAdmin) {
-                url += '?hotel_uuid=' + ADHotelListSrv.getSelectedProperty();
-            }
-
-            ADBaseWebSrv.postJSON(url, data).then(function(data) {
-                newDataToArray.id = data.user_id;
-                that.addToUsersArray(newDataToArray);
+            data.admin_only = true;
+            ADBaseWebSrv.putJSON(url, data).then(function(data) {
                 deferred.resolve(data);
             }, function(data) {
                 deferred.reject(data);
@@ -70,11 +54,41 @@ admin.service('ADAdminUserSrv', ['$http', '$q', 'ADBaseWebSrv', 'ADBaseWebSrvV2'
             return deferred.promise;
 
         };
-	/**
-    * To activate/inactivate user
-    * @param {object} data - data to activate/inactivate
-    * @return {object}
-    */
+        /*
+         * To add new user details
+         * @param {object} data - data to be added
+         * @return {object}
+         */
+        this.saveUserDetails = function(data) {
+            var params = {
+                'first_name': data.first_name,
+                'last_name': data.last_name,
+                'job_title': data.job_title,
+                'email': data.email,
+                'confirm_email': data.confirm_email,
+                'password': data.password,
+                'confirm_password': data.confirm_password,
+                'user_photo': data.user_photo,
+                'admin_only': true
+            };
+
+            var deferred = $q.defer();
+            var url = '/admin/users';
+
+            ADBaseWebSrv.postJSON(url, params).then(function(data) {
+                deferred.resolve(data);
+            }, function(data) {
+                deferred.reject(data);
+            });
+            return deferred.promise;
+
+        };
+
+        /*
+         * To activate/inactivate user
+         * @param {object} data - data to activate/inactivate
+         * @return {object}
+         */
         this.activateInactivate = function(data) {
 
             var deferred = $q.defer();
@@ -88,33 +102,11 @@ admin.service('ADAdminUserSrv', ['$http', '$q', 'ADBaseWebSrv', 'ADBaseWebSrvV2'
             return deferred.promise;
 
         };
-	/**
-    * To delete user
-    * @param {object} data - data to delete
-    * @return {object}
-    */
-        this.deleteUser = function(data) {
-
-            var deferred = $q.defer();
-            var url = '/admin/users/' + data.id;
-            var itemToRemove = data.index;
-
-            delete data['index'];
-
-            ADBaseWebSrvV2.deleteJSON(url, data).then(function(data) {
-                that.usersArray.users.splice(itemToRemove, 1);
-                deferred.resolve(data);
-            }, function(data) {
-                deferred.reject(data);
-            });
-            return deferred.promise;
-
-        };
-   /**
-    * To send invitation mail
-    * @param {object} data - user id
-    * @return {object}  status
-    */
+        /**
+         * To send invitation mail
+         * @param {object} data - user id
+         * @return {object}  status
+         */
         this.sendInvitation = function(data) {
             var deferred = $q.defer();
             var url = '/admin/user/send_invitation';
