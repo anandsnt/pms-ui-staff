803ef3f2647f9d9a093d9c15a5863c4878b0d511
rahulkookal <rahul@stayntouch.com>
2019-02-28 11:13:12 +0530

    CICO-63285: Intermediate commit - Muticurrency exchane rate

--- rover/controllers/financials/currencyExchange/RVCurrencyExchangeModalCtrl.js
+++ rover/controllers/financials/currencyExchange/RVCurrencyExchangeModalCtrl.js
@@ -1,26 +1,120 @@
 sntRover.controller('RVCurrencyExchangeModalController',
     ['$scope', 
         '$rootScope', 
-        '$filter', function($scope, $rootScope, $filter) {
+        '$filter',
+        '$timeout',
+        'rvUtilSrv',
+        'RVMultiCurrencyExchangeSrv',
+        function($scope, $rootScope, $filter, $timeout, util, RVMultiCurrencyExchangeSrv) {
+
 
             BaseCtrl.call(this, $scope);
+
+            var commonDateOptions = {
+                    dateFormat: $rootScope.jqDateFormat,
+                    changeYear: true,
+                    changeMonth: true,
+                    yearRange: '-10:'
+                },
+                setStartDateOptions = function () {
+                    $scope.startDateOptions = _.extend({
+                        onSelect: startDateChoosed
+                    }, commonDateOptions);
+                },
+                startDateChoosed = function(date, datePicker) {
+                    var startDate = new tzIndependentDate(util.get_date_from_date_picker(datePicker));
+
+                    $scope.start_date = $filter('date')(startDate, $rootScope.dateFormat);
+                    $scope.end_date = $filter('date')(tzIndependentDate(moment(startDate).add(7, 'days')
+                    .calendar()), $rootScope.dateFormat);
+                    $timeout(function() {
+                        $rootScope.apply();
+                    }, 100);
+                },
+                setEndDateOptions = function () {
+                    $scope.endDateOptions = _.extend({
+                        onSelect: endDateChoosed
+                    }, commonDateOptions);
+                },
+                endDateChoosed = function(date, datePicker) {
+                    var endDate = new tzIndependentDate(util.get_date_from_date_picker(datePicker));
+
+                    $scope.end_date = $filter('date')(endDate, $rootScope.dateFormat);
+                    $scope.start_date = $filter('date')(tzIndependentDate(moment(endDate).subtract(7, 'days')
+                    .calendar()), $rootScope.dateFormat);
+
+                    $timeout(function() {
+                        $rootScope.apply();
+                    }, 200);
+                },
+                fetchExhangeRates = function() {
+
+                    var successCallBackFetchAccountsReceivables = function(data) {
+                        //$scope.exchangeRates = data;
+                    };
+
+                    var params = {
+                        'start_date': $scope.start_date,
+                        'end_date': $scope.end_date
+                    };
+
+                    $scope.invokeApi(RVMultiCurrencyExchangeSrv.fetchExchangeRates, params, successCallBackFetchAccountsReceivables );
+                },
+                isDateDisabled = function(){
+                   return false;
+                },
+                constructExchangeRateArray = function(date) {
+                    var startDate = moment(date),
+                        ExchangeRateArray = [];
+
+                    for (var i = 0; i < 7; i++) {
+                        ExchangeRateArray[i] = {
+                            day : startDate.format('dddd'),
+                            date : $filter('date')(tzIndependentDate(startDate.calendar()), $rootScope.dateFormat),
+                            exchange_rate : null,
+                            isDisabled : isDateDisabled(startDate)
+                        };
+                        startDate = startDate.add(1, 'days');
+                    }
+
+                    return ExchangeRateArray;
+                };
     
             $scope.exchangeRates = {};
             $scope.exchangeRates.data = [{ 'date': '20-02-2019',
-                'exchange_rate': 12.33 }, { 'date': '21-02-2019',
-                    'exchange_rate': 12.33 }, { 'date': '22-02-2019',
-                        'exchange_rate': 12.33 }, { 'date': '23-02-2019',
-                            'exchange_rate': 12.33 }, { 'date': '24-02-2019',
-                                'exchange_rate': 12.33 }, { 'date': '25-02-2019',
-                                    'exchange_rate': 13.33 }];
+                'conversion_rate': 12.33 }, { 'date': '21-02-2019',
+                    'conversion_rate': 12.33 }, { 'date': '22-02-2019',
+                        'conversion_rate': 12.33 }, { 'date': '23-02-2019',
+                            'conversion_rate': 12.33 }, { 'date': '24-02-2019',
+                                'conversion_rate': 12.33 }, { 'date': '25-02-2019',
+                                    'conversion_rate': 13.33 }];
 
 
             angular.forEach($scope.exchangeRates.data, function(item, index) {
                 item.day = moment(item.date, 'DD-MM-YYYY').format('dddd');
             });
 
-            var init = function() {
+            $scope.save = function() {
+                var successCallBackFetchAccountsReceivables = function(data) {
+                    //$scope.exchangeRates = data;
+                };
 
+                var params = {
+                    exchange_rates: $scope.exchangeRates
+                };
+
+                $scope.invokeApi(RVMultiCurrencyExchangeSrv.saveExchangeRates, params, successCallBackFetchAccountsReceivables );
+
+            }
+
+            var init = function() {
+                $scope.start_date = $filter('date')(tzIndependentDate($rootScope.businessDate), $rootScope.dateFormat);
+                $scope.end_date = $filter('date')(tzIndependentDate(moment($rootScope.businessDate).add(7, 'days')
+                .calendar()), $rootScope.dateFormat);
+                $scope.exchangeRates = constructExchangeRateArray($scope.start_date);
+                setStartDateOptions();
+                setEndDateOptions();
+                fetchExhangeRates();
             };
 
             init();
---
+++ rover/services/financials/rvMultiCurrencyExchangeModalSrv.js
@@ -0,0 +1,42 @@
+angular.module('sntRover').service('RVMultiCurrencyExchangeSrv', ['$http', '$q', 'BaseWebSrvV2', function($http, $q, BaseWebSrvV2) {
+
+    var that = this;
+    /*
+     * Service function to fetch Accounts Receivables
+     * @return {object} payments
+     */
+
+    that.fetchExchangeRates = function (params) {
+
+        var deferred = $q.defer();
+        var url = "/api/exchange_rates/current_exchange_rates";
+
+        BaseWebSrvV2.getJSON(url, params).then(function (data) {
+            var data1 = [{ 'date': '20-02-2019',
+                'exchange_rate': 12.33 }, { 'date': '21-02-2019',
+                'exchange_rate': 12.33 }, { 'date': '22-02-2019',
+                'exchange_rate': 12.33 }, { 'date': '23-02-2019',
+                'exchange_rate': 12.33 }, { 'date': '24-02-2019',
+                'exchange_rate': 12.33 }, { 'date': '25-02-2019',
+                'exchange_rate': 13.33 }];
+            deferred.resolve(data1);
+        }, function (data) {
+            deferred.reject(data);
+        });
+        return deferred.promise;
+    };
+
+    that.saveExchangeRates = function (params) {
+
+        var deferred = $q.defer();
+        var url = "/api/exchange_rates/save_exchange_rates";
+
+        BaseWebSrvV2.postJSON(url, params).then(function (data) {
+            deferred.resolve(data);
+        }, function (data) {
+            deferred.reject(data);
+        });
+        return deferred.promise;
+    };
+
+}]);
