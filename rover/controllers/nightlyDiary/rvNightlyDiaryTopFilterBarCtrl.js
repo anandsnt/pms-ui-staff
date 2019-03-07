a0e03cb9758f9512658a8f6da5fc7da268af71b3
krishobh <krishobh@stayntouch.com>
2019-03-07 13:25:54 +0530

    CICO-61621 Controller update

--- rover/controllers/nightlyDiary/rvNightlyDiaryTopFilterBarCtrl.js
+++ rover/controllers/nightlyDiary/rvNightlyDiaryTopFilterBarCtrl.js
@@ -108,6 +108,7 @@ angular.module('sntRover')
             $scope.$emit('UPDATE_UNASSIGNED_RESERVATIONLIST');
             $scope.$emit('UPDATE_RESERVATIONLIST');
             isDateChangedFromInitialState = true;
+            $scope.diaryData.arrivalDate = $scope.diaryData.fromDate;
         });
         // Catching event from main controller, when API is completed.
         $scope.addListener('FETCH_COMPLETED_DATE_LIST_DATA', function() {
