5bcaa9951dcbf19a40a6f4519151db460a0f203c
rahulkookal <rahul@stayntouch.com>
2019-03-08 16:19:02 +0530

    CICO-63234: Clarion Collection® Hotel Bristol - Zest Station

--- asset_list/theming/zeststation/css/css_theme_mapping.js
+++ asset_list/theming/zeststation/css/css_theme_mapping.js
@@ -60,7 +60,8 @@ module.exports = {
             'twa': [themeCSSRoot + 'twa.less'],
             'carrollton-inn': [themeCSSRoot + 'carrollton-inn.less'],
             'match': [themeCSSRoot + 'match.less'],
-            'liason': [themeCSSRoot + 'liason.less']
+            'liason': [themeCSSRoot + 'liason.less'],
+            'clarion-collection': [themeCSSRoot + 'clarion-collection.less']
         };
     }
 };
