c52290c7e5f50c79d78ade4d2d2f4d79fdbc8da6
rahulkookal <rahul@stayntouch.com>
2019-03-12 19:25:03 +0530

    CICO-63111: Merrion Row Hotel and Public House - Zest Web

--- asset_list/theming/guestweb/css/css_theme_mapping.js
+++ asset_list/theming/guestweb/css/css_theme_mapping.js
@@ -5,6 +5,7 @@ module.exports = {
         var themeCSSRoot = 'stylesheets/guestweb/';
 
         return {
+            'guestweb_merrion_row_hotel_and_public_house': [ themeCSSRoot + 'guestweb_merrion_row_hotel_and_public_house.css'],
             'guestweb_clarion_collection': [ themeCSSRoot + 'guestweb_clarion_collection.css'],
             'guestweb_la_copa_inn': [ themeCSSRoot + 'guestweb_la_copa_inn.css'],
             'guestweb_hotel_e': [ themeCSSRoot + 'guestweb_hotel_e.css'],
