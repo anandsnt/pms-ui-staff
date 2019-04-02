cd15ff75f3fb8845b0b8c0b31528772f711b376b
rahulkookal <rahul@stayntouch.com>
2019-04-02 11:13:16 +0530

    CICO-62531: Inn at Discovery Coast - Zest Web

--- asset_list/theming/guestweb/css/css_theme_mapping.js
+++ asset_list/theming/guestweb/css/css_theme_mapping.js
@@ -5,6 +5,7 @@ module.exports = {
         var themeCSSRoot = 'stylesheets/guestweb/';
 
         return {
+            'guestweb_inn_at_discovery_coast': [ themeCSSRoot + 'guestweb_inn_at_discovery_coast.css'],
             'guestweb_qbic_hotels': [ themeCSSRoot + 'guestweb_qbic_hotels.css'],
             'guestweb_maison_de_la_luz': [ themeCSSRoot + 'guestweb_maison_de_la_luz.css'],
             'guestweb_ruby_hospitality': [ themeCSSRoot + 'guestweb_ruby_hospitality.css'],
