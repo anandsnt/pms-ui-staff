0009dc3d04eb9cf1af7fe96556deee18eacf706a
soumyastayntouch <soumya@stayntouch.com>
2019-04-03 18:26:52 +0530

    CICO-64336: Code climate issues

--- rover/controllers/rvContactInfoController.js
+++ rover/controllers/rvContactInfoController.js
@@ -289,21 +289,30 @@ angular.module('sntRover').controller('RVContactInfoController', ['$scope', '$ro
         /*
          * To get the deafult class 
          */
-        $scope.getDefaultClass  = function(fieldName) {
+        $scope.getDefaultClass = function(fieldName) {
             var visibleCount = 0;
 
             if (fieldName === "vehicle_registration_number") {
-                $scope.guestCardData.contactInfo.is_father_name_visible ? visibleCount++ : '';
-                $scope.guestCardData.contactInfo.is_mother_name_visible ? visibleCount++ : '';
-                $scope.guestCardData.contactInfo.is_birth_place_visible ? visibleCount++ : '';
-                $scope.guestCardData.contactInfo.is_gender_visible ? visibleCount++ : '';
+                if ($scope.guestCardData.contactInfo.is_father_name_visible) {
+                    visibleCount++;
+                }
+                if ($scope.guestCardData.contactInfo.is_mother_name_visible) {
+                    visibleCount++;
+                }
+                if ($scope.guestCardData.contactInfo.is_birth_place_visible) {
+                    visibleCount++;
+                }
+                if ($scope.guestCardData.contactInfo.is_gender_visible) {
+                    visibleCount++;
+                }
+
                 if ( visibleCount % 2 === 0) {
                     return 'margin';
                 }
 
                 return '';
             }
-        }
+        };
 
         init();
     }
