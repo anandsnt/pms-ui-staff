7ca24396101ab0843acf6ead99a58bd137f99be2
Andrew Swe <andrew@stayntouch.com>
2019-04-09 12:00:46 -0400

    CICO-63880 remove debugger

--- admin/controllers/interfaces/Common/adInterfaceMappingCtrl.js
+++ admin/controllers/interfaces/Common/adInterfaceMappingCtrl.js
@@ -65,7 +65,6 @@ admin.controller('adInterfaceMappingCtrl', [
                         $scope.state.meta = meta;
                         $scope.mapping = fetchEmptyMapping();
                         $scope.state.mode = 'ADD';
-                        debugger
                     }
                 });
             } else {
