976db47095f7584f993c137e0cd72f1628aedc25
Jason Lombardozzi <jason@stayntouch.com>
2019-04-12 15:47:22 -0400

    CICO-64125 Comtrol settings migration to use IFC proxy APIs
    Remove old controllers, update interface monitor screen

--- admin/controllers/interfaces/Comtrol/adComtrolCtrl.js
+++ admin/controllers/interfaces/Comtrol/adComtrolCtrl.js
@@ -1,6 +1,14 @@
-admin.controller('adComtrolCtrl', ['$scope', 'config', 'adInterfacesSrv', 'ngDialog', 'adComtrolSrv',
-    function($scope, config, adInterfacesSrv, ngDialog, adComtrolSrv) {
+admin.controller('adComtrolCtrl', ['$scope', 'config', 'adInterfacesSrv', 'ngDialog', 'adIFCSrv',
+    function ($scope, config, adInterfacesSrv, ngDialog, adIFCSrv) {
         BaseCtrl.call(this, $scope);
+
+        var resetToken = function() {
+            return adIFCSrv.post('authentication', 'reset_token', {integration: 'comtrol'})
+        };
+
+        var fetchToken = function() {
+            return adIFCSrv.get('authentication', 'token', {integration: 'comtrol'})
+        };
 
         $scope.interface = 'COMTROL';
 
@@ -8,7 +16,7 @@ admin.controller('adComtrolCtrl', ['$scope', 'config', 'adInterfacesSrv', 'ngDia
             activeTab: 'SETTING'
         };
 
-        $scope.toggleEnabled = function() {
+        $scope.toggleEnabled = function () {
             $scope.config.enabled = !$scope.config.enabled;
         };
 
@@ -16,7 +24,7 @@ admin.controller('adComtrolCtrl', ['$scope', 'config', 'adInterfacesSrv', 'ngDia
          *
          * @return {undefined}
          */
-        $scope.changeTab = function(name) {
+        $scope.changeTab = function (name) {
             $scope.state.activeTab = name;
         };
 
@@ -24,13 +32,13 @@ admin.controller('adComtrolCtrl', ['$scope', 'config', 'adInterfacesSrv', 'ngDia
          * when we clicked on save button
          * @return {undefined}
          */
-        $scope.saveSetup = function() {
+        $scope.saveSetup = function () {
             $scope.callAPI(adInterfacesSrv.updateSettings, {
                 params: {
                     settings: $scope.config,
                     integration: $scope.interface.toLowerCase()
                 },
-                onSuccess: function() {
+                onSuccess: function () {
                     $scope.errorMessage = '';
                     $scope.successMessage = 'SUCCESS: Settings updated!';
                 }
@@ -40,39 +48,51 @@ admin.controller('adComtrolCtrl', ['$scope', 'config', 'adInterfacesSrv', 'ngDia
         /**
          * Dialog methods
          */
-        $scope.onClickRegenerate = function() {
+        $scope.onClickRegenerate = function () {
             ngDialog.open({
                 template: '/assets/partials/interfaces/comtrol/adComtrolTokenWarning.html',
                 scope: $scope,
                 closeByDocument: true
             });
         };
-        $scope.closeDialog = function() {
+
+        $scope.closeDialog = function () {
             ngDialog.close();
         };
-        $scope.regenerateAuth = function() {
-            $scope.callAPI(adComtrolSrv.reAuthComtrol, {
-                successCallBack: function(response) {
-                    $scope.ifc_comtrol.authentication_token =  response.authentication_token;
+
+        $scope.regenerateAuth = function () {
+            $scope.callAPI(resetToken, {
+                successCallBack: function (response) {
+                    $scope.authentication_token = response.authentication_token;
                     $scope.closeDialog();
                 }
             });
         };
+
+        var loadToken = function () {
+            $scope.callAPI(fetchToken, {
+                successCallBack: function (response) {
+                    $scope.authentication_token = response.authentication_token;
+                }
+            });
+        };
+
         /**
          * Dialog methods
          */
 
-        (function() {
+        (function () {
             $scope.config = config;
+            loadToken();
             $scope.languages = [
-                { id: "0", name: "English" },
-                { id: "1", name: "Spanish" },
-                { id: "2", name: "French" },
-                { id: "3", name: "Italian" },
-                { id: "4", name: "Japanese" },
-                { id: "5", name: "Slovenian" },
-                { id: "6", name: "German" },
-                { id: "7", name: "Russian" }
+                {id: "0", name: "English"},
+                {id: "1", name: "Spanish"},
+                {id: "2", name: "French"},
+                {id: "3", name: "Italian"},
+                {id: "4", name: "Japanese"},
+                {id: "5", name: "Slovenian"},
+                {id: "6", name: "German"},
+                {id: "7", name: "Russian"}
             ];
         })();
     }
