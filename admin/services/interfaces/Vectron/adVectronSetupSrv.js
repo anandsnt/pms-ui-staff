3ae7d517d56cecc3440df4c3759739cf64ac4192
Shiju D <shiju-d@stayntouch.com>
2019-04-19 11:28:44 -0400

    CICO-64627: Cleanup the JS code

--- admin/services/interfaces/Vectron/adVectronSetupSrv.js
+++ admin/services/interfaces/Vectron/adVectronSetupSrv.js
@@ -1,102 +1,94 @@
 admin.service('adVectronSetupSrv', [
-    '$http', '$q', 'ADBaseWebSrvV2', function($http, $q, ADBaseWebSrvV2) {
+    '$http', '$q', 'ADBaseWebSrvV2',
+    function ($http, $q, ADBaseWebSrvV2) {
 
         var service = this;
-        var cache = {
-            'CHARGE_CODES': {},
-            'PAYMENT_CHARGE_CODES': {},
-            'POSTING_ACCOUNTS': {},
-            'MAPPING_TYPES': {}
-        }
+        var cache = {};
 
-        service.fetchMeta = function() {
+        service.fetchMeta = function () {
             var deferred = $q.defer(),
                 promises = [],
                 meta = {};
 
-            promises.push(service.getAllChargeCodes().
-            then(function(response) {
-                meta['charge_codes'] = response.data.charge_codes;
+            promises.push(service.getAllChargeCodes().then(function (response) {
+                meta.charge_codes = response.data.charge_codes;
             }));
 
-            promises.push(service.getAllPaymentChargeCodes().
-            then(function(response) {
-                meta['payment_charge_codes'] = response.data.charge_codes;
+            promises.push(service.getAllPaymentChargeCodes().then(function (response) {
+                meta.payment_charge_codes = response.data.charge_codes;
             }));
 
-            promises.push(service.getAllPostingAccounts().
-            then(function(response) {
-                meta['posting_accounts'] = response.posting_accounts;
+            promises.push(service.getAllPostingAccounts().then(function (response) {
+                meta.posting_accounts = response.posting_accounts;
             }));
 
-            promises.push(service.getAllMappingTypes().
-            then(function(response) {
-                meta['mapping_types'] = response.data;
+            promises.push(service.getAllMappingTypes().then(function (response) {
+                meta.mapping_types = response.data;
             }));
 
 
             $q.all(promises).
-            then(function() {
+            then(function () {
                 deferred.resolve(meta);
-            }, function(errorMessage) {
+            }, function (errorMessage) {
                 deferred.reject(errorMessage);
             });
 
             return deferred.promise;
         };
 
-        service.getAllChargeCodes = function() {
-            if (_.isEmpty(cache['CHARGE_CODES'])) {
-                cache['CHARGE_CODES'] = ADBaseWebSrvV2.getJSON('/admin/charge_codes/list.json?per_page=1000');
+        service.getAllChargeCodes = function () {
+            if (_.isEmpty(cache.charges_codes)) {
+                cache.charges_codes = ADBaseWebSrvV2.getJSON('/admin/charge_codes/list.json?per_page=1000');
             }
-            return cache['CHARGE_CODES']
+            return cache.charges_codes;
         };
 
-        service.getAllPaymentChargeCodes = function() {
-            if (_.isEmpty(cache['PAYMENT_CHARGE_CODES'])) {
-                cache['PAYMENT_CHARGE_CODES'] = ADBaseWebSrvV2.getJSON('/admin/charge_codes/list.json?per_page=1000&charge_code_type=PAYMENT');
+        service.getAllPaymentChargeCodes = function () {
+            if (_.isEmpty(cache.payment_charge_codes)) {
+                cache.payment_charge_codes = ADBaseWebSrvV2.getJSON('/admin/charge_codes/list.json?per_page=1000&charge_code_type=PAYMENT');
             }
-            return cache['PAYMENT_CHARGE_CODES']
+            return cache.payment_charge_codes;
         };
 
-        service.getAllPostingAccounts = function() {
-            if (_.isEmpty(cache['POSTING_ACCOUNTS'])) {
-                cache['POSTING_ACCOUNTS'] = ADBaseWebSrvV2.getJSON('api/posting_accounts.json');
+        service.getAllPostingAccounts = function () {
+            if (_.isEmpty(cache.posting_accounts)) {
+                cache.posting_accounts = ADBaseWebSrvV2.getJSON('api/posting_accounts.json');
             }
-            return cache['POSTING_ACCOUNTS']
+            return cache.posting_accounts;
         };
 
-        service.getAllMappingTypes = function() {
-            if (_.isEmpty(cache['MAPPING_TYPES'])) {
-                cache['MAPPING_TYPES'] = ADBaseWebSrvV2.getJSON('/ifc/proxy/mappings/types?integration=vectron');
+        service.getAllMappingTypes = function () {
+            if (_.isEmpty(cache.mapping_types)) {
+                cache.mapping_types = ADBaseWebSrvV2.getJSON('/ifc/proxy/mappings/types?integration=vectron');
             }
-            return cache['MAPPING_TYPES']
+            return cache.mapping_types;
         };
 
-        service.createMappings = function(params) {
+        service.createMappings = function (params) {
             return ADBaseWebSrvV2.postJSON('/ifc/vectron/mappings', params);
         };
 
-        service.fetchMappings = function(params) {
+        service.fetchMappings = function (params) {
             return ADBaseWebSrvV2.getJSON('/ifc/vectron/mappings', params);
         };
 
-        service.deleteMapping = function(id) {
+        service.deleteMapping = function (id) {
             return ADBaseWebSrvV2.deleteJSON('/ifc/vectron/mappings/' + id);
         };
 
-        service.updateMapping = function(params) {
+        service.updateMapping = function (params) {
             return ADBaseWebSrvV2.putJSON('/ifc/vectron/mappings/' + params.id, params);
         };
 
-        service.resetAuthToken = function() {
+        service.resetAuthToken = function () {
             return ADBaseWebSrvV2.postJSON('/api/integrations/vectron/reset_auth_token');
 
         };
 
-        service.saveMapping = function(params) {
+        service.saveMapping = function (params) {
             return ADBaseWebSrvV2.postJSON('/ifc/vectron/mappings', params);
         };
 
-    }])
-;
+    }
+]);
