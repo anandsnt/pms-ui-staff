3bbbd0006c3f4e2e8d352cdefba8ed595faa329b
ganeshsbhat <--replace-all>
2019-05-02 10:35:42 -0400

    CICO-63282 Added Alert when changing GL Account Number Length

--- admin/controllers/interfaces/Datev/adDatevCtrl.js
+++ admin/controllers/interfaces/Datev/adDatevCtrl.js
@@ -1,14 +1,15 @@
-admin.controller('adDatevCtrl', ['$scope', 'config', 'adInterfacesSrv',
-    function($scope, config, adInterfacesSrv) {
+admin.controller('adDatevCtrl', ['$scope', 'config', 'adInterfacesSrv', 'ngDialog',
+    function($scope, config, adInterfacesSrv, ngDialog) {
         BaseCtrl.call(this, $scope);
+        var glAccountNumberLength;
 
-         $scope.interface = 'DATEV';
+        $scope.interface = 'DATEV';
 
-         $scope.state = {
+        $scope.state = {
             activeTab: 'SETTING'
         };
 
-         $scope.toggleEnabled = function() {
+        $scope.toggleEnabled = function() {
             $scope.config.enabled = !$scope.config.enabled;
         };
 
@@ -20,11 +21,26 @@ admin.controller('adDatevCtrl', ['$scope', 'config', 'adInterfacesSrv',
             $scope.state.activeTab = name;
         };
 
+        $scope.acceptNewGlAccountNumber = function() {
+          glAccountNumberLength = $scope.config.gl_account_number_length;
+          $scope.saveSetup();
+          ngDialog.close();
+        }
+
          /**
          * when we clicked on save button
          * @return {undefined}
          */
         $scope.saveSetup = function() {
+            if($scope.config.gl_account_number_length && glAccountNumberLength !== $scope.config.gl_account_number_length){
+              ngDialog.open({
+                template: '/assets/partials/interfaces/datev/adGlAccountNumberChangeWarnPopup.html',
+                className: 'ngdialog-theme-default1 modal-theme1',
+                closeByDocument: true,
+                scope: $scope
+              });
+              return;
+            }
             $scope.callAPI(adInterfacesSrv.updateSettings, {
                 params: {
                     settings: $scope.config,
@@ -39,6 +55,7 @@ admin.controller('adDatevCtrl', ['$scope', 'config', 'adInterfacesSrv',
 
          (function() {
             $scope.config = config;
+            glAccountNumberLength = config.gl_account_number_length;
         })();
     }
 ]);
