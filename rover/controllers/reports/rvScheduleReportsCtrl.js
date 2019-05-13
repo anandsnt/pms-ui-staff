8ddfeee88dabe1f2445f5a2a00365331c200788d
soumyastayntouch <soumya@stayntouch.com>
2019-05-13 16:00:43 +0530

    CICO-65482: Fixed issue of file format

--- rover/controllers/reports/rvScheduleReportsCtrl.js
+++ rover/controllers/reports/rvScheduleReportsCtrl.js
@@ -944,6 +944,9 @@ angular.module('sntRover').controller('RVScheduleReportsCtrl', [
 
         // Checks whether file format dropdown should be shown or not
         $scope.shouldShowFileFormat = function (selectedEntity) {
+            if (selectedEntity.report.title === reportNames['COMPARISION_BY_DATE']) {
+                $scope.scheduleFormat = _.filter($scope.scheduleFormat, function(object){ return object.value !== "XML"; });
+            }
             return selectedEntity.report && selectedEntity.report.title === reportNames['COMPARISION_BY_DATE'];
         };
 
