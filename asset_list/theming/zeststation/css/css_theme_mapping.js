5d4c657f11fc00be4c8556b5f7245d9cc7c354ab
niyathiramesh <niyathi@stayntouch.com>
2019-05-14 14:52:15 +0530

    CICO-65428: Implement zest station for upstairs by mamas

--- asset_list/theming/zeststation/css/css_theme_mapping.js
+++ asset_list/theming/zeststation/css/css_theme_mapping.js
@@ -72,7 +72,8 @@ module.exports = {
             'gallivant': [themeCSSRoot + 'gallivant.less'],
             'hotel-e': [themeCSSRoot + 'hotel-e.less'],
             'kelley': [themeCSSRoot + 'kelley.less'],
-            'stare-miastro': [themeCSSRoot + 'stare-miastro.less']
+            'stare-miastro': [themeCSSRoot + 'stare-miastro.less'],
+            'upstairs-by-mamas': [themeCSSRoot + 'upstairs-by-mamas.less']
         };
     }
 };
