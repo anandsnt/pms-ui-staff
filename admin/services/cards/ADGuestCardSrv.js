1ac1c0bc47d28b0f1218e53000c85683f0c7d87d
soumyastayntouch <soumya@stayntouch.com>
2019-06-06 19:04:26 +0530

    CICO-64094: Guest cards mandatory settings

--- admin/services/cards/ADGuestCardSrv.js
+++ admin/services/cards/ADGuestCardSrv.js
@@ -8,6 +8,60 @@ function($q, ADBaseWebSrvV2) {
 			url = '/admin/guest_card_settings/current_settings';
 
 		ADBaseWebSrvV2.getJSON(url, params).then(function(data) {
+			data = {
+	"birth_place": {
+		"is_visible": true,
+		"is_mandatory_on_guest_card_creation": false
+	},
+	"registration_number": {
+		"is_visible": true,
+		"is_mandatory_on_guest_card_creation": false
+	},
+	"father_name": {
+		"is_visible": true,
+		"is_mandatory_on_guest_card_creation": false
+	},
+	"gender": {
+		"is_visible": true,
+		"is_mandatory_on_guest_card_creation": false
+	},
+	"home_town": {
+		"is_visible": true,
+		"is_mandatory_on_guest_card_creation": false
+	},
+	"mother_name": {
+		"is_visible": true,
+		"is_mandatory_on_guest_card_creation": false
+	},
+	"personal_id_no": {
+		"is_visible": true,
+		"is_mandatory_on_guest_card_creation": false
+	},
+	"place_of_residence": {
+		"is_visible": true,
+		"is_mandatory_on_guest_card_creation": false
+	},
+	"registraion_number": {
+		"is_visible": true,
+		"is_mandatory_on_guest_card_creation": false
+	},
+	"vehicle_country_mark": {
+		"is_visible": true,
+		"is_mandatory_on_guest_card_creation": false
+	},
+	"job_title": {
+		"is_visible": true,
+		"is_mandatory_on_guest_card_creation": false
+	},
+	"date_of_birth": {
+		"is_visible": true,
+		"is_mandatory_on_guest_card_creation": false
+	},
+	"nationality": {
+		"is_visible": true,
+		"is_mandatory_on_guest_card_creation": false
+	}
+};
 			deferred.resolve(data);
 		}, function(errorMessage) {
 			deferred.reject(errorMessage);
@@ -22,6 +76,7 @@ function($q, ADBaseWebSrvV2) {
 			url = '/admin/guest_card_settings/save';
 
 		ADBaseWebSrvV2.postJSON(url, params).then(function(data) {
+			
 			deferred.resolve(data);
 		}, function(errorMessage) {
 			deferred.reject(errorMessage);
