e8d33c323c275ac414c48994876c733522b8b01d
krishobh <krishobh@stayntouch.com>
2019-06-21 15:35:06 +0530

    CICO-66647 Handled toggle and reset button

--- rover/services/util/rvUtilSrv.js
+++ rover/services/util/rvUtilSrv.js
@@ -327,4 +327,58 @@ angular.module('sntRover').service('rvUtilSrv', ['$filter', function($filter) {
 			return feature;
 		};
 
+		/*
+         *  Utility method to Generate Time Duration List.
+         *  @param {string | null} [optional] - [minArrivalTime] 
+         *  @param {string | null} [optional] - [maxDepartureTime]
+         *  @return {Array} - [List of time objects having 12hr and 24hr formats]
+		 *
+         *  generateTimeDuration(null, '1:00')  => 
+         *	[{"12":"12:00 AM","24":"00:00"},{"12":"12:15 AM","24":"00:15"},{"12":"12:30 AM","24":"00:30"},{"12":"12:45 AM","24":"00:45"},{"12":"01:00 AM","24":"01:00"}]
+         *	
+         *  generateTimeDuration('1:00','2:00') =>
+         *	[{"12":"01:00 AM","24":"01:00"},{"12":"01:15 AM","24":"01:15"},{"12":"01:30 AM","24":"01:30"},{"12":"01:45 AM","24":"01:45"},{"12":"02:00 AM","24":"02:00"}]
+         *
+         *	generateTimeDuration('23:00', null) =>
+         *	[{"12":"11:00 PM","24":"23:00"},{"12":"11:15 PM","24":"23:15"},{"12":"11:30 PM","24":"23:30"},{"12":"11:45 PM","24":"23:45"}]
+         *	
+         *	generateTimeDuration() => Full list will return.
+         */
+        this.generateTimeDuration = function( minArrivalTime, maxDepartureTime ) {
+            var timeInterval = 15, // minutes interval
+                startTime = 0, // start time
+                endTime = (24 * 60) - timeInterval, // end time
+                ap = ['AM', 'PM'], // AM-PM
+
+                times = [], // time array - output array
+                twelveHrFormat = '',
+                twentyFourHrFormat = '',
+                hh = '',
+                mm = '',
+                obj = {};
+
+            if (minArrivalTime) {
+                startTime = minArrivalTime.split(':')[0] * 60 + minArrivalTime.split(':')[1] * 1;
+            }
+            if (maxDepartureTime) {
+                endTime = maxDepartureTime.split(':')[0] * 60 + maxDepartureTime.split(':')[1] * 1;
+            }
+
+            // loop to increment the time and push results in times array
+            for (var i = 0; startTime <= endTime; i++) {
+				hh = Math.floor(startTime / 60); // getting hours of day in 0-24 format
+				mm = (startTime % 60); // getting minutes of the hour in 0-55 format
+				twelveHrFormat = (("0" + hh % 12).slice(-2) === '00' ? '12' : ("0" + hh % 12).slice(-2)) + ':' + ("0" + mm).slice(-2) + " " + ap[Math.floor(hh / 12)]; // data in [12:00 AM- 12:00 PM format]
+				twentyFourHrFormat = ("0" + hh).slice(-2) + ':' + ("0" + mm).slice(-2); // data in [00:00 - 24:00 format]
+				obj = {
+					"12": twelveHrFormat,
+					"24": twentyFourHrFormat
+				};
+				times.push(obj);
+				startTime = startTime + timeInterval;
+            }
+
+            return times;
+        };
+
 }]);
