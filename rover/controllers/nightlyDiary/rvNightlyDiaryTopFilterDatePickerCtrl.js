ea6e14d23a83f20c189046a6e235a913824842a7
krishobh <krishobh@stayntouch.com>
2019-06-24 16:46:47 +0530

    CICO-66647 - resolved conflicts

--- rover/controllers/nightlyDiary/rvNightlyDiaryTopFilterDatePickerCtrl.js
+++ rover/controllers/nightlyDiary/rvNightlyDiaryTopFilterDatePickerCtrl.js
@@ -8,13 +8,34 @@ sntRover.controller('RVNightlyDiaryTopFilterDatePickerController', ['$scope', '$
 	}
 	else if ($scope.clickedFrom === 'BOOK_FILTER_ARRIVAL') {
 		$scope.date = $scope.diaryData.bookRoomViewFilter.fromDate;
-		minDateSelected = $scope.diaryData.fromDate;
-		maxDateSelected = $scope.diaryData.toDate;
+
+		// Logic to set minDateSelected.
+		var businessDateMinusOne = moment(tzIndependentDate($rootScope.businessDate)).subtract(1, 'days')
+                .format($rootScope.momentFormatForAPI);
+
+        if ($scope.diaryData.fromDate === businessDateMinusOne) {
+            minDateSelected = $rootScope.businessDate;
+        }
+        else {
+            minDateSelected = $scope.diaryData.fromDate;
+        }
+
+		// Logic to set maxDateSelected.
+		var dateDiff = moment($scope.diaryData.bookRoomViewFilter.toDate)
+                        .diff(moment($scope.diaryData.toDate), 'days');
+
+        // Selected book filter to-date is greater than the visible dates in diary.
+        // So we cannot show BOOK bar if data available, so resets from-date.
+        if (dateDiff > 0) {
+            maxDateSelected = $scope.diaryData.toDate;
+        }
+        else {
+            maxDateSelected = $scope.diaryData.bookRoomViewFilter.toDate;
+        }
 	}
 	else if ($scope.clickedFrom === 'BOOK_FILTER_DEPARTURE') {
 		$scope.date = $scope.diaryData.bookRoomViewFilter.toDate;
 		minDateSelected = $scope.diaryData.bookRoomViewFilter.fromDate;
-		maxDateSelected = $scope.diaryData.toDate;
 	}
 
 	$scope.setUpData = function() {
