0e1c73c86c81bfa86e9395e97a9c933c8d222edc
rahulkookal <rahul@stayntouch.com>
2019-06-27 13:25:09 +0530

    CICO-62387: Fixes for CC issues

--- admin/controllers/ADRatesSequenceCtrl.js
+++ admin/controllers/ADRatesSequenceCtrl.js
@@ -1,60 +1,60 @@
 admin.controller('ADRatesSequenceCtrl', ['$scope', 'ADRateSequenceSrv', '$anchorScroll', '$timeout', '$location',
 	function($scope, ADRateSequenceSrv, $anchorScroll, $timeout, $location) {
-    var isCustomRateSelected = function( dashboard ) {
-            return dashboard.value === 'CUSTOM_RATE';
-        },
-        fetchSelections = function() {
-            var onFetchSelectionsSuccess = function(data) {
-                $scope.sequenceState.selectedOptions = data;
-                $scope.showSearch = isCustomRateSelected( data.dashboard );
+        var isCustomRateSelected = function( dashboard ) {
+                return dashboard.value === 'CUSTOM_RATE';
+            },
+            fetchSelections = function() {
+                var onFetchSelectionsSuccess = function(data) {
+                    $scope.sequenceState.selectedOptions = data;
+                    $scope.showSearch = isCustomRateSelected( data.dashboard );
+                    $scope.$emit('hideLoader');
+                };
+
+                $scope.invokeApi(ADRateSequenceSrv.fetchSelections, {}, onFetchSelectionsSuccess);
+            },
+            initializeView = function() {
+                var onFetchPrefereneOptions = function(data) {
+                    $scope.sequenceState.availableOptions = data;
+                    fetchSelections();
+                };
+
+                $scope.invokeApi(ADRateSequenceSrv.fetchOptions, {}, onFetchPrefereneOptions);
+            };
+
+        $scope.sequenceState = {
+            availableOptions: {
+                room_rate: [],
+                rate_manager: [],
+                dashboard: []
+            },
+            selectedOptions: {
+                room_rates: {},
+                rate_manager: {},
+                dashboard: {}
+            }
+        };
+
+            // --------------------------------------------------------------------------- //
+
+
+        $scope.changePreference = function(type, option) {
+            $scope.sequenceState.selectedOptions[type] = angular.copy(option);
+            $scope.showSearch = isCustomRateSelected( option );
+        };
+
+        $scope.saveRateSortPreferences = function() {
+            var onSaveSuccess = function(data) {
                 $scope.$emit('hideLoader');
             };
 
-            $scope.invokeApi(ADRateSequenceSrv.fetchSelections, {}, onFetchSelectionsSuccess);
-        },
-        initializeView = function() {
-            var onFetchPrefereneOptions = function(data) {
-                $scope.sequenceState.availableOptions = data;
-                fetchSelections();
-            };
-
-            $scope.invokeApi(ADRateSequenceSrv.fetchOptions, {}, onFetchPrefereneOptions);
+            $scope.invokeApi(ADRateSequenceSrv.save, {
+                'room_rates': $scope.sequenceState.selectedOptions['room_rates'].id,
+                'rate_manager': $scope.sequenceState.selectedOptions['rate_manager'].id,
+                'dashboard': $scope.sequenceState.selectedOptions['dashboard'].id,
+                'dashboard_rate_id': $scope.sequenceState.selectedOptions['dashboard_rate'].id
+            }, onSaveSuccess);
         };
 
-    $scope.sequenceState = {
-        availableOptions: {
-            room_rate: [],
-            rate_manager: [],
-            dashboard: []
-        },
-        selectedOptions: {
-            room_rates: {},
-            rate_manager: {},
-            dashboard: {}
-        }
-    };
-
-		// --------------------------------------------------------------------------- //
-
-
-		$scope.changePreference = function(type, option) {
-			$scope.sequenceState.selectedOptions[type] = angular.copy(option);
-      	$scope.showSearch = isCustomRateSelected( option );
-    ;
-
-		$scope.saveRateSortPreferences = function() {
-			var onSaveSuccess = function(data) {
-				$scope.$emit('hideLoader');
-			};
-
-			$scope.invokeApi(ADRateSequenceSrv.save, {
-				"room_rates": $scope.sequenceState.selectedOptions['room_rates'].id,
-				"rate_manager": $scope.sequenceState.selectedOptions['rate_manager'].id,
-				"dashboard": $scope.sequenceState.selectedOptions['dashboard'].id,
-				"dashboard_rate_id": $scope.sequenceState.selectedOptions['dashboard_rate'].id
-			}, onSaveSuccess);
-		};
-
-		initializeView();
-	}
+        initializeView();
+    }
 ]);
