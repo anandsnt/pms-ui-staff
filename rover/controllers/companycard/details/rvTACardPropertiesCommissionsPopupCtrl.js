c051a2ef8f002f24e63362529c528b6980b68173
niyathiramesh <niyathi@stayntouch.com>
2019-07-02 20:15:09 +0530

    CICO-66671: Implement edit commissions popup

--- rover/controllers/companycard/details/rvTACardPropertiesCommissionsPopupCtrl.js
+++ rover/controllers/companycard/details/rvTACardPropertiesCommissionsPopupCtrl.js
@@ -7,7 +7,6 @@ sntRover.controller('rvTACardPropertiesCommissionsPopupCtrl', [
     'ngDialog',
     function ($scope, $rootScope, $filter, $timeout, util, ngDialog) {
         BaseCtrl.call(this, $scope);
-
         var scrollerOptions = {
             tap: true,
             preventDefault: false,
@@ -18,15 +17,22 @@ sntRover.controller('rvTACardPropertiesCommissionsPopupCtrl', [
             ngDialog.close();
         };
 
+        $scope.saveChanges = function () {
+            console.log('save');
+        };
+
+        $scope.clickedOnPopup = function (ev) {
+            ev.stopImmediatePropagation();
+            ev.stopPropagation();
+        };
+
         /*
         * Initialization method
         */
         var init = function () {
+            $scope.setScroller('rvTACardPropertiesCommissionsScroll', scrollerOptions);
             $timeout(function () {
-                $scope.setScroller('rvCompanyCardActivityLogScroll', scrollerOptions);
-            }, 500);
-            $timeout(function () {
-                $scope.refreshScroller('rvCompanyCardActivityLogScroll');
+                $scope.refreshScroller('rvTACardPropertiesCommissionsScroll');
             }, 2000);
         };
 
