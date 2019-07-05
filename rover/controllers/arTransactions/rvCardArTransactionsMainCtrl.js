cf9c78e3c1bae0e27ee358297e3929e340ac72fc
shahnaz <shahnaz@qburst.com>
2019-07-05 16:36:29 +0530

    CICO-66685 Conflicts resolved

--- rover/controllers/arTransactions/rvCardArTransactionsMainCtrl.js
+++ rover/controllers/arTransactions/rvCardArTransactionsMainCtrl.js
@@ -813,7 +813,7 @@ sntRover.controller('RVCompanyCardArTransactionsMainCtrl',
         // Send email AR statement
         $scope.emailArStatement = function() {
             var params = getParamsToSend();
-
+			$scope.printData.is_summary = $scope.filterData.isSummary;
             params.to_address = $scope.filterData.statementEmailAddress;
             $scope.closeDialog();
 
