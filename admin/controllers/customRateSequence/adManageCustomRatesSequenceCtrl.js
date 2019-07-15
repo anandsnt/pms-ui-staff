5c8c8d81020b74f25a60ad081e08fe84d31a9c4f
rahulkookal <rahul@stayntouch.com>
2019-07-15 13:09:27 +0530

    CICO-62392: Intermediate commit - integrate html/styles

--- admin/controllers/customRateSequence/adManageCustomRatesSequenceCtrl.js
+++ admin/controllers/customRateSequence/adManageCustomRatesSequenceCtrl.js
@@ -1,13 +1,41 @@
 admin.controller('ADManageCustomRatesSequenceCtrl', ['$scope', 'ADRateSequenceSrv', '$state', '$timeout',
     function($scope, ADRateSequenceSrv, $state, $timeout) {
         BaseCtrl.call(this, $scope);
+        var init = function() {
+                $scope.sequenceList = [];
+                $scope.selectedSequenceIndex = 0;
+                fetchSequences();
+            },
+            fetchSequences = function() {
+                var successCallBackFetchSequences = function (data) {
+                        $scope.sequenceList = data;
+                        $scope.selectSequence();
+                    },
+                    options = {
+                        successCallBack: successCallBackFetchSequences
+                    };
 
+                $scope.callAPI(ADRateSequenceSrv.listCustomSequence, options);
+            },
+            fetchRates = function() {
+
+            },
+            fetchAssignedRates = function() {
+
+            };
+
+        $scope.selectSequence = function() {
+            fetchRates();
+            fetchAssignedRates();
+        };
         $scope.backToRateSequence = function() {
             $state.go("admin.ratesSequence");
         };
         $scope.gotoManage = function () {
             $state.go("admin.customRatesSequence");
         };
+
+        init();
     }
 ]);
 
