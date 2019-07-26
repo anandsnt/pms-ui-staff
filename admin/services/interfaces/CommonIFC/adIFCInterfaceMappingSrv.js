3d26363a87d9c872cac363df722713ad7cbc969a
Jason Lombardozzi <jason@stayntouch.com>
2019-07-26 16:47:46 -0400

    CICO-64131

--- admin/services/interfaces/CommonIFC/adIFCInterfaceMappingSrv.js
+++ admin/services/interfaces/CommonIFC/adIFCInterfaceMappingSrv.js
@@ -12,7 +12,7 @@ admin.service('adIFCInterfaceMappingSrv', [
             'HOGIA': ['CHARGE_CODES'],
             'SUNACCOUNTING': ['CHARGE_CODES', 'MARKET_SEGMENTS'],
             'DERBYSOFT': ['CANCELLATION_POLICIES', 'CANCELLATION_CODES', 'TAX_CHARGE_CODES', 'TAX_CODES'],
-            'EXACTONLINE': ['CHARGE_CODES', 'TAX_CHARGE_CODES']
+            'EXACTONLINE': ['CHARGE_CODES', 'TAX_CHARGE_CODES'],
             'IGEL': ['CHARGE_CODES', 'PAYMENT_CODES'],
             'SAFEACCOUNTING': ['CHARGE_CODES', 'TAX_CHARGE_CODES'],
         };
