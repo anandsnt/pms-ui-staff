d02a5980707194b1cb059eeeb63e8e40c7a23823
soumyastayntouch <soumya@stayntouch.com>
2019-08-01 16:37:31 +0530

    CICO-67562: Add filter no qr code only

--- rover/controllers/financials/invoiceSearch/rvInvoiceSearchCtrl.js
+++ rover/controllers/financials/invoiceSearch/rvInvoiceSearchCtrl.js
@@ -118,14 +118,20 @@ sntRover.controller('RVInvoiceSearchController',
 					params = {
 						'query': $scope.invoiceSearchData.query,
 						'filter_id': $scope.invoiceSearchData.filter_id,
-						'no_folio_number_only': $scope.invoiceSearchData.no_folio_number_only,
-						'no_qr_code_only': $scope.invoiceSearchData.no_qr_code_only,
 						'page_no': page || 1,
 						'per_page': PER_PAGE,
 						'from_date': $scope.invoiceSearchData.from_date,
 						'to_date': $scope.invoiceSearchData.to_date
-					},
-					options = {
+					};
+
+					if ($scope.shouldShowInvoices) {
+						params.no_folio_number_only = $scope.invoiceSearchData.no_folio_number_only;
+					}
+					if ($scope.shouldShowReceipts) {
+						params.no_qr_code_only = $scope.invoiceSearchData.no_qr_code_only;
+					}
+
+					var options = {
 						params: params,
 						successCallBack: successCallBackOfSearchInvoice
 					};
