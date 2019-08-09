d30b68ecdeeb34bd5b92ee58e642414995010f2e
Dilip Sahayadhas <dilips@stayntouch.com>
2019-08-09 08:28:11 -0400

    CICO-68906
    Component to Handle notifications

--- shared/sntNotifications/app.js
+++ shared/sntNotifications/app.js
@@ -1,4 +1,4 @@
-angular.module('sntNotification', []);
+angular.module('sntNotification', ['sntFeatureToggles']);
 
 angular.module('sntNotification').service('sntNotifySrv', [function () {
     var service = this;
@@ -8,16 +8,33 @@ angular.module('sntNotification').service('sntNotifySrv', [function () {
     };
 }]);
 
-angular.module('sntNotification').directive('sntNotify', function () {
-    return {
-        link: function (scope, element, attrs) {
-            attrs.$observe('sntNotify', function (present, past) {
-                if (present) {
-                    angular.fromJson(present).forEach(message => {
-                        toastr.warning(message, 'ERR');
-                    });
-                }
-            });
-        }
-    };
+angular.module('sntNotification').component('sntNotify', {
+    template: '<div ng-show="$ctrl.message"' +
+        '           ng-class="{notice: $ctrl.message}"' +
+        '           class="error error_message"' +
+        '           ng-switch on="$ctrl.message.length"' +
+        '           ng-click="clearErrorMessage()">' +
+        '               <span class="close-btn" ng-click="clearErrorMessage()"></span>' +
+        '               <span ng-switch-when="1">{{$ctrl.message[0]}}</span>' +
+        '               <span ng-switch-default>' +
+        '                  <span ng-repeat="item in $ctrl.message track by $index" >' +
+        '                     <span ng-if=!$last>{{item}},</span>' +
+        '                     <span ng-if=$last> {{item}}</span>' +
+        '                  </span>' +
+        '               </span>' +
+        '       </div>',
+    transclude: true,
+    bindings: {
+        message: '<'
+    },
+    controller: ['sntNotifySrv', function (sntNotifySrv) {
+        var ctrl = this;
+
+        ctrl.$onChanges = function (changes) {
+            changes['message'] = changes['message'] || {};
+            if (changes['message'].currentValue) {
+                sntNotifySrv.warn(changes['message'].currentValue);
+            }
+        };
+    }]
 });
