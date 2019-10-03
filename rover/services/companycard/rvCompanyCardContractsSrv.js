352c10bd76ce7fedf69c5493dfc94b76766cd1df
krishobh <krishobh@stayntouch.com>
2019-10-03 08:25:45 +0530

    CICO-69249 Updated rvContractedNightsCtrl.js

--- rover/services/companycard/rvCompanyCardContractsSrv.js
+++ rover/services/companycard/rvCompanyCardContractsSrv.js
@@ -73,4 +73,16 @@ angular.module('sntRover').service('rvCompanyCardContractsSrv', ['$q', 'rvBaseWe
         return deferred.promise;
     };
 
+    this.updateNight = function(data) {
+        var deferred = $q.defer(),
+            url = '/api/accounts/' + data.account_id + '/contracts/' + data.contract_id + '/contract_nights';
+
+        rvBaseWebSrvV2.postJSON(url, data.postData).then(function(data) {
+            deferred.resolve(data);
+        }, function(data) {
+            deferred.reject(data);
+        });
+        return deferred.promise;
+    };
+
 }]);
