23fc0a1144b514836a894ac7f463f40d39613c65
krishobh <krishobh@stayntouch.com>
2019-10-03 08:10:44 +0530

    CICO-69249 Updated new srv

--- rover/services/companycard/rvCompanyCardContractsSrv.js
+++ rover/services/companycard/rvCompanyCardContractsSrv.js
@@ -14,4 +14,29 @@ angular.module('sntRover').service('rvCompanyCardContractsSrv', ['$q', 'rvBaseWe
         return deferred.promise;
     };
 
+    this.fetchContractsDetails = function(data) {
+        var deferred = $q.defer(),
+            url = '/api/accounts/' + data.account_id + '/contracts/' + data.contract_id;
+
+        rvBaseWebSrvV2.getJSON(url).then(function(data) {
+            deferred.resolve(data);
+        }, function(data) {
+            deferred.reject(data);
+        });
+        return deferred.promise;
+    };
+
+    this.fetchContractsList = function(data) {
+        var deferred = $q.defer(),
+            url = '/api/accounts/' + data.account_id + '/contracts';
+
+        rvBaseWebSrvV2.getJSON(url).then(function(data) {
+            contractRates = data;
+            deferred.resolve(data);
+        }, function(data) {
+            deferred.reject(data);
+        });
+        return deferred.promise;
+    };
+
 }]);
