271100fe6b86940bd3d061f81535b9d8582fc3aa
soumyastayntouch <soumya@stayntouch.com>
2019-10-04 13:17:34 +0530

    CICO-70588: Fixed issue of printing payment receipt along with ijnvoice

--- rover/controllers/bill/rvBillCardCtrl.js
+++ rover/controllers/bill/rvBillCardCtrl.js
@@ -3347,13 +3347,13 @@ sntRover.controller('RVbillCardController',
 			if (sntapp.cordovaLoaded) {
 				cordova.exec(billCardPrintCompleted,
 					function(error) {
-						billCardPrintCompleted();
+						receiptPrintCompleted();
 					}, 'RVCardPlugin', 'printWebView', []);
 			}
 			else
 			{
 				window.print();
-				// billCardPrintCompleted();
+				receiptPrintCompleted();
 			}
 		}, 1000);
 	});
