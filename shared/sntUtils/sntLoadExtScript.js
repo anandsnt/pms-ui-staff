e3ad7d759882634bc628a01ac96217166ca48457
krishobh <krishobh@stayntouch.com>
2019-10-11 19:15:49 +0530

    CICO-69533

--- shared/sntUtils/sntLoadExtScript.js
+++ shared/sntUtils/sntLoadExtScript.js
@@ -2,19 +2,19 @@ angular.module('snt.utils')
     .service('sntLoadScriptSrv', ['$q', '$timeout',
         function ($q, $timeout) {
             var service = this,
-                libraryServices = {};
+                loadedScripts = {};
 
             service.loadScript = function (url) {
                 var deferred = $q.defer();
 
-                if (libraryServices[url]) {
+                if (loadedScripts[url]) {
                     $timeout(function () {
                         deferred.resolve();
                     }, 100);
                 } else {
                     $.getScript(url)
                         .done(function () {
-                            libraryServices[url] = true;
+                            loadedScripts[url] = true;
                             deferred.resolve();
                         })
                         .fail(function (jqxhr, settings, exception) {
