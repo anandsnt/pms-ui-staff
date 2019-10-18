c6b44c5f148af1e063c0ce37283a02b8c7091b5b
niyathiramesh <niyathi@stayntouch.com>
2019-10-18 13:55:45 +0530

    CICO-70995: Add Admin UI for offline toggle and implement the functionality in reservation payment methods

--- payment/controllers/sntPaymentDirCtrl.js
+++ payment/controllers/sntPaymentDirCtrl.js
@@ -1033,7 +1033,9 @@ angular.module('sntPay').controller('sntPaymentController',
                 // If the changed payment type is CC and payment gateway is MLI show CC addition options
                 // If there are attached cards, show them first
                 if (!!selectedPaymentType && selectedPaymentType.name === 'CC') {
-                    if (PAYMENT_CONFIG[$scope.hotelConfig.paymentGateway].iFrameUrl) {
+                    if ($scope.selectedPaymentType === 'CC' && $scope.payment.shiji_token_enable_offline) {
+                        changeToCardAddMode();
+                    } else if (PAYMENT_CONFIG[$scope.hotelConfig.paymentGateway].iFrameUrl) {
                         // Add to guestcard feature for C&P
                         //  The payment info may change after adding a payment method; in such a case, should not reset back to C&P mode
                         $scope.selectedCC = $scope.selectedCC || {};
@@ -1140,6 +1142,7 @@ angular.module('sntPay').controller('sntPaymentController',
                 $scope.payment.linkedCreditCards = _.where(data.existing_payments, {
                     is_credit_card: true
                 });
+                $scope.payment.shiji_token_enable_offline = data.shiji_token_enable_offline
 
                 if ($scope.payment.linkedCreditCards.length > 0) {
                     refreshScroller('cardsList');
