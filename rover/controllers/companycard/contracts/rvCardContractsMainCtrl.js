6dc692f40c63572651c2c4472b807dbf0cf2c95f
krishobh <krishobh@stayntouch.com>
2019-11-08 17:48:40 +0530

    CICO-71557 Updated HTML changes and code clean

--- rover/controllers/companycard/contracts/rvCardContractsMainCtrl.js
+++ rover/controllers/companycard/contracts/rvCardContractsMainCtrl.js
@@ -14,7 +14,6 @@ angular.module('sntRover').controller('rvCardContractsMainCtrl', ['rvPermissionS
 				disableFields: false,
 				isPastContract: false,
 				noContracts: true,
-				noStatistics: true,			// TODO : Remove
 				selectedContractId: '',
 				rateSearchResult: [],
 				rateSearchQuery: '',
@@ -232,7 +231,6 @@ angular.module('sntRover').controller('rvCardContractsMainCtrl', ['rvPermissionS
 		 */
 		$scope.createFirstContract = function() {
 			$scope.contractData.mode = 'ADD';
-			// $scope.contractData.noContracts = false;
 			refreshContractScrollers();
 		};
 
@@ -241,7 +239,6 @@ angular.module('sntRover').controller('rvCardContractsMainCtrl', ['rvPermissionS
 		 */
 		$scope.moveToLinkContract = function() {
 			$scope.contractData.mode = 'LINK';
-			// $scope.contractData.noContracts = false;
 			refreshContractScrollers();
 		};
 		
