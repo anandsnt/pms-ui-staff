28be10aad35bdc9447f40aadd067bc6be03e3371
Mubarak Rocky <mubarakrocky@LAP-NY-JUSTIN.SNT>
2019-11-12 13:57:10 -0500

    CICO-68368 Added the CC payment type bank and commissions

--- rover/controllers/financials/ccTransactions/rvCcPaymentsCtrl.js
+++ rover/controllers/financials/ccTransactions/rvCcPaymentsCtrl.js
@@ -2,6 +2,10 @@ sntRover.controller('RVccPaymentsController', ['$scope', '$filter', '$stateParam
 
 	BaseCtrl.call(this, $scope);
 
+    $rootScope.manualCCEnabled = $rootScope.hotelDetails.payment_gateway === 'SHIJI' && $rootScope.hotelDetails.shiji_token_enable_offline;
+
+    console.log($rootScope.manualCCEnabled);
+
 	$scope.setScroller('payment_content', {});
     var refreshPaymentScroll = function() {
         setTimeout(function() {
