f6870fdb122715781b48d3628858b546fbdc2351
shahnaz067 <shahnaz@stayntouch.com>
2019-12-23 15:56:50 +0530

    CICO-73948 Decimal issue in deposit popup

--- payment/controllers/sntPaymentDirCtrl.js
+++ payment/controllers/sntPaymentDirCtrl.js
@@ -1039,7 +1039,7 @@ angular.module('sntPay').controller('sntPaymentController',
                 if (shouldReset) {
                     $scope.payment.selectedPaymentCurrencyId = $rootScope.hotelCurrencyId;
                     $scope.payment.selectedPaymentCurrencySymbol = $rootScope.currencySymbol;
-                    $scope.payment.amount = parseFloat(initialPaymentAmount);
+                    $scope.payment.amount = initialPaymentAmount;
                     $scope.feeData.calculatedFee = $scope.originalFee;
                 }
 
