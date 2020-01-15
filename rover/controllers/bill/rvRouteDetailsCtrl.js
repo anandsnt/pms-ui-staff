ee5980a8d37d7ac968e6eff3254da974dd1b6215
shahnaz067 <shahnaz@stayntouch.com>
2020-01-15 18:16:54 +0530

    cico-73967_ak fix

--- rover/controllers/bill/rvRouteDetailsCtrl.js
+++ rover/controllers/bill/rvRouteDetailsCtrl.js
@@ -1,4 +1,4 @@
-sntRover.controller('rvRouteDetailsCtrl', ['$scope', '$rootScope', '$filter', 'RVBillinginfoSrv', 'RVCompanyCardSrv', 'rvPermissionSrv', 'RVGuestCardSrv', 'ngDialog', 'RVBillCardSrv', 'RVPaymentSrv', function($scope, $rootScope, $filter, RVBillinginfoSrv, RVCompanyCardSrv, rvPermissionSrv, RVGuestCardSrv, ngDialog, RVBillCardSrv, RVPaymentSrv) {
+sntRover.controller('rvRouteDetailsCtrl', ['$scope', '$rootScope', '$filter', 'RVBillinginfoSrv', 'rvPermissionSrv', 'RVGuestCardSrv', 'ngDialog', 'RVBillCardSrv', 'RVPaymentSrv', function($scope, $rootScope, $filter, RVBillinginfoSrv, rvPermissionSrv, RVGuestCardSrv, ngDialog, RVBillCardSrv, RVPaymentSrv) {
     $scope.routeDetailsState = {
         newCard: null
     };
