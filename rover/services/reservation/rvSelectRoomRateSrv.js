a40c108cb7ebda4b512f172fa482c2f6444f239d
soumyastayntouch <soumya@stayntouch.com>
2020-01-20 13:14:28 +0530

    CICO-71925: Moved calculations from UI to API

--- rover/services/reservation/rvSelectRoomRateSrv.js
+++ rover/services/reservation/rvSelectRoomRateSrv.js
@@ -30,48 +30,112 @@ sntRover.service('RVSelectRoomRateSrv', ['$q', 'rvBaseWebSrvV2', 'dateFilter',
                 var restrictions = {},
                     amounts = {},
                     summary = [];
+                    
 
-                _.each(data.results, function(result, index) {
-                    // ---------------------------------------------Add HOUSE_FULL to the restrictions array
-                    // CICO-24923 Not needed in case of group bookings
-                    if (self.houseAvailability && !self.isGroupReservation && (index === 0 || index < data.results.length - 1)) {
-                        if (self.houseAvailability[result.date] < 1) {
-                            result.restrictions.push({
-                                restriction_type_id: 99,
-                                days: null
-                            });
-                        }
-                    }
+                    // _.each(data.dates, function(result, index) {
+                    //      _.each(result.restrictions, function(restriction) {
+                    //         if (!_.findWhere(summary, {
+                    //             restriction_type_id: restriction.restriction_type_id,
+                    //             days: restriction.days
+                    //         })) {
+                    //             summary.push(restriction);
+                    //         }
+                    //     });
+                    // });
 
-                    // --------------------------------------------- INVALID PROMO
-                    if (self.promotionValidity !== null && (index === 0 || index < data.results.length - 1)) {
-                        if (!self.promotionValidity[result.date]) {
-                            result.restrictions.push({
-                                restriction_type_id: 98,
-                                days: null
-                            });
-                        }
-                    }
 
-                    restrictions[result.date] = result.restrictions;
+                    // data.summary = summary;
+//                     data = {
+//     "total_room_cost": 40.0,
+//     "incl_stay_tax": 0,
+//     "excl_stay_tax": 0.66,
+//     "dates": {
+//         "2019-11-24": {
 
-                    amounts[result.date] = result.amount;
+//             "rate": "10.0",
+//             "restrictions": [
+//                 [],
+//                 []
+//             ],
+//             "addon": 10.0,
+//             "incl_tax": 0,
+//             "excl_tax": 0.33,
+//             "total": "20.33"
+//         },
+//         "2019-11-25": {
 
-                    _.each(result.restrictions, function(restriction) {
-                        if (!_.findWhere(summary, {
-                            restriction_type_id: restriction.restriction_type_id,
-                            days: restriction.days
-                        })) {
-                            summary.push(restriction);
-                        }
-                    });
-                });
+//             "rate": "10.0",
+//             "restrictions": [
+//                 [],
+//                 []
+//             ],
+//             "addon": 10.0,
+//             "incl_tax": 0,
+//             "excl_tax": 0.33,
+//             "total": "20.33"
+//         }
+//     },
+//     "is_eod_in_progress": false,
+//     "is_eod_manual_started": false,
+//     "is_eod_failed": true,
+//     "is_eod_process_running": false
+// };
+                    deferred.resolve(data);
 
-                deferred.resolve({
-                    summary: summary,
-                    dates: restrictions,
-                    amounts: amounts
-                });
+
+
+
+
+
+
+
+
+
+
+
+
+
+                // _.each(data.dates, function(result, index) {
+                //     // ---------------------------------------------Add HOUSE_FULL to the restrictions array
+                //     // CICO-24923 Not needed in case of group bookings
+                //     if (self.houseAvailability && !self.isGroupReservation && (index === 0 || index < data.dates.length - 1)) {
+                //         if (self.houseAvailability[result.date] < 1) {
+                //             result.restrictions.push({
+                //                 restriction_type_id: 99,
+                //                 days: null
+                //             });
+                //         }
+                //     }
+
+                //     // --------------------------------------------- INVALID PROMO
+                //     if (self.promotionValidity !== null && (index === 0 || index < data.dates.length - 1)) {
+                //         if (!self.promotionValidity[result.date]) {
+                //             result.restrictions.push({
+                //                 restriction_type_id: 98,
+                //                 days: null
+                //             });
+                //         }
+                //     }
+
+                //     restrictions[result.date] = result.restrictions;
+
+                //     amounts[result.date] = result.rate;
+
+                //     _.each(result.restrictions, function(restriction) {
+                //         if (!_.findWhere(summary, {
+                //             restriction_type_id: restriction.restriction_type_id,
+                //             days: restriction.days
+                //         })) {
+                //             summary.push(restriction);
+                //         }
+                //     });
+                // });
+
+                // deferred.resolve({
+                //     summary: summary,
+                //     dates: restrictions,
+                //     amounts: amounts
+                // });
 
             }, function(data) {
                 deferred.reject(data);
