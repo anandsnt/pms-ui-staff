d1f09007448858b6ef43406b6efe93614ff26722
vishnusnt <vishnu.prasad@shijigroup.com>
2020-01-23 17:42:41 +0530

    [CICO-74004] removed is_rate_shown_on_guest_bill - one unused dummy instance from UI

--- rover/services/reservation/RVReservationStateSrv.js
+++ rover/services/reservation/RVReservationStateSrv.js
@@ -69,7 +69,6 @@ angular.module('sntRover').service('RVReservationStateService', [
 				name: rateName,
 				description: rateDescription,
 				account_id: null,
-				is_rate_shown_on_guest_bill: false,
 				is_suppress_rate_on: false,
 				is_discount_allowed_on: true,
 				rate_type: {
