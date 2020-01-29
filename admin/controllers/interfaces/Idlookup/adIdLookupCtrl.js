4eba2b6a0754ae11ee9dbefbcc69433b7959d2fc
krishobh.valappil@shijigroup.com <krishobh@stayntouch.com>
2020-01-29 18:11:47 +0530

    CICO-74899 Setup controller

--- admin/controllers/interfaces/Idlookup/adIdLookupCtrl.js
+++ admin/controllers/interfaces/Idlookup/adIdLookupCtrl.js
@@ -1,7 +1,45 @@
-angular.module('admin').controller('adIdLookupCtrl', ['$scope', '$rootScope', 'ADIdLookupSrv', 'ngTableParams',
-    function($scope, $rootScope, ADIdLookupSrv, ngTableParams) {
+angular.module('admin').controller('adIdLookupCtrl', ['$scope', '$rootScope', 'ADIdLookupSrv', 'ngTableParams', '$timeout',
+    function($scope, $rootScope, ADIdLookupSrv, ngTableParams, $timeout) {
 
-      $scope.entities = ['Charge Codes', 'Addons', 'Rates', 'Room Types', 'Markets', 'Sources', 'Posting Accounts', 'Hotels'];
+      $scope.entities = [
+        { value: 'CHARGE_CODES', name: 'Charge Codes'},
+        { value: 'ADDONS', name: 'Addons'},
+        { value: 'RATES', name: 'Rates'},
+        { value: 'ROOM_TYPES', name: 'Room Types'},
+        { value: 'MARKETS', name: 'Markets'},
+        { value: 'SOURCES', name: 'Sources'},
+        { value: 'POSTING_ACCOUNTS', name: 'Posting Accounts'},
+        { value: 'HOTELS', name: 'Hotels'}
+      ];
+
+      $scope.filter = {
+        entity: 'CHARGE_CODES',
+        query: ''
+      };
+
+      $scope.search = function($defer, params) {
+          $scope.errorMessage = '';
+
+          $scope.callAPI(ADIdLookupSrv.search, {
+              params: _.extend($scope.filter, {
+                  page: params.page()
+              }),
+              successCallBack: function(response) {
+                  console.log(response);
+                  $timeout(function() {
+                      $scope.totalCount = response.total_count;
+                      $scope.totalPage = Math.ceil(response.total_count / $scope.displyCount);
+                      $scope.data = response.results;
+                      $scope.currentPage = params.page();
+                      params.total(response.total_count);
+                      $defer.resolve($scope.data);
+                  }, 500);
+              },
+              failureCallBack: function(errors) {
+                  $scope.errorMessage = errors;
+              }
+          });
+      };
 
       (function() {
           BaseCtrl.call(this, $scope);
@@ -10,7 +48,7 @@ angular.module('admin').controller('adIdLookupCtrl', ['$scope', '$rootScope', 'A
           $scope.tableParams = new ngTableParams(
               {
                   page: 1,
-                  count: $scope.displyCount || 10,
+                  count: $scope.displyCount || 25,
                   sorting: {
                       rate: 'asc'
                   }
