c705b38cefd8f1e16abdce6d7eb0c6201d262f6d
ragesh <ragesh@stayntouch.com>
2020-01-30 14:19:01 +0530

    CICO-71435 Implemented the refresh functionality

--- rover/controllers/search/rvReservationSearchWidgetCtrl.js
+++ rover/controllers/search/rvReservationSearchWidgetCtrl.js
@@ -1236,6 +1236,20 @@ sntRover.controller('rvReservationSearchWidgetController', ['$scope', '$rootScop
 			ngDialog.close();
 		};
 
-    	$scope.$on('ANALYTICS_VIEW_ACTIVE', $scope.clearResults);
+		$scope.$on('ANALYTICS_VIEW_ACTIVE', $scope.clearResults);
+		
+		/**
+		 * Refresh the reservation listing with open balance
+		 */
+		$scope.refreshReservationsWithOpenBalance = function () {
+			var reservationsFetchSuccess = function () {
+					$scope.fetchSearchResults();
+				};
+
+			$scope.callAPI(RVSearchSrv.refreshReservationsWithOpenBalance, {
+				onSuccess: reservationsFetchSuccess				
+			});
+
+		};
 	}
 ]);
