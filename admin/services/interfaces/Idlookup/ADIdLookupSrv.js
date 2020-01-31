2446fb664783d431977ca1e4b5ba117c3e00b9e3
ganeshsbhat <--replace-all>
2020-01-31 10:58:43 -0500

    CICO-74706 Removed unwanted comment

--- admin/services/interfaces/Idlookup/ADIdLookupSrv.js
+++ admin/services/interfaces/Idlookup/ADIdLookupSrv.js
@@ -3,12 +3,10 @@ angular.module('admin').service('ADIdLookupSrv', ['$http', '$q', 'sntBaseWebSrv'
 
         var service = this;
 
-        // Search API call.
         service.search = function (params) {
             return sntBaseWebSrv.getJSON('/admin/id_lookup', params);
         };
 
-        // Search API call.
         service.exportCSV = function (params) {
             return sntBaseWebSrv.download('/admin/id_lookup/export.csv?entity=' + params.entity, params);
         };
