08fbe8924d286098887ddfbede6ebe1dcc8f3a40
Samuel Catherman <samuel@stayntouch.com>
2020-02-03 11:10:25 -0500

    CICO-74914 d-edge admin settings UI

--- admin/adRouter/adminInterfacesRouter.js
+++ admin/adRouter/adminInterfacesRouter.js
@@ -986,4 +986,15 @@ angular.module('adminInterfacesRouter', []).config(function($stateProvider) {
         }
     });
 
+    $stateProvider.state('admin.d-edge', {
+        templateUrl: '/assets/partials/interfaces/d-edge/adDEdge.html',
+        controller: 'adDEdgeController',
+        url: '/d-edge',
+        resolve: {
+            config: [
+                'adInterfacesSrv', function (adInterfacesSrv) {
+                    return adInterfacesSrv.getSettings('dedge');
+                }]
+        }
+    });
 });
