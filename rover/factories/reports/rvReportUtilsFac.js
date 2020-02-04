fe4e1d29d48aac118d52ffa3584275362958bbca
ragesh <ragesh@stayntouch.com>
2020-02-04 15:52:04 +0530

    CICO-47082 Added the filters and time periods for the financial transactions - adjustment report

--- rover/factories/reports/rvReportUtilsFac.js
+++ rover/factories/reports/rvReportUtilsFac.js
@@ -2567,9 +2567,10 @@ angular.module('reportsModule')
          * Fill charge groups and charge codes values
          * @param {Object} filter filter object
          * @param {Object} filterValues - object holding filter values
+         * @param {String} reportName - name of the report
          * @return {void}
          */
-        factory.fillChargeGroupsAndChargeCodes = function(filter, filterValues) {
+        factory.fillChargeGroupsAndChargeCodes = function(filter, filterValues, reportName) {
             // Get the value of selectall checkbox for the charge groups
             var getChargeGroupSelectAllVal = function (chargeGroups) {
                     var selectAll = true;
@@ -2595,9 +2596,9 @@ angular.module('reportsModule')
                     return selectAll;
                 };
 
-
-            var populateChargeGroupsAndChargeCodes = function (chargeGroupsArr, chargeCodesArr) {
-                var processedCGCC = __adjustChargeGroupsCodes(chargeGroupsArr, chargeCodesArr, 'REMOVE_PAYMENTS', true),
+            // Populate charge groups and charge codes
+            var populateChargeGroupsAndChargeCodes = function (chargeGroupsArr, excludeChargeGroup, shouldPopulateChargeGroups, shouldPopulateChargeCodes, chargeCodesArr) {
+                var processedCGCC = __adjustChargeGroupsCodes(chargeGroupsArr, chargeCodesArr, excludeChargeGroup, true),
                     chargeGroupsCopy = angular.copy( processedCGCC.chargeGroups ),
                     chargeCodesCopy = angular.copy( processedCGCC.chargeCodes ),
                     selectedChargeGroupIdx,
@@ -2606,96 +2607,113 @@ angular.module('reportsModule')
                 delete filter.hasByChargeGroup;
                 delete filter.hasByChargeCode;
 
-                if (filterValues && filterValues.charge_group_ids) {
+                if (shouldPopulateChargeGroups) {
+                    if (filterValues && filterValues.charge_group_ids) {
                     
-                    chargeGroupsCopy = _.map(chargeGroupsCopy, function (chargeGroup) {
-                        selectedChargeGroupIdx = _.indexOf(filterValues.charge_group_ids, chargeGroup.id);
-                        chargeGroup.selected = false;
-                        if (selectedChargeGroupIdx > -1) {
-                            chargeGroup.selected = true;
-                        }
-
-                        return chargeGroup;
-                    });
-                } else if (filterValues && !filterValues.charge_group_ids) {
-                    chargeGroupsCopy = _.map(chargeGroupsCopy, function(chargeGroup) {
-                        chargeGroup.selected = false;
-
-                        return chargeGroup;
-                    });
-                }
-
-                filter.hasByChargeGroup = {
-                    data: chargeGroupsCopy,
-                    options: {
-                        selectAll: getChargeGroupSelectAllVal(chargeGroupsCopy),
-                        hasSearch: false,
-                        key: 'name'
-                    },
-                    affectsFilter: {
-                        name: 'hasByChargeCode',
-                        process: function(filter, selectedItems) {
-                            _.each(filter.originalData, function (od) {
-                                od.disabled = true;
-                            });
-                            /**/
-                            _.each(filter.originalData, function (od) {
-                                _.each(od.associcated_charge_groups, function (cg) {
-                                    _.each(selectedItems, function (si) {
-                                        if (cg.id === si.id) {
-                                            od.disabled = false;
-                                        }
+                        chargeGroupsCopy = _.map(chargeGroupsCopy, function (chargeGroup) {
+                            selectedChargeGroupIdx = _.indexOf(filterValues.charge_group_ids, chargeGroup.id);
+                            chargeGroup.selected = false;
+                            if (selectedChargeGroupIdx > -1) {
+                                chargeGroup.selected = true;
+                            }
+    
+                            return chargeGroup;
+                        });
+                    } else if (filterValues && !filterValues.charge_group_ids) {
+                        chargeGroupsCopy = _.map(chargeGroupsCopy, function(chargeGroup) {
+                            chargeGroup.selected = false;
+    
+                            return chargeGroup;
+                        });
+                    }
+    
+                    filter.hasByChargeGroup = {
+                        data: chargeGroupsCopy,
+                        options: {
+                            selectAll: getChargeGroupSelectAllVal(chargeGroupsCopy),
+                            hasSearch: false,
+                            key: 'name'
+                        },
+                        affectsFilter: {
+                            name: 'hasByChargeCode',
+                            process: function(filter, selectedItems) {
+                                _.each(filter.originalData, function (od) {
+                                    od.disabled = true;
+                                });
+                                /**/
+                                _.each(filter.originalData, function (od) {
+                                    _.each(od.associcated_charge_groups, function (cg) {
+                                        _.each(selectedItems, function (si) {
+                                            if (cg.id === si.id) {
+                                                od.disabled = false;
+                                            }
+                                        });
                                     });
                                 });
-                            });
-                            /**/
-                            filter.updateData();
+                                /**/
+                                filter.updateData();
+                            }
                         }
-                    }
-                };
-
-                if (filterValues && !!filterValues.charge_code_ids) {
-                    
-                    chargeCodesCopy = _.map(chargeCodesCopy, function (chargeCode) {
-                        selectedChargeCodeIdx = _.indexOf(filterValues.charge_code_ids, chargeCode.id);
-                        chargeCode.selected = false;
-                        if (selectedChargeCodeIdx > -1) {
-                            chargeCode.selected = true;
-                        }
-                        return chargeCode;
-                    });
-                } else if (filterValues && !filterValues.charge_code_ids) {
-                    chargeCodesCopy = _.map(chargeCodesCopy, function(chargeCode) {
-                        chargeCode.selected = false;
-
-                        return chargeCode;
-                    });
+                    };
                 }
 
-                filter.hasByChargeCode = {
-                    data: chargeCodesCopy,
-                    originalData: chargeCodesCopy,
-                    options: {
-                        selectAll: getChargeCodeSelectAllVal(chargeCodesCopy),
-                        hasSearch: false,
-                        key: 'name'
-                    },
-                    updateData: function() {
-                        var enabled = [];
-
-                        _.each (this.originalData, function (od) {
-                            if ( ! od.disabled ) {
-                                enabled.push(od);
+                if (shouldPopulateChargeCodes) {
+                    if (filterValues && !!filterValues.charge_code_ids) {
+                    
+                        chargeCodesCopy = _.map(chargeCodesCopy, function (chargeCode) {
+                            selectedChargeCodeIdx = _.indexOf(filterValues.charge_code_ids, chargeCode.id);
+                            chargeCode.selected = false;
+                            if (selectedChargeCodeIdx > -1) {
+                                chargeCode.selected = true;
                             }
+                            return chargeCode;
                         });
-                        this.data = enabled;
+                    } else if (filterValues && !filterValues.charge_code_ids) {
+                        chargeCodesCopy = _.map(chargeCodesCopy, function(chargeCode) {
+                            chargeCode.selected = false;
+    
+                            return chargeCode;
+                        });
                     }
-                };
+    
+                    filter.hasByChargeCode = {
+                        data: chargeCodesCopy,
+                        originalData: chargeCodesCopy,
+                        options: {
+                            selectAll: getChargeCodeSelectAllVal(chargeCodesCopy),
+                            hasSearch: false,
+                            key: 'name'
+                        },
+                        updateData: function() {
+                            var enabled = [];
+    
+                            _.each (this.originalData, function (od) {
+                                if ( ! od.disabled ) {
+                                    enabled.push(od);
+                                }
+                            });
+                            this.data = enabled;
+                        }
+                    };
+                }
 
             };
 
+            var excludeChargeGroup = 'NONE',
+                shouldPopulateChargeGroups = true,
+                shouldPopulateChargeCodes = true;
+
+            if (reportName === reportNames['DAILY_TRANSACTIONS']) {
+                excludeChargeGroup = 'REMOVE_PAYMENTS';
+            }
+
+            if (reportName === reportNames['FINANCIAL_TRANSACTIONS_ADJUSTMENT_REPORT']) {
+                shouldPopulateChargeGroups = false;
+                shouldPopulateChargeCodes = true; 
+            }
+
             reportsSubSrv.fetchChargeNAddonGroups().then(function(chargeGroupsArr) {
-                reportsSubSrv.fetchChargeCodes().then(populateChargeGroupsAndChargeCodes.bind(null, chargeGroupsArr));
+                reportsSubSrv.fetchChargeCodes().then(populateChargeGroupsAndChargeCodes.bind(null, chargeGroupsArr, excludeChargeGroup, shouldPopulateChargeGroups, shouldPopulateChargeCodes));
             });
             
         };
