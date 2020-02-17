39dcee1ff8551bfdd2d6f114916c73aa369edb6e
vishnukksnt <vishnukk@stayntouch.com>
2020-02-17 17:33:25 +0530

    Story defect

--- rover/controllers/rvAddonDatePickerCtrl.js
+++ rover/controllers/rvAddonDatePickerCtrl.js
@@ -1,8 +1,11 @@
 sntRover.controller('RVAddonDatePickerController', 
-    ['$scope', '$rootScope',
-    function($scope, $rootScope) {
+    ['$scope', '$rootScope', '$filter',
+    function($scope, $rootScope, $filter) {
 
     $scope.setUpData = function() {
+        var startDate = tzIndependentDate($scope.reservation.reservation_card.arrival_date),
+            endDate = tzIndependentDate($scope.reservation.reservation_card.departure_date);
+
         $scope.data = {};
         $scope.data.selectedDate = $scope.datePickerFor === 'start_date' ? $scope.selectedPurchesedAddon.start_date : $scope.selectedPurchesedAddon.end_date;
         $scope.dateOptions = {
@@ -10,6 +13,8 @@ sntRover.controller('RVAddonDatePickerController',
             changeYear: true,
             changeMonth: true,
             yearRange: "0:+10",
+            minDate: startDate,
+            maxDate: endDate,
             onSelect: function(dateText) {
                 $scope.dateSelected(dateText);
                 
