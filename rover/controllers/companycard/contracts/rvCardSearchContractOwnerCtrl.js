17eaffb1b25fb07985f390879491b2ad49f7fbb7
krishobh.valappil@shijigroup.com <krishobh@stayntouch.com>
2020-02-20 14:12:10 +0530

    Fix cc issues

--- rover/controllers/companycard/contracts/rvCardSearchContractOwnerCtrl.js
+++ rover/controllers/companycard/contracts/rvCardSearchContractOwnerCtrl.js
@@ -50,7 +50,7 @@ angular.module('sntRover').controller('rvCardSearchContractOwnerCtrl', ['$scope'
             var isNoContractOwner = false,
                 selectedOwner = $scope.contractData.contractOwner.selectedOwner;
 
-            if (contractData.contractOwner.results.length === 0 || selectedOwner.id === null || selectedOwner.id === '') {
+            if ($scope.contractData.contractOwner.results.length === 0 || selectedOwner.id === null || selectedOwner.id === '') {
                 isNoContractOwner = true;
             }
 
