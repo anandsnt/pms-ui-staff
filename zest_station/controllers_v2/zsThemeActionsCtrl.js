a2b2feb0dd982fa93a56a1da4b2b1955bc426dc7
vishnukksnt <vishnukk@stayntouch.com>
2020-03-02 19:01:51 +0530

    CICO-75513

--- zest_station/controllers_v2/zsThemeActionsCtrl.js
+++ zest_station/controllers_v2/zsThemeActionsCtrl.js
@@ -203,7 +203,8 @@ sntZestStation.controller('zsThemeActionsCtrl', [
                     'dimond-center',
                     'brooklyn',
                     'krowoderska',
-                    'bayou'
+                    'bayou',
+                    'moment'
                 ],
                 nonCircleNavIcons = ['public_v2'];// minor adjustment to the back/close icons for some themes (only show the inner x or <)
 
