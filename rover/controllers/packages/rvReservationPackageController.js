dc0327f3eebfc1aa3e4ba3d0c596f3b76cac50be
vishnukksnt <vishnukk@stayntouch.com>
2020-03-06 14:42:11 +0530

    story defect

--- rover/controllers/packages/rvReservationPackageController.js
+++ rover/controllers/packages/rvReservationPackageController.js
@@ -33,11 +33,11 @@ sntRover.controller('RVReservationPackageController',
 		// Get addon count
 		$scope.getAddonCount = function(amountType, postType, postingRythm, numAdults, numChildren, numNights, chargeFullWeeksOnly, quantity) {
 			if (!postingRythm) {
-				if (postType === 'WEEK' || postType === 'Every Week' || postType === 'WEEKLY') {
+				if (postType === 'WEEK' || postType === 'Every Week' || postType === 'WEEKLY' || postType === 'WEEKDAY' || postType === 'WEEKEND') {
 					postingRythm = 7;
 				} else if (postType === 'STAY' || postType === 'Entire Stay') {
 					postingRythm = 1;
-				} else if (postType === 'NIGHT' || postType === 'First Night') {
+				} else if (postType === 'NIGHT' || postType === 'First Night' || postType === 'LAST_NIGHT' || postType === 'CUSTOM') {
 					postingRythm = 0;
 				}
 			}
