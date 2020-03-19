45ae1afd84db7a0a5494407bf6be0233b4b780e4
shahnaz067 <shahnaz@stayntouch.com>
2020-03-19 12:25:20 +0530

    CICO-71296 Removed extra api call

--- rover/controllers/accounts/transactions/rvAccountTransactionsCtrl.js
+++ rover/controllers/accounts/transactions/rvAccountTransactionsCtrl.js
@@ -847,12 +847,6 @@ sntRover.controller('rvAccountTransactionsCtrl', [
 			} else if (action === "split") {
 				$scope.openSplitChargePopup();
 			} else if (action === "edit") {
-				$scope.callAPI(RVBillCardSrv.fetchAdjustmentReasons, {
-					successCallBack: function(response) {
-						$scope.adjustmentReasonOptions = response.force_adjustment_reasons;
-						$scope.showAdjustmentReason = response.force_adjustment_reason_enabled;
-					}
-				});
 				$scope.openEditChargePopup();
 			}
 		};
