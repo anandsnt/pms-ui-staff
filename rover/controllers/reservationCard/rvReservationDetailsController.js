f16ff773ff51273414d92b5b39a528adc3c582b6
rageshsnt <ragesh.kumar@shijigroup.com>
2020-03-27 15:59:22 +0530

    CICO-77090 Added room type id to the staydates hash, when stay is extended

--- rover/controllers/reservationCard/rvReservationDetailsController.js
+++ rover/controllers/reservationCard/rvReservationDetailsController.js
@@ -1476,7 +1476,8 @@ sntRover.controller('reservationDetailsController',
 						rateDetails: {
 							actual_amount: newDateDetails.rate_amount,
 							modified_amount: newDateDetails.rate_amount
-						}
+						},
+						roomTypeId: newDateDetails.room_type_id
 					};
 
 				}
