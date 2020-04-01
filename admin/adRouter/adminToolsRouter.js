fcf7b167aaad34c455aa06da0ea1d7f3bc56421d
Dilip Sahayadhas <dilips@stayntouch.com>
2020-04-01 07:49:30 -0400

    CICO-56771
    Remove Admin UI app and related assets from PMS repo

--- admin/adRouter/adminToolsRouter.js
+++
@@ -1,7 +0,0 @@
-angular.module('adminToolsRouter', []).config(function($stateProvider) {
-    $stateProvider.state('admin.telnet', {
-		url: '/admin/telnet',
-		templateUrl: '/assets/partials/tools/adTelnetInterface.html',
-		controller: 'adTelnetInterfaceCtrl'
-	});	
-});
