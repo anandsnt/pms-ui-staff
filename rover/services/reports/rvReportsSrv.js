09179fd5491821641aefd27e1d4bb33d365b6780
rageshsnt <ragesh.kumar@shijigroup.com>
2020-04-02 15:41:42 +0530

    CICO-77333 Configured the filter and time periods for the cancellation by cancel date export

--- rover/services/reports/rvReportsSrv.js
+++ rover/services/reports/rvReportsSrv.js
@@ -206,6 +206,28 @@ angular.module('sntRover').service('RVreportsSrv', [
                 'NEXT_THIRTY_DAYS',
                 'NEXT_MONTH',
                 'DATE_RANGE'
+            ],
+            'Cancellations by Cancel Date': [
+                'YESTERDAY',
+                'LAST_SEVEN_DAYS',
+                'LAST_THIRTY_DAYS',
+                'LAST_MONTH',
+                'LAST_JANUARY',
+                'LAST_FEBRUARY',
+                'LAST_MARCH',
+                'LAST_APRIL',
+                'LAST_MAY',
+                'LAST_JUNE',
+                'LAST_JULY',
+                'LAST_AUGUST',
+                'LAST_SEPTEMBER',
+                'LAST_OCTOBER',
+                'LAST_NOVEMBER',
+                'LAST_DECEMBER',
+                'NEXT_SEVEN_DAYS',
+                'NEXT_THIRTY_DAYS',
+                'NEXT_MONTH',
+                'DATE_RANGE'
             ]
         };
 
