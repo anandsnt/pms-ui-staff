ae2ac26779ee8ccba898db4bc67dce4692f3202b
vishnukk <vishnukk@stayntouch.com>
2020-04-02 19:17:21 +0530

    CICO-77290: zest station for sage inn v2

--- zest_station/services_v2/zsGeneralSrv.js
+++ zest_station/services_v2/zsGeneralSrv.js
@@ -139,7 +139,8 @@ sntZestStation.service('zsGeneralSrv', ['$http', '$q', 'zsBaseWebSrv', 'zsBaseWe
             "guestweb_sheffield_jonas_hotel": "jonas",
             "guestweb_westminster_hotel": "westminster",
             "guestweb_coyote_south": "coyote",
-            "guestweb_seehotel_hermitage": "hermitage"
+            "guestweb_seehotel_hermitage": "hermitage",
+            "guestweb_seehotel_sage_inn_v2": "sage-inn-v2"
         };
 
 
