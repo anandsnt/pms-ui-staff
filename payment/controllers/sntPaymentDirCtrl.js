341d9f2edb857ec80f0ad13947c2d9aadf0bd9a5
soumyastayntouch <soumya.cyriac@intl.shijigroup.com>
2020-04-15 14:40:44 +0530

    CICO-76625: Fixed issue of split payment

--- payment/controllers/sntPaymentDirCtrl.js
+++ payment/controllers/sntPaymentDirCtrl.js
@@ -1085,7 +1085,6 @@ angular.module('sntPay').controller('sntPaymentController',
                 if ($scope.selectedPaymentType !== '') {
                     $scope.$emit('PAYMENT_TYPE_CHANGED', $scope.selectedPaymentType);
                 }
-                // $scope.$emit('PAYMENT_TYPE_CHANGED', $scope.selectedPaymentType);
 
                 // -- CICO-33971 :: Direct Bill Payment --
                 if ($scope.selectedPaymentType === 'DB') {
