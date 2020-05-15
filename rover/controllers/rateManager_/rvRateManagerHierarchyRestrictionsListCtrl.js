5a492059d74d829ac1dc31f336665fc59b561d7a
krishobh <krishobh.valappil@stayntouch.com>
2020-05-15 20:35:13 +0530

    CICO-75895 Added delete

--- rover/controllers/rateManager_/rvRateManagerHierarchyRestrictionsListCtrl.js
+++ rover/controllers/rateManager_/rvRateManagerHierarchyRestrictionsListCtrl.js
@@ -16,10 +16,35 @@ angular.module('sntRover')
                 var refreshScroller = function() {
                     $scope.refreshScroller('hierarchyPopupListScroll');
                 };
-
-                $scope.clickedOnRemove = function( key, value ) {
+                
+                $scope.clickedOnRemove = function( key, value, index ) {
                     console.log(key);
                     console.log(value);
+
+                    let restrictionObj = {};
+                    restrictionObj[key] = value ? value : restrictionObj.listData[key].value;
+
+                    console.log(restrictionObj);
+
+                    var params = {
+                        from_date: $scope.ngDialogData.date,
+                        to_date: $scope.ngDialogData.date,
+                        restrictionObj
+                    }, 
+                    deleteSuccessCallback = () => {
+                        if (value) {
+                            delete restrictionObj.listData[key][index];
+                        }
+                        else {
+                            delete restrictionObj.listData[key];
+                        }
+                    }, 
+                    options = {
+                        params: params,
+                        onSuccess: deleteSuccessCallback
+                    };
+
+                    $scope.callAPI(hierarchySrv.deleteRestrictions, options);
                 };
 
                 setscroller();
