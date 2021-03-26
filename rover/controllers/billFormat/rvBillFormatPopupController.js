4682604a6410898ab36e776c2eb6eb48b161fe0f
mahi888 <mahesh@stayntouch.com>
2021-03-26 14:56:48 +0530

    CICO-80096: Add additional parameter to denote the linked account * This is only required for posting account * UI Disable account toggle for direct bill settlement print/email depending on this parameter

--- rover/controllers/billFormat/rvBillFormatPopupController.js
+++ rover/controllers/billFormat/rvBillFormatPopupController.js
@@ -14,6 +14,7 @@ sntRover.controller('rvBillFormatPopupCtrl', ['$scope', '$rootScope', '$filter',
                                                 && $scope.reservationBillData.is_bill_lock_enabled;
     $scope.billFormat.isInformationalInvoiceDisabled = $scope.isSettledBill
                                                 && $scope.reservationBillData.is_bill_lock_enabled;
+    $scope.disableToggleAccount = false;
     /*
     *  Get the request params for bill settings info
     */
@@ -139,6 +140,18 @@ sntRover.controller('rvBillFormatPopupCtrl', ['$scope', '$rootScope', '$filter',
              */
             if (response.data && response.data.default_bill_settings) {
                 response.data.default_bill_settings = response.data.default_bill_settings.toString();
+            }
+            /** CICO-80096
+             * Disable the invoice company/TA toggle based on the direct bill settlement account type 
+             */
+            var dbAccountType = response.data && response.data.bill_paid_account_type;
+
+            if (dbAccountType === 'COMPANY') {
+                $scope.isCompanyCardInvoice = true;
+                $scope.disableToggleAccount = true;
+            } else if (dbAccountType === 'TRAVELAGENT') {
+                $scope.isCompanyCardInvoice = false;
+                $scope.disableToggleAccount = true;
             }
 
             $scope.data = response.data;
