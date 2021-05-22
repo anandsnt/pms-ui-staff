f32c7f15f0aeb54aa13ede128ed843db2c011867
Oleg Maximov <oleg.maximoff@gmail.com>
2021-05-22 16:22:02 +0300

    CICO-84766

--- rover/controllers/reports/rvScheduleReportsCtrl.js
+++ rover/controllers/reports/rvScheduleReportsCtrl.js
@@ -2007,7 +2007,6 @@ angular.module('sntRover').controller('RVScheduleReportsCtrl', [
                     selectedEntity.report.title === reportNames['FINANCIAL_TRANSACTIONS_ADJUSTMENT_REPORT'] ||
                     selectedEntity.report.title === reportNames['ACTIONS_MANAGER'] ||
                     selectedEntity.report.title === reportNames['CREDIT_CHECK_REPORT'] ||
-                    selectedEntity.report.title === reportNames['FORECAST_BY_DATE'] || 
                     selectedEntity.report.title === reportNames['RESERVATIONS_BY_USER'] ||
                     selectedEntity.report.title === reportNames['ROOM_STATUS_REPORT'] ||
                     selectedEntity.report.title === reportNames['RATE_ADJUSTMENTS_REPORT'] || 
@@ -2023,7 +2022,8 @@ angular.module('sntRover').controller('RVScheduleReportsCtrl', [
                 $scope.scheduleFormat = _.filter(originalScheduleFormats, function(object) {
                     return object.value === 'CSV';
                 });
-            } else if (selectedEntity.report && selectedEntity.report.title === reportNames['GUEST_BALANCE_REPORT']) {
+            } else if (selectedEntity.report && (selectedEntity.report.title === reportNames['GUEST_BALANCE_REPORT'] ||
+                selectedEntity.report.title === reportNames['FORECAST_BY_DATE'] )) {
                 $scope.scheduleFormat = _.filter(originalScheduleFormats, function(object) {
                     return object.value === 'CSV' || object.value === 'PDF';
                 });
